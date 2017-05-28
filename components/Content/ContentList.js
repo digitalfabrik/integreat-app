2a7fa4692b17eff3f7d57c848ce7f330dcc6411e
Max Ammann <maximilian.ammann@googlemail.com>
2017-05-28 20:03:01 +0200

    Add simple style for ContentList

--- components/Content/ContentList.js
+++ components/Content/ContentList.js
@@ -6,22 +6,44 @@ import { values } from 'lodash/object'
 import { PageModel } from '../../src/endpoints/page'
 import { Link } from 'react-router-dom'
 
+import style from './ContentList.pcss'
+
+class ContentListElement extends React.Component {
+  static propTypes = {
+    page: PropTypes.instanceOf(PageModel).isRequired,
+    url: PropTypes.string.isRequired
+  }
+
+  render () {
+    return (
+      <Link to={this.props.url + '/' + this.props.page.id}>
+        <div className={style.element}>
+          <img className={style.elementImage} src={this.props.page.thumbnail}/>
+          {this.props.page.title}
+        </div>
+      </Link>
+    )
+  }
+}
+
 class ContentList extends React.Component {
   static propTypes = {
     page: PropTypes.instanceOf(PageModel).isRequired,
-    url: PropTypes.string
+    url: PropTypes.string.isRequired
   }
 
   render () {
     return (
       <div>
-        {
-          values(this.props.page.children).map(page => {
-            return <Link key={page.id} to={this.props.url + (this.props.url.length === 0 ? '' : '/') + page.id}>
-              {page.title}
-            </Link>
-          })
-        }
+        <div>
+          <img className={style.headingImage} src={this.props.page.thumbnail}/>
+          <div className={style.headingText}>{this.props.page.title}</div>
+        </div>
+        <div>
+          {values(this.props.page.children).map(page => {
+            return <ContentListElement key={page.id} url={this.props.url} page={page}/>
+          })}
+        </div>
       </div>
     )
   }
