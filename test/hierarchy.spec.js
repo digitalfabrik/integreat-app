a54c2ff258a05a7aa63a6a75b1052e75c544fcae
Max Ammann <maximilian.ammann@googlemail.com>
2017-06-28 17:12:33 +0200

    Fix tests

--- test/hierarchy.spec.js
+++ test/hierarchy.spec.js
@@ -3,19 +3,30 @@ import Hierarchy from 'location/hierarchy'
 import { PageModel } from 'endpoints/page'
 
 describe('hierarchy', () => {
+  let hierarchy = new Hierarchy('/123/456')
 
-  it('test', () => {
-    let hierarchy = new Hierarchy('/123/')
+  let child2 = new PageModel(456, 'Another Simple Page', 0)
+  let child1 = new PageModel(123, 'Simple Page', 0, 'Content', null, {456: child2})
+  let root = new PageModel(0, 'Augsburg', 0, 'Content', null, {123: child1})
 
-    let child = new PageModel(123, 'Simple Page', 0)
-    let rootChildren = {123: child}
-    let root = new PageModel(0, 'Augsburg', 0, 'Content', null, rootChildren)
+  hierarchy.build(root)
 
-    hierarchy.build(root)
+  it('building', () => {
+
     let pages = hierarchy.pages
 
     expect(pages[0]).to.deep.equal(root, 'The root page should be the first page')
-    expect(pages[1]).to.deep.equal(child, 'The child page does not equal')
+    expect(pages[1]).to.deep.equal(child1, 'The child1 page does not equal')
+    expect(pages[2]).to.deep.equal(child2, 'The child2 page does not equal')
   })
 
+  it('path mappings', () => {
+    let paths = []
+
+    hierarchy.map((page, path) => {
+      paths.push(path)
+    })
+
+    expect(paths).to.deep.equal(['', '/123', '/123/456'])
+  })
 })
