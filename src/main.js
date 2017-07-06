b38c024c19d4503756323bdddaa1db7819d5335a
Max Ammann <maximilian.ammann@googlemail.com>
2017-07-06 10:54:22 +0200

    Fix HMR

--- src/main.js
+++ src/main.js
@@ -22,45 +22,35 @@ import store from './store'
 import i18n from './i18n/i18n'
 
 /**
- * The root component of our app
- */
-class App extends React.Component {
-  static propTypes = {
-    history: PropTypes.any.isRequired
-  }
-
-  render () {
-    return (
-      <I18nextProvider i18n={ i18n }>
-        <Provider store={store}>
-          <Router history={this.props.history}>
-            <Switch>
-              {/* The root page */}
-              <Route path="/" exact component={LandingPage}/>
-              {/* The location page */}
-              <Route path="/location/:location/search" exact component={SearchPage}/>
-              <Route path="/location/:location/:path*" render={props => {
-                let path = props.match.params.path
-                return <LocationPage {...props} hierarchy={new Hierarchy(path)}/>
-              }}/>
-              {/* The error page */}
-              <Route component={ErrorPage}/>
-            </Switch>
-          </Router>
-        </Provider>
-      </I18nextProvider>
-    )
-  }
-}
-
-const container = document.getElementById('container')
-
-/**
  * Holds the current history implementation
  */
 export const history = createBrowserHistory()
 
-ReactDOM.render(<App history={history}/>, container)
+/**
+ * The root component of our app
+ */
+let App = <I18nextProvider i18n={ i18n }>
+  <Provider store={store}>
+    <Router history={history}>
+      <Switch>
+        {/* The root page */}
+        <Route path="/" exact component={LandingPage}/>
+        {/* The location page */}
+        <Route path="/location/:location/search" exact component={SearchPage}/>
+        <Route path="/location/:location/:path*" render={props => {
+          let path = props.match.params.path
+          return <LocationPage {...props} hierarchy={new Hierarchy(path)}/>
+        }}/>
+        {/* The error page */}
+        <Route component={ErrorPage}/>
+      </Switch>
+    </Router>
+  </Provider>
+</I18nextProvider>
+
+const container = document.getElementById('container')
+
+ReactDOM.render(App, container)
 
 // Sets the splash to hidden when the page is rendered
 document.getElementById('splash').className += ' splash-hidden'
