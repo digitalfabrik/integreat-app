b60be3674a499ab201a780cd5d6fb23eecf9e32d
Max Ammann <maximilian.ammann@googlemail.com>
2017-07-06 10:27:10 +0200

    Fix error page

--- src/ErrorPage/index.js
+++ src/ErrorPage/index.js
@@ -1,20 +1,53 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
-import NAVIGATION from 'Navigation'
-import Payload from 'endpoints/Payload'
 import Layout from 'Layout/Layout'
 import Error from 'components/Error/Error'
+import LANGUAGE_ENDPOINT from 'endpoints/language'
 
 class ErrorPage extends React.Component {
+  static propTypes = {
+    language: PropTypes.string.isRequired
+  }
+
+  componentWillUnmount () {
+    this.props.dispatch(LANGUAGE_ENDPOINT.invalidateAction())
+  }
+
+  componentWillMount () {
+    let location = this.getLocation()
+    if (!location) {
+      // Don't fetch language options if we have an invalid URL for example
+      return
+    }
+    this.props.dispatch(LANGUAGE_ENDPOINT.fetchEndpointAction({
+      location: location,
+      language: this.props.language
+    }))
+  }
+
+  getLocation () {
+    return this.props.match.params.location
+  }
+
   render () {
     return (
-      <Layout languageCallback={(code) => { /* todo */ }}
-              languagePayload={new Payload()}
-              navigation={NAVIGATION}>
+      <Layout currentLanguage={this.props.language}>
         <Error error="errors:page.notFound"/>
       </Layout>
     )
   }
 }
 
-export default ErrorPage
+/**
+ * @param state The current app state
+ * @return {{locations: {}}}  The endpoint values from the state mapped to props
+ */
+function mapStateToProps (state) {
+  return ({
+    language: state.language.language
+  })
+}
+
+export default connect(mapStateToProps)(ErrorPage)
