95b4445f8915825624798acb9701eef9028c625e
St <klinzo@yahoo.de>
2017-07-10 14:05:44 +0200

    Add disclaimer endpoint

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -2,10 +2,12 @@ import LANGUAGE_ENDPOINT from './language'
 import LOCATION_ENDPOINT from './location'
 import PAGE_ENDPOINT from './page'
 import EVENT_ENDPOINT from './event'
+import DISCLAIMER_ENDPOINT from './disclaimer'
 
 export default [
   LANGUAGE_ENDPOINT,
   LOCATION_ENDPOINT,
   PAGE_ENDPOINT,
-  EVENT_ENDPOINT
+  EVENT_ENDPOINT,
+  DISCLAIMER_ENDPOINT
 ]
