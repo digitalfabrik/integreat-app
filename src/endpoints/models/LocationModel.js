1810d67f4a53a1829f9104d874b5460c2cc3d550
Schedulaar <marklmichael98@gmail.com>
2017-07-28 00:15:20 +0200

    wrap category setter into own function

--- src/endpoints/models/LocationModel.js
+++ src/endpoints/models/LocationModel.js
@@ -7,6 +7,7 @@ export default class LocationModel {
     this._code = name
     this._name = path
     this._live = live
+    this.setCategory()
   }
 
   get live () {
@@ -22,12 +23,16 @@ export default class LocationModel {
   }
 
   get category () {
+    return this._category
+  }
+
+  setCategory () {
     let key = this._code.toLowerCase()
     for (let i = 0; i < IGNORE_PREFIXES.length; i++) {
       if (key.startsWith(IGNORE_PREFIXES[i] + ' ')) {
         key = key.substring(IGNORE_PREFIXES[i].length + 1)
       }
     }
-    return isEmpty(key) ? '?' : key[0].toUpperCase()
+    this._category = isEmpty(key) ? '?' : key[0].toUpperCase()
   }
 }
