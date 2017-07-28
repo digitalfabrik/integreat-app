58fd2dabda1fcd731622f779d86226fa5047d183
Schedulaar <marklmichael98@gmail.com>
2017-07-28 17:33:23 +0200

    Use Lambda Expression

--- src/endpoints/models/LocationModel.js
+++ src/endpoints/models/LocationModel.js
@@ -1,4 +1,5 @@
 import { isEmpty } from 'lodash/lang'
+import { find } from 'lodash/collection'
 
 const IGNORE_PREFIXES = ['stadt', 'kreis', 'landkreis']
 
@@ -27,13 +28,9 @@ export default class LocationModel {
   }
 
   setCategory () {
-    let key = this._code.toLowerCase()
-    for (let i = 0; i < IGNORE_PREFIXES.length; i++) {
-      if (key.startsWith(IGNORE_PREFIXES[i] + ' ')) {
-        key = key.substring(IGNORE_PREFIXES[i].length + 1)
-        break
-      }
-    }
+    let key = this._code.toLowerCase().trim()
+    let prefix = find(IGNORE_PREFIXES, (pre) => key.startsWith(pre + ' '))
+    key = prefix ? key.substring(prefix.length).trimStart() : key
     this._category = isEmpty(key) ? '?' : key[0].toUpperCase()
   }
 }
