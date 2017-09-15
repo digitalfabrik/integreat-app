8c69eaa99d7bb7bf87a1dc170e0c179373311546
Max Ammann <maximilian.ammann@googlemail.com>
2017-09-15 14:08:34 +0200

    WEBAPP-9: Fix test

--- test/hierarchy.spec.js
+++ test/hierarchy.spec.js
@@ -1,26 +1,27 @@
 import { expect } from 'chai'
 import Hierarchy from 'routes/LocationPage/Hierarchy'
 import PageModel from '../src/endpoints/models/PageModel'
+import { describe, it } from 'mocha'
 
 describe('hierarchy', () => {
-  let hierarchy = new Hierarchy('/123/456')
+  const hierarchy = new Hierarchy('/123/456')
 
-  let child2 = new PageModel(456, 'Another Simple Page', 0)
-  let child1 = new PageModel(123, 'Simple Page', 0, 'Content', null, {456: child2})
-  let root = new PageModel(0, 'Augsburg', 0, 'Content', null, {123: child1})
+  const child2 = new PageModel(456, '456', 'Another Simple Page', 0)
+  const child1 = new PageModel(123, '123', 'Simple Page', 0, 'Content', null, {456: child2})
+  const root = new PageModel(0, '0', 'Augsburg', 0, 'Content', null, {123: child1})
 
   hierarchy.build(root)
 
-  it('building', () => {
+  it('should build', () => {
 
-    let pages = hierarchy.pages
+    const pages = hierarchy.pages
 
     expect(pages[0]).to.deep.equal(root, 'The root page should be the first page')
     expect(pages[1]).to.deep.equal(child1, 'The child1 page does not equal')
     expect(pages[2]).to.deep.equal(child2, 'The child2 page does not equal')
   })
 
-  it('path mappings', () => {
+  it('should map paths', () => {
     let paths = []
 
     hierarchy.map((page, path) => {
