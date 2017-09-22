8e3b3305d1d7624557bec7dcbf501491cc7d546e
Schedulaar <marklmichael98@gmail.com>
2017-09-23 00:17:52 +0200

    WEBAPP-43: Introduce LocationHome

---
+++ src/components/Content/LocationHome.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { translate } from 'react-i18next'
+import { connect } from 'react-redux'
+import { Link } from 'redux-little-router'
+import { Row } from 'react-flexbox-grid'
+import FontAwesome from 'react-fontawesome'
+
+import PageModel from '../../endpoints/models/PageModel'
+import { EventsFetcher } from '../../endpoints'
+import EventModel from '../../endpoints/models/EventModel'
+import Navigation from '../../Navigation'
+import style from './LocationHome.css'
+import Categories from './Categories'
+
+const LocationHomeAdapter = connect((state) => ({ language: state.router.params.language, location: state.router.params.location }))(translate('common')(
+  class extends React.Component {
+    static propTypes = {
+      parentPage: PropTypes.instanceOf(PageModel).isRequired,
+      categories: PropTypes.arrayOf(PropTypes.shape({
+        page: PropTypes.instanceOf(PageModel).isRequired,
+        url: PropTypes.string.isRequired
+      })).isRequired,
+      events: PropTypes.arrayOf(PropTypes.instanceOf(EventModel)).isRequired
+    }
+    render () {
+      const t = this.props.t
+      return <div>
+        { this.props.events.length !== 0 ? <Link className={style.events} href={new Navigation(this.props.location, this.props.language).events}>
+              <FontAwesome name="calendar" className={style.calendarIcon}/>
+              <div className={style.eventsContainer}>
+                <div><strong>{t('common:currentEvents')}:</strong></div>
+                { this.props.events.map((event) => <div key={event.id} className={style.event}>{ event.title }</div>) }
+              </div>
+            </Link> : '' }
+        <Categories categories={this.props.categories}/>
+        { this.props.events.length === 0 ? <Row className={style.noEvents}>{t('common:thereAreCurrentlyNoEvents')}</Row> : '' }
+      </div>
+    }
+  }))
+
+export default class LocationHome extends React.Component {
+  static propTypes = {
+    parentPage: PropTypes.instanceOf(PageModel).isRequired,
+    categories: PropTypes.arrayOf(PropTypes.shape({
+      page: PropTypes.instanceOf(PageModel).isRequired,
+      url: PropTypes.string.isRequired
+    })).isRequired
+  }
+
+  render () {
+    return (
+      <EventsFetcher>
+        <LocationHomeAdapter categories={ this.props.categories } parentPage={ this.props.parentPage }/>
+      </EventsFetcher>
+    )
+  }
+}
