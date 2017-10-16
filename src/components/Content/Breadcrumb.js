768d15f53902a9b3c99af9dcdabd443f25ee1792
Max Ammann <maximilian.ammann@googlemail.com>
2017-10-16 19:58:08 +0200

    WEBAPP-42: Use LocationFetcher in Breadcrumb

--- src/components/Content/Breadcrumb.js
+++ src/components/Content/Breadcrumb.js
@@ -4,8 +4,45 @@ import PropTypes from 'prop-types'
 import style from './Breadcrumb.css'
 import Hierarchy from 'routes/LocationPage/Hierarchy'
 import { Link } from 'redux-little-router'
+import LocationModel from '../../endpoints/models/LocationModel'
+import { LocationFetcher } from '../../endpoints/index'
 
-export default class Breadcrumb extends React.Component {
+class BreadcrumbAdapter extends React.Component {
+  static propTypes = {
+    location: PropTypes.string.isRequired,
+    language: PropTypes.string.isRequired,
+    hierarchy: PropTypes.instanceOf(Hierarchy).isRequired,
+    locations: PropTypes.arrayOf(PropTypes.instanceOf(LocationModel))
+  }
+
+  getFormattedTitle (page) {
+    const location = this.props.locations.find((location) => location.code === page.title)
+
+    if (location) {
+      return location.name
+    }
+
+    return null
+  }
+
+  render () {
+    const hierarchy = this.props.hierarchy
+    return (
+      <div className={style.breadcrumbs}>{hierarchy.map((page, path) => {
+        return (
+          <Link key={page.id} className={style.breadcrumb}
+                href={`/${this.props.location}/${this.props.language}${path}`}>
+            <span className={style.separator}/>
+            <span className={style.level}>{this.getFormattedTitle(page) || page.title}</span>
+          </Link>
+        )
+      })}
+      </div>
+    )
+  }
+}
+
+class Breadcrumb extends React.Component {
   static propTypes = {
     hierarchy: PropTypes.instanceOf(Hierarchy).isRequired,
     location: PropTypes.string.isRequired,
@@ -13,16 +50,11 @@ export default class Breadcrumb extends React.Component {
   }
 
   render () {
-    let hierarchy = this.props.hierarchy
-    return <div className={style.breadcrumbs}>{hierarchy.map((page, path) => {
-      return (
-        <Link key={page.id}
-              className={style.breadcrumb}
-              href={`/${this.props.location}/${this.props.language}${path}`}>
-          <span className={style.separator}/>
-          <span className={style.level}>{page.title}</span>
-        </Link>
-      )
-    })}</div>
+    return <LocationFetcher hideSpinner={true}>
+      <BreadcrumbAdapter hierarchy={this.props.hierarchy} language={this.props.language}
+                         location={this.props.location}/>
+    </LocationFetcher>
   }
 }
+
+export default Breadcrumb
