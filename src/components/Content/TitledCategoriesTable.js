f261b88aa8b813fa2e103b9a230d9f0e9c2801a8
St <klinzo@yahoo.de>
2017-10-22 14:36:45 +0200

    WEBAPP-53: changed title on pages from url key to real location name, ex. augsburg -> Augsburg

--- src/components/Content/TitledCategoriesTable.js
+++ src/components/Content/TitledCategoriesTable.js
@@ -4,6 +4,32 @@ import PropTypes from 'prop-types'
 import style from './TitledCategoriesTable.css'
 import PageModel from 'endpoints/models/PageModel'
 import CategoriesTable from './CategoriesTable'
+import LocationModel from '../../endpoints/models/LocationModel'
+import { LocationFetcher } from '../../endpoints/index'
+
+class TitledCategoriesTableAdapter extends React.Component {
+  static propTypes = {
+    locations: PropTypes.arrayOf(PropTypes.instanceOf(LocationModel)),
+    location: PropTypes.string.isRequired,
+    categories: PropTypes.arrayOf(PropTypes.shape({
+      page: PropTypes.instanceOf(PageModel).isRequired,
+      url: PropTypes.string.isRequired
+    })).isRequired
+  }
+
+  getTitle () {
+    return this.props.locations.find((location) => location.code === this.props.location).name
+  }
+
+  render () {
+    return (
+      <div>
+        <div className={style.title}>{this.getTitle()}</div>
+        <CategoriesTable categories={this.props.categories}/>
+      </div>
+    )
+  }
+}
 
 class TitledCategoriesTable extends React.Component {
   static propTypes = {
@@ -16,10 +42,9 @@ class TitledCategoriesTable extends React.Component {
 
   render () {
     return (
-      <div>
-        <div className={style.title}>{this.props.parentPage.title}</div>
-        <CategoriesTable categories={this.props.categories}/>
-      </div>
+      <LocationFetcher hideSpinner={true}>
+        <TitledCategoriesTableAdapter location={this.props.parentPage.title} categories={this.props.categories}/>
+      </LocationFetcher>
     )
   }
 }
