e6d1805b0630d5c885cdbde8c992debc19197824
St <klinzo@yahoo.de>
2017-11-07 16:46:52 +0100

    WEBAPP-73: goto the redirect page on language change with the new pageId as query parameter

--- src/components/RichLayout/index.js
+++ src/components/RichLayout/index.js
@@ -2,23 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { push } from 'redux-little-router'
+import { isEmpty } from 'lodash/lang'
 
 import Layout from 'components/Layout'
 import Header from './Header'
 import Footer from './Footer'
+import PAGE_ENDPOINT from '../../endpoints/page'
 
 class RichLayout extends React.Component {
   static propTypes = {
     className: PropTypes.string,
-    location: PropTypes.string
+    location: PropTypes.string,
+    availableLanguages: PropTypes.arrayOf(PropTypes.shape({
+      language: PropTypes.string.isRequired,
+      pageId: PropTypes.number.isRequired
+    })).isRequired
   }
 
   constructor (props) {
     super(props)
 
-    this.gotoParent = this.gotoParent.bind(this)
+    this.gotoNewPath = this.gotoNewPath.bind(this)
   }
 
+  /**
+   * Builds the path to the location page in the new language or the root page if no location is given
+   * @param newLanguage the new language
+   * @returns {*} the path
+   */
   getParentPath (newLanguage) {
     if (!this.props.location) {
       return '/'
@@ -26,13 +37,30 @@ class RichLayout extends React.Component {
     return `/${this.props.location}/${newLanguage}`
   }
 
-  gotoParent (newLanguage) {
-    this.props.dispatch(push(this.getParentPath(newLanguage)))
+  /**
+   * Builds the url to the page in the new language
+   * @param newLanguage the new language
+   * @returns {*} the url
+   */
+  getNewUrl (newLanguage) {
+    if (!isEmpty(this.props.availableLanguages)) {
+      return `/${this.props.location}/${newLanguage}/redirect/?id=${this.props.availableLanguages[newLanguage]}`
+    }
+    return this.getParentPath(newLanguage)
+  }
+
+  /**
+   * Request fetching of the pages and loading of the current in the new language
+   * @param newLanguage the new language
+   */
+  gotoNewPath (newLanguage) {
+    this.props.dispatch(PAGE_ENDPOINT.requestAction({location: this.props.location, language: newLanguage}, {location: this.props.location}))
+    this.props.dispatch(push(this.getNewUrl(newLanguage)))
   }
 
   render () {
     return (<div>
-        <Header languageCallback={this.gotoParent}/>
+        <Header languageCallback={this.gotoNewPath}/>
 
         <Layout className={this.props.className}>
           {this.props.children}
@@ -44,6 +72,6 @@ class RichLayout extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => ({ location: state.router.params.location })
+const mapStateToProps = (state) => ({ location: state.router.params.location, availableLanguages: state.currentAvailableLanguages })
 
 export default connect(mapStateToProps)(RichLayout)
