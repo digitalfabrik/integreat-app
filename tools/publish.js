cf587531bc5055351dad959916bc276ab2d3bfa8
Max Ammann <maximilian.ammann@googlemail.com>
2017-11-15 11:18:54 +0100

    WEBAPP-28: Clean tools

--- tools/publish.js
+++
@@ -1,9 +0,0 @@
-const build = require('./build')
-const task = require('./task')
-
-module.exports = task('deploy', () => Promise.resolve()
-  .then(() => build())
-  .then(() => { throw new Error('Not yet implemented.') })
-  .then(() => {
-    setTimeout(() => process.exit())
-  }))
