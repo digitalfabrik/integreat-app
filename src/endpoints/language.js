e946667a9e8b10db57ed56a858d41a54f016bbf0
Michael Markl <marklmichael98@gmail.com>
2017-11-16 23:35:37 +0100

    WEBAPP-73: Remove wrong stateMapper for languages endpoint

--- src/endpoints/language.js
+++ src/endpoints/language.js
@@ -9,6 +9,5 @@ export default new EndpointBuilder('languages')
     language: 'de'  // todo:  This forces that the languages are always fetched in german language. German always
                     //        exists in the backend -> a langauge switch always works
   }))
-  .withStateMapper().fromArray(['location', 'language'], (state, paramName) => state.router.params[paramName])
   .withMapper(json => json.map(language => new LanguageModel(language.code, language.native_name)))
   .build()
