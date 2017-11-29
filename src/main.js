1b8c26a00638dd6440dfda82bb31df10b5d4f449
Max Ammann <maximilian.ammann@googlemail.com>
2017-11-29 09:45:45 +0100

    WEBAPP-130: Unregister previous service workers

--- src/main.js
+++ src/main.js
@@ -100,6 +100,11 @@ ReactDOM.render(App, container)
 // Sets the splash to hidden when the page is rendered
 document.getElementById('splash').className += ' splash-hidden'
 
+// Currently we do not have service workers to unregister all previous ones
+navigator.serviceWorker.getRegistrations().then(registrations => {
+  registrations.forEach(registration => { registration.unregister() })
+})
+
 // Enables hot-module-reloading if it's enabled
 if (module.hot) {
   module.hot.accept()
