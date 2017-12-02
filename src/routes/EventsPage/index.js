b4f49f02ebfdeba75b2022eddb056bbc3cdbe7d6
St <klinzo@yahoo.de>
2017-12-02 18:05:50 +0100

    WEBAPP-50: Differ between ElementsList and single Element

--- src/routes/EventsPage/index.js
+++ src/routes/EventsPage/index.js
@@ -1,27 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
-import Events from '../../components/Content/Events'
 import EventModel from 'endpoints/models/EventModel'
 import EVENTS_ENDPOINT from 'endpoints/events'
 import withFetcher from 'endpoints/withFetcher'
 import withAvailableLanguageUpdater from 'hocs/withAvailableLanguageUpdater'
 import compose from 'redux/es/compose'
+import EventList from '../../components/Content/EventsList'
+import Events from '../../components/Content/Events'
 
 class EventsPage extends React.Component {
   static propTypes = {
     /**
      * from withFetcher HOC which provides data from EVENTS_ENDPOINT
      */
-    events: PropTypes.arrayOf(PropTypes.instanceOf(EventModel)).isRequired
+    events: PropTypes.arrayOf(PropTypes.instanceOf(EventModel)).isRequired,
+    location: PropTypes.string.isRequired,
+    language: PropTypes.string.isRequired,
+    path: PropTypes.string
+  }
+
+  getPath () {
+    return `/${this.props.location}/${this.props.language}/events`
   }
 
   render () {
-    return <Events events={this.props.events}/>
+    if (this.props.path) {
+      // todo change this to one element
+      return <Events events={this.props.events}/>
+    }
+    return <EventList events={this.props.events} url={this.getPath()}/>
   }
 }
 
+const mapStateToProps = (state) => ({
+  language: state.router.params.language,
+  location: state.router.params.location,
+  path: state.router.params['_'] // _ contains all the values from *
+})
+
 export default compose(
+  connect(mapStateToProps),
   withFetcher(EVENTS_ENDPOINT),
   withAvailableLanguageUpdater((location, language) => `/${location}/${language}/events`)
 )(EventsPage)
