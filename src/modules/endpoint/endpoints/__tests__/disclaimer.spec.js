4b5a4e4cda18221420432eb2540710d6d97aa803
Max Ammann <maximilian.ammann@googlemail.com>
2017-12-21 22:41:34 +0100

    WEBAPP-127: Finish disclaimer page endpoint

--- src/modules/endpoint/endpoints/__tests__/disclaimer.spec.js
+++ src/modules/endpoint/endpoints/__tests__/disclaimer.spec.js
@@ -2,33 +2,40 @@ import disclaimer from '../disclaimer'
 import PageModel from '../../models/PageModel'
 
 describe('disclaimer', () => {
+  const pageJson = {
+    id: 1689,
+    permalink: {
+      url_page: 'feedback-kontakt-und-moegliches-engagement'
+    },
+    title: 'Feedback, Kontakt und mögliches Engagement',
+    type: 'disclaimer',
+    status: 'publish',
+    modified_gmt: '2017-06-12 12:27:57',
+    excerpt: 'Excerpt',
+    content: '<span>Content</span>',
+    parent: 0
+  }
+
   test('should map state to urls', () => {
     expect(disclaimer.mapStateToUrlParams({router: {params: {location: 'augsburg', language: 'de'}}}))
       .toEqual({location: 'augsburg', language: 'de'})
   })
 
+  test('should throw if there are multiple disclaimers', () => {
+    expect(() => disclaimer.mapResponse([pageJson, pageJson])).toThrow()
+  })
+
+  test('should throw if there is no disclaimer', () => {
+    expect(() => disclaimer.mapResponse(undefined)).toThrow()
+  })
+
+  test('should throw if the disclaimer is not published', () => {
+    const unpublishedPage = Object.assign({}, ...pageJson, {status: 'no published'})
+    expect(() => disclaimer.mapResponse([unpublishedPage])).toThrow()
+  })
+
   test('should map fetched data to models', () => {
-    const pageJson = {
-      id: 1689,
-      permalink: {
-        url_page: 'feedback-kontakt-und-moegliches-engagement'
-      },
-      title: 'Feedback, Kontakt und mögliches Engagement',
-      type: 'disclaimer',
-      status: 'publish',
-      modified_gmt: '2017-06-12 12:27:57',
-      excerpt: 'Excerpt',
-      content: '<span>Content</span>',
-      parent: 0,
-      available_languages: {
-        en: 7380,
-        ar: 7386,
-        fa: 7387
-      }
-    }
-
     const disclaimerPage = disclaimer.mapResponse([pageJson])
-
     expect(disclaimerPage).toEqual(new PageModel({
       id: pageJson.permalink.url_page,
       numericId: pageJson.id,
@@ -37,8 +44,7 @@ describe('disclaimer', () => {
       content: pageJson.content,
       thumbnail: pageJson.thumbnail,
       order: pageJson.order,
-      children: [],
-      availableLanguages: pageJson.available_languages
+      children: []
     }))
   })
 })
