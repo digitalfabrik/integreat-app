b6e6effc44795a8735027b026c27275b9ba65172
Max Ammann <maximilian.ammann@googlemail.com>
2017-12-22 19:23:14 +0100

    WEBAPP-127: Fix location test

--- src/modules/endpoint/endpoints/__tests__/location.js
+++ src/modules/endpoint/endpoints/__tests__/location.js
@@ -1,21 +1,22 @@
 import location from '../location'
+import LocationModel from '../../models/LocationModel'
 
 describe('location', () => {
-  const locationJson = [
-    {
-      id: '2',
-      name: 'Augsburg',
-      path: '/augsburg/',
-      description: 'Augsburg',
-      live: true
-    },
-    {
-      id: '10',
-      name: 'Stadt Regensburg',
-      path: '/regensburg/',
-      description: 'Stadt Regensburg',
-      live: true
-    }]
+  const location1 = {
+    id: '2',
+    name: 'Augsburg',
+    path: '/augsburg/',
+    live: true,
+    'ige-evts': '1'
+  }
+  const location2 = {
+    id: '10',
+    name: 'Stadt Regensburg',
+    path: '/regensburg/',
+    live: true,
+    'ige-evts': '0'
+  }
+  const locationJson = [location1, location2]
 
   test('should map state to urls', () => {
     expect(location.mapStateToUrlParams({})).toEqual({})
@@ -23,6 +24,21 @@ describe('location', () => {
 
   test('should map fetched data to models', () => {
     const locationModels = location.mapResponse(locationJson)
-    expect(locationModels).toEqual([])
+    expect(locationModels).toEqual([
+      new LocationModel({
+        name: location1.name,
+        code: 'augsburg',
+        live: location1.live,
+        eventsEnabled: true,
+        extrasEnabled: true // todo: Adjust this in WEBAPP-64
+      }),
+      new LocationModel({
+        name: location2.name,
+        code: 'regensburg',
+        live: location2.live,
+        eventsEnabled: false,
+        extrasEnabled: true // todo: Adjust this in WEBAPP-64
+      })
+    ])
   })
 })
