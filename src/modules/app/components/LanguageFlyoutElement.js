d023e6a844e1b6c3f79417d317a6df9bd9d956c1
Michael Markl <marklmichael98@gmail.com>
2017-12-23 12:22:28 +0100

    WEBAPP-126: Extract LanguageFlyoutElement

---
+++ src/modules/app/components/LanguageFlyoutElement.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import LanguageModel from '../../endpoint/models/LanguageModel'
+import React from 'react'
+import cx from 'classnames'
+import {Link} from 'redux-little-router'
+
+import style from '../containers/LanguageFlyout.css'
+
+class LanguageFlyoutElement extends React.Component {
+  static propTypes = {
+    language: PropTypes.instanceOf(LanguageModel).isRequired,
+    active: PropTypes.bool.isRequired,
+    path: PropTypes.string.isRequired,
+    onClick: PropTypes.func
+  }
+
+  render () {
+    return (
+      <Link
+        href={this.props.active ? '#' : this.props.path}
+        className={cx(style.element, this.props.active ? style.elementActive : '')}
+        onClick={this.props.onClick}>
+        {this.props.language.name}
+      </Link>
+    )
+  }
+}
+
+export default LanguageFlyoutElement
--- src/modules/app/containers/LanguageFlyout.js
+++ src/modules/app/containers/LanguageFlyout.js
@@ -1,37 +1,14 @@
 import React from 'react'
-import cx from 'classnames'
-
 import PropTypes from 'prop-types'
 import { isEmpty } from 'lodash/lang'
 import { connect } from 'react-redux'
-import {Link} from 'redux-little-router'
 import compose from 'lodash/fp/compose'
 
 import withFetcher from 'modules/endpoint/hocs/withFetcher'
 import LANGUAGE_ENDPOINT from 'modules/endpoint/endpoints/language'
 import LanguageModel from 'modules/endpoint/models/LanguageModel'
-
 import style from './LanguageFlyout.css'
-
-class LanguageElement extends React.Component {
-  static propTypes = {
-    language: PropTypes.instanceOf(LanguageModel).isRequired,
-    active: PropTypes.bool.isRequired,
-    path: PropTypes.string.isRequired,
-    onClick: PropTypes.func
-  }
-
-  render () {
-    return (
-      <Link
-        href={this.props.active ? '#' : this.props.path}
-        className={cx(style.element, this.props.active ? style.elementActive : '')}
-        onClick={this.props.onClick}>
-        {this.props.language.name}
-      </Link>
-    )
-  }
-}
+import LanguageFlyoutElement from '../components/LanguageFlyoutElement'
 
 class LanguageFlyout extends React.Component {
   static propTypes = {
@@ -50,15 +27,14 @@ class LanguageFlyout extends React.Component {
       <div className={style.languageFlyout}>
         {!isEmpty(this.props.languages) &&
         this.props.languages.map(language => (
-            <LanguageElement
+            <LanguageFlyoutElement
               key={language.code}
               onClick={this.props.closeDropDownCallback}
               active={this.props.language === language.code}
               language={language}
               path={this.getPathForLanguage(language.code)}
             />
-          )
-        )
+          ))
         }
       </div>
     )
