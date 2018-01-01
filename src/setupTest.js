dc481de7692638c66826ca928bfada217acb44cd
St <klinzo@yahoo.de>
2018-01-01 16:22:00 +0100

    WEBAPP-144,WEBAPP-166: Mock endpoints with test results

--- src/setupTest.js
+++ src/setupTest.js
@@ -3,8 +3,9 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import EndpointBuilder from './modules/endpoint/EndpointBuilder'
 import LanguageModel from './modules/endpoint/models/LanguageModel'
-import EventModel from './modules/endpoint/models/EventModel'
 import DisclaimerModel from './modules/endpoint/models/DisclaimerModel'
+import CategoryModel from './modules/endpoint/models/CategoryModel'
+import CategoriesContainer from './modules/endpoint/models/CategoriesContainer'
 
 configure({adapter: new Adapter()})
 
@@ -15,6 +16,42 @@ const languages = [
   new LanguageModel('de', 'Deutsch'),
   new LanguageModel('en', 'English')
 ]
+
+const categories = new CategoriesContainer([
+  new CategoryModel({
+    id: 3650,
+    url: '/augsburg/de/anlaufstellen',
+    title: 'Anlaufstellen zu sonstigen Themen',
+    content: '',
+    parentId: 0,
+    parentUrl: '/augsburg/de',
+    order: 75,
+    availableLanguages: {
+      en: 4361, ar: 4367, fa: 4368
+    },
+    thumbnail: 'https://cms.integreat-ap…/03/Hotline-150x150.png'
+  }),
+  new CategoryModel({
+    id: 3649,
+    url: '/augsburg/de/willkommen',
+    title: 'Willkommen',
+    content: '',
+    parentId: 0,
+    parentUrl: '/augsburg/de',
+    order: 11,
+    availableLanguages: {
+      en: 4804, ar: 4819, fa: 4827
+    },
+    thumbnail: 'https://cms.integreat-ap…03/Beratung-150x150.png'
+  }),
+  new CategoryModel({id: 0, url: '/augsburg/de', title: 'augsburg'})
+])
+
+const disclaimer = new DisclaimerModel({
+  id: 1234,
+  title: 'Disclaimer',
+  content: 'Disclaimer test content'
+})
 
 const mockEventsEndpoint = new EndpointBuilder('events')
   .withUrl('https://weird-endpoint/api.json')
@@ -33,7 +70,7 @@ jest.mock('modules/endpoint/endpoints/languages', () => mockLanguagesEndpoint)
 const mockCategoriesEndpoint = new EndpointBuilder('categories')
   .withUrl('https://weird-endpoint/api.json')
   .withMapper(json => json)
-  .withResponseOverride({})
+  .withResponseOverride(categories)
   .build()
 jest.mock('modules/endpoint/endpoints/categories', () => mockCategoriesEndpoint)
 
@@ -47,6 +84,6 @@ jest.mock('modules/endpoint/endpoints/locations', () => mockLocationsEndpoint)
 const mockDisclaimerEndpoint = new EndpointBuilder('disclaimer')
   .withUrl('https://weird-endpoint/api.json')
   .withMapper(json => json)
-  .withResponseOverride(new DisclaimerModel({id: 1234, title: 'Disclaimer', content: 'test content'}))
+  .withResponseOverride(disclaimer)
   .build()
 jest.mock('modules/endpoint/endpoints/disclaimer', () => mockDisclaimerEndpoint)
