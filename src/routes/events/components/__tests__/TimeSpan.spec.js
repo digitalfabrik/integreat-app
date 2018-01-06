3f60889e737702100a7cd94be348aab4e7c81e49
St <klinzo@yahoo.de>
2018-01-06 15:34:14 +0100

    WEBAPP-142: Rename tests to start with 'should'

--- src/routes/events/components/__tests__/TimeSpan.spec.js
+++ src/routes/events/components/__tests__/TimeSpan.spec.js
@@ -18,75 +18,77 @@ describe('TimeSpan', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  test('toTimeSpanString() should return start date + time and end date + time', () => {
-    const startDate = moment('2017-11-27 19:30:00')
-    const endDate = moment('2017-11-28 21:30:00')
-    const allDay = false
+  describe('toTimeSpanString()', () => {
+    test('should return start date + time and end date + time', () => {
+      const startDate = moment('2017-11-27 19:30:00')
+      const endDate = moment('2017-11-28 21:30:00')
+      const allDay = false
 
-    const timeSpan = shallow(
-      <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
-    ).instance()
+      const timeSpan = shallow(
+        <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
+      ).instance()
 
-    expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
-  })
+      expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
+    })
 
-  test('toTimeSpanString() should return only start date + time and end time if the dates are the same', () => {
-    const startDate = moment('2017-11-27 19:30:00')
-    const endDate = moment('2017-11-27 21:30:00')
-    const allDay = false
+    test('should return only start date + time and end time if the dates are the same', () => {
+      const startDate = moment('2017-11-27 19:30:00')
+      const endDate = moment('2017-11-27 21:30:00')
+      const allDay = false
 
-    const timeSpan = shallow(
-      <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
-    ).instance()
+      const timeSpan = shallow(
+        <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
+      ).instance()
 
-    expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
-  })
+      expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
+    })
 
-  test('toTimeSpanString() should return only start date + time if start and end are the same', () => {
-    const startDate = moment('2017-11-27 19:30:00')
-    const endDate = moment('2017-11-27 19:30:00')
-    const allDay = false
+    test('should return only start date + time if start and end are the same', () => {
+      const startDate = moment('2017-11-27 19:30:00')
+      const endDate = moment('2017-11-27 19:30:00')
+      const allDay = false
 
-    const timeSpan = shallow(
-      <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
-    ).instance()
+      const timeSpan = shallow(
+        <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
+      ).instance()
 
-    expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
-  })
+      expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
+    })
 
-  test('toTimeSpanString() should return only start date + end date if allDay is true', () => {
-    const startDate = moment('2017-11-27 19:30:00')
-    const endDate = moment('2017-11-28 21:30:00')
-    const allDay = true
+    test('should return only start date + end date if allDay is true', () => {
+      const startDate = moment('2017-11-27 19:30:00')
+      const endDate = moment('2017-11-28 21:30:00')
+      const allDay = true
 
-    const timeSpan = shallow(
-      <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
-    ).instance()
+      const timeSpan = shallow(
+        <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
+      ).instance()
 
-    expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
-  })
+      expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
+    })
 
-  test('toTimeSpanString() should return only start date if allDay is true and the dates are the same', () => {
-    const startDate = moment('2017-11-27 19:30:00')
-    const endDate = moment('2017-11-27 21:30:00')
-    const allDay = true
+    test('should return only start date if allDay is true and the dates are the same', () => {
+      const startDate = moment('2017-11-27 19:30:00')
+      const endDate = moment('2017-11-27 21:30:00')
+      const allDay = true
 
-    const timeSpan = shallow(
-      <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
-    ).instance()
+      const timeSpan = shallow(
+        <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
+      ).instance()
 
-    expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
-  })
+      expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
+    })
 
-  test('toTimeSpanString() should only return start date (+ time) if endDate is not valid', () => {
-    const startDate = moment('2017-11-27 19:30:00')
-    const endDate = moment('')
-    const allDay = false
+    test('should only return start date (+ time) if endDate is not valid', () => {
+      const startDate = moment('2017-11-27 19:30:00')
+      const endDate = moment('')
+      const allDay = false
 
-    const timeSpan = shallow(
-      <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
-    ).instance()
+      const timeSpan = shallow(
+        <TimeSpan startDate={startDate} endDate={endDate} allDay={allDay} locale={locale} />
+      ).instance()
 
-    expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
+      expect(locales.map(locale => locale + ': ' + timeSpan.toTimeSpanString(locale))).toMatchSnapshot()
+    })
   })
 })
