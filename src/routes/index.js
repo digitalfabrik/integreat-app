eed4ffd60559162dbc434e37bee32e9ce6b877f9
Max Ammann <maximilian.ammann@googlemail.com>
2018-01-06 14:23:01 +0100

    WEBAPP-90: Remove unused titles

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,13 @@
 export default {
   '/': {
     '/:location/:language': {
-      title: 'Categories page',
-      '/*': {
-        title: 'Categories page'
-      },
-      '/search': {
-        title: 'Search page'
-      },
-      '/disclaimer': {
-        title: 'Disclaimer page'
-      },
-      '/events(/:id)': {
-        title: 'Events page'
-      },
-      '/fetch-pdf': {
-        title: 'FetchPdf page'
-      }
+      '/*': {},
+      '/search': {},
+      '/disclaimer': {},
+      '/events(/:id)': {},
+      '/fetch-pdf': {}
     },
-    '/:language(/)': {
-      title: 'Landing page'
-    },
-    '/disclaimer': {
-      title: 'Static disclaimer'
-    }
+    '/:language(/)': {},
+    '/disclaimer': {}
   }
 }
