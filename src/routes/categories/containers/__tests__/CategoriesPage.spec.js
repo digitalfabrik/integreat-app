acdf984a46db4a53f64531e0448b126351b2091d
St <klinzo@yahoo.de>
2018-01-16 14:59:46 +0100

    WEBAPP-167: Add real store to CategoriesPage test

--- src/routes/categories/containers/__tests__/CategoriesPage.spec.js
+++ src/routes/categories/containers/__tests__/CategoriesPage.spec.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { mount, shallow } from 'enzyme'
 import { Provider } from 'react-redux'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
+
+import createHistory from 'modules/app/createHistory'
+import createReduxStore from 'modules/app/createReduxStore'
 
 import ConnectedCategoriesPage, { CategoriesPage } from '../CategoriesPage'
-import Payload from 'modules/endpoint/Payload'
 import LocationModel from 'modules/endpoint/models/LocationModel'
 import LanguageModel from 'modules/endpoint/models/LanguageModel'
 import CategoryModel from 'modules/endpoint/models/CategoryModel'
@@ -80,24 +80,6 @@ describe('CategoriesPage', () => {
   ]
 
   const language = 'en'
-
-  const categoriesEndpoint = new EndpointBuilder('categories')
-    .withUrl('https://weird-endpoint/api.json')
-    .withMapper(json => json)
-    .withResponseOverride(categories)
-    .build()
-
-  const locationsEndpoint = new EndpointBuilder('locations')
-    .withUrl('https://weird-endpoint/api.json')
-    .withMapper(json => json)
-    .withResponseOverride(locations)
-    .build()
-
-  const languagesEndpoint = new EndpointBuilder('languages')
-    .withUrl('https://weird-endpoint/api.json')
-    .withMapper(json => json)
-    .withResponseOverride(languages)
-    .build()
 
   test('should render a Page if page has no children', () => {
     const mockSetLanguageChangeUrls = jest.fn()
@@ -295,19 +277,36 @@ describe('CategoriesPage', () => {
     expect(categoriesPage.getPdfFetchPath()).toBe('/augsburg/en/fetch-pdf?url=/augsburg/de/willkommen')
   })
 
-  const mockStore = configureMockStore([thunk])
-
   describe('connect', () => {
-    test('should map state to props', () => {
-      const pathname = '/augsburg/de'
-      const id = 1234
-      const store = mockStore({
-        categories: new Payload(false),
-        languages: new Payload(false),
-        locations: new Payload(false),
-        router: {params: {location: location, language: language}, pathname: pathname, query: {id: id}}
-      })
+    const languagesEndpoint = new EndpointBuilder('languages')
+      .withUrl('https://weird-endpoint/api.json')
+      .withMapper(json => json)
+      .withResponseOverride(languages)
+      .build()
 
+    const locationsEndpoint = new EndpointBuilder('locations')
+      .withUrl('https://weird-endpoint/api.json')
+      .withMapper(json => json)
+      .withResponseOverride(locations)
+      .build()
+
+    const categoriesEndpoint = new EndpointBuilder('categories')
+      .withUrl('https://weird-endpoint/api.json')
+      .withMapper(json => json)
+      .withResponseOverride(categories)
+      .build()
+
+    const pathname = '/augsburg/de/willkommen'
+    const id = '1234'
+
+    const store = createReduxStore(createHistory, {
+      router: {params: {location: location, language: language},
+        pathname: pathname,
+        query: {id: id}},
+      languageChangeUrls: {}
+    })
+
+    test('should map state to props', () => {
       const tree = mount(
         <Provider store={store}>
           <EndpointProvider endpoints={[categoriesEndpoint, locationsEndpoint, languagesEndpoint]}>
@@ -316,40 +315,32 @@ describe('CategoriesPage', () => {
         </Provider>
       )
 
-      const categoriesPageProps = tree.find(ConnectedCategoriesPage).childAt(0).props()
+      const categoriesPageProps = tree.find(CategoriesPage)
 
-      // todo add categories, locations and languages
-      expect(categoriesPageProps).toEqual({
+      expect(categoriesPageProps.props()).toEqual({
         location: location,
         language: language,
         path: pathname,
         categoryId: id,
         setLanguageChangeUrls: expect.any(Function),
-        replaceUrl: expect.any(Function)
+        replaceUrl: expect.any(Function),
+        categories: categories,
+        locations: locations,
+        languages: languages
       })
     })
 
-    // we don't have to test replaceUrl here, because it will be redundant after availableLanguages
-    // in the cms are changed to the path instead of the id
     test('should map dispatch to props', () => {
-      const pathname = '/augsburg/de'
-      const store = mockStore({
-        categories: new Payload(false),
-        languages: new Payload(false),
-        locations: new Payload(false),
-        router: {params: {location: location, language: language}, pathname: pathname, query: {}}
-      })
-
       const mapLanguageToUrl = (language, id) => 'test' + language + id
 
       const testUrls = {
-        en: 'testenundefined',
-        de: 'testde1235',
+        en: 'testen1235',
+        de: 'testdeundefined',
         ar: 'testar1236'
       }
 
       const availableLanguages = {
-        de: '1235',
+        en: '1235',
         ar: '1236'
       }
 
@@ -361,20 +352,11 @@ describe('CategoriesPage', () => {
         </Provider>
       )
 
-      // todo expect setLanguageChangeUrls action to be in store, but as we don't get categories, locations and
-      // languages from our mocked endpoint no action is dispatched
+      const categoriesPage = tree.find(CategoriesPage)
 
-      const categoriesPageProps = tree.find(ConnectedCategoriesPage).childAt(0).props()
+      categoriesPage.props().setLanguageChangeUrls(mapLanguageToUrl, languages, availableLanguages)
 
-      let countActions = store.getActions().length
-
-      categoriesPageProps.setLanguageChangeUrls(mapLanguageToUrl, languages, availableLanguages)
-      expect(store.getActions()).toHaveLength(countActions + 1)
-
-      expect(store.getActions()).toContainEqual({
-        payload: testUrls,
-        type: 'SET_LANGUAGE_CHANGE_URLS'
-      })
+      expect(store.getState().languageChangeUrls).toEqual(testUrls)
     })
   })
 })
