369fe548c76a05850596e33921eacd25e5779cce
Michael Markl <marklmichael98@gmail.com>
2018-01-21 15:52:37 +0100

    WEBAPP-132: Use Headroom for scroll effect

--- src/modules/common/components/ScrollingSearchBox.js
+++ src/modules/common/components/ScrollingSearchBox.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import cx from 'classnames'
 
 import style from './ScrollingSearchBox.css'
 import { animateScroll } from 'react-scroll'
 import SearchInput from './SearchInput'
+import Headroom from 'react-headroom'
 
-const SEARCH_INPUT_HEIGHT = 45
 const SCROLL_ANIMATION_DURATION = 500
+const SEARCH_INPUT_HEIGHT = 45
 
 export class ScrollingSearchBox extends React.Component {
   static propTypes = {
@@ -21,8 +23,14 @@ export class ScrollingSearchBox extends React.Component {
     stickyTop: 0
   }
 
+  constructor () {
+    super()
+    this.state = {searchUnpinned: false, searchUnfixed: true, initialized: false}
+  }
+
   onClick () {
-    const documentScroll = document.documentElement.scrollTop
+    // Some browsers scroll at html level (chrome, firefox, ie11), some at body level (edge).
+    const documentScroll = Math.max(document.documentElement.scrollTop, document.body.scrollTop)
     const elementTop = this._node.offsetTop
     if (documentScroll < elementTop) {
       this.scroll()
@@ -42,21 +50,50 @@ export class ScrollingSearchBox extends React.Component {
   setReference (node) {
     if (node) {
       this._node = node
+      if (!this.state.initialized) {
+        this.setState(Object.assign({}, this.state, {initialized: true}))
+      }
     }
   }
 
+  onPinSearch () {
+    this.setState(Object.assign({}, this.state, {searchUnpinned: false, searchUnfixed: false}))
+  }
+
+  onUnpinSearch () {
+    this.setState(Object.assign({}, this.state, {searchUnpinned: true}))
+  }
+
+  onUnfixSearch () {
+    this.setState(Object.assign({}, this.state, {searchUnfixed: true}))
+  }
+
+  getChildStickyTop () {
+    return this.props.stickyTop +
+      (!this.state.searchUnfixed && !this.state.searchUnpinned) ? SEARCH_INPUT_HEIGHT : 0
+  }
+
   render () {
-    return <React.Fragment>
-      <div ref={node => this.setReference(node)} className={style.searchBar}
-           style={{'top': this.props.stickyTop + 'px'}}>
-        <SearchInput filterText={this.props.filterText} onFilterTextChange={value => this.onFilterTextChange(value)}
+    const childStickyTop = this.getChildStickyTop()
+    const {searchUnpinned, searchUnfixed} = this.state
+    return <div ref={node => this.setReference(node)}>
+      <Headroom pinStart={this._node ? this._node.offsetTop : 0}
+                className={cx({
+                  [style.headroom]: true,
+                  [style.unpinned]: searchUnpinned,
+                  [style.unfixed]: searchUnfixed
+                })}
+                disableInlineStyles
+                onPin={() => this.onPinSearch()}
+                onUnfix={() => this.onUnfixSearch()}
+                onUnpin={() => this.onUnpinSearch()}>
+        <SearchInput filterText={this.props.filterText}
+                     onFilterTextChange={value => this.onFilterTextChange(value)}
                      onClickInput={() => this.onClick()}
                      spaceSearch={this.props.spaceSearch} />
-      </div>
-      <div className={style.searching}>
-        {React.cloneElement(this.props.children, {stickyTop: this.props.stickyTop + SEARCH_INPUT_HEIGHT})}
-      </div>
-    </React.Fragment>
+      </Headroom>
+      {React.cloneElement(this.props.children, {stickyTop: childStickyTop})}
+    </div>
   }
 }
 
