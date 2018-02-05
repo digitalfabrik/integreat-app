1907ec5a50193d55a1837c5f5156828bac3a1763
Max Ammann <maximilian.ammann@googlemail.com>
2018-02-05 21:22:06 +0100

    WEBAPP-90: Fix linting

--- src/modules/app/containers/__tests__/I18nRedirect.spec.js
+++ src/modules/app/containers/__tests__/I18nRedirect.spec.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import configureMockStore from 'redux-mock-store'
 import ConnectedI18nRedirect, { I18nRedirect } from '../I18nRedirect'
-import { I18nProvider } from '../I18nProvider'
 import PropTypes from 'prop-types'
 
 describe('I18nRedirect', () => {
