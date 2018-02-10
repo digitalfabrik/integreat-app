7b7803b91b3c826f3b809e53c3a2bfb3460cefdc
Michael Markl <marklmichael98@gmail.com>
2018-02-10 09:40:07 +0100

    WEBAPP-169: Fix implementation and add documentation

--- src/modules/common/components/Headroom.js
+++ src/modules/common/components/Headroom.js
@@ -4,13 +4,16 @@ import raf from 'raf'
 
 import style from './Headroom.css'
 
+const UPWARDS = 'up'
+const DOWNWARDS = 'down'
+
 class Headroom extends React.PureComponent {
   static propTypes = {
-    children: PropTypes.any.isRequired,
-    scrollHeight: PropTypes.number.isRequired,
-    pinStart: PropTypes.number.isRequired,
-    stickyAncestor: PropTypes.node,
-    height: PropTypes.number
+    children: PropTypes.any.isRequired, // The child node to be displayed as a header
+    scrollHeight: PropTypes.number.isRequired, // The maximum amount of px the header should move up when scrolling
+    pinStart: PropTypes.number.isRequired, // The minimum scrollTop position where the transform should start
+    stickyAncestor: PropTypes.node, // Gets rendered with a corresponding stickyTop prop as an ancestor
+    height: PropTypes.number // Used for rendering stickyTop position of stickyAncestor
   }
 
   static defaultProps = {
@@ -22,11 +25,13 @@ class Headroom extends React.PureComponent {
     this.handleEvent = this.handleEvent.bind(this)
     this.update = this.update.bind(this)
     this.ticking = false
-    this.lastScrollY = 0
-    this.lastTransform = 0
-    this.lastKnownScrollY = 0
-    this.direction = 'up'
-    this.state = {transform: 0}
+    this.state = {
+      transform: 0 // the current transform in px (between -scrollHeight and 0, 0: fully visible)
+    }
+    this.lastKnownScrollTop = 0 // the very last scrollTop which we know about (to determine direction changes)
+    this.direction = DOWNWARDS // the current direction that the user is scrolling into
+    this.lastTransform = 0 // the transform in px when the user last changed his scrolling direction
+    this.lastScrollTop = 0 // the scrollTop when the user last changed his scrolling direction
   }
 
   componentDidMount () {
@@ -37,7 +42,7 @@ class Headroom extends React.PureComponent {
     window.removeEventListener('scroll touchstart touchmove', this.handleEvent)
   }
 
-  getScrollY = () => {
+  static getScrollTop () {
     if (window.pageYOffset !== undefined) {
       return window.pageYOffset
     } else if (window.scrollTop !== undefined) {
@@ -47,33 +52,39 @@ class Headroom extends React.PureComponent {
     }
   }
 
-  truncate (transform, currentScrollY) {
-    return Math.min(0, Math.max(-this.props.scrollHeight, transform, this.props.pinStart - currentScrollY))
+  /**
+   * Calculates the new transform value depending on the currentScrollTop position
+   * @param currentScrollTop
+   * @returns {number} the new transform value between -this.props.scrollHeight and 0
+   */
+  calcTransform (currentScrollTop) {
+    // Calculate new transform value while only regarding delta ScrollTop:
+    const scrolledTransform = this.lastTransform + (this.lastScrollTop - currentScrollTop)
+    // Transform should be at least:
+    // -this.props.scrollHeight, so we don't transform the header higher than we allow
+    // this.props.pinStart - currentScrollTop, so don't transform the header until we scrolled below this.props.pinStart
+    const maxTransform = Math.max(-this.props.scrollHeight, scrolledTransform, this.props.pinStart - currentScrollTop)
+    // Also we never want to translate the header down, so it should always be <= 0.
+    return Math.min(0, maxTransform)
   }
 
   update () {
-    // this.lastScrollY: ScrollTop when the scroll direction last changed
-    // this.lastTransform: Transform when the scroll direction last changed
-    // this.lastKnownScrollY: Last ScrollTop (to detect change in scroll direction)
-    // this.currentScrollY: Current ScrollTop
-    // this.direction: 'up' || 'down'
-
-    const currentScrollY = this.getScrollY()
-    if (this.lastKnownScrollY < currentScrollY) { // We're moving down
-      if (this.direction === 'up') { // We're changing direction from up to down!
+    const currentScrollTop = Headroom.getScrollTop()
+    if (this.lastKnownScrollTop < currentScrollTop) { // We're moving down
+      if (this.direction === UPWARDS) { // We're changing direction from up to down!
         this.lastTransform = this.state.transform
-        this.lastScrollY = this.lastKnownScrollY
-        this.direction = 'down'
+        this.lastScrollTop = this.lastKnownScrollTop
+        this.direction = DOWNWARDS
       }
-    } else if (this.lastKnownScrollY > currentScrollY) { // We're moving up
-      if (this.direction === 'down') { // We're changing direction from down to up!
+    } else if (this.lastKnownScrollTop > currentScrollTop) { // We're moving up
+      if (this.direction === DOWNWARDS) { // We're changing direction from down to up!
         this.lastTransform = this.state.transform
-        this.lastScrollY = this.lastKnownScrollY
-        this.direction = 'up'
+        this.lastScrollTop = this.lastKnownScrollTop
+        this.direction = UPWARDS
       }
     }
-    this.lastKnownScrollY = currentScrollY
-    const newTransform = this.truncate(this.lastTransform + (this.lastScrollY - currentScrollY), currentScrollY)
+    this.lastKnownScrollTop = currentScrollTop
+    const newTransform = this.calcTransform(currentScrollTop)
     if (newTransform !== this.state.transform) {
       this.setState({transform: newTransform})
     }
@@ -92,7 +103,7 @@ class Headroom extends React.PureComponent {
     const {transform} = this.state
     return <React.Fragment>
       <div className={style.headroom2}
-                  style={{transform: `translateY(${transform}px)`}}>
+           style={{transform: `translateY(${transform}px)`}}>
         {children}
       </div>
       {stickyAncestor && React.cloneElement(stickyAncestor, {stickyTop: height + transform})}
