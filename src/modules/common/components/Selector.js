73c6313d02b72a5464285d68d552d42cd5c66c61
Michael Markl <marklmichael98@gmail.com>
2018-03-12 01:14:37 +0100

    WEBAPP-219: Refactor Selector

--- src/modules/common/components/Selector.js
+++ src/modules/common/components/Selector.js
@@ -1,10 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'redux-little-router'
-import cx from 'classnames'
+import styled, { css } from 'styled-components'
 
-import style from './Selector.css'
 import SelectorItemModel from '../models/SelectorItemModel'
+
+const Element = styled(Link)`
+  ${props => props.theme.helpers.removeA};
+  height: ${props => props.theme.dimensions.headerHeight};
+  min-width: 90px;
+  max-width: 120px;
+  flex: 1 1 90px;
+  color: ${props => props.theme.colors.textSecondaryColor};
+  font-size: 1.2em;
+  line-height: ${props => props.theme.dimensions.headerHeight};
+  text-align: center;
+  transition: background-color 0.2s, border-radius 0.2s;
+  border-radius: 30px;
+  user-select: none;
+  
+  :hover {
+    color: ${props => props.theme.colors.textColor};
+    font-weight: 700;
+    border-radius: 0;
+  }
+
+  @media ${props => props.theme.dimensions.smallViewport} {
+      height: ${props => props.theme.dimensions.headerHeightSmall};
+      min-width: 70px;
+      flex: 1 1 70px;
+      font-size: 1em;
+      line-height: ${props => props.theme.dimensions.headerHeightSmall};
+  }
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-flow: row wrap;
+  justify-content: center;
+  color: var(--text-color);
+  text-align: center;
+  
+  ${props => props.vertical && css`
+    flex-flow: column;
+    align-items: center;
+  
+    & ${Element} {
+      flex: 1;
+    }
+  `}
+`
+
+const ActiveElement = Element.withComponent('span').extend`
+  color: ${props => props.theme.colors.textColor};
+  font-weight: 700;
+`
 
 /**
  * Displays a Selector showing different items
@@ -22,20 +73,18 @@ class Selector extends React.Component {
     return this.props.items.map(item => {
       if (item.code === this.props.activeItemCode) {
         return (
-          <span key={item.code}
-                className={cx(style.element, style.elementActive)}
-                onClick={this.props.closeDropDownCallback}>
+          <ActiveElement key={item.code}
+                         onClick={this.props.closeDropDownCallback}>
             {item.name}
-            </span>
+          </ActiveElement>
         )
       } else {
         return (
-          <Link key={item.code}
-                className={style.element}
-                onClick={this.props.closeDropDownCallback}
-                href={item.path}>
+          <Element key={item.code}
+                   onClick={this.props.closeDropDownCallback}
+                   href={item.path}>
             {item.name}
-          </Link>
+          </Element>
         )
       }
     })
@@ -43,9 +92,9 @@ class Selector extends React.Component {
 
   render () {
     return (
-      <div className={cx({[style.selector]: true, [style.vertical]: this.props.verticalLayout})}>
+      <Wrapper vertical={this.props.verticalLayout}>
         {this.getItems()}
-      </div>
+      </Wrapper>
     )
   }
 }
