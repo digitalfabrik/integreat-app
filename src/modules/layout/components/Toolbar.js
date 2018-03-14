6afb30e73f8b25eea5231d063d2dc4203314002d
Nikolas Klug <klug.nikolas@googlemail.com>
2018-03-14 12:37:05 +0100

    WEBAPP-49: Apply small changes

--- src/modules/layout/components/Toolbar.js
+++ src/modules/layout/components/Toolbar.js
@@ -1,15 +1,17 @@
 // @flow
 
 import React from 'react'
+import cx from 'classnames'
 import style from './Toolbar.css'
 
 type Props = {
+  className?: string,
   children: Array<Node>
 }
 
 class Toolbar extends React.Component<Props> {
   render () {
-    return <div className={style.toolbar}>
+    return <div className={cx(style.toolbar, this.props.className)}>
       {this.props.children}
     </div>
   }
