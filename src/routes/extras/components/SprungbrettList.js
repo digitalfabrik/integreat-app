6aa5c9c04e250df00b958f4f52b8876825161501
Max Ammann <maximilian.ammann@googlemail.com>
2018-03-14 14:39:45 +0100

    WEBAPP-227: Fix typo

--- src/routes/extras/components/SprungbrettList.js
+++ src/routes/extras/components/SprungbrettList.js
@@ -8,11 +8,11 @@ import SprungbrettJobModel from 'modules/endpoint/models/SprungbrettJobModel'
 import SprungbrettListItem from './SprungbrettListItem'
 import style from './SprungbrettList.css'
 
-type Prop = {
+type Props = {
   jobs: Array<SprungbrettJobModel>
 }
 
-class SprungbrettList extends React.Component<Prop> {
+class SprungbrettList extends React.Component<Props> {
   getListItems (): Array<Node> {
     return this.props.jobs.map(job => <SprungbrettListItem key={job.id} job={job} />)
   }
