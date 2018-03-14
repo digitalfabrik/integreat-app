d01026f0309a0e7b056f1f7e239c14bf86562bc2
St <klinzo@yahoo.de>
2018-03-14 14:49:03 +0100

    WEBAPP-223: Fix disclaimer route

--- src/modules/app/routes/disclaimer.js
+++ src/modules/app/routes/disclaimer.js
@@ -1,3 +1,26 @@
-const route = '/:location/:language/disclaimer'
+import { citiesFetcher, disclaimerFetcher, eventsFetcher, languagesFetcher } from '../../endpoint/fetchers'
 
+const route = {
+  path: '/:city/:language/disclaimer',
+  thunk: async (dispatch, getState) => {
+    const state = getState()
+    const {city, language} = state.location.payload
+
+    if (!state.cities) {
+      await citiesFetcher(dispatch, city)
+    }
+
+    if (!state.languages) {
+      await languagesFetcher({city}, dispatch, language)
+    }
+
+    if (!state.events) {
+      await eventsFetcher({city, language}, dispatch)
+    }
+
+    if (!state.disclaimer) {
+      await disclaimerFetcher({city, language}, dispatch)
+    }
+  }
+}
 export default route
