be5a29e4f8c8f719154aa5e0784fe407ea402f29
St <klinzo@yahoo.de>
2018-03-15 12:56:11 +0100

    WEBAPP-223: Fix pdf route

--- src/modules/app/routes/pdfFetcher.js
+++ src/modules/app/routes/pdfFetcher.js
@@ -1,10 +1,18 @@
-import { locationLayoutFetcher } from '../../endpoint/fetchers'
+import { categoriesFetcher, locationLayoutFetcher } from '../../endpoint/fetchers'
 
 const route = {
   path: '/:city/:language/fetch-pdf/:fetchUrl+',
   fromPath: (segment: string, key: string): string => key === 'fetchUrl' ? `/${segment}` : segment,
   thunk: async (dispatch, getState) => {
+    const state = getState()
+    const {city, language} = state.location.payload
+    const prev = state.location.prev
+
     await locationLayoutFetcher(dispatch, getState)
+
+    if (!state.categories || prev.payload.city !== city || prev.payload.language !== language) {
+      await categoriesFetcher(dispatch, {city, language})
+    }
   }
 }
 export default route
