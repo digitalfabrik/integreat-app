79fd1d8ec0898ce5390266dc9b4b0a76840f04e9
Michael Markl <marklmichael98@gmail.com>
2018-03-15 19:35:00 +0100

    WEBAPP-49: Only show full toolbar, if category is available

--- src/routes/categories/containers/CategoriesToolbar.js
+++ src/routes/categories/containers/CategoriesToolbar.js
@@ -1,17 +1,35 @@
 // @flow
 
 import { connect } from 'react-redux'
-import PdfButton from '../components/PdfButton'
 import React from 'react'
-import Toolbar from '../../../modules/layout/components/Toolbar'
+import compose from 'recompose/compose'
 
-export class CategoriesToolbar extends React.PureComponent<{ location: string, language: string, path: string }> {
+import withFetcher from '../../../modules/endpoint/hocs/withFetcher'
+import PdfButton from '../components/PdfButton'
+import Toolbar from '../../../modules/layout/components/Toolbar'
+import CategoriesMapModel from '../../../modules/endpoint/models/CategoriesMapModel'
+
+type Props = {
+  location: string,
+  language: string,
+  path: string,
+  categories: CategoriesMapModel
+}
+
+export class CategoriesToolbar extends React.PureComponent<Props> {
   getPdfFetchPath () {
     return `/${this.props.location}/${this.props.language}/fetch-pdf?url=${this.props.path}`
   }
 
   render () {
-    return <Toolbar><PdfButton href={this.getPdfFetchPath()} /></Toolbar>
+    try {
+      this.props.categories.getCategoryByUrl(this.props.path)
+      return <Toolbar>
+        <PdfButton href={this.getPdfFetchPath()} />
+      </Toolbar>
+    } catch (e) {
+      return <Toolbar />
+    }
   }
 }
 
@@ -21,4 +39,7 @@ const mapStateToProps = state => ({
   path: state.router.pathname
 })
 
-export default connect(mapStateToProps)(CategoriesToolbar)
+export default compose(
+  withFetcher('categories', null, null),
+  connect(mapStateToProps)
+)(CategoriesToolbar)
