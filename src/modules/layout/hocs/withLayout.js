83a3a7c1e6ca7ab18bed656fc30d8dd062812dea
Michael Markl <marklmichael98@gmail.com>
2018-03-19 21:30:50 +0100

    WEBAPP-49: Fix linting

--- src/modules/layout/hocs/withLayout.js
+++ src/modules/layout/hocs/withLayout.js
@@ -10,33 +10,34 @@ import style from './Layout.css'
  * If a footer is supplied and there's not enough content (in header and children) to fill the viewbox, the footer will
  * always stick to the bottom of the viewbox.
  */
-const withLayout = (Header: ComponentType<{ onStickyTopChanged: number => void }>,
-                    Toolbar: ?ComponentType<{}>,
-                    Footer: ComponentType<{}>) => (WrappedComponent: ComponentType<{}>) =>
-  class Layout extends React.Component<{}, { stickyTop: number }> {
-    state = {stickyTop: 0}
+const withLayout = (
+  Header: ComponentType<{ onStickyTopChanged: number => void }>,
+  Toolbar: ?ComponentType<{}>,
+  Footer: ComponentType<{}>) => (WrappedComponent: ComponentType<{}>
+) => class Layout extends React.Component<{}, { stickyTop: number }> {
+  state = {stickyTop: 0}
 
-    onStickyTopChanged = (stickyTop: number) => this.setState({stickyTop})
+  onStickyTopChanged = (stickyTop: number) => this.setState({stickyTop})
 
-    render () {
-      return (
-        <div className={style.richLayout}>
-          <div>
-            {Header && <Header onStickyTopChanged={this.onStickyTopChanged} {...this.props} />}
-            <div className={style.body}>
-              <aside style={{top: `${this.state.stickyTop}px`}} className={style.aside}>
-                {Toolbar && <Toolbar {...this.props} />}
-              </aside>
-              <main className={style.main}>
-                <WrappedComponent {...this.props} />
-              </main>
-            </div>
+  render () {
+    return (
+      <div className={style.richLayout}>
+        <div>
+          {Header && <Header onStickyTopChanged={this.onStickyTopChanged} {...this.props} />}
+          <div className={style.body}>
+            <aside style={{top: `${this.state.stickyTop}px`}} className={style.aside}>
+              {Toolbar && <Toolbar {...this.props} />}
+            </aside>
+            <main className={style.main}>
+              <WrappedComponent {...this.props} />
+            </main>
           </div>
-          {Footer && <Footer {...this.props} />}
-          <ReactTooltip effect='solid' delayShow={0} />
         </div>
-      )
-    }
+        {Footer && <Footer {...this.props} />}
+        <ReactTooltip effect='solid' delayShow={0} />
+      </div>
+    )
   }
+}
 
 export default withLayout
