9fa6391575a3be0cf17c5563a5402debcfd0f966
Michael Markl <marklmichael98@gmail.com>
2018-03-23 20:24:11 +0100

    WEBAPP-219: Move styles out of Caption

---
+++ src/modules/common/components/Caption.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+  margin: 25px 0;
+  font-size: 2rem;
+  text-align: center;
+  
+  @media ${props => props.theme.dimensions.smallViewport} {
+    margin: 10px 0;
+  }
+`
