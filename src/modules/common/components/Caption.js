3cb0a0175de68c61668c60d5fb2f1a82aa13b1d5
Michael Markl <marklmichael98@gmail.com>
2018-03-23 20:24:11 +0100

    WEBAPP-219: Move styles out of Caption

--- src/modules/common/components/Caption.js
+++ src/modules/common/components/Caption.js
@@ -1,17 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import styled from 'styled-components'
-
-const H1 = styled.h1`
-  margin: 25px 0;
-  font-size: 2rem;
-  text-align: center;
-  
-  @media ${props => props.theme.dimensions.smallViewport} {
-    margin: 10px 0;
-  }
-`
+import { H1 } from './Caption.styles'
 
 class Caption extends React.Component {
   static propTypes = {
