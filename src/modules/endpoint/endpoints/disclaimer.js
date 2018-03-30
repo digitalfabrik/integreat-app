721325c007b1e8da48c3c0a9b43edd2163fef0b6
St <klinzo@yahoo.de>
2018-03-30 18:19:52 +0200

    WEBAPP-223: Throw in endpoints if a param is missing

--- src/modules/endpoint/endpoints/disclaimer.js
+++ src/modules/endpoint/endpoints/disclaimer.js
@@ -4,14 +4,18 @@ import { apiUrl } from '../constants'
 import DisclaimerModel from '../models/DisclaimerModel'
 import { isEmpty } from 'lodash/lang'
 import EndpointBuilder from '../EndpointBuilder'
-
-type Params = {
-  city: string,
-  language: string
-}
+import type { Params } from '../Endpoint'
 
 export default new EndpointBuilder('disclaimer')
-  .withParamsToUrlMapper((params: Params): string => `${apiUrl}/${params.city}/${params.language}/wp-json/extensions/v0/modified_content/disclaimer?since=1970-01-01T00:00:00Z`)
+  .withParamsToUrlMapper((params: Params): string => {
+    if (!params.city) {
+      throw new Error('The city is missing. Could not map the params to the disclaimer endpoint url.')
+    }
+    if (!params.language) {
+      throw new Error('The language is missing. Could not map the params to the disclaimer endpoint url.')
+    }
+    return `${apiUrl}/${params.city}/${params.language}/wp-json/extensions/v0/modified_content/disclaimer?since=1970-01-01T00:00:00Z`
+  })
   .withMapper((json: any): DisclaimerModel => {
     if (isEmpty(json)) {
       throw new Error('disclaimer:notAvailable')
--- src/modules/endpoint/endpoints/languages.js
+++ src/modules/endpoint/endpoints/languages.js
@@ -3,13 +3,15 @@
 import LanguageModel from '../models/LanguageModel'
 import { apiUrl } from '../constants'
 import EndpointBuilder from '../EndpointBuilder'
-
-type Params = {
-  city: string
-}
+import type { Params } from '../Endpoint'
 
 export default new EndpointBuilder('languages')
-  .withParamsToUrlMapper((params: Params): string => `${apiUrl}/${params.city}/de/wp-json/extensions/v0/languages/wpml`)
+  .withParamsToUrlMapper((params: Params): string => {
+    if (!params.city) {
+      throw new Error('The city is missing. Could not map the params to the languages endpoint url.')
+    }
+    return `${apiUrl}/${params.city}/de/wp-json/extensions/v0/languages/wpml`
+  })
   .withMapper((json: any): Array<LanguageModel> => json
     .map(language => new LanguageModel(language.code, language.native_name))
     .sort((lang1, lang2) => lang1.code.localeCompare(lang2.code))
