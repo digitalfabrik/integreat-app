6ed51f6b2f3f264502fcdaadd70e4cd4248266ac
Michael Markl <marklmichael98@gmail.com>
2018-04-07 00:22:44 +0200

    WEBAPP-49: Migrate Layout to StyledComponents

--- src/modules/app/constants/dimensions.js
+++ src/modules/app/constants/dimensions.js
@@ -1,5 +1,8 @@
 export default {
   smallViewport: `(max-width: 750px)`,
+  minMaxWidth: `screen and (min-width: 1100px)`,
+  maxWidth: '1100px',
+  toolbarWidth: '125px',
   headerHeight: '85px',
   headerHeightSmall: '55px'
 }
