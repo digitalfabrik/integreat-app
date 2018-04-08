11a1883a404d3958153450b3020d2252ba9cb0d7
St <klinzo@yahoo.de>
2018-04-08 15:39:24 +0200

    WEBAPP-240: Add LanguageSelectorDropDownItem

---
+++ src/modules/layout/components/LanguageSelectorDropDownItem.styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import ReactTooltip from 'react-tooltip'
+
+export const Tooltip = styled(ReactTooltip)`
+  display: inline-block
+`
+export const InactiveImage = styled.img`
+  opacity: 0.5
+`
