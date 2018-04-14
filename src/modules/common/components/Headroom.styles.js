182d6794c28dbb5571cf33f33033c03a35eddde1
Michael Markl <marklmichael98@gmail.com>
2018-04-14 13:14:12 +0200

    WEBAPP-241: Set positionStickyDisabled as Headroom prop

--- src/modules/common/components/Headroom.styles.js
+++ src/modules/common/components/Headroom.styles.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
-import withPlatform from '../../platform/hocs/withPlatform'
 
-export const HeaderWrapper = withPlatform(styled.div`
-  position: ${props => props.platform.positionStickyDisabled ? 'static' : 'sticky'};
+export const HeaderWrapper = styled.div`
+  position: ${props => props.positionStickyDisabled ? 'static' : 'sticky'};
   top: ${props => props.top}px;
   z-index: 1;
   transform: translateY(${props => props.translateY}px);
   ${props => props.transition && `transition: transform 0.2s ease-out;`}
   ${props => props.static && `transition: none;`}
-`)
+`
