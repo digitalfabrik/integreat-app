c49002a0f2dbcb587dc122707c6effd4d59a039a
Steffen Kleinle <steffen.kleinle@web.de>
2018-04-20 13:03:24 +0200

    WEBAPP-215: Style CategoryTimestamp

---
+++ src/routes/categories/components/CategoryTimestamp.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Timestamp = styled.div`
+  font-family: ${props => props.theme.fonts.contentFontFamily};
+  font-size: ${props => props.theme.fonts.contentFontSize};
+  opacity: 0.5;
+  padding-top: 10px;
+`
