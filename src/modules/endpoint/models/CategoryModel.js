b0df259082d41a1fce69a3b2aa23b61bc3a27ac6
Steffen Kleinle <steffen.kleinle@web.de>
2018-04-25 12:35:03 +0200

    WEBAPP-262: Use api v3 for categories endpoint
    - Use api v3
    - Remove parentId
    - Rename url to path
    - Remove parentUrl mapping

--- src/modules/endpoint/models/CategoryModel.js
+++ src/modules/endpoint/models/CategoryModel.js
@@ -4,25 +4,23 @@ import type { Moment } from 'moment'
 
 class CategoryModel {
   _id: number
-  _url: string
+  _path: string
   _path: string
   _title: string
   _content: string
-  _parentId: number
-  _parentUrl: string
+  _parentPath: string
   _thumbnail: string
   _order: number
   _availableLanguages: Map<string, string>
   _lastUpdate: Moment
 
-  constructor (params: {| id: number, path: string, url: string, title: string, content: string, parentId: number, thumbnail: string,
-    parentUrl: string, order: number, availableLanguages: Map<string, string>, lastUpdate: Moment |}) {
+  constructor (params: {| id: number, path: string, path: string, title: string, content: string, thumbnail: string,
+    parentPath: string, order: number, availableLanguages: Map<string, string>, lastUpdate: Moment |}) {
     this._id = params.id
-    this._url = params.url
+    this._path = params.path
     this._title = params.title
     this._content = params.content
-    this._parentId = params.parentId
-    this._parentUrl = params.parentUrl
+    this._parentPath = params.parentPath
     this._thumbnail = params.thumbnail
     this._order = params.order
     this._path = params.path
@@ -38,10 +36,6 @@ class CategoryModel {
     return this._id
   }
 
-  get url (): string {
-    return this._url
-  }
-
   get path (): string {
     return this._path
   }
@@ -54,16 +48,8 @@ class CategoryModel {
     return this._content
   }
 
-  get parentId (): number {
-    return this._parentId
-  }
-
-  get parentUrl (): string {
-    return this._parentUrl
-  }
-
-  setParentUrl (parentUrl: string) {
-    this._parentUrl = parentUrl
+  get parentPath (): string {
+    return this._parentPath
   }
 
   get order (): number {
