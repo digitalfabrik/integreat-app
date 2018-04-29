ba4de2d1c81b5675a9a66112a752fcc51e414f48
Steffen Kleinle <steffen.kleinle@web.de>
2018-04-29 11:51:09 +0200

    WEBAPP-231: Move tooltip to a separate class

---
+++ src/modules/common/components/Tooltip.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react'
+import ReactTooltip from 'react-tooltip'
+
+class Tooltip extends React.Component<{}> {
+  render () {
+    return <ReactTooltip effect='solid' delayShow={0} />
+  }
+}
+
+export default Tooltip
