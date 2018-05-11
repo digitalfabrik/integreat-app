e328ccccfef8b9fa88505785128e97be53c32dcc
Max Ammann <maximilian.ammann@googlemail.com>
2018-05-11 23:06:21 +0200

    WEBAPP-289: Play with prettier and some eslint rule

--- src/modules/layout/containers/LocationHeader.js
+++ src/modules/layout/containers/LocationHeader.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react'
-import { translate } from 'react-i18next'
+import {translate} from 'react-i18next'
 import compose from 'lodash/fp/compose'
 
 import LanguageSelector from '../../common/containers/LanguageSelector'
@@ -10,16 +10,16 @@ import landingIcon from '../assets/location-icon.svg'
 import Header from 'modules/layout/components/Header'
 import HeaderNavigationItem from '../components/HeaderNavigationItem'
 import HeaderActionItem from '../HeaderActionItem'
-import { EXTRAS_ROUTE, goToExtras } from '../../app/routes/extras'
-import { CATEGORIES_ROUTE, goToCategories } from '../../app/routes/categories'
-import { EVENTS_ROUTE, goToEvents } from '../../app/routes/events'
-import { goToSearch } from '../../app/routes/search'
-import { goToLanding } from '../../app/routes/landing'
-import { connect } from 'react-redux'
+import {EXTRAS_ROUTE, goToExtras} from '../../app/routes/extras'
+import {CATEGORIES_ROUTE, goToCategories} from '../../app/routes/categories'
+import {EVENTS_ROUTE, goToEvents} from '../../app/routes/events'
+import {goToSearch} from '../../app/routes/search'
+import {goToLanding} from '../../app/routes/landing'
+import {connect} from 'react-redux'
 
-import type { LocationState } from 'redux-first-router/dist/flow-types'
+import type {LocationState} from 'redux-first-router/dist/flow-types'
 import EventModel from '../../endpoint/models/EventModel'
-import type { I18nTranslate, State } from '../../../flowTypes'
+import type {I18nTranslate, State} from '../../../flowTypes'
 
 type Props = {
   events: ?Array<EventModel>,
@@ -36,8 +36,14 @@ export class LocationHeader extends React.Component<Props> {
     const {location} = this.props
     const {city, language} = location.payload
     return [
-      new HeaderActionItem({href: goToSearch(city, language), iconSrc: searchIcon}),
-      new HeaderActionItem({href: goToLanding(language), iconSrc: landingIcon}),
+      new HeaderActionItem({
+        href: goToSearch(city, language),
+        iconSrc: searchIcon
+      }),
+      new HeaderActionItem({
+        href: goToLanding(language),
+        iconSrc: landingIcon
+      }),
       new HeaderActionItem({node: <LanguageSelector isHeaderActionItem />})
     ]
   }
@@ -50,39 +56,60 @@ export class LocationHeader extends React.Component<Props> {
     const isEventsActive = events ? events.length > 0 : false
     const isCategoriesEnabled = isExtrasEnabled || isEventsEnabled
 
-    const extrasNavigationItem = isExtrasEnabled && <HeaderNavigationItem
-      key='extras'
-      href={goToExtras(city, language)}
-      selected={currentRoute === EXTRAS_ROUTE}
-      text={t('extras')}
-      active />
+    const items: Array<Element<typeof HeaderNavigationItem>> = []
 
-    const categoriesNavigationItem = isCategoriesEnabled && <HeaderNavigationItem
-      key='categories'
-      href={goToCategories(city, language)}
-      selected={currentRoute === CATEGORIES_ROUTE}
-      text={t('categories')}
-      active />
+    if (isExtrasEnabled) {
+      items.push(
+        <HeaderNavigationItem
+          key='extras'
+          href={goToExtras(city, language)}
+          selected={currentRoute === EXTRAS_ROUTE}
+          text={t('extras')}
+          active
+        />
+      )
+    }
 
-    const eventsNavigationItem = isEventsEnabled && <HeaderNavigationItem
-      key='events'
-      href={goToEvents(city, language)}
-      selected={currentRoute === EVENTS_ROUTE}
-      text={t('news')}
-      tooltip={t('noNews')}
-      active={isEventsActive} />
+    if (isCategoriesEnabled) {
+      items.push(
+        <HeaderNavigationItem
+          key='categories'
+          href={goToCategories(city, language)}
+          selected={currentRoute === CATEGORIES_ROUTE}
+          text={t('categories')}
+          active
+        />
+      )
+    }
 
-    return [extrasNavigationItem, categoriesNavigationItem, eventsNavigationItem].filter(isEnabled => isEnabled)
+    if (isEventsEnabled) {
+      items.push(
+        <HeaderNavigationItem
+          key='events'
+          href={goToEvents(city, language)}
+          selected={currentRoute === EVENTS_ROUTE}
+          text={t('news')}
+          tooltip={t('noNews')}
+          active={isEventsActive}
+        />
+      )
+    }
+
+    return items
   }
 
   render () {
     const {city, language} = this.props.location.payload
 
-    return <Header viewportSmall={this.props.viewportSmall}
-                   logoHref={goToCategories(city, language)}
-                   actionItems={this.getActionItems()}
-                   navigationItems={this.getNavigationItems()}
-                   onStickyTopChanged={this.props.onStickyTopChanged} />
+    return (
+      <Header
+        viewportSmall={this.props.viewportSmall}
+        logoHref={goToCategories(city, language)}
+        actionItems={this.getActionItems()}
+        navigationItems={this.getNavigationItems()}
+        onStickyTopChanged={this.props.onStickyTopChanged}
+      />
+    )
   }
 }
 
@@ -93,7 +120,6 @@ const mapStateToProps = (state: State) => ({
   events: state.events.data
 })
 
-export default compose(
-  connect(mapStateToProps),
-  translate('layout')
-)(LocationHeader)
+export default compose(connect(mapStateToProps), translate('layout'))(
+  LocationHeader
+)
