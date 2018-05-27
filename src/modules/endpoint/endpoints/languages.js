41be9e79f7dfa5e44d5242e2d0e159072dc2a47d
Steffen Kleinle <klinzo@yahoo.de>
2018-05-27 13:43:06 +0200

    WEBAPP-265: Use api v3 in languages endpoint

--- src/modules/endpoint/endpoints/languages.js
+++ src/modules/endpoint/endpoints/languages.js
@@ -13,10 +13,13 @@ export default new EndpointBuilder(LANGUAGES_ENDPOINT_NAME)
     if (!params.city) {
       throw new ParamMissingError(LANGUAGES_ENDPOINT_NAME, 'city')
     }
-    return `${apiUrl}/${params.city}/de/wp-json/extensions/v0/languages/wpml`
+    return `${apiUrl}/${params.city}/de/wp-json/extensions/v3`
   })
   .withMapper((json: any): Array<LanguageModel> => json
-    .map(language => new LanguageModel(language.code, language.native_name))
+    .map(language => new LanguageModel(
+      language.code,
+      language.native_name
+    ))
     .sort((lang1, lang2) => lang1.code.localeCompare(lang2.code))
   )
   .build()
