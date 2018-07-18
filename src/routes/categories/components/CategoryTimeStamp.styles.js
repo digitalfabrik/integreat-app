de04fd24a54db9ca634cb3f9d92194ec5e0677b9
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-18 20:00:14 +0200

    WEBAPP-307: Fix style linting

--- src/routes/categories/components/CategoryTimeStamp.styles.js
+++ src/routes/categories/components/CategoryTimeStamp.styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 export const TimeStamp = styled.p`
+  padding-top: 15px;
+  color: ${props => props.theme.colors.textSecondaryColor};
   font-family: ${props => props.theme.fonts.contentFontFamily};
   font-size: ${props => props.theme.fonts.contentFontSize};
-  color: ${props => props.theme.colors.textSecondaryColor};
-  padding-top: 15px;
 `
