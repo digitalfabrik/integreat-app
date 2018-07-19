63b3351169a411db767575e4565ff4241456607b
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-19 19:23:17 +0200

    WEBAPP-290: Add sprungbrett and wohnen route

--- src/modules/app/routes/extras.js
+++ src/modules/app/routes/extras.js
@@ -1,11 +1,9 @@
 // @flow
 
 import extrasEndpoint from '../../endpoint/endpoints/extras'
-import sprungbrettEndpoint from '../../endpoint/endpoints/sprungbrettJobs'
 import { createAction } from 'redux-actions'
 
 import type { Dispatch, GetState } from 'redux-first-router'
-import ExtraModel from '../../endpoint/models/ExtraModel'
 
 export const EXTRAS_ROUTE = 'EXTRAS'
 
@@ -20,24 +18,11 @@ export const getExtraPath = (city: string, language: string, internalExtra: ?str
  * @type {{path: string, thunk: function(Dispatch, GetState)}}
  */
 export const extrasRoute = {
-  path: '/:city/:language/extras/:internalExtra?',
+  path: '/:city/:language/extras',
   thunk: async (dispatch: Dispatch, getState: GetState) => {
     const state = getState()
-    const {city, language, internalExtra} = state.location.payload
+    const {city, language} = state.location.payload
 
-    const extrasPayload = await extrasEndpoint.loadData(dispatch, state.extras, {city, language})
-
-    // https://github.com/gajus/eslint-plugin-flowtype/issues/342
-    // if (Array.isArray(extrasPayload.data) && extrasPayload.data instanceof Array<ExtraModel>) {
-    if (Array.isArray(extrasPayload.data)) {
-      if (internalExtra === 'sprungbrett') {
-        // $FlowFixMe
-        const sprungbrettModel: ExtraModel = extrasPayload.data
-          .find(extra => extra instanceof ExtraModel && extra.alias === internalExtra)
-        if (sprungbrettModel) {
-          await sprungbrettEndpoint.loadData(dispatch, state.sprungbrettJobs, {city, language, url: sprungbrettModel.path})
-        }
-      }
-    }
+    await extrasEndpoint.loadData(dispatch, state.extras, {city, language})
   }
 }
