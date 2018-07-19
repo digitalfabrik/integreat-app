167c2db7bffe63f4422646ca2643c4ef60277877
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-19 13:58:03 +0200

    WEBAPP-290: Play with recompose and types...

--- src/modules/app/routes/extras.js
+++ src/modules/app/routes/extras.js
@@ -9,31 +9,31 @@ import ExtraModel from '../../endpoint/models/ExtraModel'
 
 export const EXTRAS_ROUTE = 'EXTRAS'
 
-export const goToExtras = (city: string, language: string, extraAlias: ?string) =>
-  createAction(EXTRAS_ROUTE)({city, language, extraAlias})
+export const goToExtras = (city: string, language: string, internalExtra: ?string) =>
+  createAction(EXTRAS_ROUTE)({city, language, internalExtra})
 
-export const getExtraPath = (city: string, language: string, extraAlias: ?string): string =>
-  `/${city}/${language}/extras${extraAlias ? `/${extraAlias}` : ''}`
+export const getExtraPath = (city: string, language: string, internalExtra: ?string): string =>
+  `/${city}/${language}/extras${internalExtra ? `/${internalExtra}` : ''}`
 
 /**
  * ExtrasRoute, matches /augsburg/de/extras and /augsburg/de/extras/sprungbrett
  * @type {{path: string, thunk: function(Dispatch, GetState)}}
  */
 export const extrasRoute = {
-  path: '/:city/:language/extras/:extraAlias?',
+  path: '/:city/:language/extras/:internalExtra?',
   thunk: async (dispatch: Dispatch, getState: GetState) => {
     const state = getState()
-    const {city, language, extraAlias} = state.location.payload
+    const {city, language, internalExtra} = state.location.payload
 
     const extrasPayload = await extrasEndpoint.loadData(dispatch, state.extras, {city, language})
 
     // https://github.com/gajus/eslint-plugin-flowtype/issues/342
     // if (Array.isArray(extrasPayload.data) && extrasPayload.data instanceof Array<ExtraModel>) {
     if (Array.isArray(extrasPayload.data)) {
-      if (extraAlias === 'sprungbrett') {
+      if (internalExtra === 'sprungbrett') {
         // $FlowFixMe
         const sprungbrettModel: ExtraModel = extrasPayload.data
-          .find(extra => extra instanceof ExtraModel && extra.alias === extraAlias)
+          .find(extra => extra instanceof ExtraModel && extra.alias === internalExtra)
         if (sprungbrettModel) {
           await sprungbrettEndpoint.loadData(dispatch, state.sprungbrettJobs, {city, language, url: sprungbrettModel.path})
         }
--- src/routes/extras/containers/ExtrasPage.js
+++ src/routes/extras/containers/ExtrasPage.js
@@ -1,15 +1,11 @@
 // @flow
 
 import * as React from 'react'
-import compose from 'lodash/fp/compose'
 import { connect } from 'react-redux'
 
-import SprungbrettList from '../components/SprungbrettList'
 import TileModel from 'modules/common/models/TileModel'
 import Tiles from 'modules/common/components/Tiles'
 import ExtraModel from 'modules/endpoint/models/ExtraModel'
-import SprungbrettJobModel from '../../../modules/endpoint/models/SprungbrettJobModel'
-import Spinner from 'react-spinkit'
 import Caption from '../../../modules/common/components/Caption'
 import { translate } from 'react-i18next'
 import ContentNotFoundError from '../../../modules/common/errors/ContentNotFoundError'
@@ -18,17 +14,19 @@ import CityModel from '../../../modules/endpoint/models/CityModel'
 import type { StateType } from '../../../flowTypes'
 import type { TFunction } from 'react-i18next'
 import Helmet from '../../../modules/common/containers/Helmet'
+import SprungbrettExtra from 'routes/extras/containers/SprungbrettExtra'
+import { branch, renderComponent, compose } from 'recompose'
+import type { ComponentType } from 'react'
 
 const SPRUNGBRETT_EXTRA = 'sprungbrett'
 
 type PropsType = {
   city: string,
   language: string,
-  extraAlias?: string,
-  extras: Array<ExtraModel>,
-  sprungbrettJobs?: Array<SprungbrettJobModel>,
+  extras: ?Array<ExtraModel>,
+
   t: TFunction,
-  cities: Array<CityModel>
+  cities: ?Array<CityModel>
 }
 
 /**
@@ -39,60 +37,80 @@ export class ExtrasPage extends React.Component<PropsType> {
     return `/${this.props.city}/${this.props.language}/extras/${SPRUNGBRETT_EXTRA}`
   }
 
-  getTileModels (): Array<TileModel> {
-    return this.props.extras.map(extra => new TileModel({
-      id: extra.alias,
-      title: extra.title,
-      // the url stored in the sprungbrett extra is the url of the endpoint
-      path: extra.alias === SPRUNGBRETT_EXTRA ? this.getSprungbrettPath() : extra.path,
-      thumbnail: extra.thumbnail,
-      // every extra except from the sprungbrett extra is just a link to an external site
-      isExternalUrl: extra.alias !== SPRUNGBRETT_EXTRA
-    }))
-  }
-
-  getContent (): React.Node {
-    const LoadingSpinner = () => <Spinner name='line-scale-party' />
-
-    const {extraAlias, extras, sprungbrettJobs, city, language, cities, t} = this.props
-    const cityName = CityModel.findCityName(cities, city)
-    if (extraAlias) {
-      const extra = extras.find(_extra => _extra.alias === extraAlias)
-
-      if (extra && extraAlias === SPRUNGBRETT_EXTRA) {
-        return <React.Fragment>
-          <Helmet title={`${extra.title} - ${cityName}`} />
-          {sprungbrettJobs ? <SprungbrettList title={extra.title} jobs={sprungbrettJobs} /> : <LoadingSpinner />}
-        </React.Fragment>
-      } else {
-      // we currently only implement the sprungbrett extra, so there is no other valid extra path
-        const error = new ContentNotFoundError({type: 'extra', id: extraAlias, city, language})
-        return <FailureSwitcher error={error} />
-      }
-    } else {
-      return <React.Fragment>
-        <Helmet title={`${t('pageTitle')} - ${cityName}`} />
-        <Caption title={t('extras')} />
-        <Tiles tiles={this.getTileModels()} />
-      </React.Fragment>
-    }
+  getTileModels (extras: Array<ExtraModel>): Array<TileModel> {
+    return extras.map(
+      extra =>
+        new TileModel({
+          id: extra.alias,
+          title: extra.title,
+          // the url stored in the sprungbrett extra is the url of the endpoint
+          path: extra.alias === SPRUNGBRETT_EXTRA ? this.getSprungbrettPath() : extra.path,
+          thumbnail: extra.thumbnail,
+          // every extra except from the sprungbrett extra is just a link to an external site
+          isExternalUrl: extra.alias !== SPRUNGBRETT_EXTRA
+        })
+    )
   }
 
   render () {
-    return this.getContent()
+    const { city, cities, extras, t } = this.props
+
+    if (!cities || !extras) {
+      throw new Error('Data not ready')
+    }
+
+    const cityName = CityModel.findCityName(cities, city)
+    // if (internalExtra) {
+    // const extra = extras.find(_extra => _extra.alias === internalExtra)
+
+    // if (extra && internalExtra === SPRUNGBRETT_EXTRA) {
+    //   return <SprungbrettExtra />
+    // } else {
+    // // we currently only implement the sprungbrett extra, so there is no other valid extra path
+    //   const error = new ContentNotFoundError({type: 'extra', id: internalExtra, city, language})
+    //   return <FailureSwitcher error={error} />
+    // }
+
+    return (
+      <React.Fragment>
+        <Helmet title={`${t('pageTitle')} - ${cityName}`} />
+        <Caption title={t('extras')} />
+        <Tiles tiles={this.getTileModels(extras)} />
+      </React.Fragment>
+    )
   }
 }
 
-const mapStateTypeToProps = (stateType: StateType) => ({
-  city: stateType.location.payload.city,
-  language: stateType.location.payload.language,
-  extraAlias: stateType.location.payload.extraAlias,
-  extras: stateType.extras.data,
-  sprungbrettJobs: stateType.sprungbrettJobs.data,
-  cities: stateType.cities.data
+const mapStateToProps = (state: StateType) => ({
+  city: state.location.payload.city,
+  language: state.location.payload.language,
+  internalExtra: state.location.payload.internalExtra,
+  extras: state.extras.data,
+  cities: state.cities.data
 })
 
+class ExtraFinder extends React.Component<PropsType> {
+  render () {
+    const extra = extras.find(_extra => _extra.alias === internalExtra)
+
+    if (extra) {
+      if (internalExtra == SPRUNGBRETT_EXTRA) {
+        return <SprungbrettExtra />
+      }
+    }
+    const error = new ContentNotFoundError({ type: 'extra', id: internalExtra, city, language })
+    return <FailureSwitcher error={error} />
+  }
+}
+
+const preload = test => branch(test, renderComponent(ExtraFinder))
+
 export default compose(
-  connect(mapStateTypeToProps),
-  translate('extras')
-)(ExtrasPage)
+  connect(state => ({ internalExtra: state.location.payload.internalExtra })),
+  preload(props => !!props.internalExtra)
+)(
+  compose(
+    connect(mapStateToProps),
+    translate('extras')
+  )(ExtrasPage)
+)
