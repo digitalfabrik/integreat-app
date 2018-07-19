ef71726d595a7b0d717bb5f8d93ae2695cdb4330
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-19 13:58:03 +0200

    WEBAPP-290: Play with recompose and types...

--- src/modules/app/routes/__tests__/extras.spec.js
+++ src/modules/app/routes/__tests__/extras.spec.js
@@ -4,18 +4,18 @@ describe('extras route', () => {
   it('should create the right action', () => {
     const city = 'augsburg'
     const language = 'de'
-    const extraAlias = 'sprungbrett'
-    expect(goToExtras(city, language, extraAlias)).toEqual({
+    const internalExtra = 'sprungbrett'
+    expect(goToExtras(city, language, internalExtra)).toEqual({
       type: EXTRAS_ROUTE,
       payload: {
         city,
         language,
-        extraAlias
+        internalExtra
       }
     })
   })
 
   it('should have the right path', () => {
-    expect(extrasRoute.path).toBe('/:city/:language/extras/:extraAlias?')
+    expect(extrasRoute.path).toBe('/:city/:language/extras/:internalExtra?')
   })
 })
