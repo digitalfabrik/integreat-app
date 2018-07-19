2506269de33a3246c7685db64c2246670f9874e2
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-19 20:23:41 +0200

    WEBAPP-290: Generify Endpoint

--- src/modules/endpoint/endpoints/languages.js
+++ src/modules/endpoint/endpoints/languages.js
@@ -4,18 +4,17 @@ import LanguageModel from '../models/LanguageModel'
 import { apiUrl } from '../constants'
 import EndpointBuilder from '../EndpointBuilder'
 import ParamMissingError from '../errors/ParamMissingError'
-import type { EndpointParamsType, PayloadDataType } from '../../../flowTypes'
 
 const LANGUAGES_ENDPOINT_NAME = 'languages'
 
 export default new EndpointBuilder(LANGUAGES_ENDPOINT_NAME)
-  .withParamsToUrlMapper((params: EndpointParamsType): string => {
+  .withParamsToUrlMapper((params): string => {
     if (!params.city) {
       throw new ParamMissingError(LANGUAGES_ENDPOINT_NAME, 'city')
     }
     return `${apiUrl}/${params.city}/de/wp-json/extensions/v3/languages`
   })
-  .withMapper((json: any): PayloadDataType => json
+  .withMapper((json: any) => json
     .map(language => new LanguageModel(
       language.code,
       language.native_name
