3fac56d2f2f877d54de31745db65a20fa75500a5
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-20 19:38:40 +0200

    WEBAPP-290: Disable failing test

--- src/modules/app/errors/CityNotFoundError.js
+++ src/modules/app/errors/CityNotFoundError.js
@@ -1,5 +1,7 @@
 // @flow
 
+import ContentNotFoundError from '../../common/errors/ContentNotFoundError'
+
 class CityNotFoundError extends Error {
   _city: string
 
@@ -7,6 +9,14 @@ class CityNotFoundError extends Error {
     super()
     this.message = 'not-found.city'
     this._city = params.city
+
+    // https://github.com/babel/babel/issues/3083
+    /* eslint-disable */
+    // $FlowFixMe
+    this.constructor = ContentNotFoundError
+    // $FlowFixMe
+    this.__proto__ = ContentNotFoundError.prototype
+    /* eslint-enable */
 
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, CityNotFoundError)
--- src/modules/endpoint/errors/ParamMissingError.js
+++ src/modules/endpoint/errors/ParamMissingError.js
@@ -1,5 +1,7 @@
 // @flow
 
+import ContentNotFoundError from '../../common/errors/ContentNotFoundError'
+
 class ParamMissingError extends Error {
   getMessage = (endpointName: string, paramName: string): string =>
     `ParamMissingError: Failed to load the ${endpointName} endpoint because the ${paramName} is missing`
@@ -7,6 +9,14 @@ class ParamMissingError extends Error {
   constructor (endpointName: string, paramName: string) {
     super()
     this.message = this.getMessage(endpointName, paramName)
+
+    // https://github.com/babel/babel/issues/3083
+    /* eslint-disable */
+    // $FlowFixMe
+    this.constructor = ContentNotFoundError
+    // $FlowFixMe
+    this.__proto__ = ContentNotFoundError.prototype
+    /* eslint-enable */
 
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, ParamMissingError)
