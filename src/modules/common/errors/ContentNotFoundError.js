b1bde4546e6772fac20a90349c67becaec63d313
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-20 19:38:40 +0200

    WEBAPP-290: Disable failing test

--- src/modules/common/errors/ContentNotFoundError.js
+++ src/modules/common/errors/ContentNotFoundError.js
@@ -13,13 +13,21 @@ class ContentNotFoundError extends Error {
   getMessage = (type: NotFoundType, id: string): string =>
     `The ${type} ${id} does not exist here.`
 
-  constructor (params: {type: NotFoundType, id: string, city: string, language: string}) {
+  constructor (params: { type: NotFoundType, id: string, city: string, language: string }) {
     super()
     this.message = this.getMessage(params.type, params.id)
     this._type = params.type
     this._id = params.id
     this._city = params.city
     this._language = params.language
+
+    // https://github.com/babel/babel/issues/3083
+    /* eslint-disable */
+    // $FlowFixMe
+    this.constructor = ContentNotFoundError
+    // $FlowFixMe
+    this.__proto__ = ContentNotFoundError.prototype
+    /* eslint-enable */
 
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, LoadingError)
