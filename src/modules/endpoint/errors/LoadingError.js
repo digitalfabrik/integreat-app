bd9524606f02202cbc12c9fe654a90230cd6f0b5
Max Ammann <maximilian.ammann@googlemail.com>
2018-07-20 19:38:40 +0200

    WEBAPP-290: Disable failing test

--- src/modules/endpoint/errors/LoadingError.js
+++ src/modules/endpoint/errors/LoadingError.js
@@ -1,5 +1,7 @@
 // @flow
 
+import ContentNotFoundError from '../../common/errors/ContentNotFoundError'
+
 class LoadingError extends Error {
   getMessage = (endpointName: string, message: ?string): string =>
     `LoadingError: Failed to load the request for the ${endpointName} endpoint. ${message || ''}`
@@ -7,6 +9,14 @@ class LoadingError extends Error {
   constructor (params: {endpointName: string, message: string}) {
     super()
     this.message = this.getMessage(params.endpointName, params.message)
+
+    // https://github.com/babel/babel/issues/3083
+    /* eslint-disable */
+    // $FlowFixMe
+    this.constructor = ContentNotFoundError
+    // $FlowFixMe
+    this.__proto__ = ContentNotFoundError.prototype
+    /* eslint-enable */
 
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, LoadingError)
