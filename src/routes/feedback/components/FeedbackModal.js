ee6d143eda8fa9d5c394c681da8840e1a0ba27ea
Steffen Kleinle <klinzo@yahoo.de>
2018-07-26 19:21:47 +0200

    WEBAPP-208: Always send rating

--- src/routes/feedback/components/FeedbackModal.js
+++ src/routes/feedback/components/FeedbackModal.js
@@ -6,6 +6,8 @@ import CityModel from '../../../modules/endpoint/models/CityModel'
 import FeedbackButton from './FeedbackLink'
 import styled from 'styled-components'
 import CleanLink from '../../../modules/common/components/CleanLink'
+import FeedbackEndpoint, { DEFAULT_FEEDBACK_LANGUAGE, INTEGREAT_INSTANCE }
+  from '../../../modules/endpoint/FeedbackEndpoint'
 
 const Overlay = styled(CleanLink)`
   position: fixed;
@@ -58,18 +60,43 @@ type PropsType = {
   alias?: string,
   query?: string,
   route: string,
-  isPositiveRating: boolean,
+  isPositiveRatingSelected: boolean,
   pathname: string,
   isOpen: boolean
 }
 
 class FeedbackModal extends React.Component<PropsType> {
+  onPositiveFeedbackClicked = (): void => this.onFeedbackClicked(true)
+  onNegativeFeedbackClicked = (): void => this.onFeedbackClicked(false)
+
+  onFeedbackClicked = (isPositiveFeedbackClicked: boolean): void => {
+    const {selectedFeedbackOption} = this.state
+    const {id, city, language, alias, query} = this.props
+
+    if (selectedFeedbackOption) {
+      const feedbackData = {
+        feedbackType: selectedFeedbackOption.feedbackType,
+        isPositiveRating: isPositiveFeedbackClicked,
+        comment: null,
+        id,
+        city: city || INTEGREAT_INSTANCE,
+        language: language || DEFAULT_FEEDBACK_LANGUAGE,
+        alias,
+        query
+      }
+      FeedbackEndpoint.postData(feedbackData)
+    }
+  }
+
   render () {
     const {pathname, isOpen} = this.props
     return (
       <div>
-        <FeedbackToolbarItem isPositiveRatingLink pathname={pathname} />
-        <FeedbackToolbarItem isPositiveRatingLink={false} pathname={pathname} />
+        <FeedbackToolbarItem isPositiveRatingLink pathname={pathname} onClick={this.onPositiveFeedbackClicked} />
+        <FeedbackToolbarItem
+          isPositiveRatingLink={false}
+          pathname={pathname}
+          onClick={this.onNegativeFeedbackClicked} />
         <ModalContainer isOpen={isOpen}>
           <FeedbackContainer>
             <Feedback {...this.props} />
