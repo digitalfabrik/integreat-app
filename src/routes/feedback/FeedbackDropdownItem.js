1bfea5b839d3fdb12512b1f2be3bddf60240a32d
Steffen Kleinle <klinzo@yahoo.de>
2018-07-31 11:42:17 +0200

    WEBAPP-324: Use SyntheticInputEvent

--- src/routes/feedback/FeedbackDropdownItem.js
+++ src/routes/feedback/FeedbackDropdownItem.js
@@ -5,7 +5,7 @@ class FeedbackDropdownItem {
   feedbackType: ?string
   label: string
 
-  constructor (label: string, feedbackType: string) {
+  constructor (label: string, feedbackType: ?string) {
     this.value = label
     this.feedbackType = feedbackType
     this.label = label
