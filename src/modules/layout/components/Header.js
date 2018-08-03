578d01e95f614f92b33a46f7976c8069e33403cb
Max Ammann <maximilian.ammann@googlemail.com>
2018-08-03 15:48:15 +0200

    Add buttons to header

--- src/modules/layout/components/Header.js
+++ src/modules/layout/components/Header.js
@@ -1,7 +1,9 @@
 import * as React from 'react'
 import logo from '../assets/integreat-app-logo.png'
 import styled from 'styled-components'
-import { Text } from 'react-native-elements'
+import HeaderButtons, { HeaderButton, Item } from 'react-navigation-header-buttons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import { StyleSheet } from 'react-native'
 
 const Horizonal = styled.View`
   flex:1;
@@ -27,15 +29,41 @@ const BoxShadow = styled.View`
   background-color: #fafafa;
   height: 60px;
 `
+
+const MaterialHeaderButton = props => (
+  <HeaderButton {...props} IconComponent={MaterialIcons} iconSize={23} color='black' />
+)
+
+export const MaterialHeaderButtons = props => {
+  return (
+    <HeaderButtons
+      HeaderButtonComponent={MaterialHeaderButton}
+      OverflowIcon={<MaterialIcons name='more-vert' size={23} color='black' />}
+      {...props}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  headerButtons: {
+    alignSelf: 'flex-end'
+  }
+})
+
 const Header = props => {
   const {options} = props.scene.descriptor
   const headerTitle = options.headerTitle
-  console.log(JSON.stringify(options))
+
   return (
     <BoxShadow>
       <Horizonal>
         <Logo source={logo} />
         <Title>{headerTitle}</Title>
+        <MaterialHeaderButtons>
+          <Item title='add' iconName='search' onPress={() => console.warn('add')} />
+          <Item title='edit' iconName='edit' onPress={() => console.warn('edit')} />
+          <Item title='asdf' show='never' onPress={() => console.warn('edit')} />
+        </MaterialHeaderButtons>
       </Horizonal>
     </BoxShadow>
   )
