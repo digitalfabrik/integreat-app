d3a8c4c3c36aab77fa7295b222b31666564d54b9
Michael Markl <marklmichael98@gmail.com>
2018-08-06 19:59:18 +1000

    WEBAPP-334: Add Tile test

--- src/modules/common/components/__tests__/Tile.spec.js
+++ src/modules/common/components/__tests__/Tile.spec.js
@@ -20,6 +20,20 @@ const tile2 = new TileModel({
   isExternalUrl: true
 })
 
+const lehrstellenRadarPostData = new Map()
+lehrstellenRadarPostData.set('partner', '0006')
+lehrstellenRadarPostData.set('radius', '50')
+lehrstellenRadarPostData.set('plz', '86150')
+
+const tile3 = new TileModel({
+  id: 3649,
+  path: 'https://example.com',
+  title: 'Example',
+  thumbnail: 'https://cms.integreat-ap…03/Beratung-150x150.png',
+  isExternalUrl: true,
+  postData: lehrstellenRadarPostData
+})
+
 describe('Tile', () => {
   it('should render a Link if path is not an external url', () => {
     const wrapper = shallow(
@@ -31,6 +45,13 @@ describe('Tile', () => {
   it('should render a a-Tag if path is not an external url', () => {
     const wrapper = shallow(
       <Tile tile={tile2} />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('should render a form-Tag if path is not an external url and has postData', () => {
+    const wrapper = shallow(
+      <Tile tile={tile3} />
     )
     expect(wrapper).toMatchSnapshot()
   })
