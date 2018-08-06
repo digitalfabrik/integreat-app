fe528c94d035670837265182db000d1da4101369
Steffen Kleinle <klinzo@yahoo.de>
2018-08-06 13:10:50 +0200

    WEBAPP-208: Fix size of toolbar items

--- src/routes/feedback/components/FeedbackLink.js
+++ src/routes/feedback/components/FeedbackLink.js
@@ -9,6 +9,9 @@ import ReactTooltip from 'react-tooltip'
 import { NEGATIVE_RATING, POSITIVE_RATING } from '../../../modules/endpoint/FeedbackEndpoint'
 import type { LocationState } from 'redux-first-router'
 import { goToFeedback } from '../../../modules/app/routes/feedback'
+import { StyledToolbarItem } from '../../../modules/layout/components/ToolbarItem'
+
+const StyledLink = StyledToolbarItem.withComponent(CleanLink)
 
 type PropsType = {
   isPositiveRatingLink: boolean,
@@ -26,14 +29,14 @@ export class FeedbackLink extends React.PureComponent<PropsType> {
   render () {
     const {t, isPositiveRatingLink, location, className} = this.props
     return (
-      <CleanLink
+      <StyledLink
         className={className}
         to={goToFeedback(location, isPositiveRatingLink ? POSITIVE_RATING : NEGATIVE_RATING)}>
         <FontAwesome
           className={className}
           data-tip={isPositiveRatingLink ? t('positiveRating') : t('negativeRating')}
           name={isPositiveRatingLink ? 'smile-o' : 'frown-o'} />
-      </CleanLink>
+      </StyledLink>
     )
   }
 }
