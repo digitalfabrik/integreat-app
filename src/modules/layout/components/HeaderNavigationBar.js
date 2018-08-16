273bd9136af61b52c32c170b4a2057dd6e413797
Steffen Kleinle <klinzo@yahoo.de>
2018-08-16 15:21:25 +0200

    WEBAPP-257: Add flow to more files

--- src/modules/layout/components/HeaderNavigationBar.js
+++ src/modules/layout/components/HeaderNavigationBar.js
@@ -1,29 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+// @flow
+
+import * as React from 'react'
 import cx from 'classnames'
 import { isEmpty } from 'lodash/lang'
 
 import style from './HeaderNavigationBar.css'
 
+type PropsType = {
+  className?: string,
+  children: React.Node
+}
+
 /**
  * Designed to work with Header. In the MenuBar you can display textual links. Should be used for navigating as a
  * main menu.
  */
-class HeaderNavigationBar extends React.Component {
-  static propTypes = {
-    className: PropTypes.string,
-    children: PropTypes.node
-  }
-
+class HeaderNavigationBar extends React.Component<PropsType> {
   render () {
-    return <div
-      className={cx({
-        [this.props.className]: this.props.className,
-        [style.navigationBar]: true,
-        [style.hidden]: isEmpty(this.props.children)
-      })}>
-      {this.props.children}
-    </div>
+    const {className, children} = this.props
+    const combinedClassName = {
+      [style.navigationBar]: true,
+      [style.hidden]: isEmpty(children)
+    }
+    if (className) {
+      combinedClassName[className] = className
+    }
+
+    return (
+      <div className={cx(combinedClassName)}>
+        {children}
+      </div>
+    )
   }
 }
 
