2f8c3db13e551d6aca9e8bf1bd3bb5c6c7379e2b
Michael Markl <marklmichael98@gmail.com>
2018-08-20 09:58:33 +1000

    WEBAPP-256: Fix two endpoint tests

--- src/modules/endpoint/__tests__/Payload.spec.js
+++ src/modules/endpoint/__tests__/Payload.spec.js
@@ -3,7 +3,7 @@ import Payload from '../Payload'
 
 describe('Payload', () => {
   const data = {data: 'something'}
-  const error = 'Something bad happened'
+  const error = new Error('Something bad happened')
   const fetchUrl = 'https://weird-endpoint/api.json'
   const fetchDate = 0
 
