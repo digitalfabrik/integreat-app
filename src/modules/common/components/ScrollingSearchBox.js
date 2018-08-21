c2221141f043b25c8429e9eb95b235cf24850123
Steffen Kleinle <klinzo@yahoo.de>
2018-08-21 11:31:07 +0200

    WEBAPP-257: Revert fix children prop type

--- src/modules/common/components/ScrollingSearchBox.js
+++ src/modules/common/components/ScrollingSearchBox.js
@@ -1,6 +1,6 @@
 // @flow
 
-import type { Node } from 'react'
+import type { Element } from 'react'
 import React from 'react'
 
 import SearchInput from './SearchInput'
@@ -12,7 +12,7 @@ type PropsType = {
   filterText: string,
   onFilterTextChange: (string) => void,
   spaceSearch: boolean,
-  children: Node,
+  children: Element<*>,
   placeholderText: string
 }
 
