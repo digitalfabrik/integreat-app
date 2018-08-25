85838c134b23fbdc0cb50e2e8e31569790991a4e
Max Ammann <maximilian.ammann@googlemail.com>
2018-08-25 10:43:47 +0200

    Fix fetching while not debugging

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import { AppRegistry } from 'react-native'
 import App from './src/modules/app/components/App'
 
+// @see: https://github.com/facebook/react-native/issues/9599
+if (typeof global.self === 'undefined') {
+  global.self = global
+}
+
 AppRegistry.registerComponent('Integreat', () => App)
