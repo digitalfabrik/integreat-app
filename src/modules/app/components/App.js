bb9a04d38c089bb8df237949b31d314875c803d2
Max Ammann <maximilian.ammann@googlemail.com>
2018-08-25 10:44:18 +0200

    Add i18next support

--- src/modules/app/components/App.js
+++ src/modules/app/components/App.js
@@ -1,11 +1,14 @@
 // @flow
 
 import * as React from 'react'
-import { Text } from 'react-native-elements'
 import PlatformContext from '../../platform/PlatformContext'
 import Platform from '../../platform/Platform'
 import LayoutContainer from '../../layout/container/LayoutContainer'
 import Navigator from './Navigator'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '../i18n'
+import { ThemeProvider } from 'styled-components'
+import theme from 'modules/layout/constants/theme'
 
 class App extends React.Component<{}> {
   platform: Platform
@@ -17,11 +20,15 @@ class App extends React.Component<{}> {
 
   render () {
     return (
-      <PlatformContext.Provider value={this.platform}>
-        <LayoutContainer>
-          <Navigator />
-        </LayoutContainer>
-      </PlatformContext.Provider>
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider theme={theme}>
+        <PlatformContext.Provider value={this.platform}>
+          <LayoutContainer>
+            <Navigator />
+          </LayoutContainer>
+        </PlatformContext.Provider>
+        </ThemeProvider>
+      </I18nextProvider>
     )
   }
 }
