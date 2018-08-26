2980629e74226c16625c4aaacdf2c83d7c81dcad
Michael Markl <marklmichael98@gmail.com>
2018-08-26 13:16:13 +1000

    WEBAPP-312: Remove Toolbar.css

--- src/modules/layout/components/Toolbar.js
+++ src/modules/layout/components/Toolbar.js
@@ -1,9 +1,33 @@
 // @flow
 
-import React from 'react'
 import type { Node } from 'react'
-import cx from 'classnames'
-import style from './Toolbar.css'
+import React from 'react'
+import styled from 'styled-components'
+
+const ToolbarContainer = styled.div`
+  display: flex;
+  width: 75px;
+  box-sizing: border-box;
+  flex-direction: column;
+  align-items: center;
+  padding: 15px 0;
+  
+  & > * {
+    opacity: 0.7;
+    font-size: 1.5rem;
+    transition: 0.2s opacity;
+  }
+
+  & > *:hover {
+    opacity: 1;
+  }
+
+@media ${props => props.theme.dimensions.smallViewport} {
+  width: 100%;
+  flex-direction: row;
+  justify-content: center;
+}
+`
 
 type PropsType = {
   className?: string,
@@ -12,9 +36,9 @@ type PropsType = {
 
 class Toolbar extends React.Component<PropsType> {
   render () {
-    return <div className={cx(style.toolbar, this.props.className)}>
+    return <ToolbarContainer className={this.props.className}>
       {this.props.children}
-    </div>
+    </ToolbarContainer>
   }
 }
 
