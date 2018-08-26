5370dd69b3360f97dd02be7c00aa8bb446d14414
Michael Markl <marklmichael98@gmail.com>
2018-08-26 14:27:05 +1000

    WEBAPP-312: Remove Tile.css

--- src/modules/common/components/Tile.js
+++ src/modules/common/components/Tile.js
@@ -1,41 +1,75 @@
 // @flow
 
 import * as React from 'react'
-import Link from 'redux-first-router-link'
 import { Col } from 'react-styled-flexboxgrid'
 
-import style from './Tile.css'
+import styled from 'styled-components'
 import TileModel from '../models/TileModel'
+import CleanLink from './CleanLink'
+import CleanAnchor from './CleanAnchor'
 
 type PropsType = {
   tile: TileModel
 }
+
+const Thumbnail = styled.div`
+  position: relative;
+  display: block;
+  width: 100%;
+  margin: 0 auto;
+  padding-top: 100%;
+
+  & img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    transition: transform 0.2s;
+    object-fit: contain;
+  }
+
+  & img:hover {
+    transform: scale(1.01);
+  }
+`
+
+const ThumbnailSizer = styled.div`
+  width: 150px;
+  max-width: 33.3vw;
+  margin: 0 auto;
+`
+
+const TileTitle = styled.div`
+  margin: 5px 0;
+  text-align: center;
+`
+
+const StyledCol = styled(Col)`
+  margin: 5px 0;
+  text-align: center;
+`
 
 /**
  * Displays a single Tile
  */
 class Tile extends React.Component<PropsType> {
   getTileContent (): React.Node {
-    return <React.Fragment>
-      <div className={style.thumbnailSizer}>
-        <div className={style.thumbnail}>
-          <img src={this.props.tile.thumbnail} />
-        </div>
-      </div>
-      <div className={style.title}>{this.props.tile.title}</div>
-    </React.Fragment>
+    return <>
+      <ThumbnailSizer>
+        <Thumbnail><img src={this.props.tile.thumbnail} /></Thumbnail>
+      </ThumbnailSizer>
+      <TileTitle>{this.props.tile.title}</TileTitle>
+    </>
   }
 
   getTile (): React.Node {
     const tile = this.props.tile
     if (!tile.isExternalUrl) {
-      return <Link to={tile.path}>
-        {this.getTileContent()}
-      </Link>
+      return <CleanLink to={tile.path}>{this.getTileContent()}</CleanLink>
     } else if (!tile.postData) {
-      return <a href={tile.path} target='_blank'>
-        {this.getTileContent()}
-      </a>
+      return <CleanAnchor href={tile.path} target='_blank'>{this.getTileContent()}</CleanAnchor>
     } else {
       const inputs = []
       tile.postData.forEach((value, key) => inputs.unshift(<input type='hidden' value={value} key={key} name={key} />))
@@ -48,9 +82,9 @@ class Tile extends React.Component<PropsType> {
 
   render () {
     return (
-      <Col xs={6} sm={4} md={3} className={style.tile}>
+      <StyledCol xs={6} sm={4} md={3}>
         {this.getTile()}
-      </Col>
+      </StyledCol>
     )
   }
 }
