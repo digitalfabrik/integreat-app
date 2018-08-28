5348061b6e948efa52a80927f6d06669dc53da3d
Michael Markl <marklmichael98@gmail.com>
2018-08-28 12:49:43 +1000

    WEBAPP-344: Use FortAwesome instead of ReactFontAwesome

--- src/modules/layout/components/__tests__/ToolbarItem.spec.js
+++ src/modules/layout/components/__tests__/ToolbarItem.spec.js
@@ -2,13 +2,14 @@
 
 import React from 'react'
 import { mount, shallow } from 'enzyme'
+import { faSmile } from 'modules/app/constants/icons'
 
 import ToolbarItem from '../ToolbarItem'
 import ReactTooltip from 'react-tooltip'
 
 describe('ToolbarItem', () => {
   it('should render', () => {
-    const component = shallow(<ToolbarItem href='http://example.com' name='testName' text='Click here!' />)
+    const component = shallow(<ToolbarItem href='http://example.com' icon={faSmile} text='Click here!' />)
     expect(component).toMatchSnapshot()
   })
 
@@ -16,7 +17,7 @@ describe('ToolbarItem', () => {
     const original = ReactTooltip.rebuild
     ReactTooltip.rebuild = jest.fn()
 
-    mount(<ToolbarItem href='http://example.com' name='testName' text='Click here!' />)
+    mount(<ToolbarItem href='http://example.com' icon={faSmile} text='Click here!' />)
     expect(ReactTooltip.rebuild).toHaveBeenCalled()
 
     ReactTooltip.rebuild = original
