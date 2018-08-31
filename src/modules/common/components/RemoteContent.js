bcf172f18bdf34765f1112f405745a15081f9df9
Michael Markl <marklmichael98@gmail.com>
2018-08-31 11:21:10 +1000

    WEBAPP-351: Replace hosts by regexp

--- src/modules/common/components/RemoteContent.js
+++ src/modules/common/components/RemoteContent.js
@@ -8,15 +8,18 @@ type PropsType = {
   dangerouslySetInnerHTML: {
     __html: string
   },
-  hosts: Array<string>,
+  hijackRegExp: RegExp,
   onInternLinkClick: string => void,
   centered: boolean
 }
 
+const HIJACK =
+  /https?:\/\/(cms.integreat-app.de|web.integreat-app.de|integreat.app)(?!\/[^/]*\/(wp-content|wp-admin|wp-json)\/.*).*/
+
 class RemoteContent extends React.Component<PropsType> {
   static defaultProps = {
     centered: false,
-    hosts: ['cms.integreat-app.de', 'web.integreat-app.de']
+    hijackRegExp: HIJACK
   }
 
   sandBoxRef: { current: null | React$ElementRef<*> }
@@ -38,7 +41,7 @@ class RemoteContent extends React.Component<PropsType> {
     }
     const collection: HTMLCollection<HTMLAnchorElement> = this.sandBoxRef.current.getElementsByTagName('a')
     Array.from(collection).forEach(node => {
-      if (this.props.hosts.includes(new URL(node.href).hostname)) {
+      if (this.props.hijackRegExp.test(node.href)) {
         node.addEventListener('click', this.handleClick)
       }
     })
