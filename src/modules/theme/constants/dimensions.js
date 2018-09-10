3e81442bd558044870b097e5edf9711cd76dff86
Max Ammann <maximilian.ammann@googlemail.com>
2018-09-10 11:41:35 +0200

    Adapt dimensions for mobile

--- src/modules/theme/constants/dimensions.js
+++ src/modules/theme/constants/dimensions.js
@@ -1,19 +1,9 @@
 // @flow
 
 export type DimensionsType = {
-  smallViewport: string,
-  minMaxWidth: string,
-  maxWidth: number,
-  toolbarWidth: number,
-  headerHeightLarge: number,
-  headerHeightSmall: number
+  headerHeight: number
 }
 
 export default {
-  smallViewport: '(max-width: 750px)',
-  minMaxWidth: 'screen and (min-width: 1100px)',
-  maxWidth: 1100,
-  toolbarWidth: 125,
-  headerHeightLarge: 85,
-  headerHeightSmall: 55
+  headerHeight: 60
 }
