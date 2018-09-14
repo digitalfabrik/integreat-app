cefcf40322be69645a968aef3c20870b5a62fe1a
Max Ammann <maximilian.ammann@googlemail.com>
2018-09-14 10:49:52 +0200

    Make state readonly

--- src/modules/app/StateType.js
+++ src/modules/app/StateType.js
@@ -4,13 +4,13 @@ import type { StoreActionType } from './StoreActionType'
 import type { PersistState } from 'redux-persist/src/types'
 
 export type StateType = {
-  uiDirection: string,
-  language: string | void,
-  darkMode: boolean,
-  network: { isConnected: boolean, actionQueue: Array<StoreActionType> },
-  data: {
-    cities: any | void,
-    categories: any | void
+  +uiDirection: string,
+  +language: string | void,
+  +darkMode: boolean,
+  +network: { +isConnected: boolean, +actionQueue: Array<StoreActionType> },
+  +data: {
+    +cities: { +json: any | void },
+    +categories: { +json: any | void, +city: string | void }
   },
-  _persist: PersistState
+  +_persist: PersistState
 }
