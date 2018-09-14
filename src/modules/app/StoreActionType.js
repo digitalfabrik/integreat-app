1010666e76be137bd7760e867f75dac4bd1f9ca5
Max Ammann <maximilian.ammann@googlemail.com>
2018-09-14 10:49:41 +0200

    Specify all actions

--- src/modules/app/StoreActionType.js
+++ src/modules/app/StoreActionType.js
@@ -3,12 +3,18 @@
 import Payload from '../endpoint/Payload'
 import { offlineActionTypes } from 'react-native-offline'
 
-type PayloadActionType<T> = { type: string, payload: Payload<T> }
+type ActionType<T> = { type: T }
 
-export type CitiesActionType = PayloadActionType<any> & { params: { language: string } }
+type PayloadActionType<T, P> = ActionType<T> & { payload: Payload<P> }
 
-export type CategoriesActionType = PayloadActionType<any> & { params: { language: string, city: string } }
+export type FetchCitiesRequestActionType = ActionType<'FETCH_CITIES_REQUEST'> & { params: { language: string } }
+export type CitiesFetchSucceededActionType = PayloadActionType<'CITIES_FETCH_SUCCEEDED', any>
+export type CitiesActionType = FetchCitiesRequestActionType | CitiesFetchSucceededActionType
 
-export type ConnectionChangeActionType = { type: offlineActionTypes.CONNECTION_CHANGE, payload: Payload<boolean> }
+export type FetchCategoriesRequestActionType = ActionType<'FETCH_CATEGORIES_REQUEST'> & { params: { language: string, city: string } }
+export type CategoriesFetchSucceededActionType = PayloadActionType<'CATEGORIES_FETCH_SUCCEEDED', any>
+export type CategoriesActionType = FetchCategoriesRequestActionType | CategoriesFetchSucceededActionType
+
+export type ConnectionChangeActionType = ActionType<offlineActionTypes.CONNECTION_CHANGE> & { payload: boolean }
 
 export type StoreActionType = ConnectionChangeActionType | CitiesActionType | CategoriesActionType
