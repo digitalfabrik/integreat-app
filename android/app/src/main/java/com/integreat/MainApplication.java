ca2f2459e77402b3fa32b87120a3cb4cc0919487
Max Ammann <maximilian.ammann@googlemail.com>
2018-09-20 22:24:10 +0200

    Upgrade react-native-webview
    It should compile for ios now

--- android/app/src/main/java/com/integreat/MainApplication.java
+++ android/app/src/main/java/com/integreat/MainApplication.java
@@ -3,7 +3,7 @@ package com.integreat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.webview.RCTWebViewPackage;
+import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RCTWebViewPackage(),
+            new RNCWebViewPackage(),
             new RNFetchBlobPackage(),
             new VectorIconsPackage()
       );
