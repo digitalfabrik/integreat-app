702f390fb0add10a49d50ece476b91125299bfe2
Steffen Kleinle <klinzo@yahoo.de>
2018-09-30 18:31:52 +0200

    WEBAPP-339: Fix merge

--- src/modules/layout/components/__tests__/LocationToolbar.spec.js
+++ src/modules/layout/components/__tests__/LocationToolbar.spec.js
@@ -4,13 +4,11 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import LocationToolbar from '../LocationToolbar'
-import { EVENTS_ROUTE } from '../../../app/routes/events'
 
 describe('LocationToolbar', () => {
   it('should match snapshot', () => {
-    const location = {type: EVENTS_ROUTE, pathname: '/augsburg/de/events', payload: {city: 'augsburg', language: 'de'}}
     const component = shallow(
-      <LocationToolbar location={location} openFeedbackModal={() => {}}>
+      <LocationToolbar openFeedbackModal={() => {}}>
         <div>MockNode</div>
       </LocationToolbar>
     )
