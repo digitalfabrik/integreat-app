b43e4a1d786c4a1c77215b642693bd27db0518c3
Michael Markl <marklmichael98@gmail.com>
2018-10-01 13:02:04 +1000

    WEBAPP-361: Upgrade version to 0.0.2 and publish to npm

--- tools/lib.webpack.config.js
+++ tools/lib.webpack.config.js
@@ -11,7 +11,7 @@ const isVerbose = process.argv.includes('--verbose') || process.argv.includes('-
 
 const basePackageValues = {
   'name': '@integreat-app/shared',
-  'version': '0.0.1',
+  'version': '0.0.2',
   'main': './index.js'
 }
 
