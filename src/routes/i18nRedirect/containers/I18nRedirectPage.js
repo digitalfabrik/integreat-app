93eb10e5298a981af32673aa7b7932dbe3958b7c
Steffen Kleinle <klinzo@yahoo.de>
2018-10-03 11:42:44 +0200

    WEBAPP-339: Remove Provider in (18nRedirectPage tests

--- src/routes/i18nRedirect/containers/I18nRedirectPage.js
+++ src/routes/i18nRedirect/containers/I18nRedirectPage.js
@@ -15,9 +15,7 @@ import type { StateType } from '../../../modules/app/StateType'
 type PropsType = {|
   redirect: Action => void,
   cities: ?Array<CityModel>,
-  param?: string,
-  // Custom redux state for testing purposes
-  store?: StateType
+  param?: string
 |}
 
 /**
