37443d22d4a91d7fed9d5b1c4e2d479d382719c1
Steffen Kleinle <klinzo@yahoo.de>
2018-10-09 11:42:46 +0200

    WEBAPP-363: Revert endpoints

--- src/modules/endpoint/endpoints/categories.js
+++ src/modules/endpoint/endpoints/categories.js
@@ -5,8 +5,9 @@ import CategoriesMapModel from '../models/CategoriesMapModel'
 import { apiUrl } from '../constants'
 import EndpointBuilder from '../EndpointBuilder'
 import moment from 'moment'
-import type { JsonPageType } from '../types'
-import mapPages from './mapPages'
+import type { JsonCategoryType } from '../types'
+import mapAvailableLanguages from '../mapAvailableLanguages'
+import normalizePath from '../normalizePath'
 
 const CATEGORIES_ENDPOINT_NAME = 'categories'
 
@@ -16,17 +17,24 @@ export default new EndpointBuilder(CATEGORIES_ENDPOINT_NAME)
   .withParamsToUrlMapper((params: ParamsType): string =>
     `${apiUrl}/${params.city}/${params.language}/wp-json/extensions/v3/pages`
   )
-  .withMapper((json: Array<JsonPageType>, params: ParamsType): CategoriesMapModel => {
+  .withMapper((json: Array<JsonCategoryType>, params: ParamsType): CategoriesMapModel => {
     const basePath = `/${params.city}/${params.language}`
-    const categories = mapPages(json, basePath).map(page => {
-      const {order, parentPath, date, location, ...pageProps} = page
-      if (!order && order !== 0) {
-        throw new Error(`The order of ${pageProps.path} is missing`)
-      } else if (!parentPath) {
-        throw new Error(`The parentPath of ${pageProps.path} is missing`)
-      }
-      return new CategoryModel({order, parentPath, ...pageProps})
-    })
+
+    const categories = json
+      .map(category => {
+        return new CategoryModel({
+          id: category.id,
+          path: normalizePath(category.path),
+          title: category.title,
+          content: category.content,
+          thumbnail: category.thumbnail,
+          order: category.order,
+          availableLanguages: mapAvailableLanguages(category.available_languages),
+          parentPath: normalizePath(category.parent.path || basePath),
+          lastUpdate: moment(category.modified_gmt),
+          excerpt: category.excerpt
+        })
+      })
 
     categories.push(new CategoryModel({
       id: 0,
--- src/modules/endpoint/endpoints/pois.js
+++ src/modules/endpoint/endpoints/pois.js
@@ -2,9 +2,12 @@
 
 import { apiUrl } from '../constants'
 import EndpointBuilder from '../EndpointBuilder'
-import type { JsonPageType } from '../types'
-import mapPages from './mapPages'
+import type { JsonPoiType } from '../types'
 import PoiModel from '../models/PoiModel'
+import normalizePath from '../normalizePath'
+import mapAvailableLanguages from '../mapAvailableLanguages'
+import moment from 'moment'
+import LocationModel from '../models/LocationModel'
 
 const POIS_ENDPOINT_NAME = 'pois'
 
@@ -13,13 +16,24 @@ type ParamsType = { city: string, language: string }
 export default new EndpointBuilder(POIS_ENDPOINT_NAME)
   .withParamsToUrlMapper((params: ParamsType): string =>
     `${apiUrl}/${params.city}/${params.language}/wp-json/extensions/v3/locations`)
-  .withMapper((json: Array<JsonPageType>): Array<PoiModel> => mapPages(json)
-    .map(page => {
-      const {order, parentPath, date, location, ...pageProps} = page
-      if (!location) {
-        throw new Error('A location is required to create an PoiModel')
-      }
-      return new PoiModel({location, ...pageProps})
-    })
-  )
+  .withMapper((json: Array<JsonPoiType>) =>
+    json.map(poi => {
+      return new PoiModel({
+        id: poi.id,
+        path: normalizePath(poi.path),
+        title: poi.title,
+        content: poi.content,
+        thumbnail: poi.thumbnail,
+        availableLanguages: mapAvailableLanguages(poi.available_languages),
+        excerpt: poi.excerpt,
+        location: new LocationModel({
+          address: poi.location.address,
+          town: poi.location.town,
+          postcode: poi.location.postcode,
+          longitude: poi.location.longitude,
+          latitude: poi.location.latitude
+        }),
+        lastUpdate: moment(poi.modified_gmt)
+      })
+    }))
   .build()
