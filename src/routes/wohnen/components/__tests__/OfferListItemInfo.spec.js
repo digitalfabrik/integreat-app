57092003e8fb0babb22112fa7e184f1d9c954bf1
Steffen Kleinle <klinzo@yahoo.de>
2018-10-09 17:17:06 +0200

    WEBAPP-376: Update tests and snapshots

--- src/routes/wohnen/components/__tests__/OfferListItemInfo.spec.js
+++ src/routes/wohnen/components/__tests__/OfferListItemInfo.spec.js
@@ -2,61 +2,17 @@
 
 import { shallow } from 'enzyme'
 import React from 'react'
-import WohnenOfferModel from '../../../../modules/endpoint/models/WohnenOfferModel'
-import moment from 'moment'
-import WohnenFormData from '../../../../modules/endpoint/models/WohnenFormData'
 import OfferListItemInfo from '../OfferListItemInfo'
 
-describe('OfferList', () => {
-  const offer = new WohnenOfferModel({
-    email: 'mail@mail.com',
-    createdDate: moment('2018-07-24T00:00:00.000Z'),
-    formDataType: WohnenFormData,
-    formData: new WohnenFormData(
-      {
-        firstName: 'Max',
-        lastName: 'Ammann',
-        phone: ''
-      },
-      {
-        ofRooms: ['kitchen', 'child2', 'child1', 'bed'],
-        title: 'Test Angebot',
-        location: 'Augsburg',
-        totalArea: 120,
-        totalRooms: 4,
-        moveInDate: moment('2018-07-19T15:35:12.000Z'),
-        ofRoomsDiff: ['bath', 'wc', 'child3', 'livingroom', 'hallway', 'store', 'basement', 'balcony']
-      },
-      {
-        ofRunningServices: ['chimney', 'other'],
-        ofAdditionalServices: ['garage'],
-        baseRent: 1000,
-        runningCosts: 0,
-        hotWaterInHeatingCosts: true,
-        additionalCosts: 200,
-        ofRunningServicesDiff: ['heating', 'water', 'garbage'],
-        ofAdditionalServicesDiff: []
-      })
-  })
-
-  class InvalidForm {
-    test: string
-  }
-
-  const notRenderableOffer = new WohnenOfferModel({
-    email: 'mail@mail.com',
-    createdDate: moment('2018-07-24T00:00:00.000Z'),
-    formData: new InvalidForm()
-  })
+describe('OfferListItemInfo', () => {
+  const totalArea = 120
+  const totalRooms = 4
+  const baseRent = 1000
 
   it('should render offer item', () => {
     const offerDetail = shallow(
-      <OfferListItemInfo offer={offer} />
+      <OfferListItemInfo totalArea={totalArea} totalRooms={totalRooms} baseRent={baseRent} />
     )
     expect(offerDetail).toMatchSnapshot()
-  })
-
-  it('should throw error if form is not renderable', () => {
-    expect(() => shallow(<OfferListItemInfo offer={notRenderableOffer} />)).toThrowErrorMatchingSnapshot()
   })
 })
