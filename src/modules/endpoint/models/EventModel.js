d1f99df414902267a304bbdab98c46302f1901ca
Steffen Kleinle <klinzo@yahoo.de>
2018-10-09 11:44:01 +0200

    WEBAPP-363: Make EventModel inherit from PageModel

--- src/modules/endpoint/models/EventModel.js
+++ src/modules/endpoint/models/EventModel.js
@@ -2,22 +2,28 @@
 
 import type Moment from 'moment'
 import LocationModel from './LocationModel'
-import PoiModel from './PoiModel'
 import DateModel from './DateModel'
+import PageModel from './PageModel'
 
-class EventModel extends PoiModel {
+class EventModel extends PageModel {
   _date: DateModel
+  _location: LocationModel
 
   constructor (params: {|id: number, path: string, title: string, content: string, thumbnail: string,
     date: DateModel, location: LocationModel, excerpt: string, availableLanguages: Map<string, string>,
     lastUpdate: Moment|}) {
-    const {date, ...other} = params
+    const {date, location, ...other} = params
     super(other)
     this._date = date
+    this._location = location
   }
 
   get date (): DateModel {
     return this._date
+  }
+
+  get location (): LocationModel {
+    return this._location
   }
 }
 
