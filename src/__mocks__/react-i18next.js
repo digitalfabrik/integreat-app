a917acd94b94e550fe8ea52c0c17543009d3f1ed
Max Ammann <maximilian.ammann@googlemail.com>
2018-11-07 21:40:24 +0100

    WEBAPP-393: Readd withI18n mock

--- src/__mocks__/react-i18next.js
+++ src/__mocks__/react-i18next.js
@@ -1,6 +1,7 @@
 // @flow
 
 import * as React from 'react'
+import i18next from 'i18next'
 
 // this mock makes sure any components using the translate HoC receive the t function as a prop
 export const translate = <Props> (namespace: string) => (Component: React.ComponentType<Props>) => {
@@ -10,3 +11,8 @@ export const translate = <Props> (namespace: string) => (Component: React.Compon
 }
 
 export const I18nextProvider = ({ children }: { children: React.Node }) => children
+
+export const withI18n = <P>() => {
+  const i18n = i18next.createInstance()
+  return (Component: React.ComponentType<P>) => (props: P) => <Component i18n={i18n} {...props} />
+}
