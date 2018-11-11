d11b8af8b090087f7674800cd413c02109413980
Michael Markl <marklmichael98@gmail.com>
2018-11-11 14:42:43 +0100

    WEBAPP-391: Use onStickyTopChanged instead of stickyAncestor

--- src/modules/common/components/ScrollingSearchBox.js
+++ src/modules/common/components/ScrollingSearchBox.js
@@ -13,7 +13,8 @@ type PropsType = {|
   onFilterTextChange: (string) => void,
   spaceSearch: boolean,
   children: Element<*>,
-  placeholderText: string
+  placeholderText: string,
+  onStickyTopChanged: (number) => void
 |}
 
 type StateType = {|
@@ -39,18 +40,19 @@ export class ScrollingSearchBox extends React.PureComponent<PropsType, StateType
   }
 
   render () {
-    const {children, filterText, placeholderText, spaceSearch} = this.props
+    const {children, filterText, placeholderText, spaceSearch, onStickyTopChanged, onFilterTextChange} = this.props
 
     return <div ref={this.setReference}>
       <Headroom pinStart={this._node ? this._node.offsetTop : 0}
                 scrollHeight={SEARCH_BAR_HEIGHT}
                 height={SEARCH_BAR_HEIGHT}
-                stickyAncestor={children}>
+                onStickyTopChanged={onStickyTopChanged}>
         <SearchInput filterText={filterText}
                      placeholderText={placeholderText}
-                     onFilterTextChange={this.props.onFilterTextChange}
+                     onFilterTextChange={onFilterTextChange}
                      spaceSearch={spaceSearch} />
       </Headroom>
+      {children}
     </div>
   }
 }
