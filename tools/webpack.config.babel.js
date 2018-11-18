a1995dd7aee1370e547ea6c566b8fd08f336ca17
Max Ammann <maximilian.ammann@googlemail.com>
2018-11-18 13:48:53 +0100

    Add webpack config

---
+++ tools/webpack.config.babel.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const babelConfig = require('../.babelrc.js')
+
+module.exports.default = () => ({
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'index-legacy.js',
+    library: 'integreatapiclient',
+    libraryTarget: 'umd'
+  },
+  optimization: {
+    minimizer: []
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: babelConfig
+        }
+      }
+    ]
+  }
+})
