855af26ad27069b7052472ca3b5391156c8c4517
Steffen Kleinle <klinzo@yahoo.de>
2018-11-30 13:55:00 +0100

    WEBAPP-401: Update snaps

--- src/createLocation.js
+++ src/createLocation.js
@@ -1,16 +1,16 @@
 // @flow
 
 import type { LocationState, Location } from 'redux-first-router'
-import { routesMap } from './modules/app/route-configs'
+import type { RoutesMap } from 'redux-first-router/dist/flow-types'
 
-const createLocation = ({pathname, payload, type, query, prev}: {|
-  pathname?: string, payload: Object, type: string, query?: Object, prev?: Location|}): LocationState => ({
+const createLocation = ({pathname, payload, type, query, prev, routesMap}: {|pathname?: string, payload: Object,
+  type: string, query?: Object, prev?: Location, routesMap?: RoutesMap|}): LocationState => ({
   pathname: pathname || '/random_pathname',
   type,
   payload,
   query: query || {},
   prev: prev || {pathname: '/random_prev_pathname', payload: {}, type: 'RANDOM_PREV_TYPE'},
-  routesMap,
+  routesMap: routesMap || {},
   kind: undefined,
   history: undefined
 })
