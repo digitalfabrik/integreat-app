bc2796e964d666499640f392eb436b37d088b221
Steffen Kleinle <klinzo@yahoo.de>
2018-12-02 10:50:08 +0100

    WEBAPP-401: Remove file extensions from type imports

--- src/modules/app/route-configs/SprungbrettRouteConfig.js
+++ src/modules/app/route-configs/SprungbrettRouteConfig.js
@@ -8,7 +8,7 @@ import {
   SprungbrettModel,
   sprungbrettEndpoint
 } from '@integreat-app/integreat-api-client'
-import type { Dispatch, GetState, Route } from 'redux-first-router/dist/flow-types.js.flow'
+import type { Dispatch, GetState, Route } from 'redux-first-router/dist/flow-types'
 import fetchData from '../fetchData'
 import type {
   AllPayloadsType,
