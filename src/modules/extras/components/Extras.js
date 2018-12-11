23a0355a18867bf1546f2ec034e8351611c1fefc
Nikolas Klug <klug.nikolas@googlemail.com>
2018-12-11 15:48:49 +0100

    NATIVE-60: Attach extras to navigator

--- src/modules/extras/components/Extras.js
+++ src/modules/extras/components/Extras.js
@@ -5,41 +5,41 @@ import TileModel from '../../common/models/TileModel'
 import { ExtraModel } from '@integreat-app/integreat-api-client'
 import Tiles from '../../common/components/Tiles'
 import type { TFunction } from 'react-i18next'
-import { getSprungbrettRoutePath, getWohnenRoutePath, SPRUNGBRETT_EXTRA, WOHNEN_EXTRA } from '../ExtrasConfig'
+import {
+  SPRUNGBRETT_EXTRA,
+  SPRUNGBRETT_ROUTE,
+  WOHNEN_EXTRA, WOHNEN_ROUTE
+} from '../ExtrasConfig'
 
 type PropsType = {|
-  city: string,
-  language: string,
   extras: Array<ExtraModel>,
-  extraId: ?string,
-  navigateToExtras: (path: string) => void,
+  navigateToExtras: (path: string, isExternalUrl: boolean) => void,
   t: TFunction
 |}
 
 export default class Extras extends React.Component<PropsType> {
   onTilePress = (tile: TileModel) => {
-    this.props.navigateToExtras(tile.path)
+    this.props.navigateToExtras(tile.path, tile.isExternalUrl)
   }
 
   toTileModels (extras: Array<ExtraModel>): Array<TileModel> {
-    const {city, language} = this.props
     return extras.map(
       extra => {
-        let path = extra.path
+        let route = extra.path
         if (extra.alias === SPRUNGBRETT_EXTRA) {
-          path = getSprungbrettRoutePath({city, language})
+          route = SPRUNGBRETT_ROUTE
         } else if (extra.alias === WOHNEN_EXTRA) {
-          path = getWohnenRoutePath({city, language})
+          route = WOHNEN_ROUTE
         }
 
         return new TileModel({
           id: extra.alias,
           title: extra.title,
-          // the url stored in the sprungbrett extra is the url of the endpoint
-          path: path,
+          // the
+          path: route,
           thumbnail: extra.thumbnail,
           // every extra except from the sprungbrett extra is just a link to an external site
-          isExternalUrl: path === extra.path,
+          isExternalUrl: route === extra.path,
           postData: extra.postData
         })
       }
