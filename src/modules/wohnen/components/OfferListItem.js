53fe971eef142527b125eedc83d3bf828810af79
Nikolas Klug <klug.nikolas@googlemail.com>
2018-12-11 14:20:23 +0100

    NATIVE-60: Adjust navigation in Wohnen extra

---
+++ src/modules/wohnen/components/OfferListItem.js
@@ -0,0 +1,47 @@
+// @flow
+
+import * as React from 'react'
+import { WohnenOfferModel, WohnenFormData } from '@integreat-app/integreat-api-client'
+import ListItem from '../../../modules/common/components/ListItem'
+import styled from 'styled-components'
+
+const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Price = styled.span`
+  align-self: flex-end;
+`
+
+type PropsType = {|
+  offer: WohnenOfferModel,
+  hashFunction: WohnenOfferModel => string,
+  navigateToOffer: () => void
+|}
+
+class OfferListItem extends React.PureComponent<PropsType> {
+  render () {
+    const {offer, navigateToOffer} = this.props
+
+    if (offer.formData instanceof WohnenFormData) {
+      const specificOffer: WohnenOfferModel = offer
+      const accommodation = specificOffer.formData.accommodation
+      const costs = specificOffer.formData.costs
+
+      return (
+        <ListItem title={accommodation.title} navigateTo={navigateToOffer}>
+          <Description>
+            <div>{accommodation.totalArea} m²</div>
+            <div>{accommodation.totalRooms} Zimmer</div>
+            <Price>{costs.baseRent} €</Price>
+          </Description>
+        </ListItem>
+      )
+    } else {
+      throw new Error(`Failed to render form ${JSON.stringify(offer.formData)} because it is not supported!`)
+    }
+  }
+}
+
+export default OfferListItem
