b5c8761506325514ab117bae59ab922f521ceff1
Maximilian Ammann <max@maxammann.org>
2018-12-16 20:42:28 +0100

    NATIVE-1: Rename FileCallback to FileDownloadCallback

--- android/app/src/main/java/com/integreat/fetcher/FileDownloadCallback.java
+++ android/app/src/main/java/com/integreat/fetcher/FileDownloadCallback.java
@@ -3,7 +3,7 @@ package com.integreat.fetcher;
 import java.io.File;
 import java.io.IOException;
 
-interface FileCallback {
+interface FileDownloadCallback {
     void failed(String url, IOException e);
 
     void downloaded(String url, File target);
