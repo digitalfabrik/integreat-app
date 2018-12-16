3080b1ea11c6803b29134ba09b44c28df6aba40f
Maximilian Ammann <max@maxammann.org>
2018-12-16 12:38:40 +0100

    NATIVE-1: Add progress

--- android/app/src/main/java/com/integreat/fetcher/DownloadResultCollector.java
+++ android/app/src/main/java/com/integreat/fetcher/DownloadResultCollector.java
@@ -1,13 +1,11 @@
 package com.integreat.fetcher;
 
-import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.Promise;
-import com.facebook.react.bridge.WritableArray;
-import com.facebook.react.bridge.WritableMap;
+import android.support.annotation.Nullable;
+import com.facebook.react.bridge.*;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,21 +15,25 @@ public class DownloadResultCollector implements FileCallback {
     private final Map<String, String> succeededUrls = new HashMap<>();
     private final Promise promise;
     private final List<String> expectedUrls;
+    private final ReactContext reactContext;
 
-    public DownloadResultCollector(Promise promise, List<String> expectedUrls) {
+    public DownloadResultCollector(ReactContext reactContext, List<String> expectedUrls, Promise promise) {
         this.promise = promise;
         this.expectedUrls = expectedUrls;
+        this.reactContext = reactContext;
     }
 
     @Override
     public void failed(String url, IOException e) {
         failedUrls.put(url, e.getMessage());
+        sendProgress();
         tryToResolve();
     }
 
     @Override
     public void downloaded(String url, File target) {
         succeededUrls.put(url, target.getAbsolutePath());
+        sendProgress();
         tryToResolve();
     }
 
@@ -58,5 +60,16 @@ public class DownloadResultCollector implements FileCallback {
         result.putMap("successFilePaths", succeeded);
 
         promise.resolve(result);
+    }
+
+    private void sendProgress() {
+        sendEvent("progress", (double) (failedUrls.size() + succeededUrls.size()) / expectedUrls.size());
+    }
+
+    private void sendEvent(String eventName,
+                           @Nullable Object params) {
+        reactContext
+                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
     }
 }
