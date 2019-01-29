80db34c9533ae8af9e8bdcab232638c55e8cda60
Maximilian Ammann <max@maxammann.org>
2019-01-29 14:49:11 +0100

    NATIVE-63: Refactor state to be able to contains a subtree

--- src/modules/app/StateType.js
+++ src/modules/app/StateType.js
@@ -2,18 +2,45 @@
 
 import type { StoreActionType } from './StoreActionType'
 import type { PersistState } from 'redux-persist/src/types'
+import { CategoryModel, CityModel } from '@integreat-app/integreat-api-client'
 
 export type FilesStateType = { [url: string]: string }
 
+type PathType = string
 export type CategoriesStateType = {
+  +models: { [path: PathType]: CategoryModel },
+  // +root: ?PathType,
+  +children: { [path: PathType]: Array<PathType> },
+
+  +routeMapping: { [key: string]: PathType },
+
   +lastUpdated: ?string,
   +error: ?string
 }
 
+export const defaultCategoriesState: CategoriesStateType = {
+  models: {},
+  children: {},
+
+  routeMapping: {},
+
+  error: undefined,
+  lastUpdated: undefined
+}
+
 export type CitiesStateType = {|
+  +models: Array<CityModel>,
+
   +lastUpdated: ?string,
   +error: ?string
 |}
+
+export const defaultCitiesState: CitiesStateType = {
+  models: [],
+
+  error: undefined,
+  lastUpdated: undefined
+}
 
 export type DirectionStateType = 'ltr' | 'rtl'
 
