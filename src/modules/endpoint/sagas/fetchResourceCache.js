2b33c5dfc5d74d9ff284fd91c6931dd47b6a7654
Maximilian Ammann <max@maxammann.org>
2019-01-29 14:55:31 +0100

    NATIVE-63: Disable timing code

--- src/modules/endpoint/sagas/fetchResourceCache.js
+++ src/modules/endpoint/sagas/fetchResourceCache.js
@@ -12,29 +12,31 @@ import type { FetchResultType } from '../../fetcher/FetcherModule'
 import type { ResourceCacheType } from '../ResourceCacheType'
 import performanceNow from '../../app/performanceNow'
 
-export default function * fetchResourceCache (city: string, urls: ResourceCacheType): Saga<void> {
+export default function * fetchResourceCache (city: string, language: string, urls: ResourceCacheType): Saga<void> {
   try {
-    const start = performanceNow()
+    // const start = performanceNow()
     const result: FetchResultType = yield call(FetcherModule.downloadAsync, urls, p => console.log(p))
-    const end = performanceNow()
-    console.warn(`download resources: ${end - start}ms`)
+    // const end = performanceNow()
+    // console.warn(`download resources: ${end - start}ms`)
 
     if (!isEmpty(result.failureMessages)) {
       const message = reduce(result.failureMessages, (message, error, url) => {
         return `${message}'${url}': ${error}\n`
       }, '')
 
-      const failed: ResourcesDownloadFailedActionType = {type: `RESOURCES_DOWNLOAD_FAILED`, city, message}
+      const failed: ResourcesDownloadFailedActionType = {type: `RESOURCES_DOWNLOAD_FAILED`, city, language, message}
       yield put(failed)
       return
     }
 
     const success: ResourcesDownloadSucceededActionType = {
-      type: 'RESOURCES_DOWNLOAD_SUCCEEDED', city, files: result.successFilePaths
+      type: 'RESOURCES_DOWNLOAD_SUCCEEDED', city, language
     }
     yield put(success)
   } catch (e) {
-    const failed: ResourcesDownloadFailedActionType = {type: `RESOURCES_DOWNLOAD_FAILED`, city, message: e.message}
+    const failed: ResourcesDownloadFailedActionType = {
+      type: `RESOURCES_DOWNLOAD_FAILED`, city, language, message: e.message
+    }
     yield put(failed)
     throw e
   }
