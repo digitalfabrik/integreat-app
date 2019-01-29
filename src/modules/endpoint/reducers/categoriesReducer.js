ea620ef9513376f7ddf3eab662efc5b3f456507b
Maximilian Ammann <max@maxammann.org>
2019-01-29 14:52:17 +0100

    NATIVE-63: Change categories reducer to build new state structure from CategoriesMapModel

--- src/modules/endpoint/reducers/categoriesReducer.js
+++ src/modules/endpoint/reducers/categoriesReducer.js
@@ -1,19 +1,59 @@
 // @flow
 
-import type {
-  CategoriesFetchActionType
-} from '../../app/StoreActionType'
+import type { CategoriesFetchActionType } from '../../app/StoreActionType'
 import type { CategoriesStateType } from '../../app/StateType'
+import { defaultCategoriesState } from '../../app/StateType'
+import { CategoryModel } from '@integreat-app/integreat-api-client'
+import { times } from 'lodash/util'
+import { keyBy } from 'lodash/collection'
 
-export default (state: CategoriesStateType = {
-  lastUpdated: undefined,
-  error: undefined
-}, action: CategoriesFetchActionType): any => {
+export default (
+  state: CategoriesStateType = defaultCategoriesState, action: CategoriesFetchActionType
+): CategoriesStateType => {
   switch (action.type) {
     case 'FETCH_CATEGORIES_REQUEST':
-      return {}
+      return state
     case 'CATEGORIES_FETCH_SUCCEEDED':
-      return {...state, lastUpdated: new Date().toISOString(), error: undefined}
+      const {categoriesMap, path, depth, key} = action.payload
+
+      if (!depth) {
+        throw new Error('You need to specify a depth!')
+      }
+
+      if (!key) {
+        throw new Error('You need to specify a key!')
+      }
+
+      const root: CategoryModel = categoriesMap.findCategoryByPath(path)
+      const models = {}
+      const children = {}
+
+      models[root.path] = root
+
+      let childrenStack = [root]
+      times(depth, () => {
+        const newChildrenStack = []
+        childrenStack.forEach(category => {
+          models[category.path] = category
+
+          const childrenCategories = categoriesMap.getChildren(category)
+          children[category.path] = childrenCategories.map(child => child.path)
+          Object.assign(models, keyBy(childrenCategories, child => child.path))
+
+          newChildrenStack.push(...childrenCategories)
+        })
+
+        childrenStack = newChildrenStack
+      })
+
+      return {
+        models,
+        children,
+        root: root.path,
+        routeMapping: {...state.routeMapping, [key]: root.path},
+        lastUpdated: new Date().toISOString(),
+        error: undefined
+      }
     case 'CATEGORIES_FETCH_FAILED':
       return {...state, lastUpdated: undefined, error: action.message}
     default:
--- src/modules/endpoint/reducers/cititesReducer.js
+++ src/modules/endpoint/reducers/cititesReducer.js
@@ -2,16 +2,14 @@
 
 import type { CitiesFetchActionType } from '../../app/StoreActionType'
 import type { CitiesStateType } from '../../app/StateType'
+import { defaultCitiesState } from '../../app/StateType'
 
-export default (state: CitiesStateType = {
-  lastUpdated: undefined,
-  error: undefined
-}, action: CitiesFetchActionType): any => {
+export default (state: CitiesStateType = defaultCitiesState, action: CitiesFetchActionType): CitiesStateType => {
   switch (action.type) {
     case 'FETCH_CITIES_REQUEST':
-      return {}
+      return {models: [], lastUpdated: undefined, error: undefined}
     case 'CITIES_FETCH_SUCCEEDED':
-      return {...state, lastUpdated: new Date().toISOString(), error: undefined}
+      return {models: action.payload.cities, lastUpdated: new Date().toISOString(), error: undefined}
     case 'CITIES_FETCH_FAILED':
       return {...state, lastUpdated: undefined, error: action.message}
     default:
