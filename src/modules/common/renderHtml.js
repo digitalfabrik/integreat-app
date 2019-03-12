8c12996fe72b1477297f457f56482de3bc9e95b6
Nikolas Klug <klug.nikolas@googlemail.com>
2019-03-12 15:51:03 +0100

    NATIVE-32: Use AutoHeightWebview

--- src/modules/common/renderHtml.js
+++ src/modules/common/renderHtml.js
@@ -41,6 +41,8 @@ export default (html: string, files: FilesStateType) => {
   // language=HTML
   return `
 <html>
+<!-- disables zooming https://stackoverflow.com/questions/44625680/disable-zoom-on-web-view-react-native-->
+<meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
 <body style="display: none;">
 ${html}
 <script>${renderJS(files)}</script>
