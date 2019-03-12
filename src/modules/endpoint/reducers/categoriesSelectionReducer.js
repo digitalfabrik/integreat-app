23bdd0e4a62a914fe2e4c192e252b9f094ae911d
Maximilian Ammann <max@maxammann.org>
2019-03-12 13:01:00 +0100

    NATIVE-63: Fix rename reducer when there are inconsistencies between languages

--- src/modules/endpoint/reducers/categoriesSelectionReducer.js
+++ src/modules/endpoint/reducers/categoriesSelectionReducer.js
@@ -69,7 +69,9 @@ const switchLanguage = (
   state: CategoriesSelectionStateType, action: SwitchCategorySelectionLanguageActionType
 ) => {
   const {categoriesMap, newLanguage} = action.params
-  const {routeMapping} = state
+  const {routeMapping, currentCity} = state
+
+  console.dir(routeMapping)
 
   const translatedRouteMapping = mapValues(routeMapping, (value: RouteStateType, key: string) => {
     const {models, children, depth, root} = value
@@ -80,7 +82,13 @@ const switchLanguage = (
       return defaultRouteState
     }
 
-    const translatedRoot = models[root].availableLanguages.get(newLanguage)
+    let translatedRoot
+
+    if (models[root].id === 0) {
+      translatedRoot = `/${currentCity}/${newLanguage}`
+    } else {
+      translatedRoot = models[root].availableLanguages.get(newLanguage)
+    }
 
     if (!translatedRoot) {
       console.error(`Route ${key} is not translatable!`)
@@ -88,40 +96,77 @@ const switchLanguage = (
     }
 
     const translatedChildren = reduce(children, (result, value: Array<string>, path: string) => {
-      const translatedKey = models[path].availableLanguages.get(newLanguage)
+      let translatedKey
 
-      if (!translatedKey) {
+      if (models[path].id === 0) {
+        translatedKey = `/${currentCity}/${newLanguage}`
+      } else {
+        translatedKey = models[path].availableLanguages.get(newLanguage)
+
+        if (!translatedKey) {
+          return result
+        }
+      }
+
+      if (!categoriesMap.findCategoryByPath(translatedKey)) {
+        // child in translation not available
         return result
       }
 
       const translatedArray = children[path].map(key => {
         const translatedKey = models[key].availableLanguages.get(newLanguage)
         if (!translatedKey) {
+          // child in translation not available
           return null
         }
+
+        if (!categoriesMap.findCategoryByPath(translatedKey)) {
+          // child in translation not available
+          return null
+        }
+
         return translatedKey
-      })
+      }).filter(model => !!model)
 
       result[translatedKey] = translatedArray
       return result
     }, {})
 
     const translatedModels = reduce(models, (result, value: CategoryModel) => {
-      const translatedKey = value.availableLanguages.get(newLanguage)
-      if (!translatedKey) {
+      let translatedKey
+
+      if (value.id === 0) {
+        translatedKey = `/${currentCity}/${newLanguage}`
+      } else {
+        translatedKey = value.availableLanguages.get(newLanguage)
+        if (!translatedKey) {
+          return result
+        }
+      }
+
+      const category = categoriesMap.findCategoryByPath(translatedKey)
+
+      if (!category) {
         return result
       }
-      result[translatedKey] = categoriesMap.findCategoryByPath(translatedKey)
+
+      result[translatedKey] = category
       return result
     }, {})
 
-    return {
+    console.log('route')
+    const newVar = {
       root: translatedRoot,
       models: translatedModels,
       children: translatedChildren,
       depth: depth
     }
-  }, {})
+    console.dir(newVar)
+    return newVar
+  })
+
+  console.log(translatedRouteMapping)
+  console.dir(translatedRouteMapping)
 
   return {
     ...state,
