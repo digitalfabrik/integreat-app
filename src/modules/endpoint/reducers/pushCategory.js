387c73ce687d6ba7201d6dc017b795d9c576086b
Maximilian Ammann <max@maxammann.org>
2019-03-19 18:22:26 +0100

    NATIVE-104: Change forEachTreeNode to iterate depth+1 for models

--- src/modules/endpoint/reducers/pushCategory.js
+++ src/modules/endpoint/reducers/pushCategory.js
@@ -3,17 +3,28 @@
 import type { CategoriesStateType } from '../../app/StateType'
 import type { PushCategoryActionType } from '../../app/StoreActionType'
 import { CategoryModel } from '@integreat-app/integreat-api-client'
-import { keyBy } from 'lodash/collection'
 
+/**
+ * Iterates through a tree until depth is reached. A depth of 0 means only the root is visited.
+ * A depth of 1 means the root and the children of it are visited. Please note that for each children of the root
+ * nodeAction is called once with the category and null for the children as parameters.
+ *
+ * @param root The root to start iterating from
+ * @param resolveChildren The function which is used to resolve children
+ * @param depth The depth
+ * @param nodeAction The action to trigger for each node and children
+ */
 const forEachTreeNode = (
   root: CategoryModel,
   resolveChildren: CategoryModel => Array<CategoryModel>,
   depth: number,
-  nodeAction: (CategoryModel, Array<CategoryModel>) => void
+  nodeAction: (CategoryModel, ?Array<CategoryModel>) => void
 ) => {
-  const children = resolveChildren(root)
-  nodeAction(root, children)
-  if (depth > 0) {
+  if (depth === 0) {
+    nodeAction(root, null)
+  } else {
+    const children = resolveChildren(root)
+    nodeAction(root, children)
     children.forEach(child => forEachTreeNode(child, resolveChildren, depth - 1, nodeAction))
   }
 }
@@ -37,8 +48,9 @@ const pushCategory = (
 
   forEachTreeNode(root, node => categoriesMap.getChildren(node), depth, (node, children) => {
     resultModels[node.path] = node
-    Object.assign(resultModels, keyBy(children, child => child.path))
-    resultChildren[node.path] = children.map(child => child.path)
+    if (children) {
+      resultChildren[node.path] = children.map(child => child.path)
+    }
   })
 
   return {
