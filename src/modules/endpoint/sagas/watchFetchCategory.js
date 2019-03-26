4adbd5e9ebef01c90e5620bb014e277a3aead92c
Maximilian Ammann <max@maxammann.org>
2019-03-26 13:01:27 +0100

    NATIVE-106: Fix language changing for categories

--- src/modules/endpoint/sagas/watchFetchCategory.js
+++ src/modules/endpoint/sagas/watchFetchCategory.js
@@ -1,19 +1,42 @@
 // @flow
 
 import type { Saga } from 'redux-saga'
-import { call, put, takeLatest } from 'redux-saga/effects'
+import { call, put, select, takeLatest } from 'redux-saga/effects'
 import type {
   FetchCategoryActionType,
   FetchCategoryFailedActionType,
-  PushCategoryActionType
+  PushCategoryActionType, SwitchCityContentLanguageActionType
 } from '../../app/StoreActionType'
 import MemoryDatabase from '../MemoryDatabase'
 import loadCityContent from './loadCityContent'
+import type { StateType } from '../../app/StateType'
 
 function * fetchCategory (database: MemoryDatabase, action: FetchCategoryActionType): Saga<void> {
   const {city, language, path, depth, key} = action.params
   try {
     yield call(loadCityContent, database, city, language)
+
+    const currentLanguage = yield select((state: StateType) => state.cityContent.language)
+
+    if (path === undefined || depth === undefined) {
+      // you did not provide a new key and depth it is most likely a language change
+
+      if (currentLanguage === language) {
+        // If you request the same language as we already have just return
+        return
+      }
+
+      const insert: SwitchCityContentLanguageActionType = {
+        type: `SWITCH_CITY_CONTENT_LANGUAGE`,
+        params: {
+          newCategoriesMap: database.categoriesMap,
+          newLanguage: language
+        }
+      }
+      yield put(insert)
+
+      return
+    }
 
     const insert: PushCategoryActionType = {
       type: `PUSH_CATEGORY`,
