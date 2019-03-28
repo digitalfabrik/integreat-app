410d720aa9b3e7a2ddb9c7b14538f8d6cfb2b68e
Maximilian Ammann <max@maxammann.org>
2019-03-28 12:08:46 +0100

    NATIVE-139: Create an abstraction for searching urls in categories and events

---
+++ src/modules/endpoint/ResourceURLFinder.js
@@ -0,0 +1,62 @@
+// @flow
+
+import getExtension from './getExtension'
+import { Parser } from 'htmlparser2'
+import type { FetchMapType } from './sagas/fetchResourceCache'
+import { keyBy, reduce } from 'lodash/collection'
+
+export default class ResourceURLFinder {
+  parser: Parser
+  onAttributeTagFound: (name: string, value: string) => void
+  _foundUrls: Set<string> = new Set<string>()
+
+  init () {
+    this.onAttributeTagFound = (name: string, value: string) => {
+      if (name === 'href' || name === 'src') {
+        if (['png', 'jpg', 'jpeg', 'pdf'].includes(getExtension(value))) {
+          this.foundUrls.add(value)
+        }
+      }
+    }
+
+    this.parser = new Parser({onattribute: this.onAttributeTagFound}, {decodeEntities: true})
+  }
+
+  finalize () {
+    this.parser.end()
+  }
+
+  findResourceUrls = (html: string): Set<string> => {
+    this._foundUrls.clear()
+    this.parser.write(html)
+    return this.foundUrls
+  }
+
+  get foundUrls (): Set<string> {
+    return this._foundUrls
+  }
+
+  buildFetchMap (
+    inputs: Array<{ path: string, content: string, thumbnail: string }>,
+    buildFilePath: (url: string, path: string) => string
+  ): FetchMapType {
+    return reduce(inputs, (result, input: { path: string, content: string, thumbnail: string }) => {
+      const path = input.path
+
+      this.findResourceUrls(input.content)
+
+      const urlSet = this.foundUrls
+      if (input.thumbnail) {
+        urlSet.add(input.thumbnail)
+      }
+
+      return {
+        ...result,
+        ...keyBy(
+          Array.from(urlSet).map(url => [url, path]),
+          ([url, path]) => buildFilePath(url, path)
+        )
+      }
+    }, {})
+  }
+}
