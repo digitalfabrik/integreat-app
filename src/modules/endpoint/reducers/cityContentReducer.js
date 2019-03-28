51a2dd2c9a650235d0332f1b9ff1a02f82be84ac
Maximilian Ammann <max@maxammann.org>
2019-03-28 14:28:13 +0100

    NATIVE-140: Rename SWITCH_CONTENT_LANGUAGE to MORPH_CONTENT_LANGUAGE and add new SWITCH_CONTENT_LANGUAGE

--- src/modules/endpoint/reducers/cityContentReducer.js
+++ src/modules/endpoint/reducers/cityContentReducer.js
@@ -1,7 +1,7 @@
 // @flow
 
 import { defaultCityContentState } from '../../app/StateType'
-import switchContentLanguage from './switchContentLanguage'
+import morphContentLanguage from './morphContentLanguage'
 import pushCategory from './pushCategory'
 import type { CityContentStateType } from '../../app/StateType'
 import pushEvent from './pushEvent'
@@ -15,8 +15,8 @@ export default (
       return pushCategory(state, action)
     case 'PUSH_EVENT':
       return pushEvent(state, action)
-    case 'SWITCH_CONTENT_LANGUAGE': // todo:  Not working
-      return switchContentLanguage(state, action)
+    case 'MORPH_CONTENT_LANGUAGE':
+      return morphContentLanguage(state, action)
     case 'CLEAR_CATEGORY':
       const {key} = action.params
       delete state.categoriesRouteMapping[key]
