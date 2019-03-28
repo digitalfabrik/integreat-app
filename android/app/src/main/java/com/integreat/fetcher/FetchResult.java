9306dba11c49bd547a16583eb0237a87cde68fa2
Maximilian Ammann <max@maxammann.org>
2019-03-28 12:08:06 +0100

    NATIVE-139: Change return type of a fetch. Also fix the collection of errors when there are duplicate urls

--- android/app/src/main/java/com/integreat/fetcher/FetchResult.java
+++ android/app/src/main/java/com/integreat/fetcher/FetchResult.java
@@ -2,20 +2,30 @@ package com.integreat.fetcher;
 
 import java.time.ZonedDateTime;
 
+import javax.annotation.Nullable;
+
 public class FetchResult {
     private final String url;
     private final ZonedDateTime lastUpdate;
+    private final String errorMessage;
 
-    public FetchResult(String url, ZonedDateTime lastUpdate) {
+    public FetchResult(String url, ZonedDateTime lastUpdate, String errorMessage) {
         this.url = url;
         this.lastUpdate = lastUpdate;
+        this.errorMessage = errorMessage;
     }
 
     public String getUrl() {
         return url;
     }
 
+    @Nullable
     public ZonedDateTime getLastUpdate() {
         return lastUpdate;
+    }
+
+    @Nullable
+    public String getErrorMessage() {
+        return errorMessage;
     }
 }
