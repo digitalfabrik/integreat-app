d55be5bfd264e41a4ae168e39c24d8bc6bec5f7f
Andrew <kud@integreat-app.de>
2019-04-13 15:34:20 +0300

    Use pathname while sharing link

--- src/modules/layout/containers/HeaderContainer.js
+++ src/modules/layout/containers/HeaderContainer.js
@@ -1,7 +1,9 @@
 // @flow
-
 import Header from '../components/Header'
+import { connect } from 'react-redux'
 import { withTheme } from 'styled-components'
 
-// $FlowFixMe
-export default withTheme(Header)
+const mapStateToProps = ({ categories }: CategoriesStateType) => categories
+
+export default connect(mapStateToProps, null)(withTheme(Header))
+
