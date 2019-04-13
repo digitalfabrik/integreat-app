cb1f8cff91dc93e4e6709ba37ab803df60cb5193
Andrew <kud@integreat-app.de>
2019-04-13 15:49:48 +0300

    Add correct type

--- src/modules/layout/containers/HeaderContainer.js
+++ src/modules/layout/containers/HeaderContainer.js
@@ -2,7 +2,8 @@
 import Header from '../components/Header'
 import { connect } from 'react-redux'
 import { withTheme } from 'styled-components'
+import type { CategoryRouteStateType } from '../app/StateType'
 
-const mapStateToProps = ({ categories }: CategoriesStateType) => categories
+const mapStateToProps = ({ categories }: CategoryRouteStateType) => categories
 
 export default connect(mapStateToProps)(withTheme(Header))
