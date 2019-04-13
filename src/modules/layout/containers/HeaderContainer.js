671b04fc44a9351657372c27e74c5de8d21c5f22
Andrew <kud@integreat-app.de>
2019-04-13 15:37:36 +0300

    cleanup

--- src/modules/layout/containers/HeaderContainer.js
+++ src/modules/layout/containers/HeaderContainer.js
@@ -6,4 +6,3 @@ import { withTheme } from 'styled-components'
 const mapStateToProps = ({ categories }: CategoriesStateType) => categories
 
 export default connect(mapStateToProps)(withTheme(Header))
-
