31c5667be0a7e562f44fb4f31704821cd12cd969
Maximilian Ammann <max@maxammann.org>
2019-04-16 11:43:40 +0200

    NATIVE-156: Add comment for process.env usage

--- src/modules/e2e/testID.js
+++ src/modules/e2e/testID.js
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native'
 
 export default id => {
-  if (process.env.E2E_TAGGING) {
+  if (process.env.E2E_TAGGING) { // This is replaced by transform-inline-environment-variables during bundling
     return Platform.OS === 'android' ? {accessible: true, accessibilityLabel: id} : {testID: id}
   }
 
