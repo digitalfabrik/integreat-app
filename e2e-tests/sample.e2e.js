f95d488c64270c041c3c368240c14b0681be690a
Maximilian Ammann <max@maxammann.org>
2019-04-16 15:56:09 +0200

    NATIVE-156: Wait for element

--- e2e-tests/sample.e2e.js
+++ e2e-tests/sample.e2e.js
@@ -8,6 +8,7 @@ describe('Sample tests', () => {
   })
 
   it('should be able to see search bar', async () => {
+    await driver.waitForElementByAccessibilityId('Search-Input')
     expect(await driver.hasElementByAccessibilityId('Search-Input')).toBe(true)
     const element = await driver.elementByAccessibilityId('Search-Input')
     await driver.clickElement(element)
