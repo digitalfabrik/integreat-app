be957d54b645f5c30e2f4b5da75bbeeca7113d5c
Maximilian Ammann <max@maxammann.org>
2019-04-17 11:48:55 +0200

    NATIVE-83: Detect language on android and ios

--- src/modules/i18n/LanguageDetector.js
+++ src/modules/i18n/LanguageDetector.js
@@ -1,7 +1,18 @@
+import { NativeModules, Platform } from 'react-native'
+
 export default {
   type: 'languageDetector',
-  async: false, // flags below detection to be async
-  detect: callback => { return 'en' },
+  async: false,
+  detect: () => {
+    let systemLanguage = 'en'
+    if (Platform.OS === 'android') {
+      systemLanguage = NativeModules.I18nManager.localeIdentifier
+    } else {
+      systemLanguage = NativeModules.SettingsManager.settings.AppleLocale
+    }
+
+    return systemLanguage.substring(0, 2)
+  },
   init: () => {},
   cacheUserLanguage: () => {}
 }
