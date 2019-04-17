11e9216290fd3d8e2d7f5af9e9464eff5fa04df3
Maximilian Ammann <max@maxammann.org>
2019-04-17 11:48:37 +0200

    NATIVE-83: Listen for language changes on android and reload the bundle

--- android/app/src/main/java/com/integreat/MainActivity.java
+++ android/app/src/main/java/com/integreat/MainActivity.java
@@ -1,8 +1,19 @@
 package com.integreat;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactInstanceManager;
 
 public class MainActivity extends ReactActivity {
+
+    static String currentLocale;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MainActivity.currentLocale = getResources().getConfiguration().locale.toString();
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -11,5 +22,17 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "Integreat";
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        String locale = newConfig.locale.toString();
+        if (!MainActivity.currentLocale.equals(locale)) {
+            MainActivity.currentLocale = locale;
+            final ReactInstanceManager instanceManager = getReactInstanceManager();
+            instanceManager.recreateReactContextInBackground();
+        }
     }
 }
