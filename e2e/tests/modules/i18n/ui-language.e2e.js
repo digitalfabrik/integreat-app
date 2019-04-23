0f580019db2c83b72f82e6285f3fc611c98cb0fe
Maximilian Ammann <max@maxammann.org>
2019-04-23 13:40:14 +0200

    NATIVE-83: Refactor e2e directory and use page object pattern

---
+++ e2e/tests/modules/i18n/ui-language.e2e.js
@@ -0,0 +1,32 @@
+import { select, setupDriver, stopDriver } from '../../../driver/driver'
+import LandingPage from '../../../pages/routes/landing/LandingPage'
+
+describe('UI language', () => {
+  it('should match the system language de', async () => {
+    const driver = await setupDriver(select({
+      android: {language: 'de', locale: 'DE'},
+      ios: {language: 'de', locale: 'de_DE'}
+    }))
+
+    const landingPage = new LandingPage(driver)
+    await landingPage.ready()
+
+    expect(await landingPage.getSearchInput().text()).toBe('Search for your city')
+
+    await stopDriver(driver)
+  })
+
+  it('should match the system language en', async () => {
+    const driver = await setupDriver(select({
+      android: {language: 'en', locale: 'US'},
+      ios: {language: 'en', locale: 'en_US'}
+    }))
+
+    const landingPage = new LandingPage(driver)
+    await landingPage.ready()
+
+    expect(await landingPage.getSearchInput().text()).toBe('Search for your city')
+
+    await stopDriver(driver)
+  })
+})
