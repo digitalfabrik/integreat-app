d19d6dbb39b66f9f6e6a4a88c2efbd24cfe49431
Maximilian Ammann <max@maxammann.org>
2019-04-23 13:52:21 +0200

    NATIVE-83: Fix tests and change promise syntax

--- e2e/pages/routes/landing/LandingPage.js
+++ e2e/pages/routes/landing/LandingPage.js
@@ -8,11 +8,11 @@ export default class LandingPage {
     this.driver = driver
   }
 
-  async ready () {
-    await this.driver.waitForElementByAccessibilityId('Search-Input')
+  ready (): Promise<void> {
+    return this.driver.waitForElementByAccessibilityId('Search-Input')
   }
 
-  async getSearchInput (): wd.Element {
+  getSearchInput (): Promise<wd.Element> {
     return this.driver.elementByAccessibilityId('Search-Input')
   }
 }
