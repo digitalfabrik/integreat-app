a355afe5d585a0bc294ee4719918b46dcd54c5fd
Maximilian Ammann <max@maxammann.org>
2019-04-27 23:15:05 +0200

    NATIVE-66: Add temporary test for iOS FetcherModule

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { AppRegistry, YellowBox, NativeModules, NativeEventEmitter } from 'react-native'
 import App from './src/modules/app/components/App'
-import 'moment/locale/de' // fixme
+import 'moment/locale/de'
+import { RESOURCE_CACHE_DIR_PATH, URL_PREFIX } from './src/modules/platform/constants/webview' // fixme
 
 // @see: https://github.com/facebook/react-native/issues/9599
 if (typeof global.self === 'undefined') {
@@ -17,6 +18,6 @@ FetcherModuleEmitter.addListener(
   'progress',
   res => console.warn('fetchAsync event', res)
 )
-NativeModules.FetcherModule.fetchAsync()
+NativeModules.FetcherModule.fetchAsync({[`${URL_PREFIX + RESOURCE_CACHE_DIR_PATH}/hello.png`]: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'})
   .then(res => console.warn(JSON.stringify(res)))
   .catch(e => console.error(e.message, e.code))
