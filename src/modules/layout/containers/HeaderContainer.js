f4a6e2ecd0df84da2a6fead493162c235190cd97
Andrew Kud <a-kud@users.noreply.github.com>
2019-04-28 22:42:19 +0200

    fix lint

--- src/modules/layout/containers/HeaderContainer.js
+++ src/modules/layout/containers/HeaderContainer.js
@@ -2,7 +2,6 @@
 import Header from '../components/Header'
 import { connect } from 'react-redux'
 import { withTheme } from 'styled-components'
-import type { CategoryRouteStateType } from '../../app/StateType'
 import { CategoryModel } from '@integreat-app/integreat-api-client'
 
 const mapStateToProps = ({ categories }: CategoryModel) => categories
