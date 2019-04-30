b189d3c9b52b0e561b3bc3f02e81b2c1517626c2
Maximilian Ammann <max@maxammann.org>
2019-04-30 14:40:19 +0200

    NATIVE-66: Remove test code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import { AppRegistry, YellowBox, NativeModules, NativeEventEmitter } from 'react-native'
+import { AppRegistry, YellowBox } from 'react-native'
 import App from './src/modules/app/components/App'
-import 'moment/locale/de'
-import { RESOURCE_CACHE_DIR_PATH, URL_PREFIX } from './src/modules/platform/constants/webview' // fixme
+import 'moment/locale/de' // fixme
 
 // @see: https://github.com/facebook/react-native/issues/9599
 if (typeof global.self === 'undefined') {
@@ -12,12 +11,3 @@ if (typeof global.self === 'undefined') {
 YellowBox.ignoreWarnings(['Require cycle:'])
 
 AppRegistry.registerComponent('Integreat', () => App)
-const FetcherModuleEmitter = new NativeEventEmitter(NativeModules.FetcherModule)
-// subscribe to event
-FetcherModuleEmitter.addListener(
-  'progress',
-  res => console.warn('fetchAsync event', res)
-)
-NativeModules.FetcherModule.fetchAsync({[`${URL_PREFIX + RESOURCE_CACHE_DIR_PATH}/hello.png`]: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'})
-  .then(res => console.warn(JSON.stringify(res)))
-  .catch(e => console.error(e.message, e.code))
