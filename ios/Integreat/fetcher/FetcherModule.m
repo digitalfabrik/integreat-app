07dcaf7faa799f4b2f8cd66c1e90099a9c7fc273
Maximilian Ammann <max@maxammann.org>
2019-04-30 14:41:02 +0200

    NATIVE-66: Remap module to Fetcher

--- ios/Integreat/fetcher/FetcherModule.m
+++ ios/Integreat/fetcher/FetcherModule.m
@@ -1,7 +1,7 @@
 #import "React/RCTBridgeModule.h"
 #import "React/RCTEventEmitter.h"
 
-@interface RCT_EXTERN_MODULE(FetcherModule, RCTEventEmitter)
+@interface RCT_EXTERN_REMAP_MODULE(Fetcher, FetcherModule, RCTEventEmitter)
 RCT_EXTERN_METHOD(
                   fetchAsync: (NSDictionary)fetchMap
                   resolver: (RCTPromiseResolveBlock)resolve
