57f0655120f21677ff8e95e0fdbffcfc354e7fdb
Steffen Kleinle <klinzo@yahoo.de>
2019-05-02 17:39:50 +0200

    Use selectors to get availableLanguages for route

--- src/modules/layout/containers/HeaderContainer.js
+++ src/modules/layout/containers/HeaderContainer.js
@@ -6,19 +6,11 @@ import type { StateType } from '../../app/StateType'
 import { connect } from 'react-redux'
 import React from 'react'
 import { withNavigation } from 'react-navigation'
+import { availableLanguagesSelector } from '../../common/selectors/availableLanguagesSelector'
 
-const mapStateToProps = (state: StateType, ownProps) => {
-  const key: string = ownProps.navigation.getParam('key')
-  // todo add availableLanguages for other routes
-  const targetRoute = state.cityContent.categoriesRouteMapping[key]
-  const model = targetRoute && targetRoute.root && targetRoute.models[targetRoute.root]
-  const cityLanguages = state.cityContent.languages && state.cityContent.languages.map(languageModel => languageModel.code)
-  const availableLanguages = model && !model.isRoot() ? Array.from(model.availableLanguages.keys()) : cityLanguages
-
-  return {
-    availableLanguages
-  }
-}
+const mapStateToProps = (state: StateType, ownProps) => ({
+  availableLanguages: availableLanguagesSelector(state, ownProps)
+})
 
 const themed = withTheme(props => <Header {...props} />)
 export default withNavigation(connect(mapStateToProps)(themed))
