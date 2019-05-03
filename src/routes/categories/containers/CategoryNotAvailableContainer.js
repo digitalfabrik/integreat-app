dff4b97a2884a1c532344a0e4246d27a40a803cf
Michael Markl <marklmichael98@gmail.com>
2019-05-03 23:16:36 +0200

    NATIVE-107: Fix flow

--- src/routes/categories/containers/CategoryNotAvailableContainer.js
+++ src/routes/categories/containers/CategoryNotAvailableContainer.js
@@ -2,18 +2,22 @@
 
 import { connect } from 'react-redux'
 import type { Dispatch } from 'redux'
-import type { CategoriesRouteMappingType, StateType } from '../../../modules/app/StateType'
+import type { CategoryRouteStateType, StateType } from '../../../modules/app/StateType'
 import type { StoreActionType } from '../../../modules/app/StoreActionType'
 import CategoryNotAvailable from '../components/CategoryNotAvailable'
 import { withTheme } from 'styled-components/native'
 
 const mapStateToProps = (state: StateType, ownProps) => {
   const key: string = ownProps.navigation.getParam('key')
-  const route: CategoriesRouteMappingType = state.cityContent.categoriesRouteMapping[key]
+  const route: CategoryRouteStateType = state.cityContent.categoriesRouteMapping[key]
+  const languages = state.cityContent.languages
+  if (!languages) {
+    throw new Error('languages have not been set.')
+  }
 
   return {
     city: state.cityContent.city,
-    languages: state.cityContent.languages.filter(language => route.allAvailableLanguages.has(language.code))
+    languages: languages.filter(language => route.allAvailableLanguages.has(language.code))
   }
 }
 
