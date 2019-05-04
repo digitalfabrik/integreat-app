42ad10be0354cff2ce20d02e4365f61d8d3f1d43
Michael Markl <marklmichael98@gmail.com>
2019-05-04 17:35:55 +0200

    NATIVE-107: Remove unnecessary checks

--- src/modules/endpoint/reducers/morphContentLanguage.js
+++ src/modules/endpoint/reducers/morphContentLanguage.js
@@ -6,66 +6,57 @@ import { EventModel } from '@integreat-app/integreat-api-client'
 import type { MorphContentLanguageActionType } from '../../app/StoreActionType'
 import CategoriesMapModel from '@integreat-app/integreat-api-client/models/CategoriesMapModel'
 import forEachTreeNode from '../../common/forEachTreeNode'
-import { isEmpty } from 'lodash'
 
 const categoryRouteTranslator = (newCategoriesMap: CategoriesMapModel, newLanguage: string) =>
-  (route: CategoryRouteStateType, navKey: string): CategoryRouteStateType => {
-    try {
-      const {depth, root, allAvailableLanguages} = route
+  (route: CategoryRouteStateType): CategoryRouteStateType => {
+    const {depth, root, allAvailableLanguages} = route
 
-      const translatedRoot = allAvailableLanguages.get(newLanguage)
+    const translatedRoot = allAvailableLanguages.get(newLanguage)
 
-      if (!translatedRoot) { // Route is not translatable
-        return {...route}
-      }
+    if (!translatedRoot) { // Route is not translatable
+      return route
+    }
 
-      const rootModel = newCategoriesMap.findCategoryByPath(translatedRoot)
-      if (!rootModel) {
-        console.warn(`Inconsistent data detected: ${translatedRoot} does not exist,
+    const rootModel = newCategoriesMap.findCategoryByPath(translatedRoot)
+    if (!rootModel) {
+      console.warn(`Inconsistent data detected: ${translatedRoot} does not exist,
                       but is referenced in ${root} as translation for ${newLanguage}.`)
-        return {...route}
+      return route
+    }
+
+    const resultModels = {}
+    const resultChildren = {}
+
+    forEachTreeNode(rootModel, node => newCategoriesMap.getChildren(node), depth, (node, children) => {
+      resultModels[node.path] = node
+      if (children) {
+        resultChildren[node.path] = children.map(child => child.path)
       }
+    })
 
-      const resultModels = {}
-      const resultChildren = {}
-
-      forEachTreeNode(rootModel, node => newCategoriesMap.getChildren(node), depth, (node, children) => {
-        resultModels[node.path] = node
-        if (children) {
-          resultChildren[node.path] = children.map(child => child.path)
-        }
-      })
-
-      return {
-        root: translatedRoot,
-        models: resultModels,
-        children: resultChildren,
-        depth,
-        allAvailableLanguages
-      }
-    } catch (e) {
-      console.error(e.message)
-      console.error(`Failed while translating route with key ${navKey}`)
-      throw e
+    return {
+      root: translatedRoot,
+      models: resultModels,
+      children: resultChildren,
+      depth,
+      allAvailableLanguages
     }
   }
 
 const eventRouteTranslator = (newLanguage: string, newEvents: Array<EventModel>) =>
-  (route: EventRouteStateType, navKey: string): EventRouteStateType => {
-    const {path, models, allAvailableLanguages} = route
+  (route: EventRouteStateType): EventRouteStateType => {
+    const {path, allAvailableLanguages} = route
+
     if (!path) { // Route is a list of all events
       return {path: null, models: newEvents, allAvailableLanguages}
     }
-    // Route is a single event
 
-    if (isEmpty(models)) {
-      throw new Error(`No model for route ${navKey}!`)
-    }
+    // Route is a single event
     const translatedPath = allAvailableLanguages.get(newLanguage)
 
     if (!translatedPath) {
       // There is no translation for this event
-      return {...route}
+      return route
     }
 
     const translatedEvent = newEvents.find(newEvent => translatedPath === newEvent.path)
@@ -73,7 +64,7 @@ const eventRouteTranslator = (newLanguage: string, newEvents: Array<EventModel>)
     if (!translatedEvent) {
       console.warn(`Inconsistent data detected: ${translatedPath} does not exist,
                     but is referenced in ${path} as translation for ${newLanguage}.`)
-      return {...route}
+      return route
     }
 
     return {path: translatedPath, models: [translatedEvent], allAvailableLanguages}
