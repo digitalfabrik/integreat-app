bcf94b68e2a39474c3defa07c029eacc8b7661b1
Maximilian Ammann <max@maxammann.org>
2019-05-07 11:28:28 +0200

    NATIVE-43: Fetch wohnen offers instead of using mock data

--- src/routes/wohnen/components/WohnenExtra.js
+++ src/routes/wohnen/components/WohnenExtra.js
@@ -4,11 +4,7 @@ import * as React from 'react'
 
 import {
   WohnenOfferModel,
-  ExtraModel,
-  SprungbrettJobModel,
-  Payload,
-  createSprungbrettJobsEndpoint
-} from '@integreat-app/integreat-api-client'
+  ExtraModel} from '@integreat-app/integreat-api-client'
 import OfferDetail from './OfferDetail'
 import Caption from '../../../modules/common/components/Caption'
 import OfferListItem from './OfferListItem'
@@ -16,73 +12,14 @@ import List from '../../../modules/common/components/List'
 import type { TFunction } from 'react-i18next'
 import { hashWohnenOffer } from '../../extras/hashWohnenOffer'
 import Failure from '../../../modules/error/components/Failure'
-import { WOHNEN_EXTRA } from '../../extras/constants'
-import request from '../../../modules/endpoint/request'
-import { ActivityIndicator } from '../../sprungbrett/containers/SprungbrettExtraContainer'
-import SprungbrettExtra from '../../sprungbrett/containers/SprungbrettExtraContainer'
 
 type PropsType = {|
   offers: Array<WohnenOfferModel>,
   offerHash?: WohnenOfferModel,
-  extra: ExtraModel,
+  wohnenExtra: ExtraModel,
   navigateToOffer: (offerHash: string) => void,
   t: TFunction
 |}
-
-type PropsType = {|
-  sprungbrettExtra: ?ExtraModel,
-  t: TFunction
-|}
-
-type SprungbrettStateType = {|
-  jobs: ?Array<SprungbrettJobModel>,
-  error: ?Error
-|}
-
-// HINT: If you are copy-pasting this container think about generalizing this way of fetching
-class WohnenExtraContainer extends React.Component<PropsType, SprungbrettStateType> {
-  constructor (props: PropsType) {
-    super(props)
-    this.state = {jobs: null, error: null}
-  }
-
-  componentWillMount () {
-    this.loadSprungbrett()
-  }
-
-  async loadSprungbrett () {
-    const {sprungbrettExtra} = this.props
-
-    if (!sprungbrettExtra) {
-      this.setState(() => ({error: new Error('The Sprungbrett extra is not supported.'), jobs: null}))
-      return
-    }
-
-    const payload: Payload<Array<ExtraModel>> = await request(createSprungbrettJobsEndpoint(sprungbrettExtra.path))
-
-    if (payload.error) {
-      this.setState(() => ({error: payload.error, jobs: null}))
-      return
-    }
-
-    this.setState(() => ({error: null, jobs: payload.data}))
-  }
-
-  render () {
-    const {sprungbrettExtra, t} = this.props
-    const {jobs, error} = this.state
-
-    if (error) {
-      return error.message
-    }
-
-    if (!jobs) {
-      return <ActivityIndicator size='large' color='#0000ff' />
-    }
-
-    return <SprungbrettExtra sprungbrettExtra={sprungbrettExtra} sprungbrettJobs={jobs} t={t} />
-  }
-}
 
 class WohnenExtra extends React.Component<PropsType> {
   renderOfferListItem = (offer: WohnenOfferModel) => {
@@ -98,12 +35,7 @@ class WohnenExtra extends React.Component<PropsType> {
   }
 
   render () {
-    const {offers, extra, offerHash, t} = this.props
-    const extra: ExtraModel | void = extras.find(extra => extra.alias === WOHNEN_EXTRA)
-
-    if (!extra) {
-      return <Failure error={new Error('The Wohnen extra is not supported.')} />
-    }
+    const {offers, wohnenExtra, offerHash, t} = this.props
 
     if (offerHash) {
       const offer = offers.find(_offer => hashWohnenOffer(_offer) === offerHash)
@@ -119,7 +51,7 @@ class WohnenExtra extends React.Component<PropsType> {
 
     return (
       <>
-        <Caption title={extra.title} />
+        <Caption title={wohnenExtra.title} />
         <List noItemsMessage={t('noOffersAvailable')}
               items={offers}
               renderItem={this.renderOfferListItem} />
