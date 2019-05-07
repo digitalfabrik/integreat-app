be3fff6d3e8ad62bd0c48bdc0f53142f18bd2fc9
Maximilian Ammann <max@maxammann.org>
2019-05-07 09:48:43 +0200

    NATIVE-43: Prepare WohnenExtra for real data

--- src/routes/wohnen/containers/WohnenExtraContainer.js
+++ src/routes/wohnen/containers/WohnenExtraContainer.js
@@ -20,76 +20,7 @@ const mapStateToProps = (state: StateType, ownProps) => {
     }
   }
 
-  // MockData:
-  const offers = [
-    {
-      'email': 'erika@mustermann.de',
-      'createdDate': '2018-05-27T11:19:49.185Z',
-      'formData': {
-        'landlord': {
-          'lastName': 'Mustermann',
-          'firstName': 'Erika',
-          'phone': '12342345346'
-        },
-        'accommodation': {
-          'title': 'Super flat in cityCode center',
-          'location': 'Augsburg',
-          'totalArea': 105,
-          'totalRooms': 3,
-          'ofRooms': ['bath', 'child1'],
-          'ofRoomsDiff': ['child2', 'child3', 'bed'],
-          'moveInDate': '2018-05-24'
-        },
-        'costs': {
-          'baseRent': 450.30,
-          'runningCosts': 30.20,
-          'ofRunningServices': ['heating'],
-          'ofRunningServicesDiff': ['garbage', 'chimney'],
-          'hotWaterInHeatingCosts': true,
-          'additionalCosts': 0,
-          'ofAdditionalServices': [],
-          'ofAdditionalServicesDiff': ['garage', 'other']
-        }
-      }
-    }
-  ].map(offer => {
-    const landlord = offer.formData.landlord
-    const accommodation = offer.formData.accommodation
-    const costs = offer.formData.costs
-    return new WohnenOfferModel({
-      email: offer.email,
-      createdDate: moment(offer.createdDate),
-      formData: new WohnenFormData(
-        {
-          firstName: landlord.firstName,
-          lastName: landlord.lastName,
-          phone: landlord.phone
-        },
-        {
-          ofRooms: accommodation.ofRooms,
-          title: accommodation.title,
-          location: accommodation.location,
-          totalArea: accommodation.totalArea,
-          totalRooms: accommodation.totalRooms,
-          moveInDate: moment(accommodation.moveInDate),
-          ofRoomsDiff: accommodation.ofRoomsDiff
-        },
-        {
-          ofRunningServices: costs.ofRunningServices,
-          ofAdditionalServices: costs.ofAdditionalServices,
-          baseRent: costs.baseRent,
-          runningCosts: costs.runningCosts,
-          hotWaterInHeatingCosts: costs.hotWaterInHeatingCosts === 'true',
-          additionalCosts: costs.additionalCosts,
-          ofRunningServicesDiff: costs.ofRunningServicesDiff,
-          ofAdditionalServicesDiff: costs.ofAdditionalServicesDiff
-        }),
-      formDataType: WohnenFormData
-    })
-  })
-
   return {
-    offers: offers,
     offerHash: offerHash,
     extras: extras,
     navigateToOffer: navigateToOffer
