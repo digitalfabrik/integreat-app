97ae59f5bc9e9eb707a20e3dce29e6f060a019fc
Steffen Kleinle <klinzo@yahoo.de>
2019-05-09 11:19:17 +0200

    NATIVE-111: Refactor

--- src/modules/app/createNavigateToIntegreatUrl.js
+++ src/modules/app/createNavigateToIntegreatUrl.js
@@ -4,6 +4,7 @@ import createNavigateToCategory from './createNavigateToCategory'
 import type { Dispatch } from 'redux'
 import type { NavigationScreenProp } from 'react-navigation'
 import createNavigateToEvent from './createNavigateToEvent'
+import type { SwitchContentLanguageActionType } from './StoreActionType'
 
 export default (dispatch: Dispatch<*>, navigation: NavigationScreenProp<*>) =>
   (url: string, cityCode: string, language: string) => {
@@ -12,14 +13,17 @@ export default (dispatch: Dispatch<*>, navigation: NavigationScreenProp<*>) =>
     const pathname = pathnameParts.reduce((acc, part) => `${acc}/${part}`, '')
 
     if (pathnameParts[1] !== language) {
-      dispatch({
+      const switchContentLanguage: SwitchContentLanguageActionType = {
         type: 'SWITCH_CONTENT_LANGUAGE',
         params: {
           city: pathnameParts[0],
           newLanguage: pathnameParts[1]
         }
-      })
+      }
+      dispatch(switchContentLanguage)
     }
+
+    // todo add support for linking to different cities
 
     if (pathnameParts[0] === cityCode) {
       // same city
@@ -41,5 +45,4 @@ export default (dispatch: Dispatch<*>, navigation: NavigationScreenProp<*>) =>
         )
       }
     }
-    // todo add support for linking to different cities
   }
