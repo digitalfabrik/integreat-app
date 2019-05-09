93ce0a5a7da724dea25c4c2459d96ab0c23010c2
Steffen Kleinle <klinzo@yahoo.de>
2019-05-09 15:39:17 +0200

    Native-111: Introduce vars for better readability

--- src/modules/app/createNavigateToIntegreatUrl.js
+++ src/modules/app/createNavigateToIntegreatUrl.js
@@ -10,42 +10,45 @@ export default (dispatch: Dispatch<*>, navigation: NavigationScreenProp<*>) =>
   ({url, cityCode, language}: {|url: string, cityCode: string, language: string|}) => {
     const parts = url.split('/').filter(segment => segment)
     const pathnameParts = parts.splice(2)
+    const newCity = pathnameParts[0]
+    const newLanguage = pathnameParts[1]
     const pathname = pathnameParts.reduce((acc, part) => `${acc}/${part}`, '')
-
-    if (pathnameParts[1] !== language) {
-      const switchContentLanguage: PushContentLanguageActionType = {
-        type: 'PUSH_CONTENT_LANGUAGE',
-        params: {
-          city: pathnameParts[0],
-          newLanguage: pathnameParts[1]
-        }
-      }
-      dispatch(switchContentLanguage)
-    }
 
     // todo add support for linking to different cities
 
-    if (pathnameParts[0] === cityCode) {
+    if (newCity === cityCode) {
       // same city
+
+      if (newLanguage !== language) {
+        const switchContentLanguage: PushContentLanguageActionType = {
+          type: 'PUSH_CONTENT_LANGUAGE',
+          params: {
+            city: newCity,
+            newLanguage
+          }
+        }
+        dispatch(switchContentLanguage)
+      }
+
       if (pathnameParts[2] === 'events') {
         if (pathnameParts[3]) {
           // '/augsburg/de/events/some_event'
           createNavigateToEvent(dispatch, navigation)(
-            {cityCode, language: pathnameParts[1], path: pathname, previousLanguage: language})
+            {cityCode, language: newLanguage, path: pathname, previousLanguage: language})
         } else {
           // '/augsburg/de/events'
           createNavigateToEvent(dispatch, navigation)(
-            {cityCode, language: pathnameParts[1], previousLanguage: language})
+            {cityCode, language: newLanguage, previousLanguage: language})
         }
       } else if (pathnameParts[2]) {
         // '/augsburg/de/willkommen'
         createNavigateToCategory('Categories', dispatch, navigation)(
-          {cityCode, language: pathnameParts[1], previousLanguage: language, path: pathname})
+          {cityCode, language: newLanguage, previousLanguage: language, path: pathname})
       } else {
-        // '/augsburg/de'
-        const path = pathnameParts[1] ? pathname : `${pathname}/${language}`
+        // '/augsburg/de' or '/augsburg'
+        const path = newLanguage ? pathname : `${pathname}/${language}`
         createNavigateToCategory('Dashboard', dispatch, navigation)(
-          {cityCode, language: pathnameParts[1], path, previousLanguage: language}
+          {cityCode, language: newLanguage || language, path, previousLanguage: language}
         )
       }
     }
