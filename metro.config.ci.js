d82cdcd9b21bc2ed7b08dc2aaa2c81c73a87140f
Michael Markl <marklmichael98@gmail.com>
2019-05-12 21:43:01 +0200

    NATIVE-204: Remove unused config options

--- metro.config.ci.js
+++ metro.config.ci.js
@@ -1,11 +1,5 @@
 module.exports = {
   transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false
-      }
-    }),
     assetPlugins: ['react-native-svg-asset-plugin']
   },
   cacheStores: []
