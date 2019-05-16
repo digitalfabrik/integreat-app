fa9dd4a21338e7f2a346d2f592794aa8ee00f518
Andrew <kud@integreat-app.de>
2019-05-16 19:48:01 +0300

    linting

--- src/modules/layout/containers/HeaderContainer.js
+++ src/modules/layout/containers/HeaderContainer.js
@@ -1,19 +1,12 @@
 // @flow
-import Header from '../components/Header'
-// import { connect } from 'react-redux'
-// import { withTheme } from 'styled-components'
-// import { CategoryModel } from '@integreat-app/integreat-api-client'
-
-// const mapStateToProps = ({ categories }: CategoryModel) => categories
-
-// // $FlowFixMe
-// export default connect(mapStateToProps)(withTheme(Header))
-import { withTheme } from 'styled-components/native'
-import type { StateType } from '../../app/StateType'
-import { connect } from 'react-redux'
 import React from 'react'
+import { connect } from 'react-redux'
 import { withNavigation } from 'react-navigation'
 import { translate } from 'react-i18next'
+
+import Header from '../components/Header'
+import { withTheme } from 'styled-components/native'
+import type { StateType } from '../../app/StateType'
 import { availableLanguagesSelector } from '../../common/selectors/availableLanguagesSelector'
 
 const mapStateToProps = (state: StateType, ownProps) => ({
@@ -22,4 +15,4 @@ const mapStateToProps = (state: StateType, ownProps) => ({
 })
 
 const themed = withTheme(props => <Header {...props} />)
-export default translate('heading')(withNavigation(connect(mapStateToProps)(themed)))
+export default translate('header')(withNavigation(connect(mapStateToProps)(themed)))
