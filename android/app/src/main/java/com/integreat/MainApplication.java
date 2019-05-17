3fdf6829d88dc9f788bf52fad934b58f63cfa1df
Maximilian Ammann <max@maxammann.org>
2019-05-17 20:33:28 +0200

    NATIVE-192: Add sentry and manually link it for android

--- android/app/src/main/java/com/integreat/MainApplication.java
+++ android/app/src/main/java/com/integreat/MainApplication.java
@@ -15,6 +15,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.sentry.RNSentryPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+                    new RNSentryPackage(),
                     new RNFetchBlobPackage(),
                     new RNCWebViewPackage(),
                     new PDFViewPackage(),
