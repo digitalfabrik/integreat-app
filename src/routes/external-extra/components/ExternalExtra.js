12c2add2ed7c6de3491ff1295047ab5c84917f76
Michael Markl <marklmichael98@gmail.com>
2019-05-24 15:58:20 +0200

    NATIVE-212: Use url-encoded string as body

--- src/routes/external-extra/components/ExternalExtra.js
+++ src/routes/external-extra/components/ExternalExtra.js
@@ -1,9 +1,9 @@
 // @flow
 
 import * as React from 'react'
-import { TFunction, translate } from 'react-i18next'
 import { WebView } from 'react-native-webview'
 import { Text } from 'react-native'
+import { stringify } from 'query-string'
 
 type PropsType = {|
   url: ?string,
@@ -13,7 +13,7 @@ type PropsType = {|
 class ExternalExtra extends React.Component<PropsType> {
   render () {
     const {url, postData} = this.props
-    const body = !postData ? '' : JSON.stringify(Object.fromEntries(postData))
+    const body = !postData ? '' : stringify(Object.fromEntries(postData))
     return <WebView
       source={{
         uri: url,
