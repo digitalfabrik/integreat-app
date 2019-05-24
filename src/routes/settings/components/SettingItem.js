18fdd927cb0522b7b6428a27f422c736c238b5d4
Maximilian Ammann <max@maxammann.org>
2019-05-24 17:04:08 +0200

    NATIVE-11: Use NativeFeedback for touch

--- src/routes/settings/components/SettingItem.js
+++ src/routes/settings/components/SettingItem.js
@@ -1,15 +1,15 @@
 // @flow
 
 import * as React from 'react'
-import { TouchableOpacity, View } from 'react-native'
+import { TouchableNativeFeedback, View } from 'react-native'
 import styled from 'styled-components/native'
 import type { ThemeType } from '../../../modules/theme/constants/theme'
 
 type PropType = {
   title: string,
   description: ?string,
-  onPress: () => void,
-  children: React.Node,
+  onPress: ?() => void,
+  children: ?React.Node,
   theme: ThemeType
 }
 const PadView = styled.View`
@@ -44,7 +44,7 @@ export default class SettingItem extends React.Component<PropType> {
   render () {
     const {title, description, onPress, children, theme} = this.props
 
-    return <TouchableOpacity onPress={onPress}>
+    return <TouchableNativeFeedback onPress={onPress}>
       <PadView>
         <ContentContainer>
           <View><Title theme={theme}>{title}</Title></View>
@@ -52,6 +52,6 @@ export default class SettingItem extends React.Component<PropType> {
         </ContentContainer>
         <RightContentContainer>{children}</RightContentContainer>
       </PadView>
-    </TouchableOpacity>
+    </TouchableNativeFeedback>
   }
 }
