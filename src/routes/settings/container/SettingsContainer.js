a8779eb439a3b7c807fe3a69997013575c60c471
Maximilian Ammann <max@maxammann.org>
2019-05-25 23:03:43 +0200

    NATIVE-11: Do not expect theme as it comes from a HOC

--- src/routes/settings/container/SettingsContainer.js
+++ src/routes/settings/container/SettingsContainer.js
@@ -3,7 +3,6 @@
 import Settings from '../components/Settings'
 import { withTheme } from 'styled-components/native'
 import type { NavigationScreenProp } from 'react-navigation'
-import type { ThemeType } from '../../../modules/theme/constants/theme'
 import { translate } from 'react-i18next'
 import { connect } from 'react-redux'
 import type { StateType } from '../../../modules/app/StateType'
@@ -11,8 +10,7 @@ import type { StateType } from '../../../modules/app/StateType'
 type OwnPropsType = {| navigation: NavigationScreenProp<*> |}
 
 type PropsType = {
-  navigation: NavigationScreenProp<*>,
-  theme: ThemeType
+  navigation: NavigationScreenProp<*>
 }
 
 const mapStateToProps = (state: StateType) => {
