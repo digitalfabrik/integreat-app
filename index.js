d6b5d7f390acc5939a9387f938500fa2e4b40999
Maximilian Ammann <max@maxammann.org>
2019-05-28 15:39:48 +0200

    NATIVE-229: Import all locales in moment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { AppRegistry, YellowBox } from 'react-native'
 import App from './src/modules/app/components/App'
-import 'moment/locale/de' // fixme
+import 'moment/min/locales'
 
 // @see: https://github.com/facebook/react-native/issues/9599
 if (typeof global.self === 'undefined') {
