16984cd3dbf0f9c59092ab5c514e3269f16d3ed3
Michael Markl <marklmichael98@gmail.com>
2019-05-29 22:19:25 +0200

    NATIVE-212: Make PropsType strict

--- src/routes/external-extra/containers/ExternalExtraContainer.js
+++ src/routes/external-extra/containers/ExternalExtraContainer.js
@@ -5,7 +5,7 @@ import ExternalExtra from '../components/ExternalExtra'
 import type { NavigationScreenProp } from 'react-navigation'
 import type { PropsType } from '../components/ExternalExtra'
 
-export default mapProps<PropsType, {navigation: NavigationScreenProp<*>}>(
+export default mapProps<PropsType, {| navigation: NavigationScreenProp<*> |}>(
   ({navigation}) => ({
     url: navigation.getParam('url'),
     postData: navigation.getParam('postData')
