9b7c1fe79509ccb0094d410d72c6d96d8ae5c978
Michael Markl <marklmichael98@gmail.com>
2019-06-15 13:31:01 +0200

    WEBAPP-476: Remove favicon from AppConfigType

--- tools/integreat-config/index.js
+++ tools/integreat-config/index.js
@@ -6,7 +6,6 @@ import brightTheme from '../../src/modules/theme/constants/theme'
 
 const IntegreatAppConfig: AppConfigType = {
   appTitle: 'Integreat',
-  favicon: 'asdfasdf', // todo
   itunesAppId: '1072353915',
   theme: brightTheme,
   cmsUrl: 'https://cms.integreat-app.de'
