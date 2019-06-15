ca3e9c74c3f8fc0944caf5baf1edf5956013b5d6
Michael Markl <marklmichael98@gmail.com>
2019-06-15 16:26:25 +0200

    WEBAPP-476: Add malteser config

---
+++ tools/malteser-config/index.js
@@ -0,0 +1,31 @@
+// @flow
+
+import type { AppConfigType } from '../../src/AppConfigType'
+import type { ThemeType } from '../../src/modules/theme/constants/theme'
+import dimensions from '../../src/modules/theme/constants/dimensions'
+import fonts from '../../src/modules/theme/constants/fonts'
+import helpers from '../../src/modules/theme/constants/helpers'
+
+const theme: ThemeType = {
+  colors: {
+    themeColor: '#e2001a',
+    themeDarkColor: '#830012',
+    backgroundAccentColor: '#fafafa',
+    textColor: '#000000',
+    textSecondaryColor: '#585858',
+    textDecorationColor: '#c7c7c7',
+    textDisabledColor: '#d0d0d0',
+    backgroundColor: '#ffffff'
+  },
+  dimensions: dimensions,
+  fonts: fonts,
+  helpers: helpers
+}
+
+const IntegreatAppConfig: AppConfigType = {
+  appTitle: 'Malteser',
+  theme: theme,
+  cmsUrl: 'https://malteser.tuerantuer.org'
+}
+
+module.exports = IntegreatAppConfig
