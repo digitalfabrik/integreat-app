431a4d2a7aa2e6c78f3f9284d4691624bc17cbbb
Michael Markl <marklmichael98@gmail.com>
2019-06-15 16:46:55 +0200

    WEBAPP-476: Use apiUrl of config

--- src/modules/app/constants/urls.js
+++ src/modules/app/constants/urls.js
@@ -2,5 +2,5 @@
 
 export const integreatApiBaseUrl =
   (window.localStorage && window.localStorage.getItem && window.localStorage.getItem('api-url')) ||
-  'https://cms.integreat-app.de'
+  __CONFIG__.cmsUrl
 export const wohnenApiBaseUrl = 'https://api.wohnen.integreat-app.de/v0'
