f6e6b6a918be0e659c6ca6f17df11ec77bd70608
Michael Markl <marklmichael98@gmail.com>
2019-06-15 16:02:46 +0200

    WEBAPP-476: Use process arguments in task runner if args is empty

--- tools/task.js
+++ tools/task.js
@@ -11,9 +11,10 @@
 function run (task, action, ...args) {
   const command = process.argv[2]
   const taskName = command && !command.startsWith('-') ? `${task}:${command}` : task
+  const newArgs = args.length === 0 ? process.argv.slice(2) : args
   const start = new Date()
   process.stdout.write(`Starting '${taskName}'...\n`)
-  return Promise.resolve().then(() => action(...args)).then(result => {
+  return Promise.resolve().then(() => action(...newArgs)).then(result => {
     process.stdout.write(`Finished '${taskName}' after ${new Date().getTime() - start.getTime()}ms\n`)
     return Promise.resolve(result)
   }, err => {
