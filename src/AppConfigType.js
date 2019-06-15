daf8c4ee87ce0f906fe0bb24b2f2a0af2ef396bc
Michael Markl <marklmichael98@gmail.com>
2019-06-15 13:31:01 +0200

    WEBAPP-476: Remove favicon from AppConfigType

--- src/AppConfigType.js
+++ src/AppConfigType.js
@@ -4,7 +4,6 @@ import type { ThemeType } from './modules/theme/constants/theme'
 
 export type AppConfigType = {|
   appTitle: string,
-  favicon: string,
   cmsUrl: string,
   theme: ThemeType,
   itunesAppId?: string
