b03ee2922fa14edb3a485eddf5221c81f33ab02d
Michael Markl <marklmichael98@gmail.com>
2019-06-18 15:11:08 +0200

    NATIVE-207: Fix flow error

--- src/routes/feedback/containers/FeedbackModalContainer.js
+++ src/routes/feedback/containers/FeedbackModalContainer.js
@@ -2,23 +2,23 @@
 
 import * as React from 'react'
 import type { NavigationScreenProp } from 'react-navigation'
-import { type TFunction, translate } from 'react-i18next'
+import { translate } from 'react-i18next'
 import withTheme from '../../../modules/theme/hocs/withTheme'
 import FeedbackModal from '../components/FeedbackModal'
 import FeedbackVariant from '../FeedbackVariant'
 
 const TranslatedFeedbackModal = translate('feedback')(withTheme()(FeedbackModal))
 
-class FeedbackModalContainer extends React.Component<{| navigation: NavigationScreenProp<*>, t: TFunction |}> {
+class FeedbackModalContainer extends React.Component<{| navigation: NavigationScreenProp<*> |}> {
   closeModal = () => this.props.navigation.goBack()
 
   render () {
-    const {t, navigation} = this.props
+    const {navigation} = this.props
     const feedbackItems: Array<FeedbackVariant> = navigation.getParam('feedbackItems')
     const isPositiveFeedback: boolean = navigation.getParam('isPositiveFeedback')
-    return <TranslatedFeedbackModal t={t} closeModal={this.closeModal} feedbackItems={feedbackItems}
+    return <TranslatedFeedbackModal closeModal={this.closeModal} feedbackItems={feedbackItems}
                                     isPositiveFeedback={isPositiveFeedback} />
   }
 }
 
-export default translate('feedback')(FeedbackModalContainer)
+export default FeedbackModalContainer
