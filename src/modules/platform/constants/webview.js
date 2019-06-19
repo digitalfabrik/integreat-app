17e3be5336eca0a78898d3534975e5b6b0f43f59
Maximilian Ammann <max@maxammann.org>
2019-06-19 19:56:42 +0200

    NATIVE-110: Extract functions for webview

--- src/modules/platform/constants/webview.js
+++ src/modules/platform/constants/webview.js
@@ -2,6 +2,7 @@
 
 import RNFetchblob from 'rn-fetch-blob'
 import { Platform } from 'react-native'
+import type { WebViewSource } from 'react-native-webview'
 
 export const URL_PREFIX = 'file://'
 
@@ -15,4 +16,36 @@ export const getResourceCacheFilesPath = (city: string) => `${RESOURCE_CACHE_DIR
 export const getFontFaceSource = (fontName: string) => Platform.select({
   ios: `local('${fontName}') url('${fontName}.ttf') format('truetype')`,
   android: `url('file:///android_asset/fonts/${fontName}.ttf') format('truetype')`
+})
+
+export const createPostSource = (
+  uri: string,
+  body: string,
+  contentType: string = 'application/x-www-form-urlencoded'
+): WebViewSource => ({
+  uri: uri,
+  method: 'POST',
+  body,
+  headers: Platform.select({
+    ios: {'Content-Type': contentType},
+    android: undefined /* `headers` is not supported on Android.
+                           In this case Android figures the out which content type to use */
+  })
+})
+
+export const createGetSource = (
+  uri: string,
+  body: string
+): WebViewSource => ({
+  uri: uri,
+  method: 'GET',
+  body
+})
+
+export const createHtmlSource = (
+  html: string,
+  baseUrl: string
+): WebViewSource => ({
+  baseUrl: baseUrl,
+  html: html
 })
