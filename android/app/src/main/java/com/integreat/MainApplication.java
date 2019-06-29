9b2d15f8bee98691cb0703f4cc6c95063db5de97
Andrii Kudriavtsev <akudriavtsev@rubiconproject.com>
2019-06-29 12:38:05 +0200

    add share feature for image viewer

--- android/app/src/main/java/com/integreat/MainApplication.java
+++ android/app/src/main/java/com/integreat/MainApplication.java
@@ -3,6 +3,7 @@ package com.integreat;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
 import com.rumax.reactnative.pdfviewer.PDFViewPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new RNSharePackage(),
                     new RNSentryPackage(),
                     new RNFetchBlobPackage(),
                     new RNCWebViewPackage(),
--- src/modules/layout/components/TransparentHeader.js
+++ src/modules/layout/components/TransparentHeader.js
@@ -1,10 +1,14 @@
 // @flow
 
 import * as React from 'react'
+import Share from 'react-native-share'
 import styled from 'styled-components/native'
-import type { NavigationScene } from 'react-navigation'
+import type { NavigationScreenProp } from 'react-navigation'
 import type { ThemeType } from 'modules/theme/constants/theme'
 import HeaderBackButton from 'react-navigation-stack/lib/module/views/Header/HeaderBackButton'
+import HeaderButtons, { HeaderButton, Item } from 'react-navigation-header-buttons'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
+import type { TFunction } from 'react-i18next'
 
 const Horizontal = styled.View`
   flex:1;
@@ -30,28 +34,67 @@ const BoxShadow = styled.View`
 `
 
 type PropsType = {
-  scene: NavigationScene,
-  scenes: Array<NavigationScene>,
-  theme: ThemeType
+  navigation: NavigationScreenProp<*>,
+  theme: ThemeType,
+  t: TFunction
+}
+
+const MaterialHeaderButton = props => (
+  <HeaderButton {...props} IconComponent={MaterialIcon} iconSize={23} color='black' />
+)
+
+const MaterialHeaderButtons = props => {
+  return (
+    <HeaderButtons
+      HeaderButtonComponent={MaterialHeaderButton}
+      OverflowIcon={<MaterialIcon name='more-vert' size={23} color='black' />}
+      {...props}
+    />
+  )
 }
 
 class TransparentHeader extends React.PureComponent<PropsType> {
-  getDescriptor (): { [key: string]: any } {
-    // $FlowFixMe
-    return this.props.scene.descriptor
+  goBack = () => {
+    this.props.navigation.goBack(null)
   }
 
-  goBack = () => {
-    this.getDescriptor().navigation.goBack(null)
+  onShare = async () => {
+    const { navigation } = this.props
+    const { url } = navigation.state.params
+    // const configOptions = { fileCache: true, path: url }
+    // RNFetchBlob.config(configOptions)
+    //   .then(resp => {
+    //     return resp.readFile('base64')
+    //   })
+    //   .then(async base64Data => {
+    //     // base64Data = `data:${type};base64,` + base64Data;
+    //     // await Share.open({ url: base64Data });
+    //     console.log("TCL: TransparentHeader -> onShare -> base64Data", base64Data)
+    //     // remove the image or pdf from device's storage
+    //     // await RNFS.unlink(filePath);
+    //   })
+    try {
+      await Share.open({
+        type: 'image/jpg',
+        url
+      })
+    } catch (e) {
+      alert(e.message)
+    }
   }
 
   render () {
+    const { t } = this.props
+
     return (
       <BoxShadow theme={this.props.theme}>
         <Horizontal>
           <HorizontalLeft>
             <HeaderBackButton onPress={this.goBack} />
           </HorizontalLeft>
+          <MaterialHeaderButtons>
+            <Item title={t('share')} show='never' onPress={this.onShare} />
+          </MaterialHeaderButtons>
         </Horizontal>
       </BoxShadow>
     )
