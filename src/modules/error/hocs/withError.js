11cfb15dfdec3ca81c725c582af17ec2c7fb24ea
Michael Markl <marklmichael98@gmail.com>
2019-07-15 17:38:26 +0200

    NATIVE-44: Adjust withError.js

--- src/modules/error/hocs/withError.js
+++ src/modules/error/hocs/withError.js
@@ -1,38 +1,48 @@
 // @flow
 
 import * as React from 'react'
+
 import { LanguageModel } from '@integreat-app/integreat-api-client'
-import LanguageNotAvailableContainer from '../../common/containers/LanguageNotAvailableContainer'
+import { RefreshControl, ScrollView } from 'react-native'
 import Failure from '../components/Failure'
+import LanguageNotAvailableContainer from '../../common/containers/LanguageNotAvailableContainer'
 
-export type PropsType = {|
-  error: boolean,
-  languageNotAvailable: boolean,
-  availableLanguages?: Array<LanguageModel>,
-  currentCityCode?: string,
-  changeUnavailableLanguage?: (city: string, newLanguage: string) => void
-|}
+export type PropsType<T> = {| routeInitialized: false |} |
+  {| routeInitialized: true, error: true |} |
+  {|
+    routeInitialized: true, error: false, languageNotAvailable: true, availableLanguages: Array<LanguageModel>,
+    changeUnavailableLanguage: (city: string, newLanguage: string) => void, cityCode: string
+  |} |
+  {| routeInitialized: true, error: false, languageNotAvailable: false, loading: true |} |
+  {| routeInitialized: true, error: false, languageNotAvailable: false, loading: false, innerProps: T |}
 
-const withError = <T: {}>(
-  Component: React.AbstractComponent<T>
-): React.AbstractComponent<T & PropsType> => {
-  return class extends React.Component<T & PropsType> {
+const withError = <T: {}> (
+  Component: React.AbstractComponent<T>,
+  refresh: () => Promise<void>
+): React.AbstractComponent<PropsType<T>> => {
+  return class extends React.Component<PropsType<T>> {
     render () {
-      const {
-        error, languageNotAvailable, currentCityCode, availableLanguages, changeUnavailableLanguage, ...props
-      } = this.props
-
-      if (languageNotAvailable && currentCityCode && availableLanguages && changeUnavailableLanguage) {
-        return <LanguageNotAvailableContainer city={currentCityCode}
-                                              languages={availableLanguages}
-                                              changeLanguage={changeUnavailableLanguage} />
+      const props = this.props
+      if (!props.routeInitialized) {
+        return null
       }
-
-      if (error) {
-        return <Failure />
+      if (props.error) {
+        return <ScrollView refreshControl={<RefreshControl onRefresh={refresh} refreshing={false} />}
+                           contentContainerStyle={{ flexGrow: 1 }}>
+          <Failure />
+        </ScrollView>
+      } else if (props.languageNotAvailable) {
+        return <LanguageNotAvailableContainer city={props.cityCode} languages={props.availableLanguages}
+                                              changeLanguage={props.changeUnavailableLanguage} />
+      } else if (props.loading) {
+        return <ScrollView refreshControl={<RefreshControl onRefresh={refresh} refreshing />}
+                           contentContainerStyle={{ flexGrow: 1 }} />
+      } else {
+        return <ScrollView refreshControl={<RefreshControl onRefresh={refresh} refreshing={false} />}
+                           contentContainerStyle={{ flexGrow: 1 }}>
+          <Component {...props.innerProps} />
+        </ScrollView>
       }
-
-      return <Component {...props} />
     }
   }
 }
