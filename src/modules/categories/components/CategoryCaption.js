5920c2598341fcd581e2389f85c210b641a5c9cb
Michael Markl <marklmichael98@gmail.com>
2019-07-15 15:18:54 +0200

    LINT-10: Fix flow

--- src/modules/categories/components/CategoryCaption.js
+++ src/modules/categories/components/CategoryCaption.js
@@ -1,11 +1,14 @@
 // @flow
 
-import styled from 'styled-components/native'
+import styled, { type StyledComponent } from 'styled-components/native'
+import type { ThemeType } from '../../theme/constants/theme'
 
-export default styled.Text`
+const CategoryCaption: StyledComponent<{}, ThemeType, *> = styled.Text`
   align-self: center;
   padding: 15px 5px;
   border-bottom-width: 2px;
   border-bottom-color: ${props => props.theme.colors.themeColor};
   flex: 1;
 `
+
+export default CategoryCaption
