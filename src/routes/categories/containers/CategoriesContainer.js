27ca84b8e8775fd49815b3f294b648bb9ca2e4ea
Michael Markl <marklmichael98@gmail.com>
2019-07-21 11:28:48 +0200

    NATIVE-44: Implement new withError for CategoriesContainer

--- src/routes/categories/containers/CategoriesContainer.js
+++ src/routes/categories/containers/CategoriesContainer.js
@@ -4,94 +4,146 @@ import { connect } from 'react-redux'
 import type { LanguageResourceCacheStateType, StateType } from '../../../modules/app/StateType'
 import { type Dispatch } from 'redux'
 import CategoriesRouteStateView from '../../../modules/app/CategoriesRouteStateView'
-import type { StoreActionType } from '../../../modules/app/StoreActionType'
-import type { NavigateToCategoryParamsType } from '../../../modules/app/createNavigateToCategory'
+import type { StoreActionType, SwitchContentLanguageActionType } from '../../../modules/app/StoreActionType'
 import createNavigateToCategory from '../../../modules/app/createNavigateToCategory'
-import type { NavigateToIntegreatUrlParamsType } from '../../../modules/app/createNavigateToIntegreatUrl'
 import createNavigateToIntegreatUrl from '../../../modules/app/createNavigateToIntegreatUrl'
-import type { PropsType as CategoriesScrollViewPropsType } from '../components/CategoriesScrollView'
-import CategoriesScrollView from '../components/CategoriesScrollView'
 import type { NavigationScreenProp } from 'react-navigation'
+import type { StatusPropsType } from '../../../modules/error/hocs/withError'
 import withError from '../../../modules/error/hocs/withError'
-import { CityModel, LanguageModel } from '@integreat-app/integreat-api-client'
+import { CityModel } from '@integreat-app/integreat-api-client'
 import withTheme from '../../../modules/theme/hocs/withTheme'
 import { translate } from 'react-i18next'
 import withRouteCleaner from '../../../modules/endpoint/hocs/withRouteCleaner'
+import Categories from '../../../modules/categories/components/Categories'
+import React from 'react'
+
+type ContainerPropsType = {|
+  navigation: NavigationScreenProp<*>,
+  cities: Array<CityModel>,
+  cityCode: string,
+  language: string,
+  stateView: CategoriesRouteStateView,
+  resourceCache: LanguageResourceCacheStateType,
+  dispatch: Dispatch<StoreActionType>
+|}
+
+type RefreshPropsType = {|
+  cityCode: string,
+  language: string,
+  path: string,
+  navigation: NavigationScreenProp<*>
+|}
 
 type OwnPropsType = {| navigation: NavigationScreenProp<*> |}
-
-type StatePropsType = {|
-  error: boolean,
-  languageNotAvailable: boolean,
-  availableLanguages?: Array<LanguageModel>,
-  currentCityCode?: string,
-  cityCode?: string,
-  cities?: Array<CityModel>,
-  language?: string,
-  stateView?: CategoriesRouteStateView,
-  resourceCache?: LanguageResourceCacheStateType
-|}
-
-type DispatchPropsType = {|
-  navigateToCategory: NavigateToCategoryParamsType => void,
-  navigateToIntegreatUrl: NavigateToIntegreatUrlParamsType => void,
-  changeUnavailableLanguage?: (city: string, newLanguage: string) => void
-|}
-
+type StatePropsType = StatusPropsType<ContainerPropsType, RefreshPropsType>
+type DispatchPropsType = {| dispatch: Dispatch<StoreActionType> |}
 type PropsType = {| ...OwnPropsType, ...StatePropsType, ...DispatchPropsType |}
 
-const mapStateToProps = (state: StateType, ownProps: OwnPropsType): StatePropsType => {
-  if (!state.cityContent || state.cityContent.switchingLanguage) {
-    return { languageNotAvailable: false, error: false }
+const createChangeUnavailableLanguage = (path: string) => (
+  dispatch: Dispatch<StoreActionType>, navigation: NavigationScreenProp<*>, city: string, newLanguage: string
+) => {
+  const switchContentLanguage: SwitchContentLanguageActionType = {
+    type: 'SWITCH_CONTENT_LANGUAGE',
+    params: {
+      city,
+      newLanguage
+    }
   }
-  const { resourceCache, categoriesRouteMapping, city } = state.cityContent
+  dispatch(switchContentLanguage)
+  const navigateToCategory = createNavigateToCategory('Categories', dispatch, navigation)
+  navigateToCategory({
+    cityCode: city,
+    language: newLanguage,
+    path,
+    forceUpdate: false,
+    key: navigation.getParam('key')
+  })
+}
 
+const mapStateToProps = (state: StateType, ownProps: OwnPropsType): StatePropsType => {
+  if (!state.cityContent) {
+    return { status: 'routeNotInitialized' }
+  }
+
+  const { resourceCache, categoriesRouteMapping, city, switchingLanguage } = state.cityContent
+  const globalLanguage = state.contentLanguage
   if (state.cities.errorMessage !== undefined ||
     categoriesRouteMapping.errorMessage !== undefined ||
     resourceCache.errorMessage !== undefined) {
-    return { error: true, languageNotAvailable: false }
+    return {
+      status: 'error',
+      refreshProps: { cityCode: city, language: globalLanguage, path: '/', navigation: ownProps.navigation }
+    } // todo: refProps
   }
 
   const cities = state.cities.models
   const route = categoriesRouteMapping[ownProps.navigation.getParam('key')]
 
-  if (!route || !cities) {
-    return { languageNotAvailable: false, error: false }
+  if (!route) {
+    return { status: 'routeNotInitialized' }
+  }
+
+  if (route.loading || switchingLanguage || !cities) {
+    return { status: 'loading' }
   }
 
   const languages = Array.from(route.allAvailableLanguages.keys())
   const stateView = new CategoriesRouteStateView(route.root, route.models, route.children)
-
+  const refreshProps = {
+    cityCode: city, language: route.language, path: route.root, navigation: ownProps.navigation
+  }
   if (!languages.includes(route.language)) {
-    return { languageNotAvailable: true, availableLanguages: languages, currentCityCode: city, error: false }
+    return {
+      status: 'languageNotAvailable',
+      availableLanguages: languages,
+      cityCode: city,
+      refreshProps,
+      changeUnavailableLanguage: createChangeUnavailableLanguage(route.root)
+    }
   }
 
   return {
-    error: false,
-    languageNotAvailable: false,
-    cityCode: city,
-    language: route.language,
-    cities,
-    stateView,
-    resourceCache
+    status: 'success',
+    refreshProps,
+    innerProps: {
+      cityCode: city,
+      language: route.language,
+      cities,
+      stateView,
+      resourceCache,
+      navigation: ownProps.navigation
+    }
   }
 }
 
-const mapDispatchToProps = (dispatch: Dispatch<StoreActionType>, ownProps: OwnPropsType): DispatchPropsType => ({
-  navigateToCategory: createNavigateToCategory('Categories', dispatch, ownProps.navigation),
-  navigateToIntegreatUrl: createNavigateToIntegreatUrl(dispatch, ownProps.navigation),
-  changeUnavailableLanguage: (city: string, newLanguage: string) => {
-    dispatch({
-      type: 'SWITCH_CONTENT_LANGUAGE',
-      params: { city, newLanguage }
-    })
-  }
-})
+const mapDispatchToProps = (dispatch: Dispatch<StoreActionType>): DispatchPropsType => ({ dispatch })
 
-export default connect<PropsType, OwnPropsType, _, _, _, _>(mapStateToProps, mapDispatchToProps)(
-  withRouteCleaner<PropsType>(
-    withTheme(props => props.language)(
-      translate('categories')(
-        withError<CategoriesScrollViewPropsType>(
-          CategoriesScrollView
-        )))))
+const refresh = (refreshProps: RefreshPropsType, dispatch: Dispatch<StoreActionType>) => {
+  const { cityCode, language, path, navigation } = refreshProps
+  const navigateToCategories = createNavigateToCategory('Categories', dispatch, navigation)
+  navigateToCategories({
+    cityCode, language, path, forceUpdate: true, key: navigation.getParam('key')
+  })
+}
+
+class CategoriesContainer extends React.Component<ContainerPropsType> {
+  render () {
+    const { cities, language, stateView, cityCode, navigation, resourceCache, dispatch } = this.props
+    return <ThemedTranslatedCategories cities={cities} language={language} stateView={stateView} cityCode={cityCode}
+                                       navigateToCategory={createNavigateToCategory('Categories', dispatch, navigation)}
+                                       navigateToIntegreatUrl={createNavigateToIntegreatUrl(dispatch, navigation)}
+                                       navigation={navigation}
+                                       resourceCache={resourceCache} />
+  }
+}
+
+const ThemedTranslatedCategories = withTheme(props => props.language)(
+  translate('categories')(
+    Categories
+  ))
+
+export default withRouteCleaner<PropsType>(
+  connect<PropsType, OwnPropsType, _, _, _, _>(mapStateToProps, mapDispatchToProps)(
+    withError<ContainerPropsType, RefreshPropsType>(refresh)(
+      CategoriesContainer
+    )))
