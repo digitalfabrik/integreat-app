022e21c5d53ff938b978a748ccb467e7284065a3
Steffen Kleinle <klinzo@yahoo.de>
2019-07-22 15:45:38 +0200

    NATIVE-318: Fix test

--- src/modules/common/components/__tests__/PageDetail.spec.js
+++ src/modules/common/components/__tests__/PageDetail.spec.js
@@ -8,7 +8,8 @@ import brightTheme from '../../../theme/constants/theme'
 describe('PageDetail', () => {
   it('should display the given identifier followed by a colon', () => {
     const { getByType } = render(
-      <PageDetail identifier={'Test Identifier'} information={'Some important information'} theme={brightTheme} />
+      <PageDetail identifier={'Test Identifier'} information={'Some important information'} theme={brightTheme}
+                  language={'de'} />
     )
     const identifier = getByType(Identifier).props.children.join('')
     expect(identifier).toEqual('Test Identifier: ')
