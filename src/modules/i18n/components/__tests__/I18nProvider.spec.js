42cc97f2ad0da7147bb4e082ff752f27c0d5d25c
Steffen Kleinle <klinzo@yahoo.de>
2019-08-09 10:56:30 +0200

    NATIVE-322: Add i18n test

--- src/modules/i18n/components/__tests__/I18nProvider.spec.js
+++ src/modules/i18n/components/__tests__/I18nProvider.spec.js
@@ -5,6 +5,10 @@ import { render, wait } from '@testing-library/react-native'
 import React from 'react'
 import I18nProvider from '../I18nProvider'
 import createAppSettings from '../../../settings/__tests__/createAppSettings'
+import { I18nextProvider } from 'react-i18next'
+import i18next from 'i18next'
+import localesResources from '../../../../locales.json'
+import TestRenderer from 'react-test-renderer'
 
 describe('I18nProvider', () => {
   it('should transform the resources correctly', () => {
@@ -45,5 +49,37 @@ describe('I18nProvider', () => {
     await wait(() => expect(mockSetContentLanguage).toHaveBeenCalledWith('de'))
     await wait(() => expect(mockAppSettingsSetContentLanguage).toHaveBeenCalledTimes(1))
     await wait(() => expect(mockAppSettingsSetContentLanguage).toHaveBeenCalledWith('de'))
+  })
+
+  it('should initialize correct i18next instance', () => {
+    const unmockedCreateInstance = i18next.createInstance.bind(i18next)
+    const i18nInstance = unmockedCreateInstance()
+    i18next.createInstance = jest.fn(() => {
+      i18nInstance.init = jest.fn(i18nInstance.init)
+      i18nInstance.use = jest.fn(i18nInstance.use)
+      return i18nInstance
+    })
+
+    const component = TestRenderer.create(
+      <I18nProvider setContentLanguage={() => {}}
+                    getLocale={() => 'de_DE'}
+                    appSettings={createAppSettings({})}>
+        <div />
+      </I18nProvider>
+    ).root
+
+    expect(i18next.createInstance.mock.calls).toHaveLength(1)
+
+    expect(component.findByType(I18nextProvider).props.i18n).toBe(i18nInstance)
+
+    expect(i18nInstance.init.mock.calls).toHaveLength(1)
+    expect(i18nInstance.init.mock.calls[0]).toHaveLength(1)
+    const options = i18nInstance.init.mock.calls[0][0]
+
+    expect(options.resources).toEqual(I18nProvider.transformResources(localesResources))
+    delete options.resources
+
+    expect(options).toMatchSnapshot()
+    i18next.createInstance = unmockedCreateInstance
   })
 })
