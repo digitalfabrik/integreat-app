47d7b536a98de84f7214c76f609dedf66510c501
Steffen Kleinle <klinzo@yahoo.de>
2019-08-09 12:30:32 +0200

    NATIVE-322: Upate tests

--- src/modules/i18n/reducers/contentLanguageReducer.js
+++ src/modules/i18n/reducers/contentLanguageReducer.js
@@ -1,14 +1,10 @@
 // @flow
 
 import type { SetContentLanguageActionType } from '../../app/StoreActionType'
-import { handleAction, type ReduxReducer } from 'redux-actions'
 import { DEFAULT_LANGUAGE } from '../components/I18nProvider'
 
-const contentLanguageReducer: ReduxReducer<string, SetContentLanguageActionType> =
-  handleAction(
-    'SET_CONTENT_LANGUAGE',
-    (state: string, action: SetContentLanguageActionType) => action.params.contentLanguage,
-    DEFAULT_LANGUAGE
-  )
+const contentLanguageReducer = (state: string = DEFAULT_LANGUAGE, action: SetContentLanguageActionType): string => {
+  return action.params.contentLanguage
+}
 
 export default contentLanguageReducer
