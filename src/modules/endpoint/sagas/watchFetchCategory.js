3868cb5684232e382144c1788127ed55ca3d412a
Michael Markl <marklmichael98@gmail.com>
2019-08-19 22:07:41 +0200

    NATIVE-325: Streamline state and always use languageModels in routes

--- src/modules/endpoint/sagas/watchFetchCategory.js
+++ src/modules/endpoint/sagas/watchFetchCategory.js
@@ -1,7 +1,7 @@
 // @flow
 
 import type { Saga } from 'redux-saga'
-import { all, call, put, takeLatest, race, take, select } from 'redux-saga/effects'
+import { all, call, put, race, select, take, takeLatest } from 'redux-saga/effects'
 import type {
   FetchCategoryActionType,
   FetchCategoryFailedActionType,
@@ -11,19 +11,6 @@ import type { DataContainer } from '../DataContainer'
 import loadCityContent from './loadCityContent'
 import { ContentLoadCriterion } from '../ContentLoadCriterion'
 import isPeekingRoute from '../selectors/isPeekingRoute'
-
-function * getRootAvailableLanguages (
-  city: string, language: string,
-  loadCriterion: ContentLoadCriterion, dataContainer: DataContainer): Saga<Map<string, string>> {
-  if (loadCriterion.shouldLoadLanguages()) {
-    const languages = yield call(dataContainer.getLanguages, city)
-    return new Map<string, string>(languages
-      .map(languageModel => [languageModel.code, `/${city}/${languageModel.code}`]))
-  }
-
-  // If there are no loaded languages the result is an empty map because we do not have a root category
-  return new Map<string, string>()
-}
 
 /**
  * This fetch corresponds to a peek if the major content city is not equal to the city of the current route.
@@ -53,20 +40,12 @@ function * fetchCategory (dataContainer: DataContainer, action: FetchCategoryAct
         call(dataContainer.getResourceCache, city, language)
       ])
 
-      const rootAvailableLanguages = yield call(getRootAvailableLanguages, city, language, loadCriterion, dataContainer)
+      // Only get languages if we've loaded them, otherwise we're peeking
+      const cityLanguages = loadCriterion.shouldLoadLanguages() ? yield call(dataContainer.getLanguages, city) : []
 
       const push: PushCategoryActionType = {
         type: `PUSH_CATEGORY`,
-        params: {
-          categoriesMap,
-          resourceCache,
-          path,
-          rootAvailableLanguages,
-          depth,
-          key,
-          city,
-          language
-        }
+        params: { categoriesMap, resourceCache, path, cityLanguages, depth, key, city, language }
       }
 
       yield put(push)
