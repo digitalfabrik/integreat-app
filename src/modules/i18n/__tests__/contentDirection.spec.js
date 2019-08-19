4f680c9aaaae377bf75dc816d4b02d6d9fa94059
Steffen Kleinle <klinzo@yahoo.de>
2019-08-19 13:36:00 +0200

    NATIVE-322: Add more contentDirection tests

--- src/modules/i18n/__tests__/contentDirection.spec.js
+++ src/modules/i18n/__tests__/contentDirection.spec.js
@@ -1,15 +1,45 @@
 // @flow
 
-import { isContentDirectionReversalRequired } from '../contentDirection'
-
-jest.mock('react-native', () => {
-  return { I18nManager: { isRTL: true } }
-})
-
 describe('contentDirection', () => {
   describe('isContentDirectionReversalRequired', () => {
+    afterEach(() => {
+      jest.resetModules()
+    })
     it('should return true if supplied language has reverse direction of system language', () => {
-      expect(isContentDirectionReversalRequired('en')).toBe(true)
+      jest.mock('react-native', () => {
+        return { I18nManager: { isRTL: true } }
+      })
+      const contentDirection = require('../contentDirection')
+      expect(contentDirection.isContentDirectionReversalRequired('en')).toBe(true)
+    })
+
+    it('should return false if supplied language has same direction as system language', () => {
+      jest.mock('react-native', () => {
+        return { I18nManager: { isRTL: false } }
+      })
+      const contentDirection = require('../contentDirection')
+      expect(contentDirection.isContentDirectionReversalRequired('en')).toBe(false)
+    })
+  })
+
+  describe('contentDirection', () => {
+    afterEach(() => {
+      jest.resetModules()
+    })
+    it('should return row-reverse if supplied language has reverse direction of system language', () => {
+      jest.mock('react-native', () => {
+        return { I18nManager: { isRTL: true } }
+      })
+      const contentDirection = require('../contentDirection')
+      expect(contentDirection.contentDirection('en')).toBe('row-reverse')
+    })
+
+    it('should return row if supplied language has same direction as system language', () => {
+      jest.mock('react-native', () => {
+        return { I18nManager: { isRTL: false } }
+      })
+      const contentDirection = require('../contentDirection')
+      expect(contentDirection.contentDirection('en')).toBe('row')
     })
   })
 })
