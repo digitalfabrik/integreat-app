36bcd2d384eb8d37ef5c729c41116920a663b120
Michael Markl <marklmichael98@gmail.com>
2019-08-20 11:26:43 +0200

    NATIVE-326: Add first test for ResourceURLFinder

---
+++ src/modules/endpoint/__tests__/ResourceURLFinder.spec.js
@@ -0,0 +1,35 @@
+// @flow
+
+import ResourceURLFinder from '../ResourceURLFinder'
+
+describe('ResourceURLFinder', () => {
+  it('should find urls ending on png,jpg,jpeg,pdf in src and href tags', () => {
+    const finder = new ResourceURLFinder()
+    finder.init()
+    const urls = finder.findResourceUrls(`
+      <a href="https://ex.am/pl1.pdf.exe">Runterladen und zweimal klicken, um pdf zu öffnen</a>
+      <a href="https://ex.am/pl1.png">Hier ne nice png (transparent und so)</a>
+      <a href="https://ex.am/pl1.jpg">Hier 1 jpg</a>
+      <a href="https://ex.am/pl1.jpeg">Das gleiche mit e</a>
+      <a href="https://ex.am/pl1.pdf">Crazy Pdf mit Minigame und allem DrumUndDran!</a>
+      
+      <img src="https://ex.am/pl2.pdf.exe" alt="Krass!" />
+      <img src="https://ex.am/pl2.png" alt="Wow!" />
+      <img src="https://ex.am/pl2.jpg" alt="Ultra!" />
+      <img src="https://ex.am/pl2.jpeg" alt="Meeeeega!" />
+      <img src="https://ex.am/pl2.pdf" alt="Exorbitant!" />
+    `)
+    finder.finalize()
+
+    expect(urls).toEqual(new Set([
+      'https://ex.am/pl1.png',
+      'https://ex.am/pl1.jpg',
+      'https://ex.am/pl1.jpeg',
+      'https://ex.am/pl1.pdf',
+      'https://ex.am/pl2.png',
+      'https://ex.am/pl2.jpg',
+      'https://ex.am/pl2.jpeg',
+      'https://ex.am/pl2.pdf'
+    ]))
+  })
+})
