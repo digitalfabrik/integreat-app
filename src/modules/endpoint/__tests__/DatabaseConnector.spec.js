1f0f0d6642e1642b36ea8c68dc6b7c0ceb99e74e
Simple-codinger <vincentgierisch@gmail.com>
2019-08-21 02:10:06 +0200

    NATIVE-324: Add test for storing cities

--- src/modules/endpoint/__tests__/DatabaseConnector.spec.js
+++ src/modules/endpoint/__tests__/DatabaseConnector.spec.js
@@ -1,28 +1,41 @@
 // @flow
-
+import {CityModel} from '@integreat-app/integreat-api-client'
 import DatabaseConnector from '../DatabaseConnector'
 
+jest.mock('rn-fetch-blob');
+
 describe('databaseConnector', () => {
-  const existsMock = jest.fn()
-  existsMock.mockReturnValueOnce({ then: jest.fn() })
-  jest.mock('rn-fetch-blob', () => {
-    return {
-      DocumentDir: () => {},
-      fetch: () => {},
-      base64: () => {},
-      android: () => {},
-      ios: () => {},
-      config: () => {},
-      session: () => {},
-      fs: () => {},
-      wrap: () => {},
-      polyfill: () => {},
-      JSONStream: () => {}
-    }
+  const dbCon = new DatabaseConnector()
+
+  const MOCK_FILE_INFO = {
+    '/path/to/cities.json': 'console.log("file1 contents");',
+  };
+
+  beforeEach(() => {
+    // Set up some mocked out file info before each test
+    require('rn-fetch-blob').__setMockFiles(MOCK_FILE_INFO);
+  });
+
+  afterEach(() => {
+    jest.resetModules()
   })
 
-  const dbCon = new DatabaseConnector()
-  it('should call clear the city in the app settings', () => {
-    expect(dbCon.getCitiesPath()).toContain('cities')
+  it('should save and read city data', () => {
+    const testCity = new CityModel({
+      name: 'testCityName',
+      code: 'tcc',
+      live: true,
+      eventsEnabled: true,
+      extrasEnabled: true,
+      sortingName: 'testCity',
+      prefix: 'Stadt'
+    })
+
+    //store city
+    dbCon.storeCities([testCity])
+
+    return dbCon.loadCities().then(cities => {
+      expect(cities).toBe([testCity])
+    })
   })
 })
