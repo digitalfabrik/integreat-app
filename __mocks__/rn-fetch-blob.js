8fd733df3d5aefc38541075f57d6cd823e7e330f
Simple-codinger <vincentgierisch@gmail.com>
2019-08-21 02:10:30 +0200

    NATIVE-324: Add mock functions

--- __mocks__/rn-fetch-blob.js
+++ __mocks__/rn-fetch-blob.js
@@ -1,6 +1,24 @@
 const existsMock = jest.fn()
 existsMock.mockReturnValueOnce({ then: jest.fn() })
 
+let mockFiles = Object.create(null);
+
+function __setMockFiles(newMockFiles) {
+  mockFiles = Object.create(null);
+  for (const file in newMockFiles) {
+    const dir = path.dirname(file);
+
+    if (!mockFiles[dir]) {
+      mockFiles[dir] = [];
+    }
+    mockFiles[dir].push(path.basename(file));
+  }
+}
+
+function readdirSync(pathToDirectory){
+  return mockFiles[pathToDirectory];
+}
+
 export default {
   DocumentDir: () => {},
   ImageCache: {
@@ -9,6 +27,8 @@ export default {
     }
   },
   fs: {
+    readdirSync: readdirSync,
+    __setMockFiles: __setMockFiles(),
     exists: existsMock,
     dirs: {
       MainBundleDir: () => {},
