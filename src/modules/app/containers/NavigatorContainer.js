3fb65398c843202610100f285c3a3113532915b1
Michael Markl <marklmichael98@gmail.com>
2019-09-01 13:46:00 +0200

    NATIVE-343: Remove navigator ref

--- src/modules/app/containers/NavigatorContainer.js
+++ src/modules/app/containers/NavigatorContainer.js
@@ -4,38 +4,41 @@ import { type Dispatch } from 'redux'
 import { connect } from 'react-redux'
 import type { StoreActionType } from '../StoreActionType'
 import Navigator from '../components/Navigator'
-import type { NavigationAction } from 'react-navigation'
 import React from 'react'
 import AppSettings from '../../settings/AppSettings'
-import type { NavigateToCityContentParamsType } from '../createNavigateToCityContent'
-import createNavigateToCityContent from '../createNavigateToCityContent'
 
 type DispatchPropsType = {|
   setContentLanguage: (language: string) => void,
-  navigateToCityContent: NavigateToCityContentParamsType => NavigationAction,
+  clearCategory: (key: string) => void,
+  fetchDashboard: ({ city: string, language: string, key: string }) => void,
   fetchCities: (forceRefresh: boolean) => void
 |}
 
 type PropsType = DispatchPropsType
 
-type ContainerStateType = {| waitingForSettings: boolean, language?: ?string, cityCode?: ?string |}
+type ContainerStateType = {| waitingForSettings: boolean, language: ?string, cityCode: ?string |}
 
 class NavigatorContainer extends React.Component<DispatchPropsType, ContainerStateType> {
   constructor () {
     super()
-    this.state = { waitingForSettings: true }
+    this.state = { waitingForSettings: true, language: undefined, cityCode: undefined }
     const appSettings = new AppSettings()
     Promise.all([appSettings.loadSelectedCity(), appSettings.loadContentLanguage()]).then(
       ([cityCode: ?string, language: ?string]) => this.setState({
+
         waitingForSettings: false, cityCode, language
       })
     )
   }
 
   render () {
-    return this.state.waitingForSettings
-      ? null
-      : <Navigator {...this.props} contentLanguage={this.state.language} selectedCity={this.state.cityCode} />
+    if (this.state.waitingForSettings) {
+      return null
+    }
+    if (!this.state.language) {
+      throw new Error('ContentLanguage has not been set correctly')
+    }
+    return <Navigator {...this.props} contentLanguage={this.state.language} selectedCity={this.state.cityCode} />
   }
 }
 
@@ -49,7 +52,21 @@ const mapDispatchToProps = (dispatch: Dispatch<StoreActionType>): DispatchPropsT
   fetchCities: (forceRefresh: boolean) => {
     dispatch({ type: 'FETCH_CITIES', params: { forceRefresh } })
   },
-  navigateToCityContent: createNavigateToCityContent(dispatch)
+  clearCategory: (key: string) => {
+    dispatch({ type: 'CLEAR_CATEGORY', params: { key } })
+  },
+  fetchDashboard: ({ city, language, key }: { city: string, language: string, key: string }) => {
+    dispatch({
+      type: 'FETCH_CATEGORY', params: {
+        city: city,
+        language: language,
+        path: `/${city}/${language}`,
+        depth: 2,
+        key: key,
+        criterion: { forceUpdate: false, shouldRefreshResources: true }
+      }
+    })
+  }
 })
 
 export default connect<PropsType, {||}, _, _, _, _>(undefined, mapDispatchToProps)(NavigatorContainer)
