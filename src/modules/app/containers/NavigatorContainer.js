a8546b98a5cbd6ab4ff8571a792e3659e7ef8a25
Michael Markl <marklmichael98@gmail.com>
2019-09-01 15:11:26 +0200

    NATIVE-343: Extract createNavigationContainer from Navigator

--- src/modules/app/containers/NavigatorContainer.js
+++ src/modules/app/containers/NavigatorContainer.js
@@ -4,8 +4,6 @@ import { type Dispatch } from 'redux'
 import { connect } from 'react-redux'
 import type { StoreActionType } from '../StoreActionType'
 import Navigator from '../components/Navigator'
-import React from 'react'
-import AppSettings from '../../settings/AppSettings'
 
 type DispatchPropsType = {|
   setContentLanguage: (language: string) => void,
@@ -15,32 +13,6 @@ type DispatchPropsType = {|
 |}
 
 type PropsType = DispatchPropsType
-
-type ContainerStateType = {| waitingForSettings: boolean, language: ?string, cityCode: ?string |}
-
-class NavigatorContainer extends React.Component<DispatchPropsType, ContainerStateType> {
-  constructor () {
-    super()
-    this.state = { waitingForSettings: true, language: undefined, cityCode: undefined }
-    const appSettings = new AppSettings()
-    Promise.all([appSettings.loadSelectedCity(), appSettings.loadContentLanguage()]).then(
-      ([cityCode: ?string, language: ?string]) => this.setState({
-
-        waitingForSettings: false, cityCode, language
-      })
-    )
-  }
-
-  render () {
-    if (this.state.waitingForSettings) {
-      return null
-    }
-    if (!this.state.language) {
-      throw new Error('ContentLanguage has not been set correctly')
-    }
-    return <Navigator {...this.props} contentLanguage={this.state.language} selectedCity={this.state.cityCode} />
-  }
-}
 
 const mapDispatchToProps = (dispatch: Dispatch<StoreActionType>): DispatchPropsType => ({
   setContentLanguage: (language: string) => {
@@ -57,7 +29,8 @@ const mapDispatchToProps = (dispatch: Dispatch<StoreActionType>): DispatchPropsT
   },
   fetchDashboard: ({ city, language, key }: { city: string, language: string, key: string }) => {
     dispatch({
-      type: 'FETCH_CATEGORY', params: {
+      type: 'FETCH_CATEGORY',
+      params: {
         city: city,
         language: language,
         path: `/${city}/${language}`,
@@ -69,4 +42,4 @@ const mapDispatchToProps = (dispatch: Dispatch<StoreActionType>): DispatchPropsT
   }
 })
 
-export default connect<PropsType, {||}, _, _, _, _>(undefined, mapDispatchToProps)(NavigatorContainer)
+export default connect<PropsType, {||}, _, _, _, _>(undefined, mapDispatchToProps)(Navigator)
