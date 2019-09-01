6e24eb1b05b9d254a857148b3a4af2a7257447c4
Michael Markl <marklmichael98@gmail.com>
2019-09-01 15:11:26 +0200

    NATIVE-343: Extract createNavigationContainer from Navigator

--- src/modules/app/components/Navigator.js
+++ src/modules/app/components/Navigator.js
@@ -1,134 +1,49 @@
 // @flow
 
 import * as React from 'react'
-import type {
-  HeaderProps,
-  NavigationComponent,
-  NavigationContainer,
-  NavigationRouteConfig,
-  NavigationState,
-  StackNavigatorConfig
-} from 'react-navigation'
-import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation'
-import CategoriesContainer from '../../../routes/categories/containers/CategoriesContainer'
-import LandingContainer from '../../../routes/landing/containers/LandingContainer'
-import DashboardContainer from '../../../routes/dashboard/containers/DashboardContainer'
-import withLayout from '../../layout/hocs/withLayout'
-import HeaderContainer from '../../layout/containers/HeaderContainer'
-import PDFViewModal from '../../../routes/pdf/components/PDFViewModal'
-import ImageViewModal from '../../../routes/image/components/ImageViewModal'
-import ChangeLanguageModalContainer from '../../../routes/language/containers/ChangeLanguageModalContainer'
-import TransparentHeaderContainer from '../../layout/containers/TransparentHeaderContainer'
-import ExtrasContainer from '../../../routes/extras/containers/ExtrasContainer'
-import WohnenExtraContainer from '../../../routes/wohnen/containers/WohnenExtraContainer'
-import SprungbrettExtraContainer from '../../../routes/sprungbrett/containers/SprungbrettExtraContainer'
-import { EXTERNAL_EXTRA_ROUTE, SPRUNGBRETT_ROUTE, WOHNEN_ROUTE } from '../../../routes/extras/constants'
-import EventsContainer from '../../../routes/events/containers/EventsContainer'
-import SearchModalContainer from '../../../routes/search/containers/SearchModalContainer'
-import ExternalExtraContainer from '../../../routes/external-extra/containers/ExternalExtraContainer'
-import SettingsContainer from '../../../routes/settings/container/SettingsContainer'
-import FeedbackModalContainer from '../../../routes/feedback/containers/FeedbackModalContainer'
+import type { NavigationContainer } from 'react-navigation'
 import { generateKey } from '../generateRouteKey'
-
-const LayoutedDashboardContainer = withLayout(DashboardContainer)
-const LayoutedCategoriesContainer = withLayout(CategoriesContainer)
-
-const createNavigationRouteConfig = (Component: NavigationComponent, header = null): NavigationRouteConfig => ({
-  screen: Component,
-  navigationOptions: {
-    header: header
-  }
-})
-
-const transparentHeader = (headerProps: HeaderProps) =>
-  <TransparentHeaderContainer scene={headerProps.scene} scenes={headerProps.scenes} />
-
-const defaultHeader = (headerProps: HeaderProps) =>
-  <HeaderContainer scene={headerProps.scene} scenes={headerProps.scenes} />
-
-/*
- The app behaves pretty weird when you have a StackNavigator -> SwitchNavigator -> StackNavigator
- Therefore I removed the StackNavigator in the root and moved the routes to the other StackNavigator.
- We can not set the modal prop, but this is good enough atm.
- */
-const createCityContentStack = (config?: StackNavigatorConfig) => createStackNavigator(
-  {
-    'Dashboard': createNavigationRouteConfig(LayoutedDashboardContainer, defaultHeader),
-    'Categories': createNavigationRouteConfig(LayoutedCategoriesContainer, defaultHeader),
-    'Extras': createNavigationRouteConfig(ExtrasContainer, defaultHeader),
-    [WOHNEN_ROUTE]: createNavigationRouteConfig(WohnenExtraContainer, defaultHeader),
-    [SPRUNGBRETT_ROUTE]: createNavigationRouteConfig(SprungbrettExtraContainer, defaultHeader),
-    [EXTERNAL_EXTRA_ROUTE]: createNavigationRouteConfig(ExternalExtraContainer, defaultHeader),
-    'Events': createNavigationRouteConfig( // $FlowFixMe We don't know why this fails.
-      EventsContainer, defaultHeader
-    ),
-    'Settings': createNavigationRouteConfig(SettingsContainer, defaultHeader),
-    'ChangeLanguageModal': createNavigationRouteConfig(ChangeLanguageModalContainer),
-    'SearchModal': createNavigationRouteConfig(SearchModalContainer),
-    'ImageViewModal': createNavigationRouteConfig(ImageViewModal, transparentHeader),
-    'PDFViewModal': createNavigationRouteConfig(PDFViewModal, transparentHeader),
-    'FeedbackModal': createNavigationRouteConfig(FeedbackModalContainer, transparentHeader)
-  },
-  config
-)
-
-const createMainSwitch = (
-  selectedCity: ?string,
-  contentLanguage: string,
-  clearCategory: (key: string) => void,
-  fetchDashboard: ({ city: string, language: string, key: string }) => void
-) => {
-  if (!selectedCity) {
-    return createSwitchNavigator({
-      'Landing': LandingContainer,
-      'CityContent': createCityContentStack({})
-    }, { initialRouteName: 'Landing' })
-  } else {
-    const key = generateKey()
-    const cityContentStack = createCityContentStack(
-      {
-        initialRouteName: 'Dashboard',
-        initialRouteKey: key,
-        initialRouteParams: {
-          onRouteClose: () => {},
-          sharePath: `/${selectedCity}/${contentLanguage}`
-        }
-      })
-    fetchDashboard({ city: selectedCity, language: contentLanguage, key })
-
-    return createSwitchNavigator({
-      'Landing': LandingContainer,
-      'CityContent': cityContentStack
-    }, { initialRouteName: 'CityContent' })
-  }
-}
+import AppSettings from '../../settings/AppSettings'
+import createNavigationContainer from '../createNavigationContainer'
 
 type PropsType = {|
   setContentLanguage: (language: string) => void,
   clearCategory: (key: string) => void,
   fetchDashboard: ({ city: string, language: string, key: string }) => void,
-  fetchCities: (forceRefresh: boolean) => void,
-  selectedCity: ?string,
-  contentLanguage: string
+  fetchCities: (forceRefresh: boolean) => void
 |}
 
-class Navigator extends React.Component<PropsType> {
-  appContainer: NavigationContainer<NavigationState, {}, {}>
+type StateType = {| waitingForSettings: boolean |}
 
-  constructor (props: PropsType) {
-    super(props)
-    this.appContainer = createAppContainer(
-      createMainSwitch(props.selectedCity, props.contentLanguage, props.clearCategory, props.fetchDashboard)
-    )
-  }
+class Navigator extends React.Component<PropsType, StateType> {
+  appContainer: NavigationContainer<*, *, *>
+  state = { waitingForSettings: true }
 
-  componentDidMount () {
-    this.props.fetchCities(false)
+  async componentDidMount () {
+    const { fetchDashboard, clearCategory } = this.props
+    const appSettings = new AppSettings()
+    const [cityCode, language] = await Promise.all([appSettings.loadSelectedCity(), appSettings.loadContentLanguage()])
+    if (!language) {
+      throw Error('The contentLanguage has not been set correctly by I18nProvider!')
+    }
+    if (cityCode) {
+      const key = generateKey()
+      this.appContainer = createNavigationContainer({
+        initialRouteName: 'CityContent', cityCode, language, clearCategory, key
+      })
+      fetchDashboard({ city: cityCode, language, key })
+    } else {
+      this.appContainer = createNavigationContainer({ initialRouteName: 'Landing' })
+    }
+
+    // see https://github.com/yannickcr/eslint-plugin-react/issues/1110
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({ waitingForSettings: false })
   }
 
   render () {
     const AppContainer = this.appContainer
-    return <AppContainer />
+    return AppContainer ? <AppContainer /> : null
   }
 }
 
---
+++ src/modules/app/createNavigationContainer.js
@@ -0,0 +1,103 @@
+// @flow
+
+import withLayout from '../layout/hocs/withLayout'
+import DashboardContainer from '../../routes/dashboard/containers/DashboardContainer'
+import CategoriesContainer from '../../routes/categories/containers/CategoriesContainer'
+import {
+  createAppContainer,
+  createStackNavigator,
+  createSwitchNavigator,
+  type HeaderProps,
+  type NavigationComponent,
+  type NavigationContainer,
+  type NavigationRouteConfig,
+  type NavigationRouteConfigMap
+} from 'react-navigation'
+import TransparentHeaderContainer from '../layout/containers/TransparentHeaderContainer'
+import HeaderContainer from '../layout/containers/HeaderContainer'
+import ExtrasContainer from '../../routes/extras/containers/ExtrasContainer'
+import { EXTERNAL_EXTRA_ROUTE, SPRUNGBRETT_ROUTE, WOHNEN_ROUTE } from '../../routes/extras/constants'
+import WohnenExtraContainer from '../../routes/wohnen/containers/WohnenExtraContainer'
+import SprungbrettExtraContainer from '../../routes/sprungbrett/containers/SprungbrettExtraContainer'
+import ExternalExtraContainer from '../../routes/external-extra/containers/ExternalExtraContainer'
+import EventsContainer from '../../routes/events/containers/EventsContainer'
+import SettingsContainer from '../../routes/settings/container/SettingsContainer'
+import ChangeLanguageModalContainer from '../../routes/language/containers/ChangeLanguageModalContainer'
+import SearchModalContainer from '../../routes/search/containers/SearchModalContainer'
+import ImageViewModal from '../../routes/image/components/ImageViewModal'
+import PDFViewModal from '../../routes/pdf/components/PDFViewModal'
+import FeedbackModalContainer from '../../routes/feedback/containers/FeedbackModalContainer'
+import LandingContainer from '../../routes/landing/containers/LandingContainer'
+import React from 'react'
+
+const LayoutedDashboardContainer = withLayout(DashboardContainer)
+const LayoutedCategoriesContainer = withLayout(CategoriesContainer)
+
+const createNavigationRouteConfig = (Component: NavigationComponent, header = null): NavigationRouteConfig => ({
+  screen: Component,
+  navigationOptions: {
+    header: header
+  }
+})
+
+const transparentHeader = (headerProps: HeaderProps) =>
+  <TransparentHeaderContainer scene={headerProps.scene} scenes={headerProps.scenes} />
+
+const defaultHeader = (headerProps: HeaderProps) =>
+  <HeaderContainer scene={headerProps.scene} scenes={headerProps.scenes} />
+
+const cityContentRouteConfigMap: NavigationRouteConfigMap = {
+  'Dashboard': createNavigationRouteConfig(LayoutedDashboardContainer, defaultHeader),
+  'Categories': createNavigationRouteConfig(LayoutedCategoriesContainer, defaultHeader),
+  'Extras': createNavigationRouteConfig(ExtrasContainer, defaultHeader),
+  [WOHNEN_ROUTE]: createNavigationRouteConfig(WohnenExtraContainer, defaultHeader),
+  [SPRUNGBRETT_ROUTE]: createNavigationRouteConfig(SprungbrettExtraContainer, defaultHeader),
+  [EXTERNAL_EXTRA_ROUTE]: createNavigationRouteConfig(ExternalExtraContainer, defaultHeader),
+  'Events': createNavigationRouteConfig( // $FlowFixMe We don't know why this fails.
+    EventsContainer, defaultHeader
+  ),
+  'Settings': createNavigationRouteConfig(SettingsContainer, defaultHeader),
+  'ChangeLanguageModal': createNavigationRouteConfig(ChangeLanguageModalContainer),
+  'SearchModal': createNavigationRouteConfig(SearchModalContainer),
+  'ImageViewModal': createNavigationRouteConfig(ImageViewModal, transparentHeader),
+  'PDFViewModal': createNavigationRouteConfig(PDFViewModal, transparentHeader),
+  'FeedbackModal': createNavigationRouteConfig(FeedbackModalContainer, transparentHeader)
+}
+
+export type CreateNavigationContainerParamsType =
+  {| initialRouteName: 'Landing' |} |
+  {|
+    initialRouteName: 'CityContent',
+    cityCode: string,
+    language: string,
+    clearCategory: (key: string) => void,
+    key: string
+  |}
+
+const createCityContentNavigator = (params: CreateNavigationContainerParamsType) => {
+  if (params.initialRouteName === 'Landing') {
+    return createStackNavigator(cityContentRouteConfigMap)
+  } else {
+    const { clearCategory, key, cityCode, language } = params
+    return createStackNavigator(cityContentRouteConfigMap,
+      {
+        initialRouteName: 'Dashboard',
+        initialRouteKey: key,
+        initialRouteParams: {
+          onRouteClose: () => clearCategory(key),
+          sharePath: `/${cityCode}/${language}`
+        }
+      })
+  }
+}
+
+const createNavigationContainer = (params: CreateNavigationContainerParamsType): NavigationContainer<*, *, *> => {
+  const cityContentNavigator = createCityContentNavigator(params)
+  return createAppContainer(
+    createSwitchNavigator({
+      'Landing': LandingContainer,
+      'CityContent': cityContentNavigator
+    }, { initialRouteName: params.initialRouteName }))
+}
+
+export default createNavigationContainer
