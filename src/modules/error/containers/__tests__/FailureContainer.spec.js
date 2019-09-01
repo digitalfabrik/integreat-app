217159c8364612ea56f0f8eeb21bb31a9efec9a1
Maximilian Ammann <max@maxammann.org>
2019-09-01 17:37:11 +0200

    NATIVE-117: Add two ways to test a component which uses hocs

--- src/modules/error/containers/__tests__/FailureContainer.spec.js
+++ src/modules/error/containers/__tests__/FailureContainer.spec.js
@@ -1,40 +1,48 @@
 // @flow
 
-import { render } from 'react-native-testing-library'
+import { render } from '@testing-library/react-native'
 import * as React from 'react'
 import FailureContainer from '../FailureContainer'
 import AsyncStorage from '@react-native-community/async-storage'
-import MockComponent from './MockComponent'
+import Failure from '../../components/Failure'
+import { brightTheme } from '../../../theme/constants/theme'
 
 const mockAsyncStorage: AsyncStorage = require('@react-native-community/async-storage/jest/async-storage-mock').default
 jest.mock('@react-native-community/async-storage', () => mockAsyncStorage)
-jest.mock('../../components/Failure', () => require('./MockComponent').default)
-jest.mock('../../../theme/hocs/withTheme', () => () => (Component) => Component)
-jest.mock('react-i18next', () => ({translate: () => (Component) => () => <Component t={key => key} />}))
+
+// jest.mock('../../components/Failure', () => jest.fn(props => null))
+jest.mock('react-i18next')
+jest.mock('../../../theme/hocs/withTheme')
 
 describe('FailureContainer', () => {
   describe('withTheme()(translate())', () => {
-    it('should pass props', () => {
-      // render(<I18nProvider setContentLanguage={() => {}}><FailureContainer
-      //   testID={'test'} /></I18nProvider>)
-
-      // expect(MockComponent).toBeCalledWith({
-      //   t: expect.anything(),
-      //   theme: expect.anything(),
-      //   testID: expect.anything(),
-      //   tReady: expect.anything(),
-      //   reportNS: undefined,
-      //   lng: expect.anything(),
-      //   i18nOptions: expect.anything(),
-      //   i18n: expect.anything(),
-      //   defaultNS: undefined
-      // }, {})
-
+    it('should give props to inner component', () => {
       render(<FailureContainer />)
 
-      expect(MockComponent).toBeCalledWith({
-        t: expect.anything()
-      }, {})
+      const context = {}
+      expect(Failure).toBeCalledWith(expect.objectContaining({
+        t: expect.anything(),
+        theme: expect.anything()
+      }), context)
     })
+  })
+
+  it('should render inner component correctly without passing props explicitly', () => {
+    jest.unmock('../../components/Failure')
+    const t = key => key
+    const tryAgain = () => {}
+    // const tryAgain = undefined
+
+    const error = new Error()
+    const { container: containerFailure, debug: debugFailure, asJSON: asJSONFailure } =
+      render(<Failure theme={brightTheme} error={error} tryAgain={tryAgain} t={t} />)
+    const { container: containerFailureContainer, debug: debugFailureContainer, asJSON: asJSONFailureContainer } =
+      render(<FailureContainer error={error} tryAgain={tryAgain} />)
+
+    debugFailure()
+    // debugFailureContainer()
+
+    expect(containerFailure._fiber.stateNode).not.toEqual(containerFailureContainer._fiber.stateNode)
+    expect(asJSONFailure()).toEqual(asJSONFailureContainer())
   })
 })
