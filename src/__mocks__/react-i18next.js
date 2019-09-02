875f49aa857d46f3231519a8357c1726dd3b4aae
Maximilian Ammann <max@maxammann.org>
2019-09-02 11:41:34 +0200

    NATIVE-117: Start to write a proper react-i18next mock

--- src/__mocks__/react-i18next.js
+++ src/__mocks__/react-i18next.js
@@ -2,4 +2,16 @@
 
 import * as React from 'react'
 
-export const translate = () => Component => props => <Component {...props} t={key => key} />
+const reactI18next = jest.requireActual('react-i18next')
+
+reactI18next.translate = () => Component => props => <Component {...props} t={key => key} />
+
+reactI18next.I18nextProvider = null
+// fixme clean this mock up
+
+module.exports = {
+  translate: () => Component => props => <Component {...props} t={key => key} />,
+  I18nextProvider: props => <>{props.children}</>,
+  reactI18nextModule: reactI18next.reactI18nextModule,
+  default: reactI18next
+}
