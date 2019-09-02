ff3dde7baac8d0e72e46fc41b633b73cf205740f
Maximilian Ammann <max@maxammann.org>
2019-09-02 11:38:57 +0200

    NATIVE-117: Fix tests to use new testing lib

--- src/modules/i18n/__tests__/watchClearCity.spec.js
+++ src/modules/i18n/__tests__/watchClearCity.spec.js
@@ -4,8 +4,6 @@ import { runSaga } from 'redux-saga'
 import { clearCity } from '../watchClearCity'
 import AppSettings from '../../settings/AppSettings'
 
-jest.mock('@react-native-community/async-storage', () => require('@react-native-community/async-storage/jest/async-storage-mock'))
-
 describe('watchClearCity', () => {
   it('should clear selected city of app settings', async () => {
     const appSettings = new AppSettings()
