8b6340173401574140390881e3bbb599074db620
Maximilian Ammann <max@maxammann.org>
2019-09-02 11:45:02 +0200

    NATIVE-117: Remove enzyme

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,3 @@
-// setup file
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
-
 // Setup fetch mock
 global.fetch = require('jest-fetch-mock')
 
