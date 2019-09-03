157a95a2000d003bc56217d8112e0e440833bba2
Maximilian Ammann <max@maxammann.org>
2019-09-03 18:00:27 +0200

    NATIVE-117: Cleanup react-i18next mock

--- src/__mocks__/react-i18next.js
+++ src/__mocks__/react-i18next.js
@@ -1,17 +1,22 @@
 // @flow
 
 import * as React from 'react'
+import typeof I18nType from 'i18next'
 
-const reactI18next = jest.requireActual('react-i18next')
+const realModule = jest.requireActual('react-i18next')
 
-reactI18next.translate = () => Component => props => <Component {...props} t={key => key} />
-
-reactI18next.I18nextProvider = null
-// fixme clean this mock up
+const translate = <S: {||}> (namespace: string) => (
+  Component: React.AbstractComponent<{| ...S, t: string => string |}>
+): React.AbstractComponent<S> => {
+  return class extends React.Component<S> {
+    render () {
+      return <Component {...this.props} t={key => key} />
+    }
+  }
+}
 
 module.exports = {
-  translate: () => Component => props => <Component {...props} t={key => key} />,
-  I18nextProvider: props => <>{props.children}</>,
-  reactI18nextModule: reactI18next.reactI18nextModule,
-  default: reactI18next
+  translate,
+  I18nextProvider: (props: { i18n: I18nType, children: React.Node }) => <>{props.children}</>,
+  reactI18nextModule: realModule.reactI18nextModule
 }
