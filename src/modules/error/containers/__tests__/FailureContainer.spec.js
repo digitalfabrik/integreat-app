50616a7798c009f6ed40eb74acf56c3e8df8b478
Maximilian Ammann <max@maxammann.org>
2019-09-03 18:04:21 +0200

    NATIVE-117: Add unit and integration test for container and component

--- src/modules/error/containers/__tests__/FailureContainer.spec.js
+++ src/modules/error/containers/__tests__/FailureContainer.spec.js
@@ -1,39 +1,25 @@
 // @flow
 
-import { render } from '@testing-library/react-native'
+import { render, fireEvent } from '@testing-library/react-native'
 import * as React from 'react'
-import FailureContainer from '../FailureContainer'
-import Failure from '../../components/Failure'
-import { brightTheme } from '../../../theme/constants/theme'
 import TestRenderer from 'react-test-renderer'
+import typeof FailureContainer from '../FailureContainer'
 
-jest.mock('../../components/Failure', () => jest.fn(props => null))
-// jest.mock('../../components/Failure', () => {
-//  const Component = jest.fn(props => null)
-//  return props => <Component {...props} testID='some-id' />
-// })
 jest.mock('react-i18next')
 jest.mock('../../../theme/hocs/withTheme')
 
 describe('FailureContainer', () => {
-  describe('withTheme()(translate())', () => {
-    it('should give props to inner component', () => {
-      render(<FailureContainer />)
-
-      const context = {}
-      expect(Failure).toBeCalledWith(expect.objectContaining({
-        t: expect.anything(),
-        theme: expect.anything()
-      }), context)
-
-      expect(Failure).toHaveBeenCalledTimes(1)
-    })
+  beforeEach(() => {
+    jest.resetModules()
   })
 
-  it('should give props to inner component (2)', () => {
-    const rendered = TestRenderer.create(<FailureContainer />)
+  it('should give props to inner component', () => {
+    const FailureMock = () => null
+    jest.doMock('../../components/Failure', () => FailureMock)
+    const FailureContainerMock: FailureContainer = require('../FailureContainer').default
 
-    const instance = rendered.root.findByType(Failure)
+    const rendered = TestRenderer.create(<FailureContainerMock />)
+    const instance = rendered.root.findByType(FailureMock)
     expect(instance.props).toEqual({
       t: expect.anything(),
       theme: expect.anything()
@@ -41,21 +27,12 @@ describe('FailureContainer', () => {
   })
 
   it('should render inner component correctly without passing props explicitly', () => {
-    // jest.unmock('../../components/Failure')
-    const t = key => key
-    const tryAgain = () => {}
-    // const tryAgain = undefined
+    jest.dontMock('../../components/Failure')
+    const FailureContainer = require('../FailureContainer').default
 
-    const error = new Error()
-    const { container: containerFailure, debug: debugFailure, asJSON: asJSONFailure } =
-      render(<Failure theme={brightTheme} error={error} tryAgain={tryAgain} t={t} />)
-    const { container: containerFailureContainer, debug: debugFailureContainer, asJSON: asJSONFailureContainer } =
-      render(<FailureContainer error={error} tryAgain={tryAgain} />)
-
-    debugFailure()
-    // debugFailureContainer()
-
-    expect(containerFailure._fiber.stateNode).not.toEqual(containerFailureContainer._fiber.stateNode)
-    expect(asJSONFailure()).toEqual(asJSONFailureContainer())
+    const tryAgain = jest.fn()
+    const { getByTestId } = render(<FailureContainer error={new Error()} tryAgain={tryAgain} />)
+    fireEvent.press(getByTestId('button-tryAgain'))
+    expect(tryAgain).toHaveBeenCalled()
   })
 })
