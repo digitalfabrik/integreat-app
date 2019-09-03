0c4178d38a4715df9692d6f257ed3803521c9864
Maximilian Ammann <max@maxammann.org>
2019-09-03 18:03:26 +0200

    NATIVE-117: Add missing mocks

--- src/modules/i18n/__tests__/watchClearCity.spec.js
+++ src/modules/i18n/__tests__/watchClearCity.spec.js
@@ -4,6 +4,8 @@ import { runSaga } from 'redux-saga'
 import { clearCity } from '../watchClearCity'
 import AppSettings from '../../settings/AppSettings'
 
+jest.mock('@react-native-community/async-storage')
+
 describe('watchClearCity', () => {
   it('should clear selected city of app settings', async () => {
     const appSettings = new AppSettings()
