a10b0c27292c05c6def081c0d4124e1f02c24e47
Maximilian Ammann <max@maxammann.org>
2019-09-08 12:59:23 +0200

    NATIVE-117: Add comment to toHaveDirection

--- src/testing/utils.js
+++ src/testing/utils.js
@@ -1,5 +1,12 @@
 // @flow
 
+/**
+ * Checks whether the resulting components all have direction information. This allows to test whether the component
+ * is internationalizable.
+ *
+ * @param received The received result
+ * @returns {{pass: boolean, message: (function(): string)}} A jest.extend compatible matcher
+ */
 export const toHaveDirection = (received: Array<ReactTestInstance>) => {
   if (received.every(instance => instance.props.style.some(style => style.flexDirection === 'row'))) {
     return {
