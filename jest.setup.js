ae37856fc9274079b5c5474d756ddf7eb1125897
Maximilian Ammann <max@maxammann.org>
2019-09-08 13:02:03 +0200

    NATIVE-117: Cleanup walkDir

--- jest.setup.js
+++ jest.setup.js
@@ -21,9 +21,9 @@ jest.mock('rn-fetch-blob')
 
 function walkDir (dir, callback) {
   fs.readdirSync(dir).forEach(f => {
-    const dirPath = path.join(dir, f)
-    const isDirectory = fs.statSync(dirPath).isDirectory()
-    isDirectory ? walkDir(dirPath, callback) : callback(path.join(dir, f))
+    const filePath = path.join(dir, f)
+    const isDirectory = fs.statSync(filePath).isDirectory()
+    isDirectory ? walkDir(filePath, callback) : callback(filePath)
   })
 }
 
