b9caf7d1ac0a4ff9b729526a98bdbd85bcc46c56
Maximilian Ammann <max@maxammann.org>
2019-09-10 11:08:54 +0200

    NATIVE-117: Add unit test for I18nProviderContainer

--- src/modules/i18n/containers/__tests__/I18nProviderContainer.spec.js
+++ src/modules/i18n/containers/__tests__/I18nProviderContainer.spec.js
@@ -2,10 +2,11 @@
 
 import configureMockStore from 'redux-mock-store'
 import { Provider } from 'react-redux'
-import I18nProviderContainer from '../I18nProviderContainer'
 import * as React from 'react'
 import waitForExpect from 'wait-for-expect'
 import { render } from '@testing-library/react-native'
+import TestRenderer from 'react-test-renderer'
+import typeof I18nProviderContainer from '../I18nProviderContainer'
 
 jest.mock('@react-native-community/async-storage')
 jest.mock('../../../i18n/LanguageDetector')
@@ -13,7 +14,29 @@ jest.mock('../../../i18n/LanguageDetector')
 const mockStore = configureMockStore()
 
 describe('I18nProviderContainer', () => {
-  it('should pass the right props', async () => {
+  beforeEach(() => {
+    jest.resetModules()
+  })
+
+  it('should pass props to inner component', () => {
+    const I18nProviderMock = () => null
+    jest.doMock('../../components/I18nProvider', () => I18nProviderMock)
+    const I18nProviderContainerMock: I18nProviderContainer = require('../I18nProviderContainer').default
+
+    const store = mockStore({})
+    const rendered = TestRenderer.create(<Provider store={store}>
+      <I18nProviderContainerMock />
+    </Provider>)
+    const instance = rendered.root.findByType(I18nProviderMock)
+    expect(instance.props).toEqual({
+      setContentLanguage: expect.anything()
+    })
+  })
+
+  it('should trigger actions without passing props explicitly', async () => {
+    jest.dontMock('../../components/I18nProvider')
+    const I18nProviderContainer = require('../I18nProviderContainer').default
+
     const store = mockStore({})
     render(
       <Provider store={store}>
