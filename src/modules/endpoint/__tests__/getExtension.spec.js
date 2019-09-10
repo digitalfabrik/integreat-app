4071234b29fb31757c9425caeab0b73f64dbc075
Maximilian Ammann <max@maxammann.org>
2019-09-10 22:40:33 +0200

    NATIVE-327: Improve getExtension to use proper URL

--- src/modules/endpoint/__tests__/getExtension.spec.js
+++ src/modules/endpoint/__tests__/getExtension.spec.js
@@ -6,6 +6,9 @@ describe('getExtension', () => {
   it('should return the extension of a regular file-url', () => {
     expect(getExtension('https://ex.am/p.l/thumbnail.png')).toBe('png')
   })
+  it('should throw if pathname has no extension', () => {
+    expect(() => getExtension('https://ex.am/')).toThrowError('The URL does not have an extension!')
+  })
   it('should ignore any trailing query string', () => {
     expect(getExtension('https://ex.am/p.l/thumbnail.png?size=med')).toBe('png')
   })
@@ -16,5 +19,9 @@ describe('getExtension', () => {
     expect(getExtension('https://ex.am/p.l/thumbnail.png#what-ever?this-is')).toBe('png')
     expect(getExtension('https://ex.am/p.l/thumbnail.png?what-ever#this-is')).toBe('png')
     expect(getExtension('https://ex.am/p.l#serjkd/thumbnail.png?what-ever#this-is')).toBe('l')
+  })
+  it('should throw if there is no extension', () => {
+    expect(() => getExtension('https://ex.am/p.l/thumbnail')).toThrow('The URL does not have an extension!')
+    expect(() => getExtension('thumbnail')).toThrow('Invalid URL')
   })
 })
--- src/modules/endpoint/getExtension.js
+++ src/modules/endpoint/getExtension.js
@@ -1,6 +1,20 @@
 // @flow
 
-export default (url: string) => {
-  const noHashOrQuery = url.split('?')[0].split('#')[0]
-  return noHashOrQuery.substring(noHashOrQuery.lastIndexOf('.') + 1)
+import { last } from 'lodash'
+
+export default (urlString: string) => {
+  const url = new URL(urlString)
+  const pathname = url.pathname
+
+  const lastPath = last(pathname.split('/'))
+
+  if (lastPath === undefined) {
+    throw new Error('The URL does not have a pathname!')
+  }
+
+  const index = lastPath.lastIndexOf('.')
+  if (index === -1) {
+    throw new Error('The URL does not have an extension!')
+  }
+  return lastPath.substring(index + 1)
 }
