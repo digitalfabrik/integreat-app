d464842d82db728817ccc3c95cccae639a73a83c
Maximilian Ammann <max@maxammann.org>
2019-09-17 16:05:55 +0200

    NATIVE-327: Use URL polyfill

--- src/modules/endpoint/__tests__/getExtension.spec.js
+++ src/modules/endpoint/__tests__/getExtension.spec.js
@@ -6,9 +6,6 @@ describe('getExtension', () => {
   it('should return the extension of a regular file-url', () => {
     expect(getExtension('https://ex.am/p.l/thumbnail.png')).toBe('png')
   })
-  it('should throw if pathname has no extension', () => {
-    expect(() => getExtension('https://ex.am/')).toThrowError('The URL does not have an extension!')
-  })
   it('should ignore any trailing query string', () => {
     expect(getExtension('https://ex.am/p.l/thumbnail.png?size=med')).toBe('png')
   })
@@ -20,8 +17,13 @@ describe('getExtension', () => {
     expect(getExtension('https://ex.am/p.l/thumbnail.png?what-ever#this-is')).toBe('png')
     expect(getExtension('https://ex.am/p.l#serjkd/thumbnail.png?what-ever#this-is')).toBe('l')
   })
-  it('should throw if there is no extension', () => {
-    expect(() => getExtension('https://ex.am/p.l/thumbnail')).toThrow('The URL does not have an extension!')
+  it('should throw if it is no valid url', () => {
     expect(() => getExtension('thumbnail')).toThrow('Invalid URL')
+  })
+  it('should return empty string if there is no extension', () => {
+    expect(getExtension('https://ex.am/p.l/thumbnail')).toBe('')
+  })
+  it('should return empty string if there is no pathname', () => {
+    expect(getExtension('https://ex.am/')).toBe('')
   })
 })
