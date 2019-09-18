4be9593943cff02c6898a729f3f6346f90854d2f
Maximilian Ammann <max@maxammann.org>
2019-09-18 15:08:56 +0200

    NATIVE-328: Use FetchMap from builder

--- src/modules/endpoint/sagas/__tests__/fetchResourceCache.spec.js
+++ src/modules/endpoint/sagas/__tests__/fetchResourceCache.spec.js
@@ -1,25 +1,15 @@
 // @flow
 
 import { expectSaga } from 'redux-saga-test-plan'
-import fetchResourceCache, { type FetchMapType } from '../fetchResourceCache'
+import fetchResourceCache from '../fetchResourceCache'
 import DefaultDataContainer from '../../DefaultDataContainer'
 import RNFetchBlob from '../../../../__mocks__/rn-fetch-blob'
 import CategoriesMapModelBuilder from '../../../../testing/builder/CategoriesMapModelBuilder'
 import { transform, forEach } from 'lodash'
-import type { FileCacheStateType } from '../../../app/StateType'
 import Moment from 'moment'
 
 jest.mock('../../../fetcher/FetcherModule')
 jest.mock('rn-fetch-blob')
-
-const createFetchMap = (resources: { [path: string]: FileCacheStateType }): FetchMapType => {
-  return transform(resources, (result, value, path) => {
-    forEach(value, (value, url) => {
-      result[value.filePath] = [url, path, value.hash]
-    })
-    return result
-  }, {})
-}
 
 describe('fetchResourceCache', () => {
   beforeEach(() => {
@@ -36,7 +26,7 @@ describe('fetchResourceCache', () => {
 
     const categoriesBuilder = new CategoriesMapModelBuilder(1, 2)
     const resources = categoriesBuilder.buildResources()
-    const fetchMap = createFetchMap(resources)
+    const fetchMap = categoriesBuilder.buildFetchMap()
 
     await expectSaga(fetchResourceCache, city, language, fetchMap, dataContainer)
       .not.put.like({ action: { type: 'FETCH_RESOURCES_FAILED' } })
@@ -62,8 +52,7 @@ describe('fetchResourceCache', () => {
     const dataContainer = new DefaultDataContainer()
 
     const categoriesBuilder = new CategoriesMapModelBuilder(1, 2)
-    const resources = categoriesBuilder.buildResources()
-    const fetchMap = createFetchMap(resources)
+    const fetchMap = categoriesBuilder.buildFetchMap()
 
     return expectSaga(fetchResourceCache, city, language, fetchMap, dataContainer)
       .provide({
