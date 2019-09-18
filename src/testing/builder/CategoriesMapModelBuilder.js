bbc353a87dd468e1f48e48e4295c7a7ca9ffebe8
Maximilian Ammann <max@maxammann.org>
2019-09-18 15:08:22 +0200

    NATIVE-328: Build FetchMaps in builders

--- src/testing/builder/CategoriesMapModelBuilder.js
+++ src/testing/builder/CategoriesMapModelBuilder.js
@@ -5,18 +5,28 @@ import moment from 'moment-timezone'
 import type { FileCacheStateType, LanguageResourceCacheStateType } from '../../modules/app/StateType'
 import seedrandom from 'seedrandom'
 import hashUrl from '../../modules/endpoint/hashUrl'
+import type { FetchMapType } from '../../modules/endpoint/sagas/fetchResourceCache'
+import { createFetchMap } from './util'
 
 const DEFAULT_MAX_WIDTH = 3
 const DEFAULT_DEPTH = 2
 const MAX_PREDICTABLE_VALUE = 6
 
+const FILE_PATH_BUILDER = (url: string, urlHash: string) =>
+  `path/to/documentDir/resource-cache/v1/some-city/files/${urlHash}.png`
+
 class CategoriesMapModelBuilder {
   _depth: number
   _maxWidth: number
+  _buildFilePath: (url: string, urlHash: string) => string
 
-  constructor (maxWidth: number = DEFAULT_MAX_WIDTH, depth: number = DEFAULT_DEPTH) {
+  constructor (
+    maxWidth: number = DEFAULT_MAX_WIDTH,
+    depth: number = DEFAULT_DEPTH,
+    buildFilePath: (url: string, urlHash: string) => string = FILE_PATH_BUILDER) {
     this._maxWidth = maxWidth
     this._depth = depth
+    this._buildFilePath = buildFilePath
   }
 
   predictableNumber (index: number, max: number = MAX_PREDICTABLE_VALUE): number {
@@ -27,7 +37,7 @@ class CategoriesMapModelBuilder {
     const hash = hashUrl(url)
     return {
       [url]: {
-        filePath: `path/to/documentDir/resource-cache/v1/some-city/files/${hash}.png`,
+        filePath: this._buildFilePath(url, hash),
         lastUpdate: moment(lastUpdate).add(this.predictableNumber(index), 'days'),
         hash
       }
@@ -47,28 +57,30 @@ class CategoriesMapModelBuilder {
       const path = `${category.path}/category_${depth}-${i}`
       const lastUpdate = moment.tz('2017-11-18 19:30:00', 'UTC')
 
+      const thumbnailUrl = `http://thumbnails/category_${depth}-${i}.png`
+      const imgUrl1 = `https://integreat/title_${depth}-${i}-300x300.png`
+      const imgUrl2 = `https://integreat/category_${depth}-${i}-300x300.png`
+
       const newChild = new CategoryModel({
         id: 0,
         path,
         title: `Category ${depth}-${i}`,
         content: `<h1>This is a sample page</h1>
-                    <img src="https://integreat/title_${depth}-${i}-300x300.png"/>
+                    <img src="${imgUrl1}"/>
                     <p>This is a sample page</p>
-                    <img src="https://integreat/category_${depth}-${i}-300x300.png"/>`,
+                    <img src="${imgUrl2}"/>`,
         order: -1,
         availableLanguages: new Map(),
-        thumbnail: `http://thumbnails/category_${depth}-${i}.png`,
+        thumbnail: thumbnailUrl,
         parentPath: category.path,
         lastUpdate
       })
 
+      const index = depth * this._maxWidth + i
       resourceCache[path] = {
-        ...this.createResource(`https://integreat/title_${depth}-${i}-300x300.png`,
-          depth * this._maxWidth + i,
-          lastUpdate),
-        ...this.createResource(`https://integreat/category_${depth}-${i}-300x300.png`,
-          depth * this._maxWidth + i,
-          lastUpdate)
+        ...this.createResource(imgUrl1, index, lastUpdate),
+        ...this.createResource(imgUrl2, index, lastUpdate),
+        ...this.createResource(thumbnailUrl, index, lastUpdate)
       }
       categories.push(newChild)
       this.addChildren(newChild, categories, resourceCache, depth + 1)
@@ -77,6 +89,10 @@ class CategoriesMapModelBuilder {
 
   buildResources (): { [path: string]: FileCacheStateType } {
     return this.buildAll().resourceCache
+  }
+
+  buildFetchMap (): FetchMapType {
+    return createFetchMap(this.buildResources())
   }
 
   build (): CategoriesMapModel {
--- src/testing/builder/EventModelBuilder.js
+++ src/testing/builder/EventModelBuilder.js
@@ -5,16 +5,25 @@ import moment from 'moment-timezone'
 import seedrandom from 'seedrandom'
 import type { FileCacheStateType } from '../../modules/app/StateType'
 import hashUrl from '../../modules/endpoint/hashUrl'
+import type { FetchMapType } from '../../modules/endpoint/sagas/fetchResourceCache'
+import { createFetchMap } from './util'
 
 const MAX_PREDICTABLE_VALUE = 6
+
+const FILE_PATH_BUILDER = (url: string, urlHash: string) =>
+  `path/to/documentDir/resource-cache/v1/some-city/files/${urlHash}.png`
 
 class EventModelBuilder {
   _eventCount: number
   _seed: string
+  _buildFilePath: (url: string, urlHash: string) => string
 
-  constructor (seed: string, eventCount: number) {
+  constructor (
+    seed: string, eventCount: number,
+    buildFilePath: (url: string, urlHash: string) => string = FILE_PATH_BUILDER) {
     this._seed = seed
     this._eventCount = eventCount
+    this._buildFilePath = buildFilePath
   }
 
   predictableNumber (index: number, max: number = MAX_PREDICTABLE_VALUE): number {
@@ -32,11 +41,15 @@ class EventModelBuilder {
     }, {})
   }
 
+  buildFetchMap (): FetchMapType {
+    return createFetchMap(this.buildResources())
+  }
+
   createResource (url: string, index: number, lastUpdate: moment): FileCacheStateType {
     const hash = hashUrl(url)
     return {
       [url]: {
-        filePath: `path/to/documentDir/resource-cache/v1/some-city/files/${hash}.png`,
+        filePath: this._buildFilePath(url, hash),
         lastUpdate: moment(lastUpdate).add(this.predictableNumber(index), 'days'),
         hash
       }
@@ -56,6 +69,11 @@ class EventModelBuilder {
       const endDate = moment(startDate).add(this.predictableNumber(index), 'hours')
       const lastUpdate = moment(startDate).subtract(this.predictableNumber(index), 'months')
       const path = `/augsburg/en/events/event${index}`
+
+      const thumbnailUrl = `http://thumbnails/event_-${index}.png`
+      const imgUrl1 = `https://integreat/title_${index}-300x300.png`
+      const imgUrl2 = `https://integreat/event_${index}-300x300.png`
+
       return {
         path,
         event: new EventModel({
@@ -77,14 +95,15 @@ class EventModelBuilder {
           excerpt: 'excerpt',
           lastUpdate,
           content: `<h1>This is a sample event</h1>
-                    <img src="https://integreat/title_${index}-300x300.png"/>
+                    <img src="${imgUrl1}"/>
                     <p>This is a sample event</p>
-                    <img src="https://integreat/event_${index}-300x300.png"/>`,
-          thumbnail: 'https://integreat/thumbnail.png'
+                    <img src="${imgUrl2}"/>`,
+          thumbnail: thumbnailUrl
         }),
         resources: {
-          ...this.createResource(`https://integreat/title_${index}-300x300.png`, index),
-          ...this.createResource(`https://integreat/event_${index}-300x300.png`, index)
+          ...this.createResource(imgUrl1, index),
+          ...this.createResource(imgUrl2, index),
+          ...this.createResource(thumbnailUrl, index)
         }
       }
     })
