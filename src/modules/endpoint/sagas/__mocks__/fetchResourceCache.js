88a1b6de126a1b7041a6e936a3f2688eb2456a4d
Maximilian Ammann <max@maxammann.org>
2019-09-18 15:07:59 +0200

    NATIVE-328: Add mocks for load* functions which require a prepared DataContainer for "augsburg" and language "en"

---
+++ src/modules/endpoint/sagas/__mocks__/fetchResourceCache.js
@@ -0,0 +1,8 @@
+// @flow
+
+import type { DataContainer } from '../../DataContainer'
+import type { Saga } from 'redux-saga'
+import type { FetchMapType } from '../fetchResourceCache'
+
+export default function * fetchResourceCache (city: string, language: string, fetchMap: FetchMapType, dataContainer: DataContainer): Saga<void> {
+}
