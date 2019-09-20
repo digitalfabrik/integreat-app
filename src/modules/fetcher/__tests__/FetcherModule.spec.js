2a677822976d46f90bd6e484ac69e2b5519b3097
Simple-codinger <vincentgierisch@gmail.com>
2019-09-20 23:54:11 +0200

    NATIVE-120: add tests for fetchAsync

--- src/modules/fetcher/__tests__/FetcherModule.spec.js
+++ src/modules/fetcher/__tests__/FetcherModule.spec.js
@@ -22,6 +22,21 @@ describe('FetcherModule', () => {
   it('should call fetchAsync with targetFiles on native module', async () => {
     const fetcherModule = new FetcherModule()
     await fetcherModule.fetchAsync(testTargetFilePaths, progress => console.log(progress))
+    expect(NativeFetcherModule.fetchAsync).toHaveBeenCalledTimes(1)
     expect(NativeFetcherModule.fetchAsync).toHaveBeenCalledWith(testTargetFilePaths)
+  })
+  it('should not call fetchAsync if targetFiles are empty', async () => {
+    const fetcherModule = new FetcherModule()
+    await fetcherModule.fetchAsync({}, progress => console.log(progress))
+    expect(NativeFetcherModule.fetchAsync).not.toHaveBeenCalled()
+  })
+  it('should return an error if fetcher is already busy', async () => {
+    const fetcherModule = new FetcherModule()
+    FetcherModule.currentlyFetching = true
+    expect(fetcherModule.fetchAsync(testTargetFilePaths, progress => console.log(progress)))
+      .rejects.toEqual({
+        error: 'Already fetching!'
+      })
+    FetcherModule.currentlyFetching = false
   })
 })
