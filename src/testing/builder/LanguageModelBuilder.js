643059cd00700ba5e7dcdc4e408bba6585744b68
Maximilian Ammann <max@maxammann.org>
2019-09-30 16:12:27 +0200

    NATIVE-327: Add also Deutsch

--- src/testing/builder/LanguageModelBuilder.js
+++ src/testing/builder/LanguageModelBuilder.js
@@ -4,6 +4,7 @@ import { LanguageModel } from '@integreat-app/integreat-api-client'
 
 const languages = [
   new LanguageModel('en', 'English'),
+  new LanguageModel('de', 'Deutsch'),
   new LanguageModel('ar', 'اَللُّغَةُ اَلْعَرَبِيَّة')
 ]
 
