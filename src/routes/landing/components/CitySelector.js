ee6bbf15b45f7767c08cebd8121b7d6b885188ad
Simon Vortkamp <s.vortkamp@gmx.de>
2019-09-30 13:53:26 +0200

    NATIVE-15: Add filterByLocation to renderListByLocation

--- src/routes/landing/components/CitySelector.js
+++ src/routes/landing/components/CitySelector.js
@@ -6,7 +6,7 @@ import { groupBy } from 'lodash/collection'
 import CityEntry from './CityEntry'
 import { CityModel } from '@integreat-app/integreat-api-client'
 import styled, { type StyledComponent } from 'styled-components/native'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import type { ThemeType } from '../../../modules/theme/constants/theme'
 import Geolocation from '@react-native-community/geolocation'
 
@@ -47,19 +47,33 @@ const developmentCompare = (a: CityModel, b: CityModel) => {
 }
 
 const degreesToRadians = deg => {
+  if (deg === null) {
+    return null
+  }
   const degreesSemicircle = 180
   return Math.PI * deg / degreesSemicircle
 }
 
+const degreeDifference = (longitude0, latitude0, longitude1, latitude1) => {
+  return Math.acos(Math.cos(longitude0) * Math.cos(longitude1) * Math.cos(latitude0 - latitude1) +
+    Math.sin(longitude1) * Math.sin(longitude0))
+}
+
 const currentDistance = (a: CityModel, longitude, latitude) => {
+  if (a.longitude === null | a.latitude === null) {
+    return Infinity
+  }
   const longitude0 = degreesToRadians(longitude)
   const latitude0 = degreesToRadians(latitude)
-  const longitude1 = degreesToRadians(a.longitude)
-  const latitude1 = degreesToRadians(a.latitude)
   const earthRadius = 6371
-  const degreeDifference = Math.acos(Math.cos(longitude0) * Math.cos(longitude1) * Math.cos(latitude0 - latitude1) +
-    Math.sin(longitude1) * Math.sin(longitude0))
-  return degreeDifference * earthRadius
+  let coordinates = Object.values(a.aliases || {})
+  coordinates.push({ longitude: longitude, latitude: latitude })
+  coordinates = coordinates.map(coords => {
+    const longitude1 = degreesToRadians(coords.longitude)
+    const latitude1 = degreesToRadians(coords.latitude)
+    return degreeDifference(longitude0, latitude0, longitude1, latitude1) * earthRadius
+  })
+  return Math.min(coordinates)
 }
 
 const compareDistance = (a: CityModel, b: CityModel, longitude, latitude) => {
@@ -88,29 +102,6 @@ class CitySelector extends React.PureComponent<PropsType> {
     }
   }
 
-  filterByLocation = async () => {
-    const { status } = await Permissions.askAsync(Permissions.LOCATION)
-    if (status !== 'granted') {
-      return null
-    } else {
-      Geolocation.getCurrentPosition(
-        position => {
-          const currentLongitude = Number(JSON.stringify(position.coords.longitude))
-          const currentLatitude = Number(JSON.stringify(position.coords.latitude))
-          this.setState({ currentLongitude: currentLongitude })
-          this.setState({ currentLatitude: currentLatitude })
-        },
-        error => {
-          alert(error.message)
-        },
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
-      )
-      const cities = this.props.cities.sort((a: CityModel, b: CityModel) =>
-        compareDistance(a, b, this.currentLongitude, this.currentLatitude))
-      const numberOfClosestCities = 3
-      return cities.slice(0, numberOfClosestCities)
-    }
-  }
   renderList (cities: Array<CityModel>): React.Node {
     const groups = groupBy(cities, city => city.sortCategory)
     return transform(groups, (result, cities, key) => {
@@ -126,11 +117,28 @@ class CitySelector extends React.PureComponent<PropsType> {
     }, [])
   }
 
-  renderListByLocation (cities: Array<CityModel> | null): React.Node {
-    if (cities === null) {
-      return null
-    } else {
-      return <View>
+  renderListByLocation (): React.Node {
+    if (Platform.ios) {
+      Geolocation.requestAuthorization()
+    }
+    Geolocation.getCurrentPosition(
+      position => {
+        const currentLongitude = Number(position.coords.longitude)
+        const currentLatitude = Number(position.coords.longitude)
+        this.setState({ currentLongitude: currentLongitude })
+        this.setState({ currentLatitude: currentLatitude })
+      },
+      error => {
+        alert(error.message)
+        return null
+      },
+      { enableHighAccuracy: false, timeout: 20000, maximumAge: 1000 }
+    )
+    let cities = this.props.cities.sort((a: CityModel, b: CityModel) =>
+      compareDistance(a, b, this.currentLongitude, this.currentLatitude))
+    const numberOfClosestCities = 3
+    cities = cities.slice(0, numberOfClosestCities)
+    return <View>
         <CityGroup theme={this.props.theme}> Next Cities </CityGroup>
         {cities.map(city => <CityEntry
           key={city.code}
@@ -139,11 +147,10 @@ class CitySelector extends React.PureComponent<PropsType> {
           navigateToDashboard={this.props.navigateToDashboard}
           theme={this.props.theme} />)}
       </View>
-    }
   }
   render () {
     return <>
-      { this.renderListByLocation(this.filterByLocation()) }
+      { this.renderListByLocation() }
       { this.renderList(this.filter()) }
     </>
   }
