8a6d9925a359af06a0528390261d8b2074879ddc
Maximilian Ammann <max@maxammann.org>
2019-09-30 14:25:37 +0200

    NATIVE-327: Add test case if new city is not equal to city

--- src/modules/endpoint/selectors/__tests__/isPeekingRoute.spec.js
+++ src/modules/endpoint/selectors/__tests__/isPeekingRoute.spec.js
@@ -3,7 +3,7 @@
 import isPeekingRoute from '../isPeekingRoute'
 
 describe('isPeekingRoute', () => {
-  it('should return true if new city is equal to content city', () => {
+  it('should return true if new city is equal to city', () => {
     expect(isPeekingRoute({
       darkMode: false,
       cityContent: {
@@ -20,6 +20,25 @@ describe('isPeekingRoute', () => {
         status: 'loading'
       }
     }, { routeCity: 'augsburg' })).toBe(false)
+  })
+
+  it('should return false if new city is not equal to city', () => {
+    expect(isPeekingRoute({
+      darkMode: false,
+      cityContent: {
+        city: 'augsburg',
+        switchingLanguage: false,
+        languages: [],
+        categoriesRouteMapping: {},
+        eventsRouteMapping: {},
+        resourceCache: {},
+        searchRoute: null
+      },
+      contentLanguage: 'en',
+      cities: {
+        status: 'loading'
+      }
+    }, { routeCity: 'nuernberg' })).toBe(true)
   })
 
   it('should return false if there is no content', () => {
