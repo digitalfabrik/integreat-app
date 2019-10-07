4172030a972b2ceb4cc494f4751c9140213a1b1d
Simple-codinger <vincentgierisch@gmail.com>
2019-10-07 22:40:26 +0200

    NATIVE-120: remove type

--- src/modules/fetcher/__mocks__/NativeFetcherModule.js
+++ src/modules/fetcher/__mocks__/NativeFetcherModule.js
@@ -3,12 +3,6 @@
 import type { FetchResultType } from '../FetcherModule'
 import { NativeEventEmitter } from 'react-native'
 
-export type NativeFetcherModuleType = {
-  +addListener: (eventType: string) => void,
-  +removeListeners: (count: number) => void,
-  fetchAsync: (targetFilePaths: { [url: string]: string }) => Promise<FetchResultType>
-}
-
 const NativeFetcherModule = {
   addListener: jest.fn<[string], void>(),
   removeListeners: jest.fn<[number], void>(),
