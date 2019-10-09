39fb1615637bd95ccea03ab92d0f96f55b93bfa1
Steffen Kleinle <klinzo@yahoo.de>
2019-10-09 20:02:55 +0200

    NATIVE-345: Fix createFetchMap

--- src/testing/builder/util.js
+++ src/testing/builder/util.js
@@ -1,14 +1,13 @@
 // @flow
 
 import type { FileCacheStateType } from '../../modules/app/StateType'
-import type { FetchMapType } from '../../modules/endpoint/sagas/fetchResourceCache'
-import { forEach, transform } from 'lodash'
+import type { FetchMapEntryType, FetchMapType } from '../../modules/endpoint/sagas/fetchResourceCache'
+import { mapValues, reduce } from 'lodash'
 
-export const createFetchMap = (resources: { [path: string]: FileCacheStateType }): FetchMapType => {
-  return transform(resources, (result, value, path) => {
-    forEach(value, (value, url) => {
-      result[value.filePath] = [url, path, value.hash]
-    })
-    return result
-  }, {})
-}
+export const createFetchMap = (resources: { [path: string]: FileCacheStateType }): FetchMapType =>
+  mapValues(resources, (files: FileCacheStateType) =>
+    reduce<FileCacheStateType, FetchMapEntryType>(files, (fetchMapEntry, file, url) => {
+      fetchMapEntry.push({ url, filePath: file.filePath, urlHash: file.hash })
+      return fetchMapEntry
+    }, [])
+  )
