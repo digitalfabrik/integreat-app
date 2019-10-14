7f358610a26bee5a0786e8cd069927ec7201aa52
Simple-codinger <vincentgierisch@gmail.com>
2019-10-14 12:52:45 +0200

    NATIVE-120: add fetchAsync mock function

--- src/modules/fetcher/__mocks__/NativeFetcherModule.js
+++ src/modules/fetcher/__mocks__/NativeFetcherModule.js
@@ -1,12 +1,25 @@
 // @flow
 
-import type { FetchResultType } from '../FetcherModule'
+import type { FetchResultType, TargetFilePathsType } from '../FetcherModule'
 import { NativeEventEmitter } from 'react-native'
+import moment from 'moment-timezone'
+
+function mockFetchAsync (targetFilePaths: TargetFilePathsType): Promise<FetchResultType> {
+  const fetchResult: FetchResultType = {}
+  for (const path in targetFilePaths) {
+    fetchResult[path] = {
+      lastUpdate: moment.tz('20110204', 'UTC').toISOString(),
+      url: targetFilePaths[path],
+      errorMessage: null
+    }
+  }
+  return Promise.resolve(fetchResult)
+}
 
 const NativeFetcherModule = {
   addListener: jest.fn<[string], void>(),
   removeListeners: jest.fn<[number], void>(),
-  fetchAsync: jest.fn<[{ [url: string]: string }], [Promise<FetchResultType>]>()
+  fetchAsync: jest.fn<[TargetFilePathsType], Promise<FetchResultType>>(mockFetchAsync)
 }
 
 export const NativeFetcherModuleEmitter = new NativeEventEmitter(NativeFetcherModule)
