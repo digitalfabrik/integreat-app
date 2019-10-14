cb05d54896c1dbfc3ea3634e3e9312ef17696f1f
Steffen Kleinle <klinzo@yahoo.de>
2019-10-14 08:30:48 +0200

    NATIVE-345: Use moment

--- src/testing/builder/util.js
+++ src/testing/builder/util.js
@@ -1,12 +1,12 @@
 // @flow
 
 import type { FileCacheStateType } from '../../modules/app/StateType'
-import type { FetchMapEntryType, FetchMapType } from '../../modules/endpoint/sagas/fetchResourceCache'
+import type { FetchMapTargetType, FetchMapType } from '../../modules/endpoint/sagas/fetchResourceCache'
 import { mapValues, reduce } from 'lodash'
 
 export const createFetchMap = (resources: { [path: string]: FileCacheStateType }): FetchMapType =>
   mapValues(resources, (files: FileCacheStateType) =>
-    reduce<FileCacheStateType, FetchMapEntryType>(files, (fetchMapEntry, file, url) => {
+    reduce<FileCacheStateType, Array<FetchMapTargetType>>(files, (fetchMapEntry, file, url) => {
       fetchMapEntry.push({ url, filePath: file.filePath, urlHash: file.hash })
       return fetchMapEntry
     }, [])
