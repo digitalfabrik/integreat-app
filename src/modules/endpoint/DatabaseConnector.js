e2567edd784b51b5585cc80db8d0b9dc7c5bbd9c
Steffen Kleinle <klinzo@yahoo.de>
2019-11-01 15:28:42 +0100

    NATIVE-266: Fix deletion of caches

--- src/modules/endpoint/DatabaseConnector.js
+++ src/modules/endpoint/DatabaseConnector.js
@@ -85,13 +85,13 @@ type MetaCitiesEntryJsonType = {|
       last_update: string
     |}
   },
-  last_update: string
+  last_usage: string | null
 |}
 type MetaCitiesJsonType = {
   [CityCodeType]: MetaCitiesEntryJsonType
 }
 
-type CityLastUpdateType = {| city: CityCodeType, lastUpdate: Moment |}
+type CityLastUsageType = {| city: CityCodeType, lastUsage: Moment | null |}
 
 type PageResourceCacheEntryJsonType = {|
   file_path: string,
@@ -155,19 +155,21 @@ class DatabaseConnector {
     } else if (!languageCode) {
       throw Error('languageCode mustn\'t be empty')
     }
+
+    const metaData = await this.loadMetaCities() || {}
+
+    metaData[cityCode] = metaData[cityCode] || { languages: {}, last_usage: null }
+    metaData[cityCode].languages[languageCode] = { last_update: lastUpdate.toISOString() }
+
     const path = this.getMetaCitiesPath()
-
-    const fileExists: boolean = await RNFetchblob.fs.exists(path)
-    const metaData = fileExists
-      ? JSON.parse(await this.readFile(path))
-      : {}
-
-    const isoLastUpdate = lastUpdate.toISOString()
-    metaData[cityCode] = metaData[cityCode] || { languages: {} }
-    metaData[cityCode].languages[context.languageCode] = { last_update: isoLastUpdate }
-    metaData[cityCode].last_update = isoLastUpdate
-
     await this.writeFile(path, JSON.stringify(metaData))
+  }
+
+  async loadMetaCities (): Promise<MetaCitiesJsonType | null> {
+    const path = this.getMetaCitiesPath()
+    const fileExists: boolean = await RNFetchblob.fs.exists(path)
+
+    return fileExists ? JSON.parse(await this.readFile(path)) : null
   }
 
   async loadLastUpdate (context: DatabaseContext): Promise<Moment | null> {
@@ -180,30 +182,53 @@ class DatabaseConnector {
       throw new Error('Language is not set in DatabaseContext!')
     }
 
-    const path = this.getMetaCitiesPath()
-    const fileExists: boolean = await RNFetchblob.fs.exists(path)
-
-    if (!fileExists) {
+    const metaData = await this.loadMetaCities()
+    if (!metaData) {
       return null
     }
 
-    const metaData: MetaCitiesJsonType = JSON.parse(await this.readFile(path))
     // eslint-disable-next-line camelcase
     const lastUpdate = metaData[cityCode]?.languages[languageCode]?.last_update
     return lastUpdate ? moment(lastUpdate, moment.ISO_8601) : null
   }
 
-  async loadCitiesLastUpdates (): Promise<Array<CityLastUpdateType>> {
-    const path = this.getMetaCitiesPath()
-    const fileExists: boolean = await RNFetchblob.fs.exists(path)
-    if (!fileExists) {
+  async loadLastUsages (): Promise<Array<CityLastUsageType>> {
+    const metaData = await this.loadMetaCities()
+    if (!metaData) {
       return []
     }
 
-    const metaData: MetaCitiesJsonType = JSON.parse(await this.readFile(path))
-    return map<MetaCitiesEntryJsonType, MetaCitiesJsonType, CityLastUpdateType>(
-      metaData, (value, key) => ({ city: key, lastUpdate: value.last_update })
+    return map<MetaCitiesEntryJsonType, MetaCitiesJsonType, CityLastUsageType>(
+      metaData, (value, key) => ({
+        city: key,
+        lastUsage: value.last_usage ? moment(value.last_usage, moment.ISO_8601) : null
+      })
     )
+  }
+
+  async updateLastUsages (lastUsages: Array<CityLastUsageType>) {
+    if (lastUsages.length === 0) {
+      return
+    }
+    const metaData = await this.loadMetaCities() || {}
+
+    console.log('update')
+    console.log(metaData)
+    console.log(lastUsages)
+    lastUsages.forEach(lastUsage => {
+      const city = lastUsage.city
+      metaData[city] = { last_usage: lastUsage.lastUsage, languages: metaData[city].languages || {} }
+    })
+    console.log(metaData)
+
+    this.writeFile(this.getMetaCitiesPath(), JSON.stringify(metaData))
+  }
+
+  async updateLastUsage (context: DatabaseContext) {
+    if (!context.cityCode) {
+      throw Error('cityCode mustn\'t be null')
+    }
+    await this.updateLastUsages([{ city: context.cityCode, lastUsage: moment().toISOString() }])
   }
 
   async storeCategories (categoriesMap: CategoriesMapModel, context: DatabaseContext) {
@@ -393,7 +418,7 @@ class DatabaseConnector {
   }
 
   async storeResourceCache (resourceCache: CityResourceCacheStateType, context: DatabaseContext) {
-    await this.deleteOldResourceCaches()
+    await this.deleteOldResourceCaches(context)
 
     const path = this.getResourceCachePath(context)
     const json: CityResourceCacheJsonType =
@@ -410,19 +435,29 @@ class DatabaseConnector {
   }
 
   /**
-   * Delete the resource caches of all but the two latest updated cities
+   * Deletes the resource caches of all but the latest used cities
    * @return {Promise<void>}
    */
-  async deleteOldResourceCaches () {
-    const citiesLastUpdates = await this.loadCitiesLastUpdates()
-    // Sort last updates chronological, from newest to oldest
-    citiesLastUpdates.sort((a, b) =>
-      a.lastUpdate.isAfter(b.lastUpdate) ? -1 : (a.lastUpdate.isSame(b.lastUpdate) ? 0 : 1)
-    )
-    citiesLastUpdates.slice(2).forEach(async cityLastUpdate => {
-      const cityResourceCachePath = this.getResourceCachePath(new DatabaseContext(cityLastUpdate.city))
+  async deleteOldResourceCaches (context: DatabaseContext) {
+    const lastUsages: Array<{| city: CityCodeType, lastUsage: Moment |}> =
+      (await this.loadLastUsages()).filter(it => it.lastUsage !== null && it.city !== context.cityCode)
+
+    // Sort last usages chronological, from newest to oldest and remove the latest.
+    const cachesToDelete = lastUsages.sort((a, b) =>
+      a.lastUsage.isAfter(b.lastUsage) ? -1 : (a.lastUsage.isSame(b.lastUsage) ? 0 : 1)
+    ).slice(MAX_RESOURCE_CACHES)
+
+    console.log('delete')
+    console.log(cachesToDelete)
+    console.log(lastUsages)
+
+    await cachesToDelete.forEach(async cityLastUpdate => {
+      const cityResourceCachePath = `${RESOURCE_CACHE_DIR_PATH}/${cityLastUpdate.city}`
+      console.log(cityResourceCachePath)
       await this.deleteFileOrDirectory(cityResourceCachePath)
     })
+
+    this.updateLastUsages(cachesToDelete.map(it => ({ city: it.city, lastUsage: null })))
   }
 
   isCitiesPersisted (): Promise<boolean> {
