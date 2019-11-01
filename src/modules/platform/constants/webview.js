d5d1d89389efbe4ee0436864ded9fe0f5ed74f88
Steffen Kleinle <klinzo@yahoo.de>
2019-11-01 15:28:10 +0100

    NATIVE-266: Move paths

--- src/modules/platform/constants/webview.js
+++ src/modules/platform/constants/webview.js
@@ -1,17 +1,11 @@
 // @flow
 
-import RNFetchblob from 'rn-fetch-blob'
 import { Platform } from 'react-native'
-import { CONTENT_VERSION, RESOURCE_CACHE_VERSION } from '../../endpoint/persistentVersions'
 import type { WebViewSource } from 'react-native-webview'
+import { RESOURCE_CACHE_DIR_PATH } from '../../endpoint/DatabaseConnector'
 
 export const URL_PREFIX = 'file://'
 
-// Our pdf view can only load from DocumentDir. Therefore we need to use that
-export const CACHE_DIR_PATH = RNFetchblob.fs.dirs.DocumentDir
-
-export const CONTENT_DIR_PATH = `${CACHE_DIR_PATH}/content/${CONTENT_VERSION}`
-export const RESOURCE_CACHE_DIR_PATH = `${CACHE_DIR_PATH}/resource-cache/${RESOURCE_CACHE_VERSION}`
 export const getResourceCacheFilesDirPath = (city: string) => `${RESOURCE_CACHE_DIR_PATH}/${city}/files`
 export const getFontFaceSource = (fontName: string) => Platform.select({
   ios: `local('${fontName}') url('${fontName}.ttf') format('truetype')`,
