3ba31b6000fc68d8796802dfe643cdcc2c7372d0
Steffen Kleinle <klinzo@yahoo.de>
2019-11-06 17:55:36 +0100

    NATIVE-13: Add last slide

--- src/routes/intro/IntroContainer.js
+++ src/routes/intro/IntroContainer.js
@@ -10,11 +10,58 @@ import Search from './assets/Search.svg'
 import Events from './assets/Events.svg'
 import type { ThemeType } from '../../modules/theme/constants/theme'
 import withTheme from '../../modules/theme/hocs/withTheme'
+import { Dimensions, View } from 'react-native'
+import DefaultSlide from 'react-native-app-intro-slider/DefaultSlide'
+import styled from 'styled-components/native'
 
+const Content = styled.View`
+  justifyContent: space-around;
+  alignItems: center;
+  flex: 1;
+  width: ${Dimensions.get('window').width};
+`
+const StyledText = styled.Text`
+  fontSize: 16;
+  textAlign: center;
+`
+const StyledHeading = styled.Text`
+  fontSize: 26;
+`
+
+const ButtonText = styled.Text`
+  color: ${props => props.theme.colors.backgroundAccentColor}
+`
+
+const AcceptButtonContainer = styled.View`
+  position: absolute;
+  right: 0;
+  background-color: ${props => props.theme.colors.themeColor};
+
+`
+const AcceptButton = styled.TouchableOpacity`
+  flex: 1
+`
+
+type SlideType = {|
+  key: string,
+  title: string,
+  titleStyle: { color: string },
+  text: string,
+  textStyle: { color: string },
+  image: number,
+  backgroundColor: string
+|}
 type PropsType = {| t: TFunction, navigation: NavigationScreenProp<*>, theme: ThemeType |}
+type StateType = {| isLastSlide: boolean |}
 
-class Intro extends React.Component<PropsType> {
-  slides = () => {
+class Intro extends React.Component<PropsType, StateType> {
+  appIntroSlider: {current: null | React$ElementRef<AppIntroSlider>}
+  constructor (props: PropsType) {
+    super(props)
+    this.state = { isLastSlide: false }
+    this.appIntroSlider = React.createRef()
+  }
+  slides = (): Array<SlideType> => {
     const colors = this.props.theme.colors
     const backgroundColor = colors.backgroundColor
     const textStyle = { color: colors.textColor }
@@ -51,14 +98,46 @@ class Intro extends React.Component<PropsType> {
       textStyle,
       image: Language,
       backgroundColor
+    }, {
+      key: 'questions',
+      title: 'languageChange',
+      titleStyle: textStyle,
+      text: 'languageChangeDescription',
+      textStyle,
+      image: Language,
+      backgroundColor
     }]
   }
 
-  navigateToLanding = () => this.props.navigation.navigate('Landing')
+  renderItem = ({ item, index }: { item: SlideType, index: number}) => {
+    if (index === this.slides().length - 1) {
+      return <Content>
+        <StyledHeading>SOme</StyledHeading>
+      </Content>
+    }
+    return <DefaultSlide item={item} index={index} dimensions={Dimensions.get('window')} />
+  }
+
+  onDone = () => {
+    this.props.navigation.navigate('Landing')
+  }
+
+  onSlideChange = (index: number, lastIndex: number) =>
+    this.setState({ isLastSlide: index === this.slides().length - 1 })
+  onSkip = () => {
+    if (!this.appIntroSlider.current) {
+      throw Error()
+    }
+    this.appIntroSlider.current.goToSlide(this.slides().length - 1)
+    this.setState({ isLastSlide: true })
+  }
 
   render () {
     const colors = this.props.theme.colors
-    return <AppIntroSlider slides={this.slides()} onDone={this.navigateToLanding}
+    return <AppIntroSlider ref={this.appIntroSlider} slides={this.slides()} onDone={this.onDone} showSkipButton
+                           renderItem={this.renderItem} doneLabel={'Accept'}
+                           onSlideChange={this.onSlideChange}
+                           onSkip={this.onSkip} showPrevButton={this.state.isLastSlide}
                            dotStyle={{ backgroundColor: colors.textDecorationColor }}
                            activeDotStyle={{ backgroundColor: colors.textSecondaryColor }}
                            buttonTextStyle={{ color: colors.textSecondaryColor }} />
