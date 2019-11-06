bfa463d54b495b291f802e83e20e18e4868f6439
Steffen Kleinle <klinzo@yahoo.de>
2019-11-06 15:08:02 +0100

    NATIVE-13: Add intros

--- src/routes/intro/IntroContainer.js
+++ src/routes/intro/IntroContainer.js
@@ -1,11 +1,59 @@
 // @flow
 
+import { translate, type TFunction } from 'react-i18next'
 import React from 'react'
+import AppIntroSlider from 'react-native-app-intro-slider'
+import { View, Text, Image } from 'react-native'
+import type { NavigationScreenProp } from 'react-navigation'
 
-type PropsType = {||}
+type SlideType = {|
+  key: string,
+  title: string,
+  text: string,
+  image: string,
+  backgroundColor: string
+|}
 
-class IntroContainer extends React.Component<PropsType> {
+const slides: Array<SlideType> = [{
+  key: 'somethun',
+  title: 'Title 1',
+  text: 'Description.\nSay something cool',
+  image: '',
+  backgroundColor: '#59b2ab'
+},
+{
+  key: 'somethun-dos',
+  title: 'Title 2',
+  text: 'Other cool stuff',
+  image: '',
+  backgroundColor: '#febe29'
+},
+{
+  key: 'somethun1',
+  title: 'Rocket guy',
+  text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+  image: '',
+  backgroundColor: '#22bcb5'
+}]
 
+type PropsType = {| t: TFunction, navigation: NavigationScreenProp<*> |}
+
+class Intro extends React.Component<PropsType> {
+  renderItem = ({ item }) => {
+    return (
+      <View>
+        <Text>{item.title}</Text>
+        <Image source={item.image} />
+        <Text>{item.text}</Text>
+      </View>
+    )
+  }
+
+  navigateToLanding = () => this.props.navigation.navigate('Landing')
+
+  render () {
+    return <AppIntroSlider renderItem={this.renderItem} slides={slides} onDone={this.navigateToLanding} />
+  }
 }
 
-export default IntroContainer
+export default translate('intro')(Intro)
