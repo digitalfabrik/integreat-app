1e3460d7881ea296d0497fd633d95fd765130029
Simple-codinger <vincentgierisch@gmail.com>
2019-11-15 15:37:37 +0100

    NATIVE-267: delete old files

--- src/modules/layout/containers/EmptyHeaderContainer.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import EmptyHeader from '../components/EmptyHeader'
-import { translate } from 'react-i18next'
-
-import withTheme from '../../theme/hocs/withTheme'
-import { withNavigation } from 'react-navigation'
-
-export default withTheme()(withNavigation(translate('header')(EmptyHeader)))
