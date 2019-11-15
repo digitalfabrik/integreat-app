4988dfd0a0bedf6d303f525b93e786373dbdfde3
Steffen Kleinle <klinzo@yahoo.de>
2019-11-15 13:13:59 +0100

    NATIVE-13: Fix footer

--- src/routes/intro/SlideFooter.js
+++ src/routes/intro/SlideFooter.js
@@ -3,27 +3,44 @@
 import * as React from 'react'
 import type { ThemeType } from '../../modules/theme/constants/theme'
 import styled, { type StyledComponent } from 'styled-components/native'
+import { range } from 'lodash'
 
 const Container = styled.View`
-  flex: 0.2;
+  flex: 0.1;
   flex-direction: row;
   padding: 16px;
+  background-color: ${props => props.theme.colors.backgroundColor};
 `
 
-const ButtonContainer: StyledComponent<{| backgroundColor: string |}, ThemeType, *> = styled.TouchableOpacity`
+const ButtonContainer: StyledComponent<{| backgroundColor: string, end: boolean |}, ThemeType, *> =
+  styled.TouchableOpacity`
   flex: 1;
   padding: 12px;
   background-color: ${props => props.backgroundColor};
+  align-content: ${props => props.end ? 'flex-end' : 'flex-start'};
 `
 
 const ButtonText = styled.Text`
   color: ${props => props.theme.colors.textColor};
   font-size: 18px;
+  text-align: center;
 `
 
-const PaginationContainer = styled.View`
-  flex: 2;
+const DotsContainer = styled.View`
+  display: flex;
   flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`
+
+const Dot: StyledComponent<{| isActive: boolean |}, ThemeType, *> = styled.TouchableOpacity`
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  margin-horizontal: 4px;
+  background-color: ${props => props.isActive
+    ? props.theme.colors.textSecondaryColor
+    : props.theme.colors.textDecorationColor};
 `
 
 export type ButtonType = {|
@@ -37,24 +54,36 @@ type PropsType = {|
   rightButton: ButtonType,
   slideCount: number,
   currentSlide: number,
+  goToSlide: (index: number) => void,
   theme: ThemeType
 |}
 
 class SlideFooter extends React.Component<PropsType> {
-  renderButton = (button: ButtonType): React.Node => {
+  renderButton = (button: ButtonType, end: boolean): React.Node => {
     const { theme } = this.props
-    return <ButtonContainer theme={theme} onPress={button.onPress}
+    return <ButtonContainer theme={theme} onPress={button.onPress} end={end}
                             backgroundColor={button.backgroundColor || theme.colors.backgroundColor}>
       <ButtonText theme={theme}>{button.label}</ButtonText>
     </ButtonContainer>
   }
 
+  renderPagination = (): React.Node => {
+    const { slideCount, currentSlide, goToSlide, theme } = this.props
+    const goToSlideIndex = (index: number) => () => goToSlide(index)
+
+    return <DotsContainer>
+      {range(slideCount).map(index =>
+        <Dot key={index} isActive={index === currentSlide} onPress={goToSlideIndex(index)} theme={theme} />
+      )}
+    </DotsContainer>
+  }
+
   render () {
-    const { leftButton, rightButton, slideCount, currentSlide, theme } = this.props
-    return <Container>
-      {this.renderButton(leftButton)}
-      <PaginationContainer />
-      {this.renderButton(rightButton)}
+    const { leftButton, rightButton, theme } = this.props
+    return <Container theme={theme}>
+      {this.renderButton(leftButton, false)}
+      {this.renderPagination()}
+      {this.renderButton(rightButton, true)}
     </Container>
   }
 }
