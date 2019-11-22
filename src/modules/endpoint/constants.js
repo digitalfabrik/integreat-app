7518915af1692198da574544814c87daa63ce07b
Nikolas Klug <klug.nikolas@googlemail.com>
2019-11-22 11:34:40 +0100

    NATIVE-379: Use live-CMS in release mode

--- src/modules/endpoint/constants.js
+++ src/modules/endpoint/constants.js
@@ -1,5 +1,7 @@
 // @flow
 
-export const baseUrl = 'https://cms-test.integreat-app.de'
+export const testBaseUrl = 'https://cms-test.integreat-app.de'
+export const liveBaseUrl = 'https://cms.integreat-app.de'
+export const baseUrl = __DEV__ ? testBaseUrl : liveBaseUrl
 export const wohnenApiBaseUrl = 'https://api.wohnen.integreat-app.de/v0'
 export const allowedResourceHostNames = ['cms.integreat-app.de', 'cms-test.integreat-app.de']
