5f6ea4ad032fe33545e0b966532de0e97d13dc19
Michael Markl <marklmichael98@gmail.com>
2019-12-24 13:43:59 +0100

    NATIVE-379: Fix merge

--- src/modules/endpoint/__tests__/determineApiUrl.spec.js
+++ src/modules/endpoint/__tests__/determineApiUrl.spec.js
@@ -1,9 +1,8 @@
 // @flow
 
-import AsyncStorage from '@react-native-community/async-storage'
 import determineApiUrl from '../determineApiUrl'
 import { baseUrl } from '../constants'
-import { API_URL_OVERRIDE_KEY } from '../../settings/AppSettings'
+import AppSettings from '../../settings/AppSettings'
 
 jest.mock('@react-native-community/async-storage')
 
@@ -14,7 +13,7 @@ describe('determineApiUrl', () => {
   })
 
   it('should return the overrideApiUrl if it is set', async () => {
-    AsyncStorage.setItem(API_URL_OVERRIDE_KEY, 'https://super-cool-override-cms.url.com')
+    new AppSettings().setApiUrlOverride('https://super-cool-override-cms.url.com')
     const apiUrl = await determineApiUrl()
     expect(apiUrl).toEqual('https://super-cool-override-cms.url.com')
   })
