6ae400c4461e4f24bda2910e9d4dd673c991435f
Simple-codinger <vincentgierisch@gmail.com>
2019-12-30 20:17:45 +0100

    NATIVE-400: add category to FeedbackVariant

--- src/routes/feedback/FeedbackVariant.js
+++ src/routes/feedback/FeedbackVariant.js
@@ -1,20 +1,23 @@
 // @flow
 
-import type { FeedbackType } from '@integreat-app/integreat-api-client/endpoints/createFeedbackEndpoint'
+import type { FeedbackType, FeedbackCatetory } from '@integreat-app/integreat-api-client/endpoints/createFeedbackEndpoint'
 
 class FeedbackVariant {
   label: string
   language: string
   city: string
   feedbackType: FeedbackType
+  feedbackCategory: ?FeedbackCatetory
   pagePath: ?string
   alias: ?string
 
-  constructor (label: string, language: string, city: string, feedbackType: FeedbackType, pagePath?: string, alias?: string) {
+  constructor (label: string, language: string, city: string, feedbackType: FeedbackType,
+    feedbackCategory?: FeedbackCatetory, pagePath?: string, alias?: string) {
     this.label = label
     this.language = language
     this.city = city
     this.feedbackType = feedbackType
+    this.feedbackCategory = feedbackCategory
     this.pagePath = pagePath
     this.alias = alias
   }
