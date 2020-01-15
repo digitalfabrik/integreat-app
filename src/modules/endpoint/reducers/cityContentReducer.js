0b6ce2169bf94aaedaa63751afbb710e3f961bba
Steffen Kleinle <klinzo@yahoo.de>
2020-01-15 20:31:18 +0100

    NATIVE-408: Add status to language state

--- src/modules/endpoint/reducers/cityContentReducer.js
+++ src/modules/endpoint/reducers/cityContentReducer.js
@@ -12,121 +12,94 @@ import { omit } from 'lodash'
 export default (
   state: CityContentStateType | null = defaultCityContentState, action: StoreActionType
 ): CityContentStateType | null => {
-  switch (action.type) {
-    case 'SWITCH_CONTENT_LANGUAGE':
-      if (state === null) {
-        return null
-      }
-      return { ...state, switchingLanguage: true }
-    case 'SWITCH_CONTENT_LANGUAGE_FAILED':
-      if (state === null) {
-        return null
-      }
-      return { ...state, switchingLanguage: false }
-    case 'PUSH_LANGUAGES':
-      if (state === null) {
-        return null
-      }
-      return { ...state, languages: action.params.languages }
-    case 'PUSH_CATEGORY':
-      if (state === null) {
-        return null /* If you are on the landing page, then the `state` is null. If you quickly navigate
-                       to Landing screen while data is being fetched, then this line is reached. */
-      }
-      return pushCategory(state, action)
-    case 'PUSH_EVENT':
-      if (state === null) {
-        return null
-      }
-      return pushEvent(state, action)
-    case 'MORPH_CONTENT_LANGUAGE':
-      if (state === null) {
-        return null
-      }
-      return morphContentLanguage(state, action)
-    case 'FETCH_EVENT': {
-      const { language, path, key, city } = action.params
-      const initializedState = state || createCityContent(city)
-      return {
-        ...initializedState,
-        eventsRouteMapping: {
-          ...initializedState.eventsRouteMapping,
-          [key]: { status: 'loading', language, city, path }
-        }
+  if (action.type === 'FETCH_CATEGORY') {
+    const { language, path, depth, key, city } = action.params
+    const initializedState = state || createCityContent(city)
+    return {
+      ...initializedState,
+      categoriesRouteMapping: {
+        ...initializedState.categoriesRouteMapping,
+        [key]: { status: 'loading', language, depth, path, city }
       }
     }
-    case 'CLEAR_EVENT': {
-      const { key } = action.params
-      if (state === null) {
-        return null
-      }
-      return {
-        ...state,
-        eventsRouteMapping: omit(state.eventsRouteMapping, [key])
-      }
-    }
-    case 'FETCH_EVENT_FAILED': {
-      if (state === null) {
-        return null
-      }
-      const { message, key, path, allAvailableLanguages, ...rest } = action.params
-      return {
-        ...state,
-        eventsRouteMapping: {
-          ...state.eventsRouteMapping,
-          [key]: allAvailableLanguages
-            ? { status: 'languageNotAvailable', allAvailableLanguages, ...rest }
-            : { status: 'error', message, path, ...rest }
-        }
-      }
-    }
-    case 'FETCH_CATEGORY': {
-      const { language, path, depth, key, city } = action.params
-      const initializedState = state || createCityContent(city)
-      return {
-        ...initializedState,
-        categoriesRouteMapping: {
-          ...initializedState.categoriesRouteMapping,
-          [key]: { status: 'loading', language, depth, path, city }
-        }
-      }
-    }
-    case 'CLEAR_CATEGORY': {
-      const { key } = action.params
-      if (state === null) {
-        return null
-      }
-      return {
-        ...state,
-        categoriesRouteMapping: omit(state.categoriesRouteMapping, [key])
-      }
-    }
-    case 'FETCH_CATEGORY_FAILED': {
-      if (state === null) {
-        return null
-      }
-      const { message, code, key, allAvailableLanguages, path, ...rest } = action.params
-      return {
-        ...state,
-        categoriesRouteMapping: {
-          ...state.categoriesRouteMapping,
-          [key]: allAvailableLanguages
-            ? { status: 'languageNotAvailable', allAvailableLanguages, ...rest }
-            : { status: 'error', message, code, path, ...rest }
-        }
-      }
-    }
-    case 'CLEAR_CITY':
-    case 'CLEAR_RESOURCES_AND_CACHE':
+  } else {
+    if (state === null) {
       return null
-    case 'FETCH_RESOURCES_FAILED': {
-      if (state === null) {
-        return null
-      }
-      const { message, code } = action.params
-      return { ...state, resourceCache: { status: 'error', message, code } }
     }
-    default:
-      return state
+
+    switch (action.type) {
+      case 'SWITCH_CONTENT_LANGUAGE':
+        return { ...state, switchingLanguage: true }
+      case 'SWITCH_CONTENT_LANGUAGE_FAILED':
+        return { ...state, switchingLanguage: false }
+      case 'PUSH_LANGUAGES':
+        return { ...state, languages: { status: 'ready', models: action.params.languages } }
+      case 'FETCH_LANGUAGES_FAILED':
+        return { ...state, languages: { status: 'error', ...action.params } }
+      case 'PUSH_CATEGORY':
+        return pushCategory(state, action)
+      case 'PUSH_EVENT':
+        return pushEvent(state, action)
+      case 'MORPH_CONTENT_LANGUAGE':
+        return morphContentLanguage(state, action)
+      case 'FETCH_EVENT': {
+        const { language, path, key, city } = action.params
+        const initializedState = state || createCityContent(city)
+        return {
+          ...initializedState,
+          eventsRouteMapping: {
+            ...initializedState.eventsRouteMapping,
+            [key]: { status: 'loading', language, city, path }
+          }
+        }
+      }
+      case 'CLEAR_EVENT': {
+        const { key } = action.params
+        return {
+          ...state,
+          eventsRouteMapping: omit(state.eventsRouteMapping, [key])
+        }
+      }
+      case 'FETCH_EVENT_FAILED': {
+        const { message, key, path, allAvailableLanguages, ...rest } = action.params
+        return {
+          ...state,
+          eventsRouteMapping: {
+            ...state.eventsRouteMapping,
+            [key]: allAvailableLanguages
+              ? { status: 'languageNotAvailable', allAvailableLanguages, ...rest }
+              : { status: 'error', message, path, ...rest }
+          }
+        }
+      }
+      case 'CLEAR_CATEGORY': {
+        const { key } = action.params
+        return {
+          ...state,
+          categoriesRouteMapping: omit(state.categoriesRouteMapping, [key])
+        }
+      }
+      case 'FETCH_CATEGORY_FAILED': {
+        const { message, code, key, allAvailableLanguages, path, ...rest } = action.params
+        return {
+          ...state,
+          categoriesRouteMapping: {
+            ...state.categoriesRouteMapping,
+            [key]: allAvailableLanguages
+              ? { status: 'languageNotAvailable', allAvailableLanguages, ...rest }
+              : { status: 'error', message, code, path, ...rest }
+          }
+        }
+      }
+      case 'CLEAR_CITY':
+      case 'CLEAR_RESOURCES_AND_CACHE':
+        return null
+      case 'FETCH_RESOURCES_FAILED': {
+        const { message, code } = action.params
+        return { ...state, resourceCache: { status: 'error', message, code } }
+      }
+      default:
+        return state
+    }
   }
 }
