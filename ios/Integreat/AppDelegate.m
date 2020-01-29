abbffbe87388decb8e19abbbf0c7a103b5d399b8
Steffen Kleinle <klinzo@yahoo.de>
2020-01-29 16:47:28 +0100

    NATIVE-221: Revert some changes

--- ios/Integreat/AppDelegate.m
+++ ios/Integreat/AppDelegate.m
@@ -10,6 +10,11 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#if __has_include(<React/RNSentry.h>)
+#import <React/RNSentry.h> // This is used for versions of react >= 0.40
+#else
+#import "RNSentry.h" // This is used for versions of react < 0.40
+#endif
 
 @implementation AppDelegate
 
@@ -19,6 +24,9 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Integreat"
                                             initialProperties:nil];
+
+  [RNSentry installWithRootView:rootView];
+
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
