4d29b9b75b4dd39f796d03d8cabc4c56650b1d82
Michael Markl <marklmichael98@gmail.com>
2020-03-05 16:02:56 +0100

    NATIVE-301: Fix yellow separators

--- src/modules/categories/components/CategoryCaption.js
+++ src/modules/categories/components/CategoryCaption.js
@@ -4,7 +4,7 @@ import styled from 'styled-components/native'
 import { type StyledComponent } from 'styled-components'
 import type { ThemeType } from '../../theme/constants/theme'
 
-const CategoryCaption: StyledComponent<{}, ThemeType, *> = styled.Text`
+const CategoryCaption: StyledComponent<{}, ThemeType, *> = styled.View`
   align-self: center;
   padding: 15px 5px;
   border-bottom-width: 2px;
---
+++ src/routes/landing/components/CityGroup.js
@@ -0,0 +1,35 @@
+// @flow
+
+import * as React from 'react'
+import styled from 'styled-components/native'
+import { type StyledComponent } from 'styled-components'
+import type { ThemeType } from '../../../modules/theme/constants/theme'
+
+const GroupText: StyledComponent<{}, ThemeType, *> = styled.Text`
+  width: 100%;
+  margin-top: 5px;
+  padding: 10px 0;
+  background-color: ${props => props.theme.colors.backgroundColor};
+  font-family: ${props => props.theme.fonts.decorativeFontRegular};
+  color: ${props => props.theme.colors.textColor};
+`
+
+const BorderWrapper: StyledComponent<{}, ThemeType, *> = styled.View`
+  border-bottom-width: 1px;
+  border-bottom-color: ${props => props.theme.colors.themeColor};
+`
+
+type PropsType = {|
+  theme: ThemeType,
+  children: string
+|}
+
+class CityGroup extends React.Component<PropsType> {
+  render () {
+    return <BorderWrapper theme={this.props.theme}>
+      <GroupText theme={this.props.theme}>{this.props.children}</GroupText>
+    </BorderWrapper>
+  }
+}
+
+export default CityGroup
