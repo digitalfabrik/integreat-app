cffe5001004f41851e4c8cad806df52b2d12e748
Michael Markl <marklmichael98@gmail.com>
2020-03-25 20:38:55 +0100

    WEBAPP-480: Add empty alt text for location icon

--- src/routes/landing/components/Heading.js
+++ src/routes/landing/components/Heading.js
@@ -11,6 +11,6 @@ const Logo = styled.img`
   margin: 0 auto;
 `
 
-const Heading = () => <Logo src={appConfig.locationIcon} />
+const Heading = () => <Logo src={appConfig.locationIcon} alt='' />
 
 export default Heading
