a963ce852a99e9dde765e62d7c7bb3008344955a
klinzo <klinzo@yahoo.de>
2020-04-07 17:23:45 +0200

    NATIVE-443: Fix 2nd level rtl on ios

--- src/modules/categories/components/SubCategoryListItem.js
+++ src/modules/categories/components/SubCategoryListItem.js
@@ -3,15 +3,17 @@
 import * as React from 'react'
 
 import type { ThemeType } from '../../../modules/theme/constants/theme'
-import CategoryCaption from './CategoryCaption'
 import styled from 'styled-components/native'
 import { type StyledComponent } from 'styled-components'
 import { contentDirection } from '../../i18n/contentDirection'
 
-const SubCategoryCaption = styled(CategoryCaption)`
+const SubCategoryTitleContainer = styled.View`
+  flex: 1;
+  align-self: center;
   padding: 8px 0;
   border-bottom-width: 1px;
   border-bottom-color: ${props => props.theme.colors.themeColor};
+  flex-direction: ${props => contentDirection(props.language)};
 `
 
 type FlexStyledLinkPropsType = {|
@@ -46,9 +48,9 @@ class SubCategoryListItem extends React.PureComponent<PropsType> {
     return (
       <FlexStyledLink onPress={this.onSubCategoryPress} underlayColor={theme.colors.backgroundAccentColor}
                       language={language} theme={theme}>
-        <SubCategoryCaption search='' theme={theme}>
+        <SubCategoryTitleContainer language={language} theme={theme}>
           <SubCategoryTitle theme={theme}>{subCategory.title}</SubCategoryTitle>
-        </SubCategoryCaption>
+        </SubCategoryTitleContainer>
       </FlexStyledLink>
     )
   }
