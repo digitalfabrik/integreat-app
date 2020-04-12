ff7c45927b003f522bf66e40f610ca0c481fa5c7
Abdalsamad <abdalsamad.y.m@gmail.com>
2020-04-12 14:37:02 +0300

    APICLIENT-27 Add LocalNews model and createLocalNewsEndpoint

---
+++ src/endpoints/createLocalNewsEndpoint.js
@@ -0,0 +1,27 @@
+// @flow
+
+import EndpointBuilder from '../EndpointBuilder'
+import type { JsonLocalNewsType } from '../types'
+import LocalNewsModel from '../models/LocalNewsModel'
+import moment from 'moment-timezone'
+import Endpoint from '../Endpoint'
+
+export const LOCALNEWS_ENDPOINT_NAME = 'news'
+
+type ParamsType = { city: string, language: string, channel: string }
+
+export default (baseUrl: string): Endpoint<ParamsType, Array<LocalNewsModel>> =>
+  new EndpointBuilder(LOCALNEWS_ENDPOINT_NAME)
+    .withParamsToUrlMapper((params: ParamsType): string =>
+      `${baseUrl}/${params.city}/${params.language}/wp-json/extensions/v3/fcm?channel=news`
+    )
+    .withMapper((json: Array<JsonLocalNewsType>): Array<LocalNewsModel> => json
+      .map((localNews: JsonLocalNewsType) => {
+        return new LocalNewsModel({
+          timestap: moment.tz(localNews.timestap, 'GMT'),
+          title: localNews.title,
+          message: localNews.message
+        })
+      })
+    )
+    .build()
