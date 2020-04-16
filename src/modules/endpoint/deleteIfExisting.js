6223336cec2a774b029672f22ca60e2454d35fb9
Steffen Kleinle <klinzo@yahoo.de>
2020-04-16 15:36:34 +0200

    NATIVE-499: Await promise

--- src/modules/endpoint/deleteIfExisting.js
+++ src/modules/endpoint/deleteIfExisting.js
@@ -5,7 +5,7 @@ import RNFetchBlob from 'rn-fetch-blob'
 // Android throws an error if attempting to delete non existing directories/files
 // https://github.com/joltup/rn-fetch-blob/issues/333
 const deleteIfExisting = async (path: string) => {
-  if (RNFetchBlob.fs.exists((path))) {
+  if (await RNFetchBlob.fs.exists((path))) {
     await RNFetchBlob.fs.unlink(path)
   }
 }
