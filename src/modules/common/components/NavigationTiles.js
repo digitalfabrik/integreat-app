5492c50f8fb19602db1185dc333cb15532c3ae3b
abeeralshaer <abber2000g@gmail.com]>
2020-04-19 14:35:59 +0300

    NATIVE-371: Add news nav item and make navigation scrollable

--- src/modules/common/components/NavigationTiles.js
+++ src/modules/common/components/NavigationTiles.js
@@ -5,7 +5,16 @@ import React from 'react'
 import styled from 'styled-components/native'
 import TileModel from '../models/TileModel'
 import type { ThemeType } from '../../theme/constants/theme'
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import NavigationTile from './NavigationTile'
+import { ScrollView, Dimensions } from 'react-native'
+const SCREEN_WIDTH = Dimensions.get('window').width
+const isScreenWidthBig = SCREEN_WIDTH >= 375
+const ANCHORS_WIDTH = isScreenWidthBig ? 0 : 60
+const SCROLL_VIEW_WIDTH = SCREEN_WIDTH - ANCHORS_WIDTH
+const ITEM_WIDTH = isScreenWidthBig
+  ? SCROLL_VIEW_WIDTH / 4
+  : SCROLL_VIEW_WIDTH / 3
 
 type PropsType = {|
   tiles: TileModel[],
@@ -19,24 +28,85 @@ const TilesRow = styled.View`
   align-items: center;
   justify-content: space-around;
   elevation: 1;
+  flex: 1;
   shadow-color: #000000;
   shadow-opacity: 0.2;
   shadow-radius: 1px;
   shadow-offset: 1px;
 `
-
+const Icon = styled(MaterialIcon)`
+  font-size: 30px;
+  width: ${ANCHORS_WIDTH / 2}px;
+`
 /**
  * Displays a table of NavigationTiles
  */
 class NavigationTiles extends React.Component<PropsType> {
+  state = {
+    xPosition: 0
+  };
+
+  ref_ = null;
+
+  onRightAnchorPress = () => {
+    const { xPosition } = this.state
+    this.ref_.scrollTo({ y: 0, x: xPosition + ITEM_WIDTH, animated: true })
+  };
+
+  onLeftAnchorPress = () => {
+    const { xPosition } = this.state
+    this.ref_.scrollTo({ y: 0, x: xPosition - ITEM_WIDTH, animated: true })
+  };
+
+  setRef = ref => {
+    this.ref_ = ref
+  };
+
+  onMomentumScrollEnd = ({ nativeEvent }) => {
+    this.setState({ xPosition: nativeEvent.contentOffset.x })
+  };
+
   render () {
     const { tiles, theme } = this.props
+
     return (
-      <>
-        <TilesRow theme={theme}>
-          {tiles.map(tile => <NavigationTile key={tile.path} tile={tile} theme={theme} />)}
-        </TilesRow>
-      </>
+      <TilesRow theme={theme}>
+        {!isScreenWidthBig && (
+          <Icon
+            name='keyboard-arrow-left'
+            color={theme.colors.black}
+            onPress={this.onRightAnchorPress}
+          />
+        )}
+        <ScrollView
+          horizontal
+          ref={this.setRef}
+          contentContainerStyle={{ flexGrow: 1 }}
+          style={{ width: SCROLL_VIEW_WIDTH }}
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          snapToInterval={ITEM_WIDTH}
+          decelerationRate='fast'
+          bounces={false}
+          onMomentumScrollEnd={this.onMomentumScrollEnd}
+          snapToAlignment='center'>
+          {tiles.map(tile => (
+            <NavigationTile
+              key={tile.path}
+              tile={tile}
+              theme={theme}
+              width={ITEM_WIDTH}
+            />
+          ))}
+        </ScrollView>
+        {!isScreenWidthBig && (
+          <Icon
+            name='keyboard-arrow-right'
+            color={theme.colors.black}
+            onPress={this.onLeftAnchorPress}
+          />
+        )}
+      </TilesRow>
     )
   }
 }
