a84ba2670cea221c3b0c5fa8804fa6411626131b
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2020-04-19 04:21:16 +0300

    build view for localNewsDetails

---
+++ src/routes/news/components/LocalNewsDetails.js
@@ -0,0 +1,33 @@
+// @flow
+
+import * as React from 'react'
+import styled from 'styled-components'
+import type Moment from 'moment'
+// import LastUpdateInfo from '../../../modules/common/components/LastUpdateInfo'
+// import Caption from '../../../modules/common/components/Caption'
+import Page from './../../../modules/common/components/Page'
+
+const StyledContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center
+`
+
+type PropsType = {|
+  title: string,
+    message: string,
+      timestamp: Moment,
+        language: string
+          |}
+
+// This just a placeholder until the page design is ready
+class LocalNewsDetails extends React.PureComponent<PropsType> {
+  render() {
+    const { title, message, timestamp, language, t } = this.props
+    return (
+      <Page title={title} content={message} language={language} lastUpdate={timestamp} />
+    )
+  }
+}
+
+export default LocalNewsDetails
