fddaf181bfd1c35a2a4bd31a4b9935df336b0b07
Steffen Kleinle <klinzo@yahoo.de>
2020-04-20 12:53:12 +0200

    NATIVE-500: Refactor and add more information to errors

--- src/errors/ResponseError.js
+++ src/errors/ResponseError.js
@@ -1,20 +1,80 @@
 // @flow
 
+type ResponseErrorParamsType = {|
+  endpointName: string,
+  responseMessage: string,
+  responseStatus: number,
+  url: string,
+  formData: ?FormData
+|}
+
 class ResponseError extends Error {
-  response: Response
+  _endpointName: string
+  _responseMessage: string
+  _responseStatus: number
+  _url: string
+  _formData: ?FormData
+  _message: string
 
-  getMessage = (endpointName: string, response: Response): string =>
-    `ResponseError: Failed to load the request for the ${endpointName} endpoint. Response status ${response.status}`
-
-  constructor (params: { endpointName: string, response: Response }) {
+  constructor (params: ResponseErrorParamsType) {
     super()
 
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, ResponseError)
     }
 
-    this.response = params.response
-    this.message = this.getMessage(params.endpointName, params.response)
+    const { endpointName, responseStatus, responseMessage, url, formData } = params
+
+    this._message = this.createMessage(params)
+    this._endpointName = endpointName
+    this._responseStatus = responseStatus
+    this._responseMessage = responseMessage
+    this._url = url
+    this._formData = formData
+  }
+
+  createMessage ({ formData, url, endpointName, responseStatus, responseMessage }: ResponseErrorParamsType): string {
+    const stringifyFormData = (formData: ?FormData) => {
+      if (formData) {
+        const object = {}
+        // $FlowFixMe formData.forEach exists
+        formData.forEach((value, key) => { object[key] = value })
+        return ` and the formData ${JSON.stringify(object)}`
+      }
+      return ''
+    }
+
+    return `ResponseError:
+     Failed to load the request for the ${endpointName} endpoint with the url ${url}${stringifyFormData(formData)}.
+     Received response status ${responseStatus} with the message ${responseMessage}`
+  }
+
+  get message (): string {
+    return this._message
+  }
+
+  set message (value: string) {
+    this._message = value
+  }
+
+  get endpointName (): string {
+    return this._endpointName
+  }
+
+  get responseMessage (): string {
+    return this._responseMessage
+  }
+
+  get responseStatus (): number {
+    return this._responseStatus
+  }
+
+  get url (): string {
+    return this._url
+  }
+
+  get formData (): ?FormData {
+    return this._formData
   }
 }
 
