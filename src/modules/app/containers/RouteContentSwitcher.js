ba19d5f8e9e2b86845bbbf8a950c0d8d03dfe90c
Sarah <sporck@integreat-app.de>
2020-04-21 11:52:19 +0200

    WEBAPP-436 checked wether internal extras are actually available

--- src/modules/app/containers/RouteContentSwitcher.js
+++ src/modules/app/containers/RouteContentSwitcher.js
@@ -10,6 +10,8 @@ import FailureSwitcher from '../../common/components/FailureSwitcher'
 import { getRouteConfig } from '../route-configs'
 import reduce from 'lodash/reduce'
 import { getRouteContent } from '../routeContents'
+import { SPRUNGBRETT_EXTRA } from '../route-configs/SprungbrettRouteConfig'
+import { WOHNEN_EXTRA } from '../route-configs/WohnenRouteConfig'
 
 type PropsType = {|
   location: LocationState,
@@ -27,12 +29,24 @@ class RouteContentSwitcher extends React.PureComponent<PropsType> {
     return null
   }
 
+  renderExtraFailure = (payloads: {[string]: Payload<any>}): React.Node => {
+    if (payloads.extras && !payloads.extras.isFetching && payloads.extras.data && !payloads.extras.error) {
+      if (payloads.offers && !find(payloads.extras.data, extra => extra.alias === WOHNEN_EXTRA)) {
+        return <FailureSwitcher error={new Error('The Wohnen extra is not supported.')} />
+      } else if (payloads.sprungbrettJobs && !find(payloads.extras.data, extra => extra.alias === SPRUNGBRETT_EXTRA)) {
+        return <FailureSwitcher error={new Error('The Sprunbrett extra is not supported.')} />
+      }
+    }
+    return null
+  }
+
   render () {
     const { location, allPayloads } = this.props
     const currentRoute = location.type
     const RouteContent = getRouteContent(currentRoute)
     const payloads = getRouteConfig(currentRoute).getRequiredPayloads(allPayloads)
-    return this.renderFailureLoadingComponents(payloads) ||
+    return this.renderExtraFailure(payloads) ||
+      this.renderFailureLoadingComponents(payloads) ||
       <RouteContent {...reduce(payloads, (result, value, key: string) => ({ [key]: value.data, ...result }), {})} />
   }
 }
