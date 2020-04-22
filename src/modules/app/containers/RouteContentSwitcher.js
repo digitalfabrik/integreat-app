af968acdfc9369531e46309996e3b791e42544b4
Sarah <sporck@integreat-app.de>
2020-04-22 09:52:54 +0200

    WEBAPP-436 fixed typing

--- src/modules/app/containers/RouteContentSwitcher.js
+++ src/modules/app/containers/RouteContentSwitcher.js
@@ -32,12 +32,12 @@ class RouteContentSwitcher extends React.PureComponent<PropsType> {
 
   isWaitingForExtra = (extraType: 'offers' | 'sprungbrettJobs', payloads: {[string]: Payload<any>}): boolean => {
     const alias = extraType === 'offers' ? WOHNEN_EXTRA : SPRUNGBRETT_EXTRA
-    return payloads[extraType] && find(payloads.extras.data, extra => extra.alias === alias)
+    return payloads[extraType] && !find(payloads.extras.data, extra => extra.alias === alias)
   }
 
   renderExtraFailure = (payloads: {[string]: Payload<any>}, location: LocationState): React.Node => {
     if (payloads.extras && !payloads.extras.isFetching && payloads.extras.data && !payloads.extras.error) {
-      if (!this.isWaitingForExtra('offers', payloads) && !this.isWaitingForExtra('sprungbrettJobs', payloads)) {
+      if (this.isWaitingForExtra('offers', payloads) || this.isWaitingForExtra('sprungbrettJobs', payloads)) {
         return <FailureSwitcher error={new ContentNotFoundError({
           type: 'extra',
           id: location.pathname,
