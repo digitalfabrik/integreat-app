09e82edf986e75de97feb943a1be482adeb646d2
Anies Rayyes <anies.rayyes@gmail.com>
2020-04-26 12:31:02 +0300

    APICLIENT-26 implement tunews API endpoints

---
+++ src/endpoints/createTuNewsElementEndpoint.js
@@ -0,0 +1,26 @@
+// @flow
+
+import EndpointBuilder from '../EndpointBuilder'
+import type { JsonTuNewsType } from '../types'
+import TuNewsElementModel from '../models/TunewsElementModel'
+import moment from 'moment-timezone'
+import Endpoint from '../Endpoint'
+
+export const TUNEWS_ELEMENT_ENDPOINT_NAME = 'tunews_element'
+
+type ParamsType = { id: number }
+
+export default (baseUrl: string): Endpoint<ParamsType, TuNewsElementModel> => new EndpointBuilder(TUNEWS_ELEMENT_ENDPOINT_NAME)
+  .withParamsToUrlMapper((params: ParamsType): string =>
+    `${baseUrl}/v1/news/${params.id}`
+  )
+  .withMapper((json: JsonTuNewsType): TuNewsElementModel => new TuNewsElementModel({
+    id: json.id,
+    title: json.title,
+    tags: json.tags,
+    date: moment.tz(json.date, 'GMT'),
+    content: json.content,
+    enewsno: json.enewsno
+  })
+  )
+  .build()
