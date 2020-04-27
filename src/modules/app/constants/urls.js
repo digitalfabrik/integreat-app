80a0fa74ac67a60e800857b52e9dff365e481de9
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2020-04-27 12:07:12 +0300

    add URL for localNewsUrl route

--- src/modules/app/constants/urls.js
+++ src/modules/app/constants/urls.js
@@ -10,3 +10,5 @@ export const wohnenApiBaseUrl = 'https://api.wohnen.integreat-app.de/v0'
 export const tuNewsApiBaseUrl = 'https://tunews.integreat-app.de/'
 
 export const localNewsApiBaseUrl = "https://cms-test.integreat-app.de/"
+
+export const localNewsApiBaseUrlDev = "https://cms-dev.integreat-app.de/"
