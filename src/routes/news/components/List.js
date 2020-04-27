9b37bfda31c58859ee9f5da441dbd1567cbd8f5d
abeeralshaer <abber2000g@gmail.com]>
2020-04-27 11:30:00 +0300

    NATIVE-372: make generic scroll list for news

---
+++ src/routes/news/components/List.js
@@ -0,0 +1,43 @@
+// @flow
+
+import React from 'react'
+import { FlatList } from 'react-native'
+import styled from 'styled-components/native'
+
+const Loader = styled.ActivityIndicator`
+  margin-top: 7px;
+`
+
+export default ({
+  items,
+  renderItem,
+  isFetchingMore,
+  getMoreItems,
+  renderNotItemsComponent,
+  theme,
+  status,
+  setRef,
+  isFetching
+}) => (
+  <FlatList
+    data={items}
+    ref={setRef}
+    showsVerticalScrollIndicator={false}
+    contentContainerStyle={{
+      flexGrow: 1,
+      paddingHorizontal: 10
+    }}
+    onEndReached={getMoreItems}
+    ListEmptyComponent={status === 'success' && renderNotItemsComponent}
+    ListFooterComponent={
+      (isFetchingMore || isFetching) && (
+        <Loader size='large' color={theme.colors.themeDarkColor} />
+      )
+    }
+    // onRefresh={fetchItems}
+    // refreshing={isFetching}
+    onEndReachedThreshold={1}
+    keyExtractor={(item, index) => `${index}`}
+    renderItem={renderItem}
+  />
+)
