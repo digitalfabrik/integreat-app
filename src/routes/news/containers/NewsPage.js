664108a580413e96327f9e46c34d6d14fb2db243
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2020-04-28 17:53:45 +0300

    add news check to redirect in LocalNewsPageList

--- src/routes/news/containers/NewsPage.js
+++ src/routes/news/containers/NewsPage.js
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import compose from 'lodash/fp/compose'
-import { LocalNewsModel, TuNewsElementModel } from '@integreat-app/integreat-api-client'
+import { LocalNewsModel, TuNewsElementModel, CityModel } from '@integreat-app/integreat-api-client'
 import ContentNotFoundError from '../../../modules/common/errors/ContentNotFoundError'
 import FailureSwitcher from '../../../modules/common/components/FailureSwitcher'
 import type { TFunction } from 'react-i18next'
@@ -16,26 +16,39 @@ import LocalNewsDetailsPage from './../containers/LocalNewsDetails'
 import TuNewsDetails from './TuNewsDetails'
 import TuNewsElement from '../components/TuNewsElement'
 import Link from 'redux-first-router-link'
-
+import { redirect } from 'redux-first-router'
+import type { Dispatch } from 'redux'
+import type { StoreActionType } from '../../../modules/app/StoreActionType'
+import { TUNEWS_LIST_ROUTE } from './../../../modules/app/route-configs/TuNewsListRouteConfig'
+import { CATEGORIES_ROUTE } from './../../../modules/app/route-configs/CategoriesRouteConfig'
 
 type PropsType = {|
   news: Array<LocalNewsModel>,
   city: string,
-  newsId: ?string,
   language: string,
   t: TFunction,
-  path: string
+  path: string,
+  cities: Array<CityModel>,
+  redirect: any,
 |}
 
 /**
  * Displays a list of news or a single newsElement, matching the route /<location>/<language>/news(/<id>)
  */
 export class NewsPage extends React.Component<PropsType> {
-  state: any = {
-    hasNextPage: true,
-    isNextPageLoading: false,
-    items: []
-  };
+
+
+  componentDidMount() {
+
+    const currentCity: any = this.props.cities.find(cityElement => cityElement._code === this.props.city) || {}
+
+    if (!currentCity.newsEnabled && !currentCity.tuNewsEnabled) {
+      this.props.redirect({ payload: { language: this.props.language, city: this.props.city }, type: CATEGORIES_ROUTE })
+    }
+    else if (!currentCity.newsEnabled && currentCity.tuNewsEnabled) {
+      this.props.redirect({ payload: { language: this.props.language, city: this.props.city }, type: TUNEWS_LIST_ROUTE})
+    }
+  }
 
   renderLocalNewsElement = (language: string) => (newsItem: LocalNewsModel, city: string) => (<NewsElement
     newsItem={newsItem}
@@ -46,7 +59,7 @@ export class NewsPage extends React.Component<PropsType> {
   />)
 
   render() {
-    const { news,city, newsId, language, t, path } = this.props
+    const { news, city, language, t, path, cities } = this.props
       return (
         <Tabs localNews={true} tuNews={false}>
           <NewsList items={news} noItemsMessage={t("currentlyNoLocalNews")} renderItem={this.renderLocalNewsElement(language)} city={city}/>
@@ -60,13 +73,17 @@ const mapStateTypeToProps = (state: StateType) => {
     language: state.location.payload.language,
     city: state.location.payload.city,
     news: state.news.data,
-    newsId: state.location.payload.newsId,
     path: state.location.pathname,
-    location: state.location
+    location: state.location,
+    cities: state.cities._data,
   }
 }
 
+const mapDispatchToProps = (dispatch: Dispatch<StoreActionType>) => ({
+  redirect: action => dispatch(redirect(action))
+})
+
 export default compose(
-  connect<*, *, *, *, *, *>(mapStateTypeToProps),
+  connect<*, *, *, *, *, *>(mapStateTypeToProps, mapDispatchToProps),
   withTranslation('news')
 )(NewsPage)
