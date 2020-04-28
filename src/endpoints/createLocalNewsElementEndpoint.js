b7cef228dc246e2a18509dce3d21a54127f75910
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2020-04-28 18:25:40 +0300

    APICLIENT-27: create a seperate endpoint to retrieve a singleLocalNewsItem

---
+++ src/endpoints/createLocalNewsElementEndpoint.js
@@ -0,0 +1,31 @@
+// @flow
+
+import EndpointBuilder from '../EndpointBuilder'
+import type { JsonLocalNewsType } from '../types'
+import LocalNewsModel from '../models/LocalNewsModel'
+import moment from 'moment-timezone'
+import Endpoint from '../Endpoint'
+
+export const LOCALNEWS_ELEMENT_ENDPOINT_NAME = 'newsElement'
+
+type ParamsType = { city: string, language: string, id: string }
+
+export default (
+	baseUrl: string
+): Endpoint<ParamsType, Array<JsonLocalNewsType>> =>
+	new EndpointBuilder(LOCALNEWS_ELEMENT_ENDPOINT_NAME)
+		.withParamsToUrlMapper(
+			(params: ParamsType): string =>
+				`${baseUrl}/${params.city}/${params.language}/wp-json/extensions/v3/fcm?id=${params.id}`
+		)
+		.withMapper((json: Array<JsonLocalNewsType>): Array<LocalNewsModel> =>
+			json.map((localNews: JsonLocalNewsType) => {
+				return new LocalNewsModel({
+					id: localNews.id,
+					timestamp: moment.tz(localNews.timestamp, "GMT"),
+					title: localNews.title,
+					message: localNews.message,
+				});
+			})
+		)
+		.build();
