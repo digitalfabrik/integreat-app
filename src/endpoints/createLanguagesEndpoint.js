c4865bee53657f69c9b2d90e63cbff835b76a380
Abdalsamad <abdalsamad.y.m@gmail.com>
2020-04-30 14:07:20 +0300

    APICLIENT-27: Update the languages endpoint to account for tunews api

--- src/endpoints/createLanguagesEndpoint.js
+++ src/endpoints/createLanguagesEndpoint.js
@@ -10,19 +10,31 @@ export const LANGUAGES_ENDPOINT_NAME = 'languages'
 
 type ParamsType = { city: ?string }
 
-export default (baseUrl: string): Endpoint<ParamsType, Array<LanguageModel>> =>
+export default (baseUrl: string, isTunews: ?boolean): Endpoint<ParamsType, Array<LanguageModel>> =>
   new EndpointBuilder(LANGUAGES_ENDPOINT_NAME)
     .withParamsToUrlMapper(params => {
       if (!params.city) {
         throw new ParamMissingError(LANGUAGES_ENDPOINT_NAME, 'city')
       }
+      if (isTunews) {
+        return `${baseUrl}/v1/news/languages`
+      }
       return `${baseUrl}/${params.city}/de/wp-json/extensions/v3/languages`
     })
-    .withMapper((json: Array<JsonLanguageType>) => json
-      .map(language => new LanguageModel(
-        language.code,
-        language.native_name
-      ))
-      .sort((lang1, lang2) => lang1.code.localeCompare(lang2.code))
+    .withMapper((json: Array<JsonLanguageType>) => {
+      return json
+        .map(language => {
+          if (isTunews) {
+            return new LanguageModel(
+              language.code,
+              language.name
+            )
+          }
+          return new LanguageModel(
+            language.code,
+            language.native_name
+          )
+        })
+        .sort((lang1, lang2) => lang1.code.localeCompare(lang2.code))}
     )
     .build()
