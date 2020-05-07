04a025ae08c259cc58c6194241dfe2471f9662fb
Abdalsamad <abdalsamad.y.m@gmail.com>
2020-05-07 11:44:29 +0300

    APICLIENT-26: Add new endpoint for tunews languages

--- src/endpoints/createTunewsLanguagesEndpoint.js
+++ src/endpoints/createTunewsLanguagesEndpoint.js
@@ -7,27 +7,27 @@ import MappingError from '../errors/MappingError'
 import type { JsonLanguageType } from '../types'
 import Endpoint from '../Endpoint'
 
-export const LANGUAGES_ENDPOINT_NAME = 'languages'
+export const TUNEWS_LANGUAGES_ENDPOINT_NAME = 'languages'
 
 type ParamsType = { city: ?string }
 
 export default (baseUrl: string): Endpoint<ParamsType, Array<LanguageModel>> =>
-  new EndpointBuilder(LANGUAGES_ENDPOINT_NAME)
+  new EndpointBuilder(TUNEWS_LANGUAGES_ENDPOINT_NAME)
     .withParamsToUrlMapper(params => {
       if (!params.city) {
-        throw new ParamMissingError(LANGUAGES_ENDPOINT_NAME, 'city')
+        throw new ParamMissingError(TUNEWS_LANGUAGES_ENDPOINT_NAME, 'city')
       }
-      return `${baseUrl}/${params.city}/de/wp-json/extensions/v3/languages`
+      return `${baseUrl}/v1/news/languages`
     })
     .withMapper((json: Array<JsonLanguageType>) => {
       return json
         .map(language => {
-          if (!language.native_name) {
-            throw new MappingError(LANGUAGES_ENDPOINT_NAME, `Unexpected json format. Response did not contain ${language.native_name}`)
+          if (!language.name) {
+            throw new MappingError(TUNEWS_LANGUAGES_ENDPOINT_NAME, `Unexpected json format. Response did not contain ${language.name}`)
           }
           return new LanguageModel(
             language.code,
-            language.native_name
+            language.name
           )
         })
         .sort((lang1, lang2) => lang1.code.localeCompare(lang2.code))
