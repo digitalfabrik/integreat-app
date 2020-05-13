f516e837c75dd77e7098b8471c84e0969fbd7b38
abeeralshaer <abber2000g@gmail.com]>
2020-05-14 02:05:50 +0300

    NATIVE-306: rephrase comment

--- src/modules/layout/containers/HeaderContainer.js
+++ src/modules/layout/containers/HeaderContainer.js
@@ -42,7 +42,7 @@ const mapStateToProps = (state: StateType, ownProps: OwnPropsType): StatePropsTy
   const route = state.cityContent?.categoriesRouteMapping[routeKey] || state.cityContent?.eventsRouteMapping[routeKey] || state.cityContent?.newsRouteMapping[routeKey]
   const languages = state.cityContent?.languages
 
-  // Note: added this to prevent re-rendering when city is there.
+  // prevent re-rendering when city is there.
   const cities = state.cities.models || []
   const cityCode = state.cityContent?.city
   const categoriesAvailable = state.cityContent?.searchRoute !== null
