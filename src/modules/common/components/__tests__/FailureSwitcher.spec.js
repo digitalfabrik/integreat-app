a7bed36d8d0fe13c268f527e74142143b96bc771
Sarah <sporck@integreat-app.de>
2020-05-22 11:40:32 +0200

    WEBAPP-562 replace render tests with described each

--- src/modules/common/components/__tests__/FailureSwitcher.spec.js
+++ src/modules/common/components/__tests__/FailureSwitcher.spec.js
@@ -12,39 +12,29 @@ describe('FailureSwitcher', () => {
 
   const language = 'de'
 
-  describe('render component not found failure', () => {
-    it('should render a category not found failure and match snapshot', () => {
-      const error = new ContentNotFoundError({ type: 'category', id: 'willkommen', language, city })
-      const renderContentNotFoundComponent = FailureSwitcher.renderContentNotFoundComponent(error)
-      expect(renderContentNotFoundComponent).toMatchSnapshot()
+  describe.each`
+    type          | id
+    ${'category'} | ${'willkommen'}
+    ${'extra'}    | ${'sprungbrett'}
+    ${'event'}    | ${'1234'}
+    ${'poi'}      | ${'1234'}
+    `('render $type component not found failure',
+  ({ type, id }) => {
+    it(`should render a ${type} not found failure and match snapshot`, () => {
+      const error = new ContentNotFoundError({ type, id, language, city })
+      const component = FailureSwitcher.renderContentNotFoundComponent(error)
+      expect(component).toMatchSnapshot()
     })
 
-    it('should render a extra not found failure and match snapshot', () => {
-      const error = new ContentNotFoundError({ type: 'extra', id: 'sprungbrett', language, city })
-      expect(FailureSwitcher.renderContentNotFoundComponent(error)).toMatchSnapshot()
+    it('should call render content not found component and create a Failure component', () => {
+      const error = new ContentNotFoundError({ type, id, language, city })
+      const spy = jest.spyOn(FailureSwitcher, 'renderContentNotFoundComponent')
+      const component = mount(<FailureSwitcher error={error} />)
+      expect(spy).toHaveBeenCalledWith(error)
+      expect(component.find(Failure)).toHaveLength(1)
+
+      spy.mockRestore()
     })
-
-    it('should render a event not found failure and match snapshot', () => {
-      const error = new ContentNotFoundError({ type: 'event', id: '1234', language, city })
-      expect(FailureSwitcher.renderContentNotFoundComponent(error)).toMatchSnapshot()
-    })
-
-    it('should render a poi not found failure and match snapshot', () => {
-      const error = new ContentNotFoundError({ type: 'poi', id: 'poiId', language, city })
-      expect(FailureSwitcher.renderContentNotFoundComponent(error)).toMatchSnapshot()
-    })
-  })
-
-  it('should render a content not found failure if there is a ContentNotFoundError', () => {
-    const error = new ContentNotFoundError({ type: 'extra', id: 'sprungbrett', city, language })
-    const spy = jest.spyOn(FailureSwitcher, 'renderContentNotFoundComponent')
-
-    const component = mount(<FailureSwitcher error={error} />)
-
-    expect(spy).toHaveBeenCalledWith(error)
-    expect(component.find(Failure)).toHaveLength(1)
-
-    spy.mockRestore()
   })
 
   it('should render a failure as default', () => {
