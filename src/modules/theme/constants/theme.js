4d10fe7e9f8a7d6ada24b1db19f925dc59834a71
Steffen Kleinle <klinzo@yahoo.de>
2020-05-25 14:31:45 +0200

    WEBAPP-553: Rebase

--- src/modules/theme/constants/theme.js
+++ src/modules/theme/constants/theme.js
@@ -1,33 +1,10 @@
 // @flow
 
-import type { ColorsType } from './colors'
-import { brightColors, darkColors } from './colors'
-import type { DimensionsType } from './dimensions'
-import dimensions from './dimensions'
-import type { FontsType } from './fonts'
-import fonts from './fonts'
-import type { HelpersType } from './helpers'
-import helpers from './helpers'
+import appConfig from '../../app/constants/appConfig'
+import type { ThemeType } from '../../../../build/themes/ThemeType'
 
-export type ThemeType = {
-  colors: ColorsType,
-  dimensions: DimensionsType,
-  fonts: FontsType,
-  helpers: HelpersType
-}
+export const brightTheme: ThemeType = appConfig.theme
 
-export const brightTheme = {
-  colors: brightColors,
-  dimensions,
-  fonts,
-  helpers
-}
-
-export const darkTheme = {
-  colors: darkColors,
-  dimensions,
-  fonts,
-  helpers
-}
+export const darkTheme: ThemeType = appConfig.darkTheme
 
 export default brightTheme
