2cb5512eb37022f0f2397bbd1fd801305002ae1c
Michael Markl <marklmichael98@gmail.com>
2020-05-30 15:36:19 +0200

    WEBAPP-485: Fix breadcrumbs

--- src/modules/common/components/__tests__/Breadcrumbs.spec.js
+++ src/modules/common/components/__tests__/Breadcrumbs.spec.js
@@ -4,15 +4,16 @@ import React from 'react'
 import { shallow } from 'enzyme'
 
 import Breadcrumbs from '../Breadcrumbs'
+import BreadcrumbModel from '../../BreadcrumbModel'
 
 describe('Breadcrumbs', () => {
   it('should render and match snapshot', () => {
     const wrapper = shallow(
-      <Breadcrumbs direction='rtl'>
-        <a href='link1'>asdf</a>
-        <a href='link1/test'>test</a>
-      </Breadcrumbs>
-    )
+      <Breadcrumbs direction='rtl' breadcrumbs={[
+        new BreadcrumbModel({ title: 'Home', link: '/', node: <a href='/'>Home</a> }),
+        new BreadcrumbModel({ title: 'Subcategory', link: '/sub', node: <a href='/sub'>Subcategory</a> }),
+        new BreadcrumbModel({ title: 'ThisSite', link: '/sub/current', node: <a href='/sub/current'>ThisSite</a> }),
+      ]} />)
 
     expect(wrapper).toMatchSnapshot()
   })
