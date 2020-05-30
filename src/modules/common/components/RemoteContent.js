a33a5678fdbc5c06a56e02fa42bdfe747fd9c5c9
Michael Markl <marklmichael98@gmail.com>
2020-05-30 23:15:50 +0200

    WEBAPP-507: Make pre elements scroll in x direction

--- src/modules/common/components/RemoteContent.js
+++ src/modules/common/components/RemoteContent.js
@@ -13,20 +13,20 @@ const SandBox = styled.div`
     text-align: center;
     list-style-position: inside;
   `}
-  
+
   & img {
     max-width: 100%;
     max-height: 100%;
     object-fit: contain;
   }
-  
+
   & table {
     display: block;
     width: 100% !important;
     height: auto !important; /* need important because of bad-formatted remote-content */
     overflow: auto;
   }
-  
+
   & tbody,
   & thead {
     display: table; /* little bit hacky, but works in all browsers, even IE11 :O */
@@ -41,17 +41,21 @@ const SandBox = styled.div`
   & td {
     border: 1px solid ${props => props.theme.colors.backgroundAccentColor};
   }
-  
+
   & a {
     overflow-wrap: break-word;
   }
-  
+
   & details > * {
     padding: 0 25px;
   }
-  
+
   & details > summary {
     padding: 0;
+  }
+
+  & pre {
+    overflow-x: auto;
   }
 `
 
