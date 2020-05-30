cfff199bee7abafbe24924904c18796077110554
Michael Markl <marklmichael98@gmail.com>
2020-05-30 22:56:44 +0200

    WEBAPP-477: Add test for featuredImageToSrcSet

--- src/modules/common/utils/__tests__/featuredImageToSrcSet.spec.js
+++ src/modules/common/utils/__tests__/featuredImageToSrcSet.spec.js
@@ -4,15 +4,15 @@ import featuredImageToSrcSet from '../featuredImageToSrcSet'
 import { FeaturedImageModel } from '@integreat-app/integreat-api-client'
 
 describe('featuredImageToSrcSet', () => {
-  it('should calculate the correct sizes for srcSet', () => {
+  it('should calculate the correct sizes for srcSet and encode urls', () => {
     const srcSet = featuredImageToSrcSet(new FeaturedImageModel({
       description: 'Beschreibung',
-      thumbnail: { url: 'thumbnail', width: 10, height: 15 },
+      thumbnail: { url: 'thumbnail ', width: 10, height: 15 },
       medium: { url: 'medium', width: 20, height: 30 },
       large: { url: 'large', width: 30, height: 45 },
       full: { url: 'full', width: 40, height: 60 }
     }), 20)
 
-    expect(srcSet).toEqual('thumbnail 0.5x, medium 1x, large 1.5x, full 2x')
+    expect(srcSet).toEqual('thumbnail%20 0.5x, medium 1x, large 1.5x, full 2x')
   })
 })
