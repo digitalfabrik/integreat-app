ad63ece87060356b2f58f83c02b3237100b43751
Sarah <sarah-diana.sporck@gmx.de>
2020-06-05 17:39:12 +0200

    remove unneccessarily complex function, and fixed type

--- src/endpoints/createOffersEndpoint.js
+++ src/endpoints/createOffersEndpoint.js
@@ -3,28 +3,17 @@
 import OfferModel from '../models/OfferModel'
 import EndpointBuilder from '../EndpointBuilder'
 
-import ParamMissingError from '../errors/ParamMissingError'
 import type { JsonOfferPostType, JsonOfferType } from '../types'
 import Endpoint from '../Endpoint'
 
 export const OFFERS_ENDPOINT_NAME = 'extras'
 
-const createPostMap = (jsonPost: JsonOfferPostType): Map<string, string> => {
-  const map = new Map()
-  Object.keys(jsonPost).forEach(key => map.set(key, jsonPost[key]))
-  return map
-}
+const createPostMap = (jsonPost: JsonOfferPostType): Map<string, string> => new Map(Object.entries(jsonPost))
 
-type ParamsType = { city: ?string, language: ?string }
+type ParamsType = { city: string, language: string }
 
 export default (baseUrl: string): Endpoint<ParamsType, Array<OfferModel>> => new EndpointBuilder(OFFERS_ENDPOINT_NAME)
   .withParamsToUrlMapper(params => {
-    if (!params.city) {
-      throw new ParamMissingError(OFFERS_ENDPOINT_NAME, 'city')
-    }
-    if (!params.language) {
-      throw new ParamMissingError(OFFERS_ENDPOINT_NAME, 'language')
-    }
     return `${baseUrl}/${params.city}/${params.language}/wp-json/extensions/v3/extras`
   })
   .withMapper((json: Array<JsonOfferType>) => json
