8a1fbe1585b98ad11edc436882c6de56b3850b58
abeeralshaer <abber2000g@gmail.com]>
2020-06-09 06:57:21 +0300

    new lib to scrollable header

--- android/app/src/main/java/tuerantuer/app/integreat/MainApplication.java
+++ android/app/src/main/java/tuerantuer/app/integreat/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import cl.json.ShareApplication;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
