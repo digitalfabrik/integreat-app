0a2111593921a562175203f638a27461f97c0f7e
Nikolas Klug <klug.nikolas@googlemail.com>
2020-06-10 14:02:13 +0200

    WEBAPP-584: Fix flow errors

--- src/modules/app/route-configs/__tests__/MainDisclaimerRouteConfig.spec.js
+++ src/modules/app/route-configs/__tests__/MainDisclaimerRouteConfig.spec.js
@@ -38,7 +38,7 @@ describe('MainDisclaimerRouteConfig', () => {
       pathname: '/disclaimer',
       type: mainDisclaimerRouteConfig.name
     })
-    expect(mainDisclaimerRouteConfig.getPageTitle({ t, payloads: undefined, location, cityName: null }))
+    expect(mainDisclaimerRouteConfig.getPageTitle({ t, payloads: {}, location, cityName: null }))
       .toBe('pageTitles.mainDisclaimer')
   })
 
@@ -53,7 +53,7 @@ describe('MainDisclaimerRouteConfig', () => {
       type: mainDisclaimerRouteConfig.name
     })
     expect(mainDisclaimerRouteConfig.getLanguageChangePath({
-      payloads: undefined,
+      payloads: {},
       location,
       language: 'de'
     })).toBeNull()
@@ -65,6 +65,6 @@ describe('MainDisclaimerRouteConfig', () => {
       pathname: '/disclaimer',
       type: mainDisclaimerRouteConfig.name
     })
-    expect(mainDisclaimerRouteConfig.getFeedbackTargetInformation({ payloads: undefined, location })).toBeNull()
+    expect(mainDisclaimerRouteConfig.getFeedbackTargetInformation({ payloads: {}, location })).toBeNull()
   })
 })
