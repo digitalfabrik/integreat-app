f93201a83bf5733e7fa59ccf5eab060f4f155d16
Steffen Kleinle <klinzo@yahoo.de>
2020-06-18 18:35:10 +0200

    NATIVE-532: Fix tests

--- build-configs/themes/integreat/fonts.js
+++ build-configs/themes/integreat/fonts.js
@@ -4,13 +4,13 @@ import type { FontsType } from '../common/fonts'
 import { commonFonts } from '../common/fonts'
 
 const fonts: FontsType = {
-  ...commonFonts,
   decorativeFontBold: 'Raleway-Bold',
   decorativeFontRegular: 'Raleway-Regular',
   contentFontRegular: 'OpenSans-Regular',
   contentFontBold: 'OpenSans-Bold',
   arabicWebviewFontFamilies: '\'OpenSans\', \'Lateef\', sans-serif',
-  webviewFontFamilies: '\'OpenSans\', sans-serif'
+  webviewFontFamilies: '\'OpenSans\', sans-serif',
+  ...commonFonts
 }
 
 export default fonts
