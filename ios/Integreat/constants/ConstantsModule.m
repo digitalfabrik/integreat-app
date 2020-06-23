4e948eeb393cb5dc2134fb910d38b3b521b825eb
Sarah <sarah-diana.sporck@gmx.de>
2020-06-23 11:32:07 +0200

    Squashed 'locales/' changes from 61cf6901..3c04c8a2
    3c04c8a2 WEBAPP-497: basic refractoring
    REVERT: 61cf6901 Merge pull request #13 from Integreat/NATIVE-385-search-placeholder
    REVERT: 1e15c3cc NATIVE-385: Change back integreat-locales.iml
    REVERT: 69524946 Merge branch 'NATIVE-482-add-accessibility-locales'
    REVERT: 6834798c NATIVE-482: Change German translation
    REVERT: 960f3404 NATIVE-385: Change the search placeholder.
    REVERT: d8665eec NATIVE-482: Add 'refresh nearby locations' locale
    
    git-subtree-dir: locales
    git-subtree-split: 3c04c8a2864a32e042d485eb63b849e8c4ef48f5

--- ios/Integreat/constants/ConstantsModule.m
+++
@@ -1,9 +0,0 @@
-//
-//  ConstantsModule.m
-//  Integreat
-//
-
-#import "React/RCTBridgeModule.h"
-
-@interface RCT_EXTERN_MODULE(RNNativeConstants, NSObject)
-@end
--- src/routes/extras/constants/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-export const SPRUNGBRETT_ROUTE = 'SprungbrettExtra'
-export const SPRUNGBRETT_EXTRA = 'sprungbrett'
-
-export const WOHNEN_ROUTE = 'WohnenExtra'
-export const WOHNEN_EXTRA = 'wohnen'
-
-export const EXTERNAL_EXTRA_ROUTE = 'ExternalExtra'
