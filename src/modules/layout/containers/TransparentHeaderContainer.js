d6661d8a2f72a33f01742460573a8364036b9745
Sarah <sarah-diana.sporck@gmx.de>
2020-06-23 11:32:07 +0200

    Squashed 'locales/' changes from 61cf6901..3c04c8a2
    3c04c8a2 WEBAPP-497: basic refractoring
    REVERT: 61cf6901 Merge pull request #13 from Integreat/NATIVE-385-search-placeholder
    REVERT: 1e15c3cc NATIVE-385: Change back integreat-locales.iml
    REVERT: 69524946 Merge branch 'NATIVE-482-add-accessibility-locales'
    REVERT: 6834798c NATIVE-482: Change German translation
    REVERT: 960f3404 NATIVE-385: Change the search placeholder.
    REVERT: d8665eec NATIVE-482: Add 'refresh nearby locations' locale
    
    git-subtree-dir: locales
    git-subtree-split: 3c04c8a2864a32e042d485eb63b849e8c4ef48f5

--- src/modules/layout/containers/TransparentHeaderContainer.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import TransparentHeader from '../components/TransparentHeader'
-import { withTranslation } from 'react-i18next'
-
-import withTheme from '../../theme/hocs/withTheme'
-import { withNavigation } from 'react-navigation'
-
-export default withTheme()(withNavigation(withTranslation('layout')(TransparentHeader)))
