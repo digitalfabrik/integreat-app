978c4c5a3192aff0b4ba19de65612ed3ca664d3f
Steffen Kleinle <klinzo@yahoo.de>
2020-06-29 13:02:32 +0200

    NATIVE-556: Fix flow again

--- build-configs/index.js
+++ build-configs/index.js
@@ -1,16 +1,18 @@
 // @flow
 
+import type { BuildConfigType } from './configs/BuildConfigType'
+
 const integreatBuildConfig = require('../build-configs/configs/integreat')
 const integreatTestCmsBuildConfig = require('../build-configs/configs/integreat-test-cms')
 const malteBuildConfig = require('../build-configs/configs/malte')
 
-export const buildConfigs = {
+export const buildConfigs: { [string]: BuildConfigType } = {
   integreat: integreatBuildConfig,
   'integreat-test-cms': integreatTestCmsBuildConfig,
   malte: malteBuildConfig
 }
 
-const loadBuildConfig = buildConfigName => {
+const loadBuildConfig = (buildConfigName: string): BuildConfigType => {
   if (!buildConfigName) {
     throw Error('No BUILD_CONFIG_NAME supplied!')
   }
