82d04c2cffc79962af2bfabd1e246592961c81bd
Maximilian Ammann <max@maxammann.org>
2020-07-01 15:39:03 +0200

    Squashed 'locales/' changes from 61cf6901c..ffbc48286
    ffbc48286 NATIVE-306: Add missing keys for news feature
    434fbf821 Merge pull request #15 from Integreat/WEBAPP-581-ImplementNewNavBar
    11cd5308a WEBAPP-581: Change categories to localInformation
    
    git-subtree-dir: locales
    git-subtree-split: ffbc48286dbc69425f3cff8dad7c2c5bf6bfd3ea

--- e2e/pages/README.md
+++
@@ -1,1 +0,0 @@
-Classes in this directory follow the PageObject pattern: https://moduscreate.com/blog/protractor-automated-tests-structure/
--- src/modules/endpoint/__tests__/hashUrl.spec.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import hashUrl from '../hashUrl'
-
-describe('hashUrl', () => {
-  it('should hash url using md5 and encode to hex', () => {
-    expect(hashUrl('https://ex.am/p.l/thumbnail.png')).toBe('81a74f17bb169f4dad2f59bb2e4670f9')
-  })
-})
