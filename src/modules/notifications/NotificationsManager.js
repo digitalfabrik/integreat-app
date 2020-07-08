071b16e331534c70bca7098609e67c292e8bba3f
abeeralshaer <abber2000g@gmail.com]>
2020-07-08 22:08:15 +0300

    NATIVE-22: log status in all cases

--- src/modules/notifications/NotificationsManager.js
+++ src/modules/notifications/NotificationsManager.js
@@ -2,32 +2,19 @@
 
 import messaging from '@react-native-firebase/messaging'
 
-const requestUserPermissionForPushNotifications = async () => {
+export const requestPushNotificationPermission = async () => {
   const authStatus = await messaging().requestPermission()
-
-  const enabled =
-    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-    authStatus === messaging.AuthorizationStatus.PROVISIONAL
-
-  if (enabled) {
-    console.debug('Authorization status:', authStatus)
-  }
+  console.debug('Authorization status:', authStatus)
 }
 
-const unSubscribeToPreviousCityTopic = async (prevCity, prevLanguage) => {
+export const unsubscribeFromPreviousCity = async (prevCity, prevLanguage) => {
   messaging()
     .unsubscribeFromTopic(`${prevCity}-${prevLanguage}-news`)
     .then(() => console.debug(`Unsubscribed from ${prevCity}-${prevLanguage}-news topic!`))
 }
 
-const subscribeToNewCityTopic = async (newCity, newLanguage) => {
+export const subscribeToCity = async (newCity, newLanguage) => {
   messaging()
     .subscribeToTopic(`${newCity}-${newLanguage}-news`)
     .then(() => console.debug(`Subscribed to ${newCity}-${newLanguage}-news topic!`))
-}
-
-module.exports = {
-  requestUserPermissionForPushNotifications,
-  subscribeToNewCityTopic,
-  unSubscribeToPreviousCityTopic
 }
