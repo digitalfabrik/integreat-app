6676fb3cce67902f52321db38f8d1a4502f1dcb2
Steffen Kleinle <klinzo@yahoo.de>
2020-07-08 23:34:27 +0200

    NATIVE-556: Add e2e build config to loadBuildConfigs

--- build-configs/index.js
+++ build-configs/index.js
@@ -3,11 +3,13 @@
 import type { BuildConfigType } from './configs/BuildConfigType'
 import integreatBuildConfig from '../build-configs/configs/integreat'
 import integreatTestCmsBuildConfig from '../build-configs/configs/integreat-test-cms'
+import integreatE2eBuildConfig from '../build-configs/configs/integreat-e2e'
 import malteBuildConfig from '../build-configs/configs/malte'
 
 export const buildConfigs: { [string]: BuildConfigType } = {
   integreat: integreatBuildConfig,
   'integreat-test-cms': integreatTestCmsBuildConfig,
+  'integreat-e2e': integreatE2eBuildConfig,
   malte: malteBuildConfig
 }
 
