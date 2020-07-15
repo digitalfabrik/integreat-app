2de0d22067d9b15343f67df2bf35d788ca89f63b
Max Ammann <max@maxammann.org>
2020-07-15 14:02:33 +0200

    NATIVE-22: Minor changes
    Co-authored-by: Steffen Kleinle <klinzo@yahoo.de>

--- src/modules/app/Permissions.js
+++ src/modules/app/Permissions.js
@@ -24,6 +24,5 @@ export const requestLocationPermission = async (): RESULTS => {
 }
 
 export const requestPushNotificationPermission = async (): RESULTS => {
-  // TODO NATIVE-399 Really request push notification permissions
   return NotificationsManager.requestPushNotificationPermission()
 }
