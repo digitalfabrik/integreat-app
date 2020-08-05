6530109cc383cf7db0ca758bc7d5c09311e0afc1
Steffen Kleinle <klinzo@yahoo.de>
2020-08-05 18:42:06 +0200

    NATIVE-587: Add script to parse release notes

---
+++ tools/constants.js
@@ -0,0 +1,5 @@
+const RELEASE_NOTES_DIR = 'release-notes'
+const UNRELEASED_DIR = 'unreleased'
+const GITKEEP_FILE = '.gitkeep'
+
+module.exports = { RELEASE_NOTES_DIR, UNRELEASED_DIR, GITKEEP_FILE }
