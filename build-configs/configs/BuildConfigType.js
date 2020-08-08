17a933b5654e82d315ec743bd6b5fb4d29f545cf
Maximilian Ammann <max@maxammann.org>
2020-08-08 17:52:19 +0200

    NATIVE-541: Add google services information

--- build-configs/configs/BuildConfigType.js
+++ build-configs/configs/BuildConfigType.js
@@ -8,6 +8,34 @@ export type FeatureFlagsType = {|
   introSlides: boolean
 |}
 
+type AndroidGoogleServicesConfig = {|
+  google_app_id: string,
+  gcm_defaultSenderId: string,
+  default_web_client_id: string,
+  ga_trackingId: null | string,
+  firebase_database_url: string,
+  google_api_key: string,
+  google_crash_reporting_api_key: string,
+  project_id: string
+|}
+
+type AppleGoogleServicesConfig = {
+  CLIENT_ID: string,
+  REVERSED_CLIENT_ID: string,
+  API_KEY: string,
+  GCM_SENDER_ID: string,
+  PLIST_VERSION: string,
+  BUNDLE_ID: string,
+  PROJECT_ID: string,
+  STORAGE_BUCKET: string,
+  IS_ADS_ENABLED: boolean,
+  IS_ANALYTICS_ENABLED: boolean,
+  IS_APPINVITE_ENABLED: boolean,
+  IS_GCM_ENABLED: boolean,
+  IS_SIGNIN_ENABLED: boolean,
+  GOOGLE_APP_ID: string,
+  DATABASE_URL: string
+}
 export type BuildConfigType = {|
   appName: string,
   cmsUrl: string,
@@ -20,9 +48,11 @@ export type BuildConfigType = {|
   e2e?: boolean,
   android: {|
     applicationId: string,
-    googleServices: boolean
+    // According to https://developers.google.com/android/guides/google-services-plugin#processing_the_json_file
+    googleServicesConfig: ?AndroidGoogleServicesConfig
   |},
   ios: {|
-    bundleIdentifier: string
+    bundleIdentifier: string,
+    googleServicesConfig: ?AppleGoogleServicesConfig
   |}
 |}
