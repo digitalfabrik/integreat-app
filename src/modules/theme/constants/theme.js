c798594f5619df4147c3677df489e557bb4ee577
Steffen Kleinle <klinzo@yahoo.de>
2020-08-15 16:19:43 +0200

    WEBAPP-586: Refactor build config env and convert helper to function

--- src/modules/theme/constants/theme.js
+++ src/modules/theme/constants/theme.js
@@ -5,8 +5,8 @@ import type { ThemeType } from '../../../../build-configs/themes/ThemeType'
 
 export type { ThemeType }
 
-export const brightTheme: ThemeType = buildConfig.theme
+export const brightTheme: ThemeType = buildConfig().theme
 
-export const darkTheme: ThemeType = buildConfig.darkTheme
+export const darkTheme: ThemeType = buildConfig().darkTheme
 
 export default brightTheme
