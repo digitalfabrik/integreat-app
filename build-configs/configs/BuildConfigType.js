91b0a78629774e94c932b95f44d8a2fbe7722e37
Steffen Kleinle <klinzo@yahoo.de>
2020-08-15 16:19:16 +0200

    WEBAPP-586: Add locale override to build config

--- build-configs/configs/BuildConfigType.js
+++ build-configs/configs/BuildConfigType.js
@@ -3,6 +3,8 @@
 import type { ThemeType } from '../themes/ThemeType'
 import type { FeatureFlagsType } from './featureFlags'
 
+export type LocalesType = { [namespace: string]: { [language: string]: { [key: string]: string } } }
+
 export type BuildConfigType = {|
   appName: string,
   cmsUrl: string,
@@ -11,6 +13,7 @@ export type BuildConfigType = {|
   darkTheme: ThemeType,
   manifestUrl?: string,
   itunesAppId?: string,
+  localesOverride?: LocalesType,
   logoWide: string,
   locationIcon: string,
   internalLinksHijackPattern: string,
--- build-configs/configs/malte/index.js
+++ build-configs/configs/malte/index.js
@@ -3,6 +3,7 @@
 import malteTheme, { darkTheme as darkMalteTheme } from '../../themes/malte'
 import type { BuildConfigType } from '../BuildConfigType'
 import featureFlags from '../featureFlags'
+import malteOverrideLocales from '../../../locales/malte-locales.json'
 
 const MalteBuildConfig: BuildConfigType = {
   appName: 'Malte',
@@ -11,6 +12,7 @@ const MalteBuildConfig: BuildConfigType = {
   // TODO WEBAPP-567: Add itunesAppId
   cmsUrl: 'https://cms.malteapp.de',
   featureFlags,
+  localesOverride: malteOverrideLocales,
   locationIcon: '/location-big.svg',
   logoWide: '/malteser-logo.png',
   internalLinksHijackPattern: 'https?:\\/\\/malteapp\\.de(?!\\/[^/]*\\/(wp-content|wp-admin|wp-json)\\/.*).*'
