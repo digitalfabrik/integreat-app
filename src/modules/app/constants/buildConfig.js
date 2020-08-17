ab322a7cc1cf9afa8cb604a637172d37f22ce4be
Steffen Kleinle <klinzo@yahoo.de>
2020-08-17 13:44:43 +0200

    NATIVE-597: Make location marker configurable

--- src/modules/app/constants/buildConfig.js
+++ src/modules/app/constants/buildConfig.js
@@ -5,22 +5,26 @@ import loadBuildConfig from '../../../../build-configs'
 import { INTEGREAT_ICONS } from '../../../../build-configs/configs/integreat'
 import { MALTE_ICONS } from '../../../../build-configs/configs/malte'
 import integreatAppLogo from '../../../../build-configs/configs/integreat/assets/app-logo.png'
+import integreatLocationMarker from '../../../../build-configs/configs/integreat/assets/location-marker.svg'
 import malteAppLogo from '../../../../build-configs/configs/malte/assets/app-logo.svg'
+import malteLocationMarker from '../../../../build-configs/configs/malte/assets/location-marker.svg'
 
 const buildConfigIcons = (iconSet: string) => {
   if (iconSet === INTEGREAT_ICONS) {
     return {
-      appLogo: integreatAppLogo
+      appLogo: integreatAppLogo,
+      locationMarker: integreatLocationMarker
     }
   } else if (iconSet === MALTE_ICONS) {
     return {
-      appLogo: malteAppLogo
+      appLogo: malteAppLogo,
+      locationMarker: malteLocationMarker
     }
   }
   throw new Error(`Unknown icon set ${iconSet}. Check your build config!`)
 }
 
-const buildConfig = (): {| ...BuildConfigType, appLogo: string |} => {
+const buildConfig = (): {| ...BuildConfigType, appLogo: string, locationMarker: string |} => {
   const config = loadBuildConfig(process.env.BUILD_CONFIG_NAME)
   const icons = buildConfigIcons(config.iconSet)
   return { ...icons, ...config }
