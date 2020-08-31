d7275a5081f48b00ece2cf51bf3e116c98819a81
Maximilian Ammann <max@maxammann.org>
2020-08-31 15:22:05 +0200

    NATIVE-615: Add buildConfig mock

---
+++ src/modules/app/constants/__mocks__/buildConfig.js
@@ -0,0 +1,28 @@
+// @flow
+
+import type {
+  BuildConfigType
+} from '../../../../../build-configs/configs/BuildConfigType'
+import { darkTheme, lightTheme } from '../../../../../build-configs/themes/integreat'
+import { INTEGREAT_ICONS } from '../../../../../build-configs/configs/integreat'
+
+export const buildConfigIconSet = (): {| appLogo: string, locationMarker: string |} => {
+  throw new Error('Mock not yet implemented!')
+}
+
+const buildConfig = (): BuildConfigType => ({
+  appName: 'Integreat',
+  lightTheme,
+  darkTheme,
+  iconSet: INTEGREAT_ICONS,
+  cmsUrl: 'https://cms.integreat-app.de',
+  switchCmsUrl: 'https://cms-test.integreat-app.de',
+  allowedHostNames: ['cms.integreat-app.de', 'cms-test.integreat-app.de'],
+  featureFlags: {
+    pois: false,
+    newsStream: false,
+    introSlides: true
+  }
+})
+
+export default buildConfig
