fc0eb9f07eb1255fb87bd8f2f706a5654334a033
Maximilian Ammann <max@maxammann.org>
2020-09-15 15:14:35 +0200

    NATIVE-613: Remove the ignore warning

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 // @flow
 
 import * as React from 'react'
-import { AppRegistry, YellowBox } from 'react-native'
+import { AppRegistry } from 'react-native'
 import App from './modules/app/components/App'
 import 'moment/min/locales'
-
-// @see: https://github.com/facebook/metro/issues/287
-YellowBox.ignoreWarnings(['Require cycle:'])
 
 AppRegistry.registerComponent('Integreat', () =>
   () => <App />
