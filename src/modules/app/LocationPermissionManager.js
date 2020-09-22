296af60c6df63e97358463b1eb7319b46d096f90
Steffen Kleinle <klinzo@yahoo.de>
2020-09-22 13:14:33 +0200

    NATIVE-632: Fix type

--- src/modules/app/LocationPermissionManager.js
+++ src/modules/app/LocationPermissionManager.js
@@ -3,7 +3,7 @@
 import { check, PERMISSIONS, request, RESULTS } from 'react-native-permissions'
 import { Platform } from 'react-native'
 
-export const locationPermissionStatus = async (): RESULTS => {
+export const locationPermissionStatus = async (): Promise<RESULTS> => {
   return check(Platform.OS === 'ios'
     ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
     : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION
