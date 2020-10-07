a5b638dd0e69e1aca35d6bba1884fd4c4bbd54ef
Steffen Kleinle <klinzo@yahoo.de>
2020-10-07 10:05:05 +0200

    IGAPP-286: Fix scripts

--- tools/constants.js
+++ tools/constants.js
@@ -3,4 +3,12 @@ const UNRELEASED_DIR = 'unreleased'
 const GITKEEP_FILE = '.gitkeep'
 const VERSION_FILE = 'version.json'
 
-module.exports = { RELEASE_NOTES_DIR, UNRELEASED_DIR, GITKEEP_FILE, VERSION_FILE }
+const PLATFORM_ANDROID = 'android'
+const PLATFORM_IOS = 'ios'
+const PLATFORM_WEB = 'web'
+
+const PLATFORMS = [PLATFORM_WEB, PLATFORM_IOS, PLATFORM_ANDROID]
+
+const tagId = ({ platform, versionName }) => `${versionName}-${platform}`
+
+module.exports = { RELEASE_NOTES_DIR, UNRELEASED_DIR, GITKEEP_FILE, VERSION_FILE, PLATFORM_WEB, PLATFORM_IOS, PLATFORM_ANDROID, PLATFORMS, tagId }
