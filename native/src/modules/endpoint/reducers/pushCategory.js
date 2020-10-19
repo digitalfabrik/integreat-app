692a11fb8b3d6b71bf4e8cfb0d581548a3e1c20b
Nikolas Klug <klug.nikolas@googlemail.com>
2020-10-19 15:42:54 +0200

    Update routes in same city with new content

--- native/src/modules/endpoint/reducers/pushCategory.js
+++ native/src/modules/endpoint/reducers/pushCategory.js
@@ -1,6 +1,6 @@
 // @flow
 
-import type { CategoryRouteStateType, CityContentStateType } from '../../app/StateType'
+import type { CategoryRouteStateType, CityContentStateType, PathType } from '../../app/StateType'
 import type { PushCategoryActionType } from '../../app/StoreActionType'
 import { CategoryModel, LanguageModel } from '@integreat-app/integreat-api-client'
 import forEachTreeNode from '../../common/forEachTreeNode'
@@ -15,6 +15,25 @@ const getAllAvailableLanguages = (
   const allAvailableLanguages = new Map(category.availableLanguages)
   allAvailableLanguages.set(language, category.path)
   return allAvailableLanguages
+}
+
+function extractResultModelsAndChildren (root: ?CategoryModel, categoriesMap: CategoriesMapModel, depth: number): {|
+  resultModels: { [path: PathType]: CategoryModel },
+  resultChildren: { [path: PathType]: $ReadOnlyArray<PathType> }
+|} {
+  const resultModels = {}
+  const resultChildren = {}
+
+  forEachTreeNode(root, node => categoriesMap.getChildren(node), depth, (node, children) => {
+    resultModels[node.path] = node
+    if (children) {
+      resultChildren[node.path] = children.map(child => child.path)
+    }
+  })
+  return {
+    resultModels,
+    resultChildren
+  }
 }
 
 const pushCategory = (state: CityContentStateType, action: PushCategoryActionType): CityContentStateType => {
@@ -40,21 +59,24 @@ const pushCategory = (state: CityContentStateType, action: PushCategoryActionTyp
 
     return {
       ...state,
-      categoriesRouteMapping: { ...state.categoriesRouteMapping, [key]: route },
-      resourceCache: { status: 'ready', value: newResourceCache },
+      categoriesRouteMapping: {
+        ...state.categoriesRouteMapping,
+        [key]: route
+      },
+      resourceCache: {
+        status: 'ready',
+        value: newResourceCache
+      },
       searchRoute: { categoriesMap }
     }
   }
+  const { resultModels, resultChildren } = extractResultModelsAndChildren(root, categoriesMap, depth)
 
-  const resultModels = {}
-  const resultChildren = {}
-
-  forEachTreeNode(root, node => categoriesMap.getChildren(node), depth, (node, children) => {
-    resultModels[node.path] = node
-    if (children) {
-      resultChildren[node.path] = children.map(child => child.path)
-    }
-  })
+  // Check whether another page in the same city is loading, e.g. because it is being refreshed.
+  // This is important for displaying the loading spinner.
+  const otherPageLoading = Object.values(state.categoriesRouteMapping)
+    .filter(route => city === route.city && path !== route.path)
+    .reduce((acc, current) => (acc || current.status === 'loading'), false)
 
   const route: CategoryRouteStateType = {
     path: root.path,
@@ -64,13 +86,33 @@ const pushCategory = (state: CityContentStateType, action: PushCategoryActionTyp
     allAvailableLanguages: getAllAvailableLanguages(root, city, language, cityLanguages),
     language,
     city,
-    status: 'ready'
+    status: otherPageLoading ? 'loading' : 'ready'
   }
+
+  // Update all open routes in the same city with the new content
+  Object.entries(state.categoriesRouteMapping).filter(([key, route]) => city === route.city && path !== route.path)
+    .forEach(([key, route]) => {
+      const root: ?CategoryModel = categoriesMap.findCategoryByPath(route.path)
+      const { resultModels, resultChildren } = extractResultModelsAndChildren(root, categoriesMap, depth)
+      state.categoriesRouteMapping[key] = {
+        ...state.categoriesRouteMapping[key],
+        models: resultModels,
+        children: resultChildren,
+        allAvailableLanguages: getAllAvailableLanguages(root, city, language, cityLanguages),
+        status: 'ready'
+      }
+    })
 
   return {
     ...state,
-    categoriesRouteMapping: { ...state.categoriesRouteMapping, [key]: route },
-    resourceCache: { status: 'ready', value: newResourceCache },
+    categoriesRouteMapping: {
+      ...state.categoriesRouteMapping,
+      [key]: route
+    },
+    resourceCache: {
+      status: 'ready',
+      value: newResourceCache
+    },
     searchRoute: { categoriesMap }
   }
 }
