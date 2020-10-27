fc3a9a1f2058383a0d56f1341f58d0740f8119b7
Steffen Kleinle <klinzo@yahoo.de>
2020-10-27 13:49:40 +0100

    IGAPP-271: Fix flow for web build configs

--- web/src/modules/theme/constants/theme.js
+++ web/src/modules/theme/constants/theme.js
@@ -1,11 +1,11 @@
 // @flow
 
 import buildConfig from '../../app/constants/buildConfig'
-import type { ThemeType } from '../../../../build-configs/ThemeType'
+import type { ThemeType } from '../../app/constants/buildConfig'
 
 export type { ThemeType }
 
-export const brightTheme: ThemeType = buildConfig().theme
+export const brightTheme: ThemeType = buildConfig().lightTheme
 
 export const darkTheme: ThemeType = buildConfig().darkTheme
 
