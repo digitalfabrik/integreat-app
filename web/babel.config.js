a8c4bfcc6f69ce12265949335327d19dc46815d7
Steffen Kleinle <klinzo@yahoo.de>
2020-10-27 15:19:24 +0100

    IGAPP-271: Fix tests

--- web/babel.config.js
+++ web/babel.config.js
@@ -1,6 +1,6 @@
 const config = {
   presets: [
-    ['@babel/preset-env', { "modules": "commonjs" }],
+    ['@babel/preset-env', { modules: 'commonjs' }],
     '@babel/preset-react',
     '@babel/preset-flow'
   ],
@@ -12,9 +12,9 @@ const config = {
     // Partial Stage 1:
     '@babel/plugin-proposal-export-default-from',
     '@babel/plugin-proposal-logical-assignment-operators',
-    ['@babel/plugin-proposal-optional-chaining', {'loose': false}],
-    ['@babel/plugin-proposal-pipeline-operator', {'proposal': 'minimal'}],
-    ['@babel/plugin-proposal-nullish-coalescing-operator', {'loose': false}],
+    ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
+    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
     '@babel/plugin-proposal-do-expressions',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-namespace-from',
