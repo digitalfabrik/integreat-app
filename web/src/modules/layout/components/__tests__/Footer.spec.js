54d00d854b3865b269a49cc1ce4ddd04f9e40e61
Steffen Kleinle <klinzo@yahoo.de>
2020-10-31 17:44:39 +0100

    IGAPP-339: Fix tests

--- web/src/modules/layout/components/__tests__/Footer.spec.js
+++ web/src/modules/layout/components/__tests__/Footer.spec.js
@@ -6,11 +6,14 @@ import { shallow } from 'enzyme'
 
 describe('Footer', () => {
   it('should insert items', () => {
+    const preVersion = global.__VERSION__
+    global.__VERSION__ = 'vX.X'
     const component = <Footer>
       <div>MockNode</div>
       <div>AnotherMockNode</div>
     </Footer>
     expect(shallow(component)).toMatchSnapshot()
+    global.__VERSION__ = preVersion
   })
 
   it('should show Version on dev builds', () => {
