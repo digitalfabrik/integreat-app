31633af6a32ce29c6fac0478c49283805a42905f
Steffen Kleinle <klinzo@yahoo.de>
2020-10-31 12:35:13 +0100

    IGAPP-271: Fix metro and build-configs on native

--- native/metro.config.js
+++ native/metro.config.js
@@ -8,6 +8,13 @@
 const path = require('path')
 
 module.exports = {
+  // https://github.com/facebook/react-native/issues/21310#issuecomment-544071895
+  // https://github.com/facebook/metro/issues/1#issuecomment-453450709
+  resolver: {
+    extraNodeModules: new Proxy({}, {
+      get: (target, name) => path.join(process.cwd(), `node_modules/${name}`)
+    })
+  },
   watchFolders: [path.resolve(__dirname, '../')],
   transformer: {
     assetPlugins: ['react-native-svg-asset-plugin'],
