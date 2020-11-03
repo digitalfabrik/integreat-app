30a98cd69ecdb8f78944ec12de781f1b9d1d90a0
Maximilian Ammann <max@maxammann.org>
2020-11-03 16:57:54 +0100

    APICLIENT-324: Fix flow error

--- web/src/modules/layout/hooks/useOnClickOutside.js
+++ web/src/modules/layout/hooks/useOnClickOutside.js
@@ -3,7 +3,7 @@
 import * as React from 'react'
 import { useEffect } from 'react'
 
-const useOnClickOutside = (ref: React.Ref, callback: () => void) => {
+const useOnClickOutside = (ref: React.Ref<*>, callback: () => void) => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (ref.current && !ref.current.contains(event.target)) {
