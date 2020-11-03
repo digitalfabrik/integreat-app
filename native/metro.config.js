02bf56bd409a174eadcad6441339d5a65a468b13
Steffen Kleinle <klinzo@yahoo.de>
2020-11-03 16:26:38 +0100

    IGAPP-271: Make build-configs module resolving use absolute path

--- native/metro.config.js
+++ native/metro.config.js
@@ -11,10 +11,10 @@ module.exports = {
   // https://github.com/facebook/react-native/issues/21310#issuecomment-544071895
   // https://github.com/facebook/metro/issues/1#issuecomment-453450709
   // Per default metro is looking for the babel dependencies in the node_modules of the corresponding package.
-  // This leads to problems in the build-configs module as the babel dependencies are only in the native node_modules.
+  // This leads to problems in the build-configs module as the babel dependencies are only in the native .
   resolver: {
     extraNodeModules: new Proxy({}, {
-      get: (target, name) => path.join(process.cwd(), `node_modules/${name}`)
+      get: (target, name) => path.resolve(__dirname, `node_modules/${name}`)
     })
   },
   watchFolders: [path.resolve(__dirname, '../')],
