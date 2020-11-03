c8b53c13d3c3db9b2b91732763a365ef3a7d8ec5
Maximilian Ammann <max@maxammann.org>
2020-11-03 18:57:35 +0100

    APICLIENT-324: Fix flow typing

--- web/src/modules/layout/hooks/useOnClickOutside.js
+++ web/src/modules/layout/hooks/useOnClickOutside.js
@@ -1,12 +1,11 @@
 // @flow
 
-import * as React from 'react'
 import { useEffect } from 'react'
 
-const useOnClickOutside = (ref: React.Ref<*>, callback: () => void) => {
+const useOnClickOutside = (ref: {current: ?HTMLElement}, callback: () => void) => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target)) {
+      if (event.target instanceof Node && ref.current && !ref.current.contains(event.target)) {
         callback()
       }
     }
