31cb01dd3b9a628c78aab6b5a9ba5f178330d543
Maximilian Ammann <max@maxammann.org>
2020-11-13 18:12:43 +0100

    IGAPP-374: Rename jest.config.json to .js and generate coverage to web/coverage

---
+++ web/jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  rootDir: 'src/',
+  verbose: true,
+  automock: false,
+  setupFiles: [
+    '<rootDir>/setupTest.js'
+  ],
+  transformIgnorePatterns: [
+    'node_modules/(?!build-configs|api-client)'
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css)$': 'identity-obj-proxy',
+    hashids: 'hashids/dist/hashids.min.js'
+  },
+  moduleFileExtensions: [
+    'js',
+    'json'
+  ],
+  moduleDirectories: [
+    'node_modules'
+  ],
+  collectCoverageFrom: [
+    '**/*.{js,jsx}'
+  ],
+  coverageDirectory: '<rootDir>/../coverage',
+  snapshotSerializers: [
+    'enzyme-to-json/serializer'
+  ]
+}
