03a10a434f5359db6c160873c1ebed533a5eb99a
Steffen Kleinle <klinzo@yahoo.de>
2020-11-15 13:54:24 +0100

    IGAPP-364: Split build configs

--- build-configs/integreat/index.js
+++ build-configs/integreat/index.js
@@ -1,13 +1,18 @@
 // @flow
 
 import { lightTheme, darkTheme } from './theme'
-import type { BuildConfigType } from '../BuildConfigType'
+import type {
+  AndroidBuildConfigType,
+  CommonBuildConfigType,
+  iOSBuildConfigType,
+  WebBuildConfigType
+} from '../BuildConfigType'
 import { INTEGREAT_ASSETS } from '../AssetsType'
 import mainImprint from './assets/mainImprint'
 
 const APPLE_APP_ID = '1072353915'
 
-const IntegreatBuildConfig: BuildConfigType = {
+const CommonIntegreatBuildConfig: CommonBuildConfigType = {
   appName: 'Integreat',
   appIcon: 'app_icon_integreat',
   lightTheme,
@@ -26,7 +31,6 @@ const IntegreatBuildConfig: BuildConfigType = {
     introSlides: true,
     sentry: true
   },
-  mainImprint,
   aboutUrls: {
     default: 'https://integreat-app.de/about/',
     en: 'https://integreat-app.de/en/about/'
@@ -34,58 +38,72 @@ const IntegreatBuildConfig: BuildConfigType = {
   privacyUrls: {
     default: 'https://integreat-app.de/datenschutz/',
     en: 'https://integreat-app.de/en/privacy/'
-  },
-  android: {
-    splashScreen: true,
-    applicationId: 'tuerantuer.app.integreat',
-    googleServices: {
-      googleAppId: '1:164298278764:android:3fc1f67f3883df306fd549',
-      gcmDefaultSenderId: '164298278764',
-      defaultWebClientId: '164298278764-hhup7orh5rvg0oumhbabqvp5giudaste.apps.googleusercontent.com',
-      gaTrackingId: null,
-      firebaseDatabaseUrl: 'https://integreat-2020.firebaseio.com',
-      googleApiKey: 'AIzaSyBvc08_Cqww8z2Dh-EXtwHW98HXBk8T7V4',
-      googleCrashReportingApiKey: 'AIzaSyBvc08_Cqww8z2Dh-EXtwHW98HXBk8T7V4',
-      projectId: 'integreat-2020'
-    }
-  },
-  ios: {
-    bundleIdentifier: 'de.integreat-app',
-    provisioningProfileSpecifier: 'match Development de.integreat-app',
-    appleId: APPLE_APP_ID,
-    googleServices: {
-      clientId: '164298278764-pemn49q7v283m0tqml3q8q0ltr5h8pni.apps.googleusercontent.com',
-      reversedClientId: 'com.googleusercontent.apps.164298278764-pemn49q7v283m0tqml3q8q0ltr5h8pni',
-      apiKey: 'AIzaSyBSnGYrUWfIAPcEsFya1OYvTMbjvfUyOEU',
-      gcmSenderId: '164298278764',
-      plistVersion: '1',
-      bundleId: 'de.integreat-app',
-      projectId: 'integreat-2020',
-      storageBucket: 'integreat-2020.appspot.com',
-      isAdsEnabled: false,
-      isAnalyticsEnabled: false,
-      isAppInviteEnabled: true,
-      isGCMEnabled: true,
-      isSigninEnabled: true,
-      googleAppId: '1:164298278764:ios:2a69672da4d117116fd549',
-      databaseUrl: 'https://integreat-2020.firebaseio.com'
-    },
-    launchScreen: 'LaunchScreenIntegreat'
-  },
-  web: {
-    itunesAppId: APPLE_APP_ID,
-    manifestUrl: '/manifest.json',
-    icons: {
-      appLogo: '/app-logo.png',
-      locationMarker: '/location-marker.svg',
-      appleTouchIcon: '/apple-touch-icon.png',
-      favicons: '/favicons/'
-    },
-    splashScreen: {
-      backgroundColor: lightTheme.colors.themeColor,
-      imageUrl: '/app-icon-cornered.jpg'
-    }
   }
 }
 
-export default IntegreatBuildConfig
+export const AndroidIntegreatBuildConfig: AndroidBuildConfigType = {
+  ...CommonIntegreatBuildConfig,
+  splashScreen: true,
+  applicationId: 'tuerantuer.app.integreat',
+  googleServices: {
+    googleAppId: '1:164298278764:android:3fc1f67f3883df306fd549',
+    gcmDefaultSenderId: '164298278764',
+    defaultWebClientId: '164298278764-hhup7orh5rvg0oumhbabqvp5giudaste.apps.googleusercontent.com',
+    gaTrackingId: null,
+    firebaseDatabaseUrl: 'https://integreat-2020.firebaseio.com',
+    googleApiKey: 'AIzaSyBvc08_Cqww8z2Dh-EXtwHW98HXBk8T7V4',
+    googleCrashReportingApiKey: 'AIzaSyBvc08_Cqww8z2Dh-EXtwHW98HXBk8T7V4',
+    projectId: 'integreat-2020'
+  }
+}
+
+export const iOSIntegreatBuildConfig: iOSBuildConfigType = {
+  ...CommonIntegreatBuildConfig,
+  bundleIdentifier: 'de.integreat-app',
+  provisioningProfileSpecifier: 'match Development de.integreat-app',
+  appleId: APPLE_APP_ID,
+  googleServices: {
+    clientId: '164298278764-pemn49q7v283m0tqml3q8q0ltr5h8pni.apps.googleusercontent.com',
+    reversedClientId: 'com.googleusercontent.apps.164298278764-pemn49q7v283m0tqml3q8q0ltr5h8pni',
+    apiKey: 'AIzaSyBSnGYrUWfIAPcEsFya1OYvTMbjvfUyOEU',
+    gcmSenderId: '164298278764',
+    plistVersion: '1',
+    bundleId: 'de.integreat-app',
+    projectId: 'integreat-2020',
+    storageBucket: 'integreat-2020.appspot.com',
+    isAdsEnabled: false,
+    isAnalyticsEnabled: false,
+    isAppInviteEnabled: true,
+    isGCMEnabled: true,
+    isSigninEnabled: true,
+    googleAppId: '1:164298278764:ios:2a69672da4d117116fd549',
+    databaseUrl: 'https://integreat-2020.firebaseio.com'
+  },
+  launchScreen: 'LaunchScreenIntegreat'
+}
+
+export const WebIntegreatBuildConfig: WebBuildConfigType = {
+  ...CommonIntegreatBuildConfig,
+  mainImprint,
+  itunesAppId: APPLE_APP_ID,
+  manifestUrl: '/manifest.json',
+  icons: {
+    appLogo: '/app-logo.png',
+    locationMarker: '/location-marker.svg',
+    appleTouchIcon: '/apple-touch-icon.png',
+    favicons: '/favicons/'
+  },
+  splashScreen: {
+    backgroundColor: lightTheme.colors.themeColor,
+    imageUrl: '/app-icon-cornered.jpg'
+  }
+}
+
+const platformBuildConfigs = {
+  'web': WebIntegreatBuildConfig,
+  'android': AndroidIntegreatBuildConfig,
+  'ios': iOSIntegreatBuildConfig
+}
+
+export default platformBuildConfigs
+
--- build-configs/malte/index.js
+++ build-configs/malte/index.js
@@ -1,14 +1,14 @@
 // @flow
 
 import { lightTheme, darkTheme } from './theme'
-import type { BuildConfigType } from '../BuildConfigType'
+import type { AndroidBuildConfigType, iOSBuildConfigType, WebBuildConfigType } from '../BuildConfigType'
 import { MALTE_ASSETS } from '../AssetsType'
 import malteOverrideLocales from 'locales/override-locales/malte.json'
 import mainImprint from './assets/mainImprint'
 
 const APPLE_APP_ID = '1535758339'
 
-const MalteBuildConfig: BuildConfigType = {
+const CommonMalteBuildConfig = {
   appName: 'Malte',
   appIcon: 'app_icon_malte',
   lightTheme,
@@ -27,59 +27,69 @@ const MalteBuildConfig: BuildConfigType = {
     introSlides: true,
     sentry: true
   },
-  mainImprint,
   aboutUrls: {
     default: 'https://www.malteser-werke.de/malte-app'
   },
   privacyUrls: {
     default: 'https://www.malteser-werke.de/datenschutzerklaerung-malte.html'
-  },
-  android: {
-    splashScreen: false,
-    applicationId: 'de.malteapp',
-    googleServices: {
-      googleAppId: '1:146599424234:android:b110d70b79ff7ce351a793',
-      gcmDefaultSenderId: '146599424234',
-      defaultWebClientId: '146599424234-ekfbl5uo9bfi7u1p6lu417tbdolorhnl.apps.googleusercontent.com',
-      gaTrackingId: null,
-      firebaseDatabaseUrl: 'https://malte-2020.firebaseio.com',
-      googleApiKey: 'AIzaSyDZNWm7Cvh3O4DjfTupTGErQVtvz1o77q8',
-      googleCrashReportingApiKey: 'AIzaSyDZNWm7Cvh3O4DjfTupTGErQVtvz1o77q8',
-      projectId: 'malte-2020'
-    }
-  },
-  ios: {
-    bundleIdentifier: 'de.malteapp',
-    provisioningProfileSpecifier: 'match Development de.malteapp',
-    appleId: APPLE_APP_ID,
-    googleServices: {
-      clientId: '146599424234-b6rfm4skhbsv4qvob3ieh34s2chjm54k.apps.googleusercontent.com',
-      reversedClientId: 'com.googleusercontent.apps.146599424234-b6rfm4skhbsv4qvob3ieh34s2chjm54k',
-      apiKey: 'AIzaSyAWOaqdFwZ7-tbwiQ79dwMyzpmR_g1cBbI',
-      googleAppId: '1:146599424234:ios:56dde1442250260651a793',
-      gcmSenderId: '146599424234',
-      bundleId: 'de.malteapp',
-      databaseUrl: 'https://malte-2020.firebaseio.com',
-      projectId: 'malte-2020',
-      plistVersion: '1',
-      storageBucket: 'malte-2020.appspot.com',
-      isAdsEnabled: false,
-      isAnalyticsEnabled: false,
-      isAppInviteEnabled: true,
-      isGCMEnabled: true,
-      isSigninEnabled: true
-    },
-    launchScreen: 'LaunchScreenDefault'
-  },
-  web: {
-    itunesAppId: APPLE_APP_ID,
-    icons: {
-      appLogo: '/app-logo.png',
-      locationMarker: '/location-marker.svg',
-      appleTouchIcon: '/apple-touch-icon.png',
-      favicons: '/favicons/'
-    }
   }
 }
 
-export default MalteBuildConfig
+const AndroidMalteBuildConfig: AndroidBuildConfigType = {
+  ...CommonMalteBuildConfig,
+  splashScreen: false,
+  applicationId: 'de.malteapp',
+  googleServices: {
+    googleAppId: '1:146599424234:android:b110d70b79ff7ce351a793',
+    gcmDefaultSenderId: '146599424234',
+    defaultWebClientId: '146599424234-ekfbl5uo9bfi7u1p6lu417tbdolorhnl.apps.googleusercontent.com',
+    gaTrackingId: null,
+    firebaseDatabaseUrl: 'https://malte-2020.firebaseio.com',
+    googleApiKey: 'AIzaSyDZNWm7Cvh3O4DjfTupTGErQVtvz1o77q8',
+    googleCrashReportingApiKey: 'AIzaSyDZNWm7Cvh3O4DjfTupTGErQVtvz1o77q8',
+    projectId: 'malte-2020'
+  }
+}
+const iOSMalteBuildConfig: iOSBuildConfigType = {
+  ...CommonMalteBuildConfig,
+  bundleIdentifier: 'de.malteapp',
+  provisioningProfileSpecifier: 'match Development de.malteapp',
+  appleId: APPLE_APP_ID,
+  googleServices: {
+    clientId: '146599424234-b6rfm4skhbsv4qvob3ieh34s2chjm54k.apps.googleusercontent.com',
+    reversedClientId: 'com.googleusercontent.apps.146599424234-b6rfm4skhbsv4qvob3ieh34s2chjm54k',
+    apiKey: 'AIzaSyAWOaqdFwZ7-tbwiQ79dwMyzpmR_g1cBbI',
+    googleAppId: '1:146599424234:ios:56dde1442250260651a793',
+    gcmSenderId: '146599424234',
+    bundleId: 'de.malteapp',
+    databaseUrl: 'https://malte-2020.firebaseio.com',
+    projectId: 'malte-2020',
+    plistVersion: '1',
+    storageBucket: 'malte-2020.appspot.com',
+    isAdsEnabled: false,
+    isAnalyticsEnabled: false,
+    isAppInviteEnabled: true,
+    isGCMEnabled: true,
+    isSigninEnabled: true
+  },
+  launchScreen: 'LaunchScreenDefault'
+}
+const WebMalteBuildConfig: WebBuildConfigType = {
+  ...CommonMalteBuildConfig,
+  mainImprint,
+  itunesAppId: APPLE_APP_ID,
+  icons: {
+    appLogo: '/app-logo.png',
+    locationMarker: '/location-marker.svg',
+    appleTouchIcon: '/apple-touch-icon.png',
+    favicons: '/favicons/'
+  }
+}
+
+const platformBuildConfigs = {
+  'web': WebMalteBuildConfig,
+  'android': AndroidMalteBuildConfig,
+  'ios': iOSMalteBuildConfig
+}
+
+export default platformBuildConfigs
