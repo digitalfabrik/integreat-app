88fb78693185749799297db4ed90a8f1b4bf5232
Steffen Kleinle <klinzo@yahoo.de>
2020-11-15 18:06:01 +0100

    IGAPP-364: Fix usages of platform specific build configs in native

--- native/ios/Integreat/AppDelegate.m
+++ native/ios/Integreat/AppDelegate.m
@@ -33,26 +33,26 @@ static void InitializeFlipper(UIApplication *application) {
   InitializeFlipper(application);
 #endif
 
-  if ([@BUILD_CONFIG_IOS_GOOGLE_SERVICES_GOOGLE_APP_ID length] > 0 &&
-      [@BUILD_CONFIG_IOS_GOOGLE_SERVICES_GCM_SENDER_ID length] > 0 &&
-      [@BUILD_CONFIG_IOS_GOOGLE_SERVICES_API_KEY length] > 0 &&
-      [@BUILD_CONFIG_IOS_GOOGLE_SERVICES_BUNDLE_ID length] > 0 &&
-      [@BUILD_CONFIG_IOS_GOOGLE_SERVICES_CLIENT_ID length] > 0 &&
-      [@BUILD_CONFIG_IOS_GOOGLE_SERVICES_PROJECT_ID length] > 0 &&
-      [@BUILD_CONFIG_IOS_GOOGLE_SERVICES_REVERSED_CLIENT_ID length] > 0 &&
-      [@BUILD_CONFIG_IOS_GOOGLE_SERVICES_DATABASE_URL length] > 0 &&
-      [@BUILD_CONFIG_IOS_GOOGLE_SERVICES_STORAGE_BUCKET length] > 0) {
+  if ([@BUILD_CONFIG_GOOGLE_SERVICES_GOOGLE_APP_ID length] > 0 &&
+      [@BUILD_CONFIG_GOOGLE_SERVICES_GCM_SENDER_ID length] > 0 &&
+      [@BUILD_CONFIG_GOOGLE_SERVICES_API_KEY length] > 0 &&
+      [@BUILD_CONFIG_GOOGLE_SERVICES_BUNDLE_ID length] > 0 &&
+      [@BUILD_CONFIG_GOOGLE_SERVICES_CLIENT_ID length] > 0 &&
+      [@BUILD_CONFIG_GOOGLE_SERVICES_PROJECT_ID length] > 0 &&
+      [@BUILD_CONFIG_GOOGLE_SERVICES_REVERSED_CLIENT_ID length] > 0 &&
+      [@BUILD_CONFIG_GOOGLE_SERVICES_DATABASE_URL length] > 0 &&
+      [@BUILD_CONFIG_GOOGLE_SERVICES_STORAGE_BUCKET length] > 0) {
     FIROptions *options = [[FIROptions alloc]
-                           initWithGoogleAppID:@BUILD_CONFIG_IOS_GOOGLE_SERVICES_GOOGLE_APP_ID
-                           GCMSenderID:@BUILD_CONFIG_IOS_GOOGLE_SERVICES_GCM_SENDER_ID];
+                           initWithGoogleAppID:@BUILD_CONFIG_GOOGLE_SERVICES_GOOGLE_APP_ID
+                           GCMSenderID:@BUILD_CONFIG_GOOGLE_SERVICES_GCM_SENDER_ID];
     if (options) {
-      options.APIKey = @BUILD_CONFIG_IOS_GOOGLE_SERVICES_API_KEY;
-      options.bundleID = @BUILD_CONFIG_IOS_GOOGLE_SERVICES_BUNDLE_ID;
-      options.clientID = @BUILD_CONFIG_IOS_GOOGLE_SERVICES_CLIENT_ID;
-      options.projectID = @BUILD_CONFIG_IOS_GOOGLE_SERVICES_PROJECT_ID;
-      options.androidClientID = @BUILD_CONFIG_IOS_GOOGLE_SERVICES_REVERSED_CLIENT_ID;
-      options.databaseURL = @BUILD_CONFIG_IOS_GOOGLE_SERVICES_DATABASE_URL;
-      options.storageBucket = @BUILD_CONFIG_IOS_GOOGLE_SERVICES_STORAGE_BUCKET;
+      options.APIKey = @BUILD_CONFIG_GOOGLE_SERVICES_API_KEY;
+      options.bundleID = @BUILD_CONFIG_GOOGLE_SERVICES_BUNDLE_ID;
+      options.clientID = @BUILD_CONFIG_GOOGLE_SERVICES_CLIENT_ID;
+      options.projectID = @BUILD_CONFIG_GOOGLE_SERVICES_PROJECT_ID;
+      options.androidClientID = @BUILD_CONFIG_GOOGLE_SERVICES_REVERSED_CLIENT_ID;
+      options.databaseURL = @BUILD_CONFIG_GOOGLE_SERVICES_DATABASE_URL;
+      options.storageBucket = @BUILD_CONFIG_GOOGLE_SERVICES_STORAGE_BUCKET;
 
       // The following values are unset as they are probably not relevant for Messsaging:
       // options.deepLinkURLScheme = ??
@@ -61,12 +61,12 @@ static void InitializeFlipper(UIApplication *application) {
 
       // Unused info from GoogleService-Info.plist:
       // These existed in the .plist file but I was not able to add them to the FIROptions. They are probably not important.
-      // ?? = BUILD_CONFIG_IOS_GOOGLE_SERVICES_PLIST_VERSION;
-      // ?? = BUILD_CONFIG_IOS_GOOGLE_SERVICES_IS_ADS_ENABLED;
-      // ?? = BUILD_CONFIG_IOS_GOOGLE_SERVICES_IS_ANALYTICS_ENABLED;
-      // ?? = BUILD_CONFIG_IOS_GOOGLE_SERVICES_IS_APPINVITE_ENABLED;
-      // ?? = BUILD_CONFIG_IOS_GOOGLE_SERVICES_IS_GCM_ENABLED;
-      // ?? = BUILD_CONFIG_IOS_GOOGLE_SERVICES_IS_SIGNIN_ENABLED;
+      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_PLIST_VERSION;
+      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_ADS_ENABLED;
+      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_ANALYTICS_ENABLED;
+      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_APPINVITE_ENABLED;
+      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_GCM_ENABLED;
+      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_SIGNIN_ENABLED;
 
       [FIRApp configureWithOptions:options];
     }
