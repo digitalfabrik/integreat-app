6c7cc7b4da2f481ec76652cb0cbb6abd16eeae18
Maximilian Ammann <max@maxammann.org>
2020-11-16 18:45:19 +0100

    IGAPP-19: Always build sourcemaps

--- web/tools/webpack.config.babel.js
+++ web/tools/webpack.config.babel.js
@@ -22,7 +22,7 @@ const SHORT_COMMIT_SHA_LENGTH = 8
 
 // A first performance budget, which should be improved in the future: Maximum bundle size in Bytes; 2^20 = 1 MiB
 // eslint-disable-next-line no-magic-numbers
-const MAX_BUNDLE_SIZE = 1.56 * Math.pow(2, 20)
+const MAX_BUNDLE_SIZE = 1.63 * Math.pow(2, 20)
 
 const readJson = path => JSON.parse(fs.readFileSync(path))
 
@@ -98,9 +98,7 @@ const createConfig = (env = {}) => {
     optimization: {
       usedExports: true
     },
-    // Developer tool to enhance debugging, source maps
-    // http://webpack.github.io/docs/configuration.html#devtool
-    devtool: isProductionBuild ? false : 'source-map',
+    devtool: 'source-map',
     devServer: {
       contentBase: distDirectory,
       compress: true,
