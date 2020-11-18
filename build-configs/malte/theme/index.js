07516399127e922f1d4f1fd2e24b2b1b24d2aeb1
Steffen Kleinle <klinzo@yahoo.de>
2020-11-18 12:47:41 +0100

    IGAPP-364: Remove helpers and dimensions from theme

--- build-configs/malte/theme/index.js
+++ build-configs/malte/theme/index.js
@@ -2,15 +2,11 @@
 
 import { lightColors, darkColors } from './colors'
 import malteFonts from './fonts'
-import dimensions from '../../common/theme/dimensions'
-import helpers from '../../common/theme/helpers'
 import type { ThemeType } from '../../ThemeType'
 
 export const lightTheme: ThemeType = {
   colors: lightColors,
-  fonts: malteFonts,
-  dimensions,
-  helpers
+  fonts: malteFonts
 }
 
 export const darkTheme: ThemeType = {
