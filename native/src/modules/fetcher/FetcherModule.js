4a2301814015e9dc5ba3f4fe8db623b942713197
Stefan Dohren <stefan.dohren@live.de>
2020-11-23 09:54:27 +0100

    IGAPP-169:  revert changes to FetcherModule.fetchAsync

--- native/src/modules/fetcher/FetcherModule.js
+++ native/src/modules/fetcher/FetcherModule.js
@@ -1,9 +1,9 @@
 // @flow
 
+import NativeFetcherModule, { NativeFetcherModuleEmitter } from './NativeFetcherModule'
 import { isEmpty } from 'lodash'
 import type { EventChannel } from 'redux-saga'
 import { eventChannel } from 'redux-saga'
-import NativeFetcherModule, { NativeFetcherModuleEmitter } from './NativeFetcherModule'
 
 export type TargetFilePathsType = { [path: string]: string }
 
@@ -31,23 +31,27 @@ class FetcherModule {
     })
   }
 
-  fetchAsync = (targetFilePaths: TargetFilePathsType): Promise<FetchResultType> => {
+  async fetchAsync (
+    targetFilePaths: TargetFilePathsType
+  ): Promise<FetchResultType> {
+    if (isEmpty(targetFilePaths)) {
+      return {}
+    }
     FetcherModule.currentlyFetching = true
 
-    const fetchPromise: Promise<FetchResultType> = !isEmpty(targetFilePaths)
-      ? NativeFetcherModule.fetchAsync(targetFilePaths)
-      : Promise.resolve({})
-
-    fetchPromise.then(result => {
-      FetcherModule.currentlyFetching = false
+    try {
+      const result = await NativeFetcherModule.fetchAsync(targetFilePaths)
 
       if (!result) {
         // While testing the app I noticed that the FetchResultType of this function was empty. I am absolutely not sure
         // why this happened. As this cause an inconsistent state it is better to throw an error in this case.
         throw new Error('Fetch failed for some reason!')
       }
-    })
-    return fetchPromise
+
+      return result
+    } finally {
+      FetcherModule.currentlyFetching = false
+    }
   }
 }
 
