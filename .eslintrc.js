710b8873e463f2c7b5029a5ada5d51adaed887eb
Steffen Kleinle <klinzo@yahoo.de>
2020-11-30 16:11:08 +0100

    IGAPP-270: Adjust eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,13 @@
 module.exports = {
   parser: 'babel-eslint',
   plugins: [
-    'standard',
     'react',
     'react-hooks',
     'flowtype',
     'jest'
   ],
   extends: [
-    'plugin:standard',
+    'standard',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:flowtype/recommended'
@@ -18,41 +17,36 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    browser: true
+    browser: true,
+    'jest/globals': true
   },
+  ignorePatterns: [
+    '**/flow-typed/',
+    '**/reports/',
+    '**/node_modules/',
+    '**/ios/main.jsbundle',
+    '**/stylelint.config.js',
+    '**/dist/',
+    '**/lib-dist/'
+  ],
   rules: {
     'arrow-parens': ['error', 'as-needed'],
-    'curly': ['error', 'all'],
-    'jsx-quotes': 'error',
-    'max-len': ['warn', { 'code': 120 }],
+    curly: ['error', 'all'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'max-len': ['warn', { code: 120 }],
     'no-loop-func': 'error',
     'prefer-const': 'error',
     'prefer-object-spread': 'error',
     'prefer-template': 'error',
 
+    'react/display-name': 'off',
     'react/no-access-state-in-setstate': 'error',
     'react/no-did-mount-set-state': 'warn',
-    'react/no-did-update-set-state': 'error',
-    'react/no-unused-prop-types': 'error',
+    'react/no-did-update-set-state': 'warn',
+    'react/no-unescaped-entities': 'off',
+    'react/no-unused-prop-types': 'warn',
     'react/no-typos': 'error',
     'react/prefer-es6-class': ['error', 'always'],
-
-    'flowtype/delimiter-dangle': ['error', 'never'],
-    'flowtype/newline-after-flow-annotation': ['error', 'always'],
-    'flowtype/no-primitive-constructor-types': 'error',
-    'flowtype/no-weak-types': 'warn',
-    'flowtype/object-type-delimiter': ['error', 'comma'],
-    'flowtype/require-exact-type': ['warn', 'always'],
-    'flowtype/require-parameter-type': ['error', { excludeArrowFunctions: true }],
-    'flowtype/require-return-type': ['error', 'always', {
-      excludeArrowFunctions: true,
-      annotateUndefined: 'never',
-      excludeMatching: ['^render$']
-    }],
-    'flowtype/require-valid-file-annotation': ['error', 'always', { 'annotationStyle': 'line' }],
-    'flowtype/semi': ['error', 'never'],
-    'flowtype/type-id-match': ['error', '^([A-Z][a-z0-9]+)+Type$'],
-    'flowtype/valid-syntax': 'warn',
 
     'jest/consistent-test-it': 'error',
     'jest/no-disabled-tests': 'error',
@@ -64,22 +58,40 @@ module.exports = {
     'jest/valid-describe': 'error',
     'jest/valid-expect': 'error'
   },
-  overrides: [
-    {
-      files: ['*.js'],
-      excludedFiles: ['*.spec.js', '**/__mocks__/*.js'],
-      rules: {
-        'react/jsx-no-bind': ['error', {
-          ignoreRefs: false,
-          allowArrowFunctions: false,
-          allowFunctions: false,
-          allowBind: false
-        }],
-        'no-magic-numbers': ['error', {
-          ignore: [-1, 0, 1, 2],
-          ignoreArrayIndexes: true
-        }]
-      }
+  overrides: [{
+    files: ['**/src/**/*.js'],
+    rules: {
+      'flowtype/require-valid-file-annotation': ['error', 'always', { annotationStyle: 'line' }],
+      'flowtype/delimiter-dangle': ['error', 'never'],
+      'flowtype/newline-after-flow-annotation': ['error', 'always'],
+      'flowtype/no-primitive-constructor-types': 'error',
+      'flowtype/no-weak-types': 'warn',
+      'flowtype/object-type-delimiter': ['error', 'comma'],
+      'flowtype/require-exact-type': ['warn', 'always'],
+      'flowtype/require-parameter-type': ['error', { excludeArrowFunctions: true }],
+      'flowtype/require-return-type': ['error', 'always', {
+        excludeArrowFunctions: true,
+        annotateUndefined: 'never',
+        excludeMatching: ['^render$']
+      }],
+      'flowtype/semi': ['error', 'never'],
+      'flowtype/type-id-match': ['error', '^([A-Z][a-z0-9]+)+Type$'],
+      'flowtype/valid-syntax': 'warn'
     }
-  ]
+  }, {
+    files: ['*.js'],
+    excludedFiles: ['*.spec.js', '**/__mocks__/*.js'],
+    rules: {
+      'react/jsx-no-bind': ['error', {
+        ignoreRefs: false,
+        allowArrowFunctions: false,
+        allowFunctions: false,
+        allowBind: false
+      }],
+      'no-magic-numbers': ['error', {
+        ignore: [-1, 0, 1, 2],
+        ignoreArrayIndexes: true
+      }]
+    }
+  }]
 }
