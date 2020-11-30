135721099685be8d77a32d7e9719e8cd03396cde
Maximilian Ammann <max@maxammann.org>
2020-11-30 16:04:35 +0100

    IGAPP-402: Adapt changes for renaming in native

--- native/src/modules/platform/getLanguageCode.js
+++ native/src/modules/platform/getLanguageCode.js
@@ -2,18 +2,15 @@
 
 import { NativeModules, Platform } from 'react-native'
 
-/**
- * @returns {string} the system locale in the format "de_DE"
- */
 export default () => {
-  const locale = Platform.select({
+  const languageCode = Platform.select({
     android: NativeModules?.I18nManager?.localeIdentifier,
     ios: NativeModules?.SettingsManager?.settings?.AppleLocale
   })
 
-  if (!locale) {
-    throw new Error('Failed to get "locale" from native side!')
+  if (!languageCode) {
+    throw new Error('Failed to get language code from native side!')
   }
 
-  return locale
+  return languageCode
 }
