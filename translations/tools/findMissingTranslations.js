904908a542e1f00ca4c55bb9d916f716b2595d2b
Maximilian Ammann <max@maxammann.org>
2020-11-30 15:24:13 +0100

    IGAPP-402: Rename findMissingLocales

--- translations/tools/findMissingTranslations.js
+++ translations/tools/findMissingTranslations.js
@@ -3,7 +3,7 @@ const task = require('./task')
 
 const { reduce, map, union, isEmpty } = require('lodash')
 
-function findMissingLocales () {
+function findMissingTranslations () {
   const locales = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'))
   const keys = reduce(locales, (moduleAcc, languages, moduleKey) => {
     const keysPerLanguage = map(languages, values => Object.keys(values))
@@ -30,7 +30,7 @@ function findMissingLocales () {
   return missingKeys
 }
 
-module.exports = task('findMissingLocales', () => {
-  return Promise.resolve(findMissingLocales())
-    .then(locales => console.log(locales))
+module.exports = task('findMissingTranslations', () => {
+  return Promise.resolve(findMissingTranslations())
+    .then(translations => console.log(translations))
 })
