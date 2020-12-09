10873c0dec519b5c2fb3800417cef4219fce85b4
Steffen Kleinle <klinzo@yahoo.de>
2020-12-09 17:57:46 +0100

    IGAPP-191: Try to fix lehrstellenradar

--- native/src/routes/external-offer/components/ExternalOffer.js
+++ native/src/routes/external-offer/components/ExternalOffer.js
@@ -43,6 +43,10 @@ const ExternalOffer = (props: PropsType) => {
     dataDetectorTypes={['all']}
     domStorageEnabled={false}
     ref={webviewRef}
+    /* The Lehrstellenradar does not work in the webview if you are navigating back (probably because of FormData?).
+        Without this the webview itself shows an error which looks more like it is our fault.}
+        Also, without disabling cache it is not possible to navigate back with hardware buttons anymore. */
+    cacheEnabled={false}
 
     renderError={renderWebviewError}
     onNavigationStateChange={onNavigationStateChange}
