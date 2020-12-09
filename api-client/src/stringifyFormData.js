8e60415c47602dcef5d5f2ca37ed72b8426c8601
Sarah Sporck <sporck@integreat-app.de>
2020-12-09 11:01:04 +0100

    IGAPP-107: fix typing

--- api-client/src/stringifyFormData.js
+++ api-client/src/stringifyFormData.js
@@ -1,18 +1,17 @@
 // @flow
 
-type FormDataType = FormData
-  | {| getParts: () => Array<{ string: string, fieldName: string }> |}
+type FormDataType = FormData | {| getParts: () => Array<{| string: string, fieldName: string |}> |}
 
 // stringifies FormData from react-native or the browser variant of it
 export function stringifyFormData (formData: FormDataType): string {
   const entries = {}
-  if (formData.entries) {
-    for (const [key, value] of formData.entries()) {
-      entries[key] = value
-    }
-  } else {
+  if (formData.getParts) {
     for (const part of formData.getParts()) {
       entries[part.fieldName] = part.string
+    }
+  } else {
+    for (const [key, value] of formData.entries()) {
+      entries[key] = value
     }
   }
   return JSON.stringify(entries)
