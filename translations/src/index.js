d8fc98e0cdcfdd90789d09b13fd37710c1a9ad3e
Maximilian Ammann <max@maxammann.org>
2020-12-14 12:53:51 +0100

    IGAPP-386: Refactor config

--- translations/src/index.js
+++ translations/src/index.js
@@ -9,6 +9,7 @@ export type TranslationsType = ImportedTranslationsType
 export type TransformedTranslationsType = ImportedTransformedTranslationsType
 
 export { default as loadTranslations } from './loadTranslations'
+export { default as config } from './config'
 
 export {
   values,
