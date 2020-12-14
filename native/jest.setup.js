d5939b95f70c86d0c1d35bda0f3b767c6e11fd45
Maximilian Ammann <max@maxammann.org>
2020-12-14 13:39:21 +0100

    IGAPP-386: Only mock js for now

--- native/jest.setup.js
+++ native/jest.setup.js
@@ -32,8 +32,9 @@ function walkDir (dir, callback) {
 // The following code automatically unmocks the modules in `mocksPath`. This is required because jest mocks all these
 // modules automatically as soon as they are found
 const mocksPath = 'src/__mocks__/'
+const jsPath = '.js' // This only unmocks .js files not .json for example
 walkDir(mocksPath, name => {
-  if (name.endsWith('.js') || name.endsWith('.json')) {
+  if (name.endsWith(jsPath)) {
     jest.unmock(name.substring(mocksPath.length, name.length - jsPath.length))
   }
 })
