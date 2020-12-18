46ea555a13502afed0afb20b7a2e96721d6b1b7d
Steffen Kleinle <klinzo@yahoo.de>
2020-12-18 15:50:41 +0100

    IGAPP-114: Remove unnecessary params

--- native/src/modules/app/createNavigateToOffers.js
+++ native/src/modules/app/createNavigateToOffers.js
@@ -3,11 +3,12 @@
 import type { Dispatch } from 'redux'
 import type { StoreActionType } from './StoreActionType'
 import type { NavigationPropType, RoutesType } from './components/NavigationTypes'
+import { OFFERS_ROUTE } from './components/NavigationTypes'
 
 const createNavigateToOffer = <T: RoutesType>(dispatch: Dispatch<StoreActionType>, navigation: NavigationPropType<T>) =>
   ({ cityCode, language }: {| cityCode: string, language: string |}) => {
     navigation.navigate({
-      name: 'Offers',
+      name: OFFERS_ROUTE,
       params: {
         cityCode,
         sharePath: `/${cityCode}/${language}/offers`
