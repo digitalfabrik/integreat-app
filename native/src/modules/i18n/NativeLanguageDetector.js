75e06152c3404dba91f84a0928a4ac61e0b0cd4d
Maximilian Ammann <max@maxammann.org>
2020-12-18 15:19:08 +0100

    IGAPP-387: Fix some tests

--- native/src/modules/i18n/NativeLanguageDetector.js
+++ native/src/modules/i18n/NativeLanguageDetector.js
@@ -6,7 +6,7 @@ export default {
   type: 'languageDetector',
   async: false,
   // Returns array of ISO-639-2 or ISO-639-3 language codes
-  detect: (callback: (languages: string[]) => void) => {
+  detect: () => {
     const locales = getLocales()
 
     if (locales.length === 0) {
