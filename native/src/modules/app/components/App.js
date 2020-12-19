0c622318d6319750b1ea677c519598358c237695
Steffen Kleinle <klinzo@yahoo.de>
2020-12-19 14:32:36 +0100

    IGAPP-114: Fix PermissionSnackbarContainer.js

--- native/src/modules/app/components/App.js
+++ native/src/modules/app/components/App.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { Provider } from 'react-redux'
 import I18nProviderContainer from '../../i18n/containers/I18nProviderContainer'
 import createReduxStore from '../createReduxStore'
@@ -15,11 +15,20 @@ import NavigatorContainer from '../containers/NavigatorContainer'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import StaticServerProvider from '../../static-server/containers/StaticServerProvider'
 import { NavigationContainer } from '@react-navigation/native'
+import PermissionSnackbarContainer from '../../layout/containers/PermissionSnackbarContainer'
 
 const dataContainer: DataContainer = new DefaultDataContainer()
 const store: Store<StateType, StoreActionType> = createReduxStore(dataContainer)
 
 const App = () => {
+  const [routeName, setRouteName] = useState<?string>(null)
+
+  const onStateChange = useCallback(state => {
+    if (state) {
+      setRouteName(state.routes[state.index].name)
+    }
+  }, [])
+
   return (
     <Provider store={store}>
       <StaticServerProvider>
@@ -28,10 +37,11 @@ const App = () => {
             <>
               <StatusBarContainer />
               <IOSSafeAreaView>
-                <NavigationContainer>
+                <NavigationContainer onStateChange={onStateChange}>
                   <NavigatorContainer />
                 </NavigationContainer>
               </IOSSafeAreaView>
+              <PermissionSnackbarContainer routeName={routeName} />
             </>
           </SafeAreaProvider>
         </I18nProviderContainer>
