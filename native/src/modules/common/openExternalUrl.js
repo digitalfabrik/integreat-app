0e3f8bdb3540599c557e9febddba0f2823279598
Steffen Kleinle <klinzo@yahoo.de>
2020-12-22 11:06:04 +0100

    IGAPP-191: Add canOpenUrl check

--- native/src/modules/common/openExternalUrl.js
+++ native/src/modules/common/openExternalUrl.js
@@ -10,7 +10,14 @@ const openExternalUrl = async (url: string) => {
       await InAppBrowser.open(url, {
         toolbarColor: buildConfig().lightTheme.colors.themeColor
       })
-    } else { Linking.openURL(url) }
+    } else {
+      const canOpen = await Linking.canOpenURL(url)
+      if (canOpen) {
+        Linking.openURL(url)
+      } else {
+        // TODO Show a snackbar to indicate there is no suitable app to open the url
+      }
+    }
   } catch (error) {
     console.error(error)
   }
