7c4f0dcb74a4c374eeae7c7c352b79183a3d0112
Steffen Kleinle <klinzo@yahoo.de>
2021-01-06 16:38:26 +0100

    IGAPP-37: Set open graph meta tags

--- web/src/modules/common/containers/Helmet.js
+++ web/src/modules/common/containers/Helmet.js
@@ -4,6 +4,7 @@ import * as React from 'react'
 import { CityModel } from 'api-client'
 import { Helmet as ReactHelmet } from 'react-helmet'
 import type { LanguageChangePathsType } from '../../app/containers/Switcher'
+import buildConfig from '../../app/constants/buildConfig'
 
 type PropsType = {|
   pageTitle: ?string,
@@ -27,12 +28,20 @@ class Helmet extends React.PureComponent<PropsType> {
 
   render () {
     const { pageTitle, cityModel, metaDescription } = this.props
+    const previewImageUrl = new URL(buildConfig().shareBaseUrl)
+    previewImageUrl.pathname = buildConfig().icons.socialMediaPreview
 
     return <ReactHelmet>
       {pageTitle && <title>{pageTitle}</title>}
       {cityModel && !cityModel.live && <meta name='robots' content='noindex' />}
       {metaDescription && <meta name='description' content={metaDescription} />}
       {this.getLanguageLinks()}
+      {/* https://developers.facebook.com/docs/sharing/webmasters */}
+      {pageTitle && <meta property='og:title' content={pageTitle} />}
+      <meta property='og:image' content={previewImageUrl.href} />
+      {metaDescription && <meta property='og:description' content={metaDescription} />}
+      <meta property='og:url' content={window.location.href} />
+      <meta property='og:type' content='website' />
     </ReactHelmet>
   }
 }
