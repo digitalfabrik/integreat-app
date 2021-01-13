b180d04d5a0dac6ec88b6e98177f787639b9bd14
steffi <metzger@integreat-app.de>
2021-01-13 14:58:00 +0100

    IGAPP-373: Add search string to url

--- web/src/routes/search/containers/SearchPage.js
+++ web/src/routes/search/containers/SearchPage.js
@@ -30,8 +30,12 @@ type LocalStateType = {|
 const noop = () => {}
 
 export class SearchPage extends React.Component<PropsType, LocalStateType> {
-  state = {
-    filterText: ''
+  constructor (props: PropsType) {
+    super(props)
+    const initialFilterText = props.location.query?.query ? props.location.query.query : ''
+    this.state = {
+      filterText: initialFilterText
+    }
   }
 
   findCategories (): Array<CategoryEntryType> {
@@ -69,7 +73,10 @@ export class SearchPage extends React.Component<PropsType, LocalStateType> {
       .concat(categoriesWithContent)
   }
 
-  handleFilterTextChanged = (filterText: string) => this.setState({ filterText: filterText })
+  handleFilterTextChanged = (filterText: string) => {
+    this.setState({ filterText: filterText })
+    history.replaceState(null, '', `${this.props.location.pathname}?query=${filterText}`)
+  }
 
   render () {
     const categories = this.findCategories()
