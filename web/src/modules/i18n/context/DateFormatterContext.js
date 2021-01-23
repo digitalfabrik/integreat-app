05830640db465999481c44e1bdbf7a596c14923d
Maximilian Ammann <max@maxammann.org>
2021-01-23 11:19:43 +0100

    IGAPP-178: Use new api in native

--- web/src/modules/i18n/context/DateFormatterContext.js
+++ web/src/modules/i18n/context/DateFormatterContext.js
@@ -4,7 +4,6 @@ import * as React from 'react'
 import DateFormatter from 'api-client/src/i18n/DateFormatter'
 
 const DateFormatterContext = React.createContext<DateFormatter>(
-  // undefined corresponds to the ISO_8601 standard
   new DateFormatter('de')
 )
 
