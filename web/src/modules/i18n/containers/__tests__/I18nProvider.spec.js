03dd5b30c98f3b38d4c6ae7d5d1f19f8ee6078fb
Maximilian Ammann <max@maxammann.org>
2021-01-27 14:50:21 +0100

    IGAPP-18: Readd test and fix typo

--- web/src/modules/i18n/containers/__tests__/I18nProvider.spec.js
+++ web/src/modules/i18n/containers/__tests__/I18nProvider.spec.js
@@ -165,6 +165,15 @@ describe('I18nProvider', () => {
     expect(helmet.linkTags.map(link => link.href)).not.toContain('/fonts/lateef/lateef.css')
   })
 
+  it('should dispatch action for setting ui direction', async () => {
+    const store = prepareStore('en')
+    act(() => {
+      render(<Provider store={store}><I18nProvider>Hello</I18nProvider></Provider>)
+    })
+
+    expect(store.getActions()).toEqual([{ payload: 'ltr', type: 'SET_UI_DIRECTION' }])
+  })
+
   // We can not switch the language right now because it is bound to redux-first-router, we would need to trigger a
   // state update through the router which is very difficult.
   // eslint-disable-next-line jest/no-disabled-tests
