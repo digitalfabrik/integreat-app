f0bdb2677f665e8342d6011c3a4442fc0edc5614
Steffen Kleinle <klinzo@yahoo.de>
2021-02-02 15:51:47 +0100

    IGAPP-307: Add apple app id

--- build-configs/aschaffenburg/index.js
+++ build-configs/aschaffenburg/index.js
@@ -10,7 +10,7 @@ import type {
 import { ASCHAFFENBURG_ASSETS } from '../AssetsType'
 import mainImprint from './mainImprint'
 
-const APPLE_APP_ID = ''
+const APPLE_APP_ID = '1551810291'
 
 const commonAschaffenburgBuildConfig: CommonBuildConfigType = {
   appName: 'Aschaffenburg App',
@@ -19,10 +19,9 @@ const commonAschaffenburgBuildConfig: CommonBuildConfigType = {
   darkTheme,
   assets: ASCHAFFENBURG_ASSETS,
   cmsUrl: 'https://cms.integreat-app.de',
-  switchCmsUrl: 'https://cms-test.integreat-app.de',
   shareBaseUrl: 'https://aschaffenburg.app',
-  allowedHostNames: ['cms.integreat-app.de', 'cms-test.integreat-app.de'],
-  internalLinksHijackPattern: 'https?:\\/\\/(cms(-test)?\\.integreat-app\\.de|web\\.integreat-app\\.de|integreat\\.app)(?!\\/[^/]*\\/(wp-content|wp-admin|wp-json)\\/.*).*',
+  allowedHostNames: ['cms.integreat-app.de'],
+  internalLinksHijackPattern: 'https?:\\/\\/aschaffenburg\\.app(?!\\/[^/]*\\/(wp-content|wp-admin|wp-json)\\/.*).*',
   featureFlags: {
     pois: false,
     newsStream: false,
@@ -32,11 +31,11 @@ const commonAschaffenburgBuildConfig: CommonBuildConfigType = {
     developerFriendly: false,
     fixedCity: 'aschaffenburgapp'
   },
-  aboutUrls: {
+  aboutUrls: { // TODO
     default: 'https://integreat-app.de/about/',
     en: 'https://integreat-app.de/en/about/'
   },
-  privacyUrls: {
+  privacyUrls: { // TODO
     default: 'https://integreat-app.de/datenschutz/',
     en: 'https://integreat-app.de/en/privacy/'
   }
