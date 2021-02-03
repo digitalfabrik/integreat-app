826bf47498ce730f909fb807b7c7090633c89b35
steffi <metzger@integreat-app.de>
2021-02-03 13:09:31 +0100

    IGAPP-427-themable-fonts-for-body-and-heading

--- build-configs/aschaffenburg/theme/fonts.js
+++ build-configs/aschaffenburg/theme/fonts.js
@@ -4,9 +4,9 @@ import type { FontsType } from '../../common/theme/fonts'
 import { commonFonts } from '../../common/theme/fonts'
 
 const fonts: FontsType = {
-  decorativeFontBold: 'Raleway-Bold',
-  decorativeFontRegular: 'Raleway-Regular',
-  decorativeFontFamily: '\'Raleway\', \'Lateef\', sans-serif',
+  decorativeFontBold: 'VarelaRound-Regular',
+  decorativeFontRegular: 'VarelaRound-Regular',
+  decorativeFontFamily: '\'Varela Round\', \'Raleway\', \'Lateef\', sans-serif',
   contentFontRegular: 'OpenSans-Regular',
   contentFontBold: 'OpenSans-Bold',
   contentFontFamily: '\'Open Sans\', \'Lateef\', sans-serif',
