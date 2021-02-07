71e9767be5742194c27517480b5b04921b0363f4
Stefan Dohren <stefan.dohren@live.de>
2021-02-07 16:29:24 +0100

    IGAPP-173: flow 0.140, remove flow from mocks with errors

--- web/src/__mocks__/react-onclickoutside.js
+++ web/src/__mocks__/react-onclickoutside.js
@@ -1,15 +1,13 @@
-// @flow
-
 import * as React from 'react'
 import wrapDisplayName from '../modules/common/utils/wrapDisplayName'
 
 /**
  * Since enzyme rendering doesn't work with react-onclickoutside, here's a mock:
  */
-const onClickOutside = <P> (WrappedComponent: React.ComponentType<P>) => class extends React.Component<P> {
+const onClickOutside = WrappedComponent => class extends React.Component {
   static displayName = wrapDisplayName(WrappedComponent, 'OnClickOutside')
 
-  checkRef = (instance: mixed) => {
+  checkRef = instance => {
     if (!instance || typeof instance.handleClickOutside !== 'function') {
       throw new Error('WrappedComponent has no handleClickOutside handler.')
     }
