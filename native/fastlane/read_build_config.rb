c96f02acd61d0d1ed552d96898886341a9e42e7b
Maximilian Ammann <max@maxammann.org>
2021-02-24 16:19:23 +0100

    IGAPP-365: Fix command

--- native/fastlane/read_build_config.rb
+++ native/fastlane/read_build_config.rb
@@ -1,6 +1,6 @@
 require "json"
 
 def read_build_config(build_config_name, platform)
-  json = `yarn workspace --silent build-configs --silent to-json #{build_config_name} #{platform}`
+  json = `yarn workspace --silent build-configs --silent manage to-json #{build_config_name} #{platform}`
   JSON.parse(json)
 end
