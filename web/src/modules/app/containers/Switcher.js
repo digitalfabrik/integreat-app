3eb7c503ebace0a6f78444ea2a676afd2d57472e
Steffen Kleinle <klinzo@yahoo.de>
2021-02-26 12:48:52 +0100

    IGAPP-433: Fix main disclaimer page

--- web/src/modules/app/containers/Switcher.js
+++ web/src/modules/app/containers/Switcher.js
@@ -125,13 +125,10 @@ export class Switcher extends React.Component<PropsType> {
     })
 
     const cities = this.getAllPayloads().citiesPayload.data
-    if (!cities) {
-      return null
-    }
 
     const fixedCity = buildConfig().featureFlags.fixedCity
     const invalidFixedCity = city && fixedCity && city !== fixedCity
-    const invalidCity = city && (!cities.find(_city => _city.code === city) || invalidFixedCity)
+    const invalidCity = cities && city && (!cities.find(_city => _city.code === city) || invalidFixedCity)
 
     if (invalidCity || invalidLanguage || !routeConfig.isLocationLayoutRoute) {
       const showHeader = invalidLanguage || routeConfig.requiresHeader
