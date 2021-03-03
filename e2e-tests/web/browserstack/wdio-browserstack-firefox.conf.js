12d96237ddd4488d067c23e2e9aebda46ed8108e
Sarah Sporck <sporck@integreat-app.de>
2021-03-03 15:26:38 +0100

    IGAPP-15: Typescriped config files

--- e2e-tests/web/browserstack/wdio-browserstack-firefox.conf.js
+++
@@ -1,7 +0,0 @@
-const defaultConfig = require('./wdio-browserstack.conf')
-
-const config = { ...defaultConfig.config }
-config.capabilities = config.capabilities.filter(cap =>
-  cap.browserName.toLowerCase() === 'firefox'
-)
-exports.config = config
