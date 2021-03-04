f9e863f84ec730818c0a582ff041d782ab6e9757
steffi <metzger@integreat-app.de>
2021-03-04 10:43:19 +0100

    IGAPP-492: added more tests for HeaderContainer

--- native/src/modules/layout/__tests__/HeaderContainer.spec.js
+++ native/src/modules/layout/__tests__/HeaderContainer.spec.js
@@ -7,7 +7,14 @@ import LanguageModelBuilder from 'api-client/src/testing/LanguageModelBuilder'
 import type { StateType } from '../../app/StateType'
 import Header from '../components/Header'
 import { cityContentUrl, url } from '../../navigation/url'
-import { EVENTS_ROUTE, LOCAL_NEWS_TYPE, NEWS_ROUTE, OFFERS_ROUTE } from 'api-client'
+import {
+  DISCLAIMER_ROUTE,
+  EVENTS_ROUTE,
+  LOCAL_NEWS_TYPE,
+  NEWS_ROUTE,
+  OFFERS_ROUTE,
+  POIS_ROUTE
+} from 'api-client'
 
 const mockStore = configureMockStore()
 jest.mock('react-i18next')
@@ -19,8 +26,8 @@ jest.mock('../components/Header', () => {
 })
 
 describe('HeaderContainer', () => {
-  let TestRenderer
-  let Provider
+  let TestRenderer, Provider
+  let store, state
 
   beforeEach(() => {
     jest.resetModules()
@@ -28,13 +35,15 @@ describe('HeaderContainer', () => {
     // Invalid hook call https://github.com/facebook/jest/issues/8987
     TestRenderer = require('react-test-renderer')
     Provider = require('react-redux').Provider
+    state = prepareState()
+    store = mockStore(state)
   })
 
   const [city] = new CityModelBuilder(1).build()
   const languages = new LanguageModelBuilder(1).build()
   const language = languages[0]
 
-  const prepareState = (eventsRouteMapping, categoriesRouteMapping, newsRouteMapping): StateType => {
+  const prepareState = (): StateType => {
     return {
       darkMode: false,
       resourceCacheUrl: 'http://localhost:8080',
@@ -44,10 +53,59 @@ describe('HeaderContainer', () => {
           status: 'ready',
           models: [language]
         },
-        eventsRouteMapping,
-        categoriesRouteMapping,
-        poisRouteMapping: {},
-        newsRouteMapping,
+        eventsRouteMapping: {
+          routeKeyEvent1: {
+            status: 'ready',
+            path: null,
+            language: language._code,
+            city: city.name,
+            models: [],
+            allAvailableLanguages: new Map()
+          },
+          routeKeyEvent2: {
+            status: 'ready',
+            path: `${city.code}/${language._code}/${EVENTS_ROUTE}/specific-event`,
+            language: language._code,
+            city: city.name,
+            models: [],
+            allAvailableLanguages: new Map()
+          }
+        },
+        categoriesRouteMapping: {
+          routeKey1: {
+            status: 'ready',
+            path: `${city.code}/${language._code}/abc`,
+            depth: 1,
+            language: language._code,
+            city: city.name,
+            allAvailableLanguages: new Map(),
+            models: {},
+            children: {}
+          }
+        },
+        poisRouteMapping: {
+          routeKeyPois1: {
+            status: 'ready',
+            path: null,
+            language: language._code,
+            city: city.name,
+            allAvailableLanguages: new Map(),
+            models: []
+          }
+        },
+        newsRouteMapping: {
+          routeKeyNews1: {
+            status: 'ready',
+            models: [],
+            hasMoreNews: false,
+            page: 1,
+            newsId: null,
+            language: language._code,
+            city: city.name,
+            type: LOCAL_NEWS_TYPE,
+            allAvailableLanguages: new Map()
+          }
+        },
         searchRoute: null,
         resourceCache: {
           status: 'ready',
@@ -56,7 +114,7 @@ describe('HeaderContainer', () => {
         },
         switchingLanguage: false
       },
-      contentLanguage: 'de',
+      contentLanguage: 'en',
       cities: {
         status: 'ready',
         models: [city]
@@ -64,26 +122,25 @@ describe('HeaderContainer', () => {
     }
   }
 
-  it('shareUrl should be set to path for categories route', () => {
-    jest.doMock('../components/Header', () => Header)
+  const render = props => {
     const HeaderContainer = require('../containers/HeaderContainer').default
+    return TestRenderer.create(
+      <Provider store={store}>
+        {/* $FlowFixMe not all props passed */}
+        <HeaderContainer {...props}/>
+      </Provider>
+    )
+  }
 
-    const categoriesRouteMapping = {
-      routeKey1: {
-        status: 'ready',
-        path: 'abc',
-        depth: 1,
-        language: language._code,
-        city: city.name,
-        allAvailableLanguages: new Map(),
-        models: {},
-        children: {}
-      }
-    }
+  const assert = (result, expected) => {
+    const header = result.root.findByType(Header)
+    expect(header.props).toEqual(
+      expect.objectContaining(expected)
+    )
+  }
 
-    const state: StateType = prepareState({}, categoriesRouteMapping, {})
-    const store = mockStore(state)
-
+  it('shareUrl should be set correctly for categories route', () => {
+    jest.doMock('../components/Header', () => Header)
     const ownProps = {
       scene: {
         route: {
@@ -91,39 +148,13 @@ describe('HeaderContainer', () => {
         }
       }
     }
-
-    const result = TestRenderer.create(
-      <Provider store={store}>
-        {/* $FlowFixMe not all props passed */}
-        <HeaderContainer {...ownProps}/>
-      </Provider>
-    )
-
-    const header = result.root.findByType(Header)
-
-    expect(header.props).toEqual(
-      expect.objectContaining({ shareUrl: url('abc') })
-    )
+    const result = render(ownProps)
+    const expectedShareUrl = url(`${city.code}/${language._code}/abc`)
+    assert(result, { shareUrl: expectedShareUrl })
   })
 
-  it('shareUrl should be set correctly for events route', () => {
+  it('shareUrl should be set correctly for events overview route', () => {
     jest.doMock('../components/Header', () => Header)
-    const HeaderContainer = require('../containers/HeaderContainer').default
-
-    const eventsRouteMapping = {
-      routeKeyEvent1: {
-        status: 'ready',
-        path: null,
-        language: language._code,
-        city: city.name,
-        models: [],
-        allAvailableLanguages: new Map()
-      }
-    }
-
-    const state: StateType = prepareState(eventsRouteMapping, {}, {})
-    const store = mockStore(state)
-
     const ownProps = {
       scene: {
         route: {
@@ -132,41 +163,28 @@ describe('HeaderContainer', () => {
         }
       }
     }
+    const result = render(ownProps)
+    const expectedShareUrl = cityContentUrl({ cityCode: city.code, languageCode: language._code, route: EVENTS_ROUTE, path: null })
+    assert(result, { shareUrl: expectedShareUrl })
+  })
 
-    const result = TestRenderer.create(
-      <Provider store={store}>
-        {/* $FlowFixMe not all props passed */}
-        <HeaderContainer {...ownProps}/>
-      </Provider>
-    )
-
-    const header = result.root.findByType(Header)
-
-    expect(header.props).toEqual(
-      expect.objectContaining({ shareUrl: cityContentUrl({ cityCode: city.code, languageCode: language._code, route: EVENTS_ROUTE, path: null }) })
-    )
+  it('shareUrl should be set correctly for specific event route', () => {
+    jest.doMock('../components/Header', () => Header)
+    const ownProps = {
+      scene: {
+        route: {
+          name: EVENTS_ROUTE,
+          key: 'routeKeyEvent2'
+        }
+      }
+    }
+    const result = render(ownProps)
+    const expectedShareUrl = cityContentUrl({ cityCode: city.code, languageCode: language._code, route: EVENTS_ROUTE, path: 'specific-event' })
+    assert(result, { shareUrl: expectedShareUrl })
   })
 
   it('shareUrl should be set correctly for local news route', () => {
     jest.doMock('../components/Header', () => Header)
-    const HeaderContainer = require('../containers/HeaderContainer').default
-
-    const newsRouteMapping = {
-      routeKeyNews1: {
-        status: 'ready',
-        models: [],
-        hasMoreNews: false,
-        page: 1,
-        newsId: null,
-        language: language._code,
-        city: city.name,
-        type: LOCAL_NEWS_TYPE,
-        allAvailableLanguages: new Map()
-      }
-    }
-
-    const state: StateType = prepareState({}, {}, newsRouteMapping)
-    const store = mockStore(state)
 
     const ownProps = {
       scene: {
@@ -177,26 +195,13 @@ describe('HeaderContainer', () => {
       }
     }
 
-    const result = TestRenderer.create(
-      <Provider store={store}>
-        {/* $FlowFixMe not all props passed */}
-        <HeaderContainer {...ownProps}/>
-      </Provider>
-    )
-
-    const header = result.root.findByType(Header)
-
-    expect(header.props).toEqual(
-      expect.objectContaining({ shareUrl: cityContentUrl({ cityCode: city.code, languageCode: language._code, route: NEWS_ROUTE, path: LOCAL_NEWS_TYPE }) })
-    )
+    const result = render(ownProps)
+    const expectedShareUrl = cityContentUrl({ cityCode: city.code, languageCode: language._code, route: NEWS_ROUTE, path: LOCAL_NEWS_TYPE })
+    assert(result, { shareUrl: expectedShareUrl })
   })
 
   it('shareUrl should be set correctly for offers route', () => {
     jest.doMock('../components/Header', () => Header)
-    const HeaderContainer = require('../containers/HeaderContainer').default
-
-    const state: StateType = prepareState({}, {}, {})
-    const store = mockStore(state)
 
     const ownProps = {
       scene: {
@@ -206,17 +211,41 @@ describe('HeaderContainer', () => {
       }
     }
 
-    const result = TestRenderer.create(
-      <Provider store={store}>
-        {/* $FlowFixMe not all props passed */}
-        <HeaderContainer {...ownProps}/>
-      </Provider>
-    )
+    const result = render(ownProps)
+    const expectedShareUrl = cityContentUrl({ cityCode: city.code, languageCode: state.contentLanguage, route: OFFERS_ROUTE, path: null })
+    assert(result, { shareUrl: expectedShareUrl })
+  })
 
-    const header = result.root.findByType(Header)
+  it('shareUrl should be set correctly for disclaimer route', () => {
+    jest.doMock('../components/Header', () => Header)
 
-    expect(header.props).toEqual(
-      expect.objectContaining({ shareUrl: cityContentUrl({ cityCode: city.code, languageCode: state.contentLanguage, route: OFFERS_ROUTE, path: null }) })
-    )
+    const ownProps = {
+      scene: {
+        route: {
+          name: DISCLAIMER_ROUTE
+        }
+      }
+    }
+
+    const result = render(ownProps)
+    const expectedShareUrl = cityContentUrl({ cityCode: city.code, languageCode: state.contentLanguage, route: DISCLAIMER_ROUTE, path: null })
+    assert(result, { shareUrl: expectedShareUrl })
+  })
+
+  it('shareUrl should be set correctly for pois overview route', () => {
+    jest.doMock('../components/Header', () => Header)
+
+    const ownProps = {
+      scene: {
+        route: {
+          name: POIS_ROUTE,
+          key: 'routeKeyPois1'
+        }
+      }
+    }
+
+    const result = render(ownProps)
+    const expectedShareUrl = cityContentUrl({ cityCode: city.code, languageCode: state.contentLanguage, route: POIS_ROUTE, path: null })
+    assert(result, { shareUrl: expectedShareUrl })
   })
 })
