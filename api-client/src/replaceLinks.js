6d6ca3b27d671c5e3afb2335faedd3c48ac5602f
zankerbe <benjaminzanker@web.de>
2021-03-10 18:00:22 +0100

    IGAPP-488: Support links in local news

--- api-client/src/replaceLinks.js
+++ api-client/src/replaceLinks.js
@@ -3,11 +3,13 @@
 // https://stackoverflow.com/a/3809435
 const regex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w\-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[.!/\\\w]*))?)/g
 
+type ReplaceType = (match: string) => string
+
 export const linkify = (link: string): string => {
   return `<a href="${link}">${link}</a>`
 }
 
-export const replaceLinks = (content: string, replace: (match: string) => string = linkify) => {
+export const replaceLinks = (content: string, replace: ReplaceType = linkify) => {
   return content.replace(regex, replace)
 }
 
