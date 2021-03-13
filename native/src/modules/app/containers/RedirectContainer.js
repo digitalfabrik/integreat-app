77a478d94c4c10a74fe5496ef34fced086e39de6
Steffen Kleinle <klinzo@yahoo.de>
2021-03-13 15:26:56 +0100

    IGAPP-93: Add interval on top of timeout to fix already fetching error

--- native/src/modules/app/containers/RedirectContainer.js
+++ native/src/modules/app/containers/RedirectContainer.js
@@ -8,7 +8,8 @@ import { useTranslation } from 'react-i18next'
 import LayoutContainer from '../../layout/containers/LayoutContainer'
 import navigateToDeepLink from '../../navigation/navigateToDeepLink'
 
-const TIMEOUT = 50
+const TIMEOUT = 10
+const INTERVAL_TIMEOUT = 500
 
 type PropsType = {|
   route: RoutePropType<RedirectRouteType>,
@@ -25,9 +26,17 @@ const RedirectContainer = ({ route, navigation }: PropsType) => {
     // If actions are dispatched/navigate is called to early it fails and nothing happens
     // Therefore we wait for a short time period and try again if the component is still rendered
     // https://github.com/react-navigation/react-navigation/issues/8537
-    const interval = setInterval(() => {
+    const timeout = setTimeout(() => {
       navigateToDeepLink(dispatch, navigation, url, language)
     }, TIMEOUT)
+    return () => clearTimeout(timeout)
+  }, [url, dispatch, navigation, route, language])
+
+  useEffect(() => {
+    // To support potentially older devices taking longer we setup a separate interval to retry the navigation
+    const interval = setInterval(() => {
+      navigateToDeepLink(dispatch, navigation, url, language)
+    }, INTERVAL_TIMEOUT)
     return () => clearInterval(interval)
   }, [url, dispatch, navigation, route, language])
 
