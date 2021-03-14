56014d8812a12d02bbff2e2b0082ed043d3eab47
Steffen Kleinle <klinzo@yahoo.de>
2021-03-14 17:24:13 +0100

    IGAPP-522: Add navigateToSprungbrettOffer test

--- native/src/modules/navigation/__tests__/navigateToNews.spec.js
+++ native/src/modules/navigation/__tests__/navigateToNews.spec.js
@@ -4,7 +4,7 @@ import createNavigationScreenPropMock from '../../../testing/createNavigationPro
 import navigateToNews from '../navigateToNews'
 import { LOCAL_NEWS_TYPE } from 'api-client/src/routes'
 
-describe('createNavigateToNews', () => {
+describe('navigateToNews', () => {
   it('should generate key if not supplied with at least 6 chars and use it for navigation and redux actions', () => {
     const dispatch = jest.fn()
     const navigation = createNavigationScreenPropMock()
