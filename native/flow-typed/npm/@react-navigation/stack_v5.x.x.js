fc2157755895f0224c2c5ae25f497ea8fefa1bf5
Stefan Dohren <stefan.dohren@live.de>
2021-03-22 10:36:58 +0100

    IGAPP-173 run yarn flow:install-types natvie and web

--- native/flow-typed/npm/@react-navigation/stack_v5.x.x.js
+++ native/flow-typed/npm/@react-navigation/stack_v5.x.x.js
@@ -1,5 +1,5 @@
-// flow-typed signature: 2961e5cd3774ae3e34a6b8536335eaab
-// flow-typed version: 56c5bb6696/@react-navigation/stack_v5.x.x/flow_>=v0.104.x
+// flow-typed signature: 841e4d6bd26557918952256667a539e0
+// flow-typed version: 510a06cce0/@react-navigation/stack_v5.x.x/flow_>=v0.104.x
 
 declare module '@react-navigation/stack' {
 
@@ -266,7 +266,7 @@ declare module '@react-navigation/stack' {
   // Copied from
   // react-native/Libraries/Components/Touchable/TouchableWithoutFeedback.js
   declare type Stringish = string;
-  declare type EdgeInsetsProp = $ReadOnly<$Shape<EdgeInsets>>;
+  declare type EdgeInsetsProp = $ReadOnly<$Partial<EdgeInsets>>;
   declare type TouchableWithoutFeedbackProps = $ReadOnly<{|
     accessibilityActions?: ?$ReadOnlyArray<AccessibilityActionInfo>,
     accessibilityElementsHidden?: ?boolean,
@@ -419,7 +419,7 @@ declare module '@react-navigation/stack' {
   >;
   declare type $IsUndefined<X> = $IsA<X, void>;
 
-  declare type $Partial<T> = $Rest<T, {...}>;
+  declare type $Partial<T> = $ReadOnly<$Rest<T, {...}>>;
 
   /**
    * Actions, state, etc.
@@ -844,12 +844,12 @@ declare module '@react-navigation/stack' {
       State,
       EventMap,
     >>,
-    +setOptions: (options: $Shape<ScreenOptions>) => void,
+    +setOptions: (options: $Partial<ScreenOptions>) => void,
     +setParams: (
       params: $If<
         $IsUndefined<$ElementType<ParamList, RouteName>>,
         empty,
-        $Shape<$NonMaybeType<$ElementType<ParamList, RouteName>>>,
+        $Partial<$NonMaybeType<$ElementType<ParamList, RouteName>>>,
       >,
     ) => void,
     ...
@@ -896,7 +896,7 @@ declare module '@react-navigation/stack' {
           route: RouteProp<ParamList, RouteName>,
           navigation: NavProp,
         |}) => ScreenListeners<EventMap, State>,
-    +initialParams?: $Shape<$ElementType<ParamList, RouteName>>,
+    +initialParams?: $Partial<$ElementType<ParamList, RouteName>>,
   |};
 
   declare export type ScreenProps<
@@ -1179,7 +1179,7 @@ declare module '@react-navigation/stack' {
     +styleInterpolator: StackHeaderStyleInterpolator,
   |};
 
-  declare export type StackHeaderLeftButtonProps = $Shape<{|
+  declare export type StackHeaderLeftButtonProps = $Partial<{|
     +onPress: (() => void),
     +pressColorAndroid: string;
     +backImage: (props: {| tintColor: string |}) => React$Node,
@@ -1207,7 +1207,7 @@ declare module '@react-navigation/stack' {
   declare export type StackHeaderTitleInputProps =
     $Exact<StackHeaderTitleInputBase>;
 
-  declare export type StackOptions = $Shape<{|
+  declare export type StackOptions = $Partial<{|
     +title: string,
     +header: StackHeaderProps => React$Node,
     +headerShown: boolean,
@@ -1220,7 +1220,7 @@ declare module '@react-navigation/stack' {
     +gestureEnabled: boolean,
     +gestureResponseDistance: {| vertical?: number, horizontal?: number |},
     +gestureVelocityImpact: number,
-    +safeAreaInsets: $Shape<EdgeInsets>,
+    +safeAreaInsets: $Partial<EdgeInsets>,
     // Transition
     ...TransitionPreset,
     // Header
@@ -1352,7 +1352,7 @@ declare module '@react-navigation/stack' {
         >,
       |};
 
-  declare export type BottomTabOptions = $Shape<{|
+  declare export type BottomTabOptions = $Partial<{|
     +title: string,
     +tabBarLabel:
       | string
@@ -1367,7 +1367,7 @@ declare module '@react-navigation/stack' {
     +tabBarAccessibilityLabel: string,
     +tabBarTestID: string,
     +tabBarVisible: boolean,
-    +tabBarVisibilityAnimationConfig: $Shape<{|
+    +tabBarVisibilityAnimationConfig: $Partial<{|
       +show: TabBarVisibilityAnimationConfig,
       +hide: TabBarVisibilityAnimationConfig,
     |}>,
@@ -1435,7 +1435,7 @@ declare module '@react-navigation/stack' {
     BottomTabOptions,
   >;
 
-  declare export type BottomTabBarOptions = $Shape<{|
+  declare export type BottomTabBarOptions = $Partial<{|
     +keyboardHidesTabBar: boolean,
     +activeTintColor: string,
     +inactiveTintColor: string,
@@ -1449,7 +1449,7 @@ declare module '@react-navigation/stack' {
     +tabStyle: ViewStyleProp,
     +labelPosition: 'beside-icon' | 'below-icon',
     +adaptive: boolean,
-    +safeAreaInsets: $Shape<EdgeInsets>,
+    +safeAreaInsets: $Partial<EdgeInsets>,
     +style: ViewStyleProp,
   |}>;
 
@@ -1488,7 +1488,7 @@ declare module '@react-navigation/stack' {
    * Material bottom tab options
    */
 
-  declare export type MaterialBottomTabOptions = $Shape<{|
+  declare export type MaterialBottomTabOptions = $Partial<{|
     +title: string,
     +tabBarColor: string,
     +tabBarLabel: string,
@@ -1635,7 +1635,7 @@ declare module '@react-navigation/stack' {
    * Material top tab options
    */
 
-  declare export type MaterialTopTabOptions = $Shape<{|
+  declare export type MaterialTopTabOptions = $Partial<{|
     +title: string,
     +tabBarLabel:
       | string
@@ -1690,14 +1690,14 @@ declare module '@react-navigation/stack' {
     +swipeEnabled: boolean,
     +swipeVelocityImpact?: number,
     +springVelocityScale?: number,
-    +springConfig: $Shape<{|
+    +springConfig: $Partial<{|
       +damping: number,
       +mass: number,
       +stiffness: number,
       +restSpeedThreshold: number,
       +restDisplacementThreshold: number,
     |}>,
-    +timingConfig: $Shape<{|
+    +timingConfig: $Partial<{|
       +duration: number,
     |}>,
   |};
@@ -1727,7 +1727,7 @@ declare module '@react-navigation/stack' {
     +getTabWidth: number => number,
   |};
 
-  declare export type MaterialTopTabBarOptions = $Shape<{|
+  declare export type MaterialTopTabBarOptions = $Partial<{|
     +scrollEnabled: boolean,
     +bounces: boolean,
     +pressColor: string,
@@ -1773,7 +1773,7 @@ declare module '@react-navigation/stack' {
     ...$Partial<MaterialTopTabPagerCommonProps>,
     +position?: any, // Reanimated.Value<number>
     +tabBarPosition?: 'top' | 'bottom',
-    +initialLayout?: $Shape<{| +width: number, +height: number |}>,
+    +initialLayout?: $Partial<{| +width: number, +height: number |}>,
     +lazy?: boolean,
     +lazyPreloadDistance?: number,
     +removeClippedSubviews?: boolean,
@@ -1804,7 +1804,7 @@ declare module '@react-navigation/stack' {
    * Drawer options
    */
 
-  declare export type DrawerOptions = $Shape<{|
+  declare export type DrawerOptions = $Partial<{|
     title: string,
     drawerLabel:
       | string
@@ -1870,7 +1870,7 @@ declare module '@react-navigation/stack' {
     DrawerOptions,
   >;
 
-  declare export type DrawerItemListBaseOptions = $Shape<{|
+  declare export type DrawerItemListBaseOptions = $Partial<{|
     +activeTintColor: string,
     +activeBackgroundColor: string,
     +inactiveTintColor: string,
@@ -1879,7 +1879,7 @@ declare module '@react-navigation/stack' {
     +labelStyle: TextStyleProp,
   |}>;
 
-  declare export type DrawerContentOptions = $Shape<{|
+  declare export type DrawerContentOptions = $Partial<{|
     ...DrawerItemListBaseOptions,
     +contentContainerStyle: ViewStyleProp,
     +style: ViewStyleProp,
@@ -2072,10 +2072,10 @@ declare module '@react-navigation/stack' {
 
   declare export var Header: React$ComponentType<StackHeaderProps>;
 
-  declare export type StackHeaderTitleProps = $Shape<StackHeaderTitleInputBase>;
+  declare export type StackHeaderTitleProps = $Partial<StackHeaderTitleInputBase>;
   declare export var HeaderTitle: React$ComponentType<StackHeaderTitleProps>;
 
-  declare export type HeaderBackButtonProps = $Shape<{|
+  declare export type HeaderBackButtonProps = $Partial<{|
     ...StackHeaderLeftButtonProps,
     +disabled: boolean,
     +accessibilityLabel: string,
@@ -2084,7 +2084,7 @@ declare module '@react-navigation/stack' {
     HeaderBackButtonProps,
   >;
 
-  declare export type HeaderBackgroundProps = $Shape<{
+  declare export type HeaderBackgroundProps = $Partial<{
     +children: React$Node,
     +style: AnimatedViewStyleProp,
     ...
