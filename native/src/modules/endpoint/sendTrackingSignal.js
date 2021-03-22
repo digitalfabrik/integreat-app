8894a0afb2c90c8752ddd1431f5caea27e52a6de
Steffen Kleinle <klinzo@yahoo.de>
2021-03-22 16:41:50 +0100

    IGAPP-503: Send tracking signal on navigate

---
+++ native/src/modules/endpoint/sendTrackingSignal.js
@@ -0,0 +1,37 @@
+// @flow
+
+import { createTrackingEndpoint, type SpecificSignalType  } from 'api-client'
+import AppSettings from '../settings/AppSettings'
+import moment from 'moment'
+import type { SettingsType } from '../settings/AppSettings'
+
+const sendTrackingSignal = async ({ signal, offline = false }: {| signal: SpecificSignalType, offline?: boolean |}) => {
+  try {
+    const appSettings = new AppSettings()
+    const settings: SettingsType = await appSettings.loadSettings()
+    const { jpalTrackingEnabled, selectedCity, contentLanguage, allowPushNotifications, proposeNearbyCities, errorTracking, jpalTrackingCode } = settings
+
+    if (jpalTrackingCode && jpalTrackingEnabled) {
+      await createTrackingEndpoint().request({
+        ...signal,
+        trackingCode: jpalTrackingCode,
+        metadata: {
+          offline,
+          timestamp: moment().toISOString(),
+          currentCity: selectedCity,
+          currentLanguage: contentLanguage,
+          systemLanguage: '',
+          appSettings: {
+            allowPushNotifications,
+            proposeNearbyCities,
+            errorTracking
+          }
+        }
+      })
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+export default sendTrackingSignal
