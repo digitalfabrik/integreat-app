16c553005707e625eb2ef85be14474030cc59445
Steffen Kleinle <klinzo@yahoo.de>
2021-03-23 12:06:55 +0100

    IGAPP-503: Prettier

--- native/src/modules/layout/containers/HeaderContainer.js
+++ native/src/modules/layout/containers/HeaderContainer.js
@@ -49,8 +49,8 @@ const mapStateToProps = (state: StateType, ownProps: OwnPropsType): StatePropsTy
   const routeName = ownProps.scene.route.name
   const simpleRouteShareUrl =
     typeof cityCode === 'string' && simpleRoutes.includes(routeName)
-      // $FlowFixMe Flow does not understand life and just hates the whole world :/
-      ? urlFromRouteInformation({ cityCode, languageCode: state.contentLanguage, route: routeName })
+      ? // $FlowFixMe Flow does not understand life and just hates the whole world :/
+        urlFromRouteInformation({ cityCode, languageCode: state.contentLanguage, route: routeName })
       : null
 
   const languages = state.cityContent?.languages
@@ -99,10 +99,10 @@ const mapStateToProps = (state: StateType, ownProps: OwnPropsType): StatePropsTy
 
   const routeInformation: RouteInformationType =
     routeType === NEWS_ROUTE
-      // $FlowFixMe Flow does not get anything and hates the whole world :/ I am already checking the routeType!
-      ? { route: NEWS_ROUTE, languageCode: language, cityCode: city, newsType: route.type, newsId: route.newsId }
-      // $FlowFixMe Flow does not understand life and just hates the whole world :/
-      : { route: routeType, languageCode: language, cityCode: city, cityContentPath: route.path || undefined }
+      ? // $FlowFixMe Flow does not get anything and hates the whole world :/ I am already checking the routeType!
+        { route: NEWS_ROUTE, languageCode: language, cityCode: city, newsType: route.type, newsId: route.newsId }
+      : // $FlowFixMe Flow does not understand life and just hates the whole world :/
+        { route: routeType, languageCode: language, cityCode: city, cityContentPath: route.path || undefined }
 
   const shareUrl = urlFromRouteInformation(routeInformation)
 
