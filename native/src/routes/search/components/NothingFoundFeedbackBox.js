813b7ec01dc6d80bea4bd8b483c1952cba542cd4
Steffen Kleinle <klinzo@yahoo.de>
2021-03-24 13:14:06 +0100

    IGAPP-577: Refactoring

--- native/src/routes/search/components/NothingFoundFeedbackBox.js
+++ native/src/routes/search/components/NothingFoundFeedbackBox.js
@@ -1,16 +1,16 @@
 // @flow
 
-import React from 'react'
+import React, { useState } from 'react'
 import { ActivityIndicator, Text, TextInput } from 'react-native'
 import { Button } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/MaterialIcons'
-import type { ThemeType } from '../../../modules/theme/constants'
+import type { ThemeType } from 'build-configs/ThemeType'
 import type { TFunction } from 'react-i18next'
 import styled from 'styled-components/native'
 import { type StyledComponent } from 'styled-components'
 import buildConfig from '../../../modules/app/constants/buildConfig'
 
-const Input = styled(TextInput)`
+const Input: StyledComponent<{||}, ThemeType, *> = styled(TextInput)`
   margin-bottom: 15px;
   border-bottom-width: 1px;
   border-bottom-color: ${props => props.theme.colors.textSecondaryColor};
@@ -19,7 +19,7 @@ const Input = styled(TextInput)`
 const DescriptionText: StyledComponent<{||}, ThemeType, *> = styled(Text)`
   margin-top: 20px;
   color: ${props => props.theme.colors.textColor};
-  font-family: ${props => props.theme.fonts.decorativeFontRegular};
+  font-family: ${props => props.theme.fonts.native.decorativeFontRegular};
 `
 
 const TitleText: StyledComponent<{||}, ThemeType, *> = styled.Text`
@@ -37,66 +37,51 @@ type PropsType = {|
 
 type SendingStatusType = 'idle' | 'sending' | 'failed' | 'successful'
 
-type StateType = {|
-  comment: string,
-  sendingStatus: SendingStatusType
-|}
+const NothingFoundFeedbackBox = ({ query, t, theme, sendFeedback }: PropsType) => {
+  const [comment, setComment] = useState<string>('')
+  const [sendingStatus, setSendingStatus] = useState<SendingStatusType>('idle')
 
-class NothingFoundFeedbackBox extends React.Component<PropsType, StateType> {
-  state = { comment: '', sendingStatus: 'idle' }
+  const onCommentChanged = (value: string) => setComment(value)
 
-  onCommentChanged = (value: string) => this.setState({ comment: value })
-
-  onSubmit = async () => {
-    this.setState({ sendingStatus: 'sending' })
-    this.props
-      .sendFeedback(this.state.comment, this.props.query)
-      .then(() => this.setState({ sendingStatus: 'successful' }))
-      .catch(() => this.setState({ sendingStatus: 'failed' }))
+  const onSubmit = async () => {
+    setSendingStatus('sending')
+    sendFeedback(comment, query)
+      .then(() => setSendingStatus('successful'))
+      .catch(() => setSendingStatus('failed'))
   }
 
-  render() {
-    const { sendingStatus, comment } = this.state
-    const { t, theme } = this.props
-
-    if (['idle', 'failed'].includes(sendingStatus)) {
-      return (
-        <>
-          <TitleText theme={theme}>{t('feedback:nothingFound')}</TitleText>
-          {sendingStatus === 'failed' && (
-            <DescriptionText theme={theme}>{t('feedback:failedSendingFeedback')}</DescriptionText>
-          )}
-          <DescriptionText theme={theme}>{t('feedback:wantedInformation')}</DescriptionText>
-          <Input
-            theme={theme}
-            onChangeText={this.onCommentChanged}
-            value={comment}
-            multiline
-            placeholderTextColor={theme.colors.textSecondaryColor}
-            placeholder={t('feedback:yourFeedback')}
-          />
-          <Button
-            icon={<Icon name='send' size={15} color='black' style='material' />}
-            titleStyle={{ color: theme.colors.textColor }}
-            buttonStyle={{ backgroundColor: theme.colors.themeColor }}
-            onPress={this.onSubmit}
-            title={t('feedback:send')}
-          />
-        </>
-      )
-    } else if (sendingStatus === 'sending') {
-      return <ActivityIndicator size='large' color='#0000ff' />
-    } else {
-      // sendingStatus === 'successful'
-      return (
-        <>
-          <TitleText theme={theme}>{t('feedback:feedbackSent')}</TitleText>
-          <DescriptionText theme={theme}>
-            {t('feedback:thanksMessage', { appName: buildConfig().appName })}
-          </DescriptionText>
-        </>
-      )
-    }
+  if (['idle', 'failed'].includes(sendingStatus)) {
+    return (
+      <>
+        <TitleText>{t('feedback:nothingFound')}</TitleText>
+        {sendingStatus === 'failed' && <DescriptionText>{t('feedback:failedSendingFeedback')}</DescriptionText>}
+        <DescriptionText>{t('feedback:wantedInformation')}</DescriptionText>
+        <Input
+          onChangeText={onCommentChanged}
+          value={comment}
+          multiline
+          placeholderTextColor={theme.colors.textSecondaryColor}
+          placeholder={t('feedback:yourFeedback')}
+        />
+        <Button
+          icon={<Icon name='send' size={15} color='black' style='material' />}
+          titleStyle={{ color: theme.colors.textColor }}
+          buttonStyle={{ backgroundColor: theme.colors.themeColor }}
+          onPress={onSubmit}
+          title={t('feedback:send')}
+        />
+      </>
+    )
+  } else if (sendingStatus === 'sending') {
+    return <ActivityIndicator size='large' color='#0000ff' />
+  } else {
+    // sendingStatus === 'successful'
+    return (
+      <>
+        <TitleText>{t('feedback:feedbackSent')}</TitleText>
+        <DescriptionText>{t('feedback:thanksMessage', { appName: buildConfig().appName })}</DescriptionText>
+      </>
+    )
   }
 }
 
