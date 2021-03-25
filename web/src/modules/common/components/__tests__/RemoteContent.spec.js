9a5608e5968aa8cfff0e7780b353409f3727729a
Sarah Sporck <sporck@integreat-app.de>
2021-03-25 12:50:33 +0100

    IGAPP-70: Implement test for internal linking

--- web/src/modules/common/components/__tests__/RemoteContent.spec.js
+++ web/src/modules/common/components/__tests__/RemoteContent.spec.js
@@ -1,8 +1,10 @@
 // @flow
 
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
 import RemoteContent from '../RemoteContent'
+import { ThemeProvider } from 'styled-components'
+import lightTheme from '../../../theme/constants/theme'
 
 describe('RemoteContent', () => {
   it('should render', () => {
@@ -23,5 +25,36 @@ describe('RemoteContent', () => {
         />
       )
     ).toMatchSnapshot()
+  })
+
+  it('should trigger on internalLinkClick', () => {
+    const path = '/augsburg/de'
+    const href = `https://integreat.app${path}`
+    const html = `<a href=${href}>Test Anchor</a>`
+    const onInternalLinkClick = jest.fn()
+
+    // Solution to test onClick event on dangerouslySetInnerHtml:
+    // https://stackoverflow.com/a/64251522
+    const body = document.getElementsByTagName('body')[0]
+    const child = document.createElement('div')
+
+    body.appendChild(child)
+
+    mount(
+      <ThemeProvider theme={lightTheme}>
+        <RemoteContent dangerouslySetInnerHTML={{ __html: html }}
+                       onInternalLinkClick={onInternalLinkClick}
+        />
+      </ThemeProvider>, { attachTo: child }
+    )
+
+    expect(document.querySelectorAll('a')).toHaveLength(1)
+    const event = new Event('click')
+    document.querySelector('a')?.dispatchEvent(event)
+
+    expect(onInternalLinkClick).toHaveBeenCalledTimes(1)
+    expect(onInternalLinkClick).toHaveBeenLastCalledWith(path)
+
+    body.removeChild(child)
   })
 })
