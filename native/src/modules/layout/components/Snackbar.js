3d0d3cb30399afe8666bbb185451bebdfa76797d
Steffen Kleinle <klinzo@yahoo.de>
2021-03-26 13:57:23 +0100

    IGAPP-496: Add snackbar container

--- native/src/modules/layout/components/Snackbar.js
+++ native/src/modules/layout/components/Snackbar.js
@@ -6,27 +6,27 @@ import styled from 'styled-components/native'
 import { type StyledComponent } from 'styled-components'
 import type { ThemeType } from '../../theme/constants'
 
-const Container: StyledComponent<{}, ThemeType, *> = styled(Animated.View)`
+const Container: StyledComponent<{| row: boolean |}, ThemeType, *> = styled(Animated.View)`
   background-color: ${props => props.theme.colors.textSecondaryColor};
-  flex-direction: column;
+  flex-direction: ${props => props.row ? 'row' : 'column'};
   align-items: center;
   padding: 10px;
 `
 
-const Message: StyledComponent<{}, ThemeType, *> = styled.Text`
+const Message: StyledComponent<{||}, ThemeType, *> = styled.Text`
   padding: 0 10px;
   color: ${props => props.theme.colors.backgroundColor};
   font-size: 18px;
   text-align: center;
 `
 
-const ActionContainer: StyledComponent<{}, ThemeType, *> = styled.View`
-  flex-direction: row;
+const ActionContainer: StyledComponent<{| row: boolean |}, ThemeType, *> = styled.View`
+  flex-direction: ${props => props.row ? 'row' : 'column'};
   justify-content: space-around;
   align-items: center;
 `
 
-const Action: StyledComponent<{}, ThemeType, *> = styled.Text`
+const Action: StyledComponent<{||}, ThemeType, *> = styled.Text`
   flex: 1;
   color: ${props => props.theme.colors.themeColor};
   font-size: 18px;
@@ -43,37 +43,26 @@ export type SnackbarActionType = {|
 
 export type PropsType = {|
   message: string,
-  positiveAction: SnackbarActionType,
-  negativeAction: SnackbarActionType,
-  theme: ThemeType
+  positiveAction?: SnackbarActionType,
+  negativeAction?: SnackbarActionType
 |}
 
-class Snackbar extends React.Component<PropsType> {
-  onPositive = () => {
-    this.props.positiveAction.onPress()
-  }
+const Snackbar = ({ message, positiveAction, negativeAction }: PropsType) => {
+  const horizontal = !(positiveAction && negativeAction)
 
-  onNegative = () => {
-    this.props.negativeAction.onPress()
-  }
-
-  render() {
-    const { theme, message, positiveAction, negativeAction } = this.props
-
-    return (
-      <Container theme={theme}>
-        <Message theme={theme}>{message}</Message>
-        <ActionContainer>
-          <Action theme={theme} onPress={this.onNegative}>
-            {negativeAction.label}
-          </Action>
-          <Action theme={theme} onPress={this.onPositive}>
-            {positiveAction.label}
-          </Action>
-        </ActionContainer>
-      </Container>
-    )
-  }
+  return (
+    <Container row={horizontal}>
+      <Message>{message}</Message>
+      <ActionContainer row={!horizontal}>
+        {negativeAction && <Action onPress={negativeAction.onPress}>
+          {negativeAction.label}
+        </Action>}
+        {positiveAction && <Action onPress={positiveAction.onPress}>
+          {positiveAction.label}
+        </Action>}
+      </ActionContainer>
+    </Container>
+  )
 }
 
 export default Snackbar
