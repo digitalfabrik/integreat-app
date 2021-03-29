bad807cfc42486f62468e624ffc40189ae25aef0
steffi <metzger@integreat-app.de>
2021-03-29 11:24:28 +0200

    IGAPP-526: Adjustments jpal settings screen

--- native/src/routes/jpal-tracking/JpalTracking.js
+++ native/src/routes/jpal-tracking/JpalTracking.js
@@ -1,11 +1,10 @@
 // @flow
 
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import type { ThemeType } from 'build-configs/ThemeType'
 import { Switch, Text, TextInput, View } from 'react-native'
 import { useTranslation } from 'react-i18next'
-import type { StateType } from '../../modules/app/StateType'
 import AppSettings, { defaultSettings } from '../../modules/settings/AppSettings'
 import type { NavigationPropType, RoutePropType } from '../../modules/app/constants/NavigationTypes'
 import type { JpalTrackingRouteType } from 'api-client'
@@ -18,7 +17,7 @@ const ThemedText = styled.Text`
   display: flex;
   text-align: left;
   color: ${props => props.theme.colors.textColor};
-  font-family: ${props => props.theme.fonts.decorativeFontRegular};
+  font-family: ${props => props.theme.fonts.native.decorativeFontRegular};
   padding: 10px 0;
 `
 
@@ -28,7 +27,7 @@ const ErrorText = styled.Text`
   padding: 10px 0;
 `
 
-const DescriptionContainer = styled.View`
+const DescriptionContainer = styled.TouchableOpacity`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -51,11 +50,11 @@ export type PropsType = {|
 
 const errorDisplayTime = 5000
 
-const JpalTracking = (props: PropsType, state: StateType) => {
-  const appSettings = useMemo(() => new AppSettings(), [])
+const appSettings = new AppSettings()
+
+const JpalTracking = (props: PropsType) => {
   const [settings, setSettings] = useState(defaultSettings)
   const [settingsLoaded, setSettingsLoaded] = useState(false)
-  const [settingsUpdated, setSettingsUpdated] = useState(false)
   const [displayedTrackingCode, setDisplayedTrackingCode] = useState('')
   const [displayedTrackingEnabled, setDisplayedTrackingEnabled] = useState(false)
   const [error, setError] = useState(false)
@@ -67,30 +66,28 @@ const JpalTracking = (props: PropsType, state: StateType) => {
     if (routeTrackingCode) {
       appSettings.setJpalTrackingCode(routeTrackingCode)
     }
-  }, [appSettings, routeTrackingCode])
+  }, [routeTrackingCode])
 
   useEffect(() => {
     const loadSettings = async () => {
       try {
         const loadedSettings = await appSettings.loadSettings()
         setSettingsLoaded(true)
-        setSettingsUpdated(false)
         setSettings(loadedSettings)
         setDisplayedTrackingCode(loadedSettings.jpalTrackingCode)
         setDisplayedTrackingEnabled(loadedSettings.jpalTrackingEnabled)
         setTimeout(() => setError(false), errorDisplayTime)
       } catch (e) {
-        // setError(true)
+        setError(true)
       }
     }
     loadSettings()
-  }, [appSettings, settingsUpdated])
+  }, [])
 
   const toggleTrackingEnabled = () => {
     setDisplayedTrackingEnabled(!displayedTrackingEnabled)
     appSettings.setJpalTrackingEnabled(!settings.jpalTrackingEnabled).catch(() => {
       setError(true)
-      setSettingsUpdated(true)
     })
   }
 
@@ -98,9 +95,7 @@ const JpalTracking = (props: PropsType, state: StateType) => {
     setDisplayedTrackingCode(value)
     appSettings.setJpalTrackingCode(value).catch(() => {
       setError(true)
-      setSettingsUpdated(true)
     })
-    props.navigation.setParams({ trackingCode: value })
   }
 
   if (!settingsLoaded) {
@@ -110,31 +105,33 @@ const JpalTracking = (props: PropsType, state: StateType) => {
   const { theme } = props
 
   return (
-    <View style={{ padding: 40 }}>
-      <Caption title={t('tracking')} theme={theme} />
-      <Text>{t('trackingDescription')}</Text>
+    <LayoutContainer>
+      <View style={{ padding: 40 }}>
+        <Caption title={t('tracking')} theme={theme} />
+        <Text>{t('trackingDescription')}</Text>
 
-      {error && <ErrorText>{t('error:generalError')}</ErrorText>}
+        {error && <ErrorText>{t('error:generalError')}</ErrorText>}
 
-      <DescriptionContainer>
-        <ThemedText theme={props.theme}>{t('allowTracking')}</ThemedText>
-        <Switch
-          thumbColor={props.theme.colors.themeColor}
-          trackColor={{ true: props.theme.colors.themeColor }}
-          value={displayedTrackingEnabled}
-          onValueChange={toggleTrackingEnabled}
-          testID='switch'
-        />
-      </DescriptionContainer>
+        <DescriptionContainer onPress={toggleTrackingEnabled}>
+          <ThemedText theme={props.theme}>{t('allowTracking')}</ThemedText>
+          <Switch
+            thumbColor={props.theme.colors.themeColor}
+            trackColor={{ true: props.theme.colors.themeColor }}
+            value={displayedTrackingEnabled}
+            onValueChange={toggleTrackingEnabled}
+            testID='switch'
+          />
+        </DescriptionContainer>
 
-      <ThemedText theme={props.theme}>{t('trackingCode')}</ThemedText>
-      <Input
-        value={displayedTrackingCode}
-        onChangeText={setTrackingCode}
-        theme={theme}
-        editable={displayedTrackingEnabled}
-        testID='input'></Input>
-    </View>
+        <ThemedText theme={props.theme}>{t('trackingCode')}</ThemedText>
+        <Input
+          value={displayedTrackingCode}
+          onChangeText={setTrackingCode}
+          theme={theme}
+          editable={displayedTrackingEnabled}
+          testID='input'></Input>
+      </View>
+    </LayoutContainer>
   )
 }
 
