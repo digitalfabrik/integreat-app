f123a0851543fe33c2e145616bf8bf22e2a45159
Steffen Kleinle <klinzo@yahoo.de>
2021-04-08 19:40:38 +0200

    IGAPP-588: Remove intro slide settings

--- native/src/routes/intro/IntroContainer.js
+++ native/src/routes/intro/IntroContainer.js
@@ -10,16 +10,11 @@ import styled from 'styled-components/native'
 import { type StyledComponent } from 'styled-components'
 import AppSettings from '../../modules/settings/AppSettings'
 import SlideContent, { type SlideContentType } from './SlideContent'
-import initSentry from '../../modules/app/initSentry'
 import SlideFooter from './footer/SlideFooter'
 import type { ViewToken } from 'react-native/Libraries/Lists/ViewabilityHelper'
-import CustomizableIntroSettings from './CustomizableIntroSettings'
-import IntroSettings from './IntroSettings'
 import type { StateType as ReduxStateType } from '../../modules/app/StateType'
 import { connect } from 'react-redux'
-import { requestLocationPermission } from '../../modules/app/LocationPermissionManager'
 import buildConfig, { buildConfigAssets } from '../../modules/app/constants/buildConfig'
-import { requestPushNotificationPermission } from '../../modules/push-notifications/PushNotificationsManager'
 import type { NavigationPropType, RoutePropType } from '../../modules/app/constants/NavigationTypes'
 import { LANDING_ROUTE } from 'api-client/src/routes'
 import type { IntroRouteType } from 'api-client/src/routes'
@@ -27,7 +22,7 @@ import navigateToDeepLink from '../../modules/navigation/navigateToDeepLink'
 import type { StoreActionType } from '../../modules/app/StoreActionType'
 
 const Container: StyledComponent<{| width: number |}, {||}, *> = styled.View`
-  display: flex;
+  flex: 1;
   flex-direction: column;
   width: ${props => props.width}px;
   height: 100%;
@@ -65,18 +60,10 @@ type PropsType = {|
   dispatch: Dispatch<StoreActionType>
 |}
 
-export type IntroSettingsType = {|
-  allowPushNotifications: boolean,
-  proposeNearbyCities: boolean,
-  errorTracking: boolean
-|}
-
 type StateType = {|
   slideCount: number,
   currentSlide: number,
-  customizableSettings: boolean,
-  width: number,
-  ...IntroSettingsType
+  width: number
 |}
 
 class Intro extends React.Component<PropsType, StateType> {
@@ -88,10 +75,6 @@ class Intro extends React.Component<PropsType, StateType> {
     this.state = {
       slideCount: this.slides().length,
       currentSlide: 0,
-      customizableSettings: false,
-      allowPushNotifications: false,
-      proposeNearbyCities: false,
-      errorTracking: false,
       width: Dimensions.get('window').width
     }
     this._appSettings = new AppSettings()
@@ -115,11 +98,6 @@ class Intro extends React.Component<PropsType, StateType> {
           title: t('appName', { appName: buildConfig().appName }),
           description: t('appDescription', { appName: buildConfig().appName }),
           renderContent: this.renderAppIcon()
-        },
-        {
-          key: 'inquiry',
-          title: t('inquiryTitle'),
-          renderContent: this.renderSettings
         }
       ]
     }
@@ -153,84 +131,27 @@ class Intro extends React.Component<PropsType, StateType> {
         title: t('languageChange'),
         description: t('languageChangeDescription'),
         renderContent: this.renderImageContent(icons.language)
-      },
-      {
-        key: 'inquiry',
-        title: t('inquiryTitle'),
-        renderContent: this.renderSettings
       }
     ]
   }
 
-  toggleCustomizeSettings = () =>
-    this.setState(prevState => ({ customizableSettings: !prevState.customizableSettings }))
-
-  toggleAllowPushNotifications = () =>
-    this.setState(prevState => ({ allowPushNotifications: !prevState.allowPushNotifications }))
-
-  toggleProposeCities = () => this.setState(prevState => ({ proposeNearbyCities: !prevState.proposeNearbyCities }))
-
-  toggleErrorTracking = () => this.setState(prevState => ({ errorTracking: !prevState.errorTracking }))
-
-  renderSettings = () => {
-    const { customizableSettings, allowPushNotifications, proposeNearbyCities, errorTracking } = this.state
-    const { language, theme, t } = this.props
-    if (customizableSettings) {
-      return (
-        <CustomizableIntroSettings
-          allowPushNotifications={allowPushNotifications}
-          toggleSetAllowPushNotifications={this.toggleAllowPushNotifications}
-          proposeNearbyCities={proposeNearbyCities}
-          toggleProposeNearbyCities={this.toggleProposeCities}
-          errorTracking={errorTracking}
-          toggleErrorTracking={this.toggleErrorTracking}
-          theme={theme}
-          t={t}
-        />
-      )
-    } else {
-      return <IntroSettings theme={theme} language={language} t={t} />
-    }
-  }
-
-  onDone = async ({
-    errorTracking: sentry,
-    allowPushNotifications: pushNotifications,
-    proposeNearbyCities: nearbyCities
-  }: $Shape<IntroSettingsType>) => {
-    const errorTracking = sentry !== undefined ? sentry : this.state.errorTracking
-    const proposeNearbyCities = nearbyCities !== undefined ? nearbyCities : this.state.proposeNearbyCities
-    const allowPushNotifications =
-      pushNotifications !== undefined ? pushNotifications : this.state.allowPushNotifications
-
+  // TODO Request permissions
+  onDone = async () => {
     try {
-      if (errorTracking) {
-        initSentry()
-      }
+      const { dispatch, route, navigation, language } = this.props
 
-      if (proposeNearbyCities) {
-        await requestLocationPermission()
+      if (route.params?.deepLink) {
+        navigateToDeepLink(dispatch, navigation, route.params.deepLink, language)
+      } else {
+        navigation.replace(LANDING_ROUTE)
       }
-
-      if (allowPushNotifications) {
-        await requestPushNotificationPermission()
-      }
+      await this._appSettings.setIntroShown()
     } catch (e) {
       console.warn(e)
-    }
-    await this._appSettings.setSettings({ errorTracking, allowPushNotifications, proposeNearbyCities })
-    await this._appSettings.setIntroShown()
-
-    const { dispatch, route, navigation, language } = this.props
-    if (route.params?.deepLink) {
-      navigateToDeepLink(dispatch, navigation, route.params.deepLink, language)
-    } else {
-      navigation.replace(LANDING_ROUTE)
     }
   }
 
   goToSlide = (index: number) => {
-    this.setState({ customizableSettings: false })
     if (!this._flatList.current) {
       throw Error('ref not correctly set')
     }
@@ -251,7 +172,7 @@ class Intro extends React.Component<PropsType, StateType> {
 
   render() {
     const { theme, t } = this.props
-    const { customizableSettings, slideCount, currentSlide, width } = this.state
+    const { slideCount, currentSlide, width } = this.state
     return (
       <Container width={width}>
         <FlatList
@@ -268,8 +189,6 @@ class Intro extends React.Component<PropsType, StateType> {
         <SlideFooter
           slideCount={slideCount}
           onDone={this.onDone}
-          toggleCustomizeSettings={this.toggleCustomizeSettings}
-          customizableSettings={customizableSettings}
           currentSlide={currentSlide}
           goToSlide={this.goToSlide}
           theme={theme}
