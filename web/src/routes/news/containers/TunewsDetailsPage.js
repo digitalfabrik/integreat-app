132730f39b44d0150711bf182997b14db9e29979
Andreas Fischer <andreas.fischer@prodyna.com>
2021-04-16 11:31:56 +0200

    feat[IGAPP-592] Replace Title & Content of Tunews DetailPage: Replaced title & content with page component. Added sanitizer to prevent xss attacks.

--- web/src/routes/news/containers/TunewsDetailsPage.js
+++ web/src/routes/news/containers/TunewsDetailsPage.js
@@ -1,6 +1,7 @@
 // @flow
 
 import * as React from 'react'
+import { push } from 'redux-first-router'
 import styled, { type StyledComponent } from 'styled-components'
 import TunewsIcon from './../assets/TunewsActiveLogo.png'
 import { CityModel, NotFoundError, TunewsModel } from 'api-client'
@@ -12,6 +13,8 @@ import { useContext } from 'react'
 import DateFormatterContext from '../../../modules/i18n/context/DateFormatterContext'
 import { TU_NEWS_TYPE } from 'api-client/src/routes'
 import type { ThemeType } from 'build-configs/ThemeType'
+import Page from '../../../modules/common/components/Page'
+import { lastUpdateDateFormat } from '../../../modules/common/constants/news'
 
 const StyledContainer: StyledComponent<{||}, ThemeType, *> = styled.div`
   display: flex;
@@ -45,15 +48,7 @@ const StyledTitle: StyledComponent<{||}, ThemeType, *> = styled.div`
   font-size: 20px;
   font-weight: 700;
 `
-const Title: StyledComponent<{||}, ThemeType, *> = styled.h2`
-  margin-bottom: 5px;
-  font-size: 24px;
-  font-weight: 700;
-`
-const Content: StyledComponent<{||}, ThemeType, *> = styled.p`
-  font-size: 16px;
-  line-height: 1.38;
-`
+
 type PropsType = {|
   tunewsElement: TunewsModel,
   language: string,
@@ -74,6 +69,10 @@ export const TunewsDetailsPage = ({ tunewsElement, language, id, city, cities }:
   }
 
   const { title, content, date, eNewsNo } = tunewsElement
+  // Removes Last Update Timestamp in content
+  const filterPattern = `<p>${eNewsNo}</p>`;
+  const filteredContent = content.replace(filterPattern,"");
+
   return (
     <StyledContainer>
       <StyledWrapper>
@@ -82,8 +81,15 @@ export const TunewsDetailsPage = ({ tunewsElement, language, id, city, cities }:
             <StyledBannerImage src={TunewsIcon} alt='' />
           </StyledTitle>
         </StyledBanner>
-        <Title>{title}</Title>
-        <Content>{content}</Content>
+        <Page
+          title={title}
+          content={filteredContent}
+          formatter={formatter}
+          lastUpdateFormat={lastUpdateDateFormat}
+          lastUpdate={date}
+          showLastUpdateText={false}
+          onInternalLinkClick={push}
+        />
       </StyledWrapper>
       <TunewsDetailsFooter eNewsNo={eNewsNo} date={date} formatter={formatter} />
     </StyledContainer>
