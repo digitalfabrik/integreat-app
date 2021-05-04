f12ca23137ed8d7cf4cd2840b52db4c3287bf462
Sarah Sporck <sporck@integreat-app.de>
2021-05-04 13:40:58 +0200

    IGAPP-564: prettier

--- native/src/modules/feedback/__tests__/FeedbackContainer.spec.js
+++ native/src/modules/feedback/__tests__/FeedbackContainer.spec.js
@@ -30,11 +30,8 @@ describe('FeedbackContainer', () => {
   const language = 'de'
 
   it('should send feedback request on submit', async () => {
-    const { getByText, findByText } = render(<FeedbackContainer
-      routeType={CATEGORIES_ROUTE}
-      feedbackOrigin={'positive'}
-      language={language}
-      cityCode={city} />
+    const { getByText, findByText } = render(
+      <FeedbackContainer routeType={CATEGORIES_ROUTE} feedbackOrigin={'positive'} language={language} cityCode={city} />
     )
 
     const button = getByText('send')
@@ -48,12 +45,13 @@ describe('FeedbackContainer', () => {
       isPositiveRating: true,
       city,
       language,
-      comment: expect.stringContaining('Kontaktadresse: Keine Angabe'),
+      comment: expect.stringContaining('Kontaktadresse: Keine Angabe')
     })
     expect(sendTrackingSignal).toHaveBeenCalledTimes(1)
     expect(sendTrackingSignal).toHaveBeenCalledWith({
       signal: {
-        name: SEND_FEEDBACK_SIGNAL_NAME, feedback: {
+        name: SEND_FEEDBACK_SIGNAL_NAME,
+        feedback: {
           positive: true,
           numCharacters: 0,
           contactMail: false
@@ -65,14 +63,11 @@ describe('FeedbackContainer', () => {
   it('should send feedback request with comment and contact information on submit', async () => {
     const comment = 'my comment'
     const contactMail = 'test@example.com'
-    const { getByText, findByText, getAllByDisplayValue } = render(<FeedbackContainer
-      routeType={CATEGORIES_ROUTE}
-      feedbackOrigin={'positive'}
-      language={language}
-      cityCode={city} />
+    const { getByText, findByText, getAllByDisplayValue } = render(
+      <FeedbackContainer routeType={CATEGORIES_ROUTE} feedbackOrigin={'positive'} language={language} cityCode={city} />
     )
 
-    const [commentField, emailField] = getAllByDisplayValue('', )
+    const [commentField, emailField] = getAllByDisplayValue('')
     fireEvent.changeText(commentField, comment)
     fireEvent.changeText(emailField, contactMail)
 
@@ -95,7 +90,8 @@ describe('FeedbackContainer', () => {
     expect(sendTrackingSignal).toHaveBeenCalledTimes(1)
     expect(sendTrackingSignal).toHaveBeenCalledWith({
       signal: {
-        name: SEND_FEEDBACK_SIGNAL_NAME, feedback: {
+        name: SEND_FEEDBACK_SIGNAL_NAME,
+        feedback: {
           positive: true,
           numCharacters: comment.length,
           contactMail: true
