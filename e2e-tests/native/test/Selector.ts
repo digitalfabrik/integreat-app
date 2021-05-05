28d85dbe6d72565a8bb8f705fc05e1ec084a8d95
Sarah Sporck <sporck@integreat-app.de>
2021-05-05 14:38:51 +0200

    IGAPP-493: prettier

--- e2e-tests/native/test/Selector.ts
+++ e2e-tests/native/test/Selector.ts
@@ -1,21 +1,20 @@
-
 export class Selector {
-    private queries: Array<string> = new Array<string>()
+  private queries: Array<string> = new Array<string>()
 
-    public ByText(text: string): Selector {
-        if (driver.isAndroid) {
-            this.queries.push(`.text("${text}")`)
-        } else {
-            this.queries.push(`label LIKE '${text}'`)
-        }
-        return this
+  public ByText(text: string): Selector {
+    if (driver.isAndroid) {
+      this.queries.push(`.text("${text}")`)
+    } else {
+      this.queries.push(`label LIKE '${text}'`)
     }
+    return this
+  }
 
-    public build(): string {
-        if (driver.isAndroid) {
-            return `android=new UiSelector()${this.queries.join('')}`
-        } else {
-            return `-ios predicate string:${this.queries.join(' AND ')}`
-        }
+  public build(): string {
+    if (driver.isAndroid) {
+      return `android=new UiSelector()${this.queries.join('')}`
+    } else {
+      return `-ios predicate string:${this.queries.join(' AND ')}`
     }
+  }
 }
--- e2e-tests/native/test/pageobjects/landing.page.ts
+++ e2e-tests/native/test/pageobjects/landing.page.ts
@@ -1,22 +1,22 @@
-import {Page} from "./page";
-import {Selector} from "../Selector";
+import { Page } from './page'
+import { Selector } from '../Selector'
 
-class LandingPage extends Page{
-    constructor() {
-        super('Landing-Page');
-    }
+class LandingPage extends Page {
+  constructor() {
+    super('Landing-Page')
+  }
 
-    get cities() {
-        return $$('~City-Entry')
-    }
+  get cities() {
+    return $$('~City-Entry')
+  }
 
-    get search() {
-        return $('~Search-Input')
-    }
+  get search() {
+    return $('~Search-Input')
+  }
 
-    async city(name: string) {
-        return $(new Selector().ByText(name).build())
-    }
+  async city(name: string) {
+    return $(new Selector().ByText(name).build())
+  }
 }
 
 export default new LandingPage()
