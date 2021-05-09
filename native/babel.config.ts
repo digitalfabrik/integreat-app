e997997409cc40e07b36f5a32bac7c5e8a9e1bc9
Sarah Sporck <sporck@integreat-app.de>
2021-05-09 14:01:35 +0200

    IGAPP-595: Rename files to ts (native)

---
+++ native/babel.config.ts
@@ -0,0 +1,27 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset', '@babel/preset-flow'],
+  env: {
+    development: {
+      plugins: []
+    },
+    production: {
+      plugins: ['transform-remove-console']
+    }
+  },
+  // prevent babel error - only windows (using backslash)
+  exclude: [/node_modules\\decamelize.*/],
+  plugins: [
+    [
+      'transform-inline-environment-variables',
+      {
+        include: ['BUILD_CONFIG_NAME']
+      }
+    ],
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.json']
+      }
+    ]
+  ]
+}
