f3001cb3d783040b893bce770474ec7280ae0073
Sarah Sporck <sporck@integreat-app.de>
2021-05-09 12:58:59 +0200

    IGAPP-595: Rename files to ts (native)

--- native/src/index.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import * as React from 'react'
-import { AppRegistry } from 'react-native'
-import App from './App'
-// import App from './modules/app/components/App'
-// import 'moment/min/locales'
-
-AppRegistry.registerComponent('Integreat', () => () => <App />)
