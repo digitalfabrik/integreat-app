92819e2f7b06d33594b0e6d453a6ce3b42fac07c
Steffen Kleinle <klinzo@yahoo.de>
2021-05-09 12:54:44 +0200

    IGAPP-595: Fix ts errors in translations

--- translations/src/utils/object.ts
+++ translations/src/utils/object.ts
@@ -1,9 +1,6 @@
-import { $ReadOnly } from 'utility-types'
-export const values = <T>(object: $ReadOnly<Record<string, T>>): Array<T> => {
-  // $FlowFixMe
+export const values = object => {
   return Object.values(object)
 }
-export const entries = <T>(object: $ReadOnly<Record<string, T>>): Array<[string, T]> => {
-  // $FlowFixMe
+export const entries = object => {
   return Object.entries(object)
 }
