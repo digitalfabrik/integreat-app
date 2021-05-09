8258dbcfd63d79f4c115e7b379a5df7047e17d92
Sarah Sporck <sporck@integreat-app.de>
2021-05-09 14:01:35 +0200

    IGAPP-595: Rename files to ts (native)

---
+++ native/metro.config.ts
@@ -0,0 +1,33 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path')
+
+module.exports = {
+  // https://github.com/facebook/react-native/issues/21310#issuecomment-544071895
+  // https://github.com/facebook/metro/issues/1#issuecomment-453450709
+  // Per default metro is looking for the babel dependencies in the node_modules of the corresponding package.
+  // This leads to problems in the build-configs module as the babel dependencies are only in the native .
+  resolver: {
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => path.resolve(__dirname, `node_modules/${name}`)
+      }
+    )
+  },
+  watchFolders: [path.resolve(__dirname, '../')],
+  transformer: {
+    assetPlugins: ['react-native-svg-asset-plugin'],
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false
+      }
+    })
+  }
+}
---
+++ native/src/index.ts
@@ -0,0 +1,9 @@
+// @flow
+
+import * as React from 'react'
+import { AppRegistry } from 'react-native'
+import App from './App'
+// import App from './modules/app/components/App'
+// import 'moment/min/locales'
+
+AppRegistry.registerComponent('Integreat', () => () => <App />)
