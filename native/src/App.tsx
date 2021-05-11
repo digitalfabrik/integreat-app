ee9bc4f96487dfb13cd71d7d213454310589228c
Steffen Kleinle <klinzo@yahoo.de>
2021-05-11 21:48:43 +0200

    IGAPP-595: Remove native test app

--- native/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-import hello from 'build-configs'
-
-export default () => {
-  console.log(hello)
-  return <Text>Hello World!{hello}</Text>
-}
