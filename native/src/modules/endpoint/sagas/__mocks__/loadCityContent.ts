df5de19543825a3e089a78a374b15f37af2a3a87
Sarah Sporck <sporck@integreat-app.de>
2021-05-11 13:01:37 +0200

    IGAPP-595: fixes saga mocks (native)

--- native/src/modules/endpoint/sagas/__mocks__/loadCityContent.ts
+++ native/src/modules/endpoint/sagas/__mocks__/loadCityContent.ts
@@ -1,22 +1,23 @@
 import { Saga } from 'redux-saga'
-import { call } from 'redux-saga/effects'
+import { call, StrictEffect } from 'redux-saga/effects'
 import { DataContainer } from '../../DataContainer'
 import { ContentLoadCriterion } from '../../ContentLoadCriterion'
+import { LanguageModel } from 'api-client/dist/src'
 
 const loadCityContent = function* loadCityContent(
   dataContainer: DataContainer,
   newCity: string,
   newLanguage: string,
   criterion: ContentLoadCriterion
-): Saga<boolean> {
-  const languagesAvailable = yield call(() => dataContainer.languagesAvailable(newCity))
+): Generator<StrictEffect, boolean, Array<LanguageModel> | boolean> {
+  const languagesAvailable = (yield call(() => dataContainer.languagesAvailable(newCity))) as boolean
 
   if (!languagesAvailable) {
     console.error('You have to prepare the data container properly in order to use the loadCityContent.js mock!')
   }
 
   if (criterion.shouldLoadLanguages()) {
-    const languages = yield call(() => dataContainer.getLanguages(newCity))
+    const languages = (yield call(() => dataContainer.getLanguages(newCity))) as Array<LanguageModel>
 
     if (!languages.map(language => language.code).includes(newLanguage)) {
       return false
