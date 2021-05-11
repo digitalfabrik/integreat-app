53d06d076fe10c59a9805921032926d9ee71d25c
steffi <metzger@integreat-app.de>
2021-05-11 11:47:33 +0200

    IGAPP-595: watchClearCity

--- native/src/modules/settings/AppSettings.ts
+++ native/src/modules/settings/AppSettings.ts
@@ -2,6 +2,7 @@ import AsyncStorage from '@react-native-community/async-storage'
 import { mapValues, toPairs } from 'lodash/object'
 import { fromPairs } from 'lodash/array'
 import { SignalType } from 'api-client'
+
 export type SettingsType = {
   storageVersion: string | null
   contentLanguage: string | null
@@ -48,58 +49,70 @@ class AppSettings {
       return parsed
     })
   }
+
   setSettings = async (settings: Partial<SettingsType>) => {
     const settingsArray = toPairs(mapValues(settings, value => JSON.stringify(value)))
     await this.asyncStorage.multiSet(settingsArray)
   }
+
   setVersion = async (version: string) => {
     await this.setSettings({
       storageVersion: version
     })
   }
+
   loadVersion = async (): Promise<string | null> => {
     const settings = await this.loadSettings()
     return settings.storageVersion
   }
+
   setContentLanguage = async (language: string) => {
     await this.setSettings({
       contentLanguage: language
     })
   }
+
   loadContentLanguage = async (): Promise<string | null> => {
     const settings = await this.loadSettings()
     return settings.contentLanguage
   }
+
   setSelectedCity = async (city: string) => {
     await this.setSettings({
       selectedCity: city
     })
   }
+
   clearSelectedCity = async () => {
     await this.setSettings({
       selectedCity: null
     })
   }
+
   setJpalTrackingCode = async (jpalTrackingCode: string) => {
     await this.setSettings({
       jpalTrackingCode
     })
   }
+
   clearJpalTrackingCode = async () => {
     await this.setSettings({
       jpalTrackingCode: null
     })
   }
+
   setJpalTrackingEnabled = async (jpalTrackingEnabled: boolean) => {
     await this.setSettings({
       jpalTrackingEnabled
     })
   }
+
   clearJpalTrackingEnabled = async () => {
     await this.setSettings({
       jpalTrackingEnabled: false
     })
   }
+
   pushJpalSignal = async (signal: SignalType) => {
     const { jpalSignals } = await this.loadSettings()
     jpalSignals.push(signal)
@@ -107,6 +120,7 @@ class AppSettings {
       jpalSignals
     })
   }
+
   clearJpalSignals = async (): Promise<Array<SignalType>> => {
     const { jpalSignals } = await this.loadSettings()
     await this.setSettings({
@@ -114,28 +128,34 @@ class AppSettings {
     })
     return jpalSignals
   }
+
   loadSelectedCity = async (): Promise<string | null | undefined> => {
     const settings = await this.loadSettings()
     return settings.selectedCity
   }
+
   setIntroShown = async () => {
     await this.setSettings({
       introShown: true
     })
   }
+
   loadIntroShown = async (): Promise<boolean | null> => {
     const settings = await this.loadSettings()
     return settings.introShown
   }
+
   setApiUrlOverride = async (apiUrlOverride: string) => {
     await this.setSettings({
       apiUrlOverride
     })
   }
+
   loadApiUrlOverride = async (): Promise<string | null | undefined> => {
     const settings = await this.loadSettings()
     return settings.apiUrlOverride
   }
+
   clearAppSettings = async () => {
     const settingsKeys = Object.keys(defaultSettings)
     await this.asyncStorage.multiRemove(settingsKeys)
