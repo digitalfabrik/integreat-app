c2905b4587fc8c9dc6c9d7b47945b1e250050eb3
Steffen Kleinle <klinzo@yahoo.de>
2021-05-12 09:38:20 +0200

    IGAPP-595: Fix linting errors

--- build-configs/aschaffenburg/theme/colors.ts
+++ build-configs/aschaffenburg/theme/colors.ts
@@ -1,10 +1,12 @@
-import { commonLightColors, commonDarkColors } from '../../common/theme/colors'
-import { ColorsType } from '../../common/theme/colors'
+import { ColorsType, commonLightColors, commonDarkColors } from '../../common/theme/colors'
+
 const themeColor = '#009684'
+
 export const darkColors: ColorsType = {
   themeColor,
   ...commonDarkColors
 }
+
 export const lightColors: ColorsType = {
   themeColor,
   ...commonLightColors
