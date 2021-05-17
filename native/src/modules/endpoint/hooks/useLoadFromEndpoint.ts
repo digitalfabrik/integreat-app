6b713c015b67d42dcebbc8416f9bc02541761d91
Steffen Kleinle <klinzo@yahoo.de>
2021-05-17 17:41:36 +0200

    IGAPP-605: Refactoring

--- native/src/modules/endpoint/hooks/useLoadFromEndpoint.ts
+++ native/src/modules/endpoint/hooks/useLoadFromEndpoint.ts
@@ -1,13 +1,15 @@
 import determineApiUrl from '../determineApiUrl'
 import { Payload } from 'api-client'
 import { useCallback, useEffect, useState } from 'react'
+
 type RequestType<T> = (apiUrl: string) => Promise<Payload<T>>
+
 export const loadFromEndpoint = async <T>(
   request: RequestType<T>,
-  setData: (arg0: T | null | undefined) => void,
-  setError: (arg0: Error | null | undefined) => void,
-  setLoading: (arg0: boolean) => void
-) => {
+  setData: (data: T | null) => void,
+  setError: (error: Error | null) => void,
+  setLoading: (loading: boolean) => void
+): Promise<void> => {
   setLoading(true)
 
   try {
@@ -18,7 +20,7 @@ export const loadFromEndpoint = async <T>(
       setError(payload.error)
       setData(null)
     } else {
-      setData(payload.data)
+      setData(payload.data || null)
       setError(null)
     }
   } catch (e) {
@@ -28,22 +30,27 @@ export const loadFromEndpoint = async <T>(
     setLoading(false)
   }
 }
+
 type ReturnType<T> = {
-  data: T | null | undefined
-  error: Error | null | undefined
+  data: T | null
+  error: Error | null
   loading: boolean
   refresh: () => void
 }
+
 export const useLoadFromEndpoint = <T>(request: RequestType<T>): ReturnType<T> => {
-  const [data, setData] = useState<T | null | undefined>(null)
-  const [error, setError] = useState<Error | null | undefined>(null)
+  const [data, setData] = useState<T | null>(null)
+  const [error, setError] = useState<Error | null>(null)
   const [loading, setLoading] = useState<boolean>(false)
+
   const load = useCallback(() => {
     loadFromEndpoint<T>(request, setData, setError, setLoading).catch(e => setError(e))
   }, [request, setData, setError, setLoading])
+
   useEffect(() => {
     load()
   }, [load])
+
   return {
     data,
     error,
@@ -51,4 +58,5 @@ export const useLoadFromEndpoint = <T>(request: RequestType<T>): ReturnType<T> =
     refresh: load
   }
 }
+
 export default useLoadFromEndpoint
