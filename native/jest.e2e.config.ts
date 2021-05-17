3bfe949aa9c376db5d36dc067685b001b063d8d7
Steffen Kleinle <klinzo@yahoo.de>
2021-05-17 12:18:33 +0200

    IGAPP-595: Fix e2e test config

--- native/jest.e2e.config.ts
+++ native/jest.e2e.config.ts
@@ -1,3 +1,5 @@
+import { defaults as tsjPreset } from 'ts-jest/presets'
+
 export default {
   rootDir: '.',
   verbose: true,
@@ -5,5 +7,10 @@ export default {
   setupFiles: [],
   setupFilesAfterEnv: ['<rootDir>/e2e/config/jest.e2e.setup-framework.ts'],
   testMatch: ['**/e2e/**/*.e2e.[jt]s?(x)'],
-  testPathIgnorePatterns: ['/node_modules/', '/src/']
+  testPathIgnorePatterns: ['/node_modules/', '/src/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transform: {
+    ...tsjPreset.transform,
+    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js'
+  }
 }
