317620595728866949942888f4ecbea3e36c834d
Steffen Kleinle <klinzo@yahoo.de>
2021-05-18 12:22:08 +0200

    IGAPP-605: Fix web-ts ts config and generalize

--- api-client/src/stringifyFormData.ts
+++ api-client/src/stringifyFormData.ts
@@ -1,14 +1,14 @@
 // stringifies FormData from react-native or the browser variant of it
 export function stringifyFormData(formData: any): string {
-  const entries = {}
+  const entries: Record<string, any> = {}
 
   if (formData?.getParts) {
     for (const part of formData.getParts()) {
-      entries[part.fieldName] = part.string
+      entries[part.fieldName as string] = part.string
     }
   } else {
     for (const [key, value] of formData.entries()) {
-      entries[key] = value
+      entries[key as string] = value
     }
   }
 
