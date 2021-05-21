c7900c7efd52c3cc4c76e51e663b2f7f312806a1
Sarah Sporck <sporck@integreat-app.de>
2021-05-21 15:54:13 +0200

    IGAPP-661: formatting and more specific types

--- web-ts/src/__mocks__/redux-first-router-link.tsx
+++ web-ts/src/__mocks__/redux-first-router-link.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import * as React from 'react'
 
-const Link = (props: any) => <div {...props}>NoLinkHere...</div>;
+const Link = (props: React.HTMLAttributes<HTMLDivElement>) => <div {...props}>NoLinkHere...</div>
 
-Link.displayName = 'Connect(Link)';
-export default Link;
+Link.displayName = 'Connect(Link)'
+export default Link
