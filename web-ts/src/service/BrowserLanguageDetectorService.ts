8e15269ab10079633ddfdbe77f5b3b371792f9c5
Sarah Sporck <sporck@integreat-app.de>
2021-05-21 14:55:22 +0200

    IGAPP-643: typing of redux action, jest mock & formatting

--- web-ts/src/service/BrowserLanguageDetectorService.ts
+++ web-ts/src/service/BrowserLanguageDetectorService.ts
@@ -1,39 +1,44 @@
-let hasLocalStorageSupport = null;
+import {LanguageDetectorModule } from 'i18next'
+
+let hasLocalStorageSupport: boolean | null = null
 
 // Adapted from:
 // https://github.com/i18next/i18next-browser-languageDetector/blob/90284ca924353de0e6991bc51a0453f90fac3a04/src/browserLookups/localStorage.js
 const localStorageAvailable = () => {
   if (hasLocalStorageSupport !== null) {
-    return hasLocalStorageSupport;
+    return hasLocalStorageSupport
   }
 
   try {
-    const localStorage = window.localStorage;
-    hasLocalStorageSupport = window !== 'undefined' && localStorage !== null;
-    const testKey = 'i18next.translate.boo';
-    localStorage.setItem(testKey, 'foo');
-    localStorage.removeItem(testKey);
+    const localStorage = window.localStorage
+    hasLocalStorageSupport = window !== 'undefined' && localStorage !== null
+    const testKey = 'i18next.translate.boo'
+    localStorage.setItem(testKey, 'foo')
+    localStorage.removeItem(testKey)
   } catch (e) {
-    hasLocalStorageSupport = false;
+    hasLocalStorageSupport = false
   }
 
-  return hasLocalStorageSupport;
-};
+  return hasLocalStorageSupport
+}
 
-const LANGUAGE_LOCAL_STORAGE = 'i18nextLng';
-export default {
+interface NavigatorLanguage {
+  userLanguage?: string
+}
+
+const LANGUAGE_LOCAL_STORAGE = 'i18nextLng'
+const languageDetector: LanguageDetectorModule = {
   type: 'languageDetector',
-  async: false,
   init: (services, detectorOptions, i18nextOptions) => {},
   // Returns array of ISO-639-2 or ISO-639-3 language codes
-  detect: () => {
-    const bcp47Tags = [];
+  detect: (): string | undefined => {
+    const bcp47Tags: string[] = []
 
     if (localStorageAvailable()) {
-      const localStorageLanguage = window.localStorage.getItem(LANGUAGE_LOCAL_STORAGE);
+      const localStorageLanguage = window.localStorage.getItem(LANGUAGE_LOCAL_STORAGE)
 
       if (localStorageLanguage) {
-        bcp47Tags.push(localStorageLanguage);
+        bcp47Tags.push(localStorageLanguage)
       }
     }
 
@@ -43,24 +48,28 @@ export default {
       if (navigator.languages) {
         // chrome only; not an array, so can't use .push.apply instead of iterating
         for (let i = 0; i < navigator.languages.length; i++) {
-          bcp47Tags.push(navigator.languages[i]);
+          bcp47Tags.push(navigator.languages[i])
         }
       }
 
-      if (navigator.userLanguage) {
-        bcp47Tags.push(navigator.userLanguage);
+      // IE only
+      const userLanguage = (navigator as { userLanguage?: string }).userLanguage
+      if (userLanguage) {
+        bcp47Tags.push(userLanguage)
       }
 
       if (navigator.language) {
-        bcp47Tags.push(navigator.language);
+        bcp47Tags.push(navigator.language)
       }
     }
 
-    return bcp47Tags.length > 0 ? bcp47Tags : undefined;
+    return bcp47Tags.length > 0 ? bcp47Tags : undefined
   },
-  cacheUserLanguage: language => {
+  cacheUserLanguage: (language: string) => {
     if (localStorageAvailable()) {
-      window.localStorage.setItem(LANGUAGE_LOCAL_STORAGE, language);
+      window.localStorage.setItem(LANGUAGE_LOCAL_STORAGE, language)
     }
   }
-};
+}
+
+export default languageDetector
