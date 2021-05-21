26b8cf54ea352d8da4b9a5a911daa17d0b019f1b
Sarah Sporck <sporck@integreat-app.de>
2021-05-21 12:26:51 +0200

    IGAPP-643: convert files to ts

--- web-ts/src/redux/actions/setUIDirection.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-import { createAction } from 'redux-actions'
-
-export const setUiDirectionAction = 'SET_UI_DIRECTION'
-
-export type SetUiDirectionActionType = { type: 'SET_UI_DIRECTION', payload: 'ltr' | 'rtl' }
-
-export default (direction: 'ltr' | 'rtl'): SetUiDirectionActionType => createAction('SET_UI_DIRECTION')(direction)
--- web-ts/src/service/__mocks__/BrowserLanguageDetectorService.ts
+++ web-ts/src/service/__mocks__/BrowserLanguageDetectorService.ts
@@ -1,24 +1,22 @@
-// @flow
-
-const settings: {|
-  language?: string
-|} = {
+const settings: {
+  language?: string;
+} = {
   language: undefined
-}
-
+};
 export default {
   type: 'languageDetector',
   async: false,
   init: () => {},
   detect: jest.fn<[], string[]>(() => {
-    const found: string[] = []
+    const found: string[] = [];
+
     if (settings.language) {
-      found.push(settings.language)
+      found.push(settings.language);
     }
 
-    return [...found, 'en']
+    return [...found, 'en'];
   }),
   cacheUserLanguage: (language: string) => {
-    settings.language = language
+    settings.language = language;
   }
-}
+};
