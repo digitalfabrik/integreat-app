fde3a6c32b9c57e373c99b7705ca0844eb773bd6
Maximilian Ammann <max@maxammann.org>
2021-05-22 16:24:41 +0200

    IGAPP-644: Remove darkmode

--- web/src/modules/theme/reducers/index.ts
+++
@@ -1,7 +0,0 @@
-import type { ReduxReducer } from "redux-actions";
-import { handleAction } from "redux-actions";
-const toggleDarkModeReducer: ReduxReducer<boolean, {
-  type: "TOGGLE_DARK_MODE";
-  payload: boolean;
-}> = handleAction('TOGGLE_DARK_MODE', (state: boolean) => !state, false);
-export default toggleDarkModeReducer;
