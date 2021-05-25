3f0d69ee4c196b244c2bd44d9b084434f2e7d4ce
Sarah Sporck <sporck@integreat-app.de>
2021-05-25 10:44:05 +0200

    IGAPP-661: used wrapper compoonent for router in tests & convert enzyme test to tessting-library test

--- web-ts/src/__mocks__/react-i18next.tsx
+++ web-ts/src/__mocks__/react-i18next.tsx
@@ -1,20 +1,18 @@
-import * as React from "react";
-import i18next from "i18next";
-import wrapDisplayName from "../modules/common/utils/wrapDisplayName";
+import * as React from 'react'
+import i18next from 'i18next'
+import wrapDisplayName from '../services/wrapDisplayName'
 
 // this mock makes sure any components using the translate HoC receive the t function as a prop
 const withTranslation = namespace => Component => {
-  const i18n = i18next.createInstance();
+  const i18n = i18next.createInstance()
 
-  const Translated = props => <Component t={key => `${namespace}:${key}`} i18n={i18n} {...props} />;
+  const Translated = props => <Component t={key => `${namespace}:${key}`} i18n={i18n} {...props} />
 
-  Translated.displayName = wrapDisplayName(Component, 'Translate');
-  return Translated;
-};
+  Translated.displayName = wrapDisplayName(Component, 'Translate')
+  return Translated
+}
 
-const I18nextProvider = ({
-  children
-}) => children;
+const I18nextProvider = ({ children }) => children
 
-I18nextProvider.displayName = 'I18nextProvider';
-export { I18nextProvider, withTranslation };
+I18nextProvider.displayName = 'I18nextProvider'
+export { I18nextProvider, withTranslation }
