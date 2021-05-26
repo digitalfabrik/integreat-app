ac115629946d5835c27b551cad84fe2130bfea6c
Steffen Kleinle <klinzo@yahoo.de>
2021-05-26 14:23:58 +0200

    IGAPP-643: Fix ts errors

--- web-ts/src/provider/I18nProvider.tsx
+++ web-ts/src/provider/I18nProvider.tsx
@@ -1,28 +1,28 @@
 import i18next, { i18n } from 'i18next'
-import * as React from 'react'
-import { useEffect, useMemo, useState } from 'react'
+import React, { ReactNode, useEffect, useMemo, useState } from 'react'
 import { I18nextProvider } from 'react-i18next'
 import { Helmet as ReactHelmet } from 'react-helmet'
 import { loadTranslations, config } from 'translations'
-import { useDispatch, useSelector } from 'react-redux'
-import type { StateType } from '../../app/StateType'
-import DateFormatter from 'api-client/src/i18n/DateFormatter'
+import { useDispatch } from 'react-redux'
+import { DateFormatter } from 'api-client'
 import setUIDirection from '../redux/actions/setUIDirection'
 import DateFormatterContext from '../context/DateFormatterContext'
 import buildConfig from '../constants/buildConfig'
 import BrowserLanguageDetectorService from '../service/BrowserLanguageDetectorService'
 
 type PropsType = {
-  children: React.ReactNode;
-};
-export default (({
-                   children
-                 }: PropsType) => {
+  children: ReactNode
+}
+
+const I18nProvider = ({ children }: PropsType) => {
   const [language, setLanguage] = useState<string>(config.defaultFallback)
   const [errorMessage, setErrorMessage] = useState<string | null | undefined>(null)
   const [i18nextInstance, setI18nextInstance] = useState<i18n | null>(null)
   const dispatch = useDispatch()
-  const contentLanguage: string | null | undefined = useSelector((state: StateType) => state.location.payload.language)
+  // TODO Use right content language
+  // const contentLanguage: string | null | undefined = useSelector((state: StateType) => state.location.payload.language)
+  const contentLanguage: string | null | undefined = 'de'
+
   useEffect(() => {
     const initI18Next = async () => {
       const resources = loadTranslations(buildConfig().translationsOverride)
@@ -42,7 +42,6 @@ export default (({
           escapeValue: false
           /* Escaping is not needed for react apps:
                https://github.com/i18next/react-i18next/issues/277 */
-
         },
         debug: buildConfig().featureFlags.developerFriendly
       })
@@ -80,30 +79,37 @@ export default (({
       dispatch(setUIDirection(config.hasRTLScript(language) ? 'rtl' : 'ltr'))
     }
   }, [dispatch, language])
+
   const additionalFont = config.getAdditionalFont(language)
   const dateFormatter = useMemo(() => new DateFormatter(config.defaultFallback), [])
 
   if (errorMessage) {
-    return errorMessage
+    return <>{errorMessage}</>
   }
 
   if (!i18nextInstance) {
-    return null
+    return <></>
   }
 
-  return <I18nextProvider i18n={i18nextInstance}>
-    <div data-testid={'direction'}
-         dir={config.isSupportedLanguage(language) ? config.hasRTLScript(language) ? 'rtl' : 'ltr' : undefined}>
-      <ReactHelmet>
-        {additionalFont === 'lateef' && <link href='/fonts/lateef/lateef.css' rel='stylesheet' />}
-        {additionalFont === 'noto-sans-sc' && <link href='/fonts/noto-sans-sc/noto-sans-sc.css' rel='stylesheet' />}
-        {additionalFont === 'noto-sans-georgian' &&
-        <link href='/fonts/noto-sans-georgian/noto-sans-georgian.css' rel='stylesheet' />}
-        <link href='/fonts/open-sans/open-sans.css' rel='stylesheet' />
-        <link href='/fonts/raleway/raleway.css' rel='stylesheet' />
-        <link href='/fonts/varela-round/varela-round.css' rel='stylesheet' />
-      </ReactHelmet>
-      <DateFormatterContext.Provider value={dateFormatter}>{children}</DateFormatterContext.Provider>
-    </div>
-  </I18nextProvider>
-})
+  return (
+    <I18nextProvider i18n={i18nextInstance}>
+      <div
+        data-testid={'direction'}
+        dir={config.isSupportedLanguage(language) ? (config.hasRTLScript(language) ? 'rtl' : 'ltr') : undefined}>
+        <ReactHelmet>
+          {additionalFont === 'lateef' && <link href='/fonts/lateef/lateef.css' rel='stylesheet' />}
+          {additionalFont === 'noto-sans-sc' && <link href='/fonts/noto-sans-sc/noto-sans-sc.css' rel='stylesheet' />}
+          {additionalFont === 'noto-sans-georgian' && (
+            <link href='/fonts/noto-sans-georgian/noto-sans-georgian.css' rel='stylesheet' />
+          )}
+          <link href='/fonts/open-sans/open-sans.css' rel='stylesheet' />
+          <link href='/fonts/raleway/raleway.css' rel='stylesheet' />
+          <link href='/fonts/varela-round/varela-round.css' rel='stylesheet' />
+        </ReactHelmet>
+        <DateFormatterContext.Provider value={dateFormatter}>{children}</DateFormatterContext.Provider>
+      </div>
+    </I18nextProvider>
+  )
+}
+
+export default I18nProvider
