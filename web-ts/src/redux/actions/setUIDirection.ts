258c04c6dbb5543c84c7baf8a0471ef5c881978a
Steffen Kleinle <klinzo@yahoo.de>
2021-05-26 15:17:09 +0200

    IGAPP-643: Remove redux stuff

--- web-ts/src/redux/actions/setUIDirection.ts
+++
@@ -1,9 +0,0 @@
-import { createAction } from 'redux-actions'
-
-type UIDirectionType = 'ltr' | 'rtl'
-export type SetUiDirectionActionType = {
-  type: string
-  payload: UIDirectionType
-}
-export default (direction: UIDirectionType): SetUiDirectionActionType =>
-  createAction<UIDirectionType>('SET_UI_DIRECTION')(direction)
