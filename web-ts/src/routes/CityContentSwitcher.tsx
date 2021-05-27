c61b7d5f5b81e2908d2f59737ac8019d56801610
Steffen Kleinle <klinzo@yahoo.de>
2021-05-27 17:35:02 +0200

    IGAPP-645: Use layout for city content routes

--- web-ts/src/routes/CityContentSwitcher.tsx
+++ web-ts/src/routes/CityContentSwitcher.tsx
@@ -25,11 +25,16 @@ import { cmsApiBaseUrl } from '../constants/urls'
 import Layout from '../components/Layout'
 import FailureSwitcher from '../components/FailureSwitcher'
 import { LanguageFailure } from '../components/LanguageFailure'
+import GeneralHeader from '../components/GeneralHeader'
+import GeneralFooter from '../components/GeneralFooter'
+import LocationLayout from '../components/LocationLayout'
+import LoadingSpinner from '../components/LoadingSpinner'
 
 type PropsType = {
   cities: CityModel[]
 } & RouteComponentProps<{ cityCode: string; languageCode: string }>
 
+// TODO pass right props instead of constants
 const CityContentSwitcher = (props: PropsType): ReactElement => {
   const { cities, match, location } = props
   const { cityCode, languageCode } = match.params
@@ -43,23 +48,33 @@ const CityContentSwitcher = (props: PropsType): ReactElement => {
 
   if (!cityModel || !languageModel) {
     if (loading) {
-      return <>CityContentRouter loading!</>
+      return (
+        <Layout header={<GeneralHeader viewportSmall={false} />} footer={<GeneralFooter language={languageCode} />}>
+          <LoadingSpinner />
+        </Layout>
+      )
     }
 
     const error = !cityModel ? new Error('notFound.category') : languagesError
     if (error) {
-      return <Layout><FailureSwitcher error={error} /></Layout>
+      return (
+        <Layout header={<GeneralHeader viewportSmall={false} />} footer={<GeneralFooter language={languageCode} />}>
+          <FailureSwitcher error={error} />
+        </Layout>
+      )
     }
 
     return (
-      <Layout>
+      <Layout header={<GeneralHeader viewportSmall={false} />} footer={<GeneralFooter language={languageCode} />}>
         <LanguageFailure
           cities={cities}
           cityCode={cityCode}
           pathname={location.pathname}
           languageCode={languageCode}
-          // TODO
-          languageChangePaths={[]}
+          languageChangePaths={[
+            { code: 'de', name: 'Deutsch', path: '/' },
+            { code: 'fr', name: 'French', path: '/' }
+          ]}
           t={key => key}
         />
       </Layout>
@@ -67,16 +82,27 @@ const CityContentSwitcher = (props: PropsType): ReactElement => {
   }
 
   return (
-    <Switch>
-      <Route path={`/${EVENTS_ROUTE}/:eventId?`} exact component={EventsPage} />
-      <Route path={`/${OFFERS_ROUTE}/:offerId?`} exact component={OffersPage} />
-      <Route path={`/${POIS_ROUTE}/:locationId?`} exact component={PoisPage} />
-      <Route path={`/${NEWS_ROUTE}/${LOCAL_NEWS_TYPE}/:newsId?`} exact component={NewsPage} />
-      <Route path={`/${NEWS_ROUTE}/${TU_NEWS_TYPE}/:newsId?`} exact component={NewsPage} />
-      <Route path={`/${SEARCH_ROUTE}`} exact component={SearchPage} />
-      <Route path={`/${DISCLAIMER_ROUTE}`} exact component={DisclaimerPage} />
-      <Route path={`/:categoriesId*`} component={CategoriesPage} />
-    </Switch>
+    <LocationLayout
+      cities={cities}
+      categories={null}
+      viewportSmall={false}
+      feedbackTargetInformation={null}
+      languageChangePaths={null}
+      isLoading={false}
+      cityCode={cityCode}
+      languageCode={languageCode}
+      pathname={location.pathname}>
+      <Switch>
+        <Route exact path={`/${EVENTS_ROUTE}/:eventId?`} component={EventsPage} />
+        <Route exact path={`/${OFFERS_ROUTE}/:offerId?`} component={OffersPage} />
+        <Route exact path={`/${POIS_ROUTE}/:locationId?`} component={PoisPage} />
+        <Route exact path={`/${NEWS_ROUTE}/${LOCAL_NEWS_TYPE}/:newsId?`} component={NewsPage} />
+        <Route exact path={`/${NEWS_ROUTE}/${TU_NEWS_TYPE}/:newsId?`} component={NewsPage} />
+        <Route exact path={`/${SEARCH_ROUTE}`} component={SearchPage} />
+        <Route exact path={`/${DISCLAIMER_ROUTE}`} component={DisclaimerPage} />
+        <Route path={`/:categoriesId*`} component={CategoriesPage} />
+      </Switch>
+    </LocationLayout>
   )
 }
 
