d4ba65594c0f2ae29901e9f57a0b5906b3a6a2aa
Steffen Kleinle <klinzo@yahoo.de>
2021-05-27 15:49:30 +0200

    IGAPP-645: Refactor App and load cities

--- web-ts/src/routes/App.tsx
+++ web-ts/src/routes/App.tsx
@@ -1,44 +1,42 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React, { useCallback } from 'react'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
 import LandingPage from './landing/LandingPage'
-import OffersPage from './offers/OffersPage'
-import EventsPage from './events/EventsPage'
-import CategoriesPage from './categories/CategoriesPage'
-import PoisPage from './pois/PoisPage'
-import NewsPage from './news/NewsPage'
-import SearchPage from './search/SearchPage'
-import DisclaimerPage from './disclaimer/DisclaimerPage'
 import ErrorPage from './errors/ErrorPage'
-
-export const Routes = {
-  LANDING_ROUTE: 'landing',
-  CATEGORIES_ROUTE: '',
-  EVENTS_ROUTE: 'events',
-  OFFERS_ROUTE: 'offers',
-  POIS_ROUTE: 'locations',
-  LOCAL_NEWS_ROUTE: 'news/local',
-  TUNEWS_ROUTE: 'news/tu-news',
-  SEARCH_ROUTE: 'search',
-  DISCLAIMER_ROUTE: 'disclaimer',
-  NOT_FOUND_ROUTE: 'not-found'
-} as const
+import { useLoadFromEndpoint, LANDING_ROUTE, CityModel, createCitiesEndpoint } from 'api-client'
+import CityContentSwitcher from './CityContentSwitcher'
+import { cmsApiBaseUrl } from '../constants/urls'
+import { ThemeProvider } from 'styled-components'
+import buildConfig from '../constants/buildConfig'
 
 const App = () => {
+  const requestCities = useCallback(async () => createCitiesEndpoint(cmsApiBaseUrl).request(undefined), [])
+  const { data: cities, loading, error } = useLoadFromEndpoint<CityModel[]>(requestCities)
+  // TODO IGAPP-643
+  const detectedLanguage = 'de'
+
+  if (loading) {
+    return <>App loading!</>
+  }
+
+  if (error || !cities) {
+    return <>{error?.message ?? 'error'}</>
+  }
+
   return (
-    <Router>
-      <Switch>
-        <Route path={`/:language/${Routes.LANDING_ROUTE}`} exact component={LandingPage} />
-        <Route path={`/:city/:language/${Routes.EVENTS_ROUTE}/:eventId?`} exact component={EventsPage} />
-        <Route path={`/:city/:language/${Routes.OFFERS_ROUTE}/:offerId?`} exact component={OffersPage} />
-        <Route path={`/:city/:language/${Routes.POIS_ROUTE}/:locationId?`} exact component={PoisPage} />
-        <Route path={`/:city/:language/${Routes.LOCAL_NEWS_ROUTE}/:newsId?`} exact component={NewsPage} />
-        <Route path={`/:city/:language/${Routes.TUNEWS_ROUTE}/:newsId?`} exact component={NewsPage} />
-        <Route path={`/:city/:language/${Routes.SEARCH_ROUTE}`} exact component={SearchPage} />
-        <Route path={`/:city/:language/${Routes.DISCLAIMER_ROUTE}`} exact component={DisclaimerPage} />
-        <Route path={`/:city/:language/${Routes.CATEGORIES_ROUTE}:categoriesId?`} exact component={CategoriesPage} />
-        <Route path={`/`} component={ErrorPage} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={buildConfig().lightTheme}>
+      <Router>
+        <Switch>
+          <Redirect exact from={`/${LANDING_ROUTE}?`} to={`/${LANDING_ROUTE}/${detectedLanguage}`} />
+          <Route path={`/${LANDING_ROUTE}/:languageCode?`} exact component={LandingPage} />
+          <Redirect exact from={`/:cityCode`} to={`/:cityCode/${detectedLanguage}`} />
+          <Route
+            path={`/:cityCode/:languageCode`}
+            render={props => <CityContentSwitcher cities={cities} {...props} />}
+          />
+          <Route path={`/`} component={ErrorPage} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   )
 }
 
