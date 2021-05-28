68fbf1867341ba39faa2164814cf07faa392e330
Steffen Kleinle <klinzo@yahoo.de>
2021-05-28 13:17:04 +0200

    IGAPP-645: Use I18nProvider and fix language handling

--- web-ts/src/routes/App.tsx
+++ web-ts/src/routes/App.tsx
@@ -1,15 +1,20 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import buildConfig from '../constants/buildConfig'
 import RootSwitcher from './RootSwitcher'
+import I18nProvider from '../components/I18nProvider'
 
 const App = (): ReactElement => {
+  const [contentLanguage, setContentLanguage] = useState<string>()
+
   return (
     <ThemeProvider theme={buildConfig().lightTheme}>
-      <Router>
-        <RootSwitcher />
-      </Router>
+      <I18nProvider contentLanguage={contentLanguage}>
+        <Router>
+          <RootSwitcher setContentLanguage={setContentLanguage} />
+        </Router>
+      </I18nProvider>
     </ThemeProvider>
   )
 }
--- web-ts/src/routes/RootSwitcher.tsx
+++ web-ts/src/routes/RootSwitcher.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useCallback } from 'react'
-import { Redirect, Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch, useLocation, useParams, useRouteMatch } from 'react-router-dom'
 import LandingPage from './landing/LandingPage'
 import NotFoundPage from './not-found/NotFoundPage'
 import {
@@ -18,24 +18,33 @@ import GeneralFooter from '../components/GeneralFooter'
 import FailureSwitcher from '../components/FailureSwitcher'
 import MainDisclaimerPage from './main-disclaimer/MainDisclaimerPage'
 import LoadingSpinner from '../components/LoadingSpinner'
+import { useTranslation } from 'react-i18next'
 
-const RootSwitcher = (): ReactElement => {
+type PropsType = {
+  setContentLanguage: (languageCode: string) => void
+}
+
+const RootSwitcher = ({ setContentLanguage }: PropsType): ReactElement => {
   const requestCities = useCallback(async () => createCitiesEndpoint(cmsApiBaseUrl).request(undefined), [])
   const { data: cities, loading, error } = useLoadFromEndpoint<CityModel[]>(requestCities)
-  // TODO IGAPP-643
-  const detectedLanguage = 'de'
+  const { i18n } = useTranslation()
+  // LanguageCode is always the second param (if there is one)
+  const languageCode = useRouteMatch<{ languageCode?: string }>('/:slug/:languageCode')?.params.languageCode
+
+  const detectedLanguageCode = i18n.language
+  const language = languageCode ?? detectedLanguageCode
+
+  if (language !== detectedLanguageCode) {
+    setContentLanguage(language)
+  }
 
   if (loading) {
-    return (
-      <Layout header={<GeneralHeader viewportSmall={false} />} footer={<GeneralFooter language={detectedLanguage} />}>
-        <LoadingSpinner />
-      </Layout>
-    )
+    return <Layout><LoadingSpinner /></Layout>
   }
 
   if (!cities || error) {
     return (
-      <Layout header={<GeneralHeader viewportSmall={false} />} footer={<GeneralFooter language={detectedLanguage} />}>
+      <Layout header={<GeneralHeader viewportSmall={false} />} footer={<GeneralFooter language={language} />}>
         <FailureSwitcher error={error ?? new Error('Cities not available')} />
       </Layout>
     )
@@ -48,9 +57,9 @@ const RootSwitcher = (): ReactElement => {
       <Route exact path={`/${NOT_FOUND_ROUTE}`} component={NotFoundPage} />
       <Route path={`/:cityCode/:languageCode`} render={props => <CityContentSwitcher cities={cities} {...props} />} />
 
-      <Redirect exact from='/' to={`/${LANDING_ROUTE}/${detectedLanguage}`} />
-      <Redirect exact from={`/${LANDING_ROUTE}?`} to={`/${LANDING_ROUTE}/${detectedLanguage}`} />
-      <Redirect exact from={`/:cityCode`} to={`/:cityCode/${detectedLanguage}`} />
+      <Redirect exact from='/' to={`/${LANDING_ROUTE}/${language}`} />
+      <Redirect exact from={`/${LANDING_ROUTE}?`} to={`/${LANDING_ROUTE}/${language}`} />
+      <Redirect exact from={`/:cityCode`} to={`/:cityCode/${language}`} />
       {/* TODO redirects for aschaffenburg */}
     </Switch>
   )
