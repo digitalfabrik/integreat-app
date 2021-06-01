8bb64c74b7cc722320d17b6a93b878928e0ea8cc
Sarah Sporck <sporck@integreat-app.de>
2021-06-01 11:44:01 +0200

    IGAPP-646: move Platform to PlatformContext & update snapshot

--- web-ts/src/contexts/PlatformContext.ts
+++ web-ts/src/contexts/PlatformContext.ts
@@ -1,4 +1,26 @@
 import React from 'react'
-import Platform from './Platform'
+import { detect } from 'detect-browser'
+
+type BrowserType = {
+  name: string
+  version: string | null
+}
+
+export class Platform {
+  _browser: BrowserType | null
+
+  constructor() {
+    this._browser = detect()
+  }
+
+  get positionStickyDisabled(): boolean {
+    return !!(
+      this._browser &&
+      this._browser.version &&
+      this._browser.name === 'edge' &&
+      /^16\..*/.test(this._browser.version)
+    )
+  }
+}
 
 export default React.createContext<Platform>(new Platform())
