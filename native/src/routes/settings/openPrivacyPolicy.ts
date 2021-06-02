45cecc2b515b62627e54c9cbf2a1756ee4ffa519
Steffen Kleinle <klinzo@yahoo.de>
2021-06-02 17:17:22 +0200

    IGAPP-669: Flatten routes folder and move components and services

--- native/src/routes/settings/openPrivacyPolicy.ts
+++
@@ -1,9 +0,0 @@
-import buildConfig from '../../constants/buildConfig'
-import openExternalUrl from '../../services/openExternalUrl'
-
-const openPrivacyPolicy = (language: string) => {
-  const privacyUrl = buildConfig().privacyUrls[language] || buildConfig().privacyUrls.default
-  openExternalUrl(privacyUrl)
-}
-
-export default openPrivacyPolicy
