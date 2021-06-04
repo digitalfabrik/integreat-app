b40fdc4a33d41d3a3dc7a96045bd954df96c97d6
Steffen Kleinle <klinzo@yahoo.de>
2021-06-04 15:00:37 +0200

    IGAPP-668: Remove nasty route pattern casts

--- web-ts/src/routes/index.ts
+++ web-ts/src/routes/index.ts
@@ -1,50 +1,38 @@
 import {
   CATEGORIES_ROUTE,
   DISCLAIMER_ROUTE,
-  DisclaimerRouteType,
   EVENTS_ROUTE,
-  EventsRouteType,
   LANDING_ROUTE,
-  LandingRouteType,
   LOCAL_NEWS_TYPE,
-  LocalNewsType,
   MAIN_DISCLAIMER_ROUTE,
-  MainDisclaimerRouteType,
   NEWS_ROUTE,
-  NewsRouteType,
   NOT_FOUND_ROUTE,
-  NotFoundRouteType,
   OFFERS_ROUTE,
-  OffersRouteType,
   POIS_ROUTE,
-  PoisRouteType,
   SEARCH_ROUTE,
-  SearchRouteType,
   SPRUNGBRETT_OFFER_ROUTE,
-  SprungbrettOfferRouteType,
-  TU_NEWS_TYPE,
-  TuNewsType
+  TU_NEWS_TYPE
 } from 'api-client'
 import { ExtractRouteParams } from 'react-router'
 import { generatePath } from 'react-router-dom'
 
 export const cityContentPattern = `/:cityCode/:languageCode`
-type cityContentPatternType = typeof cityContentPattern
 export const RoutePatterns = {
-  [LANDING_ROUTE]: `/${LANDING_ROUTE}/:languageCode` as `/${LandingRouteType}/:languageCode`,
-  [MAIN_DISCLAIMER_ROUTE]: `/${MAIN_DISCLAIMER_ROUTE}` as `/${MainDisclaimerRouteType}`,
-  [NOT_FOUND_ROUTE]: `/${NOT_FOUND_ROUTE}` as `/${NotFoundRouteType}`,
+  [LANDING_ROUTE]: `/${LANDING_ROUTE}/:languageCode`,
+  [MAIN_DISCLAIMER_ROUTE]: `/${MAIN_DISCLAIMER_ROUTE}`,
+  [NOT_FOUND_ROUTE]: `/${NOT_FOUND_ROUTE}`,
 
-  [EVENTS_ROUTE]: `${cityContentPattern}/${EVENTS_ROUTE}/:eventId?` as `${cityContentPatternType}/${EventsRouteType}:eventId?`,
-  [SPRUNGBRETT_OFFER_ROUTE]: `${cityContentPattern}/${OFFERS_ROUTE}/${SPRUNGBRETT_OFFER_ROUTE}` as `${cityContentPatternType}/${OffersRouteType}/${SprungbrettOfferRouteType}`,
-  [OFFERS_ROUTE]: `${cityContentPattern}/${OFFERS_ROUTE}` as `${cityContentPatternType}/${OffersRouteType}`,
-  [POIS_ROUTE]: `${cityContentPattern}/${POIS_ROUTE}/:poiId?` as `${cityContentPatternType}/${PoisRouteType}:poiId?`,
-  [LOCAL_NEWS_TYPE]: `${cityContentPattern}/${NEWS_ROUTE}/${LOCAL_NEWS_TYPE}/:newsId?` as `${cityContentPatternType}/${NewsRouteType}/${LocalNewsType}/:newsId?`,
-  [TU_NEWS_TYPE]: `${cityContentPattern}/${NEWS_ROUTE}/${TU_NEWS_TYPE}/:newsId?` as `${cityContentPatternType}/${NewsRouteType}/${TuNewsType}/:newsId?`,
-  [SEARCH_ROUTE]: `${cityContentPattern}/${SEARCH_ROUTE}` as `${cityContentPatternType}/${SearchRouteType}`,
-  [DISCLAIMER_ROUTE]: `${cityContentPattern}/${DISCLAIMER_ROUTE}` as `${cityContentPatternType}/${DisclaimerRouteType}`,
-  [CATEGORIES_ROUTE]: `${cityContentPattern}/:categoryId*` as `${cityContentPatternType}/:categoryId*`
-}
+  [EVENTS_ROUTE]: `${cityContentPattern}/${EVENTS_ROUTE}/:eventId?`,
+  [SPRUNGBRETT_OFFER_ROUTE]: `${cityContentPattern}/${OFFERS_ROUTE}/${SPRUNGBRETT_OFFER_ROUTE}`,
+  [OFFERS_ROUTE]: `${cityContentPattern}/${OFFERS_ROUTE}`,
+  [POIS_ROUTE]: `${cityContentPattern}/${POIS_ROUTE}/:poiId?`,
+  [LOCAL_NEWS_TYPE]: `${cityContentPattern}/${NEWS_ROUTE}/${LOCAL_NEWS_TYPE}/:newsId?`,
+  [TU_NEWS_TYPE]: `${cityContentPattern}/${NEWS_ROUTE}/${TU_NEWS_TYPE}/:newsId?`,
+  [SEARCH_ROUTE]: `${cityContentPattern}/${SEARCH_ROUTE}`,
+  [DISCLAIMER_ROUTE]: `${cityContentPattern}/${DISCLAIMER_ROUTE}`,
+  [CATEGORIES_ROUTE]: `${cityContentPattern}/:categoryId*`
+} as const
+
 export type RouteType = keyof typeof RoutePatterns
 export type RoutePatternType = typeof RoutePatterns[keyof typeof RoutePatterns]
 
