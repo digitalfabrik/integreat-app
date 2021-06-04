91812e7a8c2a57eca147fca5850b4c2d5af9045d
Steffen Kleinle <klinzo@yahoo.de>
2021-06-04 14:57:12 +0200

    IGAPP-668: Add wrapper for generatePath

--- web-ts/src/components/GeneralHeader.tsx
+++ web-ts/src/components/GeneralHeader.tsx
@@ -5,8 +5,7 @@ import HeaderActionItemLink from './HeaderActionItemLink'
 import { withTranslation, TFunction } from 'react-i18next'
 import buildConfig from '../constants/buildConfig'
 import { LANDING_ROUTE } from 'api-client'
-import { generatePath } from 'react-router-dom'
-import { RoutePatterns } from '../routes'
+import { createPath } from '../routes'
 
 type PropsType = {
   languageCode: string
@@ -15,7 +14,7 @@ type PropsType = {
 }
 
 const GeneralHeader = ({ languageCode, viewportSmall, t }: PropsType) => {
-  const landingPath = generatePath(RoutePatterns[LANDING_ROUTE], { languageCode })
+  const landingPath = createPath(LANDING_ROUTE, { languageCode })
   const actionItems = !buildConfig().featureFlags.fixedCity
     ? [<HeaderActionItemLink key='landing' href={landingPath} iconSrc={landingIcon} text={t('changeLocation')} />]
     : []
