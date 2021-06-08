25e6962f68baf8e154c5449c514a15fd7969e662
Steffen Kleinle <klinzo@yahoo.de>
2021-06-08 20:00:21 +0200

    IGAPP-640: Fix tests

--- web-ts/src/components/__tests__/CategoriesToolbar.spec.tsx
+++ web-ts/src/components/__tests__/CategoriesToolbar.spec.tsx
@@ -1,27 +1,14 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import CategoriesToolbar from '../CategoriesToolbar'
-import CategoriesMapModelBuilder from '../../../../api-client/src/testing/CategoriesMapModelBuilder'
+import CategoriesMapModelBuilder from 'api-client/src/testing/CategoriesMapModelBuilder'
+
+jest.mock('react-i18next')
 
 describe('CategoriesToolbar', () => {
-  const t = key => key
   const city = 'augsburg'
   const language = 'de'
   const category = new CategoriesMapModelBuilder(city, language).build()[0]
-
-  it('should render nothing, if category cannot be found', () => {
-    const component = shallow(
-      <CategoriesToolbar
-        category={category}
-        cityCode={city}
-        languageCode={language}
-        openFeedbackModal={() => {}}
-        viewportSmall
-      />
-    )
-
-    expect(component.getElement()).toBeNull()
-  })
 
   it('should render Toolbar, if category can be found', () => {
     const component = shallow(
