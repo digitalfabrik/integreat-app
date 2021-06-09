b5088e730a2cacc5eee9b3e75ec3294f4e7bd521
Sarah Sporck <sporck@integreat-app.de>
2021-06-09 15:50:09 +0200

    IGAPP-648: add test for disclaimer + main disclaimer page & add load route components

--- web-ts/src/routes/MainDisclaimerPage.tsx
+++ web-ts/src/routes/MainDisclaimerPage.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import Caption from 'src/components/Caption'
+import React, { ReactElement } from 'react'
+import Caption from '../components/Caption'
 import buildConfig from '../constants/buildConfig'
+import Layout from '../components/Layout'
+import GeneralFooter from '../components/GeneralFooter'
+import GeneralHeader from '../components/GeneralHeader'
 
-const MainDisclaimerPage = () => {
+type PropsType = { languageCode: string}
+
+const MainDisclaimerPage = ({ languageCode }: PropsType): ReactElement => {
   return (
-    <div>
-      <Caption title='Impressum und Datenschutz' />
-      <div
-        dangerouslySetInnerHTML={{
-          __html: buildConfig().mainImprint
-        }}
-      />
-    </div>
+    <Layout
+      header={<GeneralHeader languageCode={languageCode} viewportSmall={false} />}
+      footer={<GeneralFooter language={languageCode} />}>
+      <div>
+        <Caption title='Impressum und Datenschutz' />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: buildConfig().mainImprint
+          }}
+        />
+      </div>
+    </Layout>
   )
 }
 
--- web-ts/src/routes/__tests__/DisclaimerPage.spec.tsx
+++ web-ts/src/routes/__tests__/DisclaimerPage.spec.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import { render } from '@testing-library/react'
 import moment from 'moment'
 import DisclaimerPage from '../DisclaimerPage'
-import { PageModel } from 'api-client'
+import { mocked } from 'ts-jest/utils'
+import { DISCLAIMER_ROUTE, PageModel, useLoadFromEndpoint } from 'api-client'
 import CityModelBuilder from '../../../../api-client/src/testing/CityModelBuilder'
 import LanguageModelBuilder from '../../../../api-client/src/testing/LanguageModelBuilder'
-import { renderWithRouter } from '../../testing/render'
+import { renderWithBrowserRouter } from '../../testing/render'
 import { Route } from 'react-router-dom'
+import { createPath, RoutePatterns } from '../index'
+import buildConfig from '../../constants/buildConfig'
+import { ThemeProvider } from 'styled-components'
+
+jest.mock('api-client', () => {
+  return {
+    ...jest.requireActual('api-client'),
+    useLoadFromEndpoint: jest.fn()
+  }
+})
+jest.mock('react-i18next')
 
 describe('DisclaimerPage', () => {
+  const mockUseLoadFromEndpointOnce = mock => {
+    mocked(useLoadFromEndpoint).mockImplementationOnce(mock)
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   const languages = new LanguageModelBuilder(2).build()
   const cities = new CityModelBuilder(2).build()
   const disclaimer = new PageModel({
@@ -19,23 +38,32 @@ describe('DisclaimerPage', () => {
     hash: '2fe6283485a93932'
   })
 
-  it('should match snapshot', () => {
+  it('should render page with title and content', () => {
     const city = cities[0]
     const language = languages[0]
-    const { getByText } = renderWithRouter(
-      <Route
-        render={props => (
-          <DisclaimerPage
-            cities={cities}
-            cityModel={city}
-            languages={languages}
-            languageModel={language}
-            disclaimer={disclaimer}
-            {...props}
-          />
-        )}
-      />
+    mockUseLoadFromEndpointOnce(() => ({
+      data: disclaimer,
+      loading: false,
+      error: null
+    }))
+    const { getByText } = renderWithBrowserRouter(
+      <ThemeProvider theme={buildConfig().lightTheme}>
+        <Route
+          path={RoutePatterns[DISCLAIMER_ROUTE]}
+          render={props => (
+            <DisclaimerPage
+              cities={cities}
+              cityModel={city}
+              languages={languages}
+              languageModel={language}
+              {...props}
+            />
+          )}
+        />
+      </ThemeProvider>,
+      { route: createPath(DISCLAIMER_ROUTE, { cityCode: city.code, languageCode: language.code }) }
     )
+
     expect(getByText(disclaimer.title)).toBeTruthy()
     expect(getByText(disclaimer.content)).toBeTruthy()
   })
