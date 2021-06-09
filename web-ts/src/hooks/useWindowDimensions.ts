08578d55edf96f8cfe71f32416771044d144547a
Steffen Kleinle <klinzo@yahoo.de>
2021-06-09 14:11:00 +0200

    IGAPP-679: Add useWindowDimensions

---
+++ web-ts/src/hooks/useWindowDimensions.ts
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react'
+
+const VIEWPORT_SMALL_THRESHOLD = 750
+
+type WindowDimensionsType = { width: number, height: number, viewportSmall: boolean }
+
+const getWindowDimensions = (): WindowDimensionsType => {
+  const { innerWidth: width, innerHeight: height } = window
+  return {
+    width,
+    height,
+    viewportSmall: width < VIEWPORT_SMALL_THRESHOLD
+  }
+}
+
+const useWindowDimensions = (): WindowDimensionsType => {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions())
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions())
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return windowDimensions
+}
+
+export default useWindowDimensions
