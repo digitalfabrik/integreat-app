dd5d2860339840df9d831ad5ddb34a4b7bcbb8a6
Sarah Sporck <sporck@integreat-app.de>
2021-06-16 11:46:34 +0200

    IGAPP-656: convert js to ts and prettier

--- web-ts/src/components/__tests__/SprungbrettListItem.spec.tsx
+++ web-ts/src/components/__tests__/SprungbrettListItem.spec.tsx
@@ -1,10 +1,7 @@
-// @flow
-
 import React from 'react'
 import { SprungbrettJobModel } from 'api-client'
 import { shallow } from 'enzyme'
 import SprungbrettListItem from '../SprungbrettListItem'
-
 describe('SprungbrettListItem', () => {
   const job = new SprungbrettJobModel({
     id: 0,
@@ -14,7 +11,6 @@ describe('SprungbrettListItem', () => {
     isApprenticeship: true,
     url: 'http://awesome-jobs.domain'
   })
-
   it('should render and match snapshot', () => {
     expect(shallow(<SprungbrettListItem job={job} />)).toMatchSnapshot()
   })
