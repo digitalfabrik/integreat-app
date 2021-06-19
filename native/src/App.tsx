7bfabd9af98a45d615e81e0c0be5178d3f770c3f
Maximilian Ammann <max@maxammann.org>
2021-06-19 12:27:58 +0200

    IGAPP-670: Reenable screens

--- native/src/App.tsx
+++ native/src/App.tsx
@@ -23,7 +23,7 @@ import sendTrackingSignal from './services/sendTrackingSignal'
 import useSendOfflineJpalSignals from './hooks/useSendOfflineJpalSignals'
 import { enableScreens } from 'react-native-screens';
 
-enableScreens(false)
+enableScreens(true)
 NetInfo.configure({
   reachabilityUrl: 'https://cms.integreat-app.de/ping'
 })
