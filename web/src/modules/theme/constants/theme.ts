8d62549a6031a6c3c0b0453569e6b177598c231c
Steffen Kleinle <klinzo@yahoo.de>
2021-06-20 18:20:43 +0200

    IGAPP-686: Delete outdated web files

--- web/src/modules/theme/constants/theme.ts
+++
@@ -1,6 +0,0 @@
-import buildConfig from "../../app/constants/buildConfig";
-import type { ThemeType } from "../../app/constants/buildConfig";
-export type { ThemeType };
-export const lightTheme: ThemeType = buildConfig().lightTheme;
-export const darkTheme: ThemeType = buildConfig().darkTheme;
-export default lightTheme;
