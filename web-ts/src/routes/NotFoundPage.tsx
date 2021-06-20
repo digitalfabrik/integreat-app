0fcd57b9b62bc279b3fc6984af9c9d90d7807f98
Steffen Kleinle <klinzo@yahoo.de>
2021-06-20 14:23:35 +0200

    IGAPP-684: Use helmet for not found route

--- web-ts/src/routes/NotFoundPage.tsx
+++ web-ts/src/routes/NotFoundPage.tsx
@@ -1,7 +1,17 @@
 import React, { ReactElement } from 'react'
+import { useTranslation } from 'react-i18next'
+import Helmet from '../components/Helmet'
 
 const NotFoundPage = (): ReactElement => {
-  return <div>not-found</div>
+  const { t } = useTranslation()
+  const pageTitle = t('app:pageTitles.notFound')
+
+  return (
+    <div>
+      <Helmet pageTitle={pageTitle} />
+      not-found
+    </div>
+  )
 }
 
 export default NotFoundPage
