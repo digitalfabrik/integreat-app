cb84d3a43aaf91358fe0052b43e2867842b6e8e1
Andrey Kud <a-kud@users.noreply.github.com>
2021-06-23 01:45:36 +0300

    Revert "don't user external dependency"
    This reverts commit 9b281af88b5da242945b1e79304105ebb16d30cd.

--- api-client/src/utils/__tests__/helpers.spec.ts
+++
@@ -1,9 +0,0 @@
-import { htmlDecode } from "../helpers";
-
-describe.skip('Helper Methods', () => {
-    describe('htmlDecode', () => {
-        it('should decode HTML entyty', () => {
-            expect(htmlDecode('&quot;mehrsprachige Laufbahnberatung&quot;')).toBe('')
-        });
-    });
-});
--- api-client/src/utils/helpers.ts
+++
@@ -1,4 +0,0 @@
-export function htmlDecode(input: string | null): string | null  {
-    const doc = new DOMParser().parseFromString(input, "text/html");
-    return doc.documentElement.textContent;
-}
