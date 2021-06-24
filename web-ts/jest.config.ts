0cad935fc9f7c14038a3469cf50cf6c2f56584f2
Sarah Sporck <sporck@integreat-app.de>
2021-06-24 10:07:44 +0200

    IGAPP-622: remove hash ids mapping

--- web-ts/jest.config.ts
+++ web-ts/jest.config.ts
@@ -16,7 +16,6 @@ const config: Config.InitialOptions = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.ts',
     '\\.(css)$': 'identity-obj-proxy',
-    hashids: 'hashids/dist/hashids.min.js'
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   moduleDirectories: ['node_modules'],
