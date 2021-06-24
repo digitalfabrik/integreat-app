3a508cf45024a7626d795fa024ab11c5988f1210
Sarah Sporck <sporck@integreat-app.de>
2021-06-24 10:47:48 +0200

    IGAPP-622: update jest config

--- web-ts/jest.config.ts
+++ web-ts/jest.config.ts
@@ -15,7 +15,6 @@ const config: Config.InitialOptions = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.ts',
-    '\\.(css)$': 'identity-obj-proxy',
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   moduleDirectories: ['node_modules'],
