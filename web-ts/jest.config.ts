3a4852c80d5e33bc98af83dc825544b822f0e9b0
Sarah Sporck <sporck@integreat-app.de>
2021-06-24 22:03:51 +0200

    IGAPP-622: prettier

--- web-ts/jest.config.ts
+++ web-ts/jest.config.ts
@@ -14,7 +14,7 @@ const config: Config.InitialOptions = {
   transformIgnorePatterns: [`node_modules/(?!${transformNodeModules.join('|')})`],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.ts',
+      '<rootDir>/__mocks__/fileMock.ts'
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   moduleDirectories: ['node_modules'],
