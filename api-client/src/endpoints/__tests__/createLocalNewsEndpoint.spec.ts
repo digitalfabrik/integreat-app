62122a271da72a225eb3f2d700bb18f4f1168d4e
Steffen Kleinle <klinzo@yahoo.de>
2021-07-04 10:00:45 +0200

    Revert "IGAPP-619: Prettier"
    This reverts commit 83c79121

--- api-client/src/endpoints/__tests__/createLocalNewsEndpoint.spec.ts
+++ api-client/src/endpoints/__tests__/createLocalNewsEndpoint.spec.ts
@@ -53,20 +53,16 @@ describe('createLocalNewsEndpoint', () => {
   })
 
   it('should sanitize html', () => {
-    const unsanitizedJson = [
-      {
-        ...item1,
-        message: '<a><script>alert("XSSS");</script>Ich bleib aber da.</a>',
-        title: 'Sanitize me!'
-      }
-    ]
-    expect(localNews.mapResponse(unsanitizedJson, params)).toEqual(
-      new LocalNewsModel({
-        id: 1,
-        title: 'Sanitize me!',
-        timestamp: moment.tz('2020-03-20 17:50:00', 'GMT'),
-        message: '<a>Ich bleib aber da.</a>'
-      })
-    )
+    const unsanitizedJson = [{
+      ...item1,
+      message: '<a><script>alert("XSSS");</script>Ich bleib aber da.</a>',
+      title: 'Sanitize me!'
+    }]
+    expect(localNews.mapResponse(unsanitizedJson, params)).toEqual(new LocalNewsModel({
+      id: 1,
+      title: 'Sanitize me!',
+      timestamp: moment.tz('2020-03-20 17:50:00', 'GMT'),
+      message: '<a>Ich bleib aber da.</a>'
+    }))
   })
 })
