788d3316b265a99b3b0ec1a49a77d065b43773a2
Sarah Sporck <sporck@integreat-app.de>
2021-07-16 13:27:08 +0200

    IGAPP-631: remove possible null value from offermodal, removed unneccessary withTheme

--- native/src/components/IOSSafeAreaView.ts
+++ native/src/components/IOSSafeAreaView.ts
@@ -1,11 +1,8 @@
 import { SafeAreaView } from 'react-native'
 import styled from 'styled-components/native'
-import withTheme from '../hocs/withTheme'
-import { ThemeType } from 'build-configs'
-import { ComponentType } from 'react'
 
 const IOSSafeAreaView = styled(SafeAreaView)`
   flex: 1;
 `
 
-export default withTheme(IOSSafeAreaView as ComponentType<{ theme: ThemeType }>)
+export default IOSSafeAreaView
