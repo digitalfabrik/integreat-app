4c60b82cb85cfbf78747fa4a6d7db91c8289db98
Andy <fischer@integreat-app.de>
2021-07-20 17:33:21 +0200

    IGAPP-582: add jest system time mock, added additional test

--- native/src/models/__tests__/ContentLoadCriterion.spec.ts
+++ native/src/models/__tests__/ContentLoadCriterion.spec.ts
@@ -3,6 +3,16 @@ import moment from 'moment'
 import { ContentLoadCriterion } from '../ContentLoadCriterion'
 
 describe('ContentLoadCriterion', () => {
+  // mock date to avoid test fails running at midnight
+  const nowDateString = '2021-07-20T00:00:00'
+  beforeAll(() => {
+    jest.useFakeTimers('modern')
+    jest.setSystemTime(moment(nowDateString).utc().toDate().getTime())
+  })
+
+  afterAll(() => {
+    jest.useRealTimers()
+  })
   const stateView = new ContentLoadCriterion(
     {
       forceUpdate: false,
@@ -10,12 +20,16 @@ describe('ContentLoadCriterion', () => {
     },
     false
   )
+
   it('should not update content if last update was now', () => {
-    const now = moment.utc()
-    expect(stateView.shouldUpdate(now)).toBeFalsy()
+    expect(stateView.shouldUpdate(moment(nowDateString).utc())).toBeFalsy()
   })
   it('should update if content was updated 5 minutes before start of today', () => {
-    const beforeStartDay = moment.utc().startOf('day').subtract(5, 'minutes')
+    const beforeStartDay = moment(nowDateString).utc().startOf('day').subtract(5, 'minutes')
     expect(stateView.shouldUpdate(beforeStartDay)).toBeTruthy()
+  })
+  it('should not update if content was updated 5 minutes after start of today', () => {
+    const afterStartDay = moment(nowDateString).utc().startOf('day').add(5, 'minutes')
+    expect(stateView.shouldUpdate(afterStartDay)).toBeFalsy()
   })
 })
