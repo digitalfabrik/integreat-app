dcec5e7d3da601ad02436d8a8986c6f886e90b14
Andy <fischer@integreat-app.de>
2021-07-26 14:44:19 +0200

    IGAPP-354: add integration of mapsLibre for ios WIP

--- native/src/components/MapView.tsx
+++ native/src/components/MapView.tsx
@@ -1,19 +1,20 @@
-import MapboxGL from '@react-native-mapbox-gl/maps'
 import React, { ReactElement } from 'react'
 import { View } from 'react-native'
 import styled from 'styled-components'
+import MapboxGL from '@react-native-mapbox-gl/maps'
+
+import { mapConfig } from 'api-client/src/maps'
 
 const StyledMap = styled(MapboxGL.MapView)`
   width: 700px;
   height: 500px;
-  resize-mode: contain;
 `
-MapboxGL.setAccessToken('<YOUR_ACCESSTOKEN>')
+MapboxGL.setAccessToken(mapConfig.accessToken)
 const MapView: React.FunctionComponent = (): ReactElement => {
   return (
     <View>
       <View>
-        <StyledMap styleJSON={'https://integreat.github.io/integreat-osm-liberty/style.json'} zoomEnabled />
+        <StyledMap styleJSON={mapConfig.styleJSON} zoomEnabled />
       </View>
     </View>
   )
