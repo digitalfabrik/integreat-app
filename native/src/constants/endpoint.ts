5877d2cd3726b116d0edd312ae547fa66776e77f
Sarah Sporck <sporck@integreat-app.de>
2021-07-27 14:24:53 +0200

    IGAPP-700: add defaultrequestoptions to fetch requests and define User-Agent

--- native/src/constants/endpoint.ts
+++ native/src/constants/endpoint.ts
@@ -1,2 +1,6 @@
+import { Platform } from 'react-native'
+import NativeConstants from './NativeConstants'
+
 export const tunewsApiUrl = 'https://tunews.integreat-app.de'
 export const tunewsWebsiteUrl = 'https://tunewsinternational.com'
+export const userAgent = `Integreat Native App (${Platform.OS} ${NativeConstants.appVersion})`
