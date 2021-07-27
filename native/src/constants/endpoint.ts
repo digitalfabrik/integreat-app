a9bca10eac8dfc9f088df992a5056946f93ba52e
Sarah Sporck <sporck@integreat-app.de>
2021-07-27 14:43:42 +0200

    IGAPP-700: prettier + add mock for nativeConstants

--- native/src/constants/endpoint.ts
+++ native/src/constants/endpoint.ts
@@ -3,4 +3,5 @@ import NativeConstants from './NativeConstants'
 
 export const tunewsApiUrl = 'https://tunews.integreat-app.de'
 export const tunewsWebsiteUrl = 'https://tunewsinternational.com'
+
 export const userAgent = `Integreat Native App (${Platform.OS} ${NativeConstants.appVersion})`
