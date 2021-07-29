051a96b3f4b37f3953fcf76955e8f22e4921deac
Michael Markl <marklmichael98@gmail.com>
2021-07-29 14:27:50 +0200

    IGAPP-715: Run prettier

--- web/src/CityContentSwitcher.tsx
+++ web/src/CityContentSwitcher.tsx
@@ -1,19 +1,20 @@
-import React, {ReactElement, Suspense, useCallback} from 'react'
-import {Route, RouteComponentProps, Switch} from 'react-router-dom'
+import React, { ReactElement, Suspense, useCallback } from 'react'
+import { Route, RouteComponentProps, Switch } from 'react-router-dom'
 import {
-    CATEGORIES_ROUTE,
-    CityModel,
-    createLanguagesEndpoint,
-    DISCLAIMER_ROUTE,
-    EVENTS_ROUTE,
-    LanguageModel, normalizePath,
-    OFFERS_ROUTE,
-    POIS_ROUTE,
-    SEARCH_ROUTE,
-    SPRUNGBRETT_OFFER_ROUTE,
-    useLoadFromEndpoint
+  CATEGORIES_ROUTE,
+  CityModel,
+  createLanguagesEndpoint,
+  DISCLAIMER_ROUTE,
+  EVENTS_ROUTE,
+  LanguageModel,
+  normalizePath,
+  OFFERS_ROUTE,
+  POIS_ROUTE,
+  SEARCH_ROUTE,
+  SPRUNGBRETT_OFFER_ROUTE,
+  useLoadFromEndpoint
 } from 'api-client'
-import {cmsApiBaseUrl} from './constants/urls'
+import { cmsApiBaseUrl } from './constants/urls'
 import Layout from './components/Layout'
 import FailureSwitcher from './components/FailureSwitcher'
 import LanguageFailure from './components/LanguageFailure'
@@ -21,7 +22,7 @@ import useWindowDimensions from './hooks/useWindowDimensions'
 import GeneralHeader from './components/GeneralHeader'
 import GeneralFooter from './components/GeneralFooter'
 import LoadingSpinner from './components/LoadingSpinner'
-import {createPath, LOCAL_NEWS_ROUTE, RoutePatterns, TU_NEWS_DETAIL_ROUTE, TU_NEWS_ROUTE} from './routes'
+import { createPath, LOCAL_NEWS_ROUTE, RoutePatterns, TU_NEWS_DETAIL_ROUTE, TU_NEWS_ROUTE } from './routes'
 import buildConfig from './constants/buildConfig'
 import LocationLayout from './components/LocationLayout'
 
@@ -37,112 +38,120 @@ const SearchPage = React.lazy(() => import('./routes/SearchPage'))
 const DisclaimerPage = React.lazy(() => import('./routes/DisclaimerPage'))
 
 type PropsType = {
-    cities: CityModel[]
+  cities: CityModel[]
 } & RouteComponentProps<{ cityCode: string; languageCode: string }>
 
-const CityContentSwitcher = ({cities, match, location}: PropsType): ReactElement => {
-    const {viewportSmall} = useWindowDimensions()
-    const {cityCode, languageCode} = match.params
-    const cityModel = cities.find(it => it.code === cityCode)
+const CityContentSwitcher = ({ cities, match, location }: PropsType): ReactElement => {
+  const { viewportSmall } = useWindowDimensions()
+  const { cityCode, languageCode } = match.params
+  const cityModel = cities.find(it => it.code === cityCode)
 
-    const requestLanguages = useCallback(async () => {
-        return createLanguagesEndpoint(cmsApiBaseUrl).request({city: cityCode})
-    }, [cityCode])
-    const {data: languages, loading, error: loadingError} = useLoadFromEndpoint<LanguageModel[]>(requestLanguages)
-    const languageModel = languages?.find(it => it.code === languageCode)
+  const requestLanguages = useCallback(async () => {
+    return createLanguagesEndpoint(cmsApiBaseUrl).request({ city: cityCode })
+  }, [cityCode])
+  const { data: languages, loading, error: loadingError } = useLoadFromEndpoint<LanguageModel[]>(requestLanguages)
+  const languageModel = languages?.find(it => it.code === languageCode)
 
-    if (!cityModel || !languageModel || !languages) {
-        if (loading) {
-            return (
-                <Layout>
-                    <LoadingSpinner/>
-                </Layout>
-            )
-        }
-
-        if (loadingError || !cityModel || !languages) {
-            const cityError = !cityModel ? new Error('notFound.category') : null
-            const error = cityError || loadingError || new Error('Languages should not be null!')
-
-            return (
-                <Layout
-                    header={<GeneralHeader languageCode={languageCode} viewportSmall={viewportSmall}/>}
-                    footer={<GeneralFooter language={languageCode}/>}>
-                    <FailureSwitcher error={error}/>
-                </Layout>
-            )
-        }
-
-        return (
-            <Layout
-                header={<GeneralHeader languageCode={languageCode} viewportSmall={viewportSmall}/>}
-                footer={<GeneralFooter language={languageCode}/>}>
-                <LanguageFailure
-                    cities={cities}
-                    cityCode={cityCode}
-                    pathname={location.pathname}
-                    languageCode={languageCode}
-                    languageChangePaths={languages.map(({code, name}) => ({
-                        code,
-                        name,
-                        path: createPath(CATEGORIES_ROUTE, {cityCode, languageCode: code})
-                    }))}
-                />
-            </Layout>
-        )
+  if (!cityModel || !languageModel || !languages) {
+    if (loading) {
+      return (
+        <Layout>
+          <LoadingSpinner />
+        </Layout>
+      )
     }
 
-    const params = {cities, languages, cityModel, languageModel}
-    const {eventsEnabled, offersEnabled} = cityModel
-    const localNewsEnabled = buildConfig().featureFlags.newsStream && cityModel.pushNotificationsEnabled
-    const tuNewsEnabled = buildConfig().featureFlags.newsStream && cityModel.tunewsEnabled
-    const poisEnabled = buildConfig().featureFlags.pois && cityModel.poisEnabled
+    if (loadingError || !cityModel || !languages) {
+      const cityError = !cityModel ? new Error('notFound.category') : null
+      const error = cityError || loadingError || new Error('Languages should not be null!')
 
-    const suspenseLayoutProps = {
-        cityModel,
-        viewportSmall,
-        feedbackTargetInformation: null,
-        languageChangePaths: null,
-        languageCode,
-        pathname: normalizePath(location.pathname),
-        isLoading: true
+      return (
+        <Layout
+          header={<GeneralHeader languageCode={languageCode} viewportSmall={viewportSmall} />}
+          footer={<GeneralFooter language={languageCode} />}>
+          <FailureSwitcher error={error} />
+        </Layout>
+      )
     }
-
-    const routes = [
-        ...(eventsEnabled ? [{route: EVENTS_ROUTE, render: props => <EventsPage {...props} {...params} />}] : []),
-        ...(offersEnabled ? [
-            {route: SPRUNGBRETT_OFFER_ROUTE, render: props => <SprungbrettOfferPage {...props} {...params} />},
-            {route: OFFERS_ROUTE, render: props => <OffersPage {...props} {...params} />}
-        ] : []),
-        ...(poisEnabled ? [{route: POIS_ROUTE, render: props => <PoisPage {...props} {...params} />}] : []),
-        ...(localNewsEnabled ? [
-            {route: LOCAL_NEWS_ROUTE, render: props => <LocalNewsPage {...props} {...params} />}
-        ] : []),
-        ...(tuNewsEnabled ? [
-            {route: TU_NEWS_ROUTE, render: props => <TuNewsPage {...props} {...params} />},
-            {route: TU_NEWS_DETAIL_ROUTE, render: props => <TuNewsDetailPage {...props} {...params} />}
-        ] : []),
-        {route: SEARCH_ROUTE, render: props => <SearchPage {...props} {...params} />},
-        {route: DISCLAIMER_ROUTE, render: props => <DisclaimerPage {...props} {...params} />},
-        {route: CATEGORIES_ROUTE, render: props => <CategoriesPage {...props} {...params} />, exact: false}
-    ]
-
 
     return (
-        <Switch>
-            {
-                routes.map(({exact = true, route, render}) =>
-                    <Route exact={exact} path={RoutePatterns[route]}
-                           render={props =>
-                               <Suspense fallback={
-                                   <LocationLayout {...suspenseLayoutProps} route={route}>
-                                       <LoadingSpinner/>
-                                   </LocationLayout>}>
-                                   {render(props)}
-                               </Suspense>}/>)
-            }
-        </Switch>
+      <Layout
+        header={<GeneralHeader languageCode={languageCode} viewportSmall={viewportSmall} />}
+        footer={<GeneralFooter language={languageCode} />}>
+        <LanguageFailure
+          cities={cities}
+          cityCode={cityCode}
+          pathname={location.pathname}
+          languageCode={languageCode}
+          languageChangePaths={languages.map(({ code, name }) => ({
+            code,
+            name,
+            path: createPath(CATEGORIES_ROUTE, { cityCode, languageCode: code })
+          }))}
+        />
+      </Layout>
     )
+  }
+
+  const params = { cities, languages, cityModel, languageModel }
+  const { eventsEnabled, offersEnabled } = cityModel
+  const localNewsEnabled = buildConfig().featureFlags.newsStream && cityModel.pushNotificationsEnabled
+  const tuNewsEnabled = buildConfig().featureFlags.newsStream && cityModel.tunewsEnabled
+  const poisEnabled = buildConfig().featureFlags.pois && cityModel.poisEnabled
+
+  const suspenseLayoutProps = {
+    cityModel,
+    viewportSmall,
+    feedbackTargetInformation: null,
+    languageChangePaths: null,
+    languageCode,
+    pathname: normalizePath(location.pathname),
+    isLoading: true
+  }
+
+  const routes = [
+    ...(eventsEnabled ? [{ route: EVENTS_ROUTE, render: props => <EventsPage {...props} {...params} /> }] : []),
+    ...(offersEnabled
+      ? [
+          { route: SPRUNGBRETT_OFFER_ROUTE, render: props => <SprungbrettOfferPage {...props} {...params} /> },
+          { route: OFFERS_ROUTE, render: props => <OffersPage {...props} {...params} /> }
+        ]
+      : []),
+    ...(poisEnabled ? [{ route: POIS_ROUTE, render: props => <PoisPage {...props} {...params} /> }] : []),
+    ...(localNewsEnabled
+      ? [{ route: LOCAL_NEWS_ROUTE, render: props => <LocalNewsPage {...props} {...params} /> }]
+      : []),
+    ...(tuNewsEnabled
+      ? [
+          { route: TU_NEWS_ROUTE, render: props => <TuNewsPage {...props} {...params} /> },
+          { route: TU_NEWS_DETAIL_ROUTE, render: props => <TuNewsDetailPage {...props} {...params} /> }
+        ]
+      : []),
+    { route: SEARCH_ROUTE, render: props => <SearchPage {...props} {...params} /> },
+    { route: DISCLAIMER_ROUTE, render: props => <DisclaimerPage {...props} {...params} /> },
+    { route: CATEGORIES_ROUTE, render: props => <CategoriesPage {...props} {...params} />, exact: false }
+  ]
+
+  return (
+    <Switch>
+      {routes.map(({ exact = true, route, render }) => (
+        <Route
+          exact={exact}
+          path={RoutePatterns[route]}
+          render={props => (
+            <Suspense
+              fallback={
+                <LocationLayout {...suspenseLayoutProps} route={route}>
+                  <LoadingSpinner />
+                </LocationLayout>
+              }>
+              {render(props)}
+            </Suspense>
+          )}
+        />
+      ))}
+    </Switch>
+  )
 }
 
 export default CityContentSwitcher
--- web/src/RootSwitcher.tsx
+++ web/src/RootSwitcher.tsx
@@ -1,95 +1,94 @@
-import React, {ReactElement, Suspense, useCallback} from 'react'
-import {Redirect, Route, Switch, useRouteMatch} from 'react-router-dom'
+import React, { ReactElement, Suspense, useCallback } from 'react'
+import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom'
 import {
-    CATEGORIES_ROUTE,
-    createCitiesEndpoint,
-    LANDING_ROUTE,
-    MAIN_DISCLAIMER_ROUTE,
-    NOT_FOUND_ROUTE,
-    useLoadFromEndpoint
+  CATEGORIES_ROUTE,
+  createCitiesEndpoint,
+  LANDING_ROUTE,
+  MAIN_DISCLAIMER_ROUTE,
+  NOT_FOUND_ROUTE,
+  useLoadFromEndpoint
 } from 'api-client'
 import CityContentSwitcher from './CityContentSwitcher'
-import {cmsApiBaseUrl} from './constants/urls'
+import { cmsApiBaseUrl } from './constants/urls'
 import Layout from './components/Layout'
 import GeneralHeader from './components/GeneralHeader'
 import GeneralFooter from './components/GeneralFooter'
 import FailureSwitcher from './components/FailureSwitcher'
 import LoadingSpinner from './components/LoadingSpinner'
-import {useTranslation} from 'react-i18next'
-import {cityContentPattern, createPath, RoutePatterns} from './routes'
+import { useTranslation } from 'react-i18next'
+import { cityContentPattern, createPath, RoutePatterns } from './routes'
 import useWindowDimensions from './hooks/useWindowDimensions'
 import buildConfig from './constants/buildConfig'
 
 type PropsType = {
-    setContentLanguage: (languageCode: string) => void
+  setContentLanguage: (languageCode: string) => void
 }
 
 const MainDisclaimerPage = React.lazy(() => import('./routes/MainDisclaimerPage'))
 const LandingPage = React.lazy(() => import('./routes/LandingPage'))
 const NotFoundPage = React.lazy(() => import('./routes/NotFoundPage'))
 
-const RootSwitcher = ({setContentLanguage}: PropsType): ReactElement => {
-    const requestCities = useCallback(async () => createCitiesEndpoint(cmsApiBaseUrl).request(), [])
-    const {data: cities, loading, error} = useLoadFromEndpoint(requestCities)
-    const {i18n} = useTranslation()
-    const fixedCity = buildConfig().featureFlags.fixedCity
-    // LanguageCode is always the second param (if there is one)
-    const languageCode = useRouteMatch<{ languageCode?: string }>('/:slug/:languageCode')?.params.languageCode
-    const {viewportSmall} = useWindowDimensions()
+const RootSwitcher = ({ setContentLanguage }: PropsType): ReactElement => {
+  const requestCities = useCallback(async () => createCitiesEndpoint(cmsApiBaseUrl).request(), [])
+  const { data: cities, loading, error } = useLoadFromEndpoint(requestCities)
+  const { i18n } = useTranslation()
+  const fixedCity = buildConfig().featureFlags.fixedCity
+  // LanguageCode is always the second param (if there is one)
+  const languageCode = useRouteMatch<{ languageCode?: string }>('/:slug/:languageCode')?.params.languageCode
+  const { viewportSmall } = useWindowDimensions()
 
-    const detectedLanguageCode = i18n.language
-    const language = languageCode ?? detectedLanguageCode
+  const detectedLanguageCode = i18n.language
+  const language = languageCode ?? detectedLanguageCode
 
-    if (language !== detectedLanguageCode) {
-        setContentLanguage(language)
-    }
+  if (language !== detectedLanguageCode) {
+    setContentLanguage(language)
+  }
 
-    const landingPath = createPath(LANDING_ROUTE, {languageCode: language})
-    const cityContentPath = createPath(CATEGORIES_ROUTE, {cityCode: fixedCity ?? ':cityCode', languageCode: language})
+  const landingPath = createPath(LANDING_ROUTE, { languageCode: language })
+  const cityContentPath = createPath(CATEGORIES_ROUTE, { cityCode: fixedCity ?? ':cityCode', languageCode: language })
 
-    if (loading) {
-        return (
-            <Layout>
-                <LoadingSpinner/>
-            </Layout>
-        )
-    }
-
-    if (!cities || error) {
-        return (
-            <Layout
-                header={<GeneralHeader languageCode={language} viewportSmall={viewportSmall}/>}
-                footer={<GeneralFooter language={language}/>}>
-                <FailureSwitcher error={error ?? new Error('Cities not available')}/>
-            </Layout>
-        )
-    }
-    const relevantCities = fixedCity ? cities.filter(city => city.code === fixedCity) : cities
-
+  if (loading) {
     return (
-        <Suspense fallback={<LoadingSpinner/>}>
-            <Switch>
-                {fixedCity && <Redirect exact from={RoutePatterns[LANDING_ROUTE]} to={cityContentPath}/>}
-
-                <Route
-                    exact
-                    path={RoutePatterns[LANDING_ROUTE]}
-                    render={props => <LandingPage cities={relevantCities} {...props} />}
-                />
-                <Route
-                    exact
-                    path={RoutePatterns[MAIN_DISCLAIMER_ROUTE]}
-                    render={() => <MainDisclaimerPage languageCode={language}/>}
-                />
-                <Route exact path={RoutePatterns[NOT_FOUND_ROUTE]} component={NotFoundPage}/>
-                <Route path={cityContentPattern}
-                       render={props => <CityContentSwitcher cities={relevantCities} {...props} />}/>
-                <Redirect exact from='/' to={landingPath}/>
-                <Redirect exact from={`/${LANDING_ROUTE}`} to={landingPath}/>
-                <Redirect exact from={`/:cityCode`} to={cityContentPath}/>
-            </Switch>
-        </Suspense>
+      <Layout>
+        <LoadingSpinner />
+      </Layout>
     )
+  }
+
+  if (!cities || error) {
+    return (
+      <Layout
+        header={<GeneralHeader languageCode={language} viewportSmall={viewportSmall} />}
+        footer={<GeneralFooter language={language} />}>
+        <FailureSwitcher error={error ?? new Error('Cities not available')} />
+      </Layout>
+    )
+  }
+  const relevantCities = fixedCity ? cities.filter(city => city.code === fixedCity) : cities
+
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <Switch>
+        {fixedCity && <Redirect exact from={RoutePatterns[LANDING_ROUTE]} to={cityContentPath} />}
+
+        <Route
+          exact
+          path={RoutePatterns[LANDING_ROUTE]}
+          render={props => <LandingPage cities={relevantCities} {...props} />}
+        />
+        <Route
+          exact
+          path={RoutePatterns[MAIN_DISCLAIMER_ROUTE]}
+          render={() => <MainDisclaimerPage languageCode={language} />}
+        />
+        <Route exact path={RoutePatterns[NOT_FOUND_ROUTE]} component={NotFoundPage} />
+        <Route path={cityContentPattern} render={props => <CityContentSwitcher cities={relevantCities} {...props} />} />
+        <Redirect exact from='/' to={landingPath} />
+        <Redirect exact from={`/${LANDING_ROUTE}`} to={landingPath} />
+        <Redirect exact from={`/:cityCode`} to={cityContentPath} />
+      </Switch>
+    </Suspense>
+  )
 }
 
 export default RootSwitcher
