3d07490e271a671f2ae51afa3d9a5a662e1ac3b8
Andy <fischer@integreat-app.de>
2021-08-02 11:22:22 +0200

    IGAPP-354: use MapLibre v5.11, fix mapConfig import

--- native/src/components/MapView.tsx
+++ native/src/components/MapView.tsx
@@ -1,22 +1,24 @@
 import React, { ReactElement } from 'react'
-import { View } from 'react-native'
-import styled from 'styled-components'
+import styled from 'styled-components/native'
 import MapboxGL from '@react-native-mapbox-gl/maps'
 
-import { mapConfig } from 'api-client/src/maps'
+import { mapConfig } from 'api-client'
 
+const StyledContainer = styled.View`
+  flex-direction: row;
+  justify-content: center;
+`
 const StyledMap = styled(MapboxGL.MapView)`
   width: 700px;
   height: 500px;
 `
+
 MapboxGL.setAccessToken(mapConfig.accessToken)
 const MapView: React.FunctionComponent = (): ReactElement => {
   return (
-    <View>
-      <View>
-        <StyledMap styleJSON={mapConfig.styleJSON} zoomEnabled />
-      </View>
-    </View>
+    <StyledContainer>
+      <StyledMap styleJSON={mapConfig.styleJSON} zoomEnabled />
+    </StyledContainer>
   )
 }
 
