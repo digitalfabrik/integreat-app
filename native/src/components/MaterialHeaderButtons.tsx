0af3c2caf2e588fd10005b8529a1eb0558e830b0
Steffen Kleinle <klinzo@yahoo.de>
2021-08-26 11:21:13 +0200

    IGAPP-748: Fix background and upgrade react-navigation-header-buttons

--- native/src/components/MaterialHeaderButtons.tsx
+++ native/src/components/MaterialHeaderButtons.tsx
@@ -1,44 +1,42 @@
-import * as React from 'react'
-import { ReactElement } from 'react'
+import React, { ReactElement, ReactNode } from 'react'
 import {
   defaultOnOverflowMenuPress,
   HeaderButton,
   HeaderButtons,
-  onOverflowMenuPressParams
+  OnOverflowMenuPressParams,
+  OverflowMenu
 } from 'react-navigation-header-buttons'
 import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 import { ThemeType } from 'build-configs'
 
-const MaterialHeaderButton = (props: {
-  title: string
-  onPress: () => void
-  getButtonElement: () => React.ReactElement<any>
-}) => <HeaderButton {...props} IconComponent={MaterialIcon} iconSize={23} color='black' />
+const MaterialHeaderButton = (props: { title: string; onPress: () => void }) => (
+  <HeaderButton {...props} IconComponent={MaterialIcon} iconSize={23} color='black' />
+)
 
 // Adjust cancel label for ios overflow menu of HeaderButtons
-const onOverflowMenuPress = (cancelButtonLabel: string) => ({
-  overflowButtonRef,
-  hiddenButtons
-}: onOverflowMenuPressParams) =>
+const onOverflowMenuPress = (cancelButtonLabel: string) => (props: OnOverflowMenuPressParams) =>
   defaultOnOverflowMenuPress({
-    overflowButtonRef,
-    hiddenButtons,
+    ...props,
     cancelButtonLabel
   })
 
 const MaterialHeaderButtons = (props: {
   cancelLabel: string
-  children: React.ReactNode
+  items: Array<ReactNode>
+  overflowItems: Array<ReactNode>
   theme: ThemeType
 }): ReactElement => {
-  const { cancelLabel, theme, ...otherProps } = props
+  const { cancelLabel, theme, items, overflowItems } = props
   return (
-    <HeaderButtons
-      HeaderButtonComponent={MaterialHeaderButton}
-      OverflowIcon={<MaterialIcon name='more-vert' size={23} color={theme.colors.textColor} />}
-      onOverflowMenuPress={onOverflowMenuPress(cancelLabel)}
-      {...otherProps}
-    />
+    <HeaderButtons HeaderButtonComponent={MaterialHeaderButton}>
+      {items}
+      <OverflowMenu
+        onPress={onOverflowMenuPress(cancelLabel)}
+        style={{ marginHorizontal: 10 }}
+        OverflowIcon={<MaterialIcon name='more-vert' size={23} color={theme.colors.textColor} />}>
+        {overflowItems}
+      </OverflowMenu>
+    </HeaderButtons>
   )
 }
 
