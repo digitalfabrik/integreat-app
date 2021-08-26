6849a76d90ef2cef9be64bd29fc59287146b658e
Sarah Sporck <sporck@integreat-app.de>
2021-08-26 10:44:55 +0200

    IGAPP-733: prettier

--- native/src/components/__tests__/Failure.spec.tsx
+++ native/src/components/__tests__/Failure.spec.tsx
@@ -9,34 +9,22 @@ jest.mock('react-i18next')
 describe('Failure', () => {
   it('should render a retry button if tryAgain is passed', () => {
     const { getByTestId } = render(
-      <Failure
-        theme={buildConfig().lightTheme}
-        tryAgain={() => {}}
-        code={ErrorCode.UnknownError}
-      />
+      <Failure theme={buildConfig().lightTheme} tryAgain={() => {}} code={ErrorCode.UnknownError} />
     )
     expect(getByTestId('button-tryAgain')).toBeTruthy()
   })
   it('should not render a retry button if tryAgain is not passed', () => {
-    const { queryByTestId } = render(
-      <Failure theme={buildConfig().lightTheme} code={ErrorCode.UnknownError} />
-    )
+    const { queryByTestId } = render(<Failure theme={buildConfig().lightTheme} code={ErrorCode.UnknownError} />)
     expect(queryByTestId('button-tryAgain')).toBeNull()
   })
   it('should have a correct message as title', () => {
-    const { getByText } = render(
-      <Failure theme={buildConfig().lightTheme} code={ErrorCode.UnknownError} />
-    )
+    const { getByText } = render(<Failure theme={buildConfig().lightTheme} code={ErrorCode.UnknownError} />)
     expect(getByText(ErrorCode.UnknownError)).toBeTruthy()
   })
   it('should try again if button is pressed', () => {
     const tryAgain = jest.fn()
     const { getByTestId } = render(
-      <Failure
-        theme={buildConfig().lightTheme}
-        code={ErrorCode.UnknownError}
-        tryAgain={tryAgain}
-      />
+      <Failure theme={buildConfig().lightTheme} code={ErrorCode.UnknownError} tryAgain={tryAgain} />
     )
     fireEvent.press(getByTestId('button-tryAgain'))
     expect(tryAgain).toHaveBeenCalled()
