377a697a3726c90798131a73451aada29ea0f5a4
Andy <fischer@integreat-app.de>
2021-08-26 13:06:46 +0200

    IGAPP-749: added clean link component that handles internal and external links, refactor components

--- web/src/components/CleanLink.tsx
+++ web/src/components/CleanLink.tsx
@@ -1,9 +1,36 @@
+import React, { ReactNode } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+import { isExternalUrl } from '../utils/UrlCheck'
 
-const CleanLink = styled(Link)`
+const StyledCleanLink = styled(Link)`
   color: inherit;
   text-decoration: none;
 `
+const StyledCleanAnchor = styled.a`
+  color: inherit;
+  text-decoration: none;
+`
+
+interface CleanLinkProps {
+  to: string
+  children: ReactNode
+  ariaLabel?: string
+  className?: string
+}
+
+const CleanLink: React.FC<CleanLinkProps> = ({ to, children, ariaLabel, className }: CleanLinkProps) => {
+  return (
+    <>
+      {isExternalUrl(to) ? (
+        <StyledCleanAnchor href={to} aria-label={ariaLabel} className={className}>
+          {children}
+        </StyledCleanAnchor>
+      ) : (
+        <StyledCleanLink to={to}>{children}</StyledCleanLink>
+      )}
+    </>
+  )
+}
 
 export default CleanLink
