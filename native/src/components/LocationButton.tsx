e367bf88eec2b2ed01e8c39b08bfec155a5be5b6
Sarah Sporck <sporck@integreat-app.de>
2021-08-31 14:23:11 +0200

    IGAPP-536: add button and center around position

---
+++ native/src/components/LocationButton.tsx
@@ -0,0 +1,58 @@
+import LocationIcon from '../assets/IconPlaceholder.svg'
+import SimpleImage from './SimpleImage'
+import styled from 'styled-components/native'
+import { TouchableOpacity, View } from 'react-native'
+import { checkLocationPermission, requestLocationPermission } from '../utils/LocationPermissionManager'
+import React, { ReactElement, useCallback, useEffect } from 'react'
+import { RESULTS } from 'react-native-permissions'
+
+const ICON_SIZE = 45
+
+const Circle = styled(View)`
+  position: absolute;
+  right: 3%;
+  bottom: 3%;
+  margin-top: 9px;
+  margin-bottom: 5px;
+  border-radius: ${ICON_SIZE}px;
+  height: ${ICON_SIZE}px;
+  width: ${ICON_SIZE}px;
+  background-color: ${props => props.theme.colors.backgroundColor};
+  align-items: center;
+  justify-content: center;
+  elevation: 2;
+  shadow-color: #000;
+  shadow-offset: 0px 1px;
+  shadow-opacity: 0.2;
+  shadow-radius: 1.41px;
+`
+
+const ThumbnailContainer = styled(SimpleImage)`
+  height: ${ICON_SIZE / Math.sqrt(2)}px;
+  width: ${ICON_SIZE / Math.sqrt(2)}px;
+`
+
+type LocationButtonProps = {
+  setLocationPermission: (locationPermission: boolean) => void
+}
+
+const LocationButton = ({ setLocationPermission }: LocationButtonProps): ReactElement => {
+
+  useEffect(() => {
+    checkLocationPermission().then(locationPermissionStatus => setLocationPermission(locationPermissionStatus === RESULTS.GRANTED))
+  }, [setLocationPermission])
+
+  const requestPermission = useCallback(() => {
+    requestLocationPermission().then(locationPermissionStatus => setLocationPermission(locationPermissionStatus === RESULTS.GRANTED))
+  }, [setLocationPermission])
+
+  return (
+    <Circle>
+      <TouchableOpacity onPress={requestPermission}>
+        <ThumbnailContainer source={LocationIcon} />
+      </TouchableOpacity>
+    </Circle>
+  )
+}
+
+export default LocationButton
--- native/src/components/MapView.tsx
+++ native/src/components/MapView.tsx
@@ -1,8 +1,9 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
 import styled from 'styled-components/native'
 import MapboxGL, { CameraSettings } from '@react-native-mapbox-gl/maps'
 import type { BBox } from 'geojson'
 import { defaultViewportConfig, mapConfig } from 'api-client'
+import LocationButton from './LocationButton'
 
 const MapContainer = styled.View`
   flex-direction: row;
@@ -20,6 +21,8 @@ type MapViewPropsType = {
 // Has to be set even if we use map libre
 MapboxGL.setAccessToken(mapConfig.accessToken)
 const MapView = ({ boundingBox }: MapViewPropsType): ReactElement => {
+  const [locationPermission, setLocationPermission] = useState(false)
+
   const bounds = {
     ne: [boundingBox[2], boundingBox[3]],
     sw: [boundingBox[0], boundingBox[1]]
@@ -29,11 +32,14 @@ const MapView = ({ boundingBox }: MapViewPropsType): ReactElement => {
     zoomLevel: defaultViewportConfig.zoom,
     bounds
   }
+
   return (
     <MapContainer>
       <StyledMap styleJSON={mapConfig.styleJSON} zoomEnabled>
+        <MapboxGL.UserLocation visible={locationPermission} />
         <MapboxGL.Camera defaultSettings={defaultSettings} />
       </StyledMap>
+      <LocationButton setLocationPermission={setLocationPermission}/>
     </MapContainer>
   )
 }
