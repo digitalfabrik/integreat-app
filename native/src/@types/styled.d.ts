86300284ad2e3dfb2d80bbf4894aee2e952c6ee1
Steffen Kleinle <klinzo@yahoo.de>
2021-09-06 15:25:33 +0200

    IGAPP-576: Fix styled component types

--- native/src/@types/styled.d.ts
+++ native/src/@types/styled.d.ts
@@ -2,5 +2,6 @@ import 'styled-components'
 import { ThemeType } from 'build-configs/ThemeType'
 
 declare module 'styled-components' {
-  export type DefaultTheme = ThemeType
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface,@typescript-eslint/consistent-type-definitions
+  export interface DefaultTheme extends ThemeType {}
 }
