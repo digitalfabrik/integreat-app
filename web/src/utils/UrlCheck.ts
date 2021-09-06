e23e33c88996dc7db2652916d8d72071946b502d
Andy <fischer@integreat-app.de>
2021-09-06 12:07:43 +0200

    IGAPP-749: remove regex check, return result of new URL

--- web/src/utils/UrlCheck.ts
+++ web/src/utils/UrlCheck.ts
@@ -1,16 +1,11 @@
-import buildConfig from '../constants/buildConfig'
-
 /**
  * Function return if path is internal or external by checking if an URL can be created that requires an absolute path
  * @param link provides absolute or relative path
  */
 export const isExternalUrl = (link: string): boolean => {
-  const InternalLinkRegexp = new RegExp(buildConfig().internalLinksHijackPattern)
   try {
     // Check whether link is a valid URL
-    // eslint-disable-next-line no-new
-    new URL(link)
-    return !InternalLinkRegexp.test(link)
+    return !!new URL(link)
   } catch (e) {
     // Link is not a valid URL and therefore just a pathname -> internal link
     return false
