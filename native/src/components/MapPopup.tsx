4fdd3be673123f12020ce4fddfc9ad0c1c102d23
Andy <fischer@integreat-app.de>
2021-09-07 20:45:23 +0200

    IGAPP-743: rm import

--- native/src/components/MapPopup.tsx
+++ native/src/components/MapPopup.tsx
@@ -1,6 +1,5 @@
 import type { Feature, Point } from 'geojson'
 import React, { ReactElement } from 'react'
-import { useTheme } from 'styled-components'
 import styled from 'styled-components/native'
 
 type MapPopupProps = {
