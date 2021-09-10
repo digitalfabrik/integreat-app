8fabd15c681bbe27adb4b2c4d46db00396e3368a
Sarah Sporck <sarah.sporck@student.uni-augsburg.de>
2021-09-10 14:48:17 +0200

    IGAPP-764: prettier

--- native/src/App.tsx
+++ native/src/App.tsx
@@ -52,34 +52,36 @@ const linking: LinkingOptions = {
       }
     ]
   }),
-  subscribe: buildConfig().featureFlags.pushNotifications ? (listener: (url: string) => void) => {
-    const onReceiveURL = ({ url }: { url: string }) => listener(url)
+  subscribe: buildConfig().featureFlags.pushNotifications
+    ? (listener: (url: string) => void) => {
+        const onReceiveURL = ({ url }: { url: string }) => listener(url)
 
-    Linking.addEventListener('url', onReceiveURL)
+        Linking.addEventListener('url', onReceiveURL)
 
-    // TODO IGAPP-263: Temporary workaround until cityCode, languageCode and newsId are part of the push notifications
-    const unsubscribeNotification = messaging().onNotificationOpenedApp(() => {
-      const appSettings = new AppSettings()
-      appSettings.loadSettings().then(settings => {
-        const { selectedCity, contentLanguage } = settings
-        if (selectedCity && contentLanguage) {
-          listener(
-            urlFromRouteInformation({
-              cityCode: selectedCity,
-              languageCode: contentLanguage,
-              route: NEWS_ROUTE,
-              newsType: LOCAL_NEWS_TYPE
-            })
-          )
+        // TODO IGAPP-263: Temporary workaround until cityCode, languageCode and newsId are part of the push notifications
+        const unsubscribeNotification = messaging().onNotificationOpenedApp(() => {
+          const appSettings = new AppSettings()
+          appSettings.loadSettings().then(settings => {
+            const { selectedCity, contentLanguage } = settings
+            if (selectedCity && contentLanguage) {
+              listener(
+                urlFromRouteInformation({
+                  cityCode: selectedCity,
+                  languageCode: contentLanguage,
+                  route: NEWS_ROUTE,
+                  newsType: LOCAL_NEWS_TYPE
+                })
+              )
+            }
+          })
+        })
+
+        return () => {
+          Linking.removeEventListener('url', onReceiveURL)
+          unsubscribeNotification()
         }
-      })
-    })
-
-    return () => {
-      Linking.removeEventListener('url', onReceiveURL)
-      unsubscribeNotification()
-    }
-  } : undefined
+      }
+    : undefined
 }
 const dataContainer: DataContainer = new DefaultDataContainer()
 const store: Store<StateType, StoreActionType> = createReduxStore(dataContainer)
