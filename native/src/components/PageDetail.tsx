6368e916265032d0766221dd0fb98f6022a3e0f9
Andy <fischer@integreat-app.de>
2021-09-21 15:35:18 +0200

    IGAPP-747: add navigation deep linking to details page

--- native/src/components/PageDetail.tsx
+++ native/src/components/PageDetail.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react'
-import { Button } from 'react-native'
+import { useTranslation } from 'react-i18next'
+import { Button, Platform, Linking } from 'react-native'
 import styled from 'styled-components/native'
 
 import { ThemeType } from 'build-configs'
@@ -22,6 +23,11 @@ const DetailContainer = styled.Text<DetailContainerPropsType>`
   color: ${props => props.theme.colors.textColor};
 `
 
+const Spacer = styled.View`
+  width: 20px;
+  height: 20px;
+`
+
 type PropsType = {
   identifier: string
   information: string
@@ -33,6 +39,12 @@ type PropsType = {
 
 const PageDetail: React.FC<PropsType> = (props: PropsType): ReactElement => {
   const { identifier, information, theme, language, onLinkClick, linkLabel } = props
+  const { t } = useTranslation('pois')
+
+  const navigationUrl = Platform.select({
+    ios: `maps:0,0?q=${information}`,
+    android: `geo:0,0?q=${information}`
+  })
   return (
     <>
       <DetailContainer theme={theme} language={language}>
@@ -40,6 +52,12 @@ const PageDetail: React.FC<PropsType> = (props: PropsType): ReactElement => {
         {information}
       </DetailContainer>
       {linkLabel && onLinkClick && <Button title={linkLabel} onPress={() => onLinkClick()} />}
+      {linkLabel && (
+        <>
+          <Spacer />
+          <Button title={t('navigation')} onPress={() => navigationUrl && Linking.openURL(navigationUrl)} />
+        </>
+      )}
     </>
   )
 }
