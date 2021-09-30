2abc1a51c884fec9dae3d6650d99774da1fa7428
Steffen Kleinle <klinzo@yahoo.de>
2021-09-30 11:06:32 +0200

    IGAPP-778: Add react/prefer-stateless-function rule

--- native/src/routes/Disclaimer.tsx
+++ native/src/routes/Disclaimer.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { ReactElement } from 'react'
 
 import { PageModel } from 'api-client'
 import { ThemeType } from 'build-configs'
@@ -13,23 +13,20 @@ type PropsType = {
   resourceCacheUrl: string
 }
 
-class Disclaimer extends React.Component<PropsType> {
-  render(): ReactNode {
-    const { disclaimer, theme, language, resourceCacheUrl, navigateToLink } = this.props
-    const { title, content, lastUpdate } = disclaimer
-    return (
-      <Page
-        title={title}
-        content={content}
-        theme={theme}
-        navigateToLink={navigateToLink}
-        files={{}}
-        language={language}
-        resourceCacheUrl={resourceCacheUrl}
-        lastUpdate={lastUpdate}
-      />
-    )
-  }
+const Disclaimer = ({ disclaimer, theme, language, resourceCacheUrl, navigateToLink }: PropsType): ReactElement => {
+  const { title, content, lastUpdate } = disclaimer
+  return (
+    <Page
+      title={title}
+      content={content}
+      theme={theme}
+      navigateToLink={navigateToLink}
+      files={{}}
+      language={language}
+      resourceCacheUrl={resourceCacheUrl}
+      lastUpdate={lastUpdate}
+    />
+  )
 }
 
 export default Disclaimer
