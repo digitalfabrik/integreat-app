a975d7d5c9e3eef83c082a4acddf9030d1352f72
Maximilian Ammann <max@maxammann.org>
2021-10-13 15:36:24 +0200

    IGAPP-99: Add naive pinch to zoom

--- native/src/routes/ImageViewModal.tsx
+++ native/src/routes/ImageViewModal.tsx
@@ -5,6 +5,7 @@ import { NavigationPropType, RoutePropType } from '../constants/NavigationTypes'
 import withTheme from '../hocs/withTheme'
 import { ThemeType } from 'build-configs'
 import { ImageViewModalRouteType } from 'api-client'
+import PinchableBox from '../components/PinchableBox'
 
 type PropsType = {
   route: RoutePropType<ImageViewModalRouteType>
@@ -24,14 +25,7 @@ const ImageViewModal = (props: PropsType): ReactElement => {
         flex: 1,
         backgroundColor: props.theme.colors.backgroundAccentColor
       }}>
-      <Image
-        style={{ flex: 1 }}
-        resizeMode='center'
-        source={{ uri: props.route.params.url }}
-        onError={() => {
-          setError(true)
-        }}
-      />
+      <PinchableBox />
     </View>
   )
 }
