ba4a8d83be137c8d74cac48725b987ddea5e8ac5
Sarah Sporck <sporck@integreat-app.de>
2021-10-13 11:10:33 +0200

    IGAPP-770: add dependencies to useEffect

--- native/src/hooks/useClearRouteOnClose.ts
+++ native/src/hooks/useClearRouteOnClose.ts
@@ -5,14 +5,17 @@ import { RoutePropType, RoutesType } from '../constants/NavigationTypes'
 import { StoreActionType } from '../redux/StoreActionType'
 
 const useClearRouteOnClose = (route: RoutePropType<RoutesType>, dispatch: Dispatch<StoreActionType>): void => {
-  useEffect(() => () => {
-    dispatch({
-      type: 'CLEAR_ROUTE',
-      params: {
-        key: route.key
-      }
-    })
-  })
+  useEffect(
+    () => () => {
+      dispatch({
+        type: 'CLEAR_ROUTE',
+        params: {
+          key: route.key
+        }
+      })
+    },
+    [route.key, dispatch]
+  )
 }
 
 export default useClearRouteOnClose
