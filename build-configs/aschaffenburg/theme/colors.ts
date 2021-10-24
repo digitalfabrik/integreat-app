f7f3f812b862d0201e1e031f1a9d5c97e5ff5874
Cmd190 <tonigaser@gmail.com>
2021-10-24 14:28:40 +0200

    IGAPP-259: Created light color for settings switch

--- build-configs/aschaffenburg/theme/colors.ts
+++ build-configs/aschaffenburg/theme/colors.ts
@@ -1,8 +1,10 @@
 import { ColorsType, commonLightColors } from '../../common/theme/colors'
 
 const themeColor = '#009684'
+const themeColorLight = 'rgba(0, 150, 132, 0.5)'
 
 export const lightColors: ColorsType = {
   themeColor,
+  themeColorLight,
   ...commonLightColors
 }
