cecf88e2e837055bf79282362b5d7e35e7275292
Maximilian Ammann <max@maxammann.org>
2021-10-25 15:41:21 +0200

    IGAPP-99: Add bounts for zoom and pan

--- native/src/components/PinchableBox.tsx
+++ native/src/components/PinchableBox.tsx
@@ -1,34 +1,26 @@
 import React, { ReactElement } from 'react'
-import { View } from 'react-native'
-import { Animated, Dimensions, StyleSheet } from 'react-native'
-import { PanGestureHandlerStateChangeEvent } from 'react-native-gesture-handler'
-import { PanGestureHandlerGestureEvent } from 'react-native-gesture-handler'
+import { Animated, Dimensions, Image, View } from 'react-native'
 import {
   PanGestureHandler,
   PinchGestureHandler,
-  RotationGestureHandler,
-  State,
+  PanGestureHandlerGestureEvent,
+  PanGestureHandlerStateChangeEvent,
   PinchGestureHandlerGestureEvent,
-  PinchGestureHandlerStateChangeEvent
+  PinchGestureHandlerStateChangeEvent,
+  State
 } from 'react-native-gesture-handler'
 
 const USE_NATIVE_DRIVER = true
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  pinchableImage: {
-    width: 250,
-    height: 250
-  }
-})
-
-type Props = {
-  url: string
+type PropsType = {
+  uri: string
 }
 
-export class PinchableBox extends React.Component {
+type StateType = {
+  imageDimensions?: { width: number; height: number }
+}
+
+export class PinchableBox extends React.Component<PropsType, StateType> {
   private lastOffset: { x: number; y: number }
   private readonly translateX: Animated.AnimatedValue
   private readonly translateY: Animated.AnimatedValue
@@ -42,8 +34,10 @@ export class PinchableBox extends React.Component {
   private lastScale: number
   private readonly onPinchGestureEvent: (event: PinchGestureHandlerGestureEvent) => void
 
-  constructor(props: Props) {
+  constructor(props: PropsType) {
     super(props)
+
+    this.state = {}
 
     // Panning
     this.translateX = new Animated.Value(0)
@@ -71,8 +65,14 @@ export class PinchableBox extends React.Component {
       useNativeDriver: USE_NATIVE_DRIVER
     })
 
+    // Make translation depend on scale
     this.scaledTranslateX = Animated.divide(this.translateX, this.scale)
     this.scaledTranslateY = Animated.divide(this.translateY, this.scale)
+
+    const { uri } = props
+    Image.getSize(uri, (width, height) => {
+      this.setState({ imageDimensions: { width, height } })
+    })
   }
 
   private onPinchHandlerStateChange = (event: PinchGestureHandlerStateChangeEvent) => {
@@ -80,57 +80,160 @@ export class PinchableBox extends React.Component {
       this.lastScale *= event.nativeEvent.scale
       this.baseScale.setValue(this.lastScale)
       this.pinchScale.setValue(1)
+
+      if (this.lastScale <= 1) {
+        Animated.spring(this.baseScale, {
+          toValue: 1,
+          useNativeDriver: USE_NATIVE_DRIVER
+        }).start(({ finished }) => {
+          if (finished) {
+            this.lastScale = 1
+          }
+        })
+      }
     }
   }
 
-  private onHandlerStateChange = (event: PanGestureHandlerStateChangeEvent) => {
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      this.lastOffset.x += event.nativeEvent.translationX
-      this.lastOffset.y += event.nativeEvent.translationY
-      this.translateX.setOffset(this.lastOffset.x)
-      this.translateX.setValue(0)
-      this.translateY.setOffset(this.lastOffset.y)
-      this.translateY.setValue(0)
-    }
-  }
+  private onPanHandlerStateChange =
+    (viewWidth: number, viewHeight: number, imageWidth: number, imageHeight: number) =>
+    (event: PanGestureHandlerStateChangeEvent) => {
+      if (event.nativeEvent.oldState === State.ACTIVE) {
+        const yDelta = event.nativeEvent.translationY
+        const xDelta = event.nativeEvent.translationX
 
-  render(): ReactElement {
+        const widthIncreaseByScale = (Math.max(0, this.lastScale - 1) * viewWidth) / 2
+        const heightIncreaseByScale = (Math.max(0, this.lastScale - 1) * viewHeight) / 2
+
+        const minX = 0 - widthIncreaseByScale
+        const maxX = viewWidth + widthIncreaseByScale - imageWidth
+        const minY = 0 - heightIncreaseByScale
+        const maxY = viewHeight + heightIncreaseByScale - imageHeight
+
+        this.lastOffset.y += yDelta
+        this.translateY.setOffset(this.lastOffset.y)
+        this.translateY.setValue(0)
+
+        this.lastOffset.x += xDelta
+        this.translateX.setOffset(this.lastOffset.x)
+        this.translateX.setValue(0)
+
+        // Fix position if below x = 0
+        if (this.lastOffset.x <= minX) {
+          // todo: what should happen if this animation is interrupted?
+          Animated.spring(this.translateX, {
+            toValue: minX - this.lastOffset.x,
+            useNativeDriver: USE_NATIVE_DRIVER
+          }).start(({ finished }) => {
+            if (finished) {
+              this.lastOffset.x = minX
+              this.translateX.setOffset(minX)
+              this.translateX.setValue(0)
+            }
+          })
+        }
+
+        // Fix position if above x = screen width
+        if (this.lastOffset.x >= maxX) {
+          Animated.spring(this.translateX, {
+            toValue: maxX - this.lastOffset.x,
+            useNativeDriver: USE_NATIVE_DRIVER
+          }).start(({ finished }) => {
+            if (finished) {
+              this.lastOffset.x = maxX
+              this.translateX.setOffset(maxX)
+              this.translateX.setValue(0)
+            }
+          })
+        }
+
+        // Fix position if below y = 0
+        if (this.lastOffset.y <= minY) {
+          Animated.spring(this.translateY, {
+            toValue: minY - this.lastOffset.y,
+            useNativeDriver: USE_NATIVE_DRIVER
+          }).start(({ finished }) => {
+            if (finished) {
+              this.lastOffset.y = minY
+              this.translateY.setOffset(minY)
+              this.translateY.setValue(0)
+            }
+          })
+        }
+
+        // Fix position if above y = screen height
+        if (this.lastOffset.y >= maxY) {
+          Animated.spring(this.translateY, {
+            toValue: maxY - this.lastOffset.y,
+            useNativeDriver: USE_NATIVE_DRIVER
+          }).start(({ finished }) => {
+            if (finished) {
+              this.lastOffset.y = maxY
+              this.translateY.setOffset(maxY)
+              this.translateY.setValue(0)
+            }
+          })
+        }
+
+        // todo: handle for zoom?
+      }
+    }
+
+  render(): ReactElement | null {
     const pinchHandler = React.createRef()
     const panHandler = React.createRef()
+    const { uri } = this.props
+    const { imageDimensions } = this.state
+
+    if (!imageDimensions) {
+      return null
+    }
+
+    const { width: imageWidth, height: imageHeight } = imageDimensions
+
+    const viewWidth = Dimensions.get('window').width
+    const realImageHeight = imageHeight * (viewWidth / imageWidth)
+    const viewHeight = realImageHeight
 
     return (
-      <PanGestureHandler
-        ref={panHandler}
-        simultaneousHandlers={pinchHandler}
-        onGestureEvent={this.onPanGestureEvent}
-        onHandlerStateChange={this.onHandlerStateChange}
-        minDist={10}>
-        <Animated.View style={styles.container} collapsable={false}>
-          <PinchGestureHandler
-            ref={pinchHandler}
-            simultaneousHandlers={panHandler}
-            onGestureEvent={this.onPinchGestureEvent}
-            onHandlerStateChange={this.onPinchHandlerStateChange}>
-            <Animated.Image
-              style={[
-                styles.pinchableImage,
-                {
-                  transform: [
-                    { scale: this.scale },
-                    { translateX: this.scaledTranslateX },
-                    { translateY: this.scaledTranslateY }
-                  ]
-                }
-              ]}
-              resizeMode='center'
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              source={{
-                uri: 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-              }}
-            />
-          </PinchGestureHandler>
-        </Animated.View>
-      </PanGestureHandler>
+      <View style={{ width: viewWidth, height: viewHeight, overflow: 'hidden' }}>
+        <PanGestureHandler
+          ref={panHandler}
+          simultaneousHandlers={pinchHandler}
+          onGestureEvent={this.onPanGestureEvent}
+          onHandlerStateChange={this.onPanHandlerStateChange(viewWidth, viewHeight, viewWidth, viewHeight)}
+          shouldCancelWhenOutside
+          minDist={10}>
+          <Animated.View style={{ flex: 1 }} collapsable={false}>
+            <PinchGestureHandler
+              ref={pinchHandler}
+              simultaneousHandlers={panHandler}
+              onGestureEvent={this.onPinchGestureEvent}
+              onHandlerStateChange={this.onPinchHandlerStateChange}
+              shouldCancelWhenOutside>
+              <Animated.Image
+                style={[
+                  {
+                    width: viewWidth,
+                    height: realImageHeight
+                  },
+                  {
+                    transform: [
+                      { scale: this.scale },
+                      { translateX: this.scaledTranslateX },
+                      { translateY: this.scaledTranslateY }
+                    ]
+                  }
+                ]}
+                resizeMode='stretch'
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+                source={{
+                  uri
+                }}
+              />
+            </PinchGestureHandler>
+          </Animated.View>
+        </PanGestureHandler>
+      </View>
     )
   }
 }
--- native/src/routes/ImageViewModal.tsx
+++ native/src/routes/ImageViewModal.tsx
@@ -1,21 +1,20 @@
 import * as React from 'react'
-import { ReactElement, useState } from 'react'
-import { View, Image, Text } from 'react-native'
+import { ReactElement, useContext, useState } from 'react'
+import { View, Text } from 'react-native'
+import { ThemeContext } from 'styled-components'
 
 import { ImageViewModalRouteType } from 'api-client'
-import { ThemeType } from 'build-configs'
 
 import PinchableBox from '../components/PinchableBox'
 import { NavigationPropType, RoutePropType } from '../constants/NavigationTypes'
-import withTheme from '../hocs/withTheme'
 
 type PropsType = {
   route: RoutePropType<ImageViewModalRouteType>
   navigation: NavigationPropType<ImageViewModalRouteType>
-  theme: ThemeType
 }
-const ImageViewModal = (props: PropsType): ReactElement => {
+const ImageViewModal = ({ route }: PropsType): ReactElement => {
   const [isError, setError] = useState(false)
+  const theme = useContext(ThemeContext)
 
   if (isError) {
     return <Text>Error</Text>
@@ -25,11 +24,12 @@ const ImageViewModal = (props: PropsType): ReactElement => {
     <View
       style={{
         flex: 1,
-        backgroundColor: props.theme.colors.backgroundAccentColor
+        justifyContent: 'center',
+        backgroundColor: theme.colors.backgroundAccentColor
       }}>
-      <PinchableBox />
+      <PinchableBox uri='https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Emperor-call_hg.jpg/180px-Emperor-call_hg.jpg' />
     </View>
   )
 }
 
-export default withTheme(ImageViewModal)
+export default ImageViewModal
