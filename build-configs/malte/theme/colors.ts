d03738b7600ed180072568a9a4d94ed0a73bc6fa
Cmd190 <tonigaser@gmail.com>
2021-10-27 11:52:42 +0200

    IGAPP-259: corrected colors in malte config

--- build-configs/malte/theme/colors.ts
+++ build-configs/malte/theme/colors.ts
@@ -1,8 +1,7 @@
 import { ColorsType, commonLightColors } from '../../common/theme/colors'
 
 const themeColor = '#ff0000'
-const themeColorLight = '#ff0000'
-// const themeColorLight = 'rgba(255, 0, 0, 0.5)'
+const themeColorLight = 'rgba(255, 0, 0, 0.5)'
 
 export const lightColors: ColorsType = {
   themeColor,
