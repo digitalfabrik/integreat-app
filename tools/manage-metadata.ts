753aee7893221952d56799f5f4212b1766ffe2d9
Steffen Kleinle <klinzo@yahoo.de>
2021-10-30 12:48:58 +0200

    IGAPP-803: Add store mapping for chinese

--- tools/manage-metadata.ts
+++ tools/manage-metadata.ts
@@ -233,7 +233,8 @@ const appstoreLanguageMap: Record<string, string[]> = {
   pl: ['pl'],
   ro: ['ro'],
   ru: ['ru'],
-  tr: ['tr']
+  tr: ['tr'],
+  'zh-CN': ['zh-Hans']
 }
 
 // Maps our translation keys to the right key used by the play store
@@ -259,7 +260,8 @@ const playstoreLanguageMap: Record<string, string[]> = {
   ru: ['ru-RU'],
   sq: ['sq'],
   tr: ['tr-TR'],
-  ur: ['ur']
+  ur: ['ur'],
+  'zh-CN': ['zh-CN']
 }
 
 program.version('0.1.0').option('-d, --debug', 'enable extreme logging')
