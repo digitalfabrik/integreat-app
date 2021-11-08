5be506a6ed9d4937042bcb540714e83af20440d4
Steffen Kleinle <klinzo@yahoo.de>
2021-11-08 16:50:21 +0100

    IGAPP-628: Add more information link

---
+++ native/src/components/Link.tsx
@@ -0,0 +1,24 @@
+import React, { ReactElement } from 'react'
+import styled from 'styled-components/native'
+
+import openExternalUrl from '../utils/openExternalUrl'
+import Touchable from './Touchable'
+
+const LinkText = styled.Text`
+  padding: 50px 0;
+  align-self: center;
+  text-decoration: underline;
+`
+
+type LinkProps = {
+  url: string
+  text: string
+}
+
+const Link = ({ url, text }: LinkProps): ReactElement => (
+  <Touchable onPress={() => openExternalUrl(url)} accessibilityRole='link'>
+    <LinkText>{text}</LinkText>
+  </Touchable>
+)
+
+export default Link
