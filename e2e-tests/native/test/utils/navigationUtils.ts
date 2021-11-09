dc0c5534495448ad8ca35061da3a57a764986f39
Sarah Sporck <sarah.sporck@student.uni-augsburg.de>
2021-11-09 09:32:16 +0100

    IGAPP-755: improve checkIfDisplayedWithSwipeUp usage

--- e2e-tests/native/test/utils/navigationUtils.ts
+++ e2e-tests/native/test/utils/navigationUtils.ts
@@ -1,7 +1,19 @@
-import { openDeepLinkUrl } from '../helpers/openDeepLink'
+import { defaultCity, filter } from '../../../shared/constants'
+import Gestures from '../helpers/Gestures'
+import Keyboard from '../helpers/Keyboard'
 import DashboardPage from '../pageobjects/dashboard.page'
+import LandingPage from '../pageobjects/landing.page'
+
+const MAX_SCROLLS = 4
 
 export const navigateToDashboard = async (): Promise<void> => {
-  await openDeepLinkUrl("integreat.app/testumgebung-e2e/de")
-  expect(await DashboardPage.exists()).toBeTruthy()
+  await expect(LandingPage.get()).toExist()
+  const search = await LandingPage.search
+  await search.click()
+  await search.addValue(filter)
+  await Keyboard.hide()
+  await Gestures.checkIfDisplayedWithSwipeUp(LandingPage.city(defaultCity), MAX_SCROLLS)
+  const filteredCity = await LandingPage.city(defaultCity)
+  await filteredCity.click()
+  await expect(await DashboardPage.get()).toExist()
 }
