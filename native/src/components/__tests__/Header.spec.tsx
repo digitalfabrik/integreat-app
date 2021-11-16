ba423dfa39bb4e33690a1cb07e57404f799baaf1
Steffen Kleinle <klinzo@yahoo.de>
2021-11-16 19:15:12 +0100

    IGAPP-834: Fix and add tests

--- native/src/components/__tests__/Header.spec.tsx
+++ native/src/components/__tests__/Header.spec.tsx
@@ -1,16 +1,28 @@
-import { fireEvent, render } from '@testing-library/react-native'
+import { fireEvent, render, waitFor } from '@testing-library/react-native'
 import React from 'react'
 
 import { SEARCH_ROUTE } from 'api-client'
 import CityModelBuilder from 'api-client/src/testing/CityModelBuilder'
 
-import createNavigationScreenPropMock from '../../testing/createNavigationPropMock'
 import mockStackHeaderProps from '../../testing/mockStackHeaderProps'
+import wrapWithTheme from '../../testing/wrapWithTheme'
 import Header from '../Header'
 
 jest.mock('react-native/Libraries/Utilities/useWindowDimensions', () => ({
   default: jest.fn(() => ({ width: 1234 }))
 }))
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => `t_${key}`
+  })
+}))
+jest.mock('@react-navigation/elements', () => {
+  const { Text } = require('react-native')
+
+  return {
+    HeaderBackButton: ({ onPress }: { onPress: () => void }) => <Text onPress={onPress}>HeaderBackButton</Text>
+  }
+})
 
 describe('Header', () => {
   beforeEach(() => {
@@ -21,51 +33,69 @@ describe('Header', () => {
   const goToLanguageChange = jest.fn()
   const dispatch = jest.fn()
   const [city] = new CityModelBuilder(1).build()
-  const navigation = createNavigationScreenPropMock()
 
   const buildProps = (
     peeking: boolean,
     categoriesAvailable: boolean,
     mode: 'float' | 'screen',
-    goToLanguageChange: () => void
+    goToLanguageChange: () => void,
+    routeIndex = 0
   ): React.ComponentProps<typeof Header> => ({
-    ...mockStackHeaderProps(),
+    ...mockStackHeaderProps({}, routeIndex),
     peeking,
     categoriesAvailable,
     goToLanguageChange,
     routeCityModel: city,
     language: 'de',
     shareUrl: 'testUrl',
-    dispatch,
-    navigation
+    dispatch
   })
 
-  it('search header button should be enabled and visible after loading was finished', () => {
-    const { getByLabelText } = render(<Header {...buildProps(false, true, 'screen', goToLanguageChange)} />)
+  it('search header button should be enabled and visible after loading was finished', async () => {
+    const props = buildProps(false, true, 'screen', goToLanguageChange)
+    const { getByLabelText } = render(<Header {...props} />, { wrapper: wrapWithTheme })
     expect(getByLabelText(t('search'))).toHaveStyle({ opacity: 1 })
     fireEvent.press(getByLabelText(t('search')))
-    expect(navigation.navigate).toHaveBeenCalledTimes(1)
-    expect(navigation.navigate).toHaveBeenCalledWith(SEARCH_ROUTE)
+    await waitFor(() => expect(props.navigation.navigate).toHaveBeenCalledTimes(1))
+    expect(props.navigation.navigate).toHaveBeenCalledWith(SEARCH_ROUTE)
   })
 
-  it('language header button should be enabled and visible after loading was finished', () => {
-    const { getByLabelText } = render(<Header {...buildProps(false, true, 'screen', goToLanguageChange)} />)
+  it('language header button should be enabled and visible after loading was finished', async () => {
+    const { getByLabelText } = render(<Header {...buildProps(false, true, 'screen', goToLanguageChange)} />, {
+      wrapper: wrapWithTheme
+    })
     expect(getByLabelText(t('changeLanguage'))).toHaveStyle({ opacity: 1 })
     fireEvent.press(getByLabelText(t('changeLanguage')))
-    expect(goToLanguageChange).toHaveBeenCalledTimes(1)
+    await waitFor(() => expect(goToLanguageChange).toHaveBeenCalledTimes(1))
   })
 
   it('search header button should be disabled and invisible while loading', () => {
-    const { getByLabelText } = render(<Header {...buildProps(true, true, 'screen', goToLanguageChange)} />)
+    const props = buildProps(true, true, 'screen', goToLanguageChange)
+    const { getByLabelText } = render(<Header {...props} />, { wrapper: wrapWithTheme })
     expect(getByLabelText(t('search'))).toHaveStyle({ opacity: 0 })
     fireEvent.press(getByLabelText(t('search')))
-    expect(navigation.navigate).not.toHaveBeenCalled()
+    expect(props.navigation.navigate).not.toHaveBeenCalled()
   })
 
   it('language header button should be disabled and invisible while loading', () => {
-    const { getByLabelText } = render(<Header {...buildProps(true, true, 'screen', goToLanguageChange)} />)
+    const { getByLabelText } = render(<Header {...buildProps(true, true, 'screen', goToLanguageChange)} />, {
+      wrapper: wrapWithTheme
+    })
     expect(getByLabelText(t('changeLanguage'))).toHaveStyle({ opacity: 0 })
     fireEvent.press(getByLabelText(t('changeLanguage')))
     expect(goToLanguageChange).not.toHaveBeenCalled()
+  })
+
+  it('should show back button and navigate back on click', () => {
+    const props = buildProps(true, true, 'screen', goToLanguageChange, 1)
+    const { getByText } = render(<Header {...props} />, { wrapper: wrapWithTheme })
+    fireEvent.press(getByText('HeaderBackButton'))
+    expect(props.navigation.goBack).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not show back button if it is the only route', () => {
+    const props = buildProps(true, true, 'screen', goToLanguageChange, 0)
+    const { queryByText } = render(<Header {...props} />, { wrapper: wrapWithTheme })
+    expect(queryByText('HeaderBackButton')).toBeFalsy()
   })
 })
--- native/src/testing/createNavigationPropMock.ts
+++ native/src/testing/createNavigationPropMock.ts
@@ -1,11 +1,20 @@
+import { DASHBOARD_ROUTE } from 'api-client'
+
 import { NavigationPropType, RoutesType } from '../constants/NavigationTypes'
 
-const createNavigationMock = <T extends RoutesType>(): NavigationPropType<T> => ({
+const createNavigationMock = <T extends RoutesType>(routeIndex = 0): NavigationPropType<T> => ({
   dispatch: jest.fn(),
   canGoBack: jest.fn(),
   goBack: jest.fn(),
   getParent: jest.fn(),
-  getState: jest.fn(),
+  getState: jest.fn(() => ({
+    index: routeIndex,
+    routes: [{ key: 'some-key-0', name: DASHBOARD_ROUTE }],
+    key: 'some-key-0',
+    routeNames: [DASHBOARD_ROUTE],
+    type: 'stack',
+    stale: false
+  })),
   navigate: jest.fn(),
   setParams: jest.fn(),
   addListener: jest.fn(),
