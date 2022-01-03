32982080c45b08a50e53150da7cbcd31f1bf8060
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-01-03 13:30:11 +0100

    IGAPP-833: Use react-refresh instead of react-hot-loader

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { hot } from 'react-hot-loader'
 
 import App from './App'
 import buildConfig from './constants/buildConfig'
 
-const HMRApp = hot(module)(App)
 const container = document.getElementById('container')
 
 if (container == null) {
   throw new Error("Couldn't find element with id container.")
 }
 
-ReactDOM.render(<HMRApp />, container)
+ReactDOM.render(<App />, container)
 
 if (buildConfig().splashScreen) {
   // Sets the splash to hidden when the page is rendered
