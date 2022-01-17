4481b2306073c56fd71d9e6f635d9f6f99dc3b23
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-01-17 15:59:04 +0100

    IGAPP-879: Fix and add tests

--- web/jest.config.ts
+++ web/jest.config.ts
@@ -32,6 +32,7 @@ const config: Config.InitialOptions = {
   ],
   snapshotSerializers: ['enzyme-to-json/serializer'],
   globals: {
+    __BUILD_CONFIG_NAME__: 'integreat-test-cms',
     __BUILD_CONFIG__: webIntegreatTestCmsBuildConfig,
     __VERSION_NAME__: '0.0.0',
     __COMMIT_SHA__: 123456789
--- web/src/utils/sentry.ts
+++ web/src/utils/sentry.ts
@@ -11,67 +11,73 @@ const developerFriendly = (): boolean => buildConfig().featureFlags.developerFri
 // webpackChunkName: "sentry"
 const loadSentry = (): Promise<typeof Sentry> => import('@sentry/react')
 
-const logSentryException = (e: Error) => {
+const logSentryException = (e: unknown) => {
   console.error(e)
   console.error('Failed to load sentry entry point!')
 }
 
-export const initSentry = (): void => {
+export const initSentry = async (): Promise<void> => {
   if (!sentryEnabled()) {
     return
   }
 
-  loadSentry()
-    .then(Sentry =>
-      Sentry.init({
-        dsn: 'https://f07e705b25464bbd8b0dbbc0a6414b11@sentry.tuerantuer.org/2',
-        release: `web-${__BUILD_CONFIG_NAME__}@${__VERSION_NAME__}`
-      })
-    )
-    .catch(logSentryException)
-}
-
-export const log = (message: string, level = 'debug'): void => {
-  loadSentry()
-    .then(Sentry => {
-      if (sentryEnabled()) {
-        Sentry.addBreadcrumb({
-          message,
-          level: Sentry.Severity.fromString(level)
-        })
-      }
-      if (developerFriendly()) {
-        switch (level) {
-          case Sentry.Severity.Fatal:
-          case Sentry.Severity.Critical:
-          case Sentry.Severity.Error:
-            console.error(message)
-            break
-          case Sentry.Severity.Warning:
-            console.warn(message)
-            break
-          case Sentry.Severity.Log:
-            console.log(message)
-            break
-          case Sentry.Severity.Info:
-            console.info(message)
-            break
-          case Sentry.Severity.Debug:
-            console.debug(message)
-            break
-        }
-      }
+  try {
+    const Sentry = await loadSentry()
+    Sentry.init({
+      dsn: 'https://f07e705b25464bbd8b0dbbc0a6414b11@sentry.tuerantuer.org/2',
+      release: `web-${__BUILD_CONFIG_NAME__}@${__VERSION_NAME__}`
     })
-    .catch(logSentryException)
+  } catch (e) {
+    logSentryException(e)
+  }
 }
 
-export const reportError = (err: unknown): void => {
+export const log = async (message: string, level = 'debug'): Promise<void> => {
+  try {
+    const Sentry = await loadSentry()
+    if (sentryEnabled()) {
+      Sentry.addBreadcrumb({
+        message,
+        level: Sentry.Severity.fromString(level)
+      })
+    }
+    if (developerFriendly()) {
+      switch (level) {
+        case Sentry.Severity.Fatal:
+        case Sentry.Severity.Critical:
+        case Sentry.Severity.Error:
+          console.error(message)
+          break
+        case Sentry.Severity.Warning:
+          console.warn(message)
+          break
+        case Sentry.Severity.Log:
+          console.log(message)
+          break
+        case Sentry.Severity.Info:
+          console.info(message)
+          break
+        case Sentry.Severity.Debug:
+          console.debug(message)
+          break
+      }
+    }
+  } catch (e) {
+    logSentryException(e)
+  }
+}
+
+export const reportError = async (err: unknown): Promise<void> => {
   if (!(err instanceof NotFoundError) && !(err instanceof FetchError) && sentryEnabled()) {
     // Report important errors if sentry is enabled (and skip e.g. errors because of no invalid internet connection)
-    loadSentry()
-      .then(Sentry => Sentry.captureException(err))
-      .catch(logSentryException)
+    try {
+      const Sentry = await loadSentry()
+      Sentry.captureException(err)
+    } catch (e) {
+      logSentryException(e)
+    }
   }
+
   if (developerFriendly()) {
     console.error(err)
   }
