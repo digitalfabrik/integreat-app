b85699182c3d0b8944637de6090337300eedf477
Sarah Sporck <sporck@integreat-app.de>
2022-01-18 13:00:28 +0100

    IGAPP-796: remove unnecessary testRegex

--- native/jest.config.ts
+++ native/jest.config.ts
@@ -30,7 +30,6 @@ export default {
     'jest-extended',
     '<rootDir>/../node_modules/@testing-library/jest-native/extend-expect'
   ],
-  testRegex: '(/__tests__/.*|\\.(spec))\\.(ts|tsx)$',
   transform: tsjPreset.transform,
   transformIgnorePatterns: [`node_modules/(?!${transformNodeModules.join('|')})`],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
