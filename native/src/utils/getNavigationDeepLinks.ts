ef3395e3fb0277d4f40b2c8a2349f2dde0166ab8
Cmd190 <tonigaser@gmail.com>
2022-01-18 18:22:14 +0100

    IGAPP-806: Fixed some minor code style issues

--- native/src/utils/getNavigationDeepLinks.ts
+++ native/src/utils/getNavigationDeepLinks.ts
@@ -5,7 +5,7 @@ import { LocationModel } from 'api-client/src'
 /**
  * Depending on the platform this function returns the correct deeplink for navigation app
  * @param address{string}: location address
- * @param location{number[]}: latitude,longitude
+ * @param location{LocationModel}: location model with coordinates
  */
 export const getNavigationDeepLinks = (address: string, location: LocationModel): string | undefined => {
   if (location.coordinates == null) {
@@ -15,7 +15,6 @@ export const getNavigationDeepLinks = (address: string, location: LocationModel)
   const x = location.coordinates[0]
   const y = location.coordinates[1]
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   return Platform.select({
     ios: `maps:${y},${x}?q=${address}`,
     android: `geo:${y},${x}?q=${address}`
