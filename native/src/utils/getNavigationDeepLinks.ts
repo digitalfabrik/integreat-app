4a5e0141303e8a3888b8dff4044a380515daac70
Cmd190 <tonigaser@gmail.com>
2022-01-23 15:33:51 +0100

    Revert "IGAPP-806: Fixed some minor code style issues"
    This reverts commit ef3395e3fb0277d4f40b2c8a2349f2dde0166ab8.

--- native/src/utils/getNavigationDeepLinks.ts
+++ native/src/utils/getNavigationDeepLinks.ts
@@ -10,6 +10,7 @@ export const getNavigationDeepLinks = (location: LocationModel): string | null =
   const x = location.coordinates[0]
   const y = location.coordinates[1]
 
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   return Platform.select({
     ios: `maps:${y},${x}?q=${location.location}`,
     android: `geo:${y},${x}?q=${location.location}`
