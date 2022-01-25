cc156d2bf85e12df614793ee0f7529327cabeb9b
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-01-25 16:41:02 +0100

    IGAPP-261: Add comment and exclude deps on ios as well

--- native/react-native.config.js
+++ native/react-native.config.js
@@ -1,6 +1,8 @@
+// Exclude libraries from our builds by disabling autolinking
+// https://github.com/react-native-community/cli/blob/master/docs/autolinking.md#how-can-i-disable-autolinking-for-unsupported-library
 const floss = JSON.parse(process.env.BUILD_CONFIG_FEATURE_FLAGS_FLOSS ?? 'false')
 const developerFriendly = JSON.parse(process.env.BUILD_CONFIG_FEATURE_FLAGS_DEVELOPER_FRIENDLY)
-const excludeConfig = { platforms: { android: null } }
+const excludeConfig = { platforms: { android: null, ios: null } }
 
 const firebase = floss
   ? {
