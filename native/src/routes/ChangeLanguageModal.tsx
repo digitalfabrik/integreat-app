92043313207f19b9408e3fafbefd043f46517fd3
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-01-26 16:14:13 +0100

    IGAPP-882: Remove ChangeLanguageModalContainer

--- native/src/routes/ChangeLanguageModal.tsx
+++ native/src/routes/ChangeLanguageModal.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react'
-import { ReactElement } from 'react'
+import React, { ReactElement } from 'react'
 import { InteractionManager } from 'react-native'
+import { useDispatch } from 'react-redux'
 import styled from 'styled-components/native'
 
-import { ChangeLanguageModalRouteType, LanguageModel } from 'api-client'
+import { ChangeLanguageModalRouteType } from 'api-client'
 
 import Selector from '../components/Selector'
 import { NavigationPropType, RoutePropType } from '../constants/NavigationTypes'
@@ -12,17 +12,25 @@ import SelectorItemModel from '../models/SelectorItemModel'
 const Wrapper = styled.ScrollView`
   background-color: ${props => props.theme.colors.backgroundColor};
 `
+
 type PropsType = {
-  currentLanguage: string
-  languages: Array<LanguageModel>
-  availableLanguages: Array<string>
-  changeLanguage: (newLanguage: string) => void
   route: RoutePropType<ChangeLanguageModalRouteType>
   navigation: NavigationPropType<ChangeLanguageModalRouteType>
 }
 
-const ChangeLanguageModal = (props: PropsType): ReactElement => {
-  const { changeLanguage, navigation, languages, availableLanguages, currentLanguage } = props
+const ChangeLanguageModal = ({ navigation, route }: PropsType): ReactElement => {
+  const { currentLanguage, languages, availableLanguages, cityCode } = route.params
+  const dispatch = useDispatch()
+
+  const changeLanguage = (newLanguage: string) => {
+    dispatch({
+      type: 'SWITCH_CONTENT_LANGUAGE',
+      params: {
+        newLanguage,
+        city: cityCode
+      }
+    })
+  }
 
   const selectorItems = languages.map(({ code, name }) => {
     const isLanguageAvailable = availableLanguages.includes(code)
