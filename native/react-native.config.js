1c822a78b59acefb3f76de325c6ce61c59ba0835
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-01-26 13:21:17 +0100

    IGAPP-261: Add default value

--- native/react-native.config.js
+++ native/react-native.config.js
@@ -1,7 +1,7 @@
 // Exclude libraries from our builds by disabling autolinking
 // https://github.com/react-native-community/cli/blob/master/docs/autolinking.md#how-can-i-disable-autolinking-for-unsupported-library
 const floss = JSON.parse(process.env.BUILD_CONFIG_FEATURE_FLAGS_FLOSS ?? 'false')
-const developerFriendly = JSON.parse(process.env.BUILD_CONFIG_FEATURE_FLAGS_DEVELOPER_FRIENDLY)
+const developerFriendly = JSON.parse(process.env.BUILD_CONFIG_FEATURE_FLAGS_DEVELOPER_FRIENDLY ?? 'false')
 const excludeConfig = { platforms: { android: null, ios: null } }
 
 const firebase = floss
