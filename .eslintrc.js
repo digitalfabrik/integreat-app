9639d1e875ba5fb2ec25ae8b684d402d56ebdaa9
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-02-01 14:15:34 +0100

    IGAPP-910: Align linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,23 +32,24 @@ module.exports = {
   rules: {
     // Overly strict rules (for now)
     'class-methods-use-this': 'off',
-    'global-require': 'off',
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'lines-between-class-members': 'off',
     'no-shadow': 'off',
     'no-underscore-dangle': 'off',
     'react/display-name': 'off',
-    'react/jsx-filename-extension': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
     'react/jsx-props-no-spreading': 'off',
-    'react/require-default-props': 'off',
     'jest/no-mocks-import': 'off',
 
-    // Disabling since better @typescript-eslint rules available
+    // Unwanted
+    'lines-between-class-members': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'react/require-default-props': 'off',
+
+    // Disabling since better @typescript-eslint rules available or they make no sense for ts projects
     'default-case': 'off',
     'no-use-before-define': 'off',
+    'import/no-unresolved': 'off',
+    'react/jsx-filename-extension': 'off',
 
     // Disabling since our class components are legacy anyway
     'react/sort-comp': 'off',
@@ -92,7 +93,7 @@ module.exports = {
 
     'prefer-arrow/prefer-arrow-functions': 'error',
 
-    //  Facilitate to disable nesting
+    // Force labels to have an id is important to connect them with input/text fields for screen reader
     'styled-components-a11y/label-has-for': [
       'error',
       {
@@ -109,12 +110,15 @@ module.exports = {
   },
   overrides: [
     {
-      files: ['*.spec.{ts,tsx}', '**/__mocks__/*.ts'],
+      files: ['*.spec.{ts,tsx}', '**/__mocks__/*.{ts,tsx}', '**/testing/*.{ts,tsx}', 'jest.setup.ts', 'jest.config.ts'],
       rules: {
+        'global-require': 'off',
+        'lines-between-class-members': 'off',
         'no-console': 'off',
         'no-magic-numbers': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
+        'import/no-extraneous-dependencies': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'jsx-a11y/no-static-element-interactions': 'off'
       }
@@ -127,9 +131,10 @@ module.exports = {
       }
     },
     {
-      files: ['**/tools/**'],
+      files: ['**/tools/**', '**/translations/**', '**/e2e-tests/**'],
       rules: {
-        'no-console': 'off'
+        'no-console': 'off',
+        'import/no-extraneous-dependencies': 'off'
       }
     }
   ],
