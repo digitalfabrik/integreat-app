e7f9b2ef43a719574c201f21b9f88fd09afa5083
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-02-17 15:04:37 +0100

    IGAPP-906: Fix return type

--- web/src/components/Caption.tsx
+++ web/src/components/Caption.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react'
+import React, { ReactElement } from 'react'
 import styled from 'styled-components'
 
 import dimensions from '../constants/dimensions'
@@ -17,6 +17,6 @@ type PropsType = {
   title: string
 }
 
-const Caption = ({ title }: PropsType): ReactNode => <H1>{title}</H1>
+const Caption = ({ title }: PropsType): ReactElement => <H1>{title}</H1>
 
 export default Caption
