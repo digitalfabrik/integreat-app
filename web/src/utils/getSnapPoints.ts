cb8b79ab701c1848268236f6a8c9944a3487eff3
Andy <fischer@integreat-app.de>
2022-02-21 11:02:06 +0100

    IGAPP-901: readded sheet title adjusted small snappoint

--- web/src/utils/getSnapPoints.ts
+++ web/src/utils/getSnapPoints.ts
@@ -1,4 +1,4 @@
-const min = 0.05
+const min = 0.1
 const mid = 0.35
 const max = 0.65
 export const getSnapPoints = (maxHeight: number): number[] => [maxHeight * min, maxHeight * mid, maxHeight * max]
