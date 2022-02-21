0d590f23f54acdc8126a8e8e559c64a7715b11a5
Andy <fischer@integreat-app.de>
2022-02-21 11:33:14 +0100

    IGAPP-805: rm mock, mapped poi content for details page

--- native/src/components/PoiDetails.tsx
+++ native/src/components/PoiDetails.tsx
@@ -60,23 +60,6 @@ const PoiDetailsContainer = styled.View`
   flex: 1;
 `
 
-// TODO IGAPP-805 receive POI content from cms
-const mockContent = `<div id='cc-m-7871392056' class='j-module n j-text '><p>
-    <span style='font-size: 14px; color: #000000;'>Das Konzept, mit dem Sie, ohne Geld zu spenden, helfen können: Sicher haben Sie einiges zu Hause, was Sie nicht mehr brauchen. Geben Sie es
-    uns!</span>
-</p>
-
-<p >
-    <span style='font-size: 14px; color: #000000;'>Wir freuen uns über gespendete, gut erhaltene und saubere Kleidung, Bücher, Möbel, Hausrat, Elektrogeräte, Lampen usw. Wir machen auch
-    Haushaltsauflösungen.</span>
-</p>
-
-<p >
-    <span style='font-size: 14px; color: #000000;'>Mit den gespendeten Dingen schaffen wir günstige Einkaufsmöglichkeiten für sozial schwache und benachteiligte Menschen, aber auch für Leute, die
-    Spass an Second-Hand Waren haben und damit Umwelt und ihren Geldbeutel schonen wollen. Jeder, der bei uns einkauft, hilft uns, die Kosten zu decken und die Preise niedrig halten zu können. Nähere
-    Infos bei "<a href='/verein/' title='Verein'>Verein</a>" und "<a href='/konzept/' title='Konzept'>Konzept</a>"</span>
-</p></div>`
-
 const PoiDetails: React.FC<PoiDetailsProps> = ({
   poi,
   feature,
@@ -87,7 +70,7 @@ const PoiDetails: React.FC<PoiDetailsProps> = ({
   const { t } = useTranslation<['pois', 'error']>(['pois', 'error'])
   const showSnackbar = useSnackbar()
 
-  // TODO this has to be removed when we get proper images from CMS IGAPP-805
+  // TODO this has to be removed when we get proper images from CMS IGAPP-920
   const thumbnail = feature.properties.thumbnail?.replace('-150x150', '') ?? EventPlaceholder1
   const { location, address, postcode, town } = poi.location
   const { distance, title } = feature.properties
@@ -131,7 +114,7 @@ const PoiDetails: React.FC<PoiDetailsProps> = ({
           </Text>
         </PoiDetailItem>
         <CollapsibleItem initExpanded headerText={t('detailInformationHeader')} language={language}>
-          <NativeHtml content={mockContent} language={language} />
+          <NativeHtml content={poi.content} language={language} />
         </CollapsibleItem>
       </InformationContainer>
     </PoiDetailsContainer>
