a5cb0368b22ad11087eff4e427ca1d64b262b7ee
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-02-23 15:55:48 +0100

    IGAPP-597: Migrate tools scripts to ts

--- tools/move-release-notes.ts
+++ tools/move-release-notes.ts
@@ -1,19 +1,31 @@
-#!/usr/bin/env node
+import { program } from 'commander'
 
-const authenticate = require('./github-authentication')
-const { GITKEEP_FILE, UNRELEASED_DIR, RELEASE_NOTES_DIR } = require('./constants')
-const { program } = require('commander')
+import { GITKEEP_FILE, UNRELEASED_DIR, RELEASE_NOTES_DIR } from './constants'
+import authenticate from './github-authentication'
 
 program
   .option('-d, --debug', 'enable extreme logging')
-  .requiredOption('--deliverino-private-key <deliverino-private-key>', 'private key of the deliverino github app in pem format with base64 encoding')
+  .requiredOption(
+    '--deliverino-private-key <deliverino-private-key>',
+    'private key of the deliverino github app in pem format with base64 encoding'
+  )
   .requiredOption('--owner <owner>', 'owner of the current repository, usually "Integreat"')
   .requiredOption('--repo <repo>', 'the current repository, usually "integreat-app"')
   .requiredOption('--branch <branch>', 'the current branch')
 
-const moveReleaseNotes = async ({ newVersionName, deliverinoPrivateKey, owner, repo, branch }) => {
+type Options = {
+  newVersionName: string
+  deliverinoPrivateKey: string
+  owner: string
+  repo: string
+  branch: string
+}
+
+const moveReleaseNotes = async ({ newVersionName, deliverinoPrivateKey, owner, repo, branch }: Options) => {
   const appOctokit = await authenticate({ deliverinoPrivateKey, owner, repo })
-  const { data: { commit } } = await appOctokit.repos.getBranch({ owner, repo, branch })
+  const {
+    data: { commit }
+  } = await appOctokit.repos.getBranch({ owner, repo, branch })
 
   // Tree of the root folder of the project
   const rootTreeSha = commit.sha
@@ -21,7 +33,7 @@ const moveReleaseNotes = async ({ newVersionName, deliverinoPrivateKey, owner, r
 
   // Tree of the 'release-notes' folder of the project
   const releaseNoteTreeObject = rootTree.data.tree.find(it => it.path === RELEASE_NOTES_DIR)
-  if (!releaseNoteTreeObject) {
+  if (!releaseNoteTreeObject || !releaseNoteTreeObject.sha) {
     console.error(rootTree)
     throw new Error('Root tree does not contain release notes tree, probably since the response is truncated.')
   }
@@ -29,7 +41,7 @@ const moveReleaseNotes = async ({ newVersionName, deliverinoPrivateKey, owner, r
 
   // Tree of the unreleased release notes
   const unreleasedTreeObject = releaseNoteTree.data.tree.find(it => it.path === UNRELEASED_DIR)
-  if (!unreleasedTreeObject) {
+  if (!unreleasedTreeObject || !unreleasedTreeObject.sha) {
     console.error(releaseNoteTree)
     throw new Error('Release note tree does not contain unreleased tree, probably since the response is truncated.')
   }
@@ -39,12 +51,18 @@ const moveReleaseNotes = async ({ newVersionName, deliverinoPrivateKey, owner, r
   const unreleasedReleaseNotes = unreleasedTree.data.tree.filter(it => it.path !== GITKEEP_FILE)
   const keepFile = unreleasedTree.data.tree.find(it => it.path === GITKEEP_FILE)
 
+  if (!keepFile) {
+    throw new Error('.gitkeep file not found!')
+  }
+
   // Creating an empty tree is not possible
   if (unreleasedReleaseNotes.length === 0) {
     return
   }
 
+  // @ts-expect-error Wrong type: mode of type string is not compatible to "100644" | "100755" | "040000" | "160000" | "120000"
   const newVersionTree = await appOctokit.git.createTree({ owner, repo, tree: unreleasedReleaseNotes })
+  // @ts-expect-error Wrong type: mode of type string is not compatible to "100644" | "100755" | "040000" | "160000" | "120000"
   const newUnreleasedTree = await appOctokit.git.createTree({ owner, repo, tree: [keepFile] })
 
   const newReleaseNotesContent = releaseNoteTree.data.tree.map(it => {
@@ -59,6 +77,7 @@ const moveReleaseNotes = async ({ newVersionName, deliverinoPrivateKey, owner, r
   // See https://developer.github.com/v3/git/trees/#create-a-tree
   newReleaseNotesContent.push({ path: newVersionName, sha: newVersionTree.data.sha, type: 'tree', mode: '040000' })
 
+  // @ts-expect-error Wrong type: mode of type string is not compatible to "100644" | "100755" | "040000" | "160000" | "120000"
   const newReleaseNotesTree = await appOctokit.git.createTree({ owner, repo, tree: newReleaseNotesContent })
 
   // Update the root tree with the new 'release-notes' tree
@@ -69,6 +88,7 @@ const moveReleaseNotes = async ({ newVersionName, deliverinoPrivateKey, owner, r
     }
     return rest
   })
+  // @ts-expect-error Wrong type: mode of type string is not compatible to "100644" | "100755" | "040000" | "160000" | "120000"
   const updatedRootTree = await appOctokit.git.createTree({ owner, repo, tree: rootUpdate })
 
   // Commit the changes in the tree
@@ -90,10 +110,16 @@ const moveReleaseNotes = async ({ newVersionName, deliverinoPrivateKey, owner, r
 
 program
   .command('move-to <new-version-name>')
-  .description('move the release notes in \'unreleased\' to a new subdirectory <new-version-name>')
-  .action(async newVersionName => {
+  .description("move the release notes in 'unreleased' to a new subdirectory <new-version-name>")
+  .action(async (newVersionName: string) => {
     try {
-      await moveReleaseNotes({ newVersionName, ...program })
+      await moveReleaseNotes({
+        newVersionName,
+        deliverinoPrivateKey: program.deliverinoPrivateKey,
+        branch: program.branch,
+        owner: program.owner,
+        repo: program.repo
+      })
     } catch (e) {
       console.error(e)
       process.exit(1)
