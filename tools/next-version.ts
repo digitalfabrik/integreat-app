915867b0e6f5ad1f20de2b3023bb6b3452d8f8e0
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-02-23 15:55:48 +0100

    IGAPP-597: Migrate tools scripts to ts

--- tools/next-version.ts
+++ tools/next-version.ts
@@ -1,18 +1,17 @@
-#!/usr/bin/env node
+import { program } from 'commander'
+import fs from 'fs'
 
-const fs = require('fs')
-const { VERSION_FILE } = require('./constants')
-const { program } = require('commander')
+import { VERSION_FILE } from './constants'
 
 const calculateNewVersion = () => {
-  const versionFile = fs.readFileSync(VERSION_FILE)
+  const versionFile = fs.readFileSync(VERSION_FILE, 'utf-8')
   // versionCode is just used in the integreat-react-native-app
   const { versionName, versionCode } = JSON.parse(versionFile)
-  const versionNameParts = versionName.split('.').map(it => parseInt(it))
+  const versionNameParts = versionName.split('.').map((it: string) => parseInt(it, 10))
 
   const date = new Date()
   const year = date.getFullYear()
-  const month = (date.getMonth() + 1)
+  const month = date.getMonth() + 1
 
   const versionNameCounter = year === versionNameParts[0] && month === versionNameParts[1] ? versionNameParts[2] + 1 : 0
   const newVersionName = `${year}.${month}.${versionNameCounter}`
