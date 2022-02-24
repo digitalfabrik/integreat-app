719000878ab0f8429911eab2a0bdd82f5c842991
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-02-24 11:25:06 +0100

    IGAPP-718: Add listTitle to translations

--- web/src/routes/PoisPage.tsx
+++ web/src/routes/PoisPage.tsx
@@ -172,7 +172,7 @@ const PoisPage = ({ cityCode, languageCode, cityModel, pathname, languages }: Ci
           currentFeature={currentFeature}
         />
       )}
-      <BottomActionSheet title={currentFeature?.properties.title || t('sheetTitle')} ref={sheetRef}>
+      <BottomActionSheet title={currentFeature?.properties.title || t('listTitle')} ref={sheetRef}>
         {sortedPois.length > 0 && !currentFeature && (
           <ListWrapper>
             <List noItemsMessage={t('noPois')} items={sortedPois} renderItem={renderPoiListItem} borderless />
