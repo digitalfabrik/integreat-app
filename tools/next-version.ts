12c9a3b4f9ae806836500e8ac5a886a9ab11f32f
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-02-24 11:42:17 +0100

    IGAPP-597: Fix working directory

--- tools/next-version.ts
+++ tools/next-version.ts
@@ -1,10 +1,11 @@
 import { program } from 'commander'
 import fs from 'fs'
+import path from 'path'
 
 import { VERSION_FILE } from './constants'
 
 const calculateNewVersion = () => {
-  const versionFile = fs.readFileSync(`../${VERSION_FILE}`, 'utf-8')
+  const versionFile = fs.readFileSync(path.resolve(__dirname, '..', VERSION_FILE), 'utf-8')
   // versionCode is just used in the integreat-react-native-app
   const { versionName, versionCode } = JSON.parse(versionFile)
   const versionNameParts = versionName.split('.').map((it: string) => parseInt(it, 10))
