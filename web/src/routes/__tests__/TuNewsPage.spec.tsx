2b9c5c2d7582aedae3a5540a7b24c03f0a12d066
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-03-09 20:20:47 +0100

    IGAPP-944: Fix tests

--- web/src/routes/__tests__/TuNewsPage.spec.tsx
+++ web/src/routes/__tests__/TuNewsPage.spec.tsx
@@ -1,17 +1,14 @@
 import { mocked } from 'jest-mock'
-import moment from 'moment'
 import React from 'react'
 
 import {
   CityModelBuilder,
   LanguageModel,
   LanguageModelBuilder,
-  loadFromEndpoint,
   NEWS_ROUTE,
   pathnameFromRouteInformation,
   ReturnType,
   TU_NEWS_TYPE,
-  TunewsModel,
   useLoadFromEndpoint
 } from 'api-client'
 
@@ -25,7 +22,7 @@ jest.mock('api-client', () => ({
   useLoadFromEndpoint: jest.fn()
 }))
 jest.mock('react-i18next')
-jest.mock('../../components/InfiniteScrollList')
+jest.mock('../../components/InfiniteScrollList', () => () => 'List')
 jest.mock('../../components/LocationHeader', () => () => null)
 
 describe('TuNewsPage', () => {
@@ -38,27 +35,6 @@ describe('TuNewsPage', () => {
   const languages = new LanguageModelBuilder(3).build()
   const city = cities[0]!
   const language = tuNewsLanguages[0]!
-
-  const tuNews = [
-    new TunewsModel({
-      id: 0,
-      title: 'Tick bite - What to do?',
-      tags: ['8 Gesundheit'],
-      date: moment('2017-11-18T19:30:00.000Z'),
-      content:
-        'In summer there are often ticks in forest and meadows with high grass. These are very small animals. They feed on the blood of people or animals they sting, like mosquitoes. But they stay in the skin longer and can transmit dangerous diseases. If you have been in high grass, you should search your body very thoroughly for ticks. They like to sit in the knees, armpits or in the groin area. If you discover a tick in your skin, you should carefully pull it out with tweezers without crushing it. If the sting inflames, you must see a doctor. tünews INTERNATIONAL',
-      eNewsNo: 'tun0000009902'
-    }),
-    new TunewsModel({
-      id: 1,
-      title: 'Rasen und Falschparken wird teurer – der neue Bußgeldkatalog',
-      tags: [],
-      date: moment('2017-11-18T19:30:00.000Z'),
-      content:
-        'Zum 9. November 2021 ist ein neuer Bußgeldkatalog für den Straßenverkehr in Kraft getreten. Dieser sieht beispielsweise bei einer Überschreitung der zulässigen Geschwindigkeit um über 20 km/h in Ortschaften ein Bußgeld von 115 Euro vor, außerhalb von Ortschaften 100 Euro. Überschreitet man die Geschwindigkeit gar um mehr als 40 km/h, kostet dies in Ortschaften 400 Euro, außerhalb von Ortschaften 320 Euro. Ein zusätzliches Fahrverbot droht bei einer Überschreitung von mehr aus 30 km/h in Ortschaften und mehr als 40 km/h außerhalb von Ortschaften.',
-      eNewsNo: 'tun21110904'
-    })
-  ]
 
   const languagesReturn: ReturnType<LanguageModel[]> = {
     data: tuNewsLanguages,
@@ -92,19 +68,11 @@ describe('TuNewsPage', () => {
   }
 
   it('should render error if loading languages fails', () => {
-    mocked(loadFromEndpoint).mockImplementation(async (_, setData) => setData(tuNews))
     const { getByText } = renderTuNewsRoute(language, { ...languagesReturn, error: new Error('my lang error') })
     expect(getByText('error:unknownError')).toBeTruthy()
   })
 
-  it('should render error if loading tunews fails', () => {
-    mocked(loadFromEndpoint).mockImplementation(async (_, _unusedSetData, setError) => setError(new Error('my error')))
-    const { getByText } = renderTuNewsRoute()
-    expect(getByText('error:unknownError')).toBeTruthy()
-  })
-
   it('should render language failure if language is not available', () => {
-    mocked(loadFromEndpoint).mockImplementation(async (_, setData) => setData(tuNews))
     const { getAllByText, queryByText } = renderTuNewsRoute(languages[2]!)
     expect(getAllByText('error:notFound.language error:chooseALanguage')).toBeTruthy()
     // Available languages
@@ -119,11 +87,8 @@ describe('TuNewsPage', () => {
     expect(queryByText(languages[2]!.name)).toBeFalsy()
   })
 
-  it('should render tunews list', () => {
-    mocked(loadFromEndpoint).mockImplementation(async (_, setData) => setData(tuNews))
+  it('should render list', () => {
     const { getByText } = renderTuNewsRoute()
-    tuNews.forEach(news => {
-      expect(getByText(`${news.title} ${news.content}`)).toBeTruthy()
-    })
+    expect(getByText('List')).toBeTruthy()
   })
 })
