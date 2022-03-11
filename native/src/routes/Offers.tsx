83fe092d304b7910648850d1f3495a4f9ee85e3e
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-03-11 15:33:55 +0100

    IGAPP-948: Use offer to decide where the feature is enabled

--- native/src/routes/Offers.tsx
+++ native/src/routes/Offers.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react'
 import { TFunction } from 'react-i18next'
 import { View } from 'react-native'
 
-import { SPRUNGBRETT_OFFER_ROUTE, OfferModel } from 'api-client'
+import { OfferModel, SPRUNGBRETT_OFFER_ROUTE } from 'api-client'
 
 import SiteHelpfulBox from '../components/SiteHelpfulBox'
 import SpaceBetween from '../components/SpaceBetween'
@@ -17,23 +17,25 @@ type PropsType = {
   language: string
 }
 
-const toTileModels = (offer: Array<OfferModel>): Array<TileModel> =>
-  offer.map(offer => {
-    const isInternalExtra = [SPRUNGBRETT_OFFER_ROUTE as string].includes(offer.alias)
-    const path = isInternalExtra ? offer.alias : offer.path
-    return new TileModel({
-      title: offer.title,
-      path,
-      thumbnail: offer.thumbnail,
-      isExternalUrl: !isInternalExtra,
-      postData: offer.postData
-    })
-  })
-
 const Offers = ({ offers, navigateToFeedback, navigateToOffer, t, language }: PropsType): ReactElement => (
   <SpaceBetween>
     <View>
-      <Tiles title={t('offers')} tiles={toTileModels(offers)} onTilePress={navigateToOffer} language={language} />
+      <Tiles
+        title={t('offers')}
+        tiles={offers.map(offer => {
+          const isInternalExtra = [SPRUNGBRETT_OFFER_ROUTE as string].includes(offer.alias)
+          const path = isInternalExtra ? offer.alias : offer.path
+          return new TileModel({
+            title: t(offer.title),
+            path,
+            thumbnail: offer.thumbnail,
+            isExternalUrl: !isInternalExtra,
+            postData: offer.postData
+          })
+        })}
+        onTilePress={navigateToOffer}
+        language={language}
+      />
     </View>
     <SiteHelpfulBox navigateToFeedback={navigateToFeedback} />
   </SpaceBetween>
