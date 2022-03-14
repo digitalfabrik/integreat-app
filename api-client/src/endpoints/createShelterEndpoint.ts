83c3a8ab28a62d9bc38d7a5502c57b6221adaedb
Andy <fischer@integreat-app.de>
2022-03-14 11:10:30 +0100

    IGAPP-951: refactor queryString for filters

--- api-client/src/endpoints/createShelterEndpoint.ts
+++ api-client/src/endpoints/createShelterEndpoint.ts
@@ -8,7 +8,7 @@ const SHELTER_URL = 'wohnraum.tuerantuer.org/wp-json/accommodations'
 export const SHELTER_ENDPOINT_NAME = 'shelter'
 type Params =
   | { type: 'detail'; id: string; cityCode: string }
-  | { type: 'list'; page: number; cityCode: string; filter?: FilterProps }
+  | { type: 'list'; page: number; cityCode: string; filter: FilterProps }
 
 export type FilterProps = {
   beds: string | null
@@ -21,6 +21,7 @@ const paramMap = new Map([
   ['pets', 'pets']
 ])
 
+// add a key and value as a string for each param
 const getFilterQueryParams = (params: FilterProps): string =>
   Object.keys(params)
     .map((key: string) => {
@@ -36,8 +37,7 @@ export default (): Endpoint<Params, ShelterModel[]> =>
     .withParamsToUrlMapper(params => {
       const baseUrl = `https://${params.cityCode}.${SHELTER_URL}`
       if (params.type === 'list') {
-        const filterParams = params.filter ? `${getFilterQueryParams(params.filter)}` : ''
-        return `${baseUrl}/list?page=${params.page}${filterParams}`
+        return `${baseUrl}/list?page=${params.page}${getFilterQueryParams(params.filter)}`
       }
       return `${baseUrl}/${params.id}`
     })
