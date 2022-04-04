a5ca32e43eca02129e50ee9f6efb8f8bbb074d7f
Michael Markl <marklmichael98@gmail.com>
2022-04-04 11:37:33 +0200

    IGAPP-974: Run prettier

--- web/src/hooks/useWindowDimensions.ts
+++ web/src/hooks/useWindowDimensions.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import dimensions from '../constants/dimensions';
+
+import dimensions from '../constants/dimensions'
 
 type WindowDimensionsType = { width: number; height: number; viewportSmall: boolean }
 
