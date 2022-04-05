2cc4677a7ec86b0037bc72e7c93c45eeaa215aba
Andy <fischer@integreat-app.de>
2022-04-05 10:30:20 +0200

    IGAPP-902: update PoisPage Test

--- web/src/routes/__tests__/PoisPage.spec.tsx
+++ web/src/routes/__tests__/PoisPage.spec.tsx
@@ -1,4 +1,6 @@
+import { fireEvent } from '@testing-library/react'
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 
 import { cityContentPath, CityModelBuilder, LanguageModelBuilder, PoiModelBuilder, POIS_ROUTE } from 'api-client'
 import {
@@ -7,15 +9,20 @@ import {
 } from 'api-client/src/testing/mockUseLoadFromEndpoint'
 
 import { mockGeolocationSuccess } from '../../__mocks__/geoLocation'
-import { renderRoute } from '../../testing/render'
+import buildConfig from '../../constants/buildConfig'
+import { renderWithRouter } from '../../testing/render'
 import PoisPage from '../PoisPage'
-import { RoutePatterns } from '../index'
 
 jest.mock('api-client', () => ({
   ...jest.requireActual('api-client'),
   useLoadFromEndpoint: jest.fn()
 }))
-jest.mock('react-i18next')
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    t: (key: string) => key
+  })
+}))
 
 // @ts-expect-error -- ignore readOnly var
 navigator.geolocation = mockGeolocationSuccess
@@ -24,7 +31,6 @@ describe('PoisPage', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
-
   const languages = new LanguageModelBuilder(2).build()
   const cities = new CityModelBuilder(2).build()
   const pois = new PoiModelBuilder(2).build()
@@ -33,11 +39,10 @@ describe('PoisPage', () => {
   const poi0 = pois[0]!
   const poi1 = pois[1]!
 
-  const routePattern = `/:cityCode/:languageCode/${RoutePatterns[POIS_ROUTE]}`
+  const pathname = cityContentPath({ route: POIS_ROUTE, cityCode: city.code, languageCode: language.code })
 
-  const renderPois = ({ id }: { id?: string } = {}) => {
-    const pathname = cityContentPath({ route: POIS_ROUTE, cityCode: city.code, languageCode: language.code, path: id })
-    return renderRoute(
+  const renderPois = (
+    <ThemeProvider theme={buildConfig().lightTheme}>
       <PoisPage
         cities={cities}
         cityModel={city}
@@ -46,38 +51,30 @@ describe('PoisPage', () => {
         pathname={pathname}
         languageCode={language.code}
         cityCode={city.code}
-      />,
-      { routePattern, pathname, wrapWithTheme: true, childRoute: ':poiId' }
-    )
-  }
+      />
+    </ThemeProvider>
+  )
 
   it('should render a list with all pois', () => {
     mockUseLoadFromEndpointWithData(pois)
-    const { getByText } = renderPois()
+    const { getByText } = renderWithRouter(renderPois)
     expect(getByText(poi0.location.name)).toBeTruthy()
     expect(getByText(poi1.location.name)).toBeTruthy()
   })
 
-  it('should render a page with poi information', () => {
-    mockUseLoadFromEndpointWithData(pois)
-    const { getByText } = renderPois({ id: 'test_path_2' })
-
-    expect(getByText(poi1.title)).toBeTruthy()
-    expect(getByText(poi1.content)).toBeTruthy()
-    expect(getByText(poi1.location.location!)).toBeTruthy()
-  })
-
-  it('should render a not found error', () => {
-    mockUseLoadFromEndpointWithData(pois)
-    const { getByText } = renderPois({ id: 'invalid' })
-
-    expect(getByText('error:notFound.poi')).toBeTruthy()
-  })
-
   it('should render an error', () => {
     mockUseLoadFromEndpointWithError('Something went wrong')
-    const { getByText } = renderPois({ id: 'test_path_2' })
-
+    const { getByText } = renderWithRouter(renderPois)
     expect(getByText('error:unknownError')).toBeTruthy()
+  })
+
+  it('should render poi details page when list item was clicked', () => {
+    mockUseLoadFromEndpointWithData(pois)
+    const { getByText, getByLabelText, debug } = renderWithRouter(renderPois)
+    fireEvent.click(getByLabelText(poi0.location.name))
+    expect(getByText(poi0.location.name)).toBeTruthy()
+    expect(getByText(poi0.location.address!)).toBeTruthy()
+    expect(getByText(poi0.content)).toBeTruthy()
+    debug()
   })
 })
