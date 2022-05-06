755f553cc63648673e674c18195bb6f0e640f87a
Sarah Sporck <sporck@integreat-app.de>
2022-05-06 12:11:16 +0200

    IGAPP-941: readd emails

--- api-client/src/replaceLinks.ts
+++ api-client/src/replaceLinks.ts
@@ -1,10 +1,11 @@
 // https://stackoverflow.com/a/3809435
-const protocol = /([A-Za-z]{3,9}:(?:\/\/)?)/g
-const baseUrl =  /(?:[-;:&=+$,\w]+@)?([A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*)/g
-const path = /(?:\/(?:[+~%/.\w-]*[+~%/\w-]+)?)?/g
-const query = /(?:\?([-+=&;%@.\w]*[-+=&;%@\w]+)?)?/g
-const hash = /(?:#([.!/\\\w]*[!/\\\w]+)?)?/g
-const regex = new RegExp(protocol.source + baseUrl.source + path.source + query.source + hash.source, "g")
+const protocol = /([A-Za-z]{3,9}:(?:\/\/)?)/
+const baseUrl =  /(?:[-;:&=+$,\w]+@)?([A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*)/
+const path = /(?:\/(?:[+~%/.\w-]*[+~%/\w-]+)?)?/
+const query = /(?:\?([-+=&;%@.\w]*[-+=&;%@\w]+)?)?/
+const hash = /(?:#([.!/\\\w]*[!/\\\w]+)?)?/
+const mail = /[.\-;:&=+$,\w]+@([A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*)/
+const regex = new RegExp(`(${protocol.source}${baseUrl.source}${path.source}${query.source}${hash.source}|${mail.source})`, "g")
 type ReplaceType = (match: string) => string
 export const linkify = (link: string): string => `<a href="${link}">${link}</a>`
 export const replaceLinks = (content: string, replace: ReplaceType = linkify): string => content.replace(regex, replace)
