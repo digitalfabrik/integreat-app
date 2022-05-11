45813441effd79b63834d391e447ffbe47348552
Sarah Sporck <sporck@integreat-app.de>
2022-05-11 10:51:49 +0200

    IGAPP-941: prettier

--- api-client/src/replaceLinks.ts
+++ api-client/src/replaceLinks.ts
@@ -1,11 +1,14 @@
 // https://stackoverflow.com/a/3809435
 const protocol = /([A-Za-z]{3,9}:(?:\/\/)?)/
-const hostname =  /(?:[-;:&=+$,\w]+@)?([A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*)/
+const hostname = /(?:[-;:&=+$,\w]+@)?([A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*)/
 const path = /(?:\/(?:[+~%/.\w-]*[+~%/\w-]+)?)?/
 const query = /(?:\?([-+=&;%@.\w]*[-+=&;%@\w]+)?)?/
 const hash = /(?:#([.!/\\\w]*[!/\\\w]+)?)?/
 const mail = /[.\-;:&=+$,\w]+@([A-Za-z0-9-]+(?:\.[A-Za-z0-9-]+)*)/
-const regex = new RegExp(`(${protocol.source}${hostname.source}${path.source}${query.source}${hash.source}|${mail.source})`, "g")
+const regex = new RegExp(
+  `(${protocol.source}${hostname.source}${path.source}${query.source}${hash.source}|${mail.source})`,
+  'g'
+)
 type ReplaceType = (match: string) => string
 export const linkify = (link: string): string => `<a href="${link}">${link}</a>`
 export const replaceLinks = (content: string, replace: ReplaceType = linkify): string => content.replace(regex, replace)
