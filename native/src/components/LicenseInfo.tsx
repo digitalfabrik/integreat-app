5ca930346df826729d705394e994b63b1ac97fea
Cmd190 <tonigaser@gmail.com>
2022-06-01 15:25:11 +0200

    IGAPP-182: trying to implement diplaying of licenses on native

--- native/src/components/LicenseInfo.tsx
+++ native/src/components/LicenseInfo.tsx
@@ -1,14 +1,16 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { FlatList, View } from 'react-native'
+import { Button } from 'react-native-elements/dist/buttons/Button'
+import { Item } from 'react-navigation-header-buttons'
 
-export interface ILicense {
+export type ILicense = {
   licenses: string
   repository: string
   licenseUrl: string
   parents: string
 }
 
-interface IFinalLicense {
+type IFinalLicense = {
   name: string
   version: string
   licenseSpecs: ILicense
@@ -16,30 +18,39 @@ interface IFinalLicense {
 
 const LicenseInfo = (): ReactElement => {
   // TODO import file with try catch
-  let licenses: ILicense[] | null = null
-  licenses = require('../../../../licenses.json')
+  const [licenses, setLicenses] = useState<ILicense[] | null>(null)
+  useEffect(() => {
+    try {
+      const license_file = require('../../../../assets/licenses_native.json')
+      console.log('test', license_file)
+      setLicenses(license_file)
+    } catch (error) {
+      console.log('error when loading licenses.json')
+    }
+  }, [])
   const numberRegex = /\d+(\.\d+)*/
   const atRegex = /(?:@)/gi
 
-  const finalLicense: IFinalLicense[] = []
-  licenses?.map(license => {
-    // Extract the version of the library from the name
-    // const version = license. .match(numberRegex);
-    console.log('license info')
-    console.log(`${license.licenses}`)
-    console.log(`${license.repository}`)
-
-    // Removes the part after the @
-    // const nameWithoutVersion = libraryName.replace(atRegex, '').replace(version ? version[0] : '', '');
-    finalLicense.push({ name: 'nameWithoutVersion', version: '', licenseSpecs: license })
-  })
+  const finalLicenses: IFinalLicense[] = []
+  //   licenses?.map(license => {
+  //     // Extract the version of the library from the name
+  //     // const version = license. .match(numberRegex);
+  //     console.log('license info')
+  //     console.log(`${license.licenses}`)
+  //     console.log(`${license.repository}`)
+  // return null
+  //     // Removes the part after the @
+  //     // const nameWithoutVersion = libraryName.replace(atRegex, '').replace(version ? version[0] : '', '');
+  //     finalLicenses.push({ name: 'nameWithoutVersion', version: '', licenseSpecs: license })
+  //   })
 
   return (
-    <View style={styles.root}>
+    <View>
+      {/* <Button title='Test' onPress={() => {}} /> */}
       <FlatList
-        data={finalLicense}
-        renderItem={renderItem}
-        keyExtractor={item => item.id}
+        data={finalLicenses}
+        renderItem={item => <Item title={item.item.name} />}
+        keyExtractor={item => item.name}
         // keyboardShouldPersistTaps={'handled'}
         // data={finalLicense}
         // contentContainerStyle={styles.listContainer}
