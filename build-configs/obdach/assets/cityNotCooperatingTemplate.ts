9e77fb2b8ff4af3ce1d0f74fd43ec3329e10f7e4
Andy <fischer@integreat-app.de>
2022-06-01 16:01:23 +0200

    IGAPP-1013: resolve issues

--- build-configs/obdach/assets/cityNotCooperatingTemplate.ts
+++
@@ -1,9 +0,0 @@
-const template =
-  'Sehr geehrte Damen und Herren,\n\n' +
-  'Ich wohne seit einiger Zeit in [Ort] und habe neulich versucht, meinen Ort in der Integreat-App zu finden. Die App stellt zahlreiche wichtige Informationen über den Alltag und die Region in verschiedenen Sprachen zur Verfügung. Das erleichtert vielen Neuzugezogenen die Eingewöhnung, was ich sehr hilfreich finde.\n\n' +
-  'Ich würde mich daher sehr freuen, wenn auch Sie zukünftig bei Integreat mitmachen.\n\n\n\n' +
-  'Auf der Webseite erfahren Sie mehr:\n\n' +
-  'https://integreat-app.de\n\n\n\n' +
-  'Herzliche Grüße'
-
-export default template
