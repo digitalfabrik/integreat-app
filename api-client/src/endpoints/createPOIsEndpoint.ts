d549ff2dfc94771de6b842c60d92452a371c63fc
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-06-25 13:13:45 +0200

    IGAPP-985: Remove unnecessary check

--- api-client/src/endpoints/createPOIsEndpoint.ts
+++ api-client/src/endpoints/createPOIsEndpoint.ts
@@ -20,33 +20,31 @@ export default (baseUrl: string): Endpoint<ParamsType, Array<PoiModel>> =>
     )
     .withMapper(
       (json: Array<JsonPoiType>): Array<PoiModel> =>
-        json
-          .filter(poi => poi.location.id && poi.location.name && poi.location.latitude && poi.location.longitude)
-          .map(
-            poi =>
-              new PoiModel({
-                path: poi.path,
-                title: poi.title,
-                content: poi.content,
-                thumbnail: poi.thumbnail,
-                availableLanguages: mapAvailableLanguages(poi.available_languages),
-                excerpt: poi.excerpt,
-                website: poi.website,
-                phoneNumber: poi.phone_number,
-                email: poi.email,
-                location: new LocationModel({
-                  id: poi.location.id,
-                  name: poi.location.name,
-                  address: poi.location.address,
-                  town: poi.location.town,
-                  postcode: poi.location.postcode,
-                  country: poi.location.country,
-                  latitude: poi.location.latitude,
-                  longitude: poi.location.longitude
-                }),
-                lastUpdate: moment.tz(poi.modified_gmt, 'GMT'),
-                hash: poi.hash
-              })
-          )
+        json.map(
+          poi =>
+            new PoiModel({
+              path: poi.path,
+              title: poi.title,
+              content: poi.content,
+              thumbnail: poi.thumbnail,
+              availableLanguages: mapAvailableLanguages(poi.available_languages),
+              excerpt: poi.excerpt,
+              website: poi.website,
+              phoneNumber: poi.phone_number,
+              email: poi.email,
+              location: new LocationModel({
+                id: poi.location.id,
+                name: poi.location.name,
+                address: poi.location.address,
+                town: poi.location.town,
+                postcode: poi.location.postcode,
+                country: poi.location.country,
+                latitude: poi.location.latitude,
+                longitude: poi.location.longitude
+              }),
+              lastUpdate: moment.tz(poi.modified_gmt, 'GMT'),
+              hash: poi.hash
+            })
+        )
     )
     .build()
