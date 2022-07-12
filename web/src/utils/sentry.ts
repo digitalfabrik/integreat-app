affed9d5bc41e6facaddc4ae0f23811a8bae99ef
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-07-12 17:15:47 +0200

    IGAPP-1008: Upgrade web dependencies

--- web/src/utils/sentry.ts
+++ web/src/utils/sentry.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import type Sentry from '@sentry/react'
+import { SeverityLevel } from '@sentry/types'
 
 import { FetchError, NotFoundError } from 'api-client'
 
@@ -32,32 +33,28 @@ export const initSentry = async (): Promise<void> => {
   }
 }
 
-export const log = async (message: string, level = 'debug'): Promise<void> => {
+export const log = async (message: string, level: SeverityLevel = 'debug'): Promise<void> => {
   try {
     const Sentry = await loadSentry()
     if (sentryEnabled()) {
-      Sentry.addBreadcrumb({
-        message,
-        level: Sentry.Severity.fromString(level)
-      })
+      Sentry.addBreadcrumb({ message, level })
     }
     if (developerFriendly()) {
       switch (level) {
-        case Sentry.Severity.Fatal:
-        case Sentry.Severity.Critical:
-        case Sentry.Severity.Error:
+        case 'fatal':
+        case 'error':
           console.error(message)
           break
-        case Sentry.Severity.Warning:
+        case 'warning':
           console.warn(message)
           break
-        case Sentry.Severity.Log:
+        case 'log':
           console.log(message)
           break
-        case Sentry.Severity.Info:
+        case 'info':
           console.info(message)
           break
-        case Sentry.Severity.Debug:
+        case 'debug':
           console.debug(message)
           break
       }
