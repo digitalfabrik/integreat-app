7f15f0f4b47f8a255b81235a525ad139c10e1e11
Jan <75899673+Cmd190@users.noreply.github.com>
2022-07-14 13:04:04 +0200

    IGAPP-182: Renaming
    Co-authored-by: Steffen Kleinle <steffen.kleinle@mailbox.org>

--- api-client/src/routes/index.ts
+++ api-client/src/routes/index.ts
@@ -39,7 +39,7 @@ export type JpalTrackingRouteType = 'jpal'
 export const JPAL_TRACKING_ROUTE: JpalTrackingRouteType = 'jpal'
 
 export type LicenseRouteType = 'licenses'
-export const LICENSE_INFO_ROUTE: LicenseRouteType = 'licenses'
+export const LICENSE_ROUTE: LicenseRouteType = 'licenses'
 
 // News types
 export type LocalNewsType = 'local'
