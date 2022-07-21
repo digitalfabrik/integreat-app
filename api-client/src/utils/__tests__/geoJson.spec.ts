9ac31e7760ee7a6d2bad1d80833e3cddc7059e43
Andy <fischer@integreat-app.de>
2022-07-21 11:50:40 +0200

    IGAPP-1028: fix tests

--- api-client/src/utils/__tests__/geoJson.spec.ts
+++ api-client/src/utils/__tests__/geoJson.spec.ts
@@ -14,7 +14,7 @@ describe('geoJson', () => {
     properties: {
       id: 1,
       title: 'Test Title',
-      symbol: 'marker_15',
+      symbol: 'marker_40',
       thumbnail: 'test',
       path: '/augsburg/de/locations/test',
       urlSlug: 'test',
