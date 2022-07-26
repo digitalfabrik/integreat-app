225a079000482fc135a9ce97cf1b6b26a98a8592
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-07-26 09:52:24 +0200

    IGAPP-1008: Export icons from index

---
+++ web/src/assets/index.ts
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-relative-packages */
+import AccessibleIcon from '../../../assets/icons/accessible.svg'
+import BathroomIcon from '../../../assets/icons/bathroom.svg'
+import BedIcon from '../../../assets/icons/bed.svg'
+import CalendarIcon from '../../../assets/icons/calendar.svg'
+import EmailIcon from '../../../assets/icons/email.svg'
+import EuroIcon from '../../../assets/icons/euro.svg'
+import HouseIcon from '../../../assets/icons/house.svg'
+import KeyIcon from '../../../assets/icons/key.svg'
+import LgbtqiIcon from '../../../assets/icons/lgbtqi.svg'
+import PetIcon from '../../../assets/icons/pet.svg'
+import PhoneIcon from '../../../assets/icons/phone.svg'
+import SmokingIcon from '../../../assets/icons/smoking.svg'
+import TimerIcon from '../../../assets/icons/timer.svg'
+import WebsiteIcon from '../../../assets/icons/website.svg.svg'
+
+export {
+  AccessibleIcon,
+  BathroomIcon,
+  BedIcon,
+  CalendarIcon,
+  EmailIcon,
+  EuroIcon,
+  HouseIcon,
+  KeyIcon,
+  LgbtqiIcon,
+  PetIcon,
+  PhoneIcon,
+  SmokingIcon,
+  TimerIcon,
+  WebsiteIcon
+}
