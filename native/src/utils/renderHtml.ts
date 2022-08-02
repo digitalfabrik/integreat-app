38c9d7270e07ed8a9678104939720f38a783a5fb
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-08-02 13:54:27 +0200

    IGAPP-983: Remove unused import

--- native/src/utils/renderHtml.ts
+++ native/src/utils/renderHtml.ts
@@ -1,5 +1,4 @@
 import { ThemeType } from 'build-configs'
-import { config } from 'translations'
 
 import { ParsedCacheDictionaryType } from '../components/Page'
 import { ERROR_MESSAGE_TYPE, getFontFaceSource, HEIGHT_MESSAGE_TYPE, WARNING_MESSAGE_TYPE } from '../constants/webview'
