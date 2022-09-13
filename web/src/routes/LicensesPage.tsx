2e7f0260c0032aaddc978f59d25c7afab4a9ce6d
Andy <fischer@integreat-app.de>
2022-09-13 13:55:28 +0200

    IGAPP-1035: add general footer & header for licenses page, fix key issue, add null condition for empty KebabMenu

--- web/src/routes/LicensesPage.tsx
+++ web/src/routes/LicensesPage.tsx
@@ -1,20 +1,22 @@
 import React, { ReactElement, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { License, LICENSES_ROUTE, parseLicenses, pathnameFromRouteInformation } from 'api-client'
+import { License, parseLicenses } from 'api-client'
 
-import { CityRouteProps } from '../CityContentSwitcher'
 import Caption from '../components/Caption'
+import GeneralFooter from '../components/GeneralFooter'
+import GeneralHeader from '../components/GeneralHeader'
+import Layout from '../components/Layout'
 import LicenseItem from '../components/LicenseItem'
 import List from '../components/List'
-import LocationLayout from '../components/LocationLayout'
 import useWindowDimensions from '../hooks/useWindowDimensions'
 import { reportError } from '../utils/sentry'
 
-const LicensesPage = ({ languageCode, pathname, cityModel, languages, cityCode }: CityRouteProps): ReactElement => {
+type PropsType = { languageCode: string }
+const LicensesPage = ({ languageCode }: PropsType): ReactElement => {
   const { t } = useTranslation(['settings', 'licenses'])
-  const [licenses, setLicenses] = useState<License[] | null>(null)
   const { viewportSmall } = useWindowDimensions()
+  const [licenses, setLicenses] = useState<License[] | null>(null)
 
   useEffect(() => {
     import('../../assets/licenses.json')
@@ -23,29 +25,22 @@ const LicensesPage = ({ languageCode, pathname, cityModel, languages, cityCode }
   }, [])
 
   const renderItem = (item: License) => (
-    <LicenseItem name={item.name} license={item.licenses} version={item.version} onPress={item.licenseUrl} />
+    <LicenseItem
+      key={item.name}
+      name={item.name}
+      license={item.licenses}
+      version={item.version}
+      onPress={item.licenseUrl}
+    />
   )
 
-  const languageChangePaths = languages.map(({ code, name }) => {
-    const disclaimerPath = pathnameFromRouteInformation({ route: LICENSES_ROUTE, cityCode, languageCode: code })
-    return { path: disclaimerPath, name, code }
-  })
-
-  const locationLayoutParams = {
-    cityModel,
-    viewportSmall,
-    languageChangePaths,
-    feedbackTargetInformation: null,
-    route: LICENSES_ROUTE,
-    languageCode,
-    pathname,
-  }
-
   return (
-    <LocationLayout isLoading={false} {...locationLayoutParams}>
+    <Layout
+      header={<GeneralHeader languageCode={languageCode} viewportSmall={viewportSmall} />}
+      footer={<GeneralFooter language={languageCode} />}>
       <Caption title={t('settings:openSourceLicenses')} />
       <List items={licenses ?? []} renderItem={renderItem} noItemsMessage={t('licenses:noLicensesMessage')} />
-    </LocationLayout>
+    </Layout>
   )
 }
 
