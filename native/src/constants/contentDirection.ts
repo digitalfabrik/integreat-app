1d085648227261871536376688d0aa6973738093
Andy <fischer@integreat-app.de>
2022-09-14 14:57:17 +0200

    IGAPP-1079: add regex fkt to calculate rtl/ltr alignment of texts, add base text component that is content aware, replaced Text component for ListItem and NewsListItem

--- native/src/constants/contentDirection.ts
+++ native/src/constants/contentDirection.ts
@@ -9,3 +9,15 @@ export const contentDirection = (contentLanguage: string): 'row' | 'row-reverse'
   isContentDirectionReversalRequired(contentLanguage) ? 'row-reverse' : 'row'
 export const contentAlignment = (contentLanguage: string): 'right' | 'left' =>
   isContentDirectionReversalRequired(contentLanguage) ? 'right' : 'left'
+export const isRTLText = (text: string): boolean => {
+  const rtlCount = (text.match(/[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/g) || []).length
+  // eslint-disable-next-line no-misleading-character-class
+  const ltrCount = (
+    text.match(
+      // eslint-disable-next-line no-misleading-character-class
+      /[A-Za-z\u00C0-\u00C0\u00D8-\u00F6\u00F8-\u02B8\u0300-\u0590\u0800-\u1FFF\u2C00-\uFB1C\uFDFE-\uFE6F\uFEFD-\uFFFF]/g
+    ) || []
+  ).length
+  return rtlCount > ltrCount
+}
+export const contentAlignmentRTLText = (text: string): 'right' | 'left' => (isRTLText(text) ? 'right' : 'left')
