de3cf5ed97f42682e4e5b854b5cb85e3df087229
Andy <fischer@integreat-app.de>
2022-10-04 11:20:09 +0200

    IGAPP-1125: update theme colors and location-marker

--- build-configs/obdach/theme/colors.ts
+++ build-configs/obdach/theme/colors.ts
@@ -1,7 +1,7 @@
 import { ColorsType, commonLightColors } from '../../common/theme/colors'
 
-const themeColor = '#fbda16'
-const themeColorLight = 'rgba(251, 218, 22, 0.5)'
+const themeColor = '#C2D440'
+const themeColorLight = 'rgba(194,212,64, 0.5)'
 
 export const lightColors: ColorsType = {
   themeColor,
