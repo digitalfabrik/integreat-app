f896039c4f95bd080a25729e8f58d0fa0ceacf70
Sarah Sporck <sporck@integreat-app.de>
2022-10-05 16:20:01 +0200

    IGAPP-1126: Add overflow-wrap:anywhere

--- web/src/components/SprungbrettListItem.tsx
+++ web/src/components/SprungbrettListItem.tsx
@@ -1,23 +1,23 @@
 import * as React from 'react'
-import { ReactNode } from 'react'
+import { ReactElement } from 'react'
+import styled from 'styled-components'
 
 import { SprungbrettJobModel } from 'api-client'
 
 import ListItem from './ListItem'
 
+const Content = styled.div`
+  overflow-wrap: anywhere;
+`
+
 type PropsType = {
   job: SprungbrettJobModel
 }
 
-class SprungbrettListItem extends React.PureComponent<PropsType> {
-  render(): ReactNode {
-    const { job } = this.props
-    return (
-      <ListItem title={job.title} path={job.url}>
-        <div dir='auto'>{job.location}</div>
-      </ListItem>
-    )
-  }
-}
+const SprungbrettListItem = ({ job }: PropsType): ReactElement => (
+  <ListItem title={job.title} path={job.url}>
+    <Content dir='auto'>{job.location}</Content>
+  </ListItem>
+)
 
 export default SprungbrettListItem
