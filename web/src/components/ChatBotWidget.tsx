f6af9270bac77a72228c79bdeef3b2145b3c5779
LeandraCL <leandra.hahn@crosslend.com>
2022-10-05 12:25:47 +0200

    IGAPP-1120 Fix Chat Widget Styling for Scrollbar

--- web/src/components/ChatBotWidget.tsx
+++ web/src/components/ChatBotWidget.tsx
@@ -1,5 +1,11 @@
 import React, { ReactElement, useEffect } from 'react'
-import { useTheme } from 'styled-components'
+import styled, { useTheme } from 'styled-components'
+
+const ChatContainer = styled.div`
+  > div {
+    right: calc(100% - 100vw + 33px);
+  }
+`
 
 const ChatBotWidget = (): ReactElement => {
   const theme = useTheme()
@@ -22,12 +28,16 @@ const ChatBotWidget = (): ReactElement => {
   }, [])
 
   return (
-    <div
-      id='rasa-chat-widget'
-      data-primary={theme.colors.themeColor}
-      data-primary-highlight={theme.colors.themeColor}
-      data-websocket-url='https://integreat-demo.translatorswithoutborders.org/'
-    />
+    <>
+      <ChatContainer id='rasa-container' />
+      <div
+        id='rasa-chat-widget'
+        data-root-element-id='rasa-container'
+        data-primary={theme.colors.themeColor}
+        data-primary-highlight={theme.colors.themeColor}
+        data-websocket-url='https://integreat-demo.translatorswithoutborders.org/'
+      />
+    </>
   )
 }
 
