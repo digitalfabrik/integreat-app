1d5fb409ece2617ebeee48f0efdabf5daa6622ff
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-10-07 14:04:55 +0200

    IGAPP-1064: Proposal for refactoring

--- web/src/hooks/useRefWithCallback.ts
+++ web/src/hooks/useRefWithCallback.ts
@@ -1,23 +1,21 @@
-import { RefObject, useCallback, useRef, useState } from 'react'
+import { RefObject, useCallback, useRef } from 'react'
 
 type UseRefWithCallbackReturnProps<T> = {
-  /** indicator for useEffect to check if node has changed, since ref.current is unsafe */
-  toggle: boolean
-  /** the callback of the ref to pass to the html container */
-  refCallback: (node: T | null) => void
-  /** refObject */
-  ref: RefObject<T>
+  current: T | null
+  ref: (node: T | null) => void
 }
 
-const useRefWithCallback = <
-  T extends HTMLSpanElement | HTMLDivElement | HTMLParagraphElement
->(): UseRefWithCallbackReturnProps<T> => {
-  const [toggle, setToggle] = useState<boolean>(false)
+const useRefWithCallback = <T extends HTMLSpanElement | HTMLDivElement | HTMLParagraphElement>(
+  callback: (ref: RefObject<T>) => void
+): UseRefWithCallbackReturnProps<T> => {
   const ref = useRef<T | null>(null)
-  const refCallback = useCallback(node => {
-    ref.current = node
-    setToggle(val => !val)
-  }, [])
-  return { toggle, refCallback, ref }
+  const refCallback = useCallback(
+    node => {
+      ref.current = node
+      callback(ref)
+    },
+    [callback]
+  )
+  return { ref: refCallback, current: ref.current }
 }
 export default useRefWithCallback
