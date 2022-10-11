09a622a9f32c70c06298be8e271283f7c999e180
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-10-11 13:36:20 +0200

    IGAPP-1119: Rename var

--- api-client/src/utils/searchCategories.ts
+++ api-client/src/utils/searchCategories.ts
@@ -9,20 +9,20 @@ type Result = {
 }
 
 const searchCategories = (categoriesMapModel: CategoriesMapModel, query: string): Result[] => {
-  const normalizedFilter = normalizeSearchString(query)
+  const normalizedQuery = normalizeSearchString(query)
   const categories = categoriesMapModel.toArray().filter(it => !it.isRoot())
 
   // Lexicographically sorted categories with match in title
   const categoriesWithTitle = categories
-    .filter(it => normalizeSearchString(it.title).includes(normalizedFilter))
+    .filter(it => normalizeSearchString(it.title).includes(normalizedQuery))
     .map(it => ({ category: it, contentWithoutHtml: parseHTML(it.content) }))
     .sort((result1, result2) => result1.category.title.localeCompare(result2.category.title))
 
   // Lexicographically sorted categories with match in content but not in title
   const categoriesWithContent = categories
-    .filter(it => !normalizeSearchString(it.title).includes(normalizedFilter))
+    .filter(it => !normalizeSearchString(it.title).includes(normalizedQuery))
     .map(it => ({ category: it, contentWithoutHtml: parseHTML(it.content) }))
-    .filter(it => normalizeSearchString(it.contentWithoutHtml).includes(normalizedFilter))
+    .filter(it => normalizeSearchString(it.contentWithoutHtml).includes(normalizedQuery))
     .sort((result1, result2) => result1.category.title.localeCompare(result2.category.title))
 
   return categoriesWithTitle.concat(categoriesWithContent)
