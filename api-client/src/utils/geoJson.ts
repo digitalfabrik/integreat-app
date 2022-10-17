28c209540c3ca0d32d02944f89cf84ac843dbd64
Sarah Sporck <sporck@integreat-app.de>
2022-10-17 11:20:42 +0200

    IGAPP-1097: sort pois by distance

--- api-client/src/utils/geoJson.ts
+++ api-client/src/utils/geoJson.ts
@@ -8,17 +8,26 @@ export const embedInCollection = (features: PoiFeature[]): PoiFeatureCollection 
   features,
 })
 
-export const prepareFeatureLocation = (poi: PoiModel, userLocation: LocationType | null): PoiFeature | null => {
+export const prepareFeatureLocation = (poi: PoiModel, userLocation: LocationType | null): PoiFeature => {
   const { featureLocation } = poi
-  if (userLocation && featureLocation) {
-    const distanceValue = distance(userLocation, featureLocation.geometry.coordinates).toFixed(1)
+  if (userLocation) {
+    const distanceValue = distance(userLocation, featureLocation.geometry.coordinates).toFixed(0)
     return { ...featureLocation, properties: { ...featureLocation.properties, distance: distanceValue } }
   }
   return featureLocation
 }
 
-export const prepareFeatureLocations = (pois: Array<PoiModel>, userLocation: LocationType | null): PoiFeature[] =>
-  pois
-    .map(poi => prepareFeatureLocation(poi, userLocation))
-    .filter((feature): feature is PoiFeature => !!feature)
-    .sort((poi1: PoiFeature, poi2: PoiFeature) => poi1.properties.title.localeCompare(poi2.properties.title))
+export const prepareFeatureLocations = (pois: Array<PoiModel>, userLocation: LocationType | null): PoiFeature[] => {
+  const poiFeatures = pois.map(poi => prepareFeatureLocation(poi, userLocation))
+
+  if (userLocation) {
+    // sort by distance to current location
+    return poiFeatures.sort(
+      (poi1: PoiFeature, poi2: PoiFeature) =>
+        parseFloat(poi1.properties.distance ?? '0') - parseFloat(poi2.properties.distance ?? '0')
+    )
+  }
+  return poiFeatures.sort((poi1: PoiFeature, poi2: PoiFeature) =>
+    poi1.properties.title.localeCompare(poi2.properties.title)
+  )
+}
