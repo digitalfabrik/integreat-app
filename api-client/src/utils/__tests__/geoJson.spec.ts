b8c45441e95fcc56519946d4bb7bf6efdfd4e3e9
Sarah Sporck <sporck@integreat-app.de>
2022-10-17 11:20:42 +0200

    IGAPP-1097: sort pois by distance

--- api-client/src/utils/__tests__/geoJson.spec.ts
+++ api-client/src/utils/__tests__/geoJson.spec.ts
@@ -1,18 +1,25 @@
-import { PoiFeature, PoiFeatureCollection } from '../../maps'
+import { PoiFeature, PoiFeatureCollection, LocationType } from '../../maps'
 import { PoiModelBuilder } from '../../testing'
-import { embedInCollection } from '../geoJson'
+import { embedInCollection, prepareFeatureLocation, prepareFeatureLocations } from '../geoJson'
 
 describe('geoJson', () => {
-  const poi = new PoiModelBuilder(1).build()[0]!
+  const pois = new PoiModelBuilder(3).build()
+
+  const poi1 = pois[0]!
+  const poi2 = pois[1]!
+  const poi3 = pois[2]!
+
+  const longitude = 31.133859
+  const latitude = 29.979848
 
   const expectedGeoJsonMarkerFeature: PoiFeature = {
     type: 'Feature',
     geometry: {
       type: 'Point',
-      coordinates: [31.133859, 29.979848],
+      coordinates: [longitude, latitude],
     },
     properties: {
-      id: 1,
+      id: 0,
       title: 'Test Title',
       symbol: 'marker_40',
       thumbnail: 'test',
@@ -21,6 +28,9 @@ describe('geoJson', () => {
       address: 'Test Address 1',
     },
   }
+
+  const userLocation: LocationType = [longitude, latitude]
+
   describe('embedInCollection', () => {
     const expectedGeoJsonFeatureCollection: PoiFeatureCollection = {
       features: [expectedGeoJsonMarkerFeature],
@@ -28,7 +38,38 @@ describe('geoJson', () => {
     }
 
     it('should embed feature to GeoJson', () => {
-      expect(embedInCollection([poi.featureLocation!])).toEqual(expectedGeoJsonFeatureCollection)
+      expect(embedInCollection([poi1.featureLocation!])).toEqual(expectedGeoJsonFeatureCollection)
+    })
+  })
+
+  describe('prepareFeatureLocation', () => {
+    it('should prepare feature location with distance', () => {
+      expect(prepareFeatureLocation(poi1, userLocation)).toEqual({
+        ...expectedGeoJsonMarkerFeature,
+        properties: {
+          ...expectedGeoJsonMarkerFeature.properties,
+          distance: '0',
+        },
+      })
+    })
+    it('should prepare feature location without distance', () => {
+      expect(prepareFeatureLocation(poi1, null)).toEqual(expectedGeoJsonMarkerFeature)
+    })
+  })
+
+  describe('prepareFeatureLocations', () => {
+    it('should sort feature location by distance ascending', () => {
+      const poiFeature1 = prepareFeatureLocation(poi1, userLocation)
+      const poiFeature2 = prepareFeatureLocation(poi2, userLocation)
+      const poiFeature3 = prepareFeatureLocation(poi3, userLocation)
+      expect(prepareFeatureLocations([poi3, poi2, poi1], userLocation)).toEqual([poiFeature1, poiFeature3, poiFeature2])
+    })
+
+    it('should sort features by name if no userLoaction is supplied', () => {
+      const poiFeature1 = prepareFeatureLocation(poi1, null) // Test Title
+      const poiFeature2 = prepareFeatureLocation(poi2, null) // name 2
+      const poiFeature3 = prepareFeatureLocation(poi3, null) // another name
+      expect(prepareFeatureLocations([poi1, poi2, poi3], null)).toEqual([poiFeature3, poiFeature2, poiFeature1])
     })
   })
 })
