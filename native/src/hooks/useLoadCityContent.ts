78cde4a7352e08d7c6cf48794928a5d1667744ac
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-10-19 19:09:34 +0200

    IGAPP-636: POC

--- native/src/hooks/useLoadCityContent.ts
+++ native/src/hooks/useLoadCityContent.ts
@@ -1,5 +1,9 @@
+import { useCallback } from 'react'
+
 import { CityModel, LanguageModel, ReturnType, useLoadAsync } from 'api-client'
 
+import { LanguageResourceCacheStateType } from '../redux/StateType'
+import { dataContainer } from '../utils/DefaultDataContainer'
 import useLoadCities from './useLoadCities'
 import useLoadLanguages from './useLoadLanguages'
 
@@ -15,6 +19,7 @@ export type CityContentReturn<T> = ReturnType<
     languages: LanguageModel[]
     city: CityModel
     language: LanguageModel
+    resourceCache: LanguageResourceCacheStateType
   } & T
 >
 
@@ -22,6 +27,13 @@ const useLoadCityContent = <T>({ cityCode, languageCode, load }: UseLoadCityCont
   const citiesReturn = useLoadCities()
   const languagesReturn = useLoadLanguages({ cityCode })
   const otherReturn = useLoadAsync(load)
+
+  const loadResourceCache = useCallback(
+    async () => dataContainer.getResourceCache(cityCode, languageCode),
+    [cityCode, languageCode]
+  )
+
+  const resourceCacheReturn = useLoadAsync(loadResourceCache)
 
   const city = citiesReturn.data?.find(it => it.code === cityCode)
   const language = languagesReturn.data?.find(it => it.code === languageCode)
@@ -33,23 +45,25 @@ const useLoadCityContent = <T>({ cityCode, languageCode, load }: UseLoadCityCont
     if (!language) {
       return new Error('Language not found')
     }
-    return citiesReturn.error ?? languagesReturn.error ?? otherReturn.error
+    return citiesReturn.error ?? languagesReturn.error ?? otherReturn.error ?? resourceCacheReturn.error
   }
 
-  const loading = citiesReturn.loading || languagesReturn.loading || otherReturn.loading
+  const loading = citiesReturn.loading || languagesReturn.loading || otherReturn.loading || resourceCacheReturn.loading
   const refresh = () => {
     citiesReturn.refresh()
     languagesReturn.refresh()
     otherReturn.refresh()
+    resourceCacheReturn.refresh()
   }
 
   const data =
-    city && language && citiesReturn.data && languagesReturn.data && otherReturn.data
+    city && language && citiesReturn.data && languagesReturn.data && otherReturn.data && resourceCacheReturn.data
       ? {
           city,
           language,
           cities: citiesReturn.data,
           languages: languagesReturn.data,
+          resourceCache: resourceCacheReturn.data,
           ...otherReturn.data,
         }
       : null
