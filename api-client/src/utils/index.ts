ee71f95a610a86496c832e7432e4f13463cc032a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-10-27 13:20:36 +0200

    IGAPP-636: Tidy up

--- api-client/src/utils/index.ts
+++ api-client/src/utils/index.ts
@@ -1,1 +1,7 @@
-export const getSlugFromPath = (path: string): string => path.split('/').pop()!
+export const getSlugFromPath = (path: string): string => {
+  const slug = path.split('/').pop()
+  if (!slug) {
+    throw new Error('Empty string is not a valid path')
+  }
+  return slug
+}
