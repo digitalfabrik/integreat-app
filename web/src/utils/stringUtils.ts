b4c90e8ae339e9cbed9e71db0a9d4b83f736d71a
Andy <fischer@integreat-app.de>
2022-11-08 13:20:32 +0100

    IGAPP-1143: switch to url slug for web

--- web/src/utils/stringUtils.ts
+++ web/src/utils/stringUtils.ts
@@ -3,3 +3,9 @@
  * @returns {string} the url of the current host and the specified path
  */
 export const urlFromPath = (path: string): string => `${window.location.origin}${path}`
+
+/**
+ * @param path: the current path
+ * @returns {string} the parent of the current path
+ */
+export const getParentPath = (path: string): string => path.replace(/[^/]*$/, '').replace(/\/$/, '')
