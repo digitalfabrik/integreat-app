babe85067449447f98682316b51a592667a7aaa2
LeandraH <leandravhahn@gmail.com>
2022-11-10 10:51:41 +0100

    IGAPP-1136: Zoom in further if selected POI has close neighbors

--- api-client/src/utils/__tests__/geoJson.spec.ts
+++ api-client/src/utils/__tests__/geoJson.spec.ts
@@ -26,6 +26,7 @@ describe('geoJson', () => {
       path: '/augsburg/de/locations/test',
       urlSlug: 'test',
       address: 'Test Address 1',
+      closeToOtherPoi: false,
     },
   }
 
@@ -44,7 +45,7 @@ describe('geoJson', () => {
 
   describe('prepareFeatureLocation', () => {
     it('should prepare feature location with distance', () => {
-      expect(prepareFeatureLocation(poi1, userLocation)).toEqual({
+      expect(prepareFeatureLocation(poi1, userLocation, [])).toEqual({
         ...expectedGeoJsonMarkerFeature,
         properties: {
           ...expectedGeoJsonMarkerFeature.properties,
@@ -52,23 +53,59 @@ describe('geoJson', () => {
         },
       })
     })
+
     it('should prepare feature location without distance', () => {
-      expect(prepareFeatureLocation(poi1, null)).toEqual(expectedGeoJsonMarkerFeature)
+      expect(prepareFeatureLocation(poi1, null, [])).toEqual(expectedGeoJsonMarkerFeature)
+    })
+
+    it('should mark feature location as having identical other locations', () => {
+      const coordinateList = [
+        [longitude, latitude],
+        [longitude, latitude],
+      ]
+      expect(prepareFeatureLocation(poi1, null, coordinateList)).toEqual({
+        ...expectedGeoJsonMarkerFeature,
+        properties: {
+          ...expectedGeoJsonMarkerFeature.properties,
+          closeToOtherPoi: true,
+        },
+      })
+    })
+
+    it('should mark feature location as having close other locations', () => {
+      const coordinateList = [
+        [longitude, latitude],
+        [longitude + 0.0001, latitude - 0.0001],
+      ]
+      expect(prepareFeatureLocation(poi1, null, coordinateList)).toEqual({
+        ...expectedGeoJsonMarkerFeature,
+        properties: {
+          ...expectedGeoJsonMarkerFeature.properties,
+          closeToOtherPoi: true,
+        },
+      })
+    })
+
+    it('should mark feature location as not having close other locations', () => {
+      const coordinateList = [[longitude, latitude]]
+      expect(prepareFeatureLocation(poi1, null, coordinateList)).toEqual(expectedGeoJsonMarkerFeature)
     })
   })
 
   describe('prepareFeatureLocations', () => {
     it('should sort feature location by distance ascending', () => {
-      const poiFeature1 = prepareFeatureLocation(poi1, userLocation)
-      const poiFeature2 = prepareFeatureLocation(poi2, userLocation)
-      const poiFeature3 = prepareFeatureLocation(poi3, userLocation)
+      const poiFeature1 = prepareFeatureLocation(poi1, userLocation, [])
+      const poiFeature2 = prepareFeatureLocation(poi2, userLocation, [])
+      console.log(poiFeature2)
+
+      const poiFeature3 = prepareFeatureLocation(poi3, userLocation, [])
       expect(prepareFeatureLocations([poi3, poi2, poi1], userLocation)).toEqual([poiFeature1, poiFeature3, poiFeature2])
     })
 
     it('should sort features by name if no userLoaction is supplied', () => {
-      const poiFeature1 = prepareFeatureLocation(poi1, null) // Test Title
-      const poiFeature2 = prepareFeatureLocation(poi2, null) // name 2
-      const poiFeature3 = prepareFeatureLocation(poi3, null) // another name
+      const poiFeature1 = prepareFeatureLocation(poi1, null, []) // Test Title
+      const poiFeature2 = prepareFeatureLocation(poi2, null, []) // name 2
+      const poiFeature3 = prepareFeatureLocation(poi3, null, []) // another name
       expect(prepareFeatureLocations([poi1, poi2, poi3], null)).toEqual([poiFeature3, poiFeature2, poiFeature1])
     })
   })
--- api-client/src/utils/geoJson.ts
+++ api-client/src/utils/geoJson.ts
@@ -1,4 +1,5 @@
 import distance from '@turf/distance'
+import { Position } from 'geojson'
 
 import { LocationType, PoiModel } from '../index'
 import { PoiFeature, PoiFeatureCollection } from '../maps'
@@ -8,8 +9,26 @@ export const embedInCollection = (features: PoiFeature[]): PoiFeatureCollection 
   features,
 })
 
-export const prepareFeatureLocation = (poi: PoiModel, userLocation: LocationType | null): PoiFeature => {
+export const prepareFeatureLocation = (
+  poi: PoiModel,
+  userLocation: LocationType | null,
+  coordinateList: Position[]
+): PoiFeature => {
   const { featureLocation } = poi
+  const { coordinates } = featureLocation.geometry
+
+  const maxDistanceForOverlap = 0.0003
+  const longitude = coordinates[0] as number
+  const latitude = coordinates[1] as number
+  const overlappingCoordinates = coordinateList.filter(
+    coord =>
+      Math.abs((coord[0] as number) - longitude) < maxDistanceForOverlap &&
+      Math.abs((coord[1] as number) - latitude) < maxDistanceForOverlap
+  )
+  if (overlappingCoordinates.length > 1) {
+    featureLocation.properties.closeToOtherPoi = true
+  }
+
   if (userLocation) {
     const distanceValue = distance(userLocation, featureLocation.geometry.coordinates).toFixed(1)
     return { ...featureLocation, properties: { ...featureLocation.properties, distance: distanceValue } }
@@ -18,7 +37,8 @@ export const prepareFeatureLocation = (poi: PoiModel, userLocation: LocationType
 }
 
 export const prepareFeatureLocations = (pois: Array<PoiModel>, userLocation: LocationType | null): PoiFeature[] => {
-  const poiFeatures = pois.map(poi => prepareFeatureLocation(poi, userLocation))
+  const coordinateList = pois.map(poi => poi.featureLocation.geometry.coordinates)
+  const poiFeatures = pois.map(poi => prepareFeatureLocation(poi, userLocation, coordinateList))
 
   if (userLocation) {
     return poiFeatures.sort(
