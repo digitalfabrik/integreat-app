c8dee16fdb62056e38dc999fb3ef2109f9378819
Andy <fischer@integreat-app.de>
2022-11-10 09:29:36 +0100

    IGAPP-1143: readded center coordinates to work on real devices

--- native/src/components/MapView.tsx
+++ native/src/components/MapView.tsx
@@ -76,6 +76,7 @@ const MapView = React.forwardRef(
     const coordinates = selectedFeature?.geometry.coordinates
     const defaultSettings: CameraSettings = {
       zoomLevel: coordinates ? detailZoom : defaultViewportConfig.zoom,
+      centerCoordinate: coordinates,
       bounds: coordinates ? undefined : bounds,
     }
 
