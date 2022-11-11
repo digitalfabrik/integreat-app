3b48888e6a4bcf16090061453407dcd639258b61
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-11-11 12:29:01 +0100

    IGAPP-636: Remove hocs

--- native/src/hocs/wrapDisplayName.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-const getDisplayName = <T>(Component: React.ComponentType<T>) =>
-  Component.displayName || Component.name || typeof Component
-
-const wrapDisplayName = <T>(Component: React.ComponentType<T>, hocName: string): string =>
-  `${hocName}(${getDisplayName(Component)})`
-
-export default wrapDisplayName
