6d7080e5a059d9195e102bc4496d0669d7cf4d80
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-11-17 13:52:30 +0100

    IGAPP-636: Fix search

--- native/src/routes/SearchModalContainer.tsx
+++ native/src/routes/SearchModalContainer.tsx
@@ -1,13 +1,15 @@
 import React, { ReactElement, useContext } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { ThemeContext } from 'styled-components'
 
-import { CategoriesMapModel, SearchRouteType } from 'api-client'
+import { SearchRouteType } from 'api-client'
 
 import { NavigationProps } from '../constants/NavigationTypes'
+import useCityAppContext from '../hooks/useCityAppContext'
+import useLoadCategories from '../hooks/useLoadCategories'
 import createNavigate from '../navigation/createNavigate'
-import { StateType } from '../redux/StateType'
+import LoadingErrorHandler from './LoadingErrorHandler'
 import SearchModal from './SearchModal'
 
 export type SearchModalContainerProps = {
@@ -15,30 +17,27 @@ export type SearchModalContainerProps = {
 }
 
 const SearchModalContainer = ({ navigation }: SearchModalContainerProps): ReactElement | null => {
-  const cityCode = useSelector<StateType, string | undefined>(state => state.cityContent?.city)
-  const language = useSelector<StateType, string>(state => state.contentLanguage)
-  const categories = useSelector<StateType, CategoriesMapModel | null>(
-    state => state.cityContent?.searchRoute?.categoriesMap ?? null
-  )
-  const dispatch = useDispatch()
+  const { cityCode, languageCode } = useCityAppContext()
+  const { data, ...response } = useLoadCategories({ cityCode, languageCode })
   const theme = useContext(ThemeContext)
   const { t } = useTranslation('search')
+  const dispatch = useDispatch()
 
-  const closeModal = () => {
-    navigation.goBack()
-  }
-
-  return cityCode && categories ? (
-    <SearchModal
-      cityCode={cityCode}
-      navigateTo={createNavigate(dispatch, navigation)}
-      closeModal={closeModal}
-      categories={categories}
-      language={language}
-      theme={theme}
-      t={t}
-    />
-  ) : null
+  return (
+    <LoadingErrorHandler {...response}>
+      {data && (
+        <SearchModal
+          cityCode={cityCode}
+          navigateTo={createNavigate(dispatch, navigation)}
+          closeModal={navigation.goBack}
+          categories={data.categories}
+          language={languageCode}
+          theme={theme}
+          t={t}
+        />
+      )}
+    </LoadingErrorHandler>
+  )
 }
 
 export default SearchModalContainer
