04488cca95c217f61b68f54ced692ca9e9bb286f
Steffen Kleinle <steffen.kleinle@mailbox.org>
2022-12-11 21:14:11 +0100

    IGAPP-636: Reuse load logic

--- native/src/hooks/useLoadPois.ts
+++ native/src/hooks/useLoadPois.ts
@@ -1,9 +1,6 @@
-import { useCallback } from 'react'
-
 import { createPOIsEndpoint, PoiModel } from 'api-client'
 
 import { dataContainer } from '../utils/DefaultDataContainer'
-import { determineApiUrl } from '../utils/helpers'
 import useLoadCityContent, { CityContentReturn } from './useLoadCityContent'
 
 type UseLoadPoisProps = {
@@ -11,23 +8,14 @@ type UseLoadPoisProps = {
   languageCode: string
 }
 
-const useLoadPois = ({ cityCode, languageCode }: UseLoadPoisProps): CityContentReturn<{ pois: PoiModel[] }> => {
-  const load = useCallback(async () => {
-    if (await dataContainer.poisAvailable(cityCode, languageCode)) {
-      return { pois: await dataContainer.getPois(cityCode, languageCode) }
-    }
-
-    const payload = await createPOIsEndpoint(await determineApiUrl()).request({
-      city: cityCode,
-      language: languageCode,
-    })
-    if (payload.data) {
-      await dataContainer.setPois(cityCode, languageCode, payload.data)
-    }
-    return payload.data ? { pois: payload.data } : null
-  }, [cityCode, languageCode])
-
-  return useLoadCityContent({ cityCode, languageCode, load })
-}
+const useLoadPois = (params: UseLoadPoisProps): CityContentReturn<{ pois: PoiModel[] }> =>
+  useLoadCityContent({
+    ...params,
+    createEndpoint: createPOIsEndpoint,
+    map: data => ({ pois: data }),
+    isAvailable: dataContainer.poisAvailable,
+    getFromDataContainer: dataContainer.getPois,
+    setToDataContainer: dataContainer.setPois,
+  })
 
 export default useLoadPois
