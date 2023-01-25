933a6137f28afa0b18990848793bf2fc3249f2cb
LeandraH <leandravhahn@gmail.com>
2023-01-25 18:36:51 +0100

    IGAPP-1061: Update RN on iOS

--- native/ios/Integreat/AppDelegate.mm
+++ native/ios/Integreat/AppDelegate.mm
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+
 #import <React/RCTAppSetupUtils.h>
 
 #if RCT_NEW_ARCH_ENABLED
@@ -12,26 +13,11 @@
 #import <React/RCTSurfacePresenter.h>
 #import <React/RCTSurfacePresenterBridgeAdapter.h>
 #import <ReactCommon/RCTTurboModuleManager.h>
+
 #import <react/config/ReactNativeConfig.h>
 
-#ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-static void InitializeFlipper(UIApplication *application) {
-  FlipperClient *client = [FlipperClient sharedClient];
-  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
-  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
-  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
-  [client addPlugin:[FlipperKitReactPlugin new]];
-  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
-  [client start];
-}
-#endif
 static NSString *const kRNConcurrentRoot = @"concurrentRoot";
+
 @interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
   RCTTurboModuleManager *_turboModuleManager;
   RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
@@ -40,51 +26,11 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 }
 @end
 #endif
+
 @implementation AppDelegate
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-#ifdef FB_SONARKIT_ENABLED
-  InitializeFlipper(application);
-#endif
-
-  if ([@BUILD_CONFIG_GOOGLE_SERVICES_GOOGLE_APP_ID length] > 0 &&
-      [@BUILD_CONFIG_GOOGLE_SERVICES_GCM_SENDER_ID length] > 0 &&
-      [@BUILD_CONFIG_GOOGLE_SERVICES_API_KEY length] > 0 &&
-      [@BUILD_CONFIG_GOOGLE_SERVICES_BUNDLE_ID length] > 0 &&
-      [@BUILD_CONFIG_GOOGLE_SERVICES_CLIENT_ID length] > 0 &&
-      [@BUILD_CONFIG_GOOGLE_SERVICES_PROJECT_ID length] > 0 &&
-      [@BUILD_CONFIG_GOOGLE_SERVICES_REVERSED_CLIENT_ID length] > 0 &&
-      [@BUILD_CONFIG_GOOGLE_SERVICES_DATABASE_URL length] > 0 &&
-      [@BUILD_CONFIG_GOOGLE_SERVICES_STORAGE_BUCKET length] > 0) {
-    FIROptions *options = [[FIROptions alloc]
-                           initWithGoogleAppID:@BUILD_CONFIG_GOOGLE_SERVICES_GOOGLE_APP_ID
-                           GCMSenderID:@BUILD_CONFIG_GOOGLE_SERVICES_GCM_SENDER_ID];
-    if (options) {
-      options.APIKey = @BUILD_CONFIG_GOOGLE_SERVICES_API_KEY;
-      options.bundleID = @BUILD_CONFIG_GOOGLE_SERVICES_BUNDLE_ID;
-      options.clientID = @BUILD_CONFIG_GOOGLE_SERVICES_CLIENT_ID;
-      options.projectID = @BUILD_CONFIG_GOOGLE_SERVICES_PROJECT_ID;
-      options.androidClientID = @BUILD_CONFIG_GOOGLE_SERVICES_REVERSED_CLIENT_ID;
-      options.databaseURL = @BUILD_CONFIG_GOOGLE_SERVICES_DATABASE_URL;
-      options.storageBucket = @BUILD_CONFIG_GOOGLE_SERVICES_STORAGE_BUCKET;
-
-      // The following values are unset as they are probably not relevant for Messsaging:
-      // options.deepLinkURLScheme = ??
-      // options.trackingID = ??
-      // options.appGroupID = ??
-
-      // Unused info from GoogleService-Info.plist:
-      // These existed in the .plist file but I was not able to add them to the FIROptions. They are probably not important.
-      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_PLIST_VERSION;
-      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_ADS_ENABLED;
-      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_ANALYTICS_ENABLED;
-      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_APPINVITE_ENABLED;
-      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_GCM_ENABLED;
-      // ?? = BUILD_CONFIG_GOOGLE_SERVICES_IS_SIGNIN_ENABLED;
-
-      [FIRApp configureWithOptions:options];
-    }
-  }
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
@@ -105,13 +51,12 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   } else {
     rootView.backgroundColor = [UIColor whiteColor];
   }
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-
-  [self clearStorage];
   return YES;
 }
 
@@ -129,11 +74,14 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 - (NSDictionary *)prepareInitialProps
 {
   NSMutableDictionary *initProps = [NSMutableDictionary new];
+
 #ifdef RCT_NEW_ARCH_ENABLED
   initProps[kRNConcurrentRoot] = @([self concurrentRootEnabled]);
 #endif
+
   return initProps;
 }
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
@@ -143,60 +91,10 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 }
 
-- (void)clearStorage
-{
-  NSUserDefaults *preferences = [NSUserDefaults standardUserDefaults];
-  NSString *lastLanguageKey = @"last_location";
-
-  if ([preferences objectForKey:lastLanguageKey] != nil)
-  {
-    //clear preferences
-    [self resetDefaults];
-
-    //clear storage
-    [self removeCache];
-  }
-}
-
-- (void)resetDefaults 
-{
-  NSUserDefaults * defs = [NSUserDefaults standardUserDefaults];
-  NSDictionary * dict = [defs dictionaryRepresentation];
-  for (id key in dict) {
-    [defs removeObjectForKey:key];
-  }
-  [defs synchronize];
-}
-
-- (void)removeCache
-{
-  NSFileManager* manager = [NSFileManager defaultManager];
-  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES);
-  NSString *libraryDirectory = [paths objectAtIndex:0];
-  NSArray *directoryContent = [manager contentsOfDirectoryAtPath: libraryDirectory error:nil];
-  for (NSString *content in directoryContent)  {
-    NSString *path = [libraryDirectory stringByAppendingPathComponent:content];
-    [manager removeItemAtPath:path error:nil];
-  }
-}
-
-- (BOOL)application:(UIApplication *)application
-   openURL:(NSURL *)url
-   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
-{
-  return [RCTLinkingManager application:application openURL:url options:options];
-}
-
-- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
- restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
-{
- return [RCTLinkingManager application:application
-                  continueUserActivity:userActivity
-                    restorationHandler:restorationHandler];
-}
-
 #if RCT_NEW_ARCH_ENABLED
+
 #pragma mark - RCTCxxBridgeDelegate
+
 - (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
 {
   _turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
@@ -204,27 +102,32 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
                                                             jsInvoker:bridge.jsCallInvoker];
   return RCTAppSetupDefaultJsExecutorFactory(bridge, _turboModuleManager);
 }
+
 #pragma mark RCTTurboModuleManagerDelegate
+
 - (Class)getModuleClassFromName:(const char *)name
 {
   return RCTCoreModulesClassProvider(name);
 }
+
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
                                                       jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
 {
   return nullptr;
 }
+
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
                                                      initParams:
                                                          (const facebook::react::ObjCTurboModule::InitParams &)params
 {
   return nullptr;
 }
+
 - (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
 {
   return RCTAppSetupDefaultModuleFromClass(moduleClass);
 }
-#endif
 
+#endif
 
 @end
