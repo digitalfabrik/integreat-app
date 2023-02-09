923682c82792c18518cd49813a69914d5e464377
Sarah Sporck <sarah.sporck@tuerantuer.org>
2023-02-09 10:13:08 +0100

    IGAPP-1227: increase e2e web build resource size
    Also run web build in minified production version and disable source
    maps.
    
    IGAPP-1227: disable source maps and use web production version
    
    IGAPP-1227: increase resource size of e2e web build

--- e2e-tests/web/waitForLocalhost.ts
+++ e2e-tests/web/waitForLocalhost.ts
@@ -1,16 +1,19 @@
 // mostly taken from https://github.com/sindresorhus/wait-for-localhost
 // copy pasted as we do not use esm at the moment
-import http from 'node:http';
+import http from 'node:http'
 
-export default function waitForLocalhost(timeout: number) {
-	return new Promise(resolve => {
-        const request = http.request({method: "head", port: 9000, family: 4, timeout}, response => {
-            if (response.statusCode === 200) {
-                resolve(200);
-                return;
-            }
-        });
+const STATUS_CODE_OK = 200
 
-        request.end();
-	});
-}
+const waitForLocalhost = (timeout: number): Promise<void> =>
+  new Promise((resolve, reject) => {
+    const request = http.request({ method: 'head', port: 9000, family: 4, timeout }, response => {
+      if (response.statusCode === STATUS_CODE_OK) {
+        resolve()
+      }
+      reject()
+    })
+
+    request.end()
+  })
+
+export default waitForLocalhost
