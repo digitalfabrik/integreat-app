cc91b38bd7d74654705f9d4d295c5290d0133063
Andy <fischer@integreat-app.de>
2023-02-14 00:14:28 +0100

    IGAPP-1229: Times of events will be exported correctly

--- api-client/src/utils/index.ts
+++ api-client/src/utils/index.ts
@@ -2,4 +2,4 @@ import { Moment } from 'moment'
 
 export const getSlugFromPath = (path: string): string => path.split('/').pop()!
 
-export const formatDateICal = (date: Moment): string => `${date.format('YYYYMMDDTHHmm')}00Z`
+export const formatDateICal = (date: Moment): string => `${date.format('YYYYMMDDTHHmm')}00`
