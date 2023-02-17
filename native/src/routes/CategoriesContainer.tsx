9091c2b3b039e08ce2cb0e6b8874b1f7522458c2
Andy <fischer@integreat-app.de>
2023-02-17 15:36:00 +0100

    IGAPP-1223: invalidate city content cache on refresh if data can't be loaded from cache

--- native/src/routes/CategoriesContainer.tsx
+++ native/src/routes/CategoriesContainer.tsx
@@ -17,6 +17,8 @@ import useResourceCache from '../hooks/useResourceCache'
 import createNavigateToFeedbackModal from '../navigation/createNavigateToFeedbackModal'
 import urlFromRouteInformation from '../navigation/url'
 import testID from '../testing/testID'
+import dataContainer from '../utils/DefaultDataContainer'
+import { reportError } from '../utils/sentry'
 import LoadingErrorHandler from './LoadingErrorHandler'
 
 const Spacing = styled.View`
@@ -64,8 +66,15 @@ const CategoriesContainer = ({ navigation, route }: CategoriesContainerProps): R
   const error =
     data?.categories && !category && previousLanguageCode === languageCode ? ErrorCode.PageNotFound : response.error
 
+  // Workaround clear cache on refresh if city content can't be loaded.
+  // TODO IGAPP-1231: Proper cache invalidation for version updates
+  const clearResourcesAndCache = () => {
+    dataContainer.clearInMemoryCache()
+    dataContainer.clearOfflineCache().catch(reportError)
+  }
+
   return (
-    <LoadingErrorHandler {...response} error={error} scrollView>
+    <LoadingErrorHandler {...response} error={error} refresh={clearResourcesAndCache} scrollView>
       {data && category && (
         <SpaceBetween {...(category.isRoot() ? testID('Dashboard-Page') : {})}>
           {category.isRoot() ? (
