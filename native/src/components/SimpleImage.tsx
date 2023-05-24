183ba43dcf271d6c82ed5521a9e295452e345b05
Andy <fischer@integreat-app.de>
2023-05-24 15:35:18 +0200

    IGAPP-1301: add release note and  add fkt getLocalPlatformFilepath

--- native/src/components/SimpleImage.tsx
+++ native/src/components/SimpleImage.tsx
@@ -7,21 +7,27 @@ type SimpleImageProps = {
   style?: StyleProp<ImageStyle>
   resizeMode?: ImageResizeMode
 }
+
 // For ios you should not use the absolute path, since it can change with a future build version, therefore we use home directory
 // https://github.com/facebook/react-native/commit/23909cd6f62056de0cd0f7c06e3997dd967c139a
-const getLocalFilePathForIosFiles = (uri: string) => `~${uri.substring(uri.indexOf('/Documents'))}`
+const getLocalPlatformFilepath = (uri: string): string => {
+  if (Platform.OS === 'ios' && uri.includes('file://')) {
+    return `~${uri.substring(uri.indexOf('/Documents'))}`
+  }
+  return uri
+}
 const getImageSource = (uri: string | number): ImageSourcePropType =>
   typeof uri === 'number'
     ? uri
     : {
-        uri,
+        uri: getLocalPlatformFilepath(uri),
         cache: 'reload',
       }
+
 const SimpleImage = ({ source, style, resizeMode = 'contain' }: SimpleImageProps): ReactElement => {
   if (source === null) {
     return <View style={style} />
   }
-  console.log(Platform.OS, source)
 
   return <Image source={getImageSource(source)} resizeMode={resizeMode} style={style} />
 }
