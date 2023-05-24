bd0ec86cc009f34916dbc2d5a977b88f41945c3a
Andy <fischer@integreat-app.de>
2023-05-24 14:56:38 +0200

    IGAPP-1301: remove file prefix for cached thubmnail

--- native/src/components/SimpleImage.tsx
+++ native/src/components/SimpleImage.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react'
-import { Image, View, ImageSourcePropType, StyleProp, ImageStyle, ImageResizeMode } from 'react-native'
+import { Image, View, ImageSourcePropType, StyleProp, ImageStyle, ImageResizeMode, Platform } from 'react-native'
 
 export type ImageSourceType = string | number | null
 type SimpleImageProps = {
@@ -7,7 +7,9 @@ type SimpleImageProps = {
   style?: StyleProp<ImageStyle>
   resizeMode?: ImageResizeMode
 }
-
+// For ios you should not use the absolute path, since it can change with a future build version, therefore we use home directory
+// https://github.com/facebook/react-native/commit/23909cd6f62056de0cd0f7c06e3997dd967c139a
+const getLocalFilePathForIosFiles = (uri: string) => `~${uri.substring(uri.indexOf('/Documents'))}`
 const getImageSource = (uri: string | number): ImageSourcePropType =>
   typeof uri === 'number'
     ? uri
@@ -15,11 +17,11 @@ const getImageSource = (uri: string | number): ImageSourcePropType =>
         uri,
         cache: 'reload',
       }
-
 const SimpleImage = ({ source, style, resizeMode = 'contain' }: SimpleImageProps): ReactElement => {
   if (source === null) {
     return <View style={style} />
   }
+  console.log(Platform.OS, source)
 
   return <Image source={getImageSource(source)} resizeMode={resizeMode} style={style} />
 }
