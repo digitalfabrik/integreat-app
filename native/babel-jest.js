30ef56766771a1b2cf22ab327f2a495facd34099
Sarah Sporck <sarah.sporck@tuerantuer.org>
2023-05-25 17:15:44 +0200

    IGAPP-1290: enable isolatedModules in tests

--- native/babel-jest.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies, @typescript-eslint/no-var-requires
-const babelJest = require('babel-jest')
-
-module.exports = babelJest.createTransformer({
-  rootMode: 'upward',
-})
--- translations/jest.config.ts
+++ translations/jest.config.ts
@@ -1,12 +1,19 @@
 export default {
   rootDir: 'src/',
-  preset: 'ts-jest',
   verbose: true,
   automock: false,
   setupFiles: ['<rootDir>/../jest.setup.ts'],
   moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   maxWorkers: '50%',
+  transform: {
+    '^.+\\.(j|t)sx?$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
   coverageDirectory: '<rootDir>/../reports/coverage',
   reporters: [
     'default',
