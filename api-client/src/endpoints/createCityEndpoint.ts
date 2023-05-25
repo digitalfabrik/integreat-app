fe60afdb1d27b0d2b8db642618255f44b935b35f
Steffen Kleinle <steffen.kleinle@mailbox.org>
2023-05-25 11:10:39 +0200

    IGAPP-1267: Add single city endpoint

---
+++ api-client/src/endpoints/createCityEndpoint.ts
@@ -0,0 +1,15 @@
+import Endpoint from '../Endpoint'
+import EndpointBuilder from '../EndpointBuilder'
+import mapCityJson from '../mapping/mapCityJson'
+import CityModel from '../models/CityModel'
+
+export const CITY_ENDPOINT_NAME = 'city'
+type Params = {
+  city: string
+}
+
+export default (baseUrl: string): Endpoint<Params, CityModel> =>
+  new EndpointBuilder<Params, CityModel>(CITY_ENDPOINT_NAME)
+    .withParamsToUrlMapper(({ city }: Params) => `${baseUrl}/wp-json/extensions/v3/sites/${city}`)
+    .withMapper(mapCityJson)
+    .build()
