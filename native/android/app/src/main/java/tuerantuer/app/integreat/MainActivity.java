da274e64b3e7e46e05479ea3f9c3169fd079eb4f
Steffen Kleinle <steffen.kleinle@mailbox.org>
2023-06-26 15:08:49 +0200

    IGAPP-1326: Remove duplicate

--- native/android/app/src/main/java/tuerantuer/app/integreat/MainActivity.java
+++ native/android/app/src/main/java/tuerantuer/app/integreat/MainActivity.java
@@ -36,11 +36,6 @@ public class MainActivity extends ReactActivity {
         return "Integreat";
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(null);
-    }
-
     /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
    * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
