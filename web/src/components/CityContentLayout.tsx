7ebfc7b7136f4c4247bd6439291c6893543297ea
Andy <fischer@integreat-app.de>
2023-06-27 10:01:14 +0200

    IGAPP-1315: fix jumping news tabs

--- web/src/components/CityContentLayout.tsx
+++ web/src/components/CityContentLayout.tsx
@@ -55,7 +55,7 @@ const CityContentLayout = (props: CityContentLayoutProps): ReactElement => {
       />
     ) : null
 
-  const toolbar = toolbarProp && isLoading === false ? toolbarProp(setFeedbackModalRating) : null
+  const toolbar = toolbarProp ? toolbarProp(setFeedbackModalRating) : null
 
   return (
     <Layout
