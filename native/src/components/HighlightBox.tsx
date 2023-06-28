297ef64b8063d29b897ec638e0241b58469e2e48
Steffen Kleinle <steffen.kleinle@mailbox.org>
2023-06-28 09:26:33 +0200

    IGAPP-1191: Add organization footer for native

---
+++ native/src/components/HighlightBox.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+
+const HighlightBox = styled.View`
+  background-color: ${props => props.theme.colors.backgroundAccentColor};
+  border-radius: 4px;
+  padding: 20px;
+`
+
+export default HighlightBox
