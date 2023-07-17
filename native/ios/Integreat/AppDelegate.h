b2b9a408987fb50f563a5a2f4d76e2677f4186b0
Leandra Hahn <LeandraH@users.noreply.github.com>
2023-07-17 11:59:27 +0200

    IGAPP-1331: Update RN to 0.71 (#966)
    * IGAPP-1331: Update RN to 0.71
    
    * IGAPP-1331: Implement review points
    
    * Update native/android/app/src/main/java/tuerantuer/app/integreat/ReactNativeFlipper.java
    
    Co-authored-by: Sarah <sarah.sporck@tuerantuer.org>
    
    * Update native/package.json
    
    Co-authored-by: Sarah <sarah.sporck@tuerantuer.org>
    
    * IGAPP-1331: Update RN types
    
    * IGAPP-1331: Fix TS complaints
    
    * IGAPP-1331: Fix Android build
    
    * IGAPP-1331: Fix Android build again
    
    * IGAPP-1331: Use new Android bundle location
    
    * IGAPP-1331: Fix typo
    
    * IGAPP-1331: Fix Android crash
    
    * IGAPP-1331: Update Webview
    
    * IGAPP-1331: Fix icons
    
    * IGAPP-1331: Fix Android crash again
    
    ---------
    
    Co-authored-by: Sarah <sarah.sporck@tuerantuer.org>

--- native/ios/Integreat/AppDelegate.h
+++ native/ios/Integreat/AppDelegate.h
@@ -1,8 +1,6 @@
-#import <React/RCTBridgeDelegate.h>
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+@interface AppDelegate : RCTAppDelegate
 
-@property (nonatomic, strong) UIWindow *window;
-
-@end 
+@end
