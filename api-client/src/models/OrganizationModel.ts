b8e713ebfa506ab3fb61afb80721aee0967d1bfe
Steffen Kleinle <steffen.kleinle@mailbox.org>
2023-07-25 10:20:58 +0200

    IGAPP-1191: Remove todos

--- api-client/src/models/OrganizationModel.ts
+++ api-client/src/models/OrganizationModel.ts
@@ -1,9 +1,9 @@
 class OrganizationModel {
   _name: string
-  _url: string | null
-  _logo: string | null
+  _url: string
+  _logo: string
 
-  constructor({ name, url, logo }: { name: string; url: string | null; logo: string | null }) {
+  constructor({ name, url, logo }: { name: string; url: string; logo: string }) {
     this._name = name
     this._url = url
     this._logo = logo
@@ -14,14 +14,11 @@ class OrganizationModel {
   }
 
   get url(): string {
-    // TODO: Remove fallback
-    return this._url ?? 'https://example.com/my-custom-url'
+    return this._url
   }
 
   get logo(): string {
-    // TODO: Remove fallback
-    // return this._logo ?? 'https://webnext.integreat.app/app-logo.png'
-    return 'https://admin.integreat-app.de/media/regions/214/2023/02/TaT_DF_LOGO.png'
+    return this._logo
   }
 
   isEqual(other: OrganizationModel | null): boolean {
