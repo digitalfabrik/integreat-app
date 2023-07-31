0e10b4df78ec5722583db493f44442d733ea7326
Andy <fischer@integreat-app.de>
2023-07-31 18:36:07 +0200

    IGAPP-100: add social media tooltip, add social media sharing icons

--- web/src/assets/index.ts
+++ web/src/assets/index.ts
@@ -3,17 +3,21 @@ import AccessibleIcon from '../../../assets/icons/accessible.svg'
 import BathroomIcon from '../../../assets/icons/bathroom.svg'
 import BedIcon from '../../../assets/icons/bed.svg'
 import CalendarIcon from '../../../assets/icons/calendar.svg'
+import CloseIcon from '../../../assets/icons/close.svg'
 import CopyIcon from '../../../assets/icons/copy.svg'
 import DoneIcon from '../../../assets/icons/done.svg'
 import EmailIcon from '../../../assets/icons/email.svg'
 import EuroIcon from '../../../assets/icons/euro.svg'
 import EventsIcon from '../../../assets/icons/events.svg'
+import FacebookIcon from '../../../assets/icons/facebook.svg'
 import FeedbackIcon from '../../../assets/icons/feedback.svg'
 import HouseIcon from '../../../assets/icons/house.svg'
 import KeyIcon from '../../../assets/icons/key.svg'
 import LanguageIcon from '../../../assets/icons/language.svg'
 import LgbtqiIcon from '../../../assets/icons/lgbtqi.svg'
 import LocationIcon from '../../../assets/icons/location.svg'
+import MailSocialIcon from '../../../assets/icons/mail-social.svg'
+import MessengerIcon from '../../../assets/icons/messenger.svg'
 import NegativeFeedbackIcon from '../../../assets/icons/negative-feedback.svg'
 import NewsIcon from '../../../assets/icons/news.svg'
 import NoteIcon from '../../../assets/icons/note.svg'
@@ -24,9 +28,13 @@ import PhoneIcon from '../../../assets/icons/phone.svg'
 import POIsIcon from '../../../assets/icons/pois.svg'
 import PositiveFeedbackIcon from '../../../assets/icons/positive-feedback.svg'
 import SearchIcon from '../../../assets/icons/search.svg'
+import ShareActiveIcon from '../../../assets/icons/share-active.svg'
+import ShareIcon from '../../../assets/icons/share.svg'
 import SmokingIcon from '../../../assets/icons/smoking.svg'
 import TimerIcon from '../../../assets/icons/timer.svg'
+import TwitterIcon from '../../../assets/icons/twitter.svg'
 import WebsiteIcon from '../../../assets/icons/website.svg'
+import WhatsappIcon from '../../../assets/icons/whatsapp.svg'
 
 /* eslint-enable import/no-relative-packages */
 
@@ -35,17 +43,21 @@ export {
   BathroomIcon,
   BedIcon,
   CalendarIcon,
+  CloseIcon,
   CopyIcon,
   DoneIcon,
   EmailIcon,
   EuroIcon,
   EventsIcon,
+  FacebookIcon,
   FeedbackIcon,
   HouseIcon,
   KeyIcon,
   LanguageIcon,
   LgbtqiIcon,
   LocationIcon,
+  MailSocialIcon,
+  MessengerIcon,
   NegativeFeedbackIcon,
   NewsIcon,
   NoteIcon,
@@ -56,7 +68,11 @@ export {
   POIsIcon,
   PositiveFeedbackIcon,
   SearchIcon,
+  ShareIcon,
+  ShareActiveIcon,
   SmokingIcon,
   TimerIcon,
+  TwitterIcon,
   WebsiteIcon,
+  WhatsappIcon,
 }
--- web/src/components/CityContentToolbar.tsx
+++ web/src/components/CityContentToolbar.tsx
@@ -1,9 +1,11 @@
 import React, { memo, ReactNode, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { CopyIcon, DoneIcon } from '../assets'
+import { CopyIcon, DoneIcon, ShareActiveIcon, ShareIcon } from '../assets'
+import useWindowDimensions from '../hooks/useWindowDimensions'
 import { RouteType } from '../routes'
 import FeedbackToolbarItem from './FeedbackToolbarItem'
+import SocialMediaTooltip from './SocialMediaTooltip'
 import Toolbar from './Toolbar'
 import ToolbarItem from './ToolbarItem'
 import Tooltip from './Tooltip'
@@ -14,15 +16,18 @@ type CityContentToolbarProps = {
   iconDirection?: 'row' | 'column'
   hasFeedbackOption?: boolean
   hideDivider?: boolean
+  title?: string
   route: RouteType
 }
 
 const COPY_TIMEOUT = 3000
 
 const CityContentToolbar = (props: CityContentToolbarProps) => {
-  const { feedbackTarget, children, iconDirection, hasFeedbackOption = true, hideDivider, route } = props
+  const { feedbackTarget, children, iconDirection, hasFeedbackOption = true, hideDivider, route, title } = props
   const [linkCopied, setLinkCopied] = useState<boolean>(false)
+  const [shareOptionsVisible, setShareOptionsVisible] = useState<boolean>(false)
   const { t } = useTranslation('categories')
+  const { viewportSmall } = useWindowDimensions()
   const copyToClipboard = () => {
     navigator.clipboard.writeText(window.location.href).catch(reportError)
     setLinkCopied(true)
@@ -34,6 +39,18 @@ const CityContentToolbar = (props: CityContentToolbarProps) => {
   return (
     <Toolbar iconDirection={iconDirection} hideDivider={hideDivider}>
       {children}
+      <SocialMediaTooltip
+        active={shareOptionsVisible}
+        shareLink={window.location.href}
+        direction={viewportSmall ? 'top' : 'right'}
+        title={title}
+        onClose={() => setShareOptionsVisible(false)}>
+        <ToolbarItem
+          icon={shareOptionsVisible ? ShareActiveIcon : ShareIcon}
+          text={t('shareUrl')}
+          onClick={() => setShareOptionsVisible(true)}
+        />
+      </SocialMediaTooltip>
       <Tooltip text={t('common:copied')} flow='up' active={linkCopied} trigger='click'>
         <ToolbarItem icon={linkCopied ? DoneIcon : CopyIcon} text={t('copyUrl')} onClick={copyToClipboard} />
       </Tooltip>
