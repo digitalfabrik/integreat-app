81d9348a79d7c7c4ef734b624911fd0f63a47acc
Steffen Kleinle <steffen.kleinle@mailbox.org>
2023-08-04 14:21:21 +0200

    IGAPP-1368: Fix poi detail design and icon alignment and refactor

---
+++ native/src/components/AddressInfo.tsx
@@ -0,0 +1,61 @@
+import Clipboard from '@react-native-clipboard/clipboard'
+import React, { ReactElement } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Platform, TouchableOpacity } from 'react-native'
+import styled from 'styled-components/native'
+
+import { getExternalMapsLink, LocationModel } from 'api-client'
+
+import ExternalLinkIcon from '../assets/ExternalLink.svg'
+import { contentDirection } from '../constants/contentDirection'
+import useSnackbar from '../hooks/useSnackbar'
+import openExternalUrl from '../utils/openExternalUrl'
+import { reportError } from '../utils/sentry'
+import Text from './base/Text'
+
+const Container = styled.View<{ language: string }>`
+  justify-content: space-between;
+  flex-direction: ${props => contentDirection(props.language)};
+`
+
+const IconContainer = styled.TouchableOpacity`
+  align-self: center;
+  padding: 0 8px;
+`
+
+type AddressInfoProps = {
+  location: LocationModel<number>
+  language: string
+}
+
+const AddressInfo = ({ location, language }: AddressInfoProps): ReactElement => {
+  const { address, postcode, town } = location
+  const showSnackbar = useSnackbar()
+  const { t } = useTranslation('pois')
+
+  const copyLocationToClipboard = (): void => {
+    Clipboard.setString(`${address}, ${postcode} ${town}`)
+    showSnackbar({ text: t('addressCopied') })
+  }
+
+  const openExternalMaps = () => {
+    const externalMapsUrl = getExternalMapsLink(location, Platform.OS)
+    openExternalUrl(externalMapsUrl, showSnackbar).catch(reportError)
+  }
+
+  return (
+    <Container language={language}>
+      <TouchableOpacity onPress={copyLocationToClipboard}>
+        <Text>{address}</Text>
+        <Text>
+          {postcode} {town}
+        </Text>
+      </TouchableOpacity>
+      <IconContainer onPress={openExternalMaps}>
+        <ExternalLinkIcon accessibilityLabel={t('openExternalMaps')} />
+      </IconContainer>
+    </Container>
+  )
+}
+
+export default AddressInfo
