760f55663c75778033269ea076ca4ecf00768e06
donwung <donwung@gmail.com>
2023-08-15 12:28:40 -0700

    Replaced JIRA issue numbers with GH issue numbers

--- api-client/src/i18n/DateFormatter.ts
+++ api-client/src/i18n/DateFormatter.ts
@@ -23,7 +23,7 @@ class DateFormatter {
     }
   ) => {
     const format = options.format || this.fallbackFormat
-    // TODO IGAPP-399: the actual locale should be used
+    // TODO #1384: the actual locale should be used
     // const requestedLocale = defaultLocale
     return format ? date.toFormat(format) : date.toISO()
   }
