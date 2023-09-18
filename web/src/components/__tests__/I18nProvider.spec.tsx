b4375b96c1cca74bc90fa1245c5a10a07c8e560a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2023-09-18 09:39:09 +0200

    2443: Hide more logs

--- web/src/components/__tests__/I18nProvider.spec.tsx
+++ web/src/components/__tests__/I18nProvider.spec.tsx
@@ -9,17 +9,21 @@ import I18nProvider from '../I18nProvider'
 
 jest.mock('../../utils/BrowserLanguageDetector')
 jest.mock('translations/src/loadTranslations')
+jest.mock('')
 
 describe('I18nProvider', () => {
   const mockDetect = mocked(BrowserLanguageDetector.detect)
   const previousConsoleLog = console.log
+  const previousConsoleDebug = console.debug
 
   beforeEach(() => {
     console.log = () => undefined
+    console.debug = () => undefined
   })
 
   afterEach(() => {
     console.log = previousConsoleLog
+    console.debug = previousConsoleDebug
   })
 
   it.each`
