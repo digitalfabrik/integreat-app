7b5fd7a5e09a3582af3c6749e73e316047e54571
LeandraH <leandravhahn@gmail.com>
2023-09-28 13:48:36 +0200

    1925: Actually capture edge case

--- native/src/components/FeedbackContainer.tsx
+++ native/src/components/FeedbackContainer.tsx
@@ -111,7 +111,7 @@ const FeedbackContainer = ({
       city: cityCode,
       language,
       comment: commentWithMail,
-      query: searchTerm ?? query,
+      query: searchTerm?.trim().length === 0 ? query : searchTerm,
       slug,
     }
   }
