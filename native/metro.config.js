efcff8f10e756084e4033cda6d2d9894d68ddb74
Sarah <sarah.sporck@tuerantuer.org>
2023-10-09 13:14:06 +0200

    2402: move metro-config to dev dependencies

--- native/metro.config.js
+++ native/metro.config.js
@@ -9,42 +9,42 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 const path = require('path')
 
-module.exports = baseConfig => {
-  /** @type {import('metro-config').MetroConfig} */
-  const defaultConfig = mergeConfig(baseConfig, getDefaultConfig(__dirname))
-  const {
-    resolver: { assetExts, sourceExts },
-  } = defaultConfig
+/** @type {import('metro-config').MetroConfig} */
+const defaultConfig = getDefaultConfig(__dirname)
+const {
+  resolver: { assetExts, sourceExts },
+} = defaultConfig
 
-  /** @type {import('metro-config').MetroConfig} */
-  const config = {
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-      extraNodeModules: {
-        'build-config-name': path.resolve(
-          __dirname,
-          '../build-configs',
-          process.env.BUILD_CONFIG_NAME || 'integreat-test-cms',
-          'build-config-name/index.ts',
-        ),
-      },
-      nodeModulesPaths: [path.resolve(__dirname, './node_modules')],
-      // Make sure we use the local copy of react and react-native to avoid multiple copies in the bundle
-      // https://github.com/facebook/react/issues/13991#issuecomment-830308729
-      resolveRequest: (context, moduleName, platform) => {
-        const module =
-          moduleName === 'react' || moduleName === 'react-native'
-            ? path.join(__dirname, 'node_modules', moduleName)
-            : moduleName
-        return context.resolveRequest(context, module, platform)
-      },
+/** @type {import('metro-config').MetroConfig} */
+const config = {
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+    extraNodeModules: {
+      'build-config-name': path.resolve(
+        __dirname,
+        '../build-configs',
+        process.env.BUILD_CONFIG_NAME || 'integreat-test-cms',
+        'build-config-name/index.ts',
+      ),
     },
-    watchFolders: [path.resolve(__dirname, '../')],
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    nodeModulesPaths: [path.resolve(__dirname, './node_modules')],
+    
+    // Make sure we use the local copy of react and react-native to avoid multiple copies in the bundle
+    // https://github.com/facebook/react/issues/13991#issuecomment-830308729
+    resolveRequest: (context, moduleName, platform) => {
+      const module =
+        moduleName === 'react' || moduleName === 'react-native'
+          ? path.join(__dirname, 'node_modules', moduleName)
+          : moduleName
+      return context.resolveRequest(context, module, platform)
     },
+  },
+  watchFolders: [path.resolve(__dirname, '../')],
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
   }
-
-  return config
 }
+  
+
+module.exports = mergeConfig(defaultConfig, config)
