0367a6e7c7501bfd262f2ff56fca5cc6da1b93a3
Sarah <sarah.sporck@tuerantuer.org>
2023-10-17 16:15:21 +0200

    2026: use global jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from '@jest/types'
+
+const config: Partial<Config.GlobalConfig> = {
+  rootDir: '.',
+  verbose: true,
+  projects: ['web', 'native', 'api-client', 'translations'],
+  reporters: [
+    ['default', {}],
+    [
+      'jest-junit',
+      {
+        outputDirectory: '<rootDir>/reports/unit-test',
+      },
+    ],
+  ],
+  coverageDirectory: '<rootDir>/reports/coverage',
+}
+
+export default config
--- translations/jest.config.ts
+++ translations/jest.config.ts
@@ -1,27 +1,10 @@
 export default {
   rootDir: 'src/',
-  verbose: true,
+  displayName: 'translations',
   automock: false,
-  setupFiles: ['<rootDir>/../jest.setup.ts'],
-  moduleDirectories: ['node_modules'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   maxWorkers: '50%',
   transform: {
-    '^.+\\.(j|t)sx?$': [
-      'ts-jest',
-      {
-        isolatedModules: true,
-      },
-    ],
+    '^.+\\.(j|t)sx?$': ['ts-jest', { isolatedModules: true }],
   },
-  coverageDirectory: '<rootDir>/../reports/coverage',
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        outputDirectory: '<rootDir>/../reports/unit-test',
-      },
-    ],
-  ],
 }
