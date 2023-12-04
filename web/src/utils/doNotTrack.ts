b1236dc760b29ae0eb9978a9fd0dd64985800791
Andreas Fischer <37902063+f1sh1918@users.noreply.github.com>
2023-12-04 22:28:00 +0100

    Update web/src/utils/doNotTrack.ts
    Co-authored-by: Steffen Kleinle <steffen.kleinle@mailbox.org>

--- web/src/utils/doNotTrack.ts
+++ web/src/utils/doNotTrack.ts
@@ -1,6 +1,6 @@
 // Vimeo supports do not track
 export const addDoNotTrackParameter = (iframe: HTMLIFrameElement): void => {
-  if (iframe.src.indexOf('vimeo') > -1) {
+  if (iframe.src.includes('vimeo')) {
     const url = new URL(iframe.src)
     url.searchParams.append('dnt', '1')
     iframe.setAttribute('src', url.href)
