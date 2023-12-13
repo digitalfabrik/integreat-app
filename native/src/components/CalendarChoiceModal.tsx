a3984ad8467e2e6d098522b3f9209fb09c4932a7
Leandra Hahn <LeandraH@users.noreply.github.com>
2023-12-13 10:50:29 +0100

    2117: Export recurring events (native) (#2561)
    * 2117: Export recurring events
    
    * 2117: Improve comment
    
    * 2117: Correct format for end date
    
    * 2117: Implement Code Review Points
    
    * 2117: Implement review points
    
    * 2117: Minor improvements
    
    * 2117: Improve legibility
    
    * 2117: Fix crash
    
    * 2117: Update rn-permissions to stable version

--- native/src/components/CalendarChoiceModal.tsx
+++ native/src/components/CalendarChoiceModal.tsx
@@ -1,30 +1,50 @@
-import React, { ReactElement } from 'react'
+import React, { Fragment, ReactElement, useState } from 'react'
 import { useTranslation } from 'react-i18next'
+import { FlatList, View } from 'react-native'
 import { Calendar } from 'react-native-calendar-events'
 import styled from 'styled-components/native'
 
-import dimensions from '../constants/dimensions'
 import Modal from './Modal'
-import Pressable from './base/Pressable'
+import RadioButton from './base/RadioButton'
+import TextButton from './base/TextButton'
 
-const CalendarButton = styled(Pressable)<{ color: string }>`
-  background-color: ${props => props.color};
-  justify-content: center;
-  align-items: center;
-  margin: 5px 0;
-  height: ${dimensions.headerHeight}px;
+const Heading = styled.Text`
+  font-family: ${props => props.theme.fonts.native.decorativeFontBold};
+  font-size: 16px;
+  margin: 16px 0;
 `
 
-const ButtonText = styled.Text`
+// styled-components doesn't have the right types for FlatList
+const StyledList = styled(FlatList as typeof FlatList<Calendar>)`
+  flex-grow: 0;
+`
+
+const ButtonTitle = styled.Text`
+  font-family: ${props => props.theme.fonts.native.decorativeFontBold};
+`
+
+const ButtonDescription = styled.Text`
+  font-family: ${props => props.theme.fonts.native.contentFontRegular};
+  color: ${props => props.theme.colors.textSecondaryColor};
+`
+
+const Divider = styled.View`
+  background-color: ${props => props.theme.colors.textDecorationColor};
+  height: 1px;
+  margin: 8px 0;
+`
+
+const StyledText = styled.Text`
   font-family: ${props => props.theme.fonts.native.decorativeFontRegular};
 `
 
 type CalendarChoiceProps = {
   calendars: Calendar[]
-  chooseCalendar: (id: string) => Promise<void>
+  chooseCalendar: (id: string | undefined, recurring: boolean) => Promise<void>
   modalVisible: boolean
   closeModal: () => void
   eventTitle: string
+  recurring: boolean
 }
 
 const CalendarChoiceModal = ({
@@ -33,15 +53,42 @@ const CalendarChoiceModal = ({
   modalVisible,
   closeModal,
   eventTitle,
+  recurring,
 }: CalendarChoiceProps): ReactElement => {
   const { t } = useTranslation('events')
+  const [selectedCalendarId, setSelectedCalendarId] = useState<string | undefined>(calendars[0]?.id ?? undefined)
+  const [exportAllEvents, setExportAllEvents] = useState<boolean>(false)
+  const calendarCount = calendars.length
+
   return (
-    <Modal modalVisible={modalVisible} closeModal={closeModal} headerTitle={eventTitle} title={t('chooseCalendar')}>
-      {calendars.map(cal => (
-        <CalendarButton key={cal.id} onPress={() => chooseCalendar(cal.id)} color={cal.color}>
-          <ButtonText>{cal.title}</ButtonText>
-        </CalendarButton>
-      ))}
+    <Modal modalVisible={modalVisible} closeModal={closeModal} headerTitle={eventTitle} scrollView={false}>
+      <Heading>{t('chooseCalendar')}</Heading>
+      <StyledList
+        data={calendars}
+        renderItem={({ item, index }) => (
+          <>
+            <RadioButton selected={selectedCalendarId === item.id} select={() => setSelectedCalendarId(item.id)}>
+              <View>
+                <ButtonTitle>{item.title}</ButtonTitle>
+                <ButtonDescription>{item.source}</ButtonDescription>
+              </View>
+            </RadioButton>
+            {index < calendarCount - 1 && <Divider />}
+          </>
+        )}
+      />
+      {recurring && (
+        <>
+          <Heading>{t('addToCalendar')}</Heading>
+          <RadioButton selected={!exportAllEvents} select={() => setExportAllEvents(false)}>
+            <StyledText>{t('onlyThisEvent')}</StyledText>
+          </RadioButton>
+          <RadioButton selected={exportAllEvents} select={() => setExportAllEvents(true)}>
+            <StyledText>{t('thisAndAllFutureEvents')}</StyledText>
+          </RadioButton>
+        </>
+      )}
+      <TextButton onPress={() => chooseCalendar(selectedCalendarId, exportAllEvents)} text={t('addToCalendar')} />
     </Modal>
   )
 }
