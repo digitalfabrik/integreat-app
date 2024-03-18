3c574b220b33e215f77349d2ed62f37da4108adf
relentless95 <marcaureltagne@yahoo.com>
2024-03-18 14:11:59 +0100

    2457: added  Only by appointemnt to the opening hours of POIS

--- web/src/components/OpeningEntry.tsx
+++ web/src/components/OpeningEntry.tsx
@@ -14,32 +14,31 @@ const fontStandard = 400
 const EntryContainer = styled.div<{ isCurrentDay: boolean }>`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   padding: 4px 0;
   font-weight: ${props => (props.isCurrentDay ? fontBold : fontStandard)};
-  border: 1px solid red;
 `
 const Timeslot = styled.div`
-  display: flex;
-  flex-direction: column;
+  /* display: flex;
+  flex-direction: row; */
 `
 
 const TimeSlotEntry = styled.span`
   &:not(:first-child) {
     margin-top: 8px;
   }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
 `
 
-const StyledTooltip = styled(Tooltip)`
-  height: 8px;
-  display: inline-block;
-  z-index: 1000000000;
-`
-
-const Tip = (
-  <StyledTooltip text='only by appoitment' flow='right'>
-    <Icon src={NoteIcon} />
-  </StyledTooltip>
-)
+// const Tip = (
+//   <StyledTooltip text='only by appoitment' flow='right'>
+//     <Icon src={NoteIcon} />
+//   </StyledTooltip>
+// )
 
 type OpeningEntryProps = {
   allDay: boolean
@@ -51,30 +50,20 @@ type OpeningEntryProps = {
 
 const OpeningEntry = ({ allDay, closed, timeSlots, weekday, isCurrentDay }: OpeningEntryProps): ReactElement => {
   const { t } = useTranslation('pois')
-  console.log(
-    'allDay',
-    allDay,
-    'closed',
-    closed,
-    'timeSlots',
-    timeSlots,
-    'weekday',
-    weekday,
-    'isCurrentDay',
-    isCurrentDay,
-  )
 
   return (
     <EntryContainer isCurrentDay={isCurrentDay} id={`openingEntryContainer-${weekday}`}>
       <span>{weekday}</span>
-      {allDay && <span>{t('allDay')}allDayy</span>}
-      {closed && <span>{t('closed')}closedd</span>}
+      {allDay && <span>{t('allDay')}</span>}
+      {closed && <span>{t('closed')}</span>}
       {!allDay && !closed && timeSlots.length > 0 && (
         <Timeslot>
           {timeSlots.map(timeSlot => (
             <TimeSlotEntry key={`${weekday}-${timeSlot.start}`}>
               {timeSlot.start}-{timeSlot.end}
-              {Tip}
+              <Tooltip text='only by appointment' flow='up'>
+                <Icon src={NoteIcon} />
+              </Tooltip>
             </TimeSlotEntry>
           ))}
         </Timeslot>
