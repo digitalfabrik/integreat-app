15ce4b2c71a560c011108ed058fd7078b1a0cdb5
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-03-18 13:53:48 +0100

    2675: Rename isExternalUrl

--- web/src/utils/isExternalUrl.ts
+++ web/src/utils/isExternalUrl.ts
@@ -1,4 +1,4 @@
-export const isExternalUrl = (link: string): boolean => {
+const isExternalUrl = (link: string): boolean => {
   // If it is possible to create a URL from the link, it is an absolute and therefore an external url
   // If it throws an error, it is relative and therefore an internal link
   // Might be refactored to URL.canParse() at a later point, got only implemented in 2023 in most browsers
@@ -9,3 +9,5 @@ export const isExternalUrl = (link: string): boolean => {
     return false
   }
 }
+
+export default isExternalUrl
