80a4c8635cf4a447890817d8d9bd98bcceb3a184
ztefanie <stefanie.metzger@tuerantuer.org>
2024-03-19 09:18:28 +0100

    2711-1: Fixes for adding apks

--- tools/github-release.ts
+++ tools/github-release.ts
@@ -8,36 +8,40 @@ type Options = {
   owner: string
   repo: string
   releaseNotes: string
-  prerelease: string
+  productionDelivery: string
 }
 
 const githubRelease = async (
   platform: string,
   newVersionName: string,
   newVersionCode: string,
-  { deliverinoPrivateKey, owner, repo, releaseNotes, prerelease }: Options,
+  { deliverinoPrivateKey, owner, repo, releaseNotes, productionDelivery }: Options,
 ): Promise<void> => {
   const versionCode = parseInt(newVersionCode, 10)
   if (Number.isNaN(versionCode)) {
     throw new Error(`Failed to parse version code string: ${newVersionCode}`)
   }
 
+  console.log('production_delivery: ', productionDelivery)
+  console.log('productionDelivery === true: ', productionDelivery === 'true')
+  console.log('productionDelivery === false: ', productionDelivery === 'false')
+
   const releaseName = `[${platform}] ${newVersionName} - ${versionCode}`
-  const body = releaseNotes
+  const body = JSON.parse(releaseNotes)
   const appOctokit = await authenticate({ deliverinoPrivateKey, owner, repo })
 
-  const release = await appOctokit.repos.createRelease({
-    owner,
-    repo,
-    tag_name: tagId({ versionName: newVersionName, platform }),
-    prerelease: prerelease === 'true',
-    make_latest: platform === 'android' ? 'true' : 'false',
-    name: releaseName,
-    body,
-  })
+  // const release = await appOctokit.repos.createRelease({
+  //   owner,
+  //   repo,
+  //   tag_name: tagId({ versionName: newVersionName, platform }),
+  //   prerelease: prerelease === 'true',
+  //   make_latest: platform === 'android' ? 'true' : 'false',
+  //   name: releaseName,
+  //   body,
+  // })
 
   // This command returns the release id of the created release, which is later needed to make updates for this release.
-  console.log(release.data.id)
+  //console.log(release.data.id)
 }
 
 program
@@ -50,7 +54,7 @@ program
   .requiredOption('--owner <owner>', 'owner of the current repository, usually "digitalfabrik"')
   .requiredOption('--repo <repo>', 'the current repository, should be integreat-app')
   .requiredOption('--release-notes <release-notes>', 'the release notes (for the selected platform) as JSON string')
-  .requiredOption('--prerelease <prerelease>', 'weather this is a prerelease (beta) or not')
+  .requiredOption('--production-delivery <production-delivery>', 'weather this is a production build or not')
   .action(async (platform: string, newVersionName: string, newVersionCode: string, options: Options) => {
     try {
       await githubRelease(platform, newVersionName, newVersionCode, options)
