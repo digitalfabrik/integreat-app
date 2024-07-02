33a3d762c6b321e9938b3476ea374b2dab6abe7b
LeandraH <leandravhahn@gmail.com>
2024-07-02 14:24:57 +0200

    2457: WIP Only by appointment

--- web/src/components/OpeningEntry.tsx
+++ web/src/components/OpeningEntry.tsx
@@ -1,8 +1,14 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
 import { TimeSlot } from 'shared/api/types'
+
+import { NoteIcon } from '../assets'
+import Portal from './Portal'
+import SharingPopup from './SharingPopup'
+import Tooltip from './Tooltip'
+import Icon from './base/Icon'
 
 const fontBold = 600
 const fontStandard = 400
@@ -12,7 +18,9 @@ const EntryContainer = styled.div<{ isCurrentDay: boolean }>`
   justify-content: space-between;
   padding: 4px 0;
   font-weight: ${props => (props.isCurrentDay ? fontBold : fontStandard)};
+  position: relative;
 `
+
 const Timeslot = styled.div`
   display: flex;
   flex-direction: column;
@@ -24,19 +32,59 @@ const TimeSlotEntry = styled.span`
   }
 `
 
+const StyledIcon = styled(Icon)`
+  width: 18px;
+  height: 18px;
+`
+
 type OpeningEntryProps = {
   allDay: boolean
   closed: boolean
   timeSlots: TimeSlot[]
   weekday: string
   isCurrentDay: boolean
+  appointmentOnly: boolean
 }
 
-const OpeningEntry = ({ allDay, closed, timeSlots, weekday, isCurrentDay }: OpeningEntryProps): ReactElement => {
+const OpeningEntry = ({
+  allDay,
+  closed,
+  timeSlots,
+  weekday,
+  isCurrentDay,
+  appointmentOnly,
+}: OpeningEntryProps): ReactElement => {
   const { t } = useTranslation('pois')
 
+  const positioningRef = useRef<HTMLDivElement>(null)
+  const [iconOffsetLeft, setIconOffsetLeft] = useState(0)
+  const [iconOffsetTop, setIconOffsetTop] = useState(0)
+
+  useEffect(() => {
+    if (positioningRef.current) {
+      setIconOffsetLeft(positioningRef.current.getBoundingClientRect().right + 10)
+    }
+  }, [positioningRef.current])
+
+  const updateOffsetTop = () => {
+    if (positioningRef.current) {
+      setIconOffsetTop(positioningRef.current.getBoundingClientRect().top)
+    }
+  }
+
+  useEffect(() => {
+    if (!appointmentOnly) {
+      return
+    }
+    updateOffsetTop()
+    window.addEventListener('wheel', updateOffsetTop)
+    return () => {
+      window.removeEventListener('wheel', updateOffsetTop)
+    }
+  }, [])
+
   return (
-    <EntryContainer isCurrentDay={isCurrentDay} id={`openingEntryContainer-${weekday}`}>
+    <EntryContainer isCurrentDay={isCurrentDay} id={`openingEntryContainer-${weekday}`} ref={positioningRef}>
       <span>{weekday}</span>
       {allDay && <span>{t('allDay')}</span>}
       {closed && <span>{t('closed')}</span>}
@@ -48,6 +96,23 @@ const OpeningEntry = ({ allDay, closed, timeSlots, weekday, isCurrentDay }: Open
             </TimeSlotEntry>
           ))}
         </Timeslot>
+      )}
+      {/* TODO: Put into a separate component */}
+      {appointmentOnly && (
+        <>
+          <Portal className='appointment-only-tooltip' show>
+            <div
+              style={{
+                position: 'fixed',
+                left: iconOffsetLeft,
+                top: iconOffsetTop,
+              }}>
+              <Tooltip text={'to be translated'} flow='up' active>
+                <StyledIcon src={NoteIcon} />
+              </Tooltip>
+            </div>
+          </Portal>
+        </>
       )}
     </EntryContainer>
   )
