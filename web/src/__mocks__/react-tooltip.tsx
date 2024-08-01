24ad1a4b55613295b25be81065739b8659d80702
LeandraH <leandravhahn@gmail.com>
2024-08-01 17:24:43 +0200

    2457: Fix lint

--- web/src/__mocks__/react-tooltip.tsx
+++ web/src/__mocks__/react-tooltip.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
 
-const Tooltip = ({ children }: { children: ReactNode }) => <div>{children}</div>
+const Tooltip = ({ children }: { children: ReactNode }): ReactNode => <div>{children}</div>
 
 export { Tooltip }
