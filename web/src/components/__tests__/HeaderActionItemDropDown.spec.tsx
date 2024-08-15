0da33beb452a17d175b3950dc6e344783a22851b
Bahaa Tuffaha <bahaa.tuffaha@yahoo.com>
2024-08-15 12:24:37 +0300

    2831: Adding a Jest test for HeaderActionItemDropDown

--- web/src/components/__tests__/HeaderActionItemDropDown.spec.tsx
+++ web/src/components/__tests__/HeaderActionItemDropDown.spec.tsx
@@ -84,3 +84,43 @@ describe('HeaderActionItemDropDown', () => {
     expect(inner).not.toBeVisible()
   })
 })
+
+describe('HeaderActionItemDropDownWithInnerText', () => {
+  let wrapperComponent: RenderResult
+  let inner: HTMLElement
+  let innerText: HTMLElement
+
+  beforeEach(() => {
+    const InnerComponent = ({ closeDropDown }: { closeDropDown: () => void }) => (
+      <span onClick={closeDropDown}>Do you see me?</span>
+    )
+
+    wrapperComponent = renderWithTheme(
+      <>
+        <span>Click me to trigger dropdown!</span>
+        <HeaderActionItemDropDown iconSrc='/someImg' text='some text' innerText='English'>
+          {closeDropDown => <InnerComponent closeDropDown={closeDropDown} />}
+        </HeaderActionItemDropDown>
+      </>,
+    )
+
+    inner = wrapperComponent.getByText('Do you see me?')
+    innerText = wrapperComponent.getByText('English')
+  })
+
+  afterEach(() => {
+    cleanup()
+  })
+
+  it('should show inner box text when passing innerText and should open & close dropdown', () => {
+    expect(innerText).toBeVisible()
+
+    const button = wrapperComponent.getByRole('button')
+    fireEvent.click(button)
+
+    expect(inner).toBeVisible()
+
+    fireEvent.click(button)
+    expect(wrapperComponent.queryByText('Do you see me?')).not.toBeVisible()
+  })
+})
