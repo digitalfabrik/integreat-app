8dc36faaa22c85d083858e097034e64ea637f539
LeandraH <leandravhahn@gmail.com>
2024-08-20 11:59:04 +0200

    2457: Clean up

--- web/src/components/base/Tooltip.tsx
+++ web/src/components/base/Tooltip.tsx
@@ -4,18 +4,10 @@ import { Tooltip as ReactTooltip, ITooltip as ReactTooltipType } from 'react-too
 type TooltipProps = {
   tooltipContent: ReactNode
   children: ReactNode
-  // container: ReactElement
   id: string
 } & ReactTooltipType
 
 const Tooltip = ({ children, id, place, tooltipContent, ...props }: TooltipProps): ReactElement => (
-  // <>
-  //   <div id={id}>{container}</div>
-  //   <ReactTooltip {...props} anchorSelect={`#${id}`}>
-  //     {children}
-  //   </ReactTooltip>
-  // </>
-
   <>
     <div id={id}>{children}</div>
     <ReactTooltip {...props} anchorSelect={`#${id}`}>
