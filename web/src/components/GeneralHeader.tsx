eec953771fab2a1585a7d18435c99f7c91cf57a1
LeandraH <leandravhahn@gmail.com>
2024-08-21 12:15:22 +0200

    2871: Fix merge conflict correctly

--- web/src/components/GeneralHeader.tsx
+++ web/src/components/GeneralHeader.tsx
@@ -17,7 +17,7 @@ const GeneralHeader = ({ languageCode }: GeneralHeaderProps): ReactElement => {
   const landingPath = pathnameFromRouteInformation({ route: LANDING_ROUTE, languageCode })
   const actionItems = buildConfig().featureFlags.fixedCity
     ? []
-    : [<HeaderActionItemLink key='landing' href={landingPath} iconSrc={LocationIcon} text={t('changeLocation')} />]
+    : [<HeaderActionItemLink key='landing' to={landingPath} iconSrc={LocationIcon} text={t('changeLocation')} />]
 
   return (
     <Header
