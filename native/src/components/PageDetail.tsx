89b56aa105a4e7a1e9e636e500470b6f96886a2f
LeandraH <leandravhahn@gmail.com>
2024-08-26 16:08:08 +0200

    2223: Fix test

--- native/src/components/PageDetail.tsx
+++ native/src/components/PageDetail.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react'
-import { Pressable, Text } from 'react-native'
 import styled from 'styled-components/native'
 
 import { InternalPathnameParser } from 'shared'
@@ -8,7 +7,7 @@ import buildConfig from '../constants/buildConfig'
 import { contentDirection } from '../constants/contentDirection'
 import useNavigate from '../hooks/useNavigate'
 
-const DetailContainer = styled.View<{ language: string }>`
+const DetailContainer = styled.Text<{ language: string }>`
   flex-direction: ${props => contentDirection(props.language)};
   font-family: ${props => props.theme.fonts.native.contentFontRegular};
   color: ${props => props.theme.colors.textColor};
@@ -21,18 +20,13 @@ const Identifier = styled.Text`
 
 const StyledButton = styled.Pressable`
   /* Offset for bold sibling + underline */
-  margin-top: 1px;
+  margin-top: -2px;
 `
 
 const ButtonText = styled.Text`
   color: ${props => props.theme.colors.linkColor};
   text-decoration: underline;
   text-decoration-color: ${props => props.theme.colors.linkColor};
-`
-
-const StyledText = styled.Text`
-  /* Offset for bold sibling */
-  margin-top: 2px;
 `
 
 type PageDetailProps = {
@@ -60,7 +54,7 @@ const PageDetail = ({ identifier, information, language, locationPath }: PageDet
   return (
     <DetailContainer language={language}>
       <Identifier>{identifier}: </Identifier>
-      <StyledText>{information}</StyledText>
+      {information}
     </DetailContainer>
   )
 }
--- native/src/components/__tests__/PageDetail.spec.tsx
+++ native/src/components/__tests__/PageDetail.spec.tsx
@@ -1,3 +1,4 @@
+import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import { I18nManager } from 'react-native'
 
@@ -7,16 +8,21 @@ import PageDetail from '../PageDetail'
 describe('PageDetail', () => {
   it('should display the given identifier followed by a colon', () => {
     const { queryAllByText, queryByText } = render(
-      <PageDetail identifier='Test Identifier' information='Some important information' language='de' />,
+      <NavigationContainer>
+        <PageDetail identifier='Test Identifier' information='Some important information' language='de' />
+      </NavigationContainer>,
     )
     expect(queryAllByText(/Test Identifier/)).toBeTruthy()
     expect(queryByText(/Some important information/)).toBeTruthy()
   })
+
   describe('should manually reverse if content language doesnt have the same direction as system language', () => {
     it('if system language is not rtl', () => {
       I18nManager.forceRTL(false)
       const { queryAllByText } = render(
-        <PageDetail identifier='Test Identifier' information='Some important information' language='de' />,
+        <NavigationContainer>
+          <PageDetail identifier='Test Identifier' information='Some important information' language='de' />
+        </NavigationContainer>,
       )
       queryAllByText(/Some important information/).forEach(element => {
         expect(element).toHaveStyle({
@@ -24,7 +30,9 @@ describe('PageDetail', () => {
         })
       })
       const { queryAllByText: queryAllByTextReverse } = render(
-        <PageDetail identifier='Test Identifier' information='Some important information' language='ar' />,
+        <NavigationContainer>
+          <PageDetail identifier='Test Identifier' information='Some important information' language='ar' />
+        </NavigationContainer>,
       )
       queryAllByTextReverse(/Some important information/).forEach(element => {
         expect(element).toHaveStyle({
@@ -32,10 +40,13 @@ describe('PageDetail', () => {
         })
       })
     })
+
     it('if system language is rtl', () => {
       I18nManager.forceRTL(true)
       const { queryAllByText: queryAllByTextReverse } = render(
-        <PageDetail identifier='Test Identifier' information='Some important information' language='de' />,
+        <NavigationContainer>
+          <PageDetail identifier='Test Identifier' information='Some important information' language='de' />
+        </NavigationContainer>,
       )
       queryAllByTextReverse(/Some important information/).forEach(element => {
         expect(element).toHaveStyle({
@@ -43,7 +54,9 @@ describe('PageDetail', () => {
         })
       })
       const { queryAllByText } = render(
-        <PageDetail identifier='Test Identifier' information='Some important information' language='ar' />,
+        <NavigationContainer>
+          <PageDetail identifier='Test Identifier' information='Some important information' language='ar' />
+        </NavigationContainer>,
       )
       queryAllByText(/Some important information/).forEach(element => {
         expect(element).toHaveStyle({
