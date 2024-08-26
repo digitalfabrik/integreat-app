546b3a3c7911cbc18dffa4cc4e3458680cb09512
LeandraH <leandravhahn@gmail.com>
2024-08-26 12:37:05 +0200

    2223: Add test

--- web/src/components/__tests__/PageDetail.spec.tsx
+++ web/src/components/__tests__/PageDetail.spec.tsx
@@ -6,10 +6,20 @@ import PageDetail from '../PageDetail'
 describe('PageDetail', () => {
   const identifier = 'Date'
   const information = 'May 22, 2020 1:00 AM'
+  const path = '/testumgebung/de/location1'
 
-  it('should render correctly', () => {
+  it('should render correctly without a path', () => {
     const { getByText } = renderWithRouterAndTheme(<PageDetail identifier={identifier} information={information} />)
     expect(getByText(`${identifier}:`)).toBeDefined()
     expect(getByText(information)).toBeDefined()
+  })
+
+  it('should render correctly with a path', () => {
+    const { getByText, getByRole, debug } = renderWithRouterAndTheme(
+      <PageDetail identifier={identifier} information={information} pathToInformation={path} />,
+    )
+    expect(getByText(`${identifier}:`)).toBeDefined()
+    expect(getByText(information)).toBeDefined()
+    expect(getByRole('link', { name: information })).toHaveAttribute('href', path)
   })
 })
