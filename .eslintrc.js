3a17b0429d0994349dc801b88de4f6d45e01f58c
Steffen Kleinle <steffen.kleinle@mailbox.org>
2024-09-04 13:20:56 +0200

    2553: Disable in tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -166,6 +166,7 @@ module.exports = {
         'jsx-a11y/click-events-have-key-events': 'off',
         'jsx-a11y/no-static-element-interactions': 'off',
         'react/jsx-no-constructed-context-values': 'off',
+        'react-native-a11y/has-valid-accessibility-descriptors': 'off',
       },
     },
     {
