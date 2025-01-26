96548e753887475931d6f1b7bb72c2629cceaca5
lunars97 <alekbarova.aizhana@gmail.com>
2025-01-26 19:16:04 +0100

    3020: Fix unexpected scrolling behaviour in mobile desktop

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -16,6 +16,8 @@ import { initSentry } from './utils/sentry'
 
 const GlobalStyle = createGlobalStyle`
   body {
+    position: relative;
+  
     /* Styling for react-tooltip: https://react-tooltip.com/docs/getting-started#styling */
     --rt-color-dark: ${props => props.theme.colors.textSecondaryColor};
     --rt-color-white: ${props => props.theme.colors.backgroundColor};
