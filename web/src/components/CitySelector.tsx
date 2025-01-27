1b769b7ed8db6871b142b0ecf25120eb3bbdc2d5
bahaa <bahaa.tuffaha@yahoo.com>
2025-01-27 20:17:30 +0200

    2356: Show nearby cities

--- web/src/components/CitySelector.tsx
+++ web/src/components/CitySelector.tsx
@@ -1,11 +1,12 @@
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement, useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled from 'styled-components'
 
-import { filterSortCities } from 'shared'
-import { CityModel } from 'shared/api'
+import { filterSortCities, getNearbyCities, LocationType } from 'shared'
+import { CityModel, useLoadAsync } from 'shared/api'
 
 import buildConfig from '../constants/buildConfig'
+import getUserLocation from '../utils/getUserLocation'
 import CityEntry from './CityEntry'
 import CrashTestingIcon from './CrashTestingIcon'
 import Failure from './Failure'
@@ -25,9 +26,36 @@ const CityListParent = styled.div<{ $stickyTop: number }>`
   background-color: ${props => props.theme.colors.backgroundColor};
   border-bottom: 1px solid ${props => props.theme.colors.themeColor};
 `
+
 const SearchCounter = styled.p`
   color: ${props => props.theme.colors.textSecondaryColor};
 `
+
+type NearbyCitiesProps = {
+  userLocation: LocationType | null
+  cities: CityModel[]
+  language: string
+  filterText: string
+}
+
+const NearbyCities = ({ userLocation, cities, language, filterText }: NearbyCitiesProps) => {
+  const { t } = useTranslation('landing')
+  const nearCities = userLocation
+    ? getNearbyCities(
+        userLocation,
+        cities.filter(city => city.live),
+      )
+    : []
+
+  return nearCities.length > 0 ? (
+    <div key='nearbyCities'>
+      <CityListParent $stickyTop={0}>{t('nearbyCities')}</CityListParent>
+      {nearCities.map(city => (
+        <CityEntry key={city.code} city={city} language={language} filterText={filterText} />
+      ))}
+    </div>
+  ) : null
+}
 
 type CitySelectorProps = {
   cities: CityModel[]
@@ -38,6 +66,13 @@ const CitySelector = ({ cities, language }: CitySelectorProps): ReactElement => 
   const [filterText, setFilterText] = useState<string>('')
   const [stickyTop, setStickyTop] = useState<number>(0)
   const { t } = useTranslation('landing')
+
+  const { data: userLocation } = useLoadAsync(
+    useCallback(async () => {
+      const userLocation = await getUserLocation()
+      return userLocation.status === 'ready' ? userLocation.coordinates : null
+    }, []),
+  )
 
   const resultCities = filterSortCities(cities, filterText, buildConfig().featureFlags.developerFriendly)
 
@@ -62,6 +97,7 @@ const CitySelector = ({ cities, language }: CitySelectorProps): ReactElement => 
         spaceSearch={false}
         onStickyTopChanged={setStickyTop}>
         <SearchCounter>{t('search:searchResultsCount', { count: resultCities.length })}</SearchCounter>
+        <NearbyCities userLocation={userLocation} cities={cities} language={language} filterText={filterText} />
         {resultCities.length === 0 ? <Failure errorMessage='search:nothingFound' /> : groups}
       </ScrollingSearchBox>
     </Container>
