b6d3abe627333d43fa338ab5a61b23da4deb7134
LeandraH <LeandraH@users.noreply.github.com>
2025-01-29 19:10:25 +0100

    2352: Fix lint

--- native/src/hooks/useAnnounceSearchResultsIOS.ts
+++ native/src/hooks/useAnnounceSearchResultsIOS.ts
@@ -2,8 +2,6 @@ import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { AccessibilityInfo, Platform } from 'react-native'
 
-import { CityModel, ExtendedPageModel } from 'shared/api'
-
 const useAnnounceSearchResultsIOS = <T>(searchResults: T[] | null): void => {
   const { t } = useTranslation('search')
 
