ac0429cad22cb96bab8a47c3a1925f364821453f
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-02-17 13:15:34 +0100

    3100: Restructure fastlane

--- native/fastlane/util.rb
+++ native/fastlane/util.rb
@@ -4,3 +4,9 @@ def read_build_config(build_config_name, platform)
   json = `yarn workspace --silent build-configs --silent manage to-json #{build_config_name} #{platform}`
   JSON.parse(json)
 end
+
+def assert_parameters(parameters)
+  if parameters.include?(nil)
+    raise "'nil' passed as parameter! Aborting..."
+  end
+end
