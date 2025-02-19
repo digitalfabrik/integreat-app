8c6a69118b8641ce372f29b271a3f225fe0d7ddc
Hanna Seithe <hannaseithe@googlemail.com>
2025-02-19 12:27:05 +0100

    2992: Replaced dependency 'normalize-string' with string.normalize()

---
+++ shared/utils/normalizeString.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-control-regex
+const nonAsciiRegex = /[^\x00-\x7F]/g
+
+const normalizeToAscii = (str: string): string => str.normalize('NFKD').replace(nonAsciiRegex, '')
+
+export const normalizeString = (str: string): string => normalizeToAscii(str).toLowerCase().trim()
+
+export default normalizeString
