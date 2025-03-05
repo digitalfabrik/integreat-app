9a77e5e2e9516fb6e33a5c712fc27ddd4f6bfba1
bahaa <bahaa.tuffaha@yahoo.com>
2025-03-05 14:38:30 +0200

    3088: Requested changes about restructuring functions

--- tools/github-promote-release.ts
+++ tools/github-promote-release.ts
@@ -14,33 +14,23 @@ type Options = {
   platform: 'web' | 'android' | 'ios'
 }
 
-const getAllPreReleased = async ({ deliverinoPrivateKey, owner, repo, platform }: Options) => {
+const getReleases = async ({ deliverinoPrivateKey, owner, repo, platform }: Options) => {
   const appOctokit = await authenticate({ deliverinoPrivateKey, owner, repo })
 
   const releases: Releases = await appOctokit.rest.repos.listReleases({
     owner,
     repo,
   })
-  const releasesWithTags = releases.data.filter(release => release.tag_name.includes(platform) && release.prerelease)
-  const isLatestPreReleased =
-    releases.data.filter(release => release.tag_name?.includes(platform))[0]?.prerelease === true
-
-  if (releasesWithTags.length > 0) {
-    return { preReleases: releasesWithTags, isLatestPreReleased: isLatestPreReleased }
-  }
-
-  console.warn('No release found to unset the prerelease tag for. Latest release may already be non-prerelease')
-  return null
+  return releases.data.filter(release => release.tag_name.includes(platform))
 }
 
-const removePreRelease = async ({ deliverinoPrivateKey, owner, repo, platform }: Options) => {
-  const allPreReleases = await getAllPreReleased({ deliverinoPrivateKey, owner, repo, platform })
-  if (!allPreReleases) {
-    return null
-  }
+const promoteReleases = async ({ deliverinoPrivateKey, owner, repo, platform }: Options) => {
+  const releases = await getReleases({ deliverinoPrivateKey, owner, repo, platform })
+  const preReleases = releases.filter(release => release.prerelease)
+
   const appOctokit = await authenticate({ deliverinoPrivateKey, owner, repo })
   await Promise.all(
-    allPreReleases.preReleases.map(async preRelease => {
+    preReleases.map(async preRelease => {
       const result = await appOctokit.rest.repos.updateRelease({
         owner,
         repo,
@@ -51,15 +41,15 @@ const removePreRelease = async ({ deliverinoPrivateKey, owner, repo, platform }:
       console.warn('Http response code of updating the result: ', result.status)
     }),
   )
-  const filteredTagNames = allPreReleases.preReleases.map(release => release.tag_name)
+
+  const filteredTagNames = preReleases.map(release => release.tag_name)
   console.warn('Unset prerelease tags of ', filteredTagNames)
 
-  if (allPreReleases.isLatestPreReleased) {
-    return allPreReleases.preReleases[0]
-  } else {
-    console.warn('Note: latest release is not tagged with prerelease')
-    return null
+  if (preReleases[0]?.prerelease) {
+    return preReleases[0]
   }
+  console.warn('Nothing to promote')
+  return null
 }
 
 program
@@ -74,7 +64,7 @@ program
   .requiredOption('--platform <platform>')
   .action(async (options: Options) => {
     try {
-      const promotedRelease = await removePreRelease(options)
+      const promotedRelease = await promoteReleases(options)
       if (promotedRelease) {
         console.log(
           `The most recent beta version was promoted to production:\n[${promotedRelease.name}](${promotedRelease.html_url})`,
