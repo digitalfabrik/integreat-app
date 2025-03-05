dd1be9a7d021370eafb7141aacb3663bb1644013
lunars97 <alekbarova.aizhana@gmail.com>
2025-03-05 10:09:04 +0100

    3135: Fix tooltip being half hidden on language change hover

--- web/src/components/base/Tooltip.tsx
+++ web/src/components/base/Tooltip.tsx
@@ -3,7 +3,7 @@ import { Tooltip as ReactTooltip, ITooltip as ReactTooltipType } from 'react-too
 import styled from 'styled-components'
 
 const StyledTooltip = styled(ReactTooltip)`
-  z-index: 1;
+  z-index: 2;
 `
 
 type TooltipProps = {
