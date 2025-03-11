82ff2a3d5380cdceb00da97ef4c07f813b83a283
LeandraH <LeandraH@users.noreply.github.com>
2025-03-11 11:05:09 +0100

    3121: Increase cache version

--- native/src/utils/DatabaseConnector.ts
+++ native/src/utils/DatabaseConnector.ts
@@ -32,7 +32,7 @@ import {
 import { deleteIfExists } from './helpers'
 import { log, reportError } from './sentry'
 
-export const CONTENT_VERSION = 'v8'
+export const CONTENT_VERSION = 'v9'
 export const RESOURCE_CACHE_VERSION = 'v1'
 
 // Our pdf view can only load from DocumentDir. Therefore we need to use that
