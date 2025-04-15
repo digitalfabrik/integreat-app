48faf3d8b67b768ea4b1021984e9e8321dd33e0d
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-04-15 09:04:47 +0200

    2928: Report search errors

--- shared/hooks/useSearch.ts
+++ shared/hooks/useSearch.ts
@@ -20,11 +20,18 @@ export const prepareSearchDocuments = (
   ...(pois || []),
 ]
 
+type UseSearchReturn = {
+  data: SearchResult[]
+  error: Error | null
+  loading: boolean
+}
+
 // WARNING: This uses the document count to check whether the search documents have already been added.
 // Modifying single documents or replacing documents with a same length array will therefore NOT trigger an update
-const useSearch = (documents: SearchResult[], query: string): SearchResult[] => {
+const useSearch = (documents: SearchResult[], query: string): UseSearchReturn => {
   const [indexing, setIndexing] = useState(false)
   const [debouncedQuery, setDebouncedQuery] = useState(normalizeString(query))
+  const [error, setError] = useState<Error | null>(null)
 
   const [search] = useState(
     new MiniSearch({
@@ -55,12 +62,15 @@ const useSearch = (documents: SearchResult[], query: string): SearchResult[] => 
       search
         .addAllAsync(documents)
         .then(() => setIndexing(false))
-        .catch()
+        .catch(setError)
     }
   }, [indexing, search, documents])
 
-  // @ts-expect-error minisearch doesn't add the returned storeFields (e.g. title or path) to its typing
-  return debouncedQuery.length === 0 ? documents : search.search(debouncedQuery)
+  return {
+    data: debouncedQuery.length === 0 ? documents : (search.search(debouncedQuery) as unknown as SearchResult[]),
+    error,
+    loading: indexing,
+  }
 }
 
 export default useSearch
