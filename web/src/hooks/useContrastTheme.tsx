14a29f494c753a008998d33211036767c7f69b17
lunars97 <alekbarova.aizhana@gmail.com>
2025-04-23 23:42:12 +0200

    Remove old files from merge conflict

--- web/src/hooks/useContrastTheme.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react'
-
-import { ContrastThemeContext, ContrastThemeContextType } from '../components/ContrastThemeContext'
-
-export const useContrastTheme = (): ContrastThemeContextType => {
-  return useContext(ContrastThemeContext)
-}
