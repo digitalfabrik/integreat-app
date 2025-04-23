b478112eb1eac02b6a4405c4e4a88ac0a2f1c71f
lunars97 <alekbarova.aizhana@gmail.com>
2025-04-23 15:39:20 +0200

    Fix eslint error

--- shared/hooks/useDebounce.ts
+++ shared/hooks/useDebounce.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 export const DEBOUNCE_TIMEOUT = 250
 
-const useDebounce = <T>(value: T, delay = DEBOUNCE_TIMEOUT) => {
+const useDebounce = <T>(value: T, delay = DEBOUNCE_TIMEOUT): T => {
   const [debouncedValue, setDebouncedValue] = useState<T>(value)
 
   useEffect(() => {
