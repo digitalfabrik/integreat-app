77dd85f3ecabbf29a1ff83f5a51a1cde24370870
bahaa <bahaa.tuffaha@yahoo.com>
2025-04-29 12:12:29 +0300

    3182: brought back my previous changes about chat loader

--- web/src/components/Chat.tsx
+++ web/src/components/Chat.tsx
@@ -72,30 +72,30 @@ const Chat = ({ messages, submitMessage, hasError, isLoading, isTyping }: ChatPr
     setTextInput('')
   }
 
+  const submitDisabled = textInput.trim().length === 0 || hasError || isLoading
   const submitOnEnter = (event: KeyboardEvent) => {
-    if (event.key === 'Enter' && !event.shiftKey) {
+    const isEnterAllowed = event.key === 'Enter' && !event.shiftKey && !submitDisabled
+    if (isEnterAllowed) {
       event.preventDefault()
       onSubmit()
     }
   }
 
-  if (isLoading && !hasError) {
-    return (
-      <LoadingContainer>
-        <StyledLoadingSpinner />
-        <LoadingText>{t('loadingText')}</LoadingText>
-      </LoadingContainer>
-    )
-  }
-
   return (
     <Container>
-      <StyledChatConversation
-        $height={deviceHeight - chatInputContainerHeight}
-        messages={messages}
-        hasError={hasError}
-        isTyping={isTyping}
-      />
+      {isLoading && !hasError ? (
+        <LoadingContainer>
+          <StyledLoadingSpinner />
+          <LoadingText>{t('loadingText')}</LoadingText>
+        </LoadingContainer>
+      ) : (
+        <StyledChatConversation
+          $height={deviceHeight - chatInputContainerHeight}
+          messages={messages}
+          hasError={hasError}
+          isTyping={isTyping}
+        />
+      )}
       <InputWrapper $height={chatInputContainerHeight}>
         <InputSection id='chat' title={messages.length > 0 ? '' : t('inputLabel')}>
           <Input
@@ -109,7 +109,7 @@ const Chat = ({ messages, submitMessage, hasError, isLoading, isTyping }: ChatPr
           />
         </InputSection>
         <SubmitContainer>
-          <SubmitButton disabled={textInput.length === 0} onClick={onSubmit} text={t('sendButton')} />
+          <SubmitButton disabled={submitDisabled} onClick={onSubmit} text={t('sendButton')} />
           <ChatSecurityInformation />
         </SubmitContainer>
       </InputWrapper>
