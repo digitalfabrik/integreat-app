8430adae3b833a4d4fcc0b90b079d4877b5750cb
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-05-01 10:21:04 +0200

    1021: Enable high contrast mode for malte, aschaffenburg and obdach

--- build-configs/obdach/index.ts
+++ build-configs/obdach/index.ts
@@ -3,13 +3,13 @@ import obdachOverrideTranslations from 'translations/override-translations/obdac
 import { OBDACH_ASSETS } from '../AssetsType'
 import { CommonBuildConfigType, WebBuildConfigType } from '../BuildConfigType'
 import mainImprint from './mainImprint'
-import { lightTheme } from './theme'
+import { contrastTheme, lightTheme } from './theme'
 
 const commonObdachBuildConfig: CommonBuildConfigType = {
   appName: 'Netzwerk Obdach & Wohnen',
   appIcon: 'app_icon_obdach',
   lightTheme,
-  contrastTheme: lightTheme,
+  contrastTheme,
   assets: OBDACH_ASSETS,
   cmsUrl: 'https://cms.netzwerkobdachwohnen.de',
   hostName: 'netzwerkobdachwohnen.de',
