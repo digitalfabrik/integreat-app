32f328261e697550f6d159a28a1ba6af2fd7679b
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-05-08 14:25:01 +0200

    1021: Use global style also for light theme

--- web/src/styles/global/GlobalStyle.tsx
+++ web/src/styles/global/GlobalStyle.tsx
@@ -1,57 +1,62 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-    body {
-        position: relative;
+  body {
+    position: relative;
 
-        /* Styling for react-tooltip: https://react-tooltip.com/docs/getting-started#styling */
-            --rt-color-dark: ${props => props.theme.colors.textSecondaryColor};
-        --rt-color-white: ${props => props.theme.colors.backgroundColor};
-        --rt-opacity: 1;
-        
-        /* Styling for react-datepicker */
-        ${props =>
-          props.theme.isContrastTheme === true &&
-          `
-          .react-datepicker__header,
-          .react-datepicker__month,
-          .react-datepicker__day {
-            background-color: ${props.theme.colors.backgroundColor};
-          }
-    
-          .react-datepicker__current-month,
-          .react-datepicker__day-names .react-datepicker__day-name,
-          .react-datepicker__week,
-          .react-datepicker__day {
-            color: ${props.theme.colors.textColor};
-          }
-          
-          .react-datepicker__day--today {
-          border: 1px solid ${props.theme.colors.linkColor};
-          background-color: transparent !important;
-          border-radius: 50% !important;
-          }
-          
-           .react-datepicker__day--selected:not([aria-disabled='true']):hover,
-           .react-datepicker__day--in-selecting-range:not([aria-disabled='true']):hover,
-           .react-datepicker__day--in-range:not([aria-disabled='true']):hover,
-           .react-datepicker__day:not([aria-disabled=true]):hover {
-             background-color: ${props.theme.colors.linkColor} !important;
-          }
-    
-           .react-datepicker__day--selected {
-             background-color: ${props.theme.colors.linkColor} !important;
-          }
-          
-          /* Styling for BottomSheet header in poi */
-          [data-rsbs-header]{
-            background-color: ${props.theme.colors.backgroundAccentColor}; 
-           }
-            
-          [data-rsbs-header]::before {
-            background-color: ${props.theme.colors.textColor};
-          }
-        `}
-        }
+    /* react-tooltip: https://react-tooltip.com/docs/getting-started#styling */
+    --rt-color-dark: ${props => props.theme.colors.textSecondaryColor};
+    --rt-color-white: ${props => props.theme.colors.backgroundColor};
+    --rt-opacity: 1;
+
+    /* stylelint-disable selector-class-pattern */
+   
+    /* react-datepicker */
+    .react-datepicker__header {
+      background-color: ${props => props.theme.colors.backgroundAccentColor};
+    }
+
+    .react-datepicker__month,
+    .react-datepicker__month-container,
+    .react-datepicker__day {
+      background-color: ${props => props.theme.colors.backgroundColor};
+    }
+
+    .react-datepicker__current-month,
+    .react-datepicker__day-names,
+    .react-datepicker__day-name,
+    .react-datepicker__week,
+    .react-datepicker__day {
+      color: ${props => props.theme.colors.textColor};
+    }
+
+    .react-datepicker__day--today {
+      border: 1px solid ${props => props.theme.colors.linkColor};
+      background-color: transparent !important;
+      border-radius: 50% !important;
+    }
+
+    .react-datepicker__day--selected {
+      background-color: ${props => props.theme.colors.linkColor} !important;
+    }
+
+    .react-datepicker__day--selected:not([aria-disabled='true']):hover,
+    .react-datepicker__day--in-selecting-range:not([aria-disabled='true']):hover,
+    .react-datepicker__day--in-range:not([aria-disabled='true']):hover,
+    .react-datepicker__day:not([aria-disabled='true']):hover {
+      background-color: ${props => props.theme.colors.linkColor} !important;
+    }
+
+    /* stylelint-enable selector-class-pattern */
+
+    /* react-spring-bottom-sheet */
+    [data-rsbs-header] {
+      background-color: ${props => props.theme.colors.backgroundAccentColor};
+    }
+
+    [data-rsbs-header]::before {
+      background-color: ${props => props.theme.colors.textColor};
+    }
+  }
 `
 export default GlobalStyle
