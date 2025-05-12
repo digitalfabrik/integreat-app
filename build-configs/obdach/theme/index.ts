4af49329a9fd03a22400cb9eb9f43204ab612f15
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-05-12 13:00:10 +0200

    3234: Setup color palette

--- build-configs/obdach/theme/index.ts
+++ build-configs/obdach/theme/index.ts
@@ -1,6 +1,32 @@
 import { LegacyThemeType } from '../../LegacyThemeType'
+import { Theme } from '../../ThemeType'
+import { commonDarkColors, commonLightColors } from '../../common/theme/colors'
 import { legacyContrastColors, legacyLightColors } from './colors'
 import obdachFonts from './fonts'
+
+const customColors = {
+  secondary: {
+    // TODO we do not yet have light/dark colors for obdach
+    light: '#E55129',
+    main: '#E55129',
+    dark: '#E55129',
+    contrastText: '#E6E0E9',
+  },
+}
+
+export const lightTheme: Theme = {
+  palette: {
+    ...commonLightColors,
+    ...customColors,
+  },
+}
+
+export const darkTheme: Theme = {
+  palette: {
+    ...commonDarkColors,
+    ...customColors,
+  },
+}
 
 export const legacyLightTheme: LegacyThemeType = {
   colors: legacyLightColors,
