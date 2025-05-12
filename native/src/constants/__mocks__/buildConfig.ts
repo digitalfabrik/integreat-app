cf2b5ccd7a548a0eb37bcde132fe9442aabe245b
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-05-12 13:00:10 +0200

    3234: Setup color palette

--- native/src/constants/__mocks__/buildConfig.ts
+++ native/src/constants/__mocks__/buildConfig.ts
@@ -1,6 +1,6 @@
 import { INTEGREAT_ASSETS } from 'build-configs/AssetsType'
 import { CommonBuildConfigType } from 'build-configs/BuildConfigType'
-import { legacyLightTheme, legacyContrastTheme } from 'build-configs/integreat/theme'
+import { legacyLightTheme, legacyContrastTheme, lightTheme, darkTheme } from 'build-configs/integreat/theme'
 
 export const buildConfigIconSet = (): {
   appLogo: string
@@ -15,6 +15,8 @@ const buildConfig = jest.fn<CommonBuildConfigType, []>(
     appName: 'Integreat',
     appIcon: 'app_icon_integreat',
     notificationIcon: 'notification_icon_integreat',
+    lightTheme,
+    darkTheme,
     legacyLightTheme,
     legacyContrastTheme,
     assets: INTEGREAT_ASSETS,
