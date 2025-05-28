f6492eb423ab5f95de3536997f38d1324cf6ed31
Bahaa Tuffaha <101111168+bahaaTuffaha@users.noreply.github.com>
2025-05-28 12:33:53 +0300

    3199: Add helper text for location input (#3281)
    * 3199: Added helper text
    
    * 3199: Added german translation for searchCityTip
    
    * 3199: provided dynamic solution for headroom height and moved StyledHelperText to SearchInput
    
    * 3199: Simplify styling (#3285)
    
    * 3199: Adjusted padding and the width of the search bar at native to be similar to web
    
    * 3199: Changed the margin for the search input to 4px
    
    ---------
    
    Co-authored-by: Steffen Kleinle <steffen.kleinle@mailbox.org>

--- native/src/components/SearchInput.tsx
+++ native/src/components/SearchInput.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from 'react'
+import { Text, View } from 'react-native'
 import styled, { useTheme } from 'styled-components/native'
 
 import { SearchIcon } from '../assets'
@@ -6,7 +7,7 @@ import testID from '../testing/testID'
 import Icon from './base/Icon'
 
 const Input = styled.TextInput`
-  margin: 0 5px;
+  margin: 0 4px;
   flex-grow: 1;
   border-bottom-width: 1px;
   border-bottom-color: ${props => props.theme.colors.textSecondaryColor};
@@ -20,11 +21,19 @@ const Wrapper = styled.View<{ space: boolean }>`
   background-color: ${props => props.theme.colors.backgroundColor};
 `
 
+const Description = styled(Text)`
+  width: fit-content;
+  padding-left: 28px;
+  color: ${props => props.theme.colors.textColor};
+  font-family: ${props => props.theme.fonts.native.decorativeFontRegular};
+`
+
 type SearchInputProps = {
   placeholderText: string
   filterText: string
   onFilterTextChange: (filterText: string) => void
   spaceSearch: boolean
+  description?: string
 }
 
 const SearchInput = ({
@@ -32,23 +41,27 @@ const SearchInput = ({
   filterText,
   onFilterTextChange,
   spaceSearch = false,
+  description,
 }: SearchInputProps): ReactElement => {
   const theme = useTheme()
 
   return (
-    <Wrapper space={spaceSearch}>
-      <Icon Icon={SearchIcon} />
-      <Input
-        {...testID('Search-Input')}
-        multiline={false}
-        placeholderTextColor={theme.colors.textSecondaryColor}
-        placeholder={placeholderText}
-        aria-label={placeholderText}
-        defaultValue={filterText}
-        onChangeText={onFilterTextChange}
-        role='searchbox'
-      />
-    </Wrapper>
+    <View>
+      <Wrapper space={spaceSearch}>
+        <Icon Icon={SearchIcon} />
+        <Input
+          {...testID('Search-Input')}
+          multiline={false}
+          placeholderTextColor={theme.colors.textSecondaryColor}
+          placeholder={placeholderText}
+          aria-label={placeholderText}
+          defaultValue={filterText}
+          onChangeText={onFilterTextChange}
+          role='searchbox'
+        />
+      </Wrapper>
+      {!!description && <Description>{description}</Description>}
+    </View>
   )
 }
 
