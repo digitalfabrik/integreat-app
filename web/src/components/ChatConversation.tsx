46e2d6c0be24c101f2c6def7fbf9f5f95f8356cf
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-06-03 08:41:45 +0200

    3277: Refactor chat to avoid using manually calculated heights

--- web/src/components/ChatConversation.tsx
+++ web/src/components/ChatConversation.tsx
@@ -9,6 +9,7 @@ import ChatMessage, { InnerChatMessage } from './ChatMessage'
 const Container = styled.div`
   font-size: ${props => props.theme.fonts.hintFontSize};
   overflow: auto;
+  padding: 0 12px;
 `
 
 const InitialMessage = styled.div`
@@ -38,8 +39,6 @@ const TypingIndicator = ({ isVisible }: TypingIndicatorProps): ReactElement | nu
   isVisible ? (
     <InnerChatMessage userIsAuthor={false} showIcon={false} isAutomaticAnswer content='...' messageId={0} />
   ) : null
-
-const TYPING_INDICATOR_TIMEOUT = 60000
 
 const ChatConversation = ({ messages, hasError, className, isTyping }: ChatConversationProps): ReactElement => {
   const { t } = useTranslation('chat')
