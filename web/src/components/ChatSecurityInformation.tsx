00ea5f8e6847e9567e1bd201ccaa058914360136
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-06-03 18:06:46 +0200

    3246: Replace text buttons with mui buttons

--- web/src/components/ChatSecurityInformation.tsx
+++ web/src/components/ChatSecurityInformation.tsx
@@ -13,7 +13,6 @@ const SecurityInformationContainer = styled.div`
 `
 const SecurityIconContainer = styled.button`
   align-self: center;
-  margin-inline-start: 8px;
   cursor: pointer;
   display: flex;
   border: none;
