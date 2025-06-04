8250767e72e7ebed2216d55a3b425e2665b3567a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-06-04 09:38:55 +0200

    3246: Merge theme providers

--- web/src/hooks/useThemeContext.tsx
+++
@@ -1,5 +0,0 @@
-import { useContext } from 'react'
-
-import { ThemeContext, ThemeContextType } from '../components/ThemeContext'
-
-export const useThemeContext = (): ThemeContextType => useContext(ThemeContext)
