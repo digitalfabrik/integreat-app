917d11bf40b2f3003b85f2086383eb5bd55e6500
Andy <fischer@integreat-app.de>
2025-06-17 18:00:15 +0200

    3235: add missing import, add additional custom variants

--- web/src/@types/mui-theme.d.ts
+++ web/src/@types/mui-theme.d.ts
@@ -1,20 +1,19 @@
-declare module '@mui/material/styles' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-  interface TypographyVariants {
-    body3: React.CSSProperties
-  }
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { TypographyPropsVariantOverrides } from '@mui/material/Typography'
 
-  // allow configuration using `createTheme()`
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
-  interface TypographyVariantsOptions {
-    body3?: React.CSSProperties
-  }
-}
-
-// Update the Typography's variant prop options
+// docs: https://mui.com/material-ui/customization/typography/#adding-amp-disabling-variants
 declare module '@mui/material/Typography' {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface TypographyPropsVariantOverrides {
+    display1: true
+    display2: true
+    display3: true
+    title1: true
+    title2: true
+    title3: true
     body3: true
+    h4: false
+    h5: false
+    h6: false
   }
 }
