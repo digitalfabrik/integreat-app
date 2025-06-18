b824e6549562d5687e300c1ac281885db8e001c7
Andy <fischer@integreat-app.de>
2025-06-18 14:18:13 +0200

    3235: added missing inter medium font, pass font to typography, add missing typos from design system, disabled unneeded variants

--- build-configs/integreat/theme/typography.ts
+++ build-configs/integreat/theme/typography.ts
@@ -1,6 +1,7 @@
 import { TypographyType } from '../../TypographyType'
 import { commonTypography } from '../../common/theme/typography'
+import fonts from './fonts'
 
 export const typography: TypographyType = {
-  ...commonTypography,
+  ...commonTypography(fonts),
 }
