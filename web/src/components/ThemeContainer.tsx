32523dce4d25a24b2943800b89d6062ebaa28915
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-06-24 15:03:41 +0200

    1021: Remove comment

--- web/src/components/ThemeContainer.tsx
+++ web/src/components/ThemeContainer.tsx
@@ -12,15 +12,6 @@ import buildConfig from '../constants/buildConfig'
 import useLocalStorage from '../hooks/useLocalStorage'
 import globalStyle from '../styles/global/GlobalStyle'
 
-// For now, we want the dark mode to be opt in as it is a beta feature for now
-// const contrastThemeMediaQueries = [
-//   '(forced-colors: active)' /* to detect enabled forced colors mode: https://developer.mozilla.org/en-US/docs/Web/CSS/@media/forced-colors */,
-//   '(prefers-contrast: more)' /* to detect a lower/higher contrast: https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-contrast */,
-//   '(prefers-color-scheme: dark)' /* is used to detect if a user has requested light or dark color themes: https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme */,
-// ].map(query => window.matchMedia(query))
-//
-// const getSystemTheme = (): ThemeKey => (contrastThemeMediaQueries.some(query => query.matches) ? 'contrast' : 'light')
-
 const ltrCache = createCache({
   key: 'mui-ltr-cache',
   stylisPlugins: [prefixer],
