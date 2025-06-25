ddeebe239ee1d723e39afd2f40a7f0028aea4631
Andy <fischer@integreat-app.de>
2025-06-25 14:56:26 +0200

    3235: directly assign commonTypography, remove type

--- build-configs/aschaffenburg/theme/typography.ts
+++ build-configs/aschaffenburg/theme/typography.ts
@@ -1,7 +1,4 @@
-import { TypographyType } from '../../TypographyType'
 import { commonTypography } from '../../common/theme/typography'
 import fonts from './fonts'
 
-export const typography: TypographyType = {
-  ...commonTypography(fonts),
-}
+export const typography = commonTypography(fonts)
