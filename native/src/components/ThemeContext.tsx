a6106472e67986124b306fb9c850a9c634871125
lunars97 <alekbarova.aizhana@gmail.com>
2025-06-30 15:00:55 +0200

    1021: Fix merge conflict

--- native/src/components/ThemeContext.tsx
+++ native/src/components/ThemeContext.tsx
@@ -1,13 +1,13 @@
 import React, { createContext, ReactElement, useCallback, useMemo } from 'react'
 import { ThemeProvider } from 'styled-components/native'
 
-import { ThemeType, ThemeKey } from 'build-configs/index'
+import { LegacyThemeType, ThemeKey } from 'build-configs/index'
 
 import buildConfig from '../constants/buildConfig'
 import { useAppContext } from '../hooks/useCityAppContext'
 
 export type ThemeContextType = {
-  theme: ThemeType
+  theme: LegacyThemeType
   themeType: ThemeKey
   toggleTheme: () => void
 }
@@ -15,7 +15,7 @@ export type ThemeContextType = {
 const themeConfig = buildConfig()
 
 export const ThemeContext = createContext<ThemeContextType>({
-  theme: themeConfig.lightTheme,
+  theme: themeConfig.legacyLightTheme,
   themeType: 'light',
   toggleTheme: () => undefined,
 })
@@ -36,8 +36,8 @@ export const ThemeContainer = ({ children }: ThemeContainerProps): ReactElement 
   const contextValue = useMemo(() => {
     const theme =
       themeType === 'contrast'
-        ? { ...themeConfig.contrastTheme, isContrastTheme: true }
-        : { ...themeConfig.lightTheme, isContrastTheme: false }
+        ? { ...themeConfig.legacyContrastTheme, isContrastTheme: true }
+        : { ...themeConfig.legacyLightTheme, isContrastTheme: false }
     return { theme, themeType, toggleTheme }
   }, [themeType, toggleTheme])
 
