b317c6a5860dd19446a4a05f66f17c0f12504125
LeandraH <LeandraH@users.noreply.github.com>
2025-07-01 13:03:47 +0200

    3315: Use MUI checkbox

--- web/src/components/base/Checkbox.tsx
+++ web/src/components/base/Checkbox.tsx
@@ -1,39 +1,19 @@
 import styled from '@emotion/styled'
+import { Checkbox as MUICheckbox, FormControlLabel } from '@mui/material'
 import React, { ReactElement } from 'react'
 
-const Container = styled.div`
-  display: flex;
-  flex: 1;
-  gap: 10px;
-`
-
-const StyledCheckbox = styled.input`
-  cursor: pointer;
-  accent-color: ${props => props.theme.colors.themeColor};
-  width: 20px;
-  height: 20px;
-  min-width: 20px;
-  align-self: center;
-`
-
-const StyledLabel = styled.label`
-  color: ${props => props.theme.colors.textColor};
-  padding: 4px;
-  cursor: pointer;
+const StyledLabel = styled(FormControlLabel)`
+  font-weight: 400;
 `
 
 type CheckboxProps = {
   checked: boolean
   setChecked: (checked: boolean) => void
   label: string | ReactElement
-  id: string
 }
 
-const Checkbox = ({ checked, setChecked, label, id }: CheckboxProps): ReactElement => (
-  <Container>
-    <StyledCheckbox type='checkbox' id={id} checked={checked} onChange={() => setChecked(!checked)} />
-    <StyledLabel htmlFor={id}>{label}</StyledLabel>
-  </Container>
+const Checkbox = ({ checked, setChecked, label }: CheckboxProps): ReactElement => (
+  <StyledLabel control={<MUICheckbox checked={checked} onChange={() => setChecked(!checked)} />} label={label} />
 )
 
 export default Checkbox
