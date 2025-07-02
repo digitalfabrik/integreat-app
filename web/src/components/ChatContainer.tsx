5df6ee13f0a2426ff0de55279bc6ad41159e6e2f
Bahaa Tuffaha <101111168+bahaaTuffaha@users.noreply.github.com>
2025-07-02 15:13:51 +0300

    3314: Replace the chat button with mui (#3351)
    * 3314: Replace chat button with mui floating action button
    
    * 3314: Removed boxShadow from ChatActionButton and used the default mui shadow

--- web/src/components/ChatContainer.tsx
+++ web/src/components/ChatContainer.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
 import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined'
+import { Fab } from '@mui/material'
 import React, { ReactElement, useContext, useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router'
 
@@ -17,24 +18,15 @@ import Icon from './base/Icon'
 
 const CHAT_BUTTON_SIZE = 48
 
-const ChatButtonContainer = styled.button<{ bottom: number }>`
+const ChatButtonContainer = styled.div<{ bottom: number }>`
   position: fixed;
   bottom: ${props => props.bottom}px;
   inset-inline-end: 16px;
   margin-bottom: 8px;
-  background-color: transparent;
-  border: none;
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: min-content;
-`
-
-const Circle = styled.div`
-  background-color: ${props => props.theme.colors.themeColor};
-  border-radius: 50%;
-  box-shadow: 0 2px 3px 3px rgb(0 0 0 / 20%);
-  align-self: center;
-  padding: 8px;
 `
 
 const StyledIcon = styled(Icon)`
@@ -49,6 +41,12 @@ const StyledIcon = styled(Icon)`
 const ChatTitle = styled.span`
   margin-top: 8px;
   color: ${props => props.theme.colors.textColor};
+`
+
+const ChatActionButton = styled(Fab)`
+  &:hover {
+    background-color: ${props => props.theme.colors.themeColor};
+  }
 `
 
 type ChatContainerProps = {
@@ -87,10 +85,10 @@ const ChatContainer = ({ city, language }: ChatContainerProps): ReactElement => 
   }
 
   return (
-    <ChatButtonContainer bottom={bottom} onClick={() => setChatVisible(true)}>
-      <Circle>
+    <ChatButtonContainer bottom={bottom}>
+      <ChatActionButton onClick={() => setChatVisible(true)} color='primary'>
         <StyledIcon src={QuestionAnswerOutlinedIcon} title={chatName} />
-      </Circle>
+      </ChatActionButton>
       {!viewportSmall && <ChatTitle>{chatName}</ChatTitle>}
     </ChatButtonContainer>
   )
