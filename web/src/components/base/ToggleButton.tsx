07a7ae36217cb24dcbfca8a727c59fb7d8bdf8c7
bahaa <bahaa.tuffaha@yahoo.com>
2025-07-03 18:25:40 +0300

    3313: Requested changes about mui theme and refactoring feedback buttons

--- web/src/components/base/ToggleButton.tsx
+++ web/src/components/base/ToggleButton.tsx
@@ -10,17 +10,14 @@ export const toggleButtonWidth = 100
 const StyledIcon = styled(Icon)<{ iconSize: 'small' | 'medium' }>`
   width: ${props => (props.iconSize === 'small' ? '24px' : '40px')};
   height: ${props => (props.iconSize === 'small' ? '24px' : '40px')};
-  color: ${props =>
-    props.theme.isContrastTheme ? props.theme.colors.backgroundColor : props.theme.colors.textSecondaryColor};
+  color: inherit;
 `
 
 const StyledButton = styled(MuiToggleButton)`
   display: flex;
   flex-direction: column;
-  box-shadow:
-    0 1px 2px rgb(0 0 0 / 25%),
-    0 1px 4px 1px rgb(0 0 0 / 15%);
   border-radius: 18px;
+  border: 0.5px solid gray !important;
   width: ${toggleButtonWidth}px;
   height: 100px;
   background-color: ${props => {
@@ -33,31 +30,23 @@ const StyledButton = styled(MuiToggleButton)`
     props.theme.isContrastTheme ? props.theme.colors.backgroundColor : props.theme.colors.textSecondaryColor};
   padding: 8px;
   text-align: center;
-
-  &[class*='Mui-selected'] {
-    background-color: ${props => props.theme.colors.themeColor};
-  }
-
-  /* This will disable hover issue */
-  &[class*='Mui-selected']:hover {
-    background-color: ${props => props.theme.colors.themeColor};
-  }
 `
 
 type ToggleButtonProps = {
   text: string
+  value: string | number
   onClick?: () => void
   icon: string | ElementType<SvgIconProps>
   iconSize?: 'small' | 'medium'
   active?: boolean
   className?: string
-  value?: string | number
 }
 
 const ToggleButton = ({ text, onClick, className, value, ...props }: ToggleButtonProps): ReactElement => (
   <StyledButton
+    color='primary'
     sx={{ textTransform: 'inherit' }}
-    value={value ?? text}
+    value={value}
     selected={props.active}
     onChange={onClick}
     className={className}>
