27986f5a907ce8367226eb87df28b6a1f83da3dd
LeandraH <LeandraH@users.noreply.github.com>
2025-07-09 14:19:54 +0200

    3237: Add our breakpoints to theme

--- web/src/components/ThemeContainer.tsx
+++ web/src/components/ThemeContainer.tsx
@@ -30,6 +30,15 @@ const createTheme = (
   contentDirection,
   isContrastTheme: themeType === 'contrast',
   ...createMuiTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 840,
+        lg: 1200,
+        xl: 1600,
+      },
+    },
     direction: contentDirection,
     colorSchemes: {
       light: buildConfig().lightTheme,
