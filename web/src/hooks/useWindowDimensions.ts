ecfc6b9017c9989c108a380f1a0d6cbddb47e736
LeandraH <LeandraH@users.noreply.github.com>
2025-07-10 15:50:00 +0200

    3237: Fix test

--- web/src/hooks/useWindowDimensions.ts
+++ web/src/hooks/useWindowDimensions.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import { BREAKPOINTS } from 'src/components/ThemeContainer'
+
+import { BREAKPOINTS } from '../components/ThemeContainer'
 
 export type WindowDimensionsType = {
   width: number
