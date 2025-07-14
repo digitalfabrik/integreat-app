9d34ac5a6c6c3899b8f946c8f1a3cd2a5f654b33
LeandraH <leandrah@users.noreply.github.com>
2025-07-14 18:57:05 +0200

    3311: Add neutral color palette

--- build-configs/ThemeType.ts
+++ build-configs/ThemeType.ts
@@ -19,6 +19,22 @@ export type ActionColor = {
   focus?: string
 }
 
+type ToneColor = {
+  /* eslint-disable no-magic-numbers */
+  1000?: string
+  900: string
+  800: string
+  700: string
+  600: string
+  500: string
+  400: string
+  300: string
+  200: string
+  100: string
+  50: string
+  /* eslint-enable no-magic-numbers */
+}
+
 export type CommonColors = {
   primary: PaletteColor
   error: PaletteColor
@@ -28,6 +44,7 @@ export type CommonColors = {
   tunews: SimplePaletteColor
   divider: string
   link: string
+  neutral: ToneColor
 }
 
 export type PaletteMode = 'light' | 'dark'
