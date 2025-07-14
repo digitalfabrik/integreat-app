15f92016ba1ae44f6c4273ae95a9c0d85e66e2b1
LeandraH <LeandraH@users.noreply.github.com>
2025-07-14 18:57:05 +0200

    3311: Add neutral color palette

--- build-configs/ThemeType.ts
+++ build-configs/ThemeType.ts
@@ -19,6 +19,22 @@ export type ActionColor = {
   focus?: string
 }
 
+type ToneColor = {
+  /* eslint-disable no-magic-numbers */
+  1000?: string
+  900: string
+  800: string
+  700: string
+  600: string
+  500: string
+  400: string
+  300: string
+  200: string
+  100: string
+  50: string
+  /* eslint-enable no-magic-numbers */
+}
+
 export type CommonColors = {
   primary: PaletteColor
   tertiary: PaletteColor
@@ -30,6 +46,7 @@ export type CommonColors = {
   action: ActionColor
   divider: string
   link: string
+  neutral: ToneColor
 }
 
 export type PaletteMode = 'light' | 'dark'
--- web/src/@types/mui-theme.d.ts
+++ web/src/@types/mui-theme.d.ts
@@ -1,10 +1,13 @@
+/* eslint-disable no-magic-numbers */
+
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { TypographyPropsVariantOverrides } from '@mui/material/Typography'
 
 // Enable and disable typography variants according to our design system
 // docs: https://mui.com/material-ui/customization/typography/#adding-amp-disabling-variants
 declare module '@mui/material/Typography' {
-  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface TypographyPropsVariantOverrides {
     display1: true
     display2: true
@@ -27,5 +30,39 @@ declare module '@mui/material/Typography' {
     subtitle2: false
     caption: false
     overline: false
+  }
+}
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    neutral: {
+      1000?: string
+      900: string
+      800: string
+      700: string
+      600: string
+      500: string
+      400: string
+      300: string
+      200: string
+      100: string
+      50: string
+    }
+  }
+
+  interface PaletteOptions {
+    neutral: {
+      1000?: string
+      900: string
+      800: string
+      700: string
+      600: string
+      500: string
+      400: string
+      300: string
+      200: string
+      100: string
+      50: string
+    }
   }
 }
