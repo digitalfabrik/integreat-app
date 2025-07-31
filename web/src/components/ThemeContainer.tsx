a2d5dcb692e3ef63f211225a7f5ba2ab80c6aca9
Andy <fischer@integreat-app.de>
2025-07-31 12:14:07 +0200

    3397: remove component override

--- web/src/components/ThemeContainer.tsx
+++ web/src/components/ThemeContainer.tsx
@@ -53,26 +53,6 @@ const createTheme = (
       shadows: muiShadowCreator(themeType),
       typography: buildConfig().typography,
       palette: isContrast ? buildConfig().darkTheme.palette : buildConfig().lightTheme.palette,
-      components: {
-        MuiIconButton: {
-          styleOverrides: {
-            root: {
-              variants: [
-                {
-                  props: { color: 'default' },
-                  style: {
-                    color: themeType === 'contrast' ? '#FFFFFF' : '#212121',
-
-                    '&:disabled': {
-                      color: themeType === 'contrast' ? '#333D51' : '#8E8E8E',
-                    },
-                  },
-                },
-              ],
-            },
-          },
-        },
-      },
     }),
   }
 }
