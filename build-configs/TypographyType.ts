65207911d3e4ddd86f1b757e160a2505dbcf278a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-08-05 11:17:25 +0200

    3374: Simplify typography type to actually used type

--- build-configs/TypographyType.ts
+++ build-configs/TypographyType.ts
@@ -1,86 +1,6 @@
 import * as CSS from 'csstype'
 import React from 'react'
 
-import { PaletteColor, PaletteMode, TypeText } from './ThemeType'
-
-type PaletteTonalOffset =
-  | number
-  | {
-      light: number
-      dark: number
-    }
-
-/* eslint-disable no-magic-numbers */
-type Color = {
-  50: string
-  100: string
-  200: string
-  300: string
-  400: string
-  500: string
-  600: string
-  700: string
-  800: string
-  900: string
-  A100: string
-  A200: string
-  A400: string
-  A700: string
-}
-
-type PaletteColorOptions = PaletteColor
-type PaletteAugmentColorOptions = {
-  color: PaletteColorOptions
-  mainShade?: number | string
-  lightShade?: number | string
-  darkShade?: number | string
-  name?: number | string
-}
-
-type TypeAction = {
-  active: string
-  hover: string
-  hoverOpacity: number
-  selected: string
-  selectedOpacity: number
-  disabled: string
-  disabledOpacity: number
-  disabledBackground: string
-  focus: string
-  focusOpacity: number
-  activatedOpacity: number
-}
-
-type TypeBackground = {
-  default: string
-  paper: string
-}
-
-type CommonColors = {
-  black: string
-  white: string
-}
-
-type Palette = {
-  common: CommonColors
-  mode: PaletteMode
-  contrastThreshold: number
-  tonalOffset: PaletteTonalOffset
-  primary: PaletteColor
-  secondary: PaletteColor
-  error: PaletteColor
-  warning: PaletteColor
-  info: PaletteColor
-  success: PaletteColor
-  grey: Color
-  text: TypeText
-  divider: string
-  action: TypeAction
-  background: TypeBackground
-  getContrastText: (background: string) => string
-  augmentColor: (options: PaletteAugmentColorOptions) => PaletteColor
-}
-
 type TypographyVariant =
   | 'display1'
   | 'display2'
@@ -141,4 +61,4 @@ type CSSProperties = BaseCSSProperties & {
 type TypographyStyle = CSSProperties
 type TypographyStyleOptions = TypographyStyle & NonNullable<unknown>
 type TypographyVariantsOptions = Partial<Record<TypographyVariant, TypographyStyleOptions> & FontStyleOptions>
-export type TypographyType = TypographyVariantsOptions | ((palette: Palette) => TypographyVariantsOptions) | undefined
+export type TypographyType = TypographyVariantsOptions
