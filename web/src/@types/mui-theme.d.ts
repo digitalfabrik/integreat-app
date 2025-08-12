9b0fb4e779d8b4c59fbfc2ed1a903d582bbc7791
LeandraH <LeandraH@users.noreply.github.com>
2025-08-12 13:29:25 +0200

    3311: Remove unnecessary TS disabling

--- web/src/@types/mui-theme.d.ts
+++ web/src/@types/mui-theme.d.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
