c37ee9be109b0f20757eb9eec10c9b19371113d6
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-08-25 13:01:58 +0200

    3384: Adjust paddings and fix html semantics error

--- web/src/components/CityListGroup.tsx
+++ web/src/components/CityListGroup.tsx
@@ -1,21 +1,35 @@
-import shouldForwardProp from '@emotion/is-prop-valid'
 import ListItem from '@mui/material/ListItem'
-import ListSubheader from '@mui/material/ListSubheader'
+import ListSubheader, { ListSubheaderProps as MuiListSubheaderProps } from '@mui/material/ListSubheader'
 import { styled } from '@mui/material/styles'
-import React, { ReactElement } from 'react'
+import React, { ElementType, ReactElement } from 'react'
 
 import { CityModel } from 'shared/api'
 
 import CityEntry from './CityEntry'
 import List from './base/List'
 
-export const StyledListSubheader = styled(ListSubheader, { shouldForwardProp })<{ stickyTop: number }>`
-  top: ${props => props.stickyTop}px;
-  transition: top 0.2s ease-out;
-`
+type ListSubheaderProps<C extends ElementType> = MuiListSubheaderProps<C, { component?: C }> & {
+  stickyTop: number
+}
+
+export const CityGroupHeader = <C extends ElementType>({
+  stickyTop,
+  children,
+}: ListSubheaderProps<C>): ReactElement => (
+  // We need to use sx here to avoid complications with the component prop
+  // https://mui.com/material-ui/guides/typescript/#complications-with-the-component-prop
+  <ListSubheader sx={{ top: stickyTop, transition: 'top 0.2s ease-out' }} component='div'>
+    {children}
+  </ListSubheader>
+)
+
+const StyledList = styled(List)`
+  padding: 0;
+` as typeof List
 
 const StyledListItem = styled(ListItem)`
   flex-direction: column;
+  padding-block: 0;
 `
 
 type CityListGroupProps = {
@@ -36,8 +50,8 @@ const CityListGroup = ({
   NoItemsMessage = 'search:nothingFound',
 }: CityListGroupProps): ReactElement => (
   <StyledListItem key={title} alignItems='flex-start'>
-    <List
-      Header={<StyledListSubheader stickyTop={stickyTop}>{title}</StyledListSubheader>}
+    <StyledList
+      Header={<CityGroupHeader stickyTop={stickyTop}>{title}</CityGroupHeader>}
       items={cities}
       renderItem={city => <CityEntry key={city.code} city={city} language={languageCode} filterText={filterText} />}
       NoItemsMessage={NoItemsMessage}
