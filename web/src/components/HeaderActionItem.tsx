b847cef2adc05b185a60bfa3103e7afc6a21ff1a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-02 12:08:33 +0200

    3392: Refactor sidebar, dropdown, language selectors and header items (#3501)
    * 3392: Remove language selector from sidebar
    
    * 3392: Remove unused wrapping and isOpen prop
    
    * 3392: Add custom button logic to sidebar
    
    * 3392: Remove unused SidebarActionItemDropDown
    
    * 3392: Render LanguageSelector in HeaderLanguageSelectorItem instead of vice versa
    
    * 3392: Merge mobile and desktop header language selectors
    
    * 3392: Use HeaderActionItem and move it outside of dropdown
    
    * 3392: Merge styled component into dropdown
    
    * 3392: Cleanup

--- web/src/components/HeaderActionItem.tsx
+++ web/src/components/HeaderActionItem.tsx
@@ -1,28 +1,35 @@
+import Button from '@mui/material/Button'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
-import { useTheme } from '@mui/material/styles'
+import { styled } from '@mui/material/styles'
 import React, { ReactElement } from 'react'
 
 import Link from './base/Link'
 
+const StyledButton = styled(Button)`
+  background-color: ${props => props.theme.palette.tertiary.light};
+  padding: 2px 12px;
+`
+
+const StyledIconButton = styled(IconButton)`
+  background-color: ${props => props.theme.palette.tertiary.light};
+`
+
 type HeaderActionItemLinkProps = {
   text: string
   icon: ReactElement
+  innerText?: string
 } & ({ to: string; onClick?: never } | { to?: never; onClick: () => void })
 
-const HeaderActionItem = ({ to, text, icon, onClick }: HeaderActionItemLinkProps): ReactElement => {
-  const theme = useTheme()
-
-  const Button = (
-    <IconButton
-      name={text}
-      onClick={onClick}
-      color='primary'
-      sx={{ backgroundColor: theme.palette.tertiary.light }}
-      size='medium'
-      aria-label={text}>
+const HeaderActionItem = ({ to, text, icon, onClick, innerText }: HeaderActionItemLinkProps): ReactElement => {
+  const Button = innerText ? (
+    <StyledButton onClick={onClick} startIcon={icon} aria-label={text}>
+      {innerText}
+    </StyledButton>
+  ) : (
+    <StyledIconButton onClick={onClick} color='primary' aria-label={text}>
       {icon}
-    </IconButton>
+    </StyledIconButton>
   )
 
   return (
--- web/src/components/LanguageSelector.tsx
+++ web/src/components/LanguageSelector.tsx
@@ -3,52 +3,42 @@ import { useTranslation } from 'react-i18next'
 
 import Selector from '../components/Selector'
 import SelectorItemModel from '../models/SelectorItemModel'
-import HeaderLanguageSelectorItem from './HeaderLanguageSelectorItem'
 
 type LanguageSelectorProps = {
-  languageCode: string
-  isHeaderActionItem: boolean
   languageChangePaths: { code: string; path: string | null; name: string }[] | null
-  inSidebarMenu?: boolean
-  closeSidebar?: () => void
+  languageCode: string
+  vertical: boolean
+  close?: () => void
+  availableOnly?: boolean
 }
 
-/**
- * Displays a dropDown menu to handle changing of the language
- */
-const LanguageSelector = (props: LanguageSelectorProps): ReactElement => {
-  const { isHeaderActionItem, languageChangePaths, languageCode, inSidebarMenu = false, closeSidebar } = props
-  const activeItemCode = languageCode
+const LanguageSelector = ({
+  languageChangePaths,
+  languageCode,
+  vertical,
+  close,
+  availableOnly = false,
+}: LanguageSelectorProps): ReactElement => {
   const { t } = useTranslation('layout')
 
   const selectorItems =
-    languageChangePaths?.map(
-      ({ code, name, path }) =>
-        new SelectorItemModel({
-          code,
-          name,
-          href: path,
-        }),
-    ) ?? []
-
-  if (isHeaderActionItem) {
-    return (
-      <HeaderLanguageSelectorItem
-        selectorItems={selectorItems}
-        activeItemCode={activeItemCode}
-        inSidebarMenu={inSidebarMenu}
-        closeSidebar={closeSidebar}
-      />
-    )
-  }
-
-  const availableItems = selectorItems.filter(item => item.href !== null)
+    languageChangePaths
+      ?.filter(it => !availableOnly || it.path)
+      .map(
+        ({ code, name, path }) =>
+          new SelectorItemModel({
+            code,
+            name,
+            href: path,
+          }),
+      ) ?? []
 
   return (
     <Selector
-      verticalLayout
-      items={availableItems}
-      activeItemCode={activeItemCode}
+      close={close}
+      verticalLayout={vertical}
+      items={selectorItems}
+      activeItemCode={languageCode}
       disabledItemTooltip={t('noTranslation')}
     />
   )
