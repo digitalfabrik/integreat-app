84db0bdac0d5b3fb95459ec889e47d7a3b883f29
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-03 14:20:57 +0200

    3375: Add route option to useCityContentParams

--- web/src/testing/render.tsx
+++ web/src/testing/render.tsx
@@ -1,16 +1,9 @@
 import { render, RenderResult } from '@testing-library/react'
 import React, { ReactElement, ReactNode } from 'react'
-import { createMemoryRouter, MemoryRouter, RouterProvider } from 'react-router'
+import { createMemoryRouter, MemoryRouter, Route, RouterProvider, Routes } from 'react-router'
 
 import ThemeContainer from '../components/ThemeContainer'
-
-type RenderRouteOptions = {
-  pathname: string
-  routePattern: string
-  searchParams?: string
-  childPattern?: string
-  previousRoutes?: { pathname: string; search?: string }[]
-}
+import { cityContentPattern, RoutePatterns, RouteType } from '../routes'
 
 const AllTheProviders = ({ children, options }: { children: ReactNode; options?: { pathname: string } }) => (
   <MemoryRouter initialEntries={options ? [options.pathname] : ['/']}>
@@ -31,6 +24,14 @@ export const renderWithRouter = (ui: ReactElement): RenderResult =>
 
 type ExtendedRenderResult = RenderResult & {
   router: ReturnType<typeof createMemoryRouter>
+}
+
+type RenderRouteOptions = {
+  pathname: string
+  routePattern: string
+  searchParams?: string
+  childPattern?: string
+  previousRoutes?: { pathname: string; search?: string }[]
 }
 
 export const renderRoute = (ui: ReactElement, options: RenderRouteOptions): ExtendedRenderResult => {
@@ -55,4 +56,45 @@ export const renderRoute = (ui: ReactElement, options: RenderRouteOptions): Exte
     ...renderWithTheme(<RouterProvider router={router} />),
     router,
   }
+}
+
+type RenderAllRoutesOptions = {
+  CityContentElement?: ReactElement
+}
+
+const DefaultRenderAllRoutesOptions: RenderAllRoutesOptions = {
+  CityContentElement: undefined,
+}
+
+export const renderAllRoutes = (path: string, options = DefaultRenderAllRoutesOptions): RenderResult => {
+  const DefaultElement = ({ route }: { route: string }) => <div>{route}</div>
+  const { CityContentElement } = options
+
+  const routes = Object.keys(RoutePatterns) as RouteType[]
+  const baseRoutes = routes.filter(route => RoutePatterns[route].startsWith('/'))
+  const cityContentRoutes = routes.filter(route => !RoutePatterns[route].startsWith('/'))
+
+  return renderWithTheme(
+    <MemoryRouter initialEntries={[path]}>
+      <Routes>
+        {baseRoutes.map(route => (
+          <Route key={route} path={RoutePatterns[route]} element={<DefaultElement route={route} />} />
+        ))}
+        <Route
+          path={cityContentPattern}
+          element={
+            <Routes>
+              {cityContentRoutes.map(route => (
+                <Route
+                  key={route}
+                  path={RoutePatterns[route]}
+                  element={CityContentElement ?? <DefaultElement route={route} />}
+                />
+              ))}
+            </Routes>
+          }
+        />
+      </Routes>
+    </MemoryRouter>,
+  )
 }
