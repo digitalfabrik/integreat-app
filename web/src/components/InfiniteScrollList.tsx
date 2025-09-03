0d49ec2aabc8b17e623fb8b6ba9c7690ea87cc5b
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-03 09:46:56 +0200

    3383: Rename to Items

--- web/src/components/InfiniteScrollList.tsx
+++ web/src/components/InfiniteScrollList.tsx
@@ -66,7 +66,7 @@ const InfiniteScrollList = <T,>({
 
   return (
     <InfiniteScroll loadMore={load} hasMore={!loading && hasMore}>
-      <List items={data.map(renderItem)} NoItemsMessage={<div />} />
+      <List Items={data.map(renderItem)} NoItemsMessage={<div />} />
     </InfiniteScroll>
   )
 }
