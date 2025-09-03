284217a0d6bb99fa59a45335ba6c8ffbd56f2f9a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-03 15:58:24 +0200

    3375: Avoid type casts

--- web/src/hooks/useCityContentParams.ts
+++ web/src/hooks/useCityContentParams.ts
@@ -5,7 +5,7 @@ import { CATEGORIES_ROUTE, RESERVED_CITY_CONTENT_SLUGS } from 'shared'
 const useCityContentParams = (): { cityCode: string; languageCode: string; route: string } => {
   const params = useParams()
   const slug = params['*']?.split('/')[0]
-  const route = slug && (RESERVED_CITY_CONTENT_SLUGS as string[]).includes(slug) ? slug : CATEGORIES_ROUTE
+  const route = slug && RESERVED_CITY_CONTENT_SLUGS.includes(slug) ? slug : CATEGORIES_ROUTE
   // This hook should only be used in city content routes where it is guaranteed that both city and language code are set
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   return { cityCode: params.cityCode!, languageCode: params.languageCode!, route }
