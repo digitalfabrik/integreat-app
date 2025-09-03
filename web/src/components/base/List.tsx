269e3ef8fa8489c14a6176e54085976bccf250b0
bahaa <bahaa.tuffaha@yahoo.com>
2025-09-03 13:05:47 +0300

    3380: Used Mui list components

--- web/src/components/base/List.tsx
+++ web/src/components/base/List.tsx
@@ -8,13 +8,14 @@ type ListProps = {
   items: ReactElement[]
   NoItemsMessage: string | ReactElement
   className?: string
+  lastDivider?: boolean
 }
 
-const List = ({ items, NoItemsMessage, className }: ListProps): ReactElement => {
+const List = ({ items, NoItemsMessage, className, lastDivider = true }: ListProps): ReactElement => {
   if (items.length === 0) {
     return typeof NoItemsMessage === 'string' ? <Failure errorMessage={NoItemsMessage} /> : NoItemsMessage
   }
-  return <MuiList className={className}>{withDividers(items)}</MuiList>
+  return <MuiList className={className}>{withDividers(items, lastDivider)}</MuiList>
 }
 
 export default List
--- web/src/utils/index.tsx
+++ web/src/utils/index.tsx
@@ -4,4 +4,7 @@ import React, { ReactElement } from 'react'
 export const join = <T, U>(items: T[], separator: (index: number) => U): (T | U)[] =>
   items.flatMap((item, index) => [item, separator(index)]).slice(0, -1)
 
-export const withDividers = (items: ReactElement[]): ReactElement[] => join(items, index => <Divider key={index} />)
+export const withDividers = (items: ReactElement[], lastDivider: boolean = true): ReactElement[] => {
+  const result = join(items, index => <Divider key={index} />)
+  return lastDivider ? [...result, <Divider key={items.length} />] : result
+}
