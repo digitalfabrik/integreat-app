bfca67a0c1c4a82e9772a7e0e8732b33e349133a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-03 14:20:57 +0200

    3375: Add route option to useCityContentParams

--- web/src/hooks/useCityContentParams.ts
+++ web/src/hooks/useCityContentParams.ts
@@ -1,10 +1,14 @@
 import { useParams } from 'react-router-dom'
 
-const useCityContentParams = (): { cityCode: string; languageCode: string } => {
+import { CATEGORIES_ROUTE, RESERVED_CITY_CONTENT_SLUGS } from 'shared'
+
+const useCityContentParams = (): { cityCode: string; languageCode: string; route: string } => {
   const params = useParams()
+  const slug = params['*']?.split('/')[0]
+  const route = slug && (RESERVED_CITY_CONTENT_SLUGS as string[]).includes(slug) ? slug : CATEGORIES_ROUTE
   // This hook should only be used in city content routes where it is guaranteed that both city and language code are set
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  return { cityCode: params.cityCode!, languageCode: params.languageCode! }
+  return { cityCode: params.cityCode!, languageCode: params.languageCode!, route }
 }
 
 export default useCityContentParams
