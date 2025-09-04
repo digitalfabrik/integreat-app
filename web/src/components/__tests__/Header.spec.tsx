9d0c26c94d7cc219e4e9c90817ae0884900ea553
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-04 07:06:14 +0200

    3375: Move city content sidebar to separate component

--- web/src/components/__tests__/Header.spec.tsx
+++ web/src/components/__tests__/Header.spec.tsx
@@ -1,4 +1,3 @@
-import { fireEvent } from '@testing-library/react'
 import { mocked } from 'jest-mock'
 import React from 'react'
 
@@ -6,9 +5,6 @@ import useWindowDimensions from '../../hooks/useWindowDimensions'
 import { renderWithRouterAndTheme } from '../../testing/render'
 import { mockWindowDimensions } from '../../testing/utils'
 import { Header } from '../Header'
-import HeaderActionItem from '../HeaderActionItem'
-import SidebarActionItem from '../SidebarActionItem'
-import Link from '../base/Link'
 
 jest.mock('../../hooks/useWindowDimensions')
 jest.mock('react-inlinesvg')
@@ -21,53 +17,8 @@ describe('Header', () => {
   it('should render correctly', () => {
     mocked(useWindowDimensions).mockImplementation(() => ({ ...mockWindowDimensions, viewportSmall: true }))
     const { getByText } = renderWithRouterAndTheme(
-      <Header logoHref='/random_route' actionItems={[]} sidebarItems={[]} cityName={cityName} language='de' />,
+      <Header logoHref='/random_route' actionItems={[]} cityName={cityName} language='de' />,
     )
     expect(getByText(cityName)).toBeDefined()
-  })
-
-  it('should render SidebarMenu with elements on small viewport', () => {
-    mocked(useWindowDimensions).mockImplementation(() => ({ ...mockWindowDimensions, viewportSmall: true }))
-    const setShowSidebar = jest.fn()
-    const { getByLabelText, getByText } = renderWithRouterAndTheme(
-      <Header
-        logoHref='/random_route'
-        actionItems={[<HeaderActionItem key={0} to='/random_route' text='random route' icon={<div />} />]}
-        sidebarItems={[
-          <Link key='location' to='/sidebar_route'>
-            <SidebarActionItem text='ChangeLocation' iconSrc='icon.jpg' />
-          </Link>,
-        ]}
-        cityName={cityName}
-        cityCode='test'
-        isSidebarOpen
-        setIsSidebarOpen={setShowSidebar}
-        language='de'
-      />,
-    )
-    expect(getByLabelText('layout:sideBarOpenAriaLabel')).toBeInTheDocument()
-    fireEvent.click(getByLabelText('layout:sideBarOpenAriaLabel'))
-    expect(getByText('ChangeLocation').parentElement!.parentElement).toHaveProperty(
-      'href',
-      'http://localhost/sidebar_route',
-    )
-    expect(getByText('layout,settings:disclaimer')).toHaveProperty('href', 'http://localhost/test/de/disclaimer')
-  })
-
-  it('should not render sidebar on large viewport', () => {
-    mocked(useWindowDimensions).mockImplementation(() => ({ ...mockWindowDimensions, viewportSmall: false }))
-    const { queryByTestId } = renderWithRouterAndTheme(
-      <Header
-        logoHref='/random_route'
-        actionItems={[<HeaderActionItem key={0} to='/random_route' text='random route' icon={<div />} />]}
-        sidebarItems={[
-          <Link key='location' to='/sidebar_route'>
-            <SidebarActionItem text='ChangeLocation' iconSrc='icon.jpg' />
-          </Link>,
-        ]}
-        language='de'
-      />,
-    )
-    expect(queryByTestId('layout:sideBarOpenAriaLabel')).not.toBeInTheDocument()
   })
 })
