badf7f8d2ec199e8a46c1394352445bfe94c2e3f
lunars97 <alekbarova.aizhana@gmail.com>
2025-09-09 16:02:57 +0200

    3382: Move Tooltip to secondaryAction and adjust styles

--- web/src/components/EventListItem.tsx
+++ web/src/components/EventListItem.tsx
@@ -26,34 +26,39 @@ import { EXCERPT_MAX_CHARS } from '../constants'
 import useWindowDimensions from '../hooks/useWindowDimensions'
 import Link from './base/Link'
 
-const StyledListItemButton = styled(ListItemButton)(({ theme }) => ({
+const StyledListItem = styled(ListItem)(() => ({
+  '& .MuiListItemSecondaryAction-root': {
+    top: 32,
+  },
+}))
+
+const StyledListItemButton = styled(ListItemButton)(() => ({
   display: 'flex',
   justifyContent: 'space-between',
+  alignItems: 'flex-start',
   gap: 16,
-  flexDirection: theme.direction === 'rtl' ? 'row-reverse' : 'row',
 })) as typeof ListItemButton
 
 const StyledListItemAvatar = styled(ListItemAvatar)(({ theme }) => ({
   '& .MuiAvatar-root': {
-    width: 100,
-    height: 100,
+    marginTop: 8,
+    width: 96,
+    height: 96,
   },
 
   [theme.breakpoints.down('sm')]: {
     '& .MuiAvatar-root': {
-      width: 60,
-      height: 60,
+      width: 64,
+      height: 64,
     },
   },
 }))
 
-const ItemTextWrapper = styled('div')`
-  display: flex;
-  justify-content: space-between;
-  gap: 8px;
-`
-
 const Description = styled('div')`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
   & [class*='MuiTypography-root'] {
     margin: 0;
   }
@@ -101,37 +106,36 @@ const EventListItem = ({
   const thumbnailSrc = event.thumbnail || getEventPlaceholder(event.path)
 
   return (
-    <ListItem dir='auto' divider disablePadding>
-      <StyledListItemButton component={Link} to={event.path} alignItems='flex-start'>
+    <StyledListItem
+      dir='auto'
+      divider
+      disablePadding
+      secondaryAction={dateIcon && <Tooltip title={t(dateIcon.tooltip)}>{dateIcon.Icon}</Tooltip>}>
+      <StyledListItemButton component={Link} to={event.path} dir='auto'>
         {Boolean(thumbnailSrc) && (
           <StyledListItemAvatar>
             <Avatar src={thumbnailSrc} alt='' variant='square' />
           </StyledListItemAvatar>
         )}
         <ListItemText
+          primary={<Typography variant='title2'>{event.title}</Typography>}
           slotProps={{
             secondary: {
               component: 'div',
             },
           }}
           secondary={
-            <ItemTextWrapper dir='auto'>
-              <Description>
-                <Typography variant='title2' component='h2' sx={{ marginBottom: '6px' }}>
-                  {event.title}
-                </Typography>
-                <Typography variant='body1'>{dateToDisplay.toFormattedString(languageCode, viewportSmall)}</Typography>
-                {event.location && <Typography variant='body1'>{event.location.fullAddress}</Typography>}
-                <Typography variant='body1' sx={{ paddingTop: '4px' }}>
-                  {getExcerpt(event.excerpt, { maxChars: EXCERPT_MAX_CHARS })}
-                </Typography>
-              </Description>
-              <div>{dateIcon && <Tooltip title={t(dateIcon.tooltip)}>{dateIcon.Icon}</Tooltip>}</div>
-            </ItemTextWrapper>
+            <Description dir='auto'>
+              <Typography variant='body1'>{dateToDisplay.toFormattedString(languageCode, viewportSmall)}</Typography>
+              {event.location && <Typography variant='body1'>{event.location.fullAddress}</Typography>}
+              <Typography variant='body1' sx={{ paddingTop: '4px' }}>
+                {getExcerpt(event.excerpt, { maxChars: EXCERPT_MAX_CHARS })}
+              </Typography>
+            </Description>
           }
         />
       </StyledListItemButton>
-    </ListItem>
+    </StyledListItem>
   )
 }
 
