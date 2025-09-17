8c11d6636ddc4adb29324d64f46c1433eca88ed4
LeandraH <LeandraH@users.noreply.github.com>
2025-09-17 13:02:16 +0200

    2978: Improve styling

--- web/src/components/DatesPageDetail.tsx
+++ web/src/components/DatesPageDetail.tsx
@@ -5,10 +5,11 @@ import { useTranslation } from 'react-i18next'
 import { MAX_DATE_RECURRENCES } from 'shared'
 import { DateModel } from 'shared/api'
 
-import { CalendarIcon, CalendarTodayIcon, ClockIcon } from '../assets'
+import { CalendarIcon, CalendarTodayIcon, ClockIcon, ExpandIcon } from '../assets'
 import dimensions from '../constants/dimensions'
 import PageDetail from './PageDetail'
 import Button from './base/Button'
+import Icon from './base/Icon'
 
 const Container = styled.div`
   display: grid;
@@ -29,8 +30,23 @@ const ContainerForThreeElements = styled(Container)`
 `
 
 const StyledButton = styled(Button)`
-  grid-column: 1 / 3;
   justify-self: start;
+  border-color: ${props => props.theme.colors.themeColor};
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 4px;
+  padding: 4px 8px;
+  display: flex;
+
+  @media ${dimensions.mediumLargeViewport} {
+    grid-column: 1 / 3;
+  }
+`
+
+const StyledIcon = styled(Icon)`
+  width: 16px;
+  height: 16px;
+  margin-inline-end: 8px;
 `
 
 type DatesPageDetailProps = {
@@ -54,6 +70,7 @@ const DatesPageDetail = ({ date }: DatesPageDetailProps): ReactElement | null =>
             </React.Fragment>
           ))}
         <StyledButton type='button' onClick={() => setClicksOnShowMore(clicksOnShowMore + 1)}>
+          <StyledIcon src={ExpandIcon} title='' />
           {t('common:showMore')}
         </StyledButton>
       </Container>
