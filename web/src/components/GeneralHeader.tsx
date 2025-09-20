77e426a7a20ab52b14dcc029ebfef47a9a5803e4
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-20 20:07:02 +0200

    3032: Add sidebar for general header

--- web/src/components/GeneralHeader.tsx
+++ web/src/components/GeneralHeader.tsx
@@ -1,4 +1,7 @@
-import React, { ReactElement } from 'react'
+import ContrastIcon from '@mui/icons-material/Contrast'
+import { useTheme } from '@mui/material/styles'
+import React, { ReactElement, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useLocation } from 'react-router-dom'
 
 import { LANDING_ROUTE, pathnameFromRouteInformation } from 'shared'
@@ -7,6 +10,8 @@ import { createCitiesEndpoint, useLoadFromEndpoint } from 'shared/api'
 import { cmsApiBaseUrl } from '../constants/urls'
 import Header from './Header'
 import HeaderLanguageSelectorItem from './HeaderLanguageSelectorItem'
+import Sidebar from './Sidebar'
+import SidebarActionItem from './SidebarActionItem'
 
 type GeneralHeaderProps = {
   languageCode: string
@@ -14,8 +19,12 @@ type GeneralHeaderProps = {
 
 const GeneralHeader = ({ languageCode }: GeneralHeaderProps): ReactElement => {
   const { data: cities } = useLoadFromEndpoint(createCitiesEndpoint, cmsApiBaseUrl, undefined)
-  const landingPath = pathnameFromRouteInformation({ route: LANDING_ROUTE, languageCode })
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const { toggleTheme } = useTheme()
+  const { t } = useTranslation('layout')
   const slug = useLocation().pathname.split('/')[1]
+
+  const landingPath = pathnameFromRouteInformation({ route: LANDING_ROUTE, languageCode })
   const languages = [
     ...new Map(cities?.flatMap(city => (city.live ? city.languages : [])).map(item => [item.code, item])).values(),
   ]
@@ -25,17 +34,19 @@ const GeneralHeader = ({ languageCode }: GeneralHeaderProps): ReactElement => {
     path: `/${slug}/${language.code}`,
   }))
 
-  const actionItems =
-    languageChangePaths.length > 0
-      ? [
-          <HeaderLanguageSelectorItem
-            key='languageChange'
-            languageChangePaths={languageChangePaths}
-            languageCode={languageCode}
-            forceText
-          />,
-        ]
-      : []
+  const actionItems = [
+    languageChangePaths.length > 0 ? (
+      <HeaderLanguageSelectorItem
+        key='languageChange'
+        languageChangePaths={languageChangePaths}
+        languageCode={languageCode}
+        forceText
+      />
+    ) : null,
+    <Sidebar key='sidebar' setOpen={setSidebarOpen} open={sidebarOpen}>
+      <SidebarActionItem key='theme' text={t('contrastTheme')} icon={<ContrastIcon />} onClick={toggleTheme} />
+    </Sidebar>,
+  ]
 
   return <Header logoHref={landingPath} actionItems={actionItems} language={languageCode} />
 }
