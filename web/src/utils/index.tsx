f823cc6e6f2e3e1e1280a8d34c21a92fc3100a47
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-23 13:28:14 +0200

    3536: Use language names from config

--- web/src/utils/index.tsx
+++ web/src/utils/index.tsx
@@ -1,7 +1,14 @@
 import Divider from '@mui/material/Divider'
 import React, { ReactElement } from 'react'
 
+import { LanguageModel } from 'shared/api'
+import { config } from 'translations'
+
 export const join = <T, U>(items: T[], separator: (index: number) => U): (T | U)[] =>
   items.flatMap((item, index) => [item, separator(index)]).slice(0, -1)
 
 export const withDividers = (items: ReactElement[]): ReactElement[] => join(items, index => <Divider key={index} />)
+
+export const supportedLanguages: LanguageModel[] = Object.entries(config.supportedLanguages)
+  .map(([code, language]) => new LanguageModel(code, language.name))
+  .sort((a, b) => a.code.localeCompare(b.code))
