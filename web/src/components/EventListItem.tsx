b0db9a455f7c6d1a3d0db05c560a6a8c851757c4
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-09-28 10:03:34 +0200

    3525: Fix event list item direction

--- web/src/components/EventListItem.tsx
+++ web/src/components/EventListItem.tsx
@@ -101,11 +101,10 @@ const EventListItem = ({
 
   return (
     <StyledListItem
-      dir='auto'
       divider
       disablePadding
       secondaryAction={dateIcon && <Tooltip title={t(dateIcon.tooltip)}>{dateIcon.Icon}</Tooltip>}>
-      <StyledListItemButton component={Link} to={event.path} dir='auto'>
+      <StyledListItemButton component={Link} to={event.path}>
         <ListItemAvatar>
           <StyledAvatar src={thumbnailSrc} alt='' variant='square' />
         </ListItemAvatar>
