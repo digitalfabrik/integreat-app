7b6c943e3a3f8f0b0698fdc884ada7b518eca8a8
bahaa <bahaa.tuffaha@yahoo.com>
2025-10-01 18:43:05 +0300

    3380: Refactor CategoryListItem component and update tests to remove unused styles and props

--- web/src/components/CategoryListItem.tsx
+++ web/src/components/CategoryListItem.tsx
@@ -4,6 +4,7 @@ import ListItem from '@mui/material/ListItem'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
+import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 import { styled } from '@mui/material/styles'
 import React, { ReactElement } from 'react'
@@ -13,28 +14,14 @@ import { CategoryModel } from 'shared/api'
 import Link from './base/Link'
 import List from './base/List'
 
-const StyledList = styled(List)`
-  padding: 0;
-`
-
 const StyledListItem = styled(ListItem)`
   flex-direction: column;
   align-items: stretch;
 `
 
-const SubCategoryListItem = styled(ListItem)`
-  padding: 0;
-`
-
 const StyledSubCategoryListItemButton = styled(ListItemButton)`
-  padding-left: 72px;
   min-height: 56px;
 ` as typeof ListItemButton
-
-const StyledTypography = styled(Typography)`
-  word-wrap: break-word;
-  font-weight: 700;
-`
 
 type CategoryListItemProps = {
   category: CategoryModel
@@ -43,35 +30,38 @@ type CategoryListItemProps = {
 
 const CategoryListItem = ({ category, subCategories }: CategoryListItemProps): ReactElement => {
   const SubCategories = subCategories.map(subCategory => {
-    const { path, thumbnail, title } = subCategory
+    const { path, title } = subCategory
     return (
-      <SubCategoryListItem key={path}>
+      <ListItem disablePadding key={path}>
         <StyledSubCategoryListItemButton component={Link} to={path}>
-          {!!thumbnail && (
-            <ListItemAvatar>
-              <Avatar src={thumbnail} variant='square' />
-            </ListItemAvatar>
-          )}
           <ListItemText primary={<Typography variant='body1'>{title}</Typography>} />
         </StyledSubCategoryListItemButton>
-      </SubCategoryListItem>
+      </ListItem>
     )
   })
 
   return (
     <StyledListItem disablePadding>
-      <ListItemButton component={Link} to={category.path} dir='auto'>
+      <ListItemButton component={Link} to={category.path}>
         {!!category.thumbnail && (
           <ListItemAvatar>
             <Avatar src={category.thumbnail} variant='square' />
           </ListItemAvatar>
         )}
-        <ListItemText primary={<StyledTypography variant='title2'>{category.title}</StyledTypography>} />
+        <ListItemText
+          primary={
+            <Typography fontWeight='bold' variant='title2'>
+              {category.title}
+            </Typography>
+          }
+        />
       </ListItemButton>
       {SubCategories.length > 0 && (
         <>
           <Divider />
-          <StyledList isInset items={SubCategories} />
+          <Stack paddingInlineStart={7}>
+            <List items={SubCategories} />
+          </Stack>
         </>
       )}
     </StyledListItem>
