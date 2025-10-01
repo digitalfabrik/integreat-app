79d22de9d4aee9bc1da79fdf8f93d770378ed848
bahaa <bahaa.tuffaha@yahoo.com>
2025-10-01 21:49:27 +0300

    3522: Used useResourceCache to get the cached PDF

--- native/src/routes/PDFViewModal.tsx
+++ native/src/routes/PDFViewModal.tsx
@@ -1,4 +1,3 @@
-import md5 from 'md5'
 import React, { ReactElement, useState, useEffect } from 'react'
 import { View } from 'react-native'
 import ReactNativeBlobUtil from 'react-native-blob-util'
@@ -11,7 +10,8 @@ import { ErrorCode } from 'shared/api'
 import Failure from '../components/Failure'
 import LoadingSpinner from '../components/LoadingSpinner'
 import { NavigationProps, RouteProps } from '../constants/NavigationTypes'
-import { reportError } from '../utils/sentry'
+import useCityAppContext from '../hooks/useCityAppContext'
+import useResourceCache from '../hooks/useResourceCache'
 
 const LoadingSpinnerContainer = styled(View)`
   flex: 1;
@@ -34,43 +34,25 @@ const PDFViewModal = ({ route, navigation: _navigation }: PDFViewModalProps): Re
   const [loading, setLoading] = useState<boolean>(true)
   const [localPath, setLocalPath] = useState<string>('')
   const { url } = route.params
+  const { cityCode, languageCode } = useCityAppContext()
+  const resourceCache = useResourceCache({ cityCode, languageCode })
 
   useEffect(() => {
     const loadPdf = async () => {
-      try {
-        if (url.startsWith('file://') || url.startsWith('/')) {
-          setLocalPath(url)
-          setLoading(false)
-          return
-        }
+      const fileHash = url.substring(url.lastIndexOf('/') + 1).split('.')[0]
+      const cachedFile = Object.values(resourceCache)
+        .flatMap(pageCache => Object.values(pageCache))
+        .find(resourceEntry => resourceEntry.hash === fileHash)
 
-        const hashedUrl = md5(url).toString()
-        const fileName = `${hashedUrl}.pdf`
-        const filePath = `${ReactNativeBlobUtil.fs.dirs.CacheDir}/${fileName}`
-        const fileUri = `file://${filePath}`
-
-        const exists = await ReactNativeBlobUtil.fs.exists(filePath)
-        if (exists) {
-          setLocalPath(fileUri)
-          setLoading(false)
-          return
-        }
-
-        await ReactNativeBlobUtil.config({
-          path: filePath,
-        }).fetch('GET', url)
-
-        setLocalPath(fileUri)
-        setLoading(false)
-      } catch (err) {
-        reportError(`Error handling PDF: ${err}`)
-        setError(true)
+      const exists = await ReactNativeBlobUtil.fs.exists(cachedFile?.filePath ?? '')
+      if (exists) {
+        setLocalPath(`file://${cachedFile?.filePath}`)
         setLoading(false)
       }
     }
 
     loadPdf()
-  }, [url])
+  }, [resourceCache, url])
 
   if (error) {
     return <Failure code={ErrorCode.UnknownError} />
