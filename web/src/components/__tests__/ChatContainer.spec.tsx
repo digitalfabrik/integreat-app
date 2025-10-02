097af6ae7fab39f9b034067f4e087cb50688baec
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-10-02 09:59:46 +0200

    3513: Fix aria-label

--- web/src/components/__tests__/ChatContainer.spec.tsx
+++ web/src/components/__tests__/ChatContainer.spec.tsx
@@ -51,7 +51,7 @@ describe('ChatContainer', () => {
     const chatButtonContainer = getAllByText(getChatName('IntegreatTestCms'))[0]!
     expect(chatButtonContainer).toBeTruthy()
     fireEvent.click(chatButtonContainer!)
-    const closeButton = getByLabelText('layout:close')
+    const closeButton = getByLabelText('layout:common:close')
     fireEvent.click(closeButton)
     expect(queryByText('chat:conversationTitle')).toBeFalsy()
     expect(queryByText('chat:conversationText')).toBeFalsy()
