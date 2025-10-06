ab40a4d1b03adaf41c22601817336a6dca9b025d
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-10-07 00:02:14 +0200

    3551: Only show legal on mobile

--- web/src/utils/index.tsx
+++ web/src/utils/index.tsx
@@ -7,7 +7,11 @@ import { config } from 'translations'
 export const join = <T, U>(items: T[], separator: (index: number) => U): (T | U)[] =>
   items.flatMap((item, index) => [item, separator(index)]).slice(0, -1)
 
-export const withDividers = (items: ReactElement[]): ReactElement[] => join(items, index => <Divider key={index} />)
+export const withDividers = (items: (ReactElement | null)[]): ReactElement[] =>
+  join(
+    items.filter(it => it !== null),
+    index => <Divider key={index} />,
+  )
 
 export const supportedLanguages: LanguageModel[] = Object.entries(config.supportedLanguages)
   .map(([code, language]) => new LanguageModel(code, language.name))
