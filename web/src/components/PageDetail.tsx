5ace376cd6197e7982e87d1972bdcaaadafa6b57
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-10-07 17:05:26 +0200

    3510: Remove legacy button component and usages

--- web/src/components/PageDetail.tsx
+++ web/src/components/PageDetail.tsx
@@ -1,36 +1,32 @@
-import { styled } from '@mui/material/styles'
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 import React, { ReactElement } from 'react'
 
 import Link from './base/Link'
-
-const Container = styled('div')<{ withPadding?: boolean }>`
-  display: flex;
-  padding-inline-start: ${props => (props.withPadding ? '32px' : '0')};
-`
-
-const Identifier = styled('span')`
-  font-weight: 700;
-`
 
 type PageDetailProps = {
   identifier?: string
   icon?: ReactElement
   information: string
+  secondaryInformation?: string
   path?: string | null
 }
 
-const PageDetail = ({ identifier, information, path, icon }: PageDetailProps): ReactElement => (
-  <Container withPadding={!icon && !identifier}>
-    {!!identifier && <Identifier>{identifier}: </Identifier>}
+const PageDetail = ({ identifier, information, secondaryInformation, path, icon }: PageDetailProps): ReactElement => (
+  <Stack direction='row' marginLeft={!icon && !identifier ? 4 : 0} gap={1}>
+    {!!identifier && <Typography variant='label2'>{identifier}: </Typography>}
     {icon}
-    {path ? (
-      <Link to={path} highlighted>
-        {information}
-      </Link>
-    ) : (
-      <span>{information}</span>
-    )}
-  </Container>
+    <Stack direction='row' flexWrap='wrap' gap={1}>
+      {path ? (
+        <Link to={path} highlighted>
+          <Typography>{information}</Typography>
+        </Link>
+      ) : (
+        <Typography>{information}</Typography>
+      )}
+      <Typography>{secondaryInformation}</Typography>
+    </Stack>
+  </Stack>
 )
 
 export default PageDetail
