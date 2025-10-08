98638f93e9748cf606de6d72256d45eed4e3c1a9
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-10-08 13:21:33 +0200

    3551: Add keyboard accessibility to menu

--- web/src/components/MenuAccordion.tsx
+++ web/src/components/MenuAccordion.tsx
@@ -1,38 +1,11 @@
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import Accordion from '@mui/material/Accordion'
-import AccordionDetails from '@mui/material/AccordionDetails'
-import AccordionSummary, { accordionSummaryClasses } from '@mui/material/AccordionSummary'
 import Divider from '@mui/material/Divider'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import MuiMenuItem from '@mui/material/MenuItem'
-import Typography from '@mui/material/Typography'
-import { styled, useTheme } from '@mui/material/styles'
 import React, { ReactElement } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { withDividers } from '../utils'
-
-const StyledAccordion = styled(Accordion)({
-  margin: 0,
-  background: 'transparent',
-
-  '&:before': {
-    display: 'none',
-  },
-})
-
-const StyledAccordionSummary = styled(AccordionSummary)({
-  padding: '6px 16px',
-  minHeight: 0,
-
-  [`.${accordionSummaryClasses.content}`]: {
-    margin: 0,
-    alignItems: 'center',
-  },
-}) as typeof AccordionSummary
-
-const StyledAccordionDetails = styled(AccordionDetails)({
-  paddingBlock: 0,
-})
+import MenuItem from './MenuItem'
 
 type MenuAccordionProps = {
   title: string
@@ -43,27 +16,24 @@ type MenuAccordionProps = {
 }
 
 const MenuAccordion = ({ title, items, setExpanded, expanded, icon }: MenuAccordionProps): ReactElement => {
-  const { contentDirection } = useTheme()
+  const { t } = useTranslation('common')
+
   return (
-    <StyledAccordion
-      onChange={(_, isExpanded) => setExpanded(isExpanded)}
-      expanded={expanded}
-      elevation={0}
-      defaultExpanded={false}
-      disableGutters
-      slotProps={{
-        heading: {
-          component: 'div',
-        },
-      }}
-      dir={contentDirection}>
-      <StyledAccordionSummary component={MuiMenuItem} expandIcon={<ExpandMoreIcon />}>
-        {icon && <ListItemIcon>{icon}</ListItemIcon>}
-        <Typography variant='body1'>{title}</Typography>
-      </StyledAccordionSummary>
-      <Divider />
-      <StyledAccordionDetails>{withDividers(items)}</StyledAccordionDetails>
-    </StyledAccordion>
+    <>
+      <MenuItem
+        onClick={() => setExpanded(!expanded)}
+        text={title}
+        icon={icon}
+        iconEnd={expanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+        aria-label={t(expanded ? 'showLess' : 'showMore')}
+      />
+      {expanded && (
+        <>
+          <Divider />
+          {withDividers(items)}
+        </>
+      )}
+    </>
   )
 }
 
