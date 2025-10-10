3451e700af0d530b29ae726b1db715d2bac44b9e
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-10-10 18:31:36 +0200

    3496: Use object notation

--- web/src/components/Tile.tsx
+++ web/src/components/Tile.tsx
@@ -1,3 +1,4 @@
+import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import { styled } from '@mui/material/styles'
 import React, { ReactElement, useCallback } from 'react'
@@ -8,42 +9,41 @@ import { useLoadAsync } from 'shared/api'
 import { fetchObjectCached } from '../utils'
 import Link from './base/Link'
 
-const StyledLink = styled(Link)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  align-self: flex-start;
-  width: min(160px, 36vw);
-  cursor: pointer;
-  gap: 8px;
-`
+const StyledButton = styled(Button)({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  alignSelf: 'flex-start',
+  width: 'min(160px, 36vw)',
+  gap: 8,
+}) as typeof Button
 
-const Outline = styled('div')`
-  width: min(140px, 32vw);
-  height: min(140px, 32vw);
+const Outline = styled('div')(({ theme }) => ({
+  width: 'min(140px, 32vw)',
+  height: 'min(140px, 32vw)',
 
-  :hover {
-    ${props =>
-      props.theme.isContrastTheme &&
-      `
-        outline: 8px solid ${props.theme.palette.secondary.main};
-        border-radius: 24px;
-      `}
-  }
-`
+  ...(theme.isContrastTheme && {
+    ':hover': {
+      outline: `8px solid ${theme.palette.secondary.main}`,
+      borderRadius: 24,
+    },
+  }),
+}))
 
-const StyledImage = styled('img')`
-  width: 100%;
-  height: 100%;
-  transition: transform 0.2s;
-  object-fit: contain;
+const StyledImage = styled('img')(({ theme }) => ({
+  width: '100%',
+  height: '100%',
+  transition: 'transform 0.2s',
+  objectFit: 'contain',
 
-  filter: ${props => (props.theme.isContrastTheme ? 'invert(1) saturate(0) brightness(7)' : 'none')};
+  ':hover': {
+    transform: 'scale(1.01)',
+  },
 
-  :hover {
-    transform: scale(1.01);
-  }
-`
+  ...(theme.isContrastTheme && {
+    filter: 'invert(1) saturate(0) brightness(7)',
+  }),
+}))
 
 type TileProps = {
   tile: TileModel
@@ -53,14 +53,14 @@ const Tile = ({ tile }: TileProps): ReactElement => {
   const { data } = useLoadAsync(useCallback(() => fetchObjectCached(tile.thumbnail), [tile.thumbnail]))
 
   return (
-    <StyledLink to={tile.path}>
+    <StyledButton component={Link} to={tile.path} color='inherit' disableFocusRipple>
       <Outline>
         <StyledImage alt='' src={data?.objectUrl} />
       </Outline>
       <Typography variant='body1' textAlign='center'>
         {tile.title}
       </Typography>
-    </StyledLink>
+    </StyledButton>
   )
 }
 
