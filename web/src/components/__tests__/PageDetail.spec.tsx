fa9bbf232418174435a0af0ba23c6f6056a5981d
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-10-14 12:01:45 +0200

    3496: Remove unused identifier

--- web/src/components/__tests__/PageDetail.spec.tsx
+++ web/src/components/__tests__/PageDetail.spec.tsx
@@ -4,21 +4,18 @@ import { renderWithRouterAndTheme } from '../../testing/render'
 import PageDetail from '../PageDetail'
 
 describe('PageDetail', () => {
-  const identifier = 'Date'
   const information = 'May 22, 2020 1:00 AM'
   const path = '/testumgebung/de/location1'
 
   it('should render correctly without a path', () => {
-    const { getByText } = renderWithRouterAndTheme(<PageDetail identifier={identifier} information={information} />)
-    expect(getByText(`${identifier}:`)).toBeDefined()
+    const { getByText } = renderWithRouterAndTheme(<PageDetail icon={<div />} information={information} />)
     expect(getByText(information)).toBeDefined()
   })
 
   it('should render correctly with a path', () => {
     const { getByText, getByRole } = renderWithRouterAndTheme(
-      <PageDetail identifier={identifier} information={information} path={path} />,
+      <PageDetail icon={<div />} information={information} path={path} />,
     )
-    expect(getByText(`${identifier}:`)).toBeDefined()
     expect(getByText(information)).toBeDefined()
     expect(getByRole('link', { name: information })).toHaveAttribute('href', path)
   })
