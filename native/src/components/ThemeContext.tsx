1f0e51349e05feffa90fb72c506a0a176626acde
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-11-04 15:56:29 +0200

    3693: Remove unused ThemeContext

--- native/src/components/ThemeContext.tsx
+++ native/src/components/ThemeContext.tsx
@@ -1,49 +1,24 @@
-import React, { createContext, ReactElement, useCallback, useMemo } from 'react'
+import React, { ReactElement, useMemo } from 'react'
 import { ThemeProvider } from 'styled-components/native'
-
-import { LegacyThemeType, ThemeKey } from 'build-configs/index'
 
 import buildConfig from '../constants/buildConfig'
 import { useAppContext } from '../hooks/useCityAppContext'
-
-export type ThemeContextType = {
-  theme: LegacyThemeType
-  themeType: ThemeKey
-  toggleTheme: () => void
-}
-
-const themeConfig = buildConfig()
-
-export const ThemeContext = createContext<ThemeContextType>({
-  theme: themeConfig.legacyLightTheme,
-  themeType: 'light',
-  toggleTheme: () => undefined,
-})
 
 type ThemeContainerProps = {
   children: ReactElement
 }
 
 export const ThemeContainer = ({ children }: ThemeContainerProps): ReactElement => {
-  const { settings, updateSettings } = useAppContext()
+  const { settings } = useAppContext()
   const themeType = settings.selectedTheme
-
-  const toggleTheme = useCallback(() => {
-    const newTheme: ThemeKey = themeType === 'light' ? 'contrast' : 'light'
-    updateSettings({ selectedTheme: newTheme })
-  }, [themeType, updateSettings])
 
   const contextValue = useMemo(() => {
     const theme =
       themeType === 'contrast'
-        ? { ...themeConfig.legacyContrastTheme, isContrastTheme: true }
-        : { ...themeConfig.legacyLightTheme, isContrastTheme: false }
-    return { theme, themeType, toggleTheme }
-  }, [themeType, toggleTheme])
+        ? { ...buildConfig().legacyContrastTheme, isContrastTheme: true }
+        : { ...buildConfig().legacyLightTheme, isContrastTheme: false }
+    return { theme }
+  }, [themeType])
 
-  return (
-    <ThemeContext.Provider value={contextValue}>
-      <ThemeProvider theme={contextValue.theme}>{children}</ThemeProvider>
-    </ThemeContext.Provider>
-  )
+  return <ThemeProvider theme={contextValue.theme}>{children}</ThemeProvider>
 }
