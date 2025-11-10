5c48ef999a92528c3e9ba8f708ea4e41d33eb553
Steffen Kleinle <steffen.kleinle@mailbox.org>
2025-11-10 11:19:09 +0200

    3097: Handle missing license repository and add names
    Signed-off-by: Steffen Kleinle <steffen.kleinle@mailbox.org>

--- shared/utils/licences.ts
+++ shared/utils/licences.ts
@@ -1,7 +1,8 @@
 export type JsonLicenses = {
   [name: string]: {
     licenses: string
-    repository: string
+    repository?: string
+    publisher?: string
   }
 }
 
@@ -9,7 +10,8 @@ export type License = {
   name: string
   version: string | undefined
   licenses: string
-  repository: string
+  repository: string | undefined
+  publisher: string | undefined
 }
 
 // matches version number e.g. 1.0.2
@@ -19,8 +21,8 @@ const versionNumberRegex = /\d+(\.\d+)*/
 const versionRegex = /(?:@)\d+(\.\d+)*/
 
 export const parseLicenses = (licenseFile: JsonLicenses): License[] =>
-  Object.entries(licenseFile).map(([name, { licenses, repository }]) => {
+  Object.entries(licenseFile).map(([name, { licenses, repository, publisher }]) => {
     const version = name.match(versionNumberRegex)?.[0] ?? ''
     const nameWithoutVersion = name.replace(versionRegex, '')
-    return { name: nameWithoutVersion, version, repository, licenses }
+    return { name: nameWithoutVersion, version, repository, licenses, publisher }
   })
