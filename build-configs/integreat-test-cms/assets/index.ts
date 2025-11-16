8228b5dc4e68097fae393ec17d45dd1bf60f7d1d
MICHELLEGRAF\49157 <michelle.graf123@gmail.com>
2025-11-16 22:03:46 +0100

    3655 - fixed asset error issue to start the app

---
+++ build-configs/integreat-test-cms/assets/index.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-relative-packages */
+import EventThumbnailPlaceholder1 from '../../../assets/event-thumbnail-placeholder-1.jpg'
+import EventThumbnailPlaceholder2 from '../../../assets/event-thumbnail-placeholder-2.jpg'
+import EventThumbnailPlaceholder3 from '../../../assets/event-thumbnail-placeholder-3.jpg'
+import BookIcon from '../../../assets/icons/book.svg'
+import CalendarTodayRecurringIcon from '../../../assets/icons/calendar-today-recurring.svg'
+import ExternalLinkIcon from '../../../assets/icons/external-link.svg'
+import PersonLightIcon from '../../../assets/icons/person-light.svg'
+import PersonIcon from '../../../assets/icons/person.svg'
+import ReadAloudIcon from '../../../assets/icons/readAloud.svg'
+import SprungbrettIcon from '../../../assets/icons/sprungbrett.svg'
+import TuNewsActiveIcon from '../../../assets/icons/tu-news-active.svg'
+import TuNewsInactiveIcon from '../../../assets/icons/tu-news-inactive.svg'
+
+export {
+  BookIcon,
+  CalendarTodayRecurringIcon,
+  EventThumbnailPlaceholder1,
+  EventThumbnailPlaceholder2,
+  EventThumbnailPlaceholder3,
+  ExternalLinkIcon,
+  PersonIcon,
+  PersonLightIcon,
+  ReadAloudIcon,
+  SprungbrettIcon,
+  TuNewsActiveIcon,
+  TuNewsInactiveIcon,
+}
