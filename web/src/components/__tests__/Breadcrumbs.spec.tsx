b22a2d5c5c28adc96dd9f10b11930cfc43e83f5f
Michael Markl <marklmichael98@gmail.com>
2025-11-21 13:51:16 +0100

    Adjust breadcrumb tests to new behavior

--- web/src/components/__tests__/Breadcrumbs.spec.tsx
+++ web/src/components/__tests__/Breadcrumbs.spec.tsx
@@ -6,11 +6,6 @@ import Breadcrumbs from '../Breadcrumbs'
 
 jest.mock('react-i18next')
 
-const homeBreadcrumb: BreadcrumbProps = {
-  title: 'Home',
-  to: '/',
-}
-
 const breadcrumb0: BreadcrumbProps = {
   title: 'Landkreis München',
   to: '/lkmuenchen/de',
@@ -26,13 +21,23 @@ const breadcrumb2: BreadcrumbProps = {
   to: '/lkmuenchen/de/ankommen-und-leben-in-deutschland/mobilitaet',
 }
 
+const breadcrumb3: BreadcrumbProps = {
+  title: 'Mobilitätsförderung',
+  to: '/lkmuenchen/de/ankommen-und-leben-in-deutschland/mobilitaet/foerderung',
+}
+
+const breadcrumb4: BreadcrumbProps = {
+  title: 'Mobilitätsförderungsdetails',
+  to: '/lkmuenchen/de/ankommen-und-leben-in-deutschland/mobilitaet/foerderung/details',
+}
+
 const render = (ancestors: BreadcrumbProps[], current: BreadcrumbProps) =>
   renderWithRouterAndTheme(<Breadcrumbs breadcrumbs={[...ancestors, current]} />)
 
 describe('Breadcrumbs', () => {
   it('should display correctly on the first level', () => {
     const ancestors = [breadcrumb0]
-    const { getAllByRole, queryByText } = render(ancestors, breadcrumb0)
+    const { getAllByRole, queryByText } = render(ancestors, breadcrumb1)
 
     const breadcrumbLink = getAllByRole('link', { name: breadcrumb0.title })
     expect(breadcrumbLink[0]?.getAttribute('href')).toBe(breadcrumb0.to)
@@ -41,24 +46,27 @@ describe('Breadcrumbs', () => {
 
   it('should display correctly on a lower level', () => {
     const ancestors = [breadcrumb0, breadcrumb1]
-    const { getAllByRole, getByRole, queryByText } = render(ancestors, breadcrumb2)
+    const { getAllByRole, queryByText } = render(ancestors, breadcrumb2)
 
-    expect(getAllByRole('link')[0]?.getAttribute('href')).toBe(breadcrumb0.to)
-    const breadcrumbLink = getByRole('link', { name: breadcrumb1.title })
-    expect(breadcrumbLink.getAttribute('href')).toBe(breadcrumb1.to)
-    expect(queryByText(breadcrumb2.title)).toBeTruthy()
+    const firstBreadcrumbLink = getAllByRole('link')[0]
+    expect(firstBreadcrumbLink?.getAttribute('href')).toBe(breadcrumb0.to)
+    expect(firstBreadcrumbLink).toHaveTextContent('')
+    const secondBreadcrumbLink = getAllByRole('link')[1]
+    expect(secondBreadcrumbLink?.getAttribute('href')).toBe(breadcrumb1.to)
+    expect(secondBreadcrumbLink).toHaveTextContent(breadcrumb1.title)
+    expect(queryByText(breadcrumb2.title)).toBeFalsy()
   })
 
-  it('should show menu button when there are more than 3 breadcrumbs', () => {
-    const ancestors = [homeBreadcrumb, breadcrumb0, breadcrumb1]
-    const { getByLabelText } = render(ancestors, breadcrumb2)
+  it('should show menu button when there are more than 4 breadcrumbs', () => {
+    const ancestors = [breadcrumb0, breadcrumb1, breadcrumb2, breadcrumb3]
+    const { getByLabelText } = render(ancestors, breadcrumb4)
 
     expect(getByLabelText('common:showMore')).toBeTruthy()
   })
 
   it('should show home icon for first breadcrumb when multiple exist', () => {
-    const ancestors = [homeBreadcrumb, breadcrumb0]
-    const { getByTestId } = render(ancestors, breadcrumb1)
+    const ancestors = [breadcrumb0, breadcrumb1]
+    const { getByTestId } = render(ancestors, breadcrumb2)
 
     expect(getByTestId('HomeOutlinedIcon')).toBeTruthy()
   })
