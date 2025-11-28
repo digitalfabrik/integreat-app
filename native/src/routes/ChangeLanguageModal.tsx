cc00ee465f70590971f11f5bf03ec1787170ca56
LeandraH <LeandraH@users.noreply.github.com>
2025-11-28 17:26:37 +0100

    3745: Add search

--- native/src/routes/ChangeLanguageModal.tsx
+++ native/src/routes/ChangeLanguageModal.tsx
@@ -1,12 +1,28 @@
-import React, { ReactElement, useContext } from 'react'
+import { shouldPolyfill } from '@formatjs/intl-displaynames/should-polyfill'
+import '@formatjs/intl-locale/polyfill'
+import React, { ReactElement, useContext, useEffect, useMemo, useRef, useState } from 'react'
+import { Searchbar } from 'react-native-paper'
 import styled from 'styled-components/native'
 
 import { ChangeLanguageModalRouteType } from 'shared'
+import { LanguageModel } from 'shared/api'
+import { config } from 'translations'
 
 import Selector from '../components/Selector'
 import { NavigationProps, RouteProps } from '../constants/NavigationTypes'
 import { AppContext } from '../contexts/AppContextProvider'
 import SelectorItemModel from '../models/SelectorItemModel'
+import importDisplayNamesPackage from '../utils/importDisplayNamesPackage'
+
+// https://formatjs.github.io/docs/polyfills/intl-displaynames/#dynamic-import--capability-detection
+const loadPolyfillIfNeeded = async (locale: string): Promise<void> => {
+  const unsupportedLocale = shouldPolyfill(locale)
+  if (unsupportedLocale === undefined) {
+    return
+  }
+  await import('@formatjs/intl-displaynames/polyfill-force')
+  await importDisplayNamesPackage(locale)
+}
 
 const Wrapper = styled.ScrollView`
   background-color: ${props => props.theme.legacy.colors.backgroundColor};
@@ -17,11 +33,44 @@ type ChangeLanguageModalProps = {
   navigation: NavigationProps<ChangeLanguageModalRouteType>
 }
 
+const filterLanguageChangePath = (
+  languageList: LanguageModel,
+  query: string,
+  languageNamesInCurrentLanguage: Intl.DisplayNames,
+  languageNamesInFallbackLanguage: Intl.DisplayNames,
+): boolean => {
+  if (query === '') {
+    return true
+  }
+  return (
+    languageList.name.toLowerCase().includes(query.toLowerCase()) ||
+    !!languageNamesInCurrentLanguage.of(languageList.code)?.toLowerCase().includes(query.toLowerCase()) ||
+    !!languageNamesInFallbackLanguage.of(languageList.code)?.toLowerCase().includes(query.toLowerCase())
+  )
+}
+
 const ChangeLanguageModal = ({ navigation, route }: ChangeLanguageModalProps): ReactElement => {
   const { languages, availableLanguages } = route.params
   const { languageCode, changeLanguageCode } = useContext(AppContext)
+  const translationsRef = useRef<InstanceType<typeof Intl.DisplayNames> | undefined>(undefined)
+  const [query, setQuery] = useState('')
 
-  const selectorItems = languages.map(({ code, name }) => {
+  useEffect(() => {
+    ;(async () => {
+      await loadPolyfillIfNeeded(languageCode)
+      translationsRef.current = new Intl.DisplayNames([languageCode], { type: 'language' })
+    })()
+  }, [languageCode])
+
+  const filteredLanguageChangePaths = useMemo(() => {
+    const languageNamesInCurrentLanguage = new Intl.DisplayNames([languageCode], { type: 'language' })
+    const languageNamesInFallbackLanguage = new Intl.DisplayNames([config.sourceLanguage], { type: 'language' })
+    return languages.filter(item =>
+      filterLanguageChangePath(item, query, languageNamesInCurrentLanguage, languageNamesInFallbackLanguage),
+    )
+  }, [languages, query, languageCode])
+
+  const selectorItems = filteredLanguageChangePaths.map(({ code, name }) => {
     const isLanguageAvailable = availableLanguages.includes(code)
     return new SelectorItemModel({
       code,
@@ -37,7 +86,8 @@ const ChangeLanguageModal = ({ navigation, route }: ChangeLanguageModalProps): R
   })
 
   return (
-    <Wrapper contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}>
+    <Wrapper contentContainerStyle={{ flexGrow: 1, justifyContent: 'flex-start' }}>
+      <Searchbar placeholder='Search' onChangeText={setQuery} value={query} mode='bar' />
       <Selector selectedItemCode={languageCode} items={selectorItems} />
     </Wrapper>
   )
