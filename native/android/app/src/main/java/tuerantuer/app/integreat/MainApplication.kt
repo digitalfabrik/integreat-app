387c836e7c4ffa3dee278e9f2195fe062355735d
bahaa <bahaa.tuffaha@yahoo.com>
2026-02-05 15:10:58 +0200

    Reverted RTL changes from MainApplication

--- native/android/app/src/main/java/tuerantuer/app/integreat/MainApplication.kt
+++ native/android/app/src/main/java/tuerantuer/app/integreat/MainApplication.kt
@@ -1,14 +1,11 @@
 package tuerantuer.app.integreat
 
 import android.app.Application
-import android.text.TextUtils
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
-import com.facebook.react.modules.i18nmanager.I18nUtil
-import java.util.Locale
 
 class MainApplication : Application(), ReactApplication {
 
@@ -25,13 +22,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    
-    I18nUtil.getInstance().allowRTL(this, true)
-    
-    // Force RTL only if Android reports the current locale as RTL.
-    val isRTL = TextUtils.getLayoutDirectionFromLocale(Locale.getDefault()) == android.view.View.LAYOUT_DIRECTION_RTL
-    I18nUtil.getInstance().forceRTL(this, isRTL)
-    
     loadReactNative(this)
   }
 }
