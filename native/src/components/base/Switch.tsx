480f46210bddddfaf70dce9f9dd7c54a2aadddb9
bahaa <bahaa.tuffaha@yahoo.com>
2026-03-04 15:44:54 +0200

    3880: Added a comment for ios_backgroundColor

--- native/src/components/base/Switch.tsx
+++ native/src/components/base/Switch.tsx
@@ -4,6 +4,7 @@ import { useTheme } from 'styled-components/native'
 
 const Switch = (props: SwitchProps): ReactElement => {
   const theme = useTheme()
+  // The ios_backgroundColor prop is used to set the background under the handle to improve contrast on iOS.
   return <RNSwitch ios_backgroundColor={theme.colors.outline} {...props} />
 }
 
