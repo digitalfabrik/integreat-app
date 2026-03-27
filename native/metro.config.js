9d2d0869fc0bda6f2a77431c86ddc396fdc88912
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-03-27 14:34:59 +0100

    2966: Add missing dependencies and disable hoisting for native to fix react native builds

--- native/metro.config.js
+++ native/metro.config.js
@@ -2,8 +2,6 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 const path = require('path')
 
-const root = path.resolve(__dirname, '..')
-
 /** @type {import('@react-native/metro-config').MetroConfig} */
 const defaultConfig = getDefaultConfig(__dirname)
 // Needed for rn-header-buttons, will be enabled by default in a future version of react-native
@@ -26,12 +24,22 @@ const config = {
         'build-config-name/index.ts',
       ),
     },
-    nodeModulesPaths: [path.resolve(root, './node_modules')],
+    nodeModulesPaths: [path.resolve(__dirname, './node_modules')],
+
+    // Make sure we use the local copy of react and react-native to avoid multiple copies in the bundle
+    // https://github.com/facebook/react/issues/13991#issuecomment-830308729
+    resolveRequest: (context, moduleName, platform) => {
+      const module =
+        moduleName === 'react' || moduleName === 'react-native'
+          ? path.join(__dirname, 'node_modules', moduleName)
+          : moduleName
+      return context.resolveRequest(context, module, platform)
+    },
     unstable_enablePackageExports: true,
   },
-  watchFolders: [root],
+  watchFolders: [path.resolve(__dirname, '../')],
   transformer: {
-    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    babelTransformerPath: require.resolve('react-native-svg-transformer/react-native'),
   },
 }
 
