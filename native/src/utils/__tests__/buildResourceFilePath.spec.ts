2c13806991de7fd0120b3db436fefa4c63307e9b
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-04-23 09:08:05 +0200

    4045: Bump database connector versions

--- native/src/utils/__tests__/buildResourceFilePath.spec.ts
+++ native/src/utils/__tests__/buildResourceFilePath.spec.ts
@@ -8,7 +8,7 @@ describe('buildResourceFilePath', () => {
     const region = 'augsburg'
     const hash = sha256(urlString)
     expect(buildResourceFilePath(urlString, region, hash)).toBe(
-      'path/to/documentDir/resource-cache/v3/augsburg/files/5aea0ed0ef75df32615272662c0419c858bfd800926bb1563a5012023ac3a6b7.png',
+      'path/to/documentDir/resource-cache/v4/augsburg/files/5aea0ed0ef75df32615272662c0419c858bfd800926bb1563a5012023ac3a6b7.png',
     )
   })
   it('should ignore invalid extension', () => {
@@ -16,7 +16,7 @@ describe('buildResourceFilePath', () => {
     const region = 'augsburg'
     const hash = sha256(urlString)
     expect(buildResourceFilePath(urlString, region, hash)).toBe(
-      'path/to/documentDir/resource-cache/v3/augsburg/files/c7e63b8dcf9e58e00e07ce99959384769c626aa25a78cce33a3863023155194f',
+      'path/to/documentDir/resource-cache/v4/augsburg/files/c7e63b8dcf9e58e00e07ce99959384769c626aa25a78cce33a3863023155194f',
     )
   })
   it('should throw if url is invalid', () => {
