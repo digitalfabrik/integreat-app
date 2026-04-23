6e5ff5d9d7c027bafe2dfe69874e5f82b572c23a
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-04-23 09:08:05 +0200

    4045: Bump database connector versions

--- native/src/utils/DatabaseConnector.ts
+++ native/src/utils/DatabaseConnector.ts
@@ -27,8 +27,8 @@ import { RegionResourceCacheStateType } from './DataContainer'
 import { deleteIfExists } from './helpers'
 import { log, reportError } from './sentry'
 
-export const CONTENT_VERSION = 'v11'
-export const RESOURCE_CACHE_VERSION = 'v3'
+export const CONTENT_VERSION = 'v12'
+export const RESOURCE_CACHE_VERSION = 'v4'
 
 // Our pdf view can only load from DocumentDir. Therefore we need to use that
 export const CACHE_DIR_PATH = BlobUtil.fs.dirs.DocumentDir
