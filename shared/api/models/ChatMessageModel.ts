f4da297068da68e0b8e8ce63537fa63c5ba78b78
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-05-21 13:47:05 +0200

    4129: Allow (de)serializing of chat messages

--- shared/api/models/ChatMessageModel.ts
+++ shared/api/models/ChatMessageModel.ts
@@ -1,11 +1,21 @@
 import { DateTime } from 'luxon'
 
+export type SerializedChatMessage = {
+  id: number
+  content: string
+  created: string
+  userIsAuthor: boolean
+  automaticAnswer: boolean
+  synced: boolean
+}
+
 class ChatMessageModel {
   _id: number
   _content: string
   _created: DateTime
   _userIsAuthor: boolean
   _automaticAnswer: boolean
+  _synced: boolean
 
   constructor(params: {
     id: number
@@ -13,13 +23,15 @@ class ChatMessageModel {
     created: DateTime
     userIsAuthor: boolean
     automaticAnswer: boolean
+    synced?: boolean
   }) {
-    const { id, content, created, userIsAuthor, automaticAnswer } = params
+    const { id, content, created, userIsAuthor, automaticAnswer, synced = true } = params
     this._id = id
     this._content = content
     this._created = created
     this._userIsAuthor = userIsAuthor
     this._automaticAnswer = automaticAnswer
+    this._synced = synced
   }
 
   get id(): number {
@@ -41,6 +53,33 @@ class ChatMessageModel {
   get isAutomaticAnswer(): boolean {
     return this._automaticAnswer
   }
+
+  get synced(): boolean {
+    return this._synced
+  }
+
+  serialize = (): SerializedChatMessage => ChatMessageModel.serialize(this)
+
+  static serialize = (message: ChatMessageModel): SerializedChatMessage => ({
+    id: message.id,
+    content: message.content,
+    created: message.created.toISO(),
+    userIsAuthor: message.userIsAuthor,
+    automaticAnswer: message.isAutomaticAnswer,
+    synced: message.synced,
+  })
+
+  static deserialize = (json: SerializedChatMessage): ChatMessageModel =>
+    new ChatMessageModel({ ...json, created: DateTime.fromISO(json.created) })
+
+  static unsyncedMessage = (content: string): SerializedChatMessage => ({
+    id: DateTime.now().toMillis(),
+    content,
+    created: DateTime.now().toISO(),
+    userIsAuthor: true,
+    automaticAnswer: false,
+    synced: false,
+  })
 }
 
 export default ChatMessageModel
