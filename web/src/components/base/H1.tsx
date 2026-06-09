96b88b9734847885802876999f807215eae595c9
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-06-09 13:21:56 +0200

    2537: Left align title

--- web/src/components/base/H1.tsx
+++ web/src/components/base/H1.tsx
@@ -6,7 +6,7 @@ import useDimensions from '../../hooks/useDimensions'
 const H1 = ({ children, ...props }: TypographyProps): ReactElement => {
   const { mobile } = useDimensions()
   return (
-    <Typography variant='h1' marginBlock={mobile ? 1 : 3} {...props}>
+    <Typography variant='h1' marginBlock={mobile ? 2 : 3} {...props}>
       {children}
     </Typography>
   )
