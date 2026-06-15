2b2a1b44d11080498d1dd3d83c678370e61b1aca
bahaa <bahaa.tuffaha@yahoo.com>
2026-06-15 13:14:54 +0300

    4127: Removed the second wrapper at searchInput

--- native/src/components/SearchInput.tsx
+++ native/src/components/SearchInput.tsx
@@ -10,13 +10,7 @@ import Text from './base/Text'
 const InputWrapper = styled.View`
   margin: 0 4px;
   flex-grow: 1;
-`
-
-const Wrapper = styled.View<{ space: boolean }>`
-  flex-direction: row;
-  ${props => (props.space ? 'margin: 50px 0;' : '')}
   justify-content: center;
-  align-items: center;
   padding: 10px 0;
   background-color: ${props => props.theme.colors.background};
 `
@@ -26,60 +20,50 @@ type SearchInputProps = {
   value: string
   setValue: (value: string) => void
   description?: string
-  spaceSearch?: boolean
   style?: StyleProp<ViewStyle>
 }
 
-const SearchInput = ({
-  placeholderText,
-  value,
-  setValue,
-  spaceSearch = false,
-  description,
-  style,
-}: SearchInputProps): ReactElement => {
+const SearchInput = ({ placeholderText, value, setValue, description, style }: SearchInputProps): ReactElement => {
   const { t } = useTranslation('search')
   const theme = useTheme()
 
   return (
     <View style={style}>
-      <Wrapper space={spaceSearch}>
-        <InputWrapper>
-          <TextInput
-            {...testID('Search-Input')}
-            multiline={false}
-            autoFocus
-            onBlur={Keyboard.dismiss}
-            placeholderTextColor={theme.colors.onSurfaceVariant}
-            placeholder={placeholderText}
-            aria-label={placeholderText}
-            value={value}
-            onChangeText={setValue}
-            role='searchbox'
-            mode='outlined'
-            outlineStyle={{ borderRadius: 24 }}
-            style={{ height: 48, backgroundColor: theme.colors.surfaceVariant }}
-            textColor={theme.colors.onSurface}
-            right={
-              value ? (
-                <TextInput.Icon
-                  icon='close'
-                  onPress={() => setValue('')}
-                  accessibilityLabel={t('delete')}
-                  color={theme.colors.onSurfaceVariant}
-                />
-              ) : (
-                <TextInput.Icon
-                  icon='magnify'
-                  accessible={false}
-                  focusable={false}
-                  color={theme.colors.onSurfaceVariant}
-                />
-              )
-            }
-          />
-        </InputWrapper>
-      </Wrapper>
+      <InputWrapper>
+        <TextInput
+          {...testID('Search-Input')}
+          multiline={false}
+          autoFocus
+          onBlur={Keyboard.dismiss}
+          placeholderTextColor={theme.colors.onSurfaceVariant}
+          placeholder={placeholderText}
+          aria-label={placeholderText}
+          value={value}
+          onChangeText={setValue}
+          role='searchbox'
+          mode='outlined'
+          outlineStyle={{ borderRadius: 24 }}
+          style={{ height: 48, backgroundColor: theme.colors.surfaceVariant }}
+          textColor={theme.colors.onSurface}
+          right={
+            value ? (
+              <TextInput.Icon
+                icon='close'
+                onPress={() => setValue('')}
+                accessibilityLabel={t('delete')}
+                color={theme.colors.onSurfaceVariant}
+              />
+            ) : (
+              <TextInput.Icon
+                icon='magnify'
+                accessible={false}
+                focusable={false}
+                color={theme.colors.onSurfaceVariant}
+              />
+            )
+          }
+        />
+      </InputWrapper>
       {!!description && (
         <Text variant='body3' style={{ paddingLeft: 28 }}>
           {description}
