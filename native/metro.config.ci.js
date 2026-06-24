8d2a3892e7995b0a143228e6e72a253afd128a1c
André Wachter <andre.wachter@tuerantuer.org>
2026-06-24 23:32:23 +0200

    4166: Improve metro config files

--- native/metro.config.ci.js
+++ native/metro.config.ci.js
@@ -2,9 +2,10 @@ import defaultConfig from './metro.config.js'
 
 export default {
   ...defaultConfig,
-  maxWorkers: process.env.TOTAL_CPUS || 1 /* CircleCI: The issue with this on Dockerized services such as CircleCI is
-                                              that the container thinks it has access to all of the hosts cores,
-                                              although its being limited down to 4 by another process.
-                                              https://github.com/facebook/create-react-app/issues/4870 */,
+  // CircleCI: The issue with this on Dockerized services such as CircleCI is
+  // that the container thinks it has access to all of the hosts cores,
+  // although it is being limited down to 4 by another process.
+  // https://github.com/facebook/create-react-app/issues/4870
+  maxWorkers: process.env.TOTAL_CPUS || 1,
   cacheStores: [],
 }
