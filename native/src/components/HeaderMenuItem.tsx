2cebc3502393232de80a658f8f467e306f20058c
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-06-29 13:38:20 +0200

    4215: Remove redundant styling and allow disabling HeaderMenuItem

--- native/src/components/HeaderMenuItem.tsx
+++ native/src/components/HeaderMenuItem.tsx
@@ -1,44 +1,25 @@
 import React, { ReactElement } from 'react'
-import { StyleSheet } from 'react-native'
 import { Menu } from 'react-native-paper'
-import { useTheme } from 'styled-components/native'
-
-import { contentAlignmentRTLText } from '../constants/contentDirection'
-
-const styles = StyleSheet.create({
-  menuItemContent: {
-    flex: 1,
-  },
-  menuItemTitle: {
-    paddingRight: 8,
-  },
-})
 
 type HeaderMenuItemProps = {
   title: string
   onPress: () => void
   closeMenu: () => void
   icon: string
+  disabled?: boolean
 }
 
-const HeaderMenuItem = ({ onPress, closeMenu, title, icon }: HeaderMenuItemProps): ReactElement => {
-  const theme = useTheme()
-  return (
-    <Menu.Item
-      accessibilityLabel={title}
-      leadingIcon={icon}
-      title={title}
-      onPress={() => {
-        closeMenu()
-        onPress()
-      }}
-      style={{
-        backgroundColor: theme.dark ? theme.colors.surfaceVariant : theme.colors.surface,
-      }}
-      contentStyle={styles.menuItemContent}
-      titleStyle={[styles.menuItemTitle, { color: theme.colors.onSurface, textAlign: contentAlignmentRTLText(title) }]}
-    />
-  )
-}
+const HeaderMenuItem = ({ onPress, closeMenu, title, icon, disabled }: HeaderMenuItemProps): ReactElement => (
+  <Menu.Item
+    accessibilityLabel={title}
+    leadingIcon={icon}
+    disabled={disabled}
+    title={title}
+    onPress={() => {
+      closeMenu()
+      onPress()
+    }}
+  />
+)
 
 export default HeaderMenuItem
