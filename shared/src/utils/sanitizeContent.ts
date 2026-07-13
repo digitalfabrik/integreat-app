d8c95cf38d4d40ff5be82b682e0f671c167bc293
Alekbarova Aizhan <77638520+lunars97@users.noreply.github.com>
2026-07-13 12:04:31 +0200

    4199: Fix unsanitized content (#4217)
    * 4199: Fix unsanitized content
    
    * 4199: Use common shared sanitizer
    
    * 4199: Remove redundant useMemo hook
    
    * 4199: Extend sanitizer config
    
    * 4199: Refine
    
    * 4199: Delete empty files
    
    * 4199:Add allowedIframeDomains

---
+++ shared/src/utils/sanitizeContent.ts
@@ -0,0 +1,20 @@
+import sanitizeHtml from 'sanitize-html'
+
+export const sanitizeContent = (content: string, options: { supportedIframeSources: string[] }): string => {
+  if (!content) {
+    return ''
+  }
+
+  const { supportedIframeSources } = options
+
+  return sanitizeHtml(content, {
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['iframe', 'img', 'details', 'summary']),
+    allowedAttributes: {
+      ...sanitizeHtml.defaults.allowedAttributes,
+      iframe: ['src', 'title', 'width', 'height', 'allow', 'allowfullscreen', 'loading', 'style'],
+      img: ['src', 'srcset', 'alt', 'title', 'width', 'height', 'loading', 'style'],
+      '*': ['class', 'style', 'dir', 'aria-*', 'data-*'],
+    },
+    allowedIframeDomains: supportedIframeSources,
+  })
+}
