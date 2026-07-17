a1eea8cfb853e8acda81634cd4c9b6bfa091a368
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-07-17 10:41:01 +0200

    4233: Remove redundant dependencies

--- native/babel.config.js
+++ native/babel.config.js
@@ -1,8 +1,4 @@
 export default {
   presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
-  plugins: [
-    '@babel/plugin-transform-export-namespace-from',
-    'react-native-paper/babel',
-    'react-native-worklets/plugin',
-  ],
+  plugins: ['react-native-paper/babel', 'react-native-worklets/plugin'],
 }
