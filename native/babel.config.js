8ae7817ffa601828866afd468da25298cb2f4dfc
bahaa <bahaa.tuffaha@yahoo.com>
2026-07-31 19:33:49 +0300

    4233: Upgraded more dependencies for native and web

--- native/babel.config.js
+++ native/babel.config.js
@@ -1,8 +1,6 @@
 export default {
   presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
   plugins: [
-    // Required for ESM-only dependencies using `export * as ns from '...'` (e.g. htmlparser2),
-    // which @react-native/babel-preset does not transform.
     '@babel/plugin-transform-export-namespace-from',
     'react-native-paper/babel',
     'react-native-worklets/plugin',
