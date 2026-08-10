b476b53f5138c6db63d2b6ec368b276fd63c433e
lunars97 <alekbarova.aizhana@gmail.com>
2026-08-10 22:26:12 +0200

    4064: Fix bottom height for tts and hide chat button when tts visible

--- native/src/BottomTabNavigator.tsx
+++ native/src/BottomTabNavigator.tsx
@@ -33,6 +33,7 @@ import useLoadRegionContent from './hooks/useLoadRegionContent'
 import useNavigate from './hooks/useNavigate'
 import useRegionAppContext from './hooks/useRegionAppContext'
 import useSetRouteTitle from './hooks/useSetRouteTitle'
+import useTtsPlayer from './hooks/useTtsPlayer'
 import CategoriesContainer from './routes/CategoriesContainer'
 import EventsContainer from './routes/EventsContainer'
 import LoadingErrorHandler from './routes/LoadingErrorHandler'
@@ -100,6 +101,7 @@ type BottomTabNavigatorProps = {
 const BottomTabNavigator = ({ route, navigation }: BottomTabNavigatorProps): ReactElement | null => {
   const { t } = useTranslation('layout')
   const { regionCode, languageCode } = useRegionAppContext()
+  const { visible } = useTtsPlayer()
   const { navigateTo } = useNavigate()
   const insets = useSafeAreaInsets()
   const { data, loading, error, refresh } = useLoadRegionContent({ regionCode, languageCode })
@@ -131,7 +133,7 @@ const BottomTabNavigator = ({ route, navigation }: BottomTabNavigatorProps): Rea
   }
 
   const { eventsEnabled, placesEnabled, localNewsEnabled, tuNewsEnabled, chatEnabled } = cachedData.region
-  const chatVisible = buildConfig().featureFlags.chat && chatEnabled && activeTab !== PLACES_TAB_ROUTE
+  const chatVisible = buildConfig().featureFlags.chat && chatEnabled && activeTab !== PLACES_TAB_ROUTE && !visible
 
   const Tabs = [
     <Tab.Screen
