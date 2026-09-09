0165412ca78c8d81d9adc81d1b2d97f8294e28e7
lunars97 <alekbarova.aizhana@gmail.com>
2026-09-09 10:52:09 +0200

    4347: Optimize events list loading time

--- web/src/components/EventList.tsx
+++ web/src/components/EventList.tsx
@@ -1,6 +1,11 @@
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
 import Stack from '@mui/material/Stack'
-import React, { ReactElement } from 'react'
+import { styled } from '@mui/material/styles'
+import { DateTime } from 'luxon'
+import React, { ReactElement, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
+import { GroupedVirtuoso, Virtuoso } from 'react-virtuoso'
 
 import { groupEventsByDate, eventGroupTitle } from 'shared'
 import { EventModel } from 'shared/api'
@@ -9,26 +14,73 @@ import EventListItem from '../components/EventListItem'
 import EventsDateFilter from '../components/EventsDateFilter'
 import useDateFilter from '../hooks/useDateFilter'
 import useDimensions from '../hooks/useDimensions'
-import { withDividers } from '../utils'
 import List, { StickyListSubheader } from './base/List'
 
-type EventListGroupProps = {
-  title: string
-  events: EventModel[]
+const Container = styled(Stack)(() => ({
+  height: '90%',
+}))
+
+const EventsListWrapper = styled(Box)(() => ({
+  flex: 1,
+  minHeight: 0,
+}))
+
+type FilteredEventListContext = {
   languageCode: string
+  filterStartDate: DateTime | null
+  filterEndDate: DateTime | null
 }
 
-const EventListGroup = ({ title, events, languageCode }: EventListGroupProps): ReactElement => {
-  const { stickyTop } = useDimensions()
+type GroupedEventListContext = {
+  languageCode: string
+  flatEvents: EventModel[]
+  groupTitles: string[]
+  stickyTop: number
+}
+
+const renderFilteredEventListItem = (
+  index: number,
+  event: EventModel,
+  { languageCode, filterStartDate, filterEndDate }: FilteredEventListContext,
+) => (
+  <>
+    <EventListItem
+      event={event}
+      languageCode={languageCode}
+      filterStartDate={filterStartDate}
+      filterEndDate={filterEndDate}
+    />
+    <Divider />
+  </>
+)
+
+const renderGroupedEventListItem = (
+  index: number,
+  _groupIndex: number,
+  _data: EventModel,
+  { languageCode, flatEvents }: GroupedEventListContext,
+) => {
+  const event = flatEvents[index]
+  if (!event) {
+    return []
+  }
   return (
-    <Stack sx={{ paddingBlock: 1 }}>
-      <StickyListSubheader component='h2' stickyTop={stickyTop}>
-        {title}
-      </StickyListSubheader>
-      {withDividers(events.map(event => <EventListItem event={event} languageCode={languageCode} key={event.path} />))}
-    </Stack>
+    <>
+      <EventListItem event={event} languageCode={languageCode} />
+      <Divider />
+    </>
   )
 }
+
+const renderGroupTitles = (index: number, { groupTitles, stickyTop }: GroupedEventListContext) => (
+  <StickyListSubheader component='h2' stickyTop={stickyTop}>
+    {groupTitles[index]}
+  </StickyListSubheader>
+)
+
+const computeItemKey = (_index: number, event: EventModel) => event.path
+const computeGroupedItemKey = (index: number, _item: EventModel, { flatEvents }: GroupedEventListContext): string =>
+  flatEvents[index]?.path ?? String(index)
 
 type EventListProps = {
   events: EventModel[]
@@ -39,6 +91,19 @@ const EventList = ({ events, languageCode }: EventListProps): ReactElement | nul
   const { t } = useTranslation()
   const { startDate, setStartDate, endDate, setEndDate, filteredEvents, startDateError, resetDates } =
     useDateFilter(events)
+  const { stickyTop } = useDimensions()
+
+  const { flatEvents, groupCounts, groupTitles } = useMemo(() => {
+    const grouped = groupEventsByDate(events)
+    return {
+      flatEvents: grouped.flatMap(([, groupEvents]) => groupEvents),
+      groupCounts: grouped.map(([, groupEvents]) => groupEvents.length),
+      groupTitles: grouped.map(([key]) => {
+        const [titleKey, params] = eventGroupTitle(key)
+        return t($ => $.events[titleKey], params)
+      }),
+    }
+  }, [events, t])
 
   const dateFilter = (
     <EventsDateFilter
@@ -51,43 +116,61 @@ const EventList = ({ events, languageCode }: EventListProps): ReactElement | nul
     />
   )
 
-  if (startDate || endDate) {
+  const isFiltering = Boolean(startDate || endDate)
+  const isEmpty = isFiltering ? filteredEvents.length === 0 : groupCounts.length === 0
+
+  if (isEmpty) {
     return (
       <>
         {dateFilter}
-        <List
-          items={filteredEvents.map(event => (
-            <EventListItem
-              event={event}
-              languageCode={languageCode}
-              key={event.path}
-              filterStartDate={startDate}
-              filterEndDate={endDate}
-            />
-          ))}
-          noItemsMessage={t($ => $.events.currentlyNoEvents)}
-        />
+        <List items={[]} noItemsMessage={t($ => $.events.currentlyNoEvents)} />
       </>
     )
   }
 
-  const dateGroups = groupEventsByDate(events).map(([key, events]) => {
-    const [titleKey, params] = eventGroupTitle(key)
+  const virtuosoStyle = {
+    height: '100%',
+    scrollbarWidth: 'none' as const,
+    msOverflowStyle: 'none' as const,
+  }
+
+  if (isFiltering) {
     return (
-      <EventListGroup
-        key={key}
-        title={t($ => $.events[titleKey], params)}
-        events={events}
-        languageCode={languageCode}
-      />
+      <Container>
+        {dateFilter}
+        <EventsListWrapper>
+          <Virtuoso
+            key='filteredEvents'
+            style={virtuosoStyle}
+            data={filteredEvents}
+            context={{
+              languageCode,
+              filterStartDate: startDate,
+              filterEndDate: endDate,
+            }}
+            computeItemKey={computeItemKey}
+            itemContent={renderFilteredEventListItem}
+          />
+        </EventsListWrapper>
+      </Container>
     )
-  })
+  }
 
   return (
-    <>
+    <Container>
       {dateFilter}
-      <List items={dateGroups} noItemsMessage={t($ => $.events.currentlyNoEvents)} showDividers={false} />
-    </>
+      <EventsListWrapper>
+        <GroupedVirtuoso
+          key='groupedEvents'
+          style={virtuosoStyle}
+          groupCounts={groupCounts}
+          context={{ languageCode, flatEvents, groupTitles, stickyTop }}
+          computeItemKey={computeGroupedItemKey}
+          groupContent={renderGroupTitles}
+          itemContent={renderGroupedEventListItem}
+        />
+      </EventsListWrapper>
+    </Container>
   )
 }
 
