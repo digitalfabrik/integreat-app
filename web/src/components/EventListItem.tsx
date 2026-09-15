a61a094804801c7cbb12e242311df7410c2d8fa2
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-09-15 13:55:51 +0200

    4347: Memoize EventListItem and event groups

--- web/src/components/EventListItem.tsx
+++ web/src/components/EventListItem.tsx
@@ -9,7 +9,7 @@ import Tooltip from '@mui/material/Tooltip'
 import Typography, { TypographyProps } from '@mui/material/Typography'
 import { styled, useTheme } from '@mui/material/styles'
 import { DateTime } from 'luxon'
-import React, { ReactElement } from 'react'
+import React, { memo, ReactElement } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { getExcerpt } from 'shared'
@@ -121,4 +121,4 @@ const EventListItem = ({
   )
 }
 
-export default EventListItem
+export default memo(EventListItem)
