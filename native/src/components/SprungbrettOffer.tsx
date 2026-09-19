4687ad34fe507454832ff5762bb103d20318b2c0
lunars97 <alekbarova.aizhana@gmail.com>
2026-09-19 11:52:08 +0200

    4360: Fix merge conflict

--- native/src/components/SprungbrettOffer.tsx
+++ native/src/components/SprungbrettOffer.tsx
@@ -23,9 +23,7 @@ const SprungbrettOffer = ({ sprungbrettOffer, languageCode }: SprungbrettOfferPr
 
   return (
     <LoadingErrorHandler error={response.error} loading={response.loading} refresh={response.refresh}>
-      {data && (
-        <List items={data} renderItem={renderListItem} noItemsMessage={t($ => $.sprungbrett.noOffersAvailable)} />
-      )}
+      {data && <List items={data} renderItem={renderListItem} />}
     </LoadingErrorHandler>
   )
 }
